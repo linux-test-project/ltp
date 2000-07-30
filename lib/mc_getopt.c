8bb5ea6ba78107b1ab2d08ba357cb4bf96b9da50
alaffin <alaffin>
2000-07-30 19:34:14 +0000

    Updated the mc_getopt routine files with GPL headers.  Oops, we missed a couple.

--- lib/mc_getopt.c
+++ lib/mc_getopt.c
@@ -1,13 +1,40 @@
-/* $Header: /cvsroot/ltp/ltp/lib/Attic/mc_getopt.c,v 1.1 2000/07/27 17:13:18 alaffin Exp $ */
-
 /*
- *	(C) COPYRIGHT CRAY RESEARCH, INC.
- *	UNPUBLISHED PROPRIETARY INFORMATION.
- *	ALL RIGHTS RESERVED.
+ * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * Further, this software is distributed without any warranty that it is
+ * free of the rightful claim of any third person regarding infringement
+ * or the like.  Any license provided herein, whether implied or
+ * otherwise, applies only to this software file.  Patent licenses, if
+ * any, provided herein do not apply to combinations of this program with
+ * other software, or any other product whatsoever.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ * 
+ * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
+ * Mountain View, CA  94043, or:
+ * 
+ * http://www.sgi.com 
+ * 
+ * For further information regarding this notice, see: 
+ * 
+ * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
  */
+
+/* $Id: mc_getopt.c,v 1.2 2000/07/30 19:34:14 alaffin Exp $ */
+
 /**********************************************************
  * 
- *    OS Testing - Cray Research, Inc.
+ *    OS Testing - Silicon Graphics, Inc.
  * 
  *    FUNCTION NAME 	: mc_getopt and mc_getoptv
  * 
