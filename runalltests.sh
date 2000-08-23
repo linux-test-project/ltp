ad4c5fab00de62d68716452285736c6e06b982b7
alaffin <alaffin>
2000-08-23 21:09:27 +0000

    Update runalltests to correctly report PASS/FAIL of the driver.

--- runalltests.sh
+++ runalltests.sh
@@ -10,10 +10,10 @@ export PATH="${PATH}:${LTPROOT}/doio:${LTPROOT}/tests"
 
 python ${LTPROOT}/runtest/runtests.py ${LTPROOT}/runtest/quickhit ${LTPROOT}/runtest/fs
 
-rm -rf /tmp/runalltests-$$
-
 if [ $? -eq "0" ]; then
   echo runtests reported PASS
 else
   echo runtests reported FAIL
 fi
+
+rm -rf /tmp/runalltests-$$
