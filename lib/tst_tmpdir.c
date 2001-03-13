ffe35e2dbaae0d74690fb6f0d8e078454c4bac20
nstraz <nstraz>
2001-03-13 16:33:54 +0000

    I've "modernized" the temporary directory handling.  We now use mkdtemp() to create temporary directories.  This is the glibc recommended way.

--- lib/tst_tmpdir.c
+++ lib/tst_tmpdir.c
@@ -30,7 +30,7 @@
  * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
  */
 
-/* $Id: tst_tmpdir.c,v 1.1 2000/07/27 17:13:19 alaffin Exp $ */
+/* $Id: tst_tmpdir.c,v 1.2 2001/03/13 16:33:54 nstraz Exp $ */
 
 /**********************************************************
  *
@@ -119,81 +119,83 @@ extern char *TESTDIR;         /* the directory created; defined in */
 void
 tst_tmpdir()
 {
-   char prefix[PREFIX_SIZE];     /* first three characters from TCID */
-   int  no_cleanup = 0;          /* !0 means TDIRECTORY env var was set */
+ 	char template[PATH_MAX];      /* template for mkstemp, mkdtemp */
+  	int  no_cleanup = 0;          /* !0 means TDIRECTORY env var was set */
+	char *env_tmpdir;            /* temporary storage for TMPDIR env var */
 
-   /*
-    * If the TDIRECTORY env variable is not set, a temp dir will be
-    * created.
-    */
-   if ( (TESTDIR = getenv(TDIRECTORY)) == NULL ) {
-      /*
-       * Create a unique name based on the first three characters of the
-       * TCID.  The last byte in "prefix" is for the null.
-       */
-      strncpy(prefix, TCID, PREFIX_SIZE - 1);
-      prefix[PREFIX_SIZE-1] = '\0';
-      if ( (TESTDIR = tempnam(TEMPDIR, prefix)) == NULL )
-         tst_brkm(TBROK, tmpdir_cleanup, "%s: tempnam(%s, %s) failed",
-                  FN_NAME, TEMPDIR, prefix);
-
-      /*
-       * Create the temporary directory.
-       */
-      if ( mkdir(TESTDIR, DIR_MODE) == -1 )
-         tst_brkm(TBROK, tmpdir_cleanup,
-                  "%s: mkdir(%s, %#o) failed; errno = %d: %s", 
-                  FN_NAME, TESTDIR, DIR_MODE, errno, strerror(errno));
-
-      /*
-       * Change the group on this temporary directory to be that of the
-       * gid of the person running the tests.
-       */
-      if ( chown(TESTDIR, -1, getgid()) == -1 )
-         tst_brkm(TBROK, tmpdir_cleanup,
-                  "chown(%s, -1, %d) failed; errno = %d: %s", 
-                  TESTDIR, getgid(), errno, strerror(errno));
-   } else {
-      /*
-       * The TDIRECTORY env. variable is set, so no temp dir is created.
-       * Also, no clean up will be done via tst_rmdir().
-       */
-      no_cleanup++;
+   	/*
+	 * If the TDIRECTORY env variable is not set, a temp dir will be
+	 * created.
+	 */
+	if ((TESTDIR = getenv(TDIRECTORY))) {
+		/*
+		 * The TDIRECTORY env. variable is set, so no temp dir is created.
+		 * Also, no clean up will be done via tst_rmdir().
+		 */
+		no_cleanup++;
 #if UNIT_TEST
-      printf("TDIRECTORY env var is set\n");
+		printf("TDIRECTORY env var is set\n");
 #endif
-   }
+	} else {
+		/*
+		 * Create a template for the temporary directory.  Use the 
+		 * environment variable TMPDIR if it is available, otherwise
+		 * use our default TEMPDIR.
+		 */
+		if ((env_tmpdir = getenv("TMPDIR"))) {
+			snprintf(template, PATH_MAX, "%s/%.3sXXXXXX", env_tmpdir, TCID);
+		} else {
+			snprintf(template, PATH_MAX, "%s/%.3sXXXXXX", TEMPDIR, TCID);
+		}
+		
+		/*
+		 * Make the temporary directory in one shot using mkdtemp()
+		 */
+		if ( (TESTDIR = mkdtemp(template)) == NULL )
+			tst_brkm(TBROK, tmpdir_cleanup, 
+							"%s: mkdtemp(%s) failed; errno = %d: %s",
+						   	FN_NAME, template, errno, strerror(errno));
 
-#if UNIT_TEST
-      printf("TESTDIR = %s\n", TESTDIR);
-#endif
-
-   /*
-    * Change to the temporary directory.  If the chdir() fails, issue
-    * TBROK messages for all test cases, attempt to remove the
-    * directory (if it was created), and exit.  If the removal also
-    * fails, also issue a TWARN message.   
-    */
-   if ( chdir(TESTDIR) == -1 ) {
-      tst_brkm(TBROK, NULL, "%s: chdir(%s) failed; errno = %d: %s",
-               FN_NAME, TESTDIR, errno, strerror(errno) );
-
-      /* Try to remove the directory */
-      if ( !no_cleanup && rmdir(TESTDIR) == -1 )
-         tst_resm(TWARN, "%s: rmdir(%s) failed; errno = %d: %s",
-                  FN_NAME, TESTDIR, errno, strerror(errno) );
-
-      tmpdir_cleanup();
-   }
+		/*
+		 * Change the group on this temporary directory to be that of the
+		 * gid of the person running the tests.
+		 */
+		if ( chown(TESTDIR, -1, getgid()) == -1 )
+			tst_brkm(TBROK, tmpdir_cleanup,
+						  	"chown(%s, -1, %d) failed; errno = %d: %s", 
+							TESTDIR, getgid(), errno, strerror(errno));
+ 	}
 
 #if UNIT_TEST
-   printf("CWD is %s\n", getcwd((char *)NULL, PATH_MAX));
+	printf("TESTDIR = %s\n", TESTDIR);
 #endif
 
-   /*
-    *  If we made through all this stuff, return.
-    */
-   return;
+ 	/*
+  	 * Change to the temporary directory.  If the chdir() fails, issue
+   	 * TBROK messages for all test cases, attempt to remove the
+	 * directory (if it was created), and exit.  If the removal also
+	 * fails, also issue a TWARN message.   
+	 */
+	if ( chdir(TESTDIR) == -1 ) {
+		tst_brkm(TBROK, NULL, "%s: chdir(%s) failed; errno = %d: %s",
+				  		FN_NAME, TESTDIR, errno, strerror(errno) );
+
+		/* Try to remove the directory */
+		if ( !no_cleanup && rmdir(TESTDIR) == -1 )
+			tst_resm(TWARN, "%s: rmdir(%s) failed; errno = %d: %s",
+							FN_NAME, TESTDIR, errno, strerror(errno) );
+
+		tmpdir_cleanup();
+	}
+	
+#if UNIT_TEST
+	printf("CWD is %s\n", getcwd((char *)NULL, PATH_MAX));
+#endif
+
+	/*
+	 *  If we made through all this stuff, return.
+	 */
+	return;
 }  /* tst_tmpdir() */
 
 
