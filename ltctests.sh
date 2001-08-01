9c9c9f08b8ca1cdcc55d0835dc5e8f0766a6eada
plars <plars>
2001-08-01 17:21:58 +0000

    added new tests to runscripts

--- ltctests.sh
+++ ltctests.sh
@@ -10,7 +10,7 @@ RT=${LTPROOT}/ltctests/runtest
 
 export PATH="${PATH}:${LTPROOT}/doio:${LTPROOT}/tests:${LTPROOT}/ltctests/bin"
 export TCtmp=${TMP}
-cat ${RT}/pth_str ${RT}/mem ${RT}/files ${RT}/fs ${RT}/ade > ${TMP}/ltctests
+cat ${RT}/ipc ${RT}/pth_str ${RT}/mem ${RT}/files ${RT}/fs ${RT}/ade > ${TMP}/ltctests
 
 ${LTPROOT}/pan/pan -e -S -a $$ -n $$ -f ${TMP}/ltctests -l /tmp/ltc.log
 if [ $? -eq "0" ]; then
