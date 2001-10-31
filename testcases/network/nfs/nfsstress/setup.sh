0017c3ba69a80e8f33297ac715f0a8e2d7f2a397
iyermanoj <iyermanoj>
2001-10-31 20:17:03 +0000

    Changed

--- testcases/network/nfs/nfsstress/setup.sh
+++ testcases/network/nfs/nfsstress/setup.sh
@@ -4,6 +4,7 @@
 # History:	Oct - 17 - 2001	Created - Manoj Iyer, IBM Austin TX.           #
 #				          email: manjo@austin.ibm.com          #
 #                                                                              #
+#		Oct - 31 - 2001 Modified.				       #
 # File:		setup.sh		                                       #
 #                                                                              #
 # Description:	This script sets up the NFS directories in the remote machine  #
@@ -14,33 +15,64 @@
 #		for more info on how to.	                               #
 ################################################################################
 
+echo -n "Enter the fully qualified name of a remote machine:"
+read RHOST
+echo -n "Enter the absolute path of a remote directory:"
+read TESTDIR
+echo -n "Enter the name of a local dir to be created:"
+read LOCALDIR
+
+echo "You have entered remote host: $RHOST, remote directory: $TESTDIR, local test directory: $LOCALDIR"
+
+
+#Create the data directory on remote host.
+rsh -n $RHOST "mkdir $TESTDIR" 2>&1 1>/dev/null
+if [ $? -ne 0 ]
+then
+	echo "Could not export dir from $RHOST"
+	exit 1
+else
+	echo "create directory $TESTDIR on remote machine $RHOST ... done"
+fi
+
+
 #Export the data directory on RHOST
-rsh -n $RHOST "/usr/sbin/exportfs -i :$TESTDIR -o rw,no_root_squash "
-[[ $? -eq 0 ]] || echo "Could not export dir from $RHOST"
+rsh -n $RHOST "/usr/sbin/exportfs -i :$TESTDIR -o rw,no_root_squash " 2>&1 1>/dev/null
+if [ $? -ne 0 ]
+then
+	echo "Could not export dir from $RHOST"
+	exit 1
+else
+	echo "export data directory $TESTDIR on $RHOST ... done."
+fi
 
 #Verify export
-/usr/sbin/showmount -e $RHOST | grep $TESTDIR
-[[ $? -eq 0 ]] || echo "$TESTDIR not exported"
+/usr/sbin/showmount -e $RHOST | grep $TESTDIR 2>&1 1>/dev/null
+if [ $? -ne 0 ]
+then
+	echo "$TESTDIR not exported"
+	exit 1
+else
+	echo "export $TESTDIR on $RHOST ... done"
+fi
 
 #Create $LOCALDIR for mount point
-mkdir $PWD/$LOCALDIR
-[[ $? -eq 0 ]] || echo "Could not create $PWD/$LOCALDIR"
+mkdir $PWD/$LOCALDIR 2>&1 1>/dev/null
+if [ $? -ne 0 ]
+then
+	echo "Could not create $PWD/$LOCALDIR"
+	exit 1
+else
+	echo "create $PWD/$LOCALDIR ... done"
+fi
 
 #Mount $TESTDIR from RHOST.
-mount $RHOST:$TESTDIR $PWD/$LOCALDIR
-
-#Change directory to new directory
-cd $PWD/$LOCALDIR
-
-#Execute the test
-$OLDPWD/make_tree
-
-#Check if test passed, on failure exit with 1
-if [ $? -eq 0 ]
+/bin/mount $RHOST:$TESTDIR $PWD/$LOCALDIR 2>&1 1>/dev/null
+if [ $? -ne 0 ]
 then
-	echo "NFS test make_tree PASSED"
-	exit 0
-else
-	echo "NFS test make_tree FAILED"
+	echo "could not mount $RHOST:$TESTDIR $PWD/$LOCALDIR"
 	exit 1
+else
+	echo "mount $RHOST:$TESTDIR $PWD/$LOCALDIR ... done"
+	echo "cd to $LOCALDIR and run ../make_tree"
 fi
