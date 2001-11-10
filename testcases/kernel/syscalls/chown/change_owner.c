4a220d00ee5f86ac5bca834789498f95f3e593f3
iyermanoj <iyermanoj>
2001-11-10 00:10:46 +0000

    Modified

--- testcases/kernel/syscalls/chown/change_owner.c
+++ testcases/kernel/syscalls/chown/change_owner.c
@@ -53,7 +53,7 @@ main(int argc, char **argv)
 		perror("change_owner: nobody not found in /etc/passwd\n");
 		exit(1);
 	}
-	if ((ltpgroup = getpwnam("nobody")) == NULL) {
+	if ((ltpgroup = getpwnam((const char *)"nobody")) == NULL) {
 		perror("change_owner: nobody's group not found in /etc/group\n");
 		exit(1);
 	}
