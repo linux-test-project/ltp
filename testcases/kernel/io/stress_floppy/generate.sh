52a79d1b58d7f20609ee0e897ac7f0156e6118e2
robbiew <robbiew>
2002-03-12 16:21:25 +0000

    Made modifications per Jay Huie's patch.

--- testcases/kernel/io/stress_floppy/generate.sh
+++ testcases/kernel/io/stress_floppy/generate.sh
@@ -4,6 +4,8 @@ COUNT=0
 FILE=1K_file
 for the_file in `echo 10K_file 100K_file 1000K_file`
 do
+  if [ -e $the_file ]; then continue; fi
+  echo "Creating $the_file"
   while [[ $COUNT -le 10 ]]
   do
     cat $FILE >> $the_file
@@ -13,7 +15,21 @@ do
   FILE=$the_file
 done
 
-mkdir dumpdir
-cp 1K_file dumpdir
-cp 10K_file dumpdir
-cp 100K_file dumpdir
+if [ ! -d dumpdir ]; then
+   mkdir dumpdir
+fi
+
+`diff --brief 1K_file dumpdir/1K_file &> /dev/null`
+if [ $? ]; then
+   cp 1K_file dumpdir
+fi
+
+`diff --brief 10K_file dumpdir/10K_file &> /dev/null`
+if [ $? ]; then
+   cp 10K_file dumpdir
+fi
+
+`diff --brief 100K_file dumpdir/100K_file &> /dev/null`
+if [ $? ]; then
+   cp 100K_file dumpdir
+fi
