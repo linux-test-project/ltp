94d79562e51d9916119ccc26ea89c6b39448334c
plars <plars>
2002-03-26 19:39:42 +0000

    fix to make getsockname01 work without a controlling tty

--- testcases/kernel/syscalls/getsockname/getsockname01.c
+++ testcases/kernel/syscalls/getsockname/getsockname01.c
@@ -42,6 +42,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -167,7 +168,10 @@ setup0(void)
 	if (tdat[testno].experrno == EBADF)
 		s = 400;	/* anything not an open file */
 	else
-		s = 0;		/* open, but not a socket */
+		if((s = open("/dev/null", O_WRONLY)) == -1)
+			tst_brkm(TBROK, cleanup, "error opening /dev/null - "
+			"errno: %s", strerror(errno));
+
 }
 
 void
