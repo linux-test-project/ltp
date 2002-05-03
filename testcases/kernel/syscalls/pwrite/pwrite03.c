b95e362deda7b3733fd92a1d86bad97e9fb2e98a
robbiew <robbiew>
2002-05-03 15:43:15 +0000

    New testcase added by Bull.

---
+++ testcases/kernel/syscalls/pwrite/pwrite03.c
@@ -0,0 +1,214 @@
+/*
+ *   Copyright (C) Bull S.A. 2001
+ *   Copyright (c) International Business Machines  Corp., 2001
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * Test Name: pwrite03
+ *
+ * Test Description:
+ *  Verify that,
+ *      pwrite(2) fails when attempted to write with buf outside accessible 
+ *      address space.
+ *
+ * Expected Result:
+ *      pwrite() should return -1 and set errno to EFAULT.
+ *
+ * Algorithm:
+ *  Setup:
+ *   Setup signal handling.
+ *   Create a temporary directory/file.
+ *
+ *  Test:
+ *   Loop if the proper options are given.
+ *   Execute system call
+ *   Check return code, if system call failed (return=-1)
+ *      if errno set == expected errno
+ *              Issue sys call fails with expected return value and errno.
+ *      Otherwise,
+ *              Issue sys call fails with unexpected errno.
+ *   Otherwise,
+ *      Issue sys call returns unexpected value.
+ *
+ *  Cleanup:
+ *   Print errno log and/or timing stats if options given
+ *   Delete the temporary directory(s)/file(s) created.
+ *
+ * Usage:  <for command-line>
+ *  pwrite03 [-c n] [-e] [-i n] [-I x] [-P x] [-t]
+ *     where,  -c n : Run n copies concurrently.
+ *             -e   : Turn on errno logging.
+ *	       -i n : Execute test n times.
+ *	       -I x : Execute test for x seconds.
+ *	       -P x : Pause for x seconds between iterations.
+ *	       -t   : Turn on syscall timing.
+ *
+ * HISTORY
+ *	04/2002 Ported by André Merlier
+ *
+ * RESTRICTIONS:
+ *  None.
+ */
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include "test.h"
+#include "usctest.h"
+
+#define TEMPFILE	"pwrite_file"
+#define NBUFS           1
+
+char *TCID="pwrite03";		/* Test program identifier.    */
+int TST_TOTAL=1;		/* Total number of test cases. */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+char *write_buf[NBUFS];		/* buffer to hold data to be written */
+int fd1;			/* file descriptor of temporary file */
+
+void setup();			/* Main setup function of test */
+void cleanup();			/* cleanup function for the test */
+void init_buffers();		/* function to initialize/allocate buffers */
+
+int exp_enos[] = {EFAULT,0};
+
+int
+main(int ac, char **av)
+{
+	int lc;			/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+	int i;			/* counter to test different test conditions */
+	size_t nbytes;		/* no. of bytes to be written */
+	off_t offset;		/* offset position in the specified file */
+	char *test_desc;	/* test specific error message */
+	
+	/* Parse standard options given to run the test. */
+	msg = parse_opts(ac, av, (option_t *)NULL, NULL);
+	if (msg != (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* Perform global setup for test */
+	setup(); 
+	
+	TEST_EXP_ENOS(exp_enos);
+	
+	/* Check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+			
+		/* Reset Tst_count in case we are looping. */
+		Tst_count=0;
+
+		test_desc = "EFAULT";
+		nbytes = 1024;
+		offset = 1;
+		write_buf[0]= sbrk(0);
+		
+		/*
+		 * Call pwrite() with the specified file descriptor,
+		 * no. of bytes to be written at specified offset.
+		 * and verify that call should fail with appropriate
+		 * errno. set.
+		 */
+		TEST(pwrite(fd1,write_buf[0] , nbytes, offset));
+						
+		/* Check for the return code of pwrite() */
+		if (TEST_RETURN != -1) {
+			tst_resm(TFAIL, "pwrite() returned %d, "
+					 "expected -1, errno:%d",
+					 TEST_RETURN, exp_enos[0]);
+		}
+
+		TEST_ERROR_LOG(TEST_ERRNO);
+
+		/*
+		* Verify whether expected errno is set.
+		*/
+		if (TEST_ERRNO == exp_enos[0]) {
+			tst_resm(TPASS, "pwrite() fails with expected "
+				"error EFAULT errno:%d",TEST_ERRNO);
+		} else {
+			tst_resm(TFAIL, "pread() fails, %s, unexpected "
+				 "errno:%d, expected:%d\n", test_desc,
+				 TEST_ERRNO, exp_enos[0]);
+		}
+		
+	}	/* End of TEST_LOOPING. */
+
+	/* Call cleanup() to undo setup done for the test. */
+	cleanup();
+	
+	/*NOTREACHED*/
+}	/* End main */
+
+
+/*
+ * setup() - performs all ONE TIME setup for this test.
+ *
+ *  Initialize/allocate write buffer.
+ */
+void 
+setup()
+{
+	
+	/* capture signals */
+	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+	
+	/* make a temp directory and cd to it */
+	tst_tmpdir();
+	
+	/* Creat a temporary file used for mapping */
+	if ((fd1 = open(TEMPFILE, O_RDWR | O_CREAT, 0777)) < 0) {
+		tst_brkm(TBROK, cleanup, "open() on %s Failed, errno=%d : %s",
+			 TEMPFILE, errno, strerror(errno));
+	}
+}
+
+/*
+ * cleanup() - performs all ONE TIME cleanup for this test at
+ *             completion or premature exit.
+ *
+ * Close the temporary file.
+ * Remove the temporary directory created.
+ */
+void 
+cleanup() 
+{
+	
+	/*
+	 * print timing stats if that option was specified.
+	 * print errno log if that option was specified.
+	 */
+	TEST_CLEANUP;
+
+	/* Close the temporary file created in setup2 */
+	if (close(fd1) < 0) {
+		tst_brkm(TBROK, NULL,
+			 "close() on %s Failed, errno=%d : %s",
+			 TEMPFILE, errno, strerror(errno));
+	}
+
+	/* Remove tmp dir and all files in it */
+	tst_rmdir();
+	/* exit with return code appropriate for results */
+	tst_exit();
+}
+
---
+++ testcases/kernel/syscalls/readdir/readdir02.c
@@ -0,0 +1,209 @@
+/*
+ *
+ *   Copyright (C) Bull S.A. 2001
+ *   Copyright (c) International Business Machines  Corp., 2001
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ *      readdir02.c
+ *
+ * DESCRIPTION
+ *      readdir02: try to readdir with Invalid directory stream descriptor dir.
+ *
+ * CALLS
+ *      readdir(3)
+ *
+ * ALGORITHM
+ *      loop if that option was specified
+ *      call readdir() with an invalid file descriptor
+ *      check the errno value
+ *        issue a PASS message if we get EBADF - errno 9
+ *      otherwise, the tests fails
+ *        issue a FAIL message
+ *        call cleanup
+ *
+ * USAGE:  <for command-line>
+ *  readdir02 [-c n] [-e] [-i n] [-I x] [-P x] [-t]
+ *     where,  -c n : Run n copies concurrently.
+ *             -e   : Turn on errno logging.
+ *             -i n : Execute test n times.
+ *             -I x : Execute test for x seconds.
+ *             -P x : Pause for x seconds between iterations.
+ *             -t   : Turn on syscall timing.
+ *
+ * NOTE
+ *	The POSIX standard says:
+ *	  The readdir() function may fail if:
+ *	  [EBADF] The dirp argument does not refer to an open directory stream.	
+ *	  (Note that readdir() is not _required_ to fail in this case.)
+ *
+ * HISTORY
+ *      04/2002 - Written by Jacky Malcles
+ *
+ * RESTRICTIONS
+ *      none
+ */
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <signal.h>
+ /* test.h and usctest.h are the two header files that are required by the
+  * quickhit package.  They contain function and macro declarations which you
+  * can use in your test programs
+  */
+#include "test.h"
+#include "usctest.h"
+
+void setup(); 
+void cleanup();
+
+char *TCID="readdir02";		/* Test program identifier.    */
+int TST_TOTAL=1;    		/* Total number of test cases. */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+int exp_enos[]={EBADF, 0};
+
+
+
+/***********************************************************************
+ * Main
+ ***********************************************************************/
+int
+main(int ac, char **av)
+{
+    int lc;		/* loop counter */
+    char *msg;		/* message returned from parse_opts */
+    DIR *test_dir;
+    struct dirent *dptr;
+
+
+        /* parse standard options */
+        if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL)) != (char *)NULL){
+                tst_brkm(TBROK, cleanup, "OPTION PARSING ERROR - %s", msg);
+        }
+
+
+    /***************************************************************
+     * perform global setup for test
+     ***************************************************************/
+    setup();
+
+    /* set the expected errnos... */
+    TEST_EXP_ENOS(exp_enos);
+
+    /***************************************************************
+     * check looping state 
+     ***************************************************************/
+    /* TEST_LOOPING() is a macro that will make sure the test continues
+     * looping according to the standard command line args. 
+     */
+    for (lc=0; TEST_LOOPING(lc); lc++) {
+
+	/* reset Tst_count in case we are looping. */
+	Tst_count=0;
+
+        if ((test_dir = opendir(".")) == NULL) {
+           tst_resm(TFAIL, "opendir(\".\") Failed, errno=%d : %s",
+                            errno, strerror(errno));
+        } else { if (closedir(test_dir) < 0) {
+		tst_resm(TFAIL, "closedir(\".\") Failed, errno=%d : %s",
+			    errno, strerror(errno));
+		} else {
+                dptr = readdir(test_dir);
+                TEST_ERRNO = errno;
+                switch(TEST_ERRNO) {
+                case EBADF:
+                        tst_resm(TPASS, "expected failure - errno = %d : %s",
+                                 TEST_ERRNO, strerror(TEST_ERRNO));
+                        break;
+                default:
+			if (dptr != NULL){
+                        tst_brkm(TFAIL, cleanup, "call failed with an "
+                                 "unexpected error - %d : %s", TEST_ERRNO,
+                                 strerror(TEST_ERRNO));
+			} else {
+			tst_resm(TINFO,"readdir() is not _required_ to fail, "
+				"errno = %d  ", TEST_ERRNO);
+			}
+                }
+                }
+
+        }
+
+    }	/* End for TEST_LOOPING */
+
+    /***************************************************************
+     * cleanup and exit
+     ***************************************************************/
+    cleanup();
+
+    return 0;
+}	/* End main */
+
+
+/***************************************************************
+ * setup() - performs all ONE TIME setup for this test.
+ ***************************************************************/
+void 
+setup()
+{
+
+    /* You will want to enable some signal handling so you can capture
+     * unexpected signals like SIGSEGV. 
+     */
+    tst_sig(NOFORK, DEF_HANDLER, cleanup);
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+
+    /* If you are doing any file work, you should use a temporary directory.  We
+     * provide tst_tmpdir() which will create a uniquely named temporary
+     * directory and cd into it.  You can now create directories in the current
+     * directory without worrying.
+     */
+    tst_tmpdir();
+
+}
+
+/***************************************************************
+ * cleanup() - performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ ***************************************************************/
+void 
+cleanup()
+{
+    /*
+     * print timing stats if that option was specified.
+     * print errno log if that option was specified.
+     */
+    TEST_CLEANUP;
+
+    /* If you use a temporary directory, you need to be sure you remove it. Use
+     * tst_rmdir() to do it automatically.  
+     */
+    tst_rmdir();
+
+    /* exit with return code appropriate for results */
+    tst_exit();
+}
