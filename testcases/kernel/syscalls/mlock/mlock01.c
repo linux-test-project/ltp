6c15356b497c3916af52b83149c00e352ffdd5b9
plars <plars>
2002-06-12 21:24:47 +0000

    New tests mlock01 and mlock02

---
+++ testcases/kernel/syscalls/mlock/mlock01.c
@@ -0,0 +1,169 @@
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2002
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ * 	mlock01.c
+ *
+ * DESCRIPTION
+ * 	Test to see that mlock works
+ * 	
+ * ALGORITHM
+ * 	test 1:
+ *		Call mlock with various valid addresses and lengths.  No 
+ *		error should be returned
+ *
+ * USAGE:  <for command-line>
+ *         -c n    Run n copies concurrently
+ *         -e      Turn on errno logging
+ *         -f      Turn off functional testing
+ *         -h      Show this help screen
+ *         -i n    Execute test n times
+ *         -I x    Execute test for x seconds
+ *         -p      Pause for SIGUSR1 before starting
+ *         -P x    Pause for x seconds between iterations
+ *         -t      Turn on syscall timing
+ *
+ * HISTORY
+ *	06/2002 Written by Paul Larson
+ *
+ * RESTRICTIONS
+ * 	None
+ */
+#include <errno.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "test.h"
+#include "usctest.h"
+
+void setup();
+void setup1(int);
+void cleanup();
+
+char *TCID = "mlock01";		/* Test program identifier.    */
+int TST_TOTAL = 4;		/* Total number of test cases. */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+int exp_enos[] = { 0 };
+
+void *addr1;
+
+struct test_case_t {
+	void **addr;
+	int len;
+	void (*setupfunc) ();
+} TC[] = {
+	/* mlock should return ENOMEM when some or all of the address 
+	 * range pointed to by addr and len are not valid mapped pages
+	 * in the address space of the process
+	 */
+	{&addr1, 1, setup1}, 
+	{&addr1, 1024, setup1}, 
+	{&addr1, 1024 * 1024, setup1}, 
+	{&addr1, 1024 * 1024 * 10, setup1}
+};
+
+/***********************************************************************
+ * Main
+ ***********************************************************************/
+int main(int ac, char **av)
+{
+	int lc, i;		/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+
+	if ((msg = parse_opts(ac, av, NULL, NULL)) != (char *) NULL) {
+		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
+		tst_exit();
+	}
+
+    /***************************************************************
+     * perform global setup for test
+     ***************************************************************/
+	setup();
+
+	/* set the expected errnos... */
+	TEST_EXP_ENOS(exp_enos);
+
+    /***************************************************************
+     * check looping state 
+     ***************************************************************/
+	/* TEST_LOOPING() is a macro that will make sure the test continues
+	 * looping according to the standard command line args. 
+	 */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+
+		for (i = 0; i < TST_TOTAL; i++) {
+
+			if (TC[i].setupfunc != NULL)
+				TC[i].setupfunc(TC[i].len);
+
+			TEST(mlock(*(TC[i].addr), TC[i].len));
+
+			/* check return code */
+			if (TEST_RETURN == -1) {
+				TEST_ERROR_LOG(TEST_ERRNO);
+				tst_resm(TFAIL, "mlock(%p, %d) Failed with "
+					 "return=%d, errno=%d : %s",
+					 TC[i].addr, TC[i].len,
+					 TEST_RETURN, TEST_ERRNO,
+					 strerror(TEST_ERRNO));
+			} else {
+				tst_resm(TPASS, "test %d passed length = %d",
+					 i, TC[i].len);
+			}
+		}
+	}			/* End for TEST_LOOPING */
+
+    /***************************************************************
+     * cleanup and exit
+     ***************************************************************/
+	cleanup();
+
+	return 0;
+}				/* End main */
+
+/***************************************************************
+ * setup() - performs all ONE TIME setup for this test.
+ ***************************************************************/
+void setup()
+{
+	TEST_PAUSE;
+}
+
+void setup1(int len)
+{
+	addr1 = (char *) malloc(len);
+	if (addr1 == NULL)
+		tst_brkm(TFAIL, cleanup, "malloc failed");
+}
+
+/***************************************************************
+ * cleanup() - performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ ***************************************************************/
+void cleanup()
+{
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}
---
+++ testcases/kernel/syscalls/mlock/mlock02.c
@@ -0,0 +1,174 @@
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2002
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/*
+ * NAME
+ * 	mlock02.c
+ *
+ * DESCRIPTION
+ * 	Test to see the proper errors are returned by mlock
+ * 	
+ * ALGORITHM
+ * 	test 1:
+ *		Call mlock with a NULL address.  ENOMEM should be returned
+ *
+ * USAGE:  <for command-line>
+ *         -c n    Run n copies concurrently
+ *         -e      Turn on errno logging
+ *         -f      Turn off functional testing
+ *         -h      Show this help screen
+ *         -i n    Execute test n times
+ *         -I x    Execute test for x seconds
+ *         -p      Pause for SIGUSR1 before starting
+ *         -P x    Pause for x seconds between iterations
+ *         -t      Turn on syscall timing
+ *
+ * HISTORY
+ *	06/2002 Written by Paul Larson
+ *
+ * RESTRICTIONS
+ * 	None
+ */
+#include <errno.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include "test.h"
+#include "usctest.h"
+
+void setup();
+void setup1();
+void cleanup();
+
+char *TCID = "mlock02";		/* Test program identifier.    */
+int TST_TOTAL = 1;		/* Total number of test cases. */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+int exp_enos[] = { ENOMEM, 0 };
+
+void *addr1;
+
+struct test_case_t {
+	void **addr;
+	int len;
+	int error;
+	void (*setupfunc) ();
+} TC[] = {
+	/* mlock should return ENOMEM when some or all of the address 
+	 * range pointed to by addr and len are not valid mapped pages
+	 * in the address space of the process
+	 */
+	{&addr1, 1024, ENOMEM, setup1}
+};
+
+/***********************************************************************
+ * Main
+ ***********************************************************************/
+int main(int ac, char **av)
+{
+	int lc, i;		/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+
+	if ((msg = parse_opts(ac, av, NULL, NULL)) != (char *) NULL) {
+		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
+		tst_exit();
+	}
+
+    /***************************************************************
+     * perform global setup for test
+     ***************************************************************/
+	setup();
+
+	/* set the expected errnos... */
+	TEST_EXP_ENOS(exp_enos);
+
+    /***************************************************************
+     * check looping state 
+     ***************************************************************/
+	/* TEST_LOOPING() is a macro that will make sure the test continues
+	 * looping according to the standard command line args. 
+	 */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+
+		for (i = 0; i < TST_TOTAL; i++) {
+
+			if (TC[i].setupfunc != NULL)
+				TC[i].setupfunc();
+
+			TEST(mlock(*(TC[i].addr), TC[i].len));
+
+			/* check return code */
+			if (TEST_RETURN == -1) {
+				TEST_ERROR_LOG(TEST_ERRNO);
+				if (TEST_ERRNO != TC[i].error)
+					tst_brkm(TFAIL, cleanup,
+						 "mlock() Failed with wrong "
+						 "errno, expected errno=%d, "
+						 "got errno=%d : %s",
+						 TC[i].error, TEST_ERRNO,
+						 strerror(TEST_ERRNO));
+				else
+					tst_resm(TPASS,
+						 "expected failure - errno "
+						 "= %d : %s",
+						 TEST_ERRNO,
+						 strerror(TEST_ERRNO));
+			} else {
+				tst_brkm(TFAIL, cleanup,
+					 "mlock() Failed, expected "
+					 "return value=-1, got %d",
+					 TEST_RETURN);
+			}
+		}
+	}			/* End for TEST_LOOPING */
+
+    /***************************************************************
+     * cleanup and exit
+     ***************************************************************/
+	cleanup();
+
+	return 0;
+}				/* End main */
+
+/***************************************************************
+ * setup() - performs all ONE TIME setup for this test.
+ ***************************************************************/
+void setup()
+{
+	TEST_PAUSE;
+}
+
+void setup1()
+{
+	addr1 = NULL;
+}
+
+/***************************************************************
+ * cleanup() - performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ ***************************************************************/
+void cleanup()
+{
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}
