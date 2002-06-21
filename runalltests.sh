731655af5a2a5afe138a58c48a8a1090012e63aa
nstraz <nstraz>
2002-06-21 20:01:59 +0000

    Add in fsx-linux tests if $SCRATCHDEV is defined.  The user still has to remove the "unset SCRATCHDEV" safeguard before this will work.

--- runalltests.sh
+++ runalltests.sh
@@ -11,12 +11,13 @@ export PATH="${PATH}:${LTPROOT}/testcases/bin"
 
 cat ${LTPROOT}/runtest/syscalls ${LTPROOT}/runtest/fs ${LTPROOT}/runtest/dio ${LTPROOT}/runtest/mm ${LTPROOT}/runtest/commands ${LTPROOT}/runtest/ipc ${LTPROOT}/runtest/sched ${LTPROOT}/runtest/float > ${TMP}/alltests
 
-# Uncomment below and define if execution of the fsx-linux tests is desired.
-# Example:
-# export SCRATCHDEV=/dev/hda
-
-# Uncomment the next line if SCRATCHDEV defined
-#cat ${LTPROOT}/runtest/fsx >> ${TMP}/alltests
+# The fsx-linux tests use the SCRATCHDEV environment variable as a location
+# that can be reformatted and run on.  Set SCRATCHDEV if you want to run 
+# these tests.  As a safeguard, this is disabled.
+unset SCRATCHDEV
+if [ -n "$SCRATCHDEV" ]; then
+  cat ${LTPROOT}/runtest/fsx >> ${TMP}/alltests
+fi
 
 ${LTPROOT}/ver_linux
 
