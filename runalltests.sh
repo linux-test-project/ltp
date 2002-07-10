fef097cae399c3c4bc0a437a4fcdb31511d378ff
martinjn <martinjn>
2002-07-10 14:08:24 +0000

    Added instance and time command line options

--- runalltests.sh
+++ runalltests.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# 
+#  7/10/02 martinjn@us.ibm.com added instance and time command line options
+#
+
 cd `dirname $0`
 export LTPROOT=${PWD}
 export TMP="/tmp/runalltests-$$"
+export PAN_LOG="/root/ltp-logfile"
 
 mkdir ${TMP}
 cd ${TMP}
@@ -19,9 +24,26 @@ if [ -n "$SCRATCHDEV" ]; then
   cat ${LTPROOT}/runtest/fsx >> ${TMP}/alltests
 fi
 
+# In case you want to specify the number of instances from the command line 
+if [ $1 ]; then
+    instances="-x $1"
+else
+    instances=""
+fi
+
+# In case you want to specify the time to run from the command line (2m = two minutes, 2h = two hours, etc)
+# Obviously if you want to use this parameter, you must include the instances parameter first
+if [ $2 ]; then
+    time="-t $2"
+else
+    time=""
+fi
+
 ${LTPROOT}/ver_linux
 
-${LTPROOT}/pan/pan -e -S -a $$ -n $$ -f ${TMP}/alltests
+${LTPROOT}/pan/pan -e -S $instances $time -a $$ -n $$ -f ${TMP}/alltests
+# Use the following pan runline for use with ltp automation scripts instead of the one above.
+# ${LTPROOT}/pan/pan -l $PAN_LOG -e -S $instances $time -a $$ -n $$ -f ${TMP}/alltests
 
 if [ $? -eq "0" ]; then
   echo pan reported PASS
