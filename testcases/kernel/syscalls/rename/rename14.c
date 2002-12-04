d6c7b1f84e7fba6d494b9a48c4f9ea54b6da1471
robbiew <robbiew>
2002-12-04 18:41:18 +0000

    Initial checkin.

---
+++ testcases/kernel/syscalls/rename/rename14.c
@@ -0,0 +1,181 @@
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2002
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+/* 11/12/2002   Port to LTP     robbiew@us.ibm.com */
+/* 06/30/2001	Port to Linux	nsharoff@us.ibm.com */
+
+/*
+ * NAME
+ *	rename14.c - create and rename files
+ *
+ * CALLS
+ *	create, unlink, rename
+ *
+ * ALGORITHM
+ *	Creates two processes.  One creates and unlinks a file.
+ *	The other renames that file.
+ *
+ */
+
+#include <stdio.h>	
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/** LTP Port **/
+#include "test.h"
+#include "usctest.h"
+
+#define FAILED 0
+#define PASSED 1
+
+int local_flag = PASSED;
+extern int errno;
+
+char *TCID="rename14";            /* Test program identifier.    */
+int TST_TOTAL=1;                /* Total number of test cases. */
+extern int Tst_count;           /* Test Case counter for tst_* routines */
+/**************/
+
+
+#define RUNTIME	45
+
+int kidpid[2];
+int parent_pid;
+
+
+int main (argc, argv)
+	int  argc;
+	char *argv[];
+{
+	int pid, status;
+	sigset_t set;
+	struct sigaction act, oact;
+	int	term();
+	int	al();
+	void dochild1();
+	void dochild2();
+
+	sigemptyset(&set);
+	act.sa_handler = (void(*)())term;
+	act.sa_mask = set;
+	act.sa_flags = 0;
+	if (sigaction(SIGTERM, &act, &oact)) {
+		tst_resm(TBROK,"Sigaction(SIGTERM)");
+		tst_exit();
+	}
+
+	sigemptyset(&set);
+	act.sa_handler = (void(*)())al;
+	act.sa_mask = set;
+	act.sa_flags = 0;
+	if (sigaction(SIGALRM, &act, 0)) {
+		tst_resm(TBROK,"Sigaction(SIGALRM)");
+		tst_exit();
+	} 
+	parent_pid = getpid();
+	tst_tmpdir();
+/*--------------------------------------------------------------*/
+
+	pid = fork();
+	if (pid < 0) {
+		tst_resm(TBROK,"fork() returned %d\n", pid);
+		tst_exit();	
+	}
+	if (pid == 0) {
+		dochild1();
+		exit(0);
+	}
+	kidpid[0] = pid;
+	pid = fork();
+	if (pid < 0) {
+		(void)kill(kidpid[0], SIGTERM);
+		(void)unlink("./rename14");
+		tst_resm(TBROK,"fork() returned %d\n", pid);
+		tst_exit();	
+	}
+	if (pid == 0) {
+		dochild2();
+		exit(0);
+	}
+	kidpid[1] = pid;
+
+	alarm(RUNTIME);
+
+	/* Collect child processes. */
+
+	(void) wait(&status);
+	kill(kidpid[0], SIGTERM);
+	kill(kidpid[1], SIGTERM);
+
+	unlink("./rename14");
+	unlink("./rename14xyz");
+	(local_flag == PASSED) ?
+	    tst_resm(TPASS, "Test Passed")
+	:   tst_resm(TFAIL, "Test Failed");
+
+	tst_rmdir();
+/*--------------------------------------------------------------*/
+	tst_exit();	/* THIS CALL DOES NOT RETURN - EXITS!!	*/
+/*--------------------------------------------------------------*/
+	return(0);
+}
+/* FUNCTIONS GO HERE */
+
+int term()
+{
+	if (parent_pid != getpid())
+		exit(0);
+	if (kidpid[0])
+		return(kill(kidpid[0], SIGTERM));
+	if (kidpid[1])
+		return(kill(kidpid[1], SIGTERM));
+        return(0);
+}
+
+int al()
+{
+	if (kidpid[0])
+		return(kill(kidpid[0], SIGTERM));
+	if (kidpid[1])
+		return(kill(kidpid[1], SIGTERM));
+        return(0);
+}
+
+void dochild1()
+{
+	int fd;
+
+	for (;;) {
+		fd = creat("./rename14", 0666);
+		unlink("./rename14");
+		close(fd);
+	}
+}
+
+void dochild2()
+{
+	for (;;) {
+		rename("./rename14", "./rename14xyz");
+	}
+}
