de6f398ec0ed200be0c86ab7deddbd3c42a35f6c
iyermanoj <iyermanoj>
2002-12-17 16:02:52 +0000

    Added GPL & other docs.

--- testcases/commands/ade/file/file_test.sh
+++ testcases/commands/ade/file/file_test.sh
@@ -1,8 +1,37 @@
+################################################################################
+##                                                                            ##
+## Copyright (c) International Business Machines  Corp., 2001                 ##
+##                                                                            ##
+## This program is free software;  you can redistribute it and#or modify      ##
+## it under the terms of the GNU General Public License as published by       ##
+## the Free Software Foundation; either version 2 of the License, or          ##
+## (at your option) any later version.                                        ##
+##                                                                            ##
+## This program is distributed in the hope that it will be useful, but        ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY ##
+## or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   ##
+## for more details.                                                          ##
+##                                                                            ##
+## You should have received a copy of the GNU General Public License          ##
+## along with this program;  if not, write to the Free Software               ##
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    ##
+##                                                                            ##
+################################################################################
+#                                                                              
+# File:           file_test.sh
+#
+# Description: This program tests the file command. The tests are aimed at
+#              testing if the file command can recognize some of the commonly
+#              used file formats like, tar, tar.gz, rpm, C, ASCII, ELF etc. 
+#
+# Author:      Manoj Iyer, manjo@mail.utexas.edu
+#
+# History:     Dec 16 2002 - Created. - Manoj Iyer manjo@austin.ibm.com.
+#              Dec 17 2002 - Added.   - GPL header and minor doc changes.
 #! /bin/sh
 
 
-export TCID=file_cmd_test		# Name of this test case
-export TST_TOTAL=8				# Number of tests in this testcase
+export TST_TOTAL=10                # Number of tests in this testcase
 
 LTPTMP=/tmp
 LTPBIN=../../../bin
@@ -15,6 +44,7 @@ RC=0
 # TEST #1
 # Test if file command recognizes ASCII text files.
 
+export TCID=file01
 export TST_COUNT=1
 
 $LTPBIN/tst_resm TINFO "TEST #1: file commad recogizes ASCII text files"
@@ -31,22 +61,23 @@ file $LTPTMP/test_file.txt &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "ASCII text" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised ASCII file correctly"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-				"file: Failed to recognise ASCII file correctlyi. Reason:"
-	fi
+    grep "ASCII text" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised ASCII file correctly"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+                "file: Failed to recognise ASCII file correctlyi. Reason:"
+    fi
 else
-	$LTPBIN/tst_res TFAIL $LTPTMP/file.out  \
-				"file: failed to recognize ASCII file correctly\t\t"
+    $LTPBIN/tst_res TFAIL $LTPTMP/file.out  \
+                "file: failed to recognize ASCII file correctly\t\t"
 fi
 
 # TEST #2
 # Test if file command can recognize bash shell script
 
+export TCID=file02
 export TST_COUNT=2
 
 $LTPBIN/tst_resm TINFO "TEST #2: file command recognizes bash shell scripts"
@@ -63,21 +94,22 @@ file $LTPTMP/bash_script.sh &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "Bourne-Again shell script" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised bash shell script correctly"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			"file: Failed to recognise bash shell script. Reason"
-	fi
+    grep "Bourne-Again shell script" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised bash shell script correctly"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+            "file: Failed to recognise bash shell script. Reason"
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "file: Failed to recognize bash shell script"
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize bash shell script"
 fi
 
 # TEST #3
 # Test if file command can recognize korn shell script
 
+export TCID=file03
 export TST_COUNT=3
 
 $LTPBIN/tst_resm TINFO "TEST #3: file command recognizes korn shell scripts"
@@ -94,22 +126,23 @@ file $LTPTMP/ksh_script.sh &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "Korn shell script" $LTPTMP/file.out 2>&1 1>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: recognised korn shell script"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			"file: Failed to recognise korn shell script. Reason:"
-	fi
+    grep "Korn shell script" $LTPTMP/file.out 2>&1 1>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: recognised korn shell script"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+            "file: Failed to recognise korn shell script. Reason:"
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "File: Failed to recognize korn shell script"
+    $LTPBIN/tst_resm TFAIL "File: Failed to recognize korn shell script"
 fi
 
 
 # TEST #4
 # Test if file command can recognize C shell script
 
+export TCID=file04
 export TST_COUNT=4
 
 $LTPBIN/tst_resm TINFO "TEST #4: file command recognizes C shell scripts"
@@ -126,22 +159,23 @@ file $LTPTMP/C_script.sh &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "C shell script" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised C shell script correctly"
-	else
-		$LTPBIN/tst_resm TFAIL $LTPTMP/file.out \
-			"file: Failed to recognise C shell script correctly. Reason:"
-	fi
+    grep "C shell script" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised C shell script correctly"
+    else
+        $LTPBIN/tst_resm TFAIL $LTPTMP/file.out \
+            "file: Failed to recognise C shell script correctly. Reason:"
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "file: Failed to recognize C shell script"
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize C shell script"
 fi
 
 
 # TEST #5
 # Test if file command can recognize C program text
 
+export TCID=file05
 export TST_COUNT=5
 
 $LTPBIN/tst_resm TINFO "TEST #5: file command recognizes C programs text"
@@ -151,8 +185,8 @@ cat > $LTPTMP/cprog.c <<EOF
 
 main()
 {
-	printf("Hello Hell\n");
-	exit(0);
+    printf("Hello Hell\n");
+    exit(0);
 }
 EOF
 
@@ -160,27 +194,28 @@ file $LTPTMP/cprog.c &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "ASCII C program text" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised C program text correctly"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			 "file: Failed to Recognize C program text correctly. Reason:"
-		
-	fi
+    grep "ASCII C program text" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised C program text correctly"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+             "file: Failed to Recognize C program text correctly. Reason:"
+        
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "file: Failed to recognize C programi text"
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize C programi text"
 fi
 
 
 # TEST #6
 # Test if file command can recognize ELF binay executables
 
+export TCID=file06
 export TST_COUNT=6
 
 $LTPBIN/tst_resm TINFO \
-		"TEST #6: file command recognizes ELF executables"
+        "TEST #6: file command recognizes ELF executables"
 
 
 cat > $LTPTMP/cprog.c <<EOF
@@ -188,8 +223,8 @@ cat > $LTPTMP/cprog.c <<EOF
 
 main()
 {
-	printf("Hello Hell\n");
-	exit(0);
+    printf("Hello Hell\n");
+    exit(0);
 }
 EOF
 
@@ -199,24 +234,25 @@ file $LTPTMP/cprog &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "ELF 32-bit LSB executable, Intel 80386" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognized ELF binary executable"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			 "file: Failed to Recognize ELF binary executable. Reason:"
-		
-	fi
+    grep "ELF 32-bit LSB executable, Intel 80386" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognized ELF binary executable"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+             "file: Failed to Recognize ELF binary executable. Reason:"
+        
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "file: Failed to recognize ELF binay executable"
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize ELF binay executable"
 fi
 
 
 # TEST #7
 # Test if file command can recognize tar files
 
-export TST_COUNT=7
+export TCID=file07
+export TST_COUN=7
 
 $LTPBIN/tst_resm TINFO "TEST #7: file command recognizes tar files."
 
@@ -238,65 +274,67 @@ file $LTPTMP/files.tar &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "GNU tar archive" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised tar files"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			 "file: Failed to Recognize tar files. Reason:"
-	fi
+    grep "GNU tar archive" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised tar files"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+             "file: Failed to Recognize tar files. Reason:"
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "file: Failed to recognize tar files."
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize tar files."
 fi
 
 
 # TEST #8
 # Test if file command can tar zip files
 
+export TCID=file08
 export TST_COUNT=8
 
 $LTPBIN/tst_resm TINFO "TEST #8: file command recognizes tar zip files"
 
 tar cf $LTPTMP/files $LTPTMP/file1 $LTPTMP/file2 $LTPTMP/file3 \
-	&>$LTPTMP/file.out
+    &>$LTPTMP/file.out
 if [ $? -ne 0 ]
 then
-	$LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL \
-		"file: tar failed unexpectedly. Reason:"
+    $LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL \
+        "file: tar failed unexpectedly. Reason:"
 fi
-	
+    
 gzip -f $LTPTMP/files.tar
 if [ $? -ne 0 ]
 then
-	$LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL \
-		"file: gzip failed unexpectedly. Reason:"
+    $LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL \
+        "file: gzip failed unexpectedly. Reason:"
 fi
-	
+    
 file $LTPTMP/files.tar.gz &>$LTPTMP/file.out || RC=$?
 if [ $RC -eq 0 ]
 then
-	grep "gzip compressed data, deflated, original filename, \`files.tar'" \
-			$LTPTMP/file.out &>$LTPTMP/file1.out
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised tar zip file"
-	else
-		$LTPBIN/tst_brkm TBROK NULL \
-				"expected string: gzip compressed data, deflated,
+    grep "gzip compressed data, deflated, original filename, \`files.tar'" \
+            $LTPTMP/file.out &>$LTPTMP/file1.out
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised tar zip file"
+    else
+        $LTPBIN/tst_brkm TBROK NULL \
+                "expected string: gzip compressed data, deflated,
 original filename, \`files.tar'"
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			 "file: Failed to Recognize tar zip. Reason:"
-	fi
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+             "file: Failed to Recognize tar zip. Reason:"
+    fi
 else
-	$LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL \
-		"file: Failed to recognize tar zip file. Reason:"
+    $LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL \
+        "file: Failed to recognize tar zip file. Reason:"
 fi
 
 
 # TEST #9
 # Test if file command can recognize RPM files.
 
+export TCID=file09
 export TST_COUNT=9
 
 $LTPBIN/tst_resm TINFO "TEST #9: file command recognizes RPM files"
@@ -336,14 +374,14 @@ EOF
 
 if [ -d /usr/src/packages/SOURCES ]
 then
-	echo "directory exists" &>$LTPTMP/file.out
+    echo "directory exists" &>$LTPTMP/file.out
 else
-	mkdir -p /usr/src/packages/SOURCES/ &>$LTPTMP/file.out || RC=$?
+    mkdir -p /usr/src/packages/SOURCES/ &>$LTPTMP/file.out || RC=$?
 fi
 
 if [ $RC -ne 0 ]
 then
-	$LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL "mkdir: brok. Reason:"
+    $LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL "mkdir: brok. Reason:"
 fi
 
 cat > /usr/src/packages/SOURCES/cprog.c <<EOF || RC=$?
@@ -357,28 +395,59 @@ main()
 EOF
 if [ $RC -ne 0 ]
 then
-	$LTPBIN/tst_brkm TBROK NULL "cat: failed to create test file cprog.c"
+    $LTPBIN/tst_brkm TBROK NULL "cat: failed to create test file cprog.c"
 fi
 
 rpm -bs $LTPTMP/files.spec &>$LTPTMP/file.out
 if [ $? -ne 0 ]
 then
-	$LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL "rpm command brok. Reason:"
+    $LTPBIN/tst_brk TBROK $LTPTMP/file.out NULL "rpm command brok. Reason:"
 fi
 
 file /usr/src/packages/SRPMS/cprog-0.0.7-3.src.rpm &>$LTPTMP/file.out || RC=$?
 
 if [ $RC -eq 0 ]
 then
-	grep "RPM v3 src i386" $LTPTMP/file.out &>/dev/null
-	if [ $? -eq 0 ]
-	then
-		$LTPBIN/tst_resm TPASS "file: Recognised RPM file correctly"
-	else
-		$LTPBIN/tst_res TFAIL $LTPTMP/file.out \
-			 "file: Failed to Recognize RPM file. Reason:"
-		
-	fi
+    grep "RPM v3 src i386" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised RPM file correctly"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+             "file: Failed to Recognize RPM file. Reason:"
+        
+    fi
 else
-	$LTPBIN/tst_resm TFAIL "file: Failed to recognize RPM file"
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize RPM file"
+fi
+
+
+# TEST #10
+# Test if file command can recognize vmlinz file
+
+export TCID=file10
+export TST_COUNT=5
+
+$LTPBIN/tst_resm TINFO "TEST #10: file command recognizes vmlinuz file"
+
+if [ -f /boot/vmlinuz ]
+then
+    file /boot/vmlinuz* &>$LTPTMP/file.out || RC=$?
+else
+    file /boot/bzImage* &>$LTPTMP/file.out || RC=$?
+fi
+
+if [ $RC -eq 0 ]
+then
+    grep -i "kernel" $LTPTMP/file.out &>/dev/null
+    if [ $? -eq 0 ]
+    then
+        $LTPBIN/tst_resm TPASS "file: Recognised vmlinuz file correctly"
+    else
+        $LTPBIN/tst_res TFAIL $LTPTMP/file.out \
+             "file: Failed to Recognize vmlinuz correctly. Reason:"
+        
+    fi
+else
+    $LTPBIN/tst_resm TFAIL "file: Failed to recognize vmlinuz file"
 fi
