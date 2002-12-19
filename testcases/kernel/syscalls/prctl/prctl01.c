d076a28c01645600907442ea84be44592eabba40
robbiew <robbiew>
2002-12-19 19:05:13 +0000

    Initial checkin of prctl() tests by Saji Kumar.

---
+++ testcases/kernel/syscalls/prctl/prctl01.c
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+/**********************************************************
+ * 
+ *    TEST IDENTIFIER	: prctl01
+ * 
+ *    EXECUTED BY	: anyone
+ * 
+ *    TEST TITLE	: Basic test for prctl(2)
+ * 
+ *    TEST CASE TOTAL	: 2
+ * 
+ *    AUTHOR		: Saji Kumar.V.R <saji.kumar@wipro.com>
+ * 
+ *    SIGNALS
+ * 	Uses SIGUSR1 to pause before test if option set.
+ * 	(See the parse_opts(3) man page).
+ *
+ *    DESCRIPTION
+ *	This is a Phase I test for the prctl(2) system call.
+ *	It is intended to provide a limited exposure of the system call.
+ * 
+ * 	Setup:
+ * 	  Setup signal handling.
+ *	  Pause for SIGUSR1 if option specified.
+ * 
+ * 	Test:
+ *	 Loop if the proper options are given.
+ *	 fork a child
+ *
+ *	 CHILD:
+ *		call prctl() with proper arguments
+ *		If call succeeds,
+ *			exit with 0
+ *		else
+ *			exit with 1
+ *	 PARENT:
+ *		wait() for child.
+ *		If child exits with exit value 0,
+ *			Test passed
+ *		else
+ *			Test Failed
+ *		
+ * 	Cleanup:
+ * 	  Print errno log and/or timing stats if options given
+ * 
+ * USAGE:  <for command-line>
+ *  prctl01 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-h] [-f] [-p]
+ *			where,  -c n : Run n copies concurrently.
+ *				-e   : Turn on errno logging.
+ *				-h   : Show help screen
+ *				-f   : Turn off functional testing
+ *				-i n : Execute test n times.
+ *				-I x : Execute test for x seconds.
+ *				-p   : Pause for SIGUSR1 before starting
+ *				-P x : Pause for x seconds between iterations.
+ *				-t   : Turn on syscall timing.
+ *
+ ****************************************************************/
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
+
+#include "test.h"
+#include "usctest.h"
+
+static void setup(void);
+static void cleanup(void);
+
+char *TCID = "prctl01";		/* Test program identifier.    */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+int option[2] = { PR_GET_PDEATHSIG, PR_SET_PDEATHSIG };
+int TST_TOTAL = 2;
+
+int
+main(int ac, char **av)
+{
+
+	int lc, i;		/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+	pid_t child_pid;
+	int status, sig;
+
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL))
+	     != (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* perform global setup for test */
+	setup();
+
+	/* check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+
+		for (i = 0; i < TST_TOTAL; ++i) {
+
+			switch (child_pid = fork()) {
+
+			case -1:
+				/* fork() failed */
+				tst_resm(TFAIL, "fork() failed");
+				continue;
+
+			case 0:
+				/* Child */
+				if (i == 1) {
+					sig = SIGUSR2;
+				}
+
+				TEST(prctl(option[i], sig));
+				if (TEST_RETURN == 0) {
+					exit(0);
+				} else {
+					tst_resm(TWARN, "prctl() returned %d"
+						" errno = %d : %s", TEST_RETURN,
+						TEST_ERRNO,
+						strerror(TEST_ERRNO));
+					exit(1);
+				}
+
+			default:
+				/* Parent */
+				if ((waitpid(child_pid, &status, 0)) < 0) {
+					tst_resm(TFAIL, "waitpid() failed");
+					continue;
+				}
+
+				if ((WIFEXITED(status)) &&
+				    (WEXITSTATUS(status) == 0)) {
+					tst_resm(TPASS, "Test Passed");
+				} else {
+					tst_resm(TFAIL, "Test Failed");
+				}
+
+			}
+		}
+	}	/* End for TEST_LOOPING */
+
+	/* cleanup and exit */
+	cleanup();
+
+	/*NOTREACHED*/
+	return 0;
+
+}/* End main */
+
+/* setup() - performs all ONE TIME setup for this test */
+void
+setup()
+{
+
+	/* capture signals */
+	tst_sig(FORK, DEF_HANDLER, cleanup);
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+
+}	/* End setup() */
+
+
+/*
+ *cleanup() -  performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ */
+void
+cleanup()
+{
+
+	/*
+	 * print timing stats if that option was specified.
+	 * print errno log if that option was specified.
+	 */
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}	/* End cleanup() */
+
---
+++ testcases/kernel/syscalls/prctl/prctl02.c
@@ -0,0 +1,217 @@
+/*
+ * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+/**********************************************************
+ * 
+ *    TEST IDENTIFIER	: prctl02
+ * 
+ *    EXECUTED BY	: anyone
+ * 
+ *    TEST TITLE	: Tests for error conditions
+ * 
+ *    TEST CASE TOTAL	: 2
+ * 
+ *    AUTHOR		: Saji Kumar.V.R <saji.kumar@wipro.com>
+ * 
+ *    SIGNALS
+ * 	Uses SIGUSR1 to pause before test if option set.
+ * 	(See the parse_opts(3) man page).
+ *
+ *    DESCRIPTION
+ *	Verify that
+ *	1) prctl() fails with errno, EINVAL when an invalid value is given for
+ *	   option
+ *	2) prctl() fails with errno, EINVAL when option is PR_SET_PDEATHSIG
+ *	   & arg2 is not zero or a valid signal number
+ * 
+ * 	Setup:
+ * 	  Setup signal handling.
+ *	  Pause for SIGUSR1 if option specified.
+ * 
+ * 	Test:
+ *	 Loop if the proper options are given.
+ *	 fork a child
+ *
+ *	 CHILD:
+ *		call prctl() with proper arguments
+ *		If call fails with expected errno,
+ *			exit with 0
+ *		else
+ *			exit with 1
+ *	 PARENT:
+ *		wait() for child.
+ *		If child exits with exit value 0,
+ *			Test passed
+ *		else
+ *			Test Failed
+ *		
+ * 	Cleanup:
+ * 	  Print errno log and/or timing stats if options given
+ * 
+ * USAGE:  <for command-line>
+ *  prctl02 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-h] [-f] [-p]
+ *			where,  -c n : Run n copies concurrently.
+ *				-e   : Turn on errno logging.
+ *				-h   : Show help screen
+ *				-f   : Turn off functional testing
+ *				-i n : Execute test n times.
+ *				-I x : Execute test for x seconds.
+ *				-p   : Pause for SIGUSR1 before starting
+ *				-P x : Pause for x seconds between iterations.
+ *				-t   : Turn on syscall timing.
+ *
+ ****************************************************************/
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/prctl.h>
+#include <sys/wait.h>
+
+#include "test.h"
+#include "usctest.h"
+
+#define OPTION_INVALID 999
+#define INVALID_ARG 999
+
+static void setup(void);
+static void cleanup(void);
+
+char *TCID = "prctl02";		/* Test program identifier.    */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+static int exp_enos[] = {EINVAL, EINVAL, 0};
+
+struct test_cases_t {
+	int option;
+	unsigned long arg2;
+	int exp_errno;
+} test_cases[] = {
+	{ OPTION_INVALID, 0, EINVAL },
+	{ PR_SET_PDEATHSIG, INVALID_ARG, EINVAL }
+};
+	
+int TST_TOTAL = sizeof(test_cases) / sizeof(test_cases[0]);
+
+int
+main(int ac, char **av)
+{
+
+	int lc, i;		/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+	pid_t child_pid;
+	int status;
+
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL))
+	     != (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* perform global setup for test */
+	setup();
+
+	/* check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+
+		for (i = 0; i < TST_TOTAL; ++i) {
+
+			switch (child_pid = fork()) {
+
+			case -1:
+				/* fork() failed */
+				tst_resm(TFAIL, "fork() failed");
+				continue;
+
+			case 0:
+				/* Child */
+
+				TEST(prctl(test_cases[i].option,
+					   test_cases[i].arg2));
+				if ((TEST_RETURN == -1) && (TEST_ERRNO ==
+						test_cases[i].exp_errno)) {
+					exit(TEST_ERRNO);
+				} else {
+					tst_resm(TWARN, "prctl() returned %d"
+						" errno = %d : %s", TEST_RETURN,
+						TEST_ERRNO,
+						strerror(TEST_ERRNO));
+					exit(TEST_ERRNO);
+				}
+
+			default:
+				/* Parent */
+				if ((waitpid(child_pid, &status, 0)) < 0) {
+					tst_resm(TFAIL, "waitpid() failed");
+					continue;
+				}
+
+				if ((WIFEXITED(status)) && (WEXITSTATUS(status)
+						== test_cases[i].exp_errno)) {
+					tst_resm(TPASS, "Test Passed");
+				} else {
+					tst_resm(TFAIL, "Test Failed");
+				}
+				TEST_ERROR_LOG(WEXITSTATUS(status));
+
+			}
+		}
+	}	/* End for TEST_LOOPING */
+
+	/* cleanup and exit */
+	cleanup();
+
+	/*NOTREACHED*/
+	return 0;
+
+}/* End main */
+
+/* setup() - performs all ONE TIME setup for this test */
+void
+setup()
+{
+
+	/* capture signals */
+	tst_sig(FORK, DEF_HANDLER, cleanup);
+	
+	/* set the expected errnos... */
+	TEST_EXP_ENOS(exp_enos);
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+
+}	/* End setup() */
+
+
+/*
+ *cleanup() -  performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ */
+void
+cleanup()
+{
+
+	/*
+	 * print timing stats if that option was specified.
+	 * print errno log if that option was specified.
+	 */
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}	/* End cleanup() */
+
