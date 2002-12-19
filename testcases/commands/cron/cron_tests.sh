cf57821e50ef2752c735639c1bf95766182f4164
iyermanoj <iyermanoj>
2002-12-19 23:19:46 +0000

    Added new test to test basic functionality of CRONTAB, CRON etc.

---
+++ testcases/commands/cron/cron_tests.sh
@@ -0,0 +1,178 @@
+################################################################################
+##                                                                            ##
+## Copyright (c) International Business Machines  Corp., 2001                 ##
+##                                                                            ##
+## This program is free software;  you can redistribute it and#or modify      ##
+## it under the terms of the GNU General Public License as published by       ##
+## the Free Software Foundation; either version 2 of the License, or          ##
+## (at your option) any later version.                                        ##
+##                                                                            ##
+## This program is distributed in the hope that it will be useful, but        ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY ##
+## or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   ##
+## for more details.                                                          ##
+##                                                                            ##
+## You should have received a copy of the GNU General Public License          ##
+## along with this program;  if not, write to the Free Software		          ##
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    ##
+##									                                          ##
+################################################################################
+#
+# File:			cron_tests.sh
+#
+# Description:	This testcase tests if crontab <filename> installs the cronjob
+# and cron schedules the job correctly. The job is set such that it will run
+# forever every minute of the day.
+# The cronjob runs a program that will print a string followed by the current
+# date and time. Five samples are taken inorder to verify if cron job is run
+# every minute. It is not practical to check if it works for the remaining
+# fields of the crontab file also.
+# 
+# Author:		Manoj Iyer manjo@mail.utexas.edu
+#
+# History:
+# 	Dec - 19 - 2002 - Created.
+
+export TST_TOTAL=1
+
+if [[ -z $LTPTMP && -z $TMPBASE ]]
+then
+    LTPTMP=/tmp
+else
+    LTPTMP=$TMPBASE
+fi
+
+if [[ -z $LTPBIN && -z $LTPROOT ]]
+then
+    LTPBIN=./bin
+else
+    LTPBIN=$LTPROOT/testcases/bin
+fi
+
+# Set return code RC variable to 0, it will be set with a non-zero return code
+# in case of error. Set TFAILCNT to 0, increment if there occures a failure.
+
+LOCTMP=${PWD}/tmp
+TFAILCNT=0
+RC=0
+
+# Test #1
+# Test if crontab <filename> installs the crontab file and cron schedules the 
+# job correctly.
+
+export TCID=cron01
+export TST_COUNT=1
+
+$LTPBIN/tst_resm TINFO "Test #1: crontab <filename> installs the crontab file and cron schedules the job correctly." 
+
+# create the cron job. The job is to run the program tst1_cronprg.sh
+# every minute, every hour, every day, every month, any weekday. 
+
+cat > $LTPTMP/tst1_cronjob.cron <<EOF 
+* * * * * /$LTPTMP/tst1_cronprg.sh
+EOF
+
+# Create the program that will be run by the cronjob. This program will print a
+# "Hello Hell" string and date time information.
+
+cat > $LTPTMP/tst1_cronprg.sh <<EOF
+#! /bin/sh
+
+DATE=\`date\`
+echo "Hello Hell today is \$DATE " &>$LTPTMP/tst1_cron.out
+exit 0
+EOF
+
+chmod +x $LTPTMP/tst1_cronprg.sh
+
+# install the cronjob, crontab <filename> does that. Sleep for 10s and the
+# check the /var/log/messages to see if there is a record of any crontab
+# activity.
+
+$LTPBIN/tst_resm TINFO "Test #1: Installing cron job"
+crontab $LTPTMP/tst1_cronjob.cron &>$LTPTMP/cron_tst2n1.out || RC=$?
+
+if [ $RC -ne 0 ]
+then
+	$LTPBIN/tst_brk TBROK $LTPTMP/cron_tst2n1.out NULL 
+		"Test #1: crontab Broke while installing cronjob. Reason:"
+	TFAILCNT=$((TFAILCN+1))
+fi
+
+sleep 10s
+
+tail -n 10 /var/log/messages | grep crontab | grep REPLACE \
+	&>$LTPTMP/cron_tst2n1.out || RC=$?
+if [ $RC -ne 0 ]
+then
+	$LTPBIN/tst_resm TFAIL \
+		"Test #1: crontab activity not recorded in var/log/messages."
+	TFAILCNT=$((TFAILCNT+1))
+fi
+
+# just wait a random time for the cron to kickoff the cronjob.
+sleep 1m
+
+# The program executed by the cron job tst1_cronprg.sh will record the date
+# and time in a file tst1_cron.out. Extract the minute recorded by the program
+# into TS_MIN1 sleep for 1m 10s so that the cron will update this file after
+# 1m, extract TS_MIN2 and check if the minute recorded has advanced by 1. Take
+# 5 such samples, if any one of the fail, flag a failure. 
+
+LOOP_CNTR=5
+TS_MIN1=0
+FAILCNT=0
+
+while [ $LOOP_CNTR -ne 0 ]
+do
+	TS_MIN1=`cat /$LTPTMP/tst1_cron.out | cut -f 8 -d " " | cut -f2 -d:`
+	if [ $TS_MIN1 -eq 59 ]
+	then
+		TS_MIN1=00
+	else
+		TS_MIN1=$((TS_MIN1+1))
+	fi
+		
+
+	# wait for the cronjob to update the tst1_cron.out file.
+	sleep 1m 2s
+
+	# check the time recorded in the tst1_cron.out file, this should be 1 minute
+	# ahead of what was recored earlier.
+
+	TS_MIN2=`cat /tmp/tst1_cron.out | cut -f 8 -d " " | cut -f2 -d:`
+
+	if [ $TS_MIN2 -ne $TS_MIN1 ]
+	then
+		# if the value of the minute field did not advance by 1
+		# flag as failure.
+		FAILCNT=$((FAILCNT+1))
+		echo "Expected $TS_MIN2 \n Received $TS_MIN1" > $LTPTMP/tst1_cron.out 
+		crontab -r &>/dev/null
+		break
+	fi
+	LOOP_CNTR=$((LOOP_CNTR-1))
+done
+
+if [ $FAILCNT -eq 0 ]
+then
+	# check if var/log/messages file was updated.
+	grep "CMD (/tmp/tst1_cronprg.sh)" /var/log/messages &>$LTPTMP/cron_tst2n1.out || RC=$?
+	if [ $RC -eq 0 ]
+	then
+		$LTPBIN/tst_resm TPASS  \
+			"Test #1: installed cronjob, and cron executed the cronjob."
+	else
+		$LTPBIN/tst_res TFAIL $LTPTMP/cron_tst2n1.out \
+			"Test #1: Test failed. Reason:"
+		TFAILCNT=$((TFAILCNT+1))
+	fi
+else
+	$LTPBIN/tst_res TFAIL $LTPTMP/cron_tst1.out \
+		"Test #1: Cron did not execute every minute"
+fi
+
+#remove the cron job that was installed.
+crontab -r &>/dev/null
+
+exit $TFAILCNT
