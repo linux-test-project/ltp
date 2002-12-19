8cb7fcd4de55a6ab3a7755083798d57bcad1dd16
robbiew <robbiew>
2002-12-19 18:20:13 +0000

    Initial checkin of ptrace() tests, written by Saji Kumar.

---
+++ testcases/kernel/syscalls/ptrace/ptrace01.c
@@ -0,0 +1,297 @@
+/*
+ * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+/**********************************************************
+ * 
+ *    TEST IDENTIFIER	: ptrace01
+ * 
+ *    EXECUTED BY	: anyone
+ * 
+ *    TEST TITLE	: functionality test for ptrace(2)
+ * 
+ *    TEST CASE TOTAL	: 2
+ * 
+ *    AUTHOR		: Saji Kumar.V.R <saji.kumar@wipro.com>
+ * 
+ *    SIGNALS
+ * 	Uses SIGUSR1 to pause before test if option set.
+ * 	(See the parse_opts(3) man page).
+ *
+ *    DESCRIPTION
+ *	This test case tests the functionality of ptrace() for
+ *	PTRACE_TRACEME & PTRACE_KILL requests.
+ *	Here, we fork a child & the child does ptrace(PTRACE_TRACEME, ...).
+ *	Then a signal is delivered to the child & verified that parent
+ *	is notified via wait(). then parent does ptrace(PTRACE_KILL, ..)
+ *	to kill the child. Again parent wait() for child to finish.
+ *	If child finished abnormally, test passes.
+ *		We test two cases
+ * 			1) By telling child to ignore SIGUSR2 signal
+ * 			2) By installing a signal handler for child for SIGUSR2
+ * 		In both cases, child should stop & notify parent on reception
+ * 		of SIGUSR2
+ * 
+ * 	Setup:
+ * 	  Setup signal handling.
+ *	  Pause for SIGUSR1 if option specified.
+ * 
+ * 	Test:
+ *	 Loop if the proper options are given.
+ *	 setup signal handler for SIGUSR2 signal
+ *	 fork a child
+ *
+ *	 CHILD:
+ *		setup signal handler for SIGUSR2 signal
+ *		call ptrace() with PTRACE_TRACEME request
+ *		send SIGUSR2 signal to self
+ *	 PARENT:
+ *		wait() for child.
+ *		if parent is notified when child gets a signal through wait(),
+ *		then
+ *			do  ptrace(PTRACE_KILL, ..) on child
+ * 			wait() for child to finish,
+ * 			if child exited abnormaly,
+ *				TEST passed
+ * 			else
+ * 				TEST failed
+ *		else
+ *			TEST failed
+ *		
+ * 	Cleanup:
+ * 	  Print errno log and/or timing stats if options given
+ * 
+ * USAGE:  <for command-line>
+ *  ptrace01 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-h] [-f] [-p]
+ *			where,  -c n : Run n copies concurrently.
+ *				-e   : Turn on errno logging.
+ *				-h   : Show help screen
+ *				-f   : Turn off functional testing
+ *				-i n : Execute test n times.
+ *				-I x : Execute test for x seconds.
+ *				-p   : Pause for SIGUSR1 before starting
+ *				-P x : Pause for x seconds between iterations.
+ *				-t   : Turn on syscall timing.
+ *
+ ****************************************************************/
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+
+#include "test.h"
+#include "usctest.h"
+
+static void setup(void);
+static void cleanup(void);
+static void child_handler();
+static void parent_handler();
+
+static int got_signal = 0;
+
+char *TCID = "ptrace01";	/* Test program identifier.    */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+int TST_TOTAL = 2;
+
+int
+main(int ac, char **av)
+{
+
+	int lc, i;		/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+	pid_t child_pid;
+	int status;
+	struct sigaction parent_act;
+	struct sigaction child_act;
+    
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL))
+	     != (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* perform global setup for test */
+	setup();
+
+	/* check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+		
+		for (i = 0; i < TST_TOTAL; ++i) {
+			got_signal = 0;
+		
+			/* Setup signal handler for parent */
+			if (i == 1) {
+				parent_act.sa_handler = parent_handler;
+				parent_act.sa_flags = SA_RESTART;
+
+				if ((sigaction(SIGUSR2, &parent_act, NULL))
+				    == -1) {
+					tst_resm(TWARN, "sigaction() failed"
+							" in parent");
+					continue;
+				}
+			}
+
+			switch (child_pid = fork()) {
+	
+			case -1:
+				/* fork() failed */
+				tst_resm(TFAIL, "fork() failed");
+				continue;
+
+			case 0:
+				/* Child */
+
+				/* Setup signal handler for child */
+				if (i == 0) {
+					child_act.sa_handler = SIG_IGN;
+				} else {
+					child_act.sa_handler = child_handler;
+				}
+				child_act.sa_flags = SA_RESTART;
+
+				if ((sigaction(SIGUSR2, &child_act, NULL))
+				    == -1) {
+					tst_resm(TWARN, "sigaction() failed"
+						" in child");
+					exit(1);
+				}
+
+				if ((ptrace(PTRACE_TRACEME, 0, 0 ,0)) == -1) {
+					tst_resm(TWARN, "ptrace() failed in"
+						 " child");
+					exit(1);
+				}
+				/* ensure that child bypasses signal handler */
+				if ((kill(getpid(), SIGUSR2)) == -1) {
+					tst_resm(TWARN, "kill() failed in"
+						 " child");
+					exit(1);
+				}
+				exit(1);
+		
+			default:
+				/* Parent */
+				if ((waitpid(child_pid, &status, 0)) < 0) {
+					tst_resm(TFAIL, "waitpid() failed");
+					continue;
+				}
+
+				/*
+				 * Check the exit status of child. If (it exits
+				 * normally with exit value 1) OR (child came
+				 * through signal handler), Test Failed
+				 */
+				
+				if (((WIFEXITED(status)) &&
+				    (WEXITSTATUS(status))) ||
+				    (got_signal == 1)) {
+					tst_resm(TFAIL, "Test Failed");
+					continue;
+				} else {
+					/* Kill child */
+					if ((ptrace(PTRACE_KILL, child_pid,
+						    0, 0)) == -1) {
+						tst_resm(TFAIL, "Test Failed:"
+						" Parent was not able to kill"
+						" child");
+						continue;
+					}
+				}
+
+				if ((waitpid(child_pid, &status, 0)) < 0) {
+					tst_resm(TFAIL, "waitpid() failed");
+					continue;
+				}
+
+				if (WIFEXITED(status)) {
+					/* Child exits normally */
+					tst_resm(TFAIL, "Test failed");
+				} else {
+					tst_resm(TPASS, "Test Passed");
+				}
+
+			}
+		} 
+	}	/* End for TEST_LOOPING */
+
+	/* cleanup and exit */
+	cleanup();
+
+	/*NOTREACHED*/
+	return 0;
+
+}/* End main */
+
+/* setup() - performs all ONE TIME setup for this test */
+void
+setup()
+{
+	
+	/* capture signals */
+	tst_sig(FORK, DEF_HANDLER, cleanup);
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+
+}	/* End setup() */
+
+
+/*
+ *cleanup() -  performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ */
+void
+cleanup()
+{
+
+	/*
+	 * print timing stats if that option was specified.
+	 * print errno log if that option was specified.
+	 */
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}	/* End cleanup() */
+
+/*
+ * child_handler() - Signal handler for child
+ */
+void
+child_handler()
+{
+
+	if ((kill(getppid(), SIGUSR2)) == -1) {
+		tst_resm(TWARN, "kill() failed in child_handler()");
+		exit(1);
+	}
+}
+
+/*
+ * parent_handler() - Signal handler for parent
+ */
+void
+parent_handler()
+{
+
+	got_signal = 1;
+}
+
---
+++ testcases/kernel/syscalls/ptrace/ptrace03.c
@@ -0,0 +1,234 @@
+/*
+ * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+/**********************************************************
+ * 
+ *    TEST IDENTIFIER	: ptrace03
+ * 
+ *    EXECUTED BY	: anyone
+ * 
+ *    TEST TITLE	: Tests for error conditions
+ * 
+ *    TEST CASE TOTAL	: 3
+ * 
+ *    AUTHOR		: Saji Kumar.V.R <saji.kumar@wipro.com>
+ * 
+ *    SIGNALS
+ * 	Uses SIGUSR1 to pause before test if option set.
+ * 	(See the parse_opts(3) man page).
+ *
+ *    DESCRIPTION
+ *	Verifies that
+ *	1) ptrace() returns -1 & sets errno to EPERM while tring to trace
+ *	   process 1
+ *	2) ptrace() returns -1 & sets errno to ESRCH if process with
+ *	   specified pid does not exist
+ *	3) ptrace() returns -1 & sets errno to EPERM if we are trying
+ *	   to trace a process which is already been traced
+ *
+ * 	Setup:
+ * 	  Setup signal handling.
+ *	  Pause for SIGUSR1 if option specified.
+ * 
+ * 	Test:
+ *	 Loop if the proper options are given.
+ *	 setup signal handler for SIGUSR2 signal
+ *	 fork a child
+ *
+ *	 CHILD:
+ *	 	call ptrace() with proper arguments
+ *	 	if ptrace() failed with expected return value & errno
+ *			exit with errno
+ *		else
+ *			Give proper error message
+ *			exit with errno
+ *
+ *	 PARENT:
+ *		Wait for child to finish
+ *		if child exits with expected errno
+ *			Test Passed
+ *	 	else
+ *			Test failed
+ *		
+ * 	Cleanup:
+ * 	  Print errno log and/or timing stats if options given
+ * 
+ * USAGE:  <for command-line>
+ *  ptrace03 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-h] [-f] [-p]
+ *			where,  -c n : Run n copies concurrently.
+ *				-e   : Turn on errno logging.
+ *				-h   : Show help screen
+ *				-f   : Turn off functional testing
+ *				-i n : Execute test n times.
+ *				-I x : Execute test for x seconds.
+ *				-p   : Pause for SIGUSR1 before starting
+ *				-P x : Pause for x seconds between iterations.
+ *				-t   : Turn on syscall timing.
+ *
+ ****************************************************************/
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <pwd.h> 
+
+#include "test.h"
+#include "usctest.h"
+
+#define INVALID_PID 999999
+
+static void setup(void);
+static void cleanup(void);
+
+static int exp_enos[] = {EPERM, ESRCH, 0};
+
+char *TCID = "ptrace03";	/* Test program identifier.    */
+extern int Tst_count;		/* Test Case counter for tst_* routines */
+
+struct test_case_t {
+	enum __ptrace_request request;
+	pid_t pid;
+	int exp_errno;
+} test_cases[] = {
+	{ PTRACE_ATTACH, 1, EPERM },
+	{ PTRACE_ATTACH, INVALID_PID, ESRCH},
+	{ PTRACE_TRACEME, 0, EPERM },
+};
+
+int TST_TOTAL = sizeof(test_cases) / sizeof(test_cases[0]);
+
+int
+main(int ac, char **av)
+{
+
+	int lc, i;			/* loop counter */
+	char *msg;		/* message returned from parse_opts */
+	pid_t child_pid;
+	int status;
+
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL))
+	     != (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* perform global setup for test */
+	setup();
+
+	/* check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+
+		for (i = 0; i < TST_TOTAL; ++i) {
+
+			/* fork() */
+			switch (child_pid = fork()) {
+
+			case -1:
+				/* fork() failed */
+				tst_resm(TFAIL, "fork() failed");
+				continue;
+
+			case 0:
+				/* Child */
+
+				/* setup for third test case */
+				if (i == 2) {
+					if ((ptrace(PTRACE_TRACEME, 0,
+						   NULL, NULL)) == -1) {
+						tst_resm(TWARN, "ptrace()"
+						" falied with errno, %d : %s",
+						errno, strerror(errno));
+						exit(0);
+					}
+				}
+
+				TEST(ptrace(test_cases[i].request,
+					    test_cases[i].pid, NULL, NULL));
+				if ((TEST_RETURN == -1) && (TEST_ERRNO ==
+						    test_cases[i].exp_errno)) {
+					exit(TEST_ERRNO);
+				} else {
+					tst_resm(TWARN, "ptrace() returned %d;"
+						" errno: %d, %s", TEST_RETURN,
+						TEST_ERRNO,
+						strerror(TEST_ERRNO));
+					exit(TEST_ERRNO);
+				}
+
+			default:
+				/* Parent */
+				if ((waitpid(child_pid, &status, 0)) < 0) {
+					tst_resm(TFAIL, "waitpid() failed");
+					continue;
+				}
+				if ((WIFEXITED(status)) &&
+				   (WEXITSTATUS(status) ==
+				    test_cases[i].exp_errno)) {
+					tst_resm(TPASS, "Test Passed");
+				} else {
+					tst_resm(TFAIL, "Test Failed");
+				}
+				TEST_ERROR_LOG(WEXITSTATUS(status));
+			}
+		}
+	}
+
+	/* cleanup and exit */
+	cleanup();
+
+	/*NOTREACHED*/
+	return 0;
+
+}/* End main */
+
+/* setup() - performs all ONE TIME setup for this test */
+void
+setup()
+{
+	
+	/* capture signals 
+	tst_sig(FORK, DEF_HANDLER, cleanup); */
+
+	/* set the expected errnos... */
+	TEST_EXP_ENOS(exp_enos);
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+	
+}	/* End setup() */
+
+
+/*
+ *cleanup() -  performs all ONE TIME cleanup for this test at
+ *		completion or premature exit.
+ */
+void
+cleanup()
+{
+
+	/*
+	 * print timing stats if that option was specified.
+	 * print errno log if that option was specified.
+	 */
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}	/* End cleanup() */
