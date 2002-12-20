c0971a65807fd0f1c1c5f447e428e364f72d06c5
iyermanoj <iyermanoj>
2002-12-20 19:56:54 +0000

    Added exec permission to file perms

--- testcases/commands/cron/cron_tests.sh
+++ testcases/commands/cron/cron_tests.sh
@@ -13,9 +13,9 @@
 ## for more details.                                                          ##
 ##                                                                            ##
 ## You should have received a copy of the GNU General Public License          ##
-## along with this program;  if not, write to the Free Software		          ##
+## along with this program;  if not, write to the Free Software		      ##
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    ##
-##									                                          ##
+##									      ##
 ################################################################################
 #
 # File:			cron_tests.sh
@@ -44,7 +44,7 @@ fi
 
 if [[ -z $LTPBIN && -z $LTPROOT ]]
 then
-    LTPBIN=./bin
+    LTPBIN=./
 else
     LTPBIN=$LTPROOT/testcases/bin
 fi
