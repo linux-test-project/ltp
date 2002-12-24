07eb41bb81863ae501a0016cf09779866db8b8f3
robbiew <robbiew>
2002-12-24 17:13:36 +0000

    Initial checkin of reboot() tests from Aniruddha Marathe <aniruddha.marathe@wipro.com>

---
+++ testcases/kernel/syscalls/reboot/reboot01.c
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+/**************************************************************************
+ * 
+ *    TEST IDENTIFIER	: reboot01 
+ * 
+ *    EXECUTED BY	: root / superuser
+ * 
+ *    TEST TITLE	: Basic test for reboot(2)
+ * 
+ *    TEST CASE TOTAL	: 2
+ * 
+ *    AUTHOR		: Aniruddha Marathe <aniruddha.marathe@wipro.com>
+ * 
+ *    SIGNALS
+ * 	Uses SIGUSR1 to pause before test if option set.
+ * 	(See the parse_opts(3) man page).
+ *
+ *    DESCRIPTION
+ *    This is a Phase I test for the reboot(2) system call.
+ *    It is intended to provide a limited exposure of the system call.
+ *     
+ * 
+ * 	Setup:
+ *	  Setup signal handling.
+ *	  Pause for SIGUSR1 if option specified.
+ *	  setting the flag value for two tests.
+ * 
+ * 	Test:
+ *	 Loop if the proper options are given.
+ *	 for two test cases for two flag values 
+ *	  Execute system call
+ *	  Check return code, if system call failed (return=-1)
+ *		Log the errno and Issue a FAIL message.
+ *	  Otherwise, Issue a PASS message.
+ * 
+ * 	Cleanup:
+ * 	  Print errno log and/or timing stats if options given
+ * 
+ * USAGE:  <for command-line>
+ * reboot01 [-c n] [-e] [-i n] [-I x] [-p x] [-t] [-h] [-f] [-p]
+ * where:
+ * 	-c n : run the test for n number of times. 
+ *	-e   : Turn on errno logging.
+ *	-i n : Execute test n times.
+ *	-I x : Execute test for x seconds.
+ *	-p   : Pause for SIGUSR1 before starting
+ *	-P x : Pause for x seconds between iterations.
+ *	-t   : Turn on syscall timing.
+ *
+ *RESTRICTIONS:
+ *for lib4 and lib5 reboot(2) system call is implemented as 
+ *int reboot(int magic, int magic2, int flag, void *arg); This test case
+ *is written for int reboot(int flag); which is implemented under glibc 
+ *Therefore this testcase may not work under libc4 and libc5 libraries
+ *****************************************************************************/
+
+#include <unistd.h>
+#include <sys/reboot.h>
+#include "test.h"
+#include "usctest.h"
+#include <errno.h>
+#include <linux/reboot.h>
+
+static void setup();
+static void cleanup();
+
+char *TCID = "reboot01"; /* Test program identifier.    */
+int TST_TOTAL = 2;       /* Total number of test cases. */
+extern int Tst_count;    /* Test Case counter for tst_* routines */
+
+static int flag[2] = {LINUX_REBOOT_CMD_CAD_ON, LINUX_REBOOT_CMD_CAD_OFF};
+static unsigned char *option_message[] = {"LINUX_REBOOT_CMD_CAD_ON", 
+					"LINUX_REBOOT_CMD_CAD_OFF"};
+
+int
+main(int ac, char **av)
+{
+
+	int lc, i;	/* loop counter */
+	char *msg;	/* message returned from parse_opts */
+
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL))
+		!= (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* perform global setup for test */
+	setup();
+
+	/* check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		/* reset Tst_count in case we are looping. */
+		Tst_count = 0;
+
+		for(i = 0; i < TST_TOTAL; i++) {
+
+			TEST(reboot(flag[i]));
+			/* check return code */
+			if (TEST_RETURN == -1) {
+				 TEST_ERROR_LOG(TEST_ERRNO);
+				 tst_resm(TFAIL, "reboot(2) Failed for "
+						 "option %s",
+						  option_message[i]);
+			} else {
+				tst_resm(TPASS, "reboot(2) Passed for "
+						"option %s", option_message[i]);
+			}
+		}	/*End of TEST CASE LOOPING*/
+	}	/*End for TEST_LOOPING*/
+
+	/*Clean up and exit*/
+	cleanup();
+
+	/*NOTREACHED*/
+	return 0;
+}
+
+/* setup() - performs all ONE TIME setup for this test */
+void
+setup()
+{
+	/* capture signals */
+	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+
+	/* Check whether we are root*/
+	if (geteuid() != 0) {
+		tst_brkm(TBROK, tst_exit, "Test must be run as root");
+	}
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+
+}	/* End setup() */
+
+/*
+ * cleanup() - Performs one time cleanup for this test at
+ * completion or premature exit
+ */
+
+void
+cleanup()
+{
+	/*
+	* print timing stats if that option was specified.
+	* print errno log if that option was specified.
+	*/
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}	/* End cleanup() */
+
---
+++ testcases/kernel/syscalls/reboot/reboot02.c
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ */
+/**************************************************************************
+ * 
+ *    TEST IDENTIFIER	: reboot02
+ *
+ * 
+ *    EXECUTED BY	: root / superuser
+ * 
+ *    TEST TITLE	: Test checking for basic error conditions
+ *    				 for reboot(2)
+ * 
+ *    TEST CASE TOTAL	: 2 
+ * 
+ *    AUTHOR		: Aniruddha Marathe <aniruddha.marathe@wipro.com>
+ * 
+ *    SIGNALS
+ * 	Uses SIGUSR1 to pause before test if option set.
+ * 	(See the parse_opts(3) man page).
+ *
+ *    DESCRIPTION
+ *	This test case checks whether reboot(2) system call  returns 
+ *	appropriate error number for invalid
+ *	flag parameter or invalid user.
+ * 
+ * 	Setup:
+ *	  Setup signal handling.
+ *	  Pause for SIGUSR1 if option specified.
+ *	  For testing error on invalid user, change the effective uid
+ * 
+ * 	Test:
+ *	  Loop if the proper options are given.
+ *	  Execute system call with invaid flag parameter
+ *	  and then for invalid user
+ *	  Check return code, if system call fails with errno == expected errno
+ *		Issue syscall passed with expected errno
+ *	  Otherwise, 
+ *	  Issue syscall failed to produce expected errno
+ * 
+ * 	Cleanup:
+ * 	  Do cleanup for the test.
+ * 	   
+ * USAGE:  <for command-line>
+ *  reboot02 [-c n] [-e] [-i n] [-I x] [-p x] [-t] [-h] [-f] [-p]
+ *  where 
+ *  	-c n: run n copies simultaneously 
+ *	-e   : Turn on errno logging.
+ *	-i n : Execute test n times.
+ *	-I x : Execute test for x seconds.
+ *	-p   : Pause for SIGUSR1 before starting
+ *	-P x : Pause for x seconds between iterations.
+ *	-t   : Turn on syscall timing.
+ *		
+ *RESTRICTIONS:
+ *for lib4 and lib5 reboot(2) system call is implemented as
+ *int reboot(int magic, int magic2, int flag, void *arg); This test case
+ *is written for int reboot(int flag); which is implemented under glibc
+ *Therefore this testcase may not work under libc4 and libc5 libraries
+ *****************************************************************************/
+
+#include <unistd.h>
+#include <sys/reboot.h>
+#include <errno.h>
+#include <linux/reboot.h>
+#include <pwd.h>
+#include "test.h"
+#include "usctest.h"
+
+#define INVALID_PARAMETER 100
+
+static void setup();
+static void cleanup();
+static int setup_test();
+
+char *TCID = "reboot02"; 	/* Test program identifier.    */
+int TST_TOTAL = 2;       	/* Total number of test cases. */
+extern int Tst_count;    	/* Test Case counter for tst_* routines */
+char nobody_uid[] = "nobody";
+struct passwd *ltpuser;
+static int exp_enos[] = {EINVAL, EPERM, 0};
+
+static struct test_case_t {
+	char *err_desc;		/*error description */
+	int  exp_errno;		/* expected error number*/
+	char *exp_errval;	/*Expected errorvalue string*/
+} testcase[] = {
+	{"Invalid flag", EINVAL, "EINVAL"},
+	{"Permission denied", EPERM, "EPERM "}
+};
+
+int
+main(int ac, char **av)
+{
+
+	int lc, i;	/* loop counter */
+	char *msg;	/* message returned from parse_opts */
+
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, (option_t *)NULL, NULL))
+		!= (char *)NULL) {
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+	}
+
+	/* perform global setup for test */
+	setup();
+
+	/* check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+
+		for(i = 0; i < TST_TOTAL; i++) {
+
+			/* reset Tst_count in case we are looping. */
+			Tst_count = 0;
+			if(i == 0) {
+				TEST(reboot(INVALID_PARAMETER));
+			} else {
+				/*change the user to nobody*/
+				if(setup_test() == 0) {
+					TEST(reboot(LINUX_REBOOT_CMD_CAD_ON));
+					/* Set effective user id back to root */
+					if (seteuid(0) == -1) {
+						tst_brkm(TBROK, cleanup, 
+							"seteuid failed to "
+							"set the effective uid"
+							" to root");
+						perror("seteuid");
+					}
+				} else {
+					tst_resm(TWARN, "skipping the test");
+					continue;
+				}
+			}
+			/* check return code */
+			if ((TEST_RETURN == -1) && (TEST_ERRNO == testcase[i].
+					exp_errno)) {
+				tst_resm(TPASS, "reboot(2) expected failure;"
+						" Got errno - %s : %s", 
+						testcase[i].exp_errval, 
+						testcase[i].err_desc);
+			} else {
+				tst_resm(TFAIL,"reboot(2) failed to produce"
+						" expected error; %d, errno" 
+					       ": %s and got %d", 
+						testcase[i].exp_errno, 
+						testcase[i].exp_errval, 
+						TEST_ERRNO);
+			}
+
+			TEST_ERROR_LOG(TEST_ERRNO);
+		}	/*End of TEST LOOPS*/
+	}		/* End of TEST_LOOPING*/
+
+	/*Clean up and exit*/
+	cleanup();
+
+	/*NOTREACHED*/
+	return 0;
+}	/*End of main*/
+
+/*
+ * setup_test() - This function sets the user as nobdy
+ */
+int
+setup_test()
+{
+	if((ltpuser = getpwnam(nobody_uid)) == NULL) {
+		tst_resm(TWARN, "\"nobody\" user not present. skipping test");
+		return -1;
+	}
+	if (seteuid(ltpuser->pw_uid) == -1) {
+		tst_resm(TWARN, "seteuid failed to "
+			"to set the effective uid to %d", ltpuser->pw_uid);
+		perror("seteuid");
+		return -1;
+	}
+	return 0;
+}
+
+/* setup() - performs all ONE TIME setup for this test */
+void
+setup()
+{
+	/* capture signals */
+	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+
+	/* set the expected errnos... */
+	TEST_EXP_ENOS(exp_enos);
+
+	/* Check whether we are root*/
+	if (geteuid() != 0) {
+		tst_brkm(TBROK, tst_exit, "Test must be run as root");
+	}
+
+	/* Pause if that option was specified */
+	TEST_PAUSE;
+
+}	/* End setup() */
+
+/*
+* cleanup() - Performs one time cleanup for this test at
+* completion or premature exit
+*/
+void
+cleanup()
+{
+	/*
+	* print timing stats if that option was specified.
+	* print errno log if that option was specified.
+	*/
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit();
+}	/* End cleanup() */
+
