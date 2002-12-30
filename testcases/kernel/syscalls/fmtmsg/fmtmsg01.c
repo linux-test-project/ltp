dc44d2e44a52f99a83b6d4e64cdbdacc6c4cc4a6
robbiew <robbiew>
2002-12-30 21:32:36 +0000

    Added fmtmsg() test ported by Ananda Venkataraman <avenkat@us.ibm.com>

---
+++ testcases/kernel/syscalls/fmtmsg/fmtmsg01.c
@@ -0,0 +1,263 @@
+/* IBM Corporation */
+/* 01/02/2003	Port to LTP avenkat@us.ibm.com */
+/* 06/30/2001	Port to Linux	nsharoff@us.ibm.com */
+
+/*
+ * $Copyright: $
+ * Copyright (c) 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Further, this software is distributed without any warranty that it is
+ * free of the rightful claim of any third person regarding infringement
+ * or the like.  Any license provided herein, whether implied or
+ * otherwise, applies only to this software file.  Patent licenses, if
+ * any, provided herein do not apply to combinations of this program with
+ * other software, or any other product whatsoever.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ 
+ */
+
+/*
+ * NAME
+ *      fmtmsg -- test fmtmsg(3C) and addseverity(3C)
+ *
+ * CALLS
+ *      fmtmsg(3), addseverity(3C)
+ *
+ * ALGORITHM
+ *      Check basic functionality using various messages and severity levels.
+ *
+ * RESTRICTIONS
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <fmtmsg.h>			/* interface definition */
+#include <string.h>
+
+/*****	LTP Port	*****/
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include "test.h"
+#include "usctest.h"
+#define FAILED 0
+#define PASSED 1
+
+char *TCID = "fmtms01";
+
+int local_flag = PASSED;
+int block_number;
+FILE *temp;
+int TST_TOTAL = 1;
+extern int Tst_count;
+
+int anyfail();
+int blenter();
+int blexit();
+void setup();
+
+/*****	**	**	*****/
+//char progname[]= "fmtmsg1()";
+
+char ch;
+char buf[80];
+char * str1 = "SPIE:fmtmsg: INFO: SPIE fmtmsg() test1 message, NOT an error";
+char * str2 = "TO FIX: This is correct output, no action needed  SPIE:msg:001";
+char * str3 = "SPIE:fmtmsg: SPIE_TEST: SPIE fmtmsg() test2 message, NOT an error";
+char * str4 = "TO FIX: This is correct output, no action needed  SPIE:msg:002";
+
+clearbuf()
+{
+	int i;
+	for (i=0; i<80; i++)
+		buf[i] = (char)NULL;
+}
+
+/*--------------------------------------------------------------*/
+main(int argc, char *argv[])
+{
+	int fd, ret_val;
+	FILE *fp;
+
+	setup();		/* temp file is now open */
+/*--------------------------------------------------------------*/
+block0: blenter();
+
+	/* Check that system SEV_LEVEL output is correct */
+
+	close(2);			/* redirect stderr to file */
+	fd = creat( "fmtfile", 0644);
+	ret_val = fmtmsg(MM_PRINT|MM_SOFT,"SPIE:fmtmsg", MM_INFO,
+			"SPIE fmtmsg() test1 message, NOT an error",
+			"This is correct output, no action needed","SPIE:msg:001");
+	close(fd);
+
+	if (ret_val != 0) {
+		fprintf(temp, "fmtmsg returned %d, expected 0\n\n", ret_val); 
+		local_flag = FAILED;
+	}
+
+	fp = fopen("fmtfile", "r");
+	clearbuf();
+	fread(buf, sizeof(buf[0]), strlen(str1), fp);
+	if (strcmp(str1, buf) != 0) {
+		fprintf(temp, "Expected string: %s\n", str1);
+		fprintf(temp, "does not match\n");
+		fprintf(temp, "received string: %s\n\n", buf);
+		local_flag = FAILED;
+	}
+
+	/* Read past spaces in output */
+	fread(&ch, sizeof(ch), 1, fp);
+	while(isspace(ch))
+		fread(&ch, sizeof(ch), 1, fp);
+	ungetc(ch, fp);
+
+	clearbuf();
+	fread(buf, sizeof(buf[0]), strlen(str2), fp);
+	fclose(fp);
+	if (strcmp(str2, buf) != 0) {
+		fprintf(temp, "Expected string: %s\n", str2);
+		fprintf(temp, "does not match\n");
+		fprintf(temp, "received string: %s\n\n", buf);
+		local_flag = FAILED;
+	}
+
+	blexit();
+/*--------------------------------------------------------------*/
+block1: blenter();
+
+	/* Check that a system defined SEV_LEVEL cannot get redefined */
+
+	ret_val = addseverity(3, "INVALID");
+	if (ret_val != MM_NOTOK) {
+		fprintf(temp, "addseverity returned %d, expected MM_NOTOK\n",
+		        ret_val);
+		local_flag = FAILED;
+	}
+
+	blexit();
+/*--------------------------------------------------------------*/
+block2: blenter();
+
+	/* Check that we can define our own */
+	/* SEV_LEVEL and output is correct  */
+
+	ret_val = addseverity(5, "SPIE_TEST");
+	if (ret_val != MM_OK) {
+		fprintf(temp, "addseverity returned %d, expected MM_OK\n",
+		               ret_val);
+		local_flag = FAILED;
+        }
+
+	close(2);		/* redirect stderr to file */
+	fd = creat( "fmtfile", 0644);
+	ret_val = fmtmsg(MM_PRINT|MM_HARD|MM_OPSYS,"SPIE:fmtmsg", 5,
+			"SPIE fmtmsg() test2 message, NOT an error",
+			"This is correct output, no action needed","SPIE:msg:002");
+	close(fd);
+
+	if (ret_val != 0) {
+                fprintf(temp, "fmtmsg returned %d, expected 0\n",ret_val);
+		local_flag = FAILED;
+        }
+
+	fp = fopen("fmtfile", "r");
+	clearbuf();
+	fread(buf, sizeof(buf[0]), strlen(str3), fp);
+	if (strcmp(str3, buf) != 0) {
+                fprintf(temp, "Expected string: %s\n", str3);
+                fprintf(temp, "does not match\n");
+                fprintf(temp, "received string: %s\n\n", buf);
+                local_flag = FAILED;
+        }
+
+	/* Read past spaces in output */
+	fread(&ch, sizeof(ch), 1, fp);
+	while(isspace(ch))
+		fread(&ch, sizeof(ch), 1, fp);
+	ungetc(ch, fp);
+
+	clearbuf();
+	fread(buf, sizeof(buf[0]), strlen(str4), fp);
+	if (strcmp(str4, buf) != 0) {
+                fprintf(temp, "Expected string: %s\n", str4);
+                fprintf(temp, "does not match\n");
+                fprintf(temp, "received string: %s\n\n", buf);
+                local_flag = FAILED;
+        }
+
+	fclose(fp);
+	remove("fmtfile");
+
+	blexit();
+/*--------------------------------------------------------------*/
+block3: blenter();
+
+	 /* Test result of writing to /dev/console */
+
+	ret_val = fmtmsg(MM_CONSOLE|MM_HARD|MM_OPSYS,"SPIE:fmtmsg", 5,
+			"SPIE fmtmsg() test3 message, NOT an error",
+			"This is correct output, no action needed","SPIE:msg:003");
+	if (ret_val != MM_OK) {
+		fprintf(temp, "fmtmsg returned %d, expected MM_OK\n", ret_val);
+		fprintf(temp, "failed to write to console\n\n");
+		local_flag = FAILED;
+	}
+
+	blexit();
+/*--------------------------------------------------------------*/
+/* Clean up any files created by test before call to anyfail.   */
+
+	anyfail();      /* THIS CALL DOES NOT RETURN - EXITS!!  */
+}
+/*--------------------------------------------------------------*/
+
+/*****	LTP Port	*****/
+/* FUNCTIONS GO HERE */
+
+int anyfail()
+{
+  (local_flag == FAILED)? tst_resm(TFAIL, "Test failed\n"): tst_resm(TPASS, "Test passed\n");
+  tst_rmdir();
+  tst_exit();
+}
+
+
+
+void setup()
+{
+ temp = stderr;
+ tst_tmpdir();
+}
+
+
+int blenter()
+{
+   //tst_resm(TINFO, "Enter block %d\n", block_number);
+   local_flag = PASSED;
+   return(0);
+}
+
+int blexit()
+{
+   //tst_resm(TINFO, "Exitng test\n");
+   (local_flag == FAILED) ? tst_resm(TFAIL, "Test failed\n") : tst_resm(TPASS, "Test passed\n");
+   return(0);
+}
+
+
+/*****	**	**	*****/
