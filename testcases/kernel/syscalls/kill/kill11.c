e237cd34d0af09f8f8ada0a5721fdd39d6c53408
robbiew <robbiew>
2002-12-31 21:21:45 +0000

    Added kill() tests ported by Ananda Venkataraman <avenkat@us.ibm.com>

---
+++ testcases/kernel/syscalls/kill/kill11.c
@@ -0,0 +1,255 @@
+/* IBM Corporation */
+/* 01/02/2003	Port to LTP	avenkat@us.ibm.com */
+/* 06/30/2001	Port to Linux	nsharoff@us.ibm.com */
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2002
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+                           /*kill1.c*/
+/*======================================================================
+>KEYS:  < kill(), wait() 
+>WHAT:  < Check that when a child is killed by its parent, it returns 
+        < the correct values to the waiting parent--default behaviour 
+	< assumed by child
+>HOW:   < For each signal: send that signal to a child, check that the
+	< child returns the correct value to the waiting parent.
+>BUGS:  <
+>REQUIREMENT(S):  Need to set ulimit to multiples of 1024.
+======================================================================*/
+#define _GNU_SOURCE 1
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+/*****  LTP Port        *****/
+#include <errno.h>
+#include "test.h"
+#include "usctest.h"
+#define ITER    3
+#define FAILED 0
+#define PASSED 1
+
+
+char *TCID = "kill11";
+
+
+int local_flag = PASSED;
+int block_number;
+FILE *temp;
+int TST_TOTAL = 1;
+extern int Tst_count;
+
+int anyfail();
+int blenter();
+void setup();
+void terror();
+void fail_exit();
+int forkfail();
+
+/*****  **      **      *****/
+
+
+
+//char progname[] = "kill1()";
+
+
+/*--------------------------------------------------------------------*/
+int main()					/***** BEGINNING OF MAIN. *****/
+{
+	register int i;
+	int core;
+	int pid, npid;
+        int sig, nsig, exno, nexno, status;
+	int ret_val = 0;
+	/*SIGIOT is 6, but since linux doesn't have SIGEMT, just using
+		SIGIOT for place filling */
+	int signum[14];
+	int j;
+	signum[1]=SIGHUP;	signum[2]=SIGINT;	signum[3]=SIGQUIT;
+	signum[4]=SIGILL;	signum[5]=SIGTRAP;	signum[6]=SIGABRT;
+	signum[7]=SIGIOT;	signum[8]=SIGFPE;	signum[9]=SIGKILL;
+	signum[10]=SIGBUS;	signum[11]=SIGSEGV;	signum[12]=SIGSYS;
+	signum[13]=SIGPIPE;	signum[14]=SIGALRM;
+	setup();
+	// tempdir();		/* move to new directory */
+/*--------------------------------------------------------------------*/
+	blenter();			/*<<<<<ENTER DATA HERE<<<<<<<<*/
+
+
+	exno = 1;
+	unlink("core");
+
+	for(j = 1; j < 14; j++) {
+		sig=signum[j];
+		if (sig != SIGKILL )
+#ifndef BCS
+		    if( sig != SIGSTOP)
+#endif
+			if (sigset(sig, SIG_DFL) == SIG_ERR) {
+				fprintf(temp, "\tsigset(%d,,) fails\n", sig);
+				local_flag = FAILED;
+				fail_exit();
+			}
+		fflush(temp);
+		pid = fork();
+
+		if (pid < 0) {
+			forkfail();
+		}
+
+		/*
+		 * Child process sleeps for up to 3 minutes giving the
+		 * parent process a chance to kill it.
+		 */
+		if (pid == 0) {
+			for (i=0; i < 180; i++)
+				sleep(1);
+			fprintf(temp, "\tChild missed sig %d\n", sig);
+			fflush(temp);
+			_exit(exno);
+		}
+
+		/*
+		 * Parent process sends signal to child.
+		 */
+
+		//fprintf(temp, "Testing signal %d\n", sig); 12/12/02
+		kill (pid, sig);
+		npid = wait(&status);
+   
+		if (npid != pid)  {
+			fprintf(temp, "wait error: unexpected pid returned\n");
+			ret_val = 1;
+		}
+		/* 12/20/02.
+		nsig = status & 0177;
+		core = status & 0200;
+		nexno = (status & 0xff00) >> 8;
+		*/
+                /*****  LTP Port        *****/
+                nsig = WTERMSIG(status);
+                #ifdef WCOREDUMP
+                        core = WCOREDUMP(status);
+                #endif
+                nexno = WIFEXITED(status);
+                /*****  **      **      *****/
+ 
+		//printf("nsig=%x, core=%x, status=%x\n", nsig,core, status); 12/12/2002
+
+	/* to check if the core dump bit has been set, bit # 7 */
+	/*****	LTP Port	*****/
+	/*  12/12/02: avenkat@us.ibm.com
+	 *  SIGILL when is not caught or not ignored it causes 
+	 *  a core dump and program termination.  So moved the condition to
+	 *  else part of the program.
+	 *  SIGQUIT like SIGABRT normally causes a program to quit and
+	 *  and dumps core.  So moved the condition to else part of the
+	 *  program.	 
+	 */
+	/*****	**	**	*****/
+		if (core) {
+			if ((sig == 1) || (sig == 2) /*|| (sig == 3) ||*/
+			   /*(sig == 4)*/ || (sig == 9) ||
+			   (sig == 13) || (sig == 14) || (sig == 15)) {
+				fprintf(temp, "signal error: core dump bit set for exception number %d\n", sig); 
+				ret_val = 1;
+			}
+		} else {
+			if((sig ==3) || (sig ==4) || (sig == 5) || (sig == 6) || 			   (sig == 7) ||
+			(sig == 8) || (sig == 10) || (sig == 11) ||
+			(sig == 12)) {
+				fprintf(temp, "signal error: core dump bit not set for exception number %d\n", sig);
+				ret_val = 1;
+			}
+		}
+
+	/* nsig is the signal number returned by wait */
+
+		if (nsig != sig) {
+			fprintf(temp, "wait error: unexpected signal %d returned, expected %d\n", nsig, sig);
+			ret_val = 1;
+		}
+
+	/* nexno is the exit number returned by wait  */
+
+		if (nexno != 0)  {
+			fprintf(temp, "signal error: unexpected exit number %d returned, expected 0\n", nexno);
+			ret_val = 1;
+		}
+	}
+	unlink("core");
+	fflush(temp);
+	if(ret_val)
+		local_flag = FAILED;
+	unlink("core");
+	tst_rmdir();
+/*--------------------------------------------------------------------*/
+	anyfail();
+	return(0);
+}
+
+/****** LTP Port        *****/
+int anyfail()
+{
+  (local_flag == FAILED)? tst_resm(TFAIL, "Test failed\n"): tst_resm(TPASS, "Test passed\n");
+  tst_exit();
+  return(0);
+}
+
+
+
+
+void setup()
+{
+ temp = stderr;
+ tst_tmpdir();
+}
+
+int blenter()
+{
+   //tst_resm(TINFO, "Enter block %d\n", block_number);
+   local_flag = PASSED;
+   return(0);
+}
+
+
+void terror(char * message)
+{
+  tst_resm(TBROK, "Reason: %s:%s\n", message, strerror(errno));
+}
+
+void fail_exit()
+{
+  local_flag = FAILED;
+  anyfail();
+
+}
+
+int forkfail()
+{
+        tst_resm(TBROK, "FORK FAILED - terminating test.");
+        tst_exit();
+	return(0);
+}
+/****** ** **   *******/
+
---
+++ testcases/kernel/syscalls/kill/kill12.c
@@ -0,0 +1,248 @@
+/* IBM Corporation */
+/* 01/02/2003	Port to LTP avenkat@us.ibm.com */
+/* 06/30/2001	Port to Linux	nsharoff@us.ibm.com */
+
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2002
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+                           /*kill2.c*/
+/*======================================================================
+>KEYS:  < kill(), wait(), signal()
+>WHAT:  < Check that when a child is killed by its parent, it returns the 
+   	< correct values to the waiting parent--the child sets signal to 
+   	< ignore the kill							
+>HOW:   < For each signal: Send that signal to a child that has elected
+	< to catch the signal, check that the correct status was returned
+	< to the waiting parent.
+	< NOTE: Signal 9 (kill) is not catchable, and must be dealt with
+	< separately.
+>BUGS:  < None known 
+======================================================================*/
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
+
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+//char progname[] = "kill2()";
+/*****  LTP Port        *****/
+#include "test.h"
+#include "usctest.h"
+#define ITER    3
+#define FAILED 0
+#define PASSED 1
+
+
+char *TCID = "kill11";
+
+
+int local_flag = PASSED;
+int block_number;
+FILE *temp;
+int TST_TOTAL = 1;
+extern int Tst_count;
+
+int anyfail();
+int blenter();
+int instress();
+void setup();
+void terror();
+void fail_exit();
+void ok_exit();
+int forkfail();
+
+/*****  **      **      *****/
+
+
+
+int chflag;
+
+/*--------------------------------------------------------------------*/
+int main()					/***** BEGINNING OF MAIN. *****/
+{
+	int pid, npid;
+        int sig, nsig, exno, nexno, status;
+	int ret_val = 0;
+	int core;
+	void chsig();
+
+	setup();
+	//tempdir();		/* move to new directory */ 12/20/2003
+/*--------------------------------------------------------------------*/
+	blenter();			/*<<<<<ENTER DATA HERE<<<<<<<<*/
+
+	exno = 1;
+
+	if (sigset(SIGCLD, chsig) == SIG_ERR) {
+		fprintf(temp, "\tsigset failed, errno = %d\n", errno);
+		fail_exit();
+	}
+
+	for(sig = 1; sig < 14; sig++) {
+		fflush(temp);
+		chflag = 0;
+
+		pid = fork();
+		if (pid < 0) {
+			forkfail();
+		}
+
+		if (pid == 0) {
+			sigset(sig, SIG_IGN);		/* set to ignore signal */
+			kill(getppid(), SIGCLD);	/* tell parent we are ready */
+			while (!chflag)
+				sleep(1);		/* wait for parent */
+
+			exit(exno);
+		} else {
+			//fprintf(temp, "Testing signal %d\n", sig);
+
+			while (!chflag)		/* wait for child */
+				sleep(1);
+
+			kill (pid, sig);	/* child should ignroe this sig */
+			kill(pid, SIGCLD);	/* child should exit */
+
+#ifdef BCS
+			while((npid = wait(&status)) != pid || (npid == -1 && errno == EINTR))
+					;
+			if( npid != pid ) {
+				fprintf(temp, "wait error: wait returned wrong pid\n");
+				ret_val = 1;
+			}
+#else
+			while((npid = waitpid(pid, &status, 0)) != -1 || errno == EINTR)
+					;
+#endif
+
+			/*
+			nsig = status & 0177;
+			core = status & 0200;
+			nexno = (status & 0xff00) >> 8;
+			*/
+                	/*****  LTP Port        *****/
+                	nsig = WTERMSIG(status);
+                	#ifdef WCOREDUMP
+                        	core = WCOREDUMP(status);
+                	#endif
+                	nexno = WIFEXITED(status);
+                	/*****  **      **      *****/
+
+
+		/* nsig is the signal number returned by wait 
+                   it should be 0, except when sig = 9		*/
+
+			if ((sig == 9) && (nsig != sig)) { 
+				fprintf(temp, "wait error: unexpected signal"
+					" returned when the signal sent was 9"
+				        " The status of the process is %d \n",	
+					 status);
+				ret_val = 1;
+			}
+			if ((sig != 9) && (nsig != 0)) {
+				fprintf(temp, "wait error: unexpected signal " 
+				       "returned, the status of the process is "
+					"%d  \n",status);
+				ret_val = 1;
+			}
+
+		/* nexno is the exit number returned by wait
+		   it should be 1, except when sig = 9          */
+
+			if (sig == 9) 
+				if (nexno != 0) {
+					fprintf(temp, "signal error: unexpected"
+						" exit number returned when" 
+						" signal sent was 9, the status"
+						" of the process is %d \n",
+						status);
+				       ret_val = 1;
+				}
+				else; 
+			else if (nexno != 1) {
+				fprintf(temp, "signal error: unexpected exit "
+					"number returned,the status of the"
+					" process is %d\n",status);
+				ret_val = 1;
+			}
+		}
+	}
+	if(ret_val)
+		local_flag = FAILED;
+
+/*--------------------------------------------------------------------*/
+	anyfail();	
+	return(0);
+}					/******** END OF MAIN. ********/
+/*--------------------------------------------------------------------*/
+
+void chsig()
+{
+	chflag++;
+}
+
+/****** LTP Port        *****/
+int anyfail()
+{
+  (local_flag == FAILED)? tst_resm(TFAIL, "Test failed\n"): tst_resm(TPASS, "Test passed\n");
+  tst_exit();
+  return(0);
+}
+
+
+void setup()
+{
+ temp = stderr;
+}
+
+int blenter()
+{
+   //tst_resm(TINFO, "Enter block %d\n", block_number);
+   local_flag = PASSED;
+   return(0);
+}
+
+
+void terror(char * message)
+{
+  tst_resm(TBROK, "Reason: %s:%s\n", message, strerror(errno));
+}
+
+void fail_exit()
+{
+  local_flag = FAILED;
+  anyfail();
+
+}
+
+int forkfail()
+{
+        tst_resm(TBROK, "FORK FAILED - terminating test.");
+        tst_exit();
+   return(0);
+}
+/****** ** **   *******/
+
