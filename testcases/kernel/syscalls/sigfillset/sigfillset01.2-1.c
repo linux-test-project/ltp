cee7eb16bdaf1a52c31971c26f1064f49a3fac8e
robbiew <robbiew>
2003-02-21 22:20:17 +0000

    Added Open POSIX* Test: sigfillset

---
+++ testcases/kernel/syscalls/sigfillset/sigfillset01.2-1.c
@@ -0,0 +1,33 @@
+/*   
+ * Copyright (c) 2002, Intel Corporation. All rights reserved.
+ * Created by:  salwan.searty REMOVE-THIS AT intel DOT com
+ * This file is licensed under the GPL license.  For the full content
+ * of this license, see the COPYING file at the top level of this 
+ * source tree.
+ 
+ Simply, as long as sigfillset returns a 0, the test passes.
+*/
+
+#include <stdio.h>
+#include <signal.h>
+#include "posixtest.h"
+
+int main()
+{
+	sigset_t signalset;
+/*
+	int returnval;
+	
+	returnval = sigfillset(&signalset);
+
+	if (returnval != 0) {
+*/
+	if (sigfillset(&signalset) != 0) {
+		perror("sigfillset failed -- test aborted");
+		return PTS_FAIL;
+	} 
+	#ifdef DEBUG
+		printf("sigfillset passed\n");
+	#endif
+	return PTS_PASS;	
+}
