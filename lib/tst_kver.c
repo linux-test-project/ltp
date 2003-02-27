fea599ea1c8cec0c54ee284d6a88e69df017f1cd
plars <plars>
2003-02-27 16:53:21 +0000

    Added tst_kver() ltp function to test for what version of the kernel you are running.  This is useful for 2.5 only tests, and tests that use obsolete kernel api's (such as create_module, query_module, etc).

---
+++ lib/tst_kver.c
@@ -0,0 +1,65 @@
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2003
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+ 
+/*
+ * 
+ *    AUTHOR
+ *    	Paul Larson <plars@linuxtestproject.org>
+ *
+ *    DESCRIPTION
+ * 	Compare a given kernel version against the current kernel version.
+ * 	If they are the same - return 0
+ * 	If the argument is > current kernel version - return positive int
+ * 	If the argument is < current kernel version - return negative int
+ *
+ */
+
+
+
+#include <unistd.h>
+#include <string.h>
+#include <sys/utsname.h>
+
+void get_kver(int *k1, int *k2, int *k3)
+{
+	struct utsname uval;
+	char *kver;
+	char *r1, *r2, *r3;
+	
+	uname(&uval);
+	kver = uval.release;
+	r1 = strsep(&kver, ".");
+	r2 = strsep(&kver, ".");
+	r3 = strsep(&kver, ".");
+
+	*k1 = atoi(r1);
+	*k2 = atoi(r2);
+	*k3 = atoi(r3);
+}
+
+int tst_kver(int r1, int r2, int r3) {
+	int a1, a2, a3;
+	int testver, currver;
+
+	get_kver(&a1, &a2, &a3);
+	testver = (r1 << 16) + (r2 << 8) + r3;
+	currver = (a1 << 16) + (a2 << 8) + a3;
+
+	return testver - currver;
+}
