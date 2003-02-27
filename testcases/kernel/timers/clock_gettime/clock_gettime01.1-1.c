1bd5bc5d9778ce02a4277aa55b260507e6293c81
robbiew <robbiew>
2003-02-27 15:35:54 +0000

    Added Open POSIX* Testsuite test: clock_gettime

---
+++ testcases/kernel/timers/clock_gettime/clock_gettime01.1-1.c
@@ -0,0 +1,34 @@
+/*   
+ * Copyright (c) 2002, Intel Corporation. All rights reserved.
+ * Created by:  julie.n.fleischer REMOVE-THIS AT intel DOT com
+ * This file is licensed under the GPL license.  For the full content
+ * of this license, see the COPYING file at the top level of this 
+ * source tree.
+
+   General test that clock_gettime() returns a non-empty tp for a given
+   clock_id (the clock_id chosen for this test is CLOCK_REALTIME).
+ */
+#include <stdio.h>
+#include <time.h>
+#include "posixtest.h"
+
+int main(int argc, char *argv[])
+{
+	struct timespec tp;
+
+	if (clock_gettime(CLOCK_REALTIME, &tp) == 0) {
+		if (NULL != &tp) {
+			printf("Test PASSED\n");
+			return PTS_PASS;
+		} else {
+			printf("clock_gettime() success, but tp not filled\n");
+			return PTS_FAIL;
+		}
+	} else {
+		printf("clock_gettime() failed\n");
+		return PTS_FAIL;
+	}
+
+	printf("This code should not be executed.\n");
+	return PTS_UNRESOLVED;
+}
---
+++ testcases/kernel/timers/clock_gettime/clock_gettime01.7-1.c
@@ -0,0 +1,29 @@
+/*   
+ * Copyright (c) 2002, Intel Corporation. All rights reserved.
+ * Created by:  julie.n.fleischer REMOVE-THIS AT intel DOT com
+ * This file is licensed under the GPL license.  For the full content
+ * of this license, see the COPYING file at the top level of this 
+ * source tree.
+
+   Test that clock_gettime() returns -1 on failure.
+ */
+#include <stdio.h>
+#include <time.h>
+#include "posixtest.h"
+
+#define INVALIDCLOCK 9999
+int main(int argc, char *argv[])
+{
+	struct timespec tp;
+
+	if (clock_gettime(INVALIDCLOCK, &tp) == -1) {
+		printf("Test PASSED\n");
+		return PTS_PASS;
+	} else {
+		printf("Test FAILED\n");
+		return PTS_FAIL;
+	}
+
+	printf("This code should not be executed.\n");
+	return PTS_UNRESOLVED;
+}
