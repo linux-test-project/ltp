084633dfcc2e1b1ed2b0d26a91e848a2c725e711
robbiew <robbiew>
2003-03-14 18:12:13 +0000

    Header file created by Andreas Jaeger to remove compiler warnings.

---
+++ testcases/kernel/io/direct_io/diotest_routines.h
@@ -0,0 +1,12 @@
+
+struct iovec;
+extern void fillbuf(char *buf, int count, char value);
+extern int filecmp(char *f1, char *f2);
+extern int bufcmp(char *b1, char *b2, int bsize);
+extern int vbufcmp(struct iovec *iv1, struct iovec *iv2, int vcnt);
+
+extern int forkchldrn(int **pidlst, int numchld, int action, int (*chldfunc)());
+extern int waitchldrn(int **pidlst, int numchld);
+extern int killchldrn(int **pidlst, int numchld, int sig);
+
+
