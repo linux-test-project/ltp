d00e71c9fc4d52af400b8e180b7b5ebfbef1bbd9
robbiew <robbiew>
2003-04-11 19:39:19 +0000

    Added tests for clock_getres() from posixtestsuite 0.9.0

---
+++ testcases/kernel/timers/clock_getres/clock_getres01.3-1.c
@@ -0,0 +1,51 @@
+/*   
+ * Copyright (c) 2002, Intel Corporation. All rights reserved.
+ * Created by:  julie.n.fleischer REMOVE-THIS AT intel DOT com
+ * This file is licensed under the GPL license.  For the full content
+ * of this license, see the COPYING file at the top level of this 
+ * source tree.
+
+ * pt:MON
+ * Test that clock_getres() supports a clock_id of CLOCK_MONOTONIC if
+ * pt:MON.
+ */
+#include <stdio.h>
+#include <time.h>
+#include "posixtest.h"
+
+#define LARGENUM 100000
+
+int main(int argc, char *argv[])
+{
+#ifdef CLOCK_MONOTONIC
+	struct timespec res;
+
+        /* Initialize res to a number much larger than the resolution
+         * could possibly be
+         */
+        res.tv_sec = LARGENUM;
+        res.tv_nsec = LARGENUM;
+        if (clock_getres(CLOCK_MONOTONIC, &res) == 0) {
+                if (res.tv_sec != LARGENUM) { //assume initialized
+#ifdef DEBUG
+                        printf("Resolution is %d sec %d nsec\n",
+                                        (int) res.tv_sec,
+                                        (int) res.tv_nsec);
+#endif
+                        printf("Test PASSED\n");
+                        return PTS_PASS;
+                } else {
+                        printf("clock_getres() success, but res not filled\n");
+                }
+        } else {
+                printf("clock_getres() failed\n");
+        }
+
+        printf("Test FAILED\n");
+        return PTS_FAIL;
+#else
+	printf("CLOCK_MONOTONIC unsupported\n");
+	return PTS_UNSUPPORTED;
+#endif
+
+}
