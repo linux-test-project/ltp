59f17d12b97cdae8db741a2b9ae91f33b67a8bb0
robbiew <robbiew>
2003-04-16 17:55:23 +0000

    Relocated the test scripts to the /testscripts directory.

---
+++ testscripts/diskio.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# These tests require the corresponding media to be present:
+#
+#  stress_floppy: Requires a writeable HD floppy disk
+#
+#  stress_cd: Requires ANY data CD with a minimum of 100MB of data.
+#
+# NOTE: The stress_floppy test is easily modifiable for use on any
+#       write device, i.e. tape drive, zip drive....etc
+#
+    
+cd `dirname $0`
+export LTPROOT=${PWD}
+
+export TMPDIR=/tmp
+
+mkdir /tmp/diskiopan-$$
+cd /tmp/diskiopan-$$
+
+export PATH="${PATH}:${LTPROOT}/testcases/bin"
+ 
+${LTPROOT}/ver_linux
+
+${LTPROOT}/pan/pan -e -l /tmp/diskiopan.log -S -a ltpdiskio -n ltpdiskio -f ${LTPROOT}/runtest/io
+
+if [ $? -eq "0" ]; then
+  echo pan reported PASS
+else
+  echo pan reported FAIL
+fi
+
---
+++ testscripts/networktests.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# This will run all the network tests, with the status logged in /tmp/netpan.log  
+cd `dirname $0`
+export LTPROOT=${PWD}
+
+
+# ---***** THESE MUST BE SET FOR CORRECT OPERATION *****---
+export RHOST=
+export PASSWD=
+# ---***************************************************---
+
+# ---***** NFS OPTIONAL SETTINGS *****---
+# DEFAULTS
+# export VERSION=2 
+# export SOCKET_TYPE=udp
+# ---*********************************---
+
+
+export TMPDIR=/tmp/netpan-$$
+mkdir $TMPDIR
+
+cat  ${LTPROOT}/runtest/tcp_cmds > $TMPDIR/network.tests
+cat  ${LTPROOT}/runtest/multicast >> $TMPDIR/network.tests
+cat  ${LTPROOT}/runtest/rpc >> $TMPDIR/network.tests
+cat  ${LTPROOT}/runtest/nfs >> $TMPDIR/network.tests
+
+cd $TMPDIR
+
+export PATH="${PATH}:${LTPROOT}/testcases/bin"
+ 
+${LTPROOT}/ver_linux
+
+${LTPROOT}/pan/pan -e -l /tmp/netpan.log -S -a ltpnet -n ltpnet -f ${TMPDIR}/network.tests
+
+if [ $? -eq "0" ]; then
+  echo pan reported PASS
+else
+  echo pan reported FAIL
+fi
+
+rm -rf ${TMPDIR}
