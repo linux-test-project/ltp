869fdf7c2d1bda76c07ed7a4071972744f50e809
robbiew <robbiew>
2003-06-18 17:00:05 +0000

    Typo corrections.

--- testcases/kernel/syscalls/recvmsg/recvmsg01.c
+++ testcases/kernel/syscalls/recvmsg/recvmsg01.c
@@ -45,10 +45,11 @@
 /* and this test to function correctly.                        */
 #ifndef MSG_CMSG_COMPAT
 
-#if defined (__powerpc64__) || defined (__x86_64__) || define (__sparc64__)
+#if defined (__powerpc64__) || defined (__x86_64__) || defined (__sparc64__)
 #define MSG_CMSG_COMPAT 0x80000000
 #else
 #define MSG_CMSG_COMPAT 0
+#endif
 
 #endif
 /***************************************************/
