4bf7f1b516b6dc2c20d19d23b203578e805a8b93
robbiew <robbiew>
2003-07-23 17:19:33 +0000

    Change test to return TINFO on failure and mention that some distros, such ash RedHat Enterprise Linux 3, allow non-root users to munlockall within the RLIMIT_MEMLOCK resource limit.

--- testcases/kernel/syscalls/munlockall/munlockall02.c
+++ testcases/kernel/syscalls/munlockall/munlockall02.c
@@ -111,8 +111,8 @@ int main(int ac, char **av)
 				" as expected for non-superuser"
 				":GOT EPERM");			
 		} else {
-	  		tst_resm(TFAIL, "munlockall() failed to produce "	
-					"expected errno :%d Got : %d, %s",
+	  		tst_resm(TINFO, "munlockall() failed to produce "	
+					"expected errno :%d Got : %d, %s. Some distros support non-superuser munlockall calls.",
 				EPERM,TEST_ERRNO,
 				strerror(TEST_ERRNO));
 					  
