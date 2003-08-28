5427d345761490ab9bff6eb225e93c0931ebebb5
mridge <mridge>
2003-08-28 21:06:05 +0000

    updatreas for 2.5.73 kernel

--- testcases/kernel/device-drivers/pci/user_tpci/user_tpci.c
+++ testcases/kernel/device-drivers/pci/user_tpci/user_tpci.c
@@ -3,7 +3,9 @@
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <linux/kernel.h>
+#include <linux/errno.h>
 #include "user_tpci.h"
 #include "../tpci/tpci.h"
 
@@ -11,19 +13,75 @@ static int tpci_fd = -1;	/* file descriptor */
 
 int
 tpciopen() {
-	struct stat	st;
 
-	if (stat("/dev/tpci", &st)) {
-		printf("\tERROR, Failed finding test pci kernel interface \n");
-		return (1);
-	}
+    dev_t devt;
+	struct stat     st;
+    int    rc = 0;
 
-	if ((tpci_fd = open("/dev/tpci", O_RDWR)) < 0) {
-		printf("\tFailed opening test pci kernel interface \n");
-		return (1);
-	}
-	
-	return 0;
+    devt = makedev(TPCI_MAJOR, 0);
+
+    if (rc) {
+        if (errno == ENOENT) {
+            /* dev node does not exist. */
+            rc = mkdir(DEVICE_NAME, (S_IFDIR | S_IRWXU |
+                                                S_IRGRP | S_IXGRP |
+                                                S_IROTH | S_IXOTH));
+        } else {
+            printf("ERROR: Problem with Base dev directory.  Error code from stat() is %d\n\n", errno);
+        }
+
+    } else {
+        if (!(st.st_mode & S_IFDIR)) {
+            rc = unlink(DEVICE_NAME);
+            if (!rc) {
+                rc = mkdir(DEVICE_NAME, (S_IFDIR | S_IRWXU |
+                                                S_IRGRP | S_IXGRP |
+                                                S_IROTH | S_IXOTH));
+            }
+        }
+    }
+
+
+    /*
+     * Check for the /dev/tbase node, and create if it does not
+     * exist.
+     */
+    rc = stat(DEVICE_NAME, &st);
+    if (rc) {
+        if (errno == ENOENT) {
+            /* dev node does not exist */
+            rc = mknod(DEVICE_NAME, (S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP), devt);
+        } else {
+            printf("ERROR:Problem with tbase device node directory.  Error code form stat() is %d\n\n", errno);
+        }
+
+    } else {
+        /*
+         * /dev/tbase CHR device exists.  Check to make sure it is for a
+         * block device and that it has the right major and minor.
+         */
+        if ((!(st.st_mode & S_IFCHR)) ||
+             (st.st_rdev != devt)) {
+
+            /* Recreate the dev node. */
+            rc = unlink(DEVICE_NAME);
+            if (!rc) {
+                rc = mknod(DEVICE_NAME, (S_IFCHR | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP), devt);
+            }
+        }
+    }
+
+    tpci_fd = open(DEVICE_NAME, O_RDWR);
+
+    if (tpci_fd < 0) {
+        printf("ERROR: Open of device %s failed %d errno = %d\n", DEVICE_NAME,tpci_fd, errno);
+        return errno;
+    }
+    else {
+        printf("Device opened successfully \n");
+        return 0;
+    }
+
 }
 
 int 
