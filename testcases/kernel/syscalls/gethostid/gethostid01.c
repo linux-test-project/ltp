fa45d7d51d288243f96a0bce3cd02ae0dded78ef
robbiew <robbiew>
2005-10-04 15:54:53 +0000

    Second attempt to resolve "ffffffff" situation.

--- testcases/kernel/syscalls/gethostid/gethostid01.c
+++ testcases/kernel/syscalls/gethostid/gethostid01.c
@@ -30,7 +30,7 @@
  * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
  *
  */
-/* $Id: gethostid01.c,v 1.5 2005/10/03 16:20:28 robbiew Exp $ */
+/* $Id: gethostid01.c,v 1.6 2005/10/04 15:54:53 robbiew Exp $ */
 /**********************************************************
  * 
  *    OS Test - Silicon Graphics, Inc.
@@ -138,7 +138,7 @@ main(int ac, char **av)
     int lc,i;		/* loop counters */
     int bit_64;          /* used when compiled 64bit on some 64bit machines */
     char *msg;		/* message returned from parse_opts */
-    char name[HOSTIDLEN], name2[HOSTIDLEN], hostid[HOSTIDLEN], hex[2]="0x";
+    char name[HOSTIDLEN], name2[HOSTIDLEN], hostid[HOSTIDLEN], hostid2[HOSTIDLEN], hex[2]="0x";
     char hex_64[1]="f";
     FILE *fp;
 
@@ -214,14 +214,18 @@ main(int ac, char **av)
 				    		 name2, hostid);
 		} else {
 			for (i=0;i<8;i++){
-				if (name[i]==hex_64[0])
+				if (hostid[i]==hex_64[0])
 					bit_64=1;
 				else
 					bit_64=0;
 			}
-			if ((bit_64==1) && (strcmp(name2, hostid) == 0))
-				tst_resm(TPASS, "Hostid command and gethostid both report hostid "
-                                                "is %s", hostid);
+			if (bit_64 == 1){
+				for (j=0;j<8;j++)
+					hostid2[j]=hostid[j+8];
+				if (strcmp(name2, hostid) == 0)
+					tst_resm(TPASS, "Hostid command and gethostid both report hostid "
+                                        	        "is %s", hostid);
+			}
 			else
 		    		tst_resm(TFAIL, "Hostid command reports hostid is %s, "
 		  		        "but gethostid() reports %s", 
