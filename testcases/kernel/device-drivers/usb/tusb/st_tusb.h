3889d936787e03cdf934a7753a284e5949e12e88
mridge <mridge>
2006-01-09 19:17:16 +0000

    Updates

--- testcases/kernel/device-drivers/usb/tusb/st_tusb.h
+++ testcases/kernel/device-drivers/usb/tusb/st_tusb.h
@@ -1,4 +1,22 @@
-//st_tusb.h
+/*
+ *
+ *   Copyright (c) International Business Machines  Corp., 2001
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+*/
 extern struct usb_bus *usb_alloc_bus(struct usb_operations *);
 extern void usb_connect(struct usb_device *dev);
 extern int usb_new_device(struct usb_device *dev);
