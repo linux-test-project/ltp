e6b8d4ebbc2beff33d6070dbd1755128094a2b34
robbiew <robbiew>
2006-06-26 17:34:59 +0000

    Corrected definition section for __powerpc__

--- testcases/kernel/syscalls/sysctl/sysctl01.c
+++ testcases/kernel/syscalls/sysctl/sysctl01.c
@@ -56,7 +56,7 @@ char *TCID = "sysctl01";
 int TST_TOTAL = 3;
 extern int Tst_count;
 
-#if defined(__ia64__)
+#if defined(__ia64__) || defined(__powerpc__)
 #define _sysctl(arg1) syscall(__NR__sysctl, arg1)
 #else
 _syscall1(int, _sysctl, struct __sysctl_args *, args);
