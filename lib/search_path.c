85944b712a3815ef073e27030f3f34b49a36de03
robbiew <robbiew>
2006-07-19 14:00:55 +0000

    Corrected the copyright information to reflect SGI, instead of Cray, which was purchased by SGI years ago.

--- lib/search_path.c
+++ lib/search_path.c
@@ -1,10 +1,38 @@
-/* $Header: /cvsroot/ltp/ltp/lib/search_path.c,v 1.2 2003/03/04 18:33:41 robbiew Exp $ */
+/* $Header: /cvsroot/ltp/ltp/lib/search_path.c,v 1.3 2006/07/19 14:00:56 robbiew Exp $ */
 
 /*
- *	(C) COPYRIGHT CRAY RESEARCH, INC.
- *	UNPUBLISHED PROPRIETARY INFORMATION.
- *	ALL RIGHTS RESERVED.
+ * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Further, this software is distributed without any warranty that it is
+ * free of the rightful claim of any third person regarding infringement
+ * or the like.  Any license provided herein, whether implied or
+ * otherwise, applies only to this software file.  Patent licenses, if
+ * any, provided herein do not apply to combinations of this program with
+ * other software, or any other product whatsoever.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write the Free Software Foundation, Inc., 59
+ * Temple Place - Suite 330, Boston MA 02111-1307, USA.
+ *
+ * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
+ * Mountain View, CA  94043, or:
+ *
+ * http://www.sgi.com
+ *
+ * For further information regarding this notice, see:
+ *
+ * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
  */
+
+
 /**********************************************************
  * 
  *    UNICOS Feature Test and Evaluation - Cray Research, Inc.
