38137e54faecc949392d6fe609e7af0b683baf9c
vapier <vapier>
2006-08-21 06:23:48 +0000

    easier to read

--- testcases/kernel/syscalls/fchown/change_owner.mode.sh
+++ testcases/kernel/syscalls/fchown/change_owner.mode.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-whoami | grep root > /dev/null
-if [ $? -eq 0 ];then
-  chown root change_owner
-  chmod 04755 change_owner
-else
- echo ""
- echo "		         ************** WARNING **************"
- echo "		Cannot change permission or ownership of \"change_owner\"."
- echo "		           Tests in this directory will fail" 
- echo "                       Run "make install" as root."
- echo "		         *************************************"
- sleep 2
-fi
 
+failed() {
+	echo ""
+	echo "             ************** WARNING **************"
+	echo "    Cannot change permission or ownership of \"change_owner\"."
+	echo "               Tests in this directory will fail" 
+	echo "                       Run "make install" as root."
+	echo "             *************************************"
+	sleep 2
+}
+
+chown root change_owner || failed
+chmod 04755 change_owner || failed
+
+exit 0
