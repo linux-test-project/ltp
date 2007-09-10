96c4b24ef12c2a0afc7ac9f7fa73326521815cf9
vapier <vapier>
2007-09-10 03:07:07 +0000

    dont use the optional POSIX postfix operator as some crappy shells actually omit it

--- testcases/network/generate.sh
+++ testcases/network/generate.sh
@@ -54,7 +54,7 @@ $makeit $data_dir/$jumbo_file $jumbo_size
 
 if [ ! -e $data_dir/bin.sm ] ; then
 	cnt=6
-	while [ $((cnt--)) -gt 0 ] ; do
+	while [ $((cnt=cnt-1)) -ge 0 ] ; do
 		gzip -1 -c datafiles/ascii.sm >> $data_dir/bin.sm
 	done
 fi
@@ -67,7 +67,7 @@ genfile() {
 		return 0
 	fi
 
-	while [ $((cnt--)) -gt 0 ] ; do
+	while [ $((cnt=cnt-1)) -ge 0 ] ; do
 		cat $input >> $output
 	done
 }
