c2aab6c5dac302afdd6a40ac93459c2330b47af6
subrata_modak <subrata_modak>
2007-12-28 09:48:30 +0000

    Changed the way Copyrights and License agreements are declared

--- testcases/kernel/containers/sysvipc/shmnstest.c
+++ testcases/kernel/containers/sysvipc/shmnstest.c
@@ -1,11 +1,25 @@
 /*
- * Copyright 2007 IBM
- * Author: Serge Hallyn <serue@us.ibm.com>
- *
- * Create shm with key 0xEAEAEA
- * clone, clone(CLONE_NEWIPC), or unshare(CLONE_NEWIPC)
- * In cloned process, try to get the created shm
- */
+* Copyright (c) International Business Machines Corp., 2007
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+* the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Serge Hallyn <serue@us.ibm.com>
+*
+* Create shm with key 0xEAEAEA
+* clone, clone(CLONE_NEWIPC), or unshare(CLONE_NEWIPC)
+* In cloned process, try to get the created shm
+
+***************************************************************************/
 
 #define _GNU_SOURCE 1
 #include <sys/wait.h>
--- testcases/kernel/containers/utsname/check_utsns_enabled.c
+++ testcases/kernel/containers/utsname/check_utsns_enabled.c
@@ -1,11 +1,24 @@
 /*
- * Copyright 2007 IBM
- * Author: Serge Hallyn <serue@us.ibm.com>
- *
- * uts namespaces were introduced around 2.6.19.  Kernels before that,
- * assume they are not enabled.  Kernels after that, check for -EINVAL
- * when trying to use CLONE_NEWUTS.
- */
+* Copyright (c) International Business Machines Corp., 2007
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+* the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+* Author: Serge Hallyn <serue@us.ibm.com>
+*
+* uts namespaces were introduced around 2.6.19.  Kernels before that,
+* assume they are not enabled.  Kernels after that, check for -EINVAL
+* when trying to use CLONE_NEWUTS.
+***************************************************************************/
 
 #include <sys/utsname.h>
 #include <sched.h>
