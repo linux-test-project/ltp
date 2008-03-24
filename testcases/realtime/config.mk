d104a94f8f09ac0bdc1a47884c44448a0fbf20fd
subrata_modak <subrata_modak>
2008-03-24 09:13:49 +0000

    Add Makefiles to all the realtime testcases without any autotools dependencies. Signed-off-by: Sebastien Dugue <sebastien.dugue@bull.net>

---
+++ testcases/realtime/config.mk
@@ -0,0 +1,24 @@
+
+
+# Check kernel/glibc support for PI and Robust mutexes and cache
+# the result.
+ifeq ($(shell [ -e $(srcdir)/.config ] && echo yes), yes)
+	HAVE_PI_MUTEX := $(shell grep HAVE_PI_MUTEX $(srcdir)/.config | \
+				 awk '{print $$2}')
+	HAVE_ROBUST_MUTEX := $(shell grep HAVE_ROBUST_MUTEX $(srcdir)/.config | \
+				     awk '{print $$2}')
+else
+	HAVE_PI_MUTEX := $(shell sh $(srcdir)/scripts/check_pi.sh)
+	HAVE_ROBUST_MUTEX:= $(shell sh $(srcdir)/scripts/check_robust.sh)
+	dummy := $(shell echo "HAVE_PI_MUTEX $(HAVE_PI_MUTEX)" > $(srcdir)/.config; \
+		echo "HAVE_ROBUST_MUTEX $(HAVE_ROBUST_MUTEX)" >> $(srcdir)/.config)
+endif
+
+# Default stuff common to all testcases
+#
+CFLAGS	+=  -Wall -Werror -O2 -I$(srcdir)/include -D_GNU_SOURCE
+LDFLAGS	+= -lpthread -lrt -lm
+LDLIBS	+= $(srcdir)/lib/libjvmsim.a \
+	   $(srcdir)/lib/librttest.a \
+	   $(srcdir)/lib/libstats.a
+CC	= gcc
