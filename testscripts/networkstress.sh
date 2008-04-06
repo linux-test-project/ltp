1fb6b972bf3a1d81460f52e6f546689afcd734ed
subrata_modak <subrata_modak>
2008-04-06 12:43:12 +0000

    This patch reviews the networkstress.sh script and adds a menu to select the network stress tests. Hoping that could be useful, by, Giuseppe Cavallaro <peppe.cavallaro@gmail.com>

--- testscripts/networkstress.sh
+++ testscripts/networkstress.sh
@@ -12,14 +12,20 @@ if [ $? -eq 0 ]; then
  export LTPROOT=${PWD}
 fi
 
-# ---***** THESE MUST BE SET FOR CORRECT OPERATION *****---
+export TMPDIR=/tmp/netst-$$
+mkdir $TMPDIR
+VERBOSE="no"
+INTERFACE="eth0"
+
+#===========================================================================
+# Network parameters
 export RHOST=
-export LHOST_HWADDRS= 
 export RHOST_HWADDRS=
 export HTTP_DOWNLOAD_DIR=
 export FTP_DOWNLOAD_DIR=
 export FTP_UPLOAD_DIR=
 export FTP_UPLOAD_URLDIR=
+
 # Set firt three octets of the network address, by default 10.0.0
 export IPV4_NETWORK=
 # Set local host last octet, by default 2
@@ -28,10 +34,9 @@ export LHOST_IPV4_HOST=
 export RHOST_IPV4_HOST=
 # Set the reverse of IPV4_NETWORK, by default 0.0.10
 export IPV4_NETWORK_REVERSE=
-# ---***************************************************---=
 
-# ---***** OPTIONAL SETTINGS *****---
-# DEFAULTS
+#===========================================================================
+# Default Test Settings
 # export LTP_RSH=rsh
 # export NS_DURATION=3600	# 1 hour
 # export NS_TIMES=10000
@@ -47,26 +52,80 @@ export IPV4_NETWORK_REVERSE=
 # export UPLOAD_REGFILESIZE=1024		# 1K byte
 # export MCASTNUM_NORMAL=20
 # export MCASTNUM_HEAVY=40000
-# ---*********************************---
+#===========================================================================
 
+usage () {
+    echo ""
+    echo "---------------------------------------------------------"
+    echo -e "\033[31m $0 [options] \033[0m "
+    echo "---------------------------------------------------------"
+    echo " -E|e: Stress test for interface"
+    echo " -I|i: Stress test for ICMP protocol"
+    echo " -T|t: Stress test for TCP/IP"
+    echo " -U|u: Stress test for UDP/IP"
+    echo " -R|r: Stress test for routing table"
+    echo " -B|b: Stress Broken IP packets"
+    echo " -M|m: Multicast stress tests"
+    echo " -S|s: Run selected tests"
+    echo " -W|w: Run whole network stress tests"
+    echo " -D|d: Test duration (default ${NS_DURATION} sec)"
+    echo " -N|n: Select the network interface (default: $INTERFACE)"
+    echo " -V|v: Enable verbose"
+    echo " -H|h: This Usage"
+    echo ""
+    exit 1
+}
 
-export TMPDIR=/tmp/netst-$$
-mkdir $TMPDIR
+while getopts EeTtIiUuRrMmSsWwBbVvN:n:D:d: OPTION
+do
+    case $OPTION in
+	E|e) TEST_CASE="network_stress.interface";;
+	B|b) TEST_CASE="network_stress.broken_ip";;
+	I|i) TEST_CASE="network_stress.icmp";;
+	T|t) TEST_CASE="network_stress.tcp";;
+	U|u) TEST_CASE="network_stress.udp";;
+	R|r) TEST_CASE="network_stress.route";;
+	M|m) TEST_CASE="network_stress.multicast";;
+	S|s) TEST_CASE="network_stress.selected";;
+	W|w) TEST_CASE="network_stress.whole";;
+	V|v) VERBOSE="yes";;
+	N|n) INTERFACE=${OPTARG};;
+	D|d) NS_DURATION=${OPTARG};;
+	H|h) usage;;
+	*) echo "Error: invalid option..."; usage; exit 1 ;;
+    esac
+done
 
-# The total number of the stress test is quite big.
-# If you'd like to reduce the number of the stress test, you can choose
-# network_stress.selected, or you can choose network_stress.<category> 
-# Of cource, you can use your custumized command file to run the tests
-# that you're interested in.
-cat ${LTPROOT}/runtest/network_stress.whole > $TMPDIR/network_stress.tests
-#cat ${LTPROOT}/runtest/network_stress.selected > $TMPDIR/network_stress.tests
-#cat (( cutimized command file location )) > $TMPDIR/network_stress.tests
+if [ -z ${TEST_CASE} ]; then
+	usage
+fi
+
+export LHOST_HWADDRS=`ifconfig | grep ${INTERFACE} | grep HWaddr |awk '{print $5}'`
+
+if [ -z ${RHOST} ]; then
+	## Just a silly check
+	echo "Error: pay attention to configure"
+	echo "  network paramaters before running tests."
+	exit 1
+fi
+
+cat ${LTPROOT}/runtest/${TEST_CASE} > $TMPDIR/network_stress.tests
 
 cd $TMPDIR
 
 export PATH="${PATH}:${LTPROOT}/testcases/bin"
  
-${LTPROOT}/ver_linux
+if [ ${VERBOSE} = "yes" ]; then
+	echo "Network parameters:"
+	echo " - ${INTERFACE} local interface (MAC address: ${LHOST_HWADDRS})"
+	echo " - Remote IP address: ${RHOST}"
+	echo " - Remote MAC address: ${RHOST_HWADDRS}"
+
+	cat $TMPDIR/network_stress.tests
+	${LTPROOT}/ver_linux
+	echo ""
+	echo ${LTPROOT}/pan/pan -e -l /tmp/netstress.log -S -a netstress -n netstress -f ${TMPDIR}/network_stress.tests
+fi
 
 ${LTPROOT}/pan/pan -e -l /tmp/netstress.log -S -a netstress -n netstress -f ${TMPDIR}/network_stress.tests
 
