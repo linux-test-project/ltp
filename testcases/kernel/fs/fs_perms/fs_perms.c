d3195737a76337638f918ea719bf2f321be08096
vapier <vapier>
2008-04-28 03:22:29 +0000

    cleanup code and add error checking

--- testcases/kernel/fs/fs_perms/fs_perms.c
+++ testcases/kernel/fs/fs_perms/fs_perms.c
@@ -1,5 +1,4 @@
 /*
- *
  *   Copyright (c) International Business Machines  Corp., 2000
  *
  *   This program is free software;  you can redistribute it and/or modify
@@ -21,14 +20,14 @@
  *  FILE(s)     : fs_perms.c simpletest.sh textx.o Makefile README
  *  DESCRIPTION : Regression test for Linux filesystem permissions.
  *  AUTHOR      : Jeff Martin (martinjn@us.ibm.com)
- *  HISTORY     : 
+ *  HISTORY     :
  *     (04/12/01)v.99  First attempt at using C for fs-regression test.  Only tests read and write bits.
  *     (04/19/01)v1.0  Added test for execute bit.
  *     (05/23/01)v1.1  Added command line parameter to specify test file.
  *     (07/12/01)v1.2  Removed conf file and went to command line parameters.
- *
  */
 
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -38,94 +37,103 @@
 #include <unistd.h>
 #include <wait.h>
 
-int testsetup(mode_t mode, int cuserId, int cgroupId);
-int testfperm(int userId, int groupId, char* fperm);
+static int testsetup(mode_t mode, int cuserId, int cgroupId)
+{
+	int ret;
 
-int main( int argc, char *argv[]) {
-   char fperm[1];
-   int result, exresult=0,  cuserId=0, cgroupId=0, userId=0, groupId=0;
-   mode_t mode;
+	ret = unlink("test.file");
+	if (ret && errno != ENOENT)
+		goto done;
+	ret = system("cp testx test.file");
+	if (ret)
+		goto done;
+	ret = chmod("test.file", mode);
+	if (ret)
+		goto done;
+	ret = chown("test.file", cuserId, cgroupId);
 
-   switch (argc) {
-      case 8:
-	      mode = strtol(argv[1],(char**)NULL,010);
-              cuserId = atoi(argv[2]);
-              cgroupId = atoi(argv[3]);
-              userId = atoi(argv[4]);
-              groupId = atoi(argv[5]);
-              fperm[0] = *argv[6];
-              exresult = atoi(argv[7]);
-	      break;
-      default:
-	      printf("Usage: %s <mode of file> <UID of file> <GID of file> <UID of tester> <GID of tester> <permission to test r|w|x> <expected result as 0|1>\n",argv[0]); 
-	      exit(0);
-   }
-
-   testsetup(mode,cuserId,cgroupId);
-   result=testfperm(userId,groupId,fperm);
-   system("rm test.file");
-   printf("%c a %03o file owned by (%d/%d) as user/group(%d/%d)  ",fperm[0],mode,cuserId,cgroupId,userId,groupId);
-   if (result == exresult) {
-      printf("PASS\n");
-      exit(0);
-      }
-   else {
-      printf("FAIL\n");
-      exit(1);
-      }
+ done:
+	return ret;
 }
 
-int testsetup(mode_t mode, int cuserId, int cgroupId) {
-   system("cp testx.file test.file");
-   chmod("test.file",mode);
-   chown("test.file",cuserId,cgroupId);
-   return(0);
+static int testfperm(int userId, int groupId, char *fperm)
+{
+	/* SET CURRENT USER/GROUP PERMISSIONS */
+	if (setegid(groupId)) {
+		printf("could not setegid to %d.\n", groupId);
+		seteuid(0);
+		setegid(0);
+		return -1;
+	}
+	if (seteuid(userId)) {
+		printf("could not seteuid to %d.\n", userId);
+		seteuid(0);
+		setegid(0);
+		return -1;
+	}
+
+	switch (tolower(fperm[0])) {
+	case 'x': {
+		int status;
+		if (fork() == 0)
+			exit(execlp("./test.file", "test.file", NULL));
+		wait(&status);
+		seteuid(0);
+		setegid(0);
+		return WEXITSTATUS(status);
+	}
+	default: {
+		FILE *testfile;
+		if ((testfile = fopen("test.file", fperm))) {
+			fclose(testfile);
+			seteuid(0);
+			setegid(0);
+			return 1;
+		} else {
+			seteuid(0);
+			setegid(0);
+			return 0;
+		}
+	}
+	}
 }
 
-int testfperm(int userId, int groupId, char* fperm) {
+int main(int argc, char *argv[])
+{
+	char *fperm;
+	int result, exresult = 0, cuserId = 0, cgroupId = 0, userId = 0, groupId = 0;
+	mode_t mode;
 
-    FILE *testfile;
-    pid_t PID;
-    int tmpi,nuthertmpi;
+	switch (argc) {
+	case 8:
+		mode = strtol(argv[1], (char **)NULL, 010);
+		cuserId = atoi(argv[2]);
+		cgroupId = atoi(argv[3]);
+		userId = atoi(argv[4]);
+		groupId = atoi(argv[5]);
+		fperm = argv[6];
+		exresult = atoi(argv[7]);
+		break;
+	default:
+		printf("Usage: %s <mode of file> <UID of file> <GID of file> <UID of tester> <GID of tester> <permission to test r|w|x> <expected result as 0|1>\n", argv[0]);
+		return 0;
+	}
 
-/*  SET CURRENT USER/GROUP PERMISSIONS */
-    if(setegid(groupId)) {
-        printf("could not setegid to %d.\n",groupId);
-           seteuid(0);
-           setegid(0);
-           return(-1);
-        }   
-    if(seteuid(userId)) {
-        printf("could not seteuid to %d.\n",userId);
-           seteuid(0);
-           setegid(0);
-           return(-1);
-        }
+	result = testsetup(mode, cuserId, cgroupId);
+	if (result) {
+		perror("testsetup() failed");
+		return result;
+	}
 
-    switch(tolower(fperm[0])) {
-       case 'x': 
-          PID = fork();
-	  if (PID == 0) {
-             execlp("./test.file","test.file",NULL); 
-	     exit(0);
-	  }
-	  wait(&tmpi);
-	  nuthertmpi=WEXITSTATUS(tmpi); 
-          seteuid(0);
-          setegid(0);
-	  return(nuthertmpi);
-
-       default: 
-          if((testfile=fopen("test.file",fperm))){
-            fclose(testfile);
-            seteuid(0);
-            setegid(0);
-            return (1);
-	  }
-          else {
-            seteuid(0);
-            setegid(0);
-            return (0);
-	  }
-    }
+	result = testfperm(userId, groupId, fperm);
+	unlink("test.file");
+	printf("%c a %03o file owned by (%d/%d) as user/group(%d/%d)  ",
+	       fperm[0], mode, cuserId, cgroupId, userId, groupId);
+	if (result == exresult) {
+		printf("PASS\n");
+		return 0;
+	} else {
+		printf("FAIL\n");
+		return 1;
+	}
 }
