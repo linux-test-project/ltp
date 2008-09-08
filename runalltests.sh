7214c77b4a2df8711f33a5834af892721d5619cb
subrata_modak <subrata_modak>
2008-09-08 11:56:11 +0000

    Chaning the script for the purpose of running all LTP test cases. Subrata Modak <subrata@linux.vnet.ibm.com>,

--- runalltests.sh
+++ runalltests.sh
@@ -18,29 +18,25 @@
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    ##
 ##                                                                            ##
 ################################################################################
-# File:        runalltests.sh
-#
-# Description:  This script just calls runltp now, and is being phased out.
-#		If you rely on this script for automation reasons, please
-#		change your scripts soon
+## File:        runalltests.sh                                                ##
+##                                                                            ##
+## Description:  This script just calls runltp now, and is being phased out.  ##
+##		If you rely on this script for automation reasons, please     ##
+##                                                                            ## 
+## History	Subrata Modak <subrata@linuc.vnet.ibm.com> changed the code   ##
+##		to include testing other testcases which are not run by       ##
+##		default, 08/09/2008                                           ##
+##                                                                            ##
+################################################################################
 
-cd `dirname $0`
-echo -e "\033[5m"
 echo "*******************************************************************"
 echo "*******************************************************************"
 echo "**                                                               **"
-echo -e "** \033[0m The runalltests.sh script is being phased out soon.  It will \033[5m**"
-echo -e "** \033[0m be replaced with the scripts called 'runltp'.  If you \033[5m       **"
-echo -e "** \033[0m currently rely on this script for automation scripts, you \033[5m   **"
-echo -e "** \033[0m should think about changing them NOW! \033[5m                       **"
+echo -e "** This script is being re-written to cover all aspects of    **"
+echo -e "** testing LTP, which includes running all those tests which  **"
+echo -e "** are not run by default with ./runltp script. Special setup **"
+echo -e "** in system environment will be done to run all those tests  **"
+echo -e "** like the File System tests, SELinuxtest, etc               **"
 echo "**                                                               **"
 echo "*******************************************************************"
 echo "*******************************************************************"
-echo -e "\033[0m"
-for i in 1 2 3 4 5 6 7 8 9 10
-do
-	echo -en "\a"
-	sleep 1
-done
-
-./runltp $*
