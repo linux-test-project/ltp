8083ac01320da034d3c22a60ea97d6f1aa656d53
subrata_modak <subrata_modak>
2008-09-08 10:20:16 +0000

    When it comes to this program, why should we care what the arch is? Shouldn't the SYS_ and __NR numbers alone tell us if unshare() is relevant here? Also, suppose both SYS_unshare and __NR_unshare aren't defined and we're running on an i386 box. This means we'll trigger the block:         #elif __i386__                 int do_check(void) { return kernel_is_too_old(); } Which is totally wrong. Fix these up by ignoring the arch and boiling it all down to a corrected check for the proper define(s). See sys/syscall.h for details on SYS_ vs. __NR_. Signed-off-by: Matt Helsley <matthltc@us.ibm.com>, Acked-by: Serge Hallyn <serue@us.ibm.com>,

--- testcases/kernel/containers/check_for_unshare.c
+++ testcases/kernel/containers/check_for_unshare.c
@@ -15,7 +15,6 @@
 *
 ***************************************************************************/
 #include <stdio.h>
-#include <test.h>
 
 int kernel_is_too_old(void) {
 	if (tst_kvercmp(2,6,16) < 0)
@@ -27,24 +26,16 @@ int kernel_is_too_old(void) {
  * yeah, to make the makefile coding easier, do_check returns 
  * 1 if unshare is not supported, 0 if it is
  */
-#ifndef SYS_unshare
-#ifdef __NR_unshare
-int do_check(void) { return kernel_is_too_old(); }
-#elif __i386__
-int do_check(void) { return kernel_is_too_old(); }
-#elif __ia64__
-int do_check(void) { return kernel_is_too_old(); }
-#elif __x86_64__
-int do_check(void) { return kernel_is_too_old(); }
-#elif __s390x__ || __s390__
-int do_check(void) { return kernel_is_too_old(); }
-#elif __powerpc__
-int do_check(void) { return kernel_is_too_old(); }
+#if defined(SYS_unshare) || defined(__NR_unshare)
+int do_check(void)
+{
+	return kernel_is_too_old();
+}
 #else
 int do_check(void) { return 1; }
 #endif
-#endif
 
-int main() {
+int main()
+{
 	return do_check();
 }
