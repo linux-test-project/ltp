9a84db5a3ab471e75e3f257b86c81e7fee4d51ef
subrata_modak <subrata_modak>
2008-09-18 10:32:03 +0000

    This patch ensures that the python scripts support utf encoding. Prior to the patch, the python scripts would exit with this error: File "parse-testpi1.py", line 3 SyntaxError: Non-ASCII character '\xc2' in file parse-testpi1.py on line 3, but no encoding declared; see http://www.python.org/peps/pep-0263.html for details This patch fixes it. Tested: Running all the python scripts. Signed-Off-By: Chirag <chirag@linux.vnet.ibm.com>.

--- testcases/realtime/func/sched_football/parse-football.py
+++ testcases/realtime/func/sched_football/parse-football.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ################################################################################
 ##                                                                            ##
 ## Copyright ©  International Business Machines  Corp., 2007, 2008            ##
@@ -15,7 +17,7 @@
 ## You should have received a copy of the GNU General Public License          ##
 ## along with this program;  if not, write to the Free Software               ##
 ## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    ##
-##									      ##
+##																			  ##
 ## NAME: parse-football.py                                                    ##
 ##                                                                            ##
 ## DESCRIPTION: Log Parser for the sched_football.c test                      ##
