078497baf4d71bab32328675b5e4e8e20de2317d
subrata_modak <subrata_modak>
2008-09-29 17:01:38 +0000

    When the libcap-2.11 or newer didn't been installed on the sysytem, the filecaps testcase would return a message instead of an error. Signed-off-by: Jin Bing Guo <guojb@cn.ibm.com>.

--- testcases/kernel/security/filecaps/filecapstest.sh
+++ testcases/kernel/security/filecaps/filecapstest.sh
@@ -22,9 +22,10 @@
 checkforlibcap
 ret=$?
 if [ $ret -ne 0 ]; then
+	echo "Filecaps 0 CONF : System doesn't support execution of the test"
 	echo setcap not installed.  Please install libcap-2.11 or newer from
 	echo ftp://ftp.kernel.org/pub/linux/libs/security/linux-privs/libcap2
-	exit 1
+	exit 0
 fi
 checkforfilecaps.sh
 ret=$?
