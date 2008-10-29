bea91c2422c8ddc8dbd30fbe0ba785c04d121375
subrata_modak <subrata_modak>
2008-10-29 06:16:59 +0000

    Add mips64 support to resvmsg01 and sendmsg01 tests. wo rivial patches to add yet another 64-bit architecture. Tested on mips64 Octeon, Linux 2.6.27. Signed-off by : Vlad Malov vlad.malov@caviumnetworks.com.

--- testcases/kernel/syscalls/recvmsg/recvmsg01.c
+++ testcases/kernel/syscalls/recvmsg/recvmsg01.c
@@ -45,7 +45,7 @@
 /* and this test to function correctly.                        */
 #ifndef MSG_CMSG_COMPAT
 
-#if defined (__powerpc64__) || defined (__x86_64__) || defined (__sparc64__)
+#if defined (__powerpc64__) || defined (__mips64) || defined (__x86_64__) || defined (__sparc64__)
 #define MSG_CMSG_COMPAT 0x80000000
 #else
 #define MSG_CMSG_COMPAT 0
