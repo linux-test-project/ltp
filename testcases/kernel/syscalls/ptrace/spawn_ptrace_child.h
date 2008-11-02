8747e0a1dd44d447b53bcd5ee0dea6fc51fb283c
vapier <vapier>
2008-11-02 06:06:00 +0000

    use feedback from Jan Kratochvil so we wait for the child to be stopped before we attempt to ptrace it

--- testcases/kernel/syscalls/ptrace/spawn_ptrace_child.h
+++ testcases/kernel/syscalls/ptrace/spawn_ptrace_child.h
@@ -17,6 +17,7 @@
 
 #include <errno.h>      /* errno */
 #include <signal.h>     /* signal() */
+#include <stdbool.h>    /* true */
 #include <string.h>     /* strcmp() */
 #include <unistd.h>     /* execlp() sleep() vfork() */
 #include <sys/ptrace.h> /* ptrace() */
@@ -26,24 +27,45 @@
 #include "usctest.h"
 
 static pid_t pid;
+static bool child_stopped = false;
+
+static void child_signal(int sig)
+{
+	int status;
+	if (sig != SIGCHLD) {
+ die:
+		tst_brkm(TBROK, tst_exit, "child signal %i: %s\n", sig, strsignal(sig));
+		kill(pid, SIGKILL);
+		exit(1);
+	}
+	if (wait(&status) == -1)
+		goto die;
+	if (!WIFSTOPPED(status))
+		goto die;
+
+	child_stopped = true;
+}
 
 static void make_a_baby(int argc, char *argv[])
 {
 	if (argc > 1 && !strcmp(argv[1], "child")) {
 		/* if we're the child, just sit around doing nothing */
-		while (1)
-			sleep(1);
+		sleep(60);
+		exit(1);
 	}
 
-	/* ptrace() stuff will fail for us if the child dies */
-	signal(SIGCHLD, SIG_IGN);
+	signal(SIGCHLD, child_signal);
 
 	pid = vfork();
 	if (pid == -1) {
 		tst_resm(TFAIL, "vfork() failed");
 		tst_exit();
-	} else if (pid)
+	} else if (pid) {
+		while (!child_stopped)
+			continue;
+		signal(SIGCHLD, SIG_IGN);
 		return;
+	}
 
 	errno = 0;
 	long ret = ptrace(PTRACE_TRACEME, 0, NULL, NULL);
