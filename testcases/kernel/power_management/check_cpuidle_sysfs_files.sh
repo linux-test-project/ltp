cc8313122bf01433dac5a88d2fa79fbd90916902
subrata_modak <subrata_modak>
2008-11-19 10:56:34 +0000

    I guess you want ">/dev/null 2>&1" to eliminate both STDERR and STDOUT. Attaching a small patch with the above change. Please accept. Signed-Off-By: Nageswara R Sastry <rnsastry@linux.vnet.ibm.com>.

--- testcases/kernel/power_management/check_cpuidle_sysfs_files.sh
+++ testcases/kernel/power_management/check_cpuidle_sysfs_files.sh
@@ -4,7 +4,7 @@ RC=0
 if [ -d /sys/devices/system/cpu/cpuidle ] ; then
 	for files in current_governor_ro current_driver
 	do
-		cat /sys/devices/system/cpu/cpuidle/${files} 2>&1 >/dev/null
+		cat /sys/devices/system/cpu/cpuidle/${files} >/dev/null 2>&1
 		if [ $? -ne 0 ] ; then
 			echo "${0}: FAIL: cat ${files}"
 			RC=1
