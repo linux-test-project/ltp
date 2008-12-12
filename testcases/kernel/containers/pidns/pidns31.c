3a14aff5ce8bb20ead063dd4b59a7e7e74587c95
subrata_modak <subrata_modak>
2008-12-12 13:17:07 +0000

    This is a proposal for 2 new testcases in the pidns testcases. They are related to POSIX message queues and notification on message arrival. Could you please review them? If a process registers for asynchronous notification on a POSIX message queue, it gets a signal and a siginfo_t structure when a message arrives on the message queue. The si_pid in the siginfo_t structure is set to the PID of the process that sent the message. If the process that sent the message is not in the same namespace as the notified one, this should be taken into account. pidns30 : notified process is in a descendant namespace wrt to the notifier. so the signal receiver doesn't know about the sender's pid. pidns31 : notified process is in an ancestor namespace wrt to the notifier. so the signal receiver should get the sender's PID in the signal receiver namespace. They currently fail, since the si_pid is not correctly set. But I just proposed a patch to fix this (see http://lkml.org/lkml/2008/12/5/13). This testcase checks if the si_pid is correctly set when a process that has registered for notification on a posix mqueue is in an ancestor namespace wrt the process that sends a message to that posix mqueue. Signed-off-by: Nadia Derbey <Nadia.Derbey@bull.net>, Acked-By: <sukadev@linux.vnet.ibm.com>.

---
+++ testcases/kernel/containers/pidns/pidns31.c
@@ -0,0 +1,333 @@
+/*
+* Copyright (c) Bull S.A.S. 2008
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+* the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+***************************************************************************
+* File: pidns31.c
+*
+*   Description:
+*    This testcase checks if the si_pid is correctly set when a process
+*    that has registered for notification on a posix mqueue is in an
+*    ancestor namespace wrt the process that sends a message to that posix
+*    mqueue.
+*
+*   Test Assertion & Strategy:
+*    Parent                                   Child
+*    --------------------------------------------------------------------------
+*    Create a POSIX mqueue.
+*    Create a PID namespace container.
+*    Register for notification when a
+*       message arrives in that mqueue
+*    Install a handler for SIGUSR1.
+*                                             Open that mqueue for writing
+*                                             Write something to the mqueue.
+*    Inside the handler, check that
+*       si_pid is set to the child's pid
+*
+*   Usage: <for command-line>
+*    pidns31
+*
+*   History:
+*    DATE      NAME                             DESCRIPTION
+*    04/12/08  Nadia Derbey               Creation of this test.
+*              <Nadia.Derbey@bull.net>
+*
+******************************************************************************/
+#define _GNU_SOURCE 1
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <mqueue.h>
+#include <usctest.h>
+#include <test.h>
+#include <libclone.h>
+
+char *TCID = "pidns31";
+int TST_TOTAL = 1;
+
+char *mqname = "/mq1";
+int result = TFAIL;
+
+int errno;
+int father_to_child[2];
+
+#define CHILD_PID       1
+#define PARENT_PID      0
+
+#define MSG      "HOW ARE YOU"
+#define MSG_PRIO 1
+
+#define NO_STEP -1
+#define F_STEP_0 0x00
+#define F_STEP_1 0x01
+#define F_STEP_2 0x02
+#define F_STEP_3 0x03
+#define C_STEP_0 0x10
+#define C_STEP_1 0x11
+
+struct notify_info {
+	mqd_t mqd;
+	pid_t pid;
+};
+
+static void remove_pipe(int *fd)
+{
+	close(fd[0]);
+	close(fd[1]);
+}
+
+static void remove_mqueue(mqd_t mqd)
+{
+	mq_close(mqd);
+	mq_unlink(mqname);
+}
+
+/*
+ * steps F_STEP_XX : called from main
+ * steps C_STEP_XX : called from child_fn
+ */
+static void cleanup_resources(int step, mqd_t mqd)
+{
+	switch (step) {
+	case C_STEP_1:
+		close(father_to_child[0]);
+		/* fall through */
+	case C_STEP_0:
+		mq_close(mqd);
+		break;
+
+	case F_STEP_3:
+		remove_mqueue(mqd);
+		close(father_to_child[1]);
+		break;
+
+	case F_STEP_2:
+		mq_notify(mqd, NULL);
+		/* fall through */
+	case F_STEP_1:
+		remove_mqueue(mqd);
+		/* fall through */
+	case F_STEP_0:
+		remove_pipe(father_to_child);
+		break;
+	default:
+		tst_resm(TWARN, "Unknown code - no resource removed.");
+		break;
+	}
+}
+
+/*
+ * cleanup() - performs all ONE TIME cleanup for this test at
+ *             completion or premature exit.
+ * step == -1 means no local resource to remove.
+ */
+static void cleanup(int result, int step, mqd_t mqd)
+{
+	if (step != NO_STEP)
+		cleanup_resources(step, mqd);
+
+	/* Clean the test testcase as LTP wants*/
+	TEST_CLEANUP;
+
+	/* exit with return code appropriate for results */
+	tst_exit(result);
+}
+
+/*
+ * child_fn() - Inside container
+ */
+int child_fn(void *arg)
+{
+	pid_t pid, ppid;
+	mqd_t mqd;
+	char buf[5];
+
+	/* Set process id and parent pid */
+	pid = getpid();
+	ppid = getppid();
+
+	if (pid != CHILD_PID || ppid != PARENT_PID) {
+		tst_resm(TBROK, "cinit: pidns is not created");
+		cleanup(TBROK, NO_STEP, 0);
+	}
+
+	/* Close the appropriate end of pipe */
+	close(father_to_child[1]);
+
+	/* Is parent ready to receive a message? */
+	read(father_to_child[0], buf, 5);
+	if (strcmp(buf, "f:ok")) {
+		tst_resm(TBROK, "cinit: parent did not send the message!");
+		cleanup(TBROK, NO_STEP, 0);
+	}
+	tst_resm(TINFO, "cinit: my father is ready to receive a message");
+
+	mqd = mq_open(mqname, O_WRONLY);
+	if (mqd == (mqd_t)-1) {
+		tst_resm(TBROK, "cinit: mq_open() failed (%s)",
+			strerror(errno));
+		cleanup(TBROK, NO_STEP, 0);
+	}
+	tst_resm(TINFO, "cinit: mq_open succeeded");
+
+	if (mq_send(mqd, MSG, strlen(MSG), MSG_PRIO) == (mqd_t)-1) {
+		tst_resm(TBROK, "cinit: mq_send() failed (%s)",
+			strerror(errno));
+		cleanup(TBROK, C_STEP_0, mqd);
+	}
+	tst_resm(TINFO, "cinit: mq_send() succeeded");
+
+	/* Cleanup and exit */
+	cleanup_resources(C_STEP_1, mqd);
+	exit(0);
+}
+
+/*
+ * father_signal_handler()
+ */
+static void father_signal_handler(int sig, siginfo_t *si, void *unused)
+{
+	char buf[256];
+	struct mq_attr attr;
+	struct notify_info *info;
+
+	if (si->si_signo != SIGUSR1) {
+		tst_resm(TBROK, "father: received %s unexpectedly",
+			strsignal(si->si_signo));
+		return;
+	}
+
+	if (si->si_code != SI_MESGQ) {
+		tst_resm(TBROK, "father: expected signal code SI_MESGQ - "
+			"Got %d", si->si_code);
+		return;
+	}
+
+	if (!si->si_ptr) {
+		tst_resm(TBROK, "father: expected si_ptr - Got NULL");
+		return;
+	}
+
+	info = (struct notify_info *)si->si_ptr;
+
+	if (si->si_pid != info->pid) {
+		tst_resm(TFAIL,
+			"father: expected signal originator PID = %d - Got %d",
+			info->pid, si->si_pid);
+		return;
+	}
+
+	tst_resm(TPASS, "father: signal originator PID = %d", si->si_pid);
+	result = TPASS;
+
+	/*
+	 * Now read the message - Be silent on errors since this is not the
+	 * test purpose.
+	 */
+	if (!mq_getattr(info->mqd, &attr))
+		mq_receive(info->mqd, buf, attr.mq_msgsize, NULL);
+}
+
+/***********************************************************************
+*   M A I N
+***********************************************************************/
+
+int main(int argc, char *argv[])
+{
+	int stack_size = getpagesize() * 4;
+	void *stack = malloc(stack_size);
+	void *childstack;
+	pid_t cpid;
+	mqd_t mqd;
+	struct sigevent notif;
+	struct sigaction sa;
+	int status;
+	struct notify_info info;
+
+	if (!stack) {
+		tst_resm(TBROK, "parent: stack creation failed.");
+		cleanup(TBROK, NO_STEP, 0);
+	}
+
+	if (pipe(father_to_child) == -1) {
+		tst_resm(TBROK, "parent: pipe() failed. aborting!");
+		cleanup(TBROK, NO_STEP, 0);
+	}
+
+	mq_unlink(mqname);
+	mqd = mq_open(mqname, O_RDWR|O_CREAT|O_EXCL, 0777, NULL);
+	if (mqd == (mqd_t)-1) {
+		tst_resm(TBROK, "parent: mq_open() failed (%s)",
+			strerror(errno));
+		cleanup(TBROK, F_STEP_0, 0);
+	}
+	tst_resm(TINFO, "parent: successfully created posix mqueue");
+
+	/* container creation on PID namespace */
+	childstack = stack + stack_size;
+	cpid = clone(child_fn, childstack, CLONE_NEWPID|SIGCHLD, NULL);
+	if (cpid < 0) {
+		tst_resm(TBROK, "parent: clone() failed(%s)", strerror(errno));
+		cleanup(TBROK, F_STEP_1, mqd);
+	}
+	tst_resm(TINFO, "parent: successfully created child (pid = %d)", cpid);
+
+	/* Register for notification on message arrival */
+	notif.sigev_notify = SIGEV_SIGNAL;
+	notif.sigev_signo = SIGUSR1;
+	info.mqd = mqd;
+	info.pid = cpid;
+	notif.sigev_value.sival_ptr = &info;
+	if (mq_notify(mqd, &notif) == (mqd_t)-1) {
+		tst_resm(TBROK, "parent: mq_notify() failed (%s)",
+			strerror(errno));
+		cleanup(TBROK, F_STEP_1, mqd);
+	}
+	tst_resm(TINFO, "parent: successfully registered for notification");
+
+	/* Define handler for SIGUSR1 */
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = father_signal_handler;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+		tst_resm(TBROK, "parent: sigaction() failed(%s)",
+			strerror(errno));
+		cleanup(TBROK, F_STEP_2, mqd);
+	}
+	tst_resm(TINFO, "parent: successfully registered handler for SIGUSR1");
+
+	/* Close the appropriate end of pipe */
+	close(father_to_child[0]);
+
+	/* Tell the child a message can be sent */
+	if (write(father_to_child[1], "f:ok", 5) != 5) {
+		tst_resm(TBROK, "parent: pipe is broken(%s)", strerror(errno));
+		cleanup(TBROK, F_STEP_2, mqd);
+	}
+
+	sleep(3);
+
+	/* Wait for child to finish */
+	if (wait(&status) == -1) {
+		tst_resm(TBROK, "parent: wait() failed(%s)", strerror(errno));
+		cleanup(TBROK, F_STEP_1, mqd);
+	}
+
+	cleanup(result, F_STEP_3, mqd);
+
+	/* NOT REACHED */
+	return 0;
+}    /* End main */
--- testcases/kernel/containers/pidns/runpidnstest.sh
+++ testcases/kernel/containers/pidns/runpidnstest.sh
@@ -61,6 +61,12 @@ if [ $rc -ne 0 ] && [ -z $err_code ]; then
 	err_code=$rc
 fi
 
+pidns31
+rc=$?
+if [ $rc -ne 0 ] && [ -z $err_code ]; then
+	err_code=$rc
+fi
+
 # If any test failed then exit with the value error-code.
 if ! [ -z $err_code ]; then
 	exit $err_code
