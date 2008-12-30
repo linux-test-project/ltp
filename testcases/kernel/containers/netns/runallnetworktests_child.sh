995126537802e688b921763b213769960bb9190e
subrata_modak <subrata_modak>
2008-12-30 06:34:25 +0000

    This patchset creates the basic infrastructure for running the existing network testcases under containers automatically. The patchset creates a command file named nw_under_ns for the purpose. Please review them and provide all your valuable feedback. I have addressed all the comments from lxc-dev list except one. The changelog looks like: 1. Added verbose comments and patch description. 2. Restructured the licensing text. 3. Merged two patches into one touching runallnetworktest_child.sh file.
    TODO:
    1. Changing the kernel version to proper one
    2. unshare not needed as clone only is suffucient. I will do it in a
    separate patch as need to do extensive testing to ensure other testcases do not break.
    
    [patch 11/12] CONTAINERS add further networktestcases: Add other network testcases to be run.
    Signed-off-by: Sudhir Kumar <skumar@linux.vnet.ibm.com>, Acked-By: "Serge E. Hallyn" <serue@us.ibm.com>.

--- testcases/kernel/containers/netns/runallnetworktests_child.sh
+++ testcases/kernel/containers/netns/runallnetworktests_child.sh
@@ -89,6 +89,53 @@ export TCbin=$LTPROOT/testcases/network/tcp_cmds/sendfile; sendfile01
 echo "Running LAN perf testcase...."
 export TCbin=$LTPROOT/testcases/network/tcp_cmds/perf_lan; perf_lan
 
+echo "Running set of rpc testcase...."
+export TCbin=$LTPROOT/testcases/network/rpc/basic_tests/rpc01; rpc01
+export TCbin=$LTPROOT/testcases/network/rpc/basic_tests/rpcinfo01; rpcinfo01
+export TCbin=$LTPROOT/testcases/network/rpc/basic_tests/rup; rup01
+export TCbin=$LTPROOT/testcases/network/rpc/basic_tests/rusers; rusers01
+
+echo "Running set of nfs testcase...."
+export VERSION SOCKET_TYPE; export TCbin=$LTPROOT/testcases/network/nfs/nfs01;
+nfs01;
+
+export VERSION SOCKET_TYPE; export TCbin=$LTPROOT/testcases/network/nfs/nfs02;
+nfs02;
+
+export VERSION SOCKET_TYPE; export TCbin=$LTPROOT/testcases/network/nfs/nfs03;
+nfs03;
+export VERSION SOCKET_TYPE; export TCbin=$LTPROOT/testcases/network/nfs/nfs04;
+nfs04;
+
+export VERSION; export TCbin=$LTPROOT/testcases/network/nfs/nfslock01; nfslock01
+
+echo "Running set of nfs stress testcase...."
+
+export VERSION SOCKET_TYPE;
+export TCbin=$LTPROOT/testcases/network/nfs/nfsstress;
+nfsstress 20 50 1
+
+export VERSION; export TCbin=$LTPROOT/testcases/network/nfs/nfsstat01; nfsstat01
+
+export VERSION SOCKET_TYPE; export TCbin=$LTPROOT/testcases/bin; fsx.sh
+
+echo "Running ftp testcase...."
+export TCbin=$LTPROOT/testcases/network/tcp_cmds/ftp; ftp01
+
+echo "Running set of ssh testcase...."
+# This requires ssh daemon to be listening on port 22
+/usr/sbin/sshd -p 22
+if [ $? -eq 0 ]; then
+	ssh01;
+	ssh02;
+	ssh03;
+	sshpid=`ps -ef | grep "sshd -p $PORT" | awk '{ print $2 ; exit 0} ' `;
+	kill -s SIGKILL $sshpid;
+else
+	echo "Failed to start ssh daemon at port 22 in container";
+	echo "Skipping the set of ssh testcases";
+fi
+
 echo "Running netstat testcase...."
 netstat01;
 
