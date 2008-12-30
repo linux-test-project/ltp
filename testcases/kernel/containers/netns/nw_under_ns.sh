b3317b4d290ca22fc045409f4ed20452f2d4eed4
subrata_modak <subrata_modak>
2008-12-30 06:25:50 +0000

    This patchset creates the basic infrastructure for running the existing network testcases under containers automatically. The patchset creates a command file named nw_under_ns for the purpose. Please review them and provide all your valuable feedback. I have addressed all the comments from lxc-dev list except one. The changelog looks like: 1. Added verbose comments and patch description. 2. Restructured the licensing text. 3. Merged two patches into one touching runallnetworktest_child.sh file.
    TODO:
    1. Changing the kernel version to proper one
    2. unshare not needed as clone only is suffucient. I will do it in a separate patch as need to do extensive testing to ensure other testcases do not break.
    
    [patch 08/12] CONTAINERS script to be called by command file:
    This patch adds the script to be called by the command file to run network testcases under containers environment.
    Signed-off-by: Sudhir Kumar <skumar@linux.vnet.ibm.com>, Acked-By: "Serge E. Hallyn" <serue@us.ibm.com>.

---
+++ testcases/kernel/containers/netns/nw_under_ns.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#############################################################################
+#                                                                           #
+# Copyright (c) International Business Machines  Corp., 2008                #
+#                                                                           #
+# This program is free software;  you can redistribute it and#or modify     #
+# it under the terms of the GNU General Public License as published by      #
+# the Free Software Foundation; either version 2 of the License, or         #
+# (at your option) any later version.                                       #
+#                                                                           #
+# This program is distributed in the hope that it will be useful, but       #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY#
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License  #
+# for more details.                                                         #
+#                                                                           #
+# You should have received a copy of the GNU General Public License         #
+# along with this program;  if not, write to the Free Software              #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA   #
+#                                                                           #
+# Author:      Sudhir Kumar <skumar@linux.vnet.ibm.com>                     #
+#############################################################################
+
+# this script is used to run all the testcases for networks under network
+# namespace. This script is called by a separate command file nw_under_ns
+# The testcases are grouped as per the requirement of the particular
+# testcase.
+
+echo "*****************************************************"
+echo "Running network testcases under containers..."
+
+create_container;
+if [ $? -ne 0 ]; then
+	echo "some of the network testcases under netns failled"
+	exit 1;
+fi
