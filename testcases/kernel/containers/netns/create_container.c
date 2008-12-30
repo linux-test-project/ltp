f296e133d2a24f120444f7065bce18d91dfd18dc
subrata_modak <subrata_modak>
2008-12-30 06:15:01 +0000

    This patchset creates the basic infrastructure for running the existing network testcases under containers automatically. The patchset creates a command file named nw_under_ns for the purpose. Please review them and provide all your valuable feedback. I have addressed all the comments from lxc-dev list except one. The changelog looks like: 1. Added verbose comments and patch description. 2. Restructured the licensing text. 3. Merged two patches into one touching runallnetworktest_child.sh file.
    TODO:
    1. Changing the kernel version to proper one
    2. unshare not needed as clone only is suffucient. I will do it in a separate patch as need to do extensive testing to ensure other testcases do not break.
    
    [patch 05/12] CONTAINERS adds binary to call the lib api:
    The binary to create the network namespace and run the script tests. The api takes two script names as arguments and run them in parent and child namespace.
    Signed-off-by: Sudhir Kumar <skumar@linux.vnet.ibm.com>, Acked-By: "Serge E. Hallyn" <serue@us.ibm.com>.

---
+++ testcases/kernel/containers/netns/create_container.c
@@ -0,0 +1,44 @@
+/*************************************************************************
+* Copyright (c) International Business Machines Corp., 2008
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+* the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+***************************************************************************/
+/* ============================================================================
+* This testcase creates the child container to test the tcp command testcases
+* inside the child namespace. The following program uses the library libclone
+* api to create the Network namespace container and may be removed to use the
+* containers management tools in future.
+*
+* The scripts runallnetworktests_parent.sh, runallnetworktests_child.sh, are
+* passed as the parameters which will be running in the parent and child
+* namespace respectively.
+*
+* Author: Sudhir Kumar <skumar@linux.vnet.ibm.com>   26/08/2008
+* ============================================================================*/
+
+#include<../libclone/libclone.h>
+
+int main()
+{
+	int status;
+	/*
+	 * The argument files contain the code to be run in the parent and
+	 * child network namespace container respectively
+	 */
+	status = create_net_namespace("runallnetworktests_parent.sh",
+						"runallnetworktests_child.sh");
+	printf("Execution of all the network testcases under network"
+			" namespace done. return value is %d\n", status);
+	return status;
+}
