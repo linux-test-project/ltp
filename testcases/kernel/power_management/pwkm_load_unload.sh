9517735501cba85622e58d326fda50fc5b1a6332
subrata_modak <subrata_modak>
2009-03-09 10:28:35 +0000

    Patch to fix cpufreq governor load and unload. Signed-Off-By: Poornima nayak <mpnayak@linux.vnet.ibm.com>.

--- testcases/kernel/power_management/pwkm_load_unload.sh
+++ testcases/kernel/power_management/pwkm_load_unload.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 #Loading modules
+loaded_governor=`cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor`
 for module in cpufreq_conservative cpufreq_powersave cpufreq_userspace
 do
 	#echo -n "Loading $module ... "
-	modprobe $module >/dev/null
-	if [ $? -ne 0 ] ; then
-		echo "${0}: FAIL: Loading of module $module or check whether you compiled as module or not"
+    if [ $module != "cpufreq_$loaded_governor" ];
+    then
+		modprobe $module >/dev/null
+		if [ $? -ne 0 ] ; then
+			echo "${0}: FAIL: Loading of module $module or check whether you compiled as module or not"
+		fi
 	fi
 done
 for module in cpufreq_conservative cpufreq_powersave cpufreq_userspace
 do
 	#echo -n "Unloading $module ... "
-	modprobe -r $module >/dev/null
-	if [ $? -ne 0 ] ; then
-		echo "${0}: FAIL: Loading of module $module or check whether you compiled as module or not"
+    if [ $module != "cpufreq_$loaded_governor" ];
+    then
+		modprobe -r $module >/dev/null
+		if [ $? -ne 0 ] ; then
+			echo "${0}: FAIL: Loading of module $module or check whether you compiled as module or not"
+		fi
 	fi
 done
