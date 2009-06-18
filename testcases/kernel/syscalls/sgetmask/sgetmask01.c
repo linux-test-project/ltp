e1860efe137e478e7d84e03fdf0e8ef7592c22bc
subrata_modak <subrata_modak>
2009-06-18 16:58:44 +0000

    Small typo in sgetmask01.c: Here follow a small patch that fix a typo in sgetmask01.c testcase, where __NR_ssetmask is badly used instead of __NR_sgetmask. Signed-off-by: Nicolas Joly <njoly@pasteur.fr>.

--- testcases/kernel/syscalls/sgetmask/sgetmask01.c
+++ testcases/kernel/syscalls/sgetmask/sgetmask01.c
@@ -141,7 +141,7 @@ int main(int ac, char **av) {
 			for (sig = -3; sig <= SIGRTMAX + 1; sig++){
 				TEST(syscall(__NR_ssetmask,sig));
                 		tst_resm(TINFO,"Setting signal : %d -- return of setmask : %d",sig,TEST_RETURN);     //call sgetmask()
-                     		TEST(syscall(__NR_ssetmask));     //call sgetmask()
+                     		TEST(syscall(__NR_sgetmask));     //call sgetmask()
                      		if(TEST_RETURN != sig) {
         				tst_resm(TINFO,"Oops,setting sig %d, got %d",sig,TEST_RETURN);
                      		} else
