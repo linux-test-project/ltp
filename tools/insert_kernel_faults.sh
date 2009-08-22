1b9e21d535804086b3c3d37eabd1429858eb2c75
subrata_modak <subrata_modak>
2009-08-22 12:44:23 +0000

    Introducing the "Kernel Fault Injection Framework" generation and testing capability in LTP. This is in line with the recent proposal made through LTP Paper at OLS 2009: "Putting LTP to test - Validating both the Linux kernel and Test-cases" (http://ltp.sourceforge.net/documentation/technical_papers/Putting_LTP_to_Test.pdf). This infrasturcture will help LTP directly in the following ways: 1) Allow test developers to test their new test cases against a faulted kernel, and then compare it on stable kernel run, impacting in better test development, 2) Allow test engineers to be able to generate more code coverage by traversing the rarely touched parts of the kernel code, As we move forward in using this, we would definitely find some other advantages of this framework. This is V2 of patchset after incorporating comments from Mike, Paul & Garret. [PATCH v2 02/05] Add Script which would actually do the job of injecting faults: Script which would actually do the job of injecting faults by changing various parametrs available under /debug/fail*. This would be done dynamically during LTP run, and, is capable of taking parameters from 0 to 100 to vary the probability of Fault the user wants to inject in the running kernel. Signed-off-by: Subrata Modak<subrata@linux.vnet.ibm.com>.

---
+++ tools/insert_kernel_faults.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+################################################################################
+##                                                                            ##
+## Copyright (c) International Business Machines  Corp., 2009                 ##
+##                                                                            ##
+## This program is free software;  you can redistribute it and/or modify      ##
+## it under the terms of the GNU General Public License as published by       ##
+## the Free Software Foundation; either version 2 of the License, or          ##
+## (at your option) any later version.                                        ##
+##                                                                            ##
+## This program is distributed in the hope that it will be useful, but        ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY ##
+## or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License   ##
+## for more details.                                                          ##
+##                                                                            ##
+## You should have received a copy of the GNU General Public License          ##
+## along with this program;  if not, write to the Free Software               ##
+## Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    ##
+##                                                                            ##
+################################################################################
+#									      ##
+# File :        insert_kernel_faults.sh					      ##
+#									      ##
+# Usage:	insert_kernel_faults.sh <fault_percentage>		      ##
+#									      ##
+# Description:  This is a simple script that inserts faults at various	      ##
+#		subsystems of the kernel. Please refer to the ltp/README      ##
+#		for the various kernel CONFIG options needed to exploit	      ##
+#		all those features					      ##
+#									      ##
+# Author:	Subrata Modak <subrata@linux.vnet.ibm.com>		      ##
+#									      ##
+# History:      Aug 11 2009 - Created - Subrata Modak.			      ##
+#		Aug 17 2009 - Changed the debugfs mount point - Subrata Modak.##
+################################################################################
+
+if [ -z "$1" ]
+	then
+	#Check if Useage has been proper
+	echo "Usage: $0 <fault_percentage>"
+	exit 1
+fi
+
+#These are the types of Subsystems where fault will be injected
+#Make sure debugfs has been mounted
+for FAILTYPE in fail_io_timeout fail_make_request fail_page_alloc failslab
+do
+	echo $1 > /sys/kernel/debug/$FAILTYPE/probability
+	echo 100 > /sys/kernel/debug/$FAILTYPE/interval
+	echo -1 > /sys/kernel/debug/$FAILTYPE/times
+	echo 0 > /sys/kernel/debug/$FAILTYPE/space
+done
+
