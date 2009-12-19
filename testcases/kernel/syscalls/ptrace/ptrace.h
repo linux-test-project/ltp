f7c545c364e5c0d7eef448e1fc8154a900b879cf
yaberauneya <yaberauneya>
2009-12-19 05:07:57 +0000

    Revert to Mike's original copy of ptrace.h.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/ptrace/ptrace.h
+++ testcases/kernel/syscalls/ptrace/ptrace.h
@@ -5,6 +5,8 @@
 #ifndef __LTP_PTRACE_H__
 #define __LTP_PTRACE_H__
 
+#define GARRETT_IS_AN_IDIOT
+#ifndef GARRETT_IS_AN_IDIOT
 #if defined(HAVE_SYS_PTRACE_H)
 # include <sys/ptrace.h>
 #elif defined(HAVE_LINUX_PTRACE_H)
@@ -17,6 +19,7 @@
 #ifdef HAVE_SYS_REG_H
 # include <sys/reg.h>
 #endif
+#undef
 
 /* 
  * ia64's a real pain..
@@ -44,6 +47,32 @@ typedef struct pt_regs ptrace_regs;
 typedef struct user_regs_struct ptrace_regs;
 #else
 #undef HAVE_STRUCT_PTRACE_REGS
+#endif
+#else
+#ifdef HAVE_SYS_PTRACE_H
+# include <sys/ptrace.h>
+#endif
+#ifdef HAVE_SYS_REG_H
+# include <sys/reg.h>
+#endif
+#ifdef __ia64__ /* what a pos */
+# define ia64_fpreg FU_ia64_fpreg
+# define pt_all_user_regs FU_pt_all_user_regs
+#endif
+#ifdef HAVE_ASM_PTRACE_H
+# include <asm/ptrace.h>
+#endif
+#ifdef HAVE_LINUX_PTRACE_H
+# include <linux/ptrace.h>
+#endif
+#undef FU_ia64_fpreg
+#undef FU_pt_all_user_regs
+
+#if defined(HAVE_STRUCT_PT_REGS)
+typedef struct pt_regs ptrace_regs;
+#elif defined(HAVE_STRUCT_USER_REGS_STRUCT)
+typedef struct user_regs_struct ptrace_regs;
+#endif
 #endif
 
 #endif
