3ea1ca7e4ef0ec65a0523068095c115b8228b802
yaberauneya <yaberauneya>
2010-02-05 16:07:12 +0000

    More whitespace only changes.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/fs/acls/acl_link_test.c
+++ testcases/kernel/fs/acls/acl_link_test.c
@@ -18,22 +18,22 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 
-       if(-1 == (s = listxattr(argv[1], list, 1024)) ) {
-               perror("listxattr");
-               return 1;
-       }
-       if(s == 0) {
-               printf("No xattrs defined for %s, further testcase useless\n",argv[1]);
-               return 1;
-       }
-       tok = strtok(list, "\0");
-       s = getxattr(argv[1], tok, (void*)value, 1024);
-       if(-1 == s) {
-               perror("getxattr");
-               return -1;
-       }
+	if(-1 == (s = listxattr(argv[1], list, 1024)) ) {
+		perror("listxattr");
+		return 1;
+	}
+	if(s == 0) {
+		printf("No xattrs defined for %s, further testcase useless\n",argv[1]);
+		return 1;
+	}
+	tok = strtok(list, "\0");
+	s = getxattr(argv[1], tok, (void*)value, 1024);
+	if(-1 == s) {
+		perror("getxattr");
+		return -1;
+	}
 
-       s = lsetxattr(argv[1], tok, (void*)value, s, 0);
+	s = lsetxattr(argv[1], tok, (void*)value, s, 0);
 
 	if (s == -1) {
 		printf ("User unable to change extended attributes %s !\n", argv[1]);
@@ -41,12 +41,12 @@ int main(int argc, char *argv[]){
 		rc = 1;
 	}
 
-       s = lremovexattr(argv[1],tok);
+	s = lremovexattr(argv[1],tok);
 	if (s == -1) {
-                printf ("User unable to remove extended attributes %s !\n", argv[1]);
-                printf("errno = %i\n", errno);
-                rc = 1;
-        }
+		printf ("User unable to remove extended attributes %s !\n", argv[1]);
+		printf("errno = %i\n", errno);
+		rc = 1;
+	}
 
 	return  rc;	
 }
