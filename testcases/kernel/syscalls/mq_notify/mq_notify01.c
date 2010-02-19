1ca6b265f9af0ffdbeee03411c38489899723fc1
Garrett Cooper <yanegomi@gmail.com>
2010-02-18 21:43:07 -0800

    Merge the remote changes (!)
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/mq_notify/mq_notify01.c
+++ testcases/kernel/syscalls/mq_notify/mq_notify01.c
@@ -341,7 +341,6 @@ static int do_test(struct test_case *tc)
          */
         errno = 0;
         sys_ret = mq_notify(fd, &ev);
-        sys_errno = sys_ret != 0 ? errno : 0;
         if (sys_ret < 0)
                 goto TEST_END;
 
