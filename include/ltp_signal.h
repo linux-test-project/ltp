cdb5d0fce58d72633fda52cfaebc7196743b0988
Garrett Cooper <yanegomi@gmail.com>
2010-02-26 04:55:11 -0800

    Commit version with changes provided by Liubo for the rt_sig* fixup diff to avoid creating a whole new file.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- include/ltp_signal.h
+++ include/ltp_signal.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2009 Cisco Systems, Inc.  All Rights Reserved.
+ * Copyright (c) 2009 FUJITSU LIMITED.  All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -20,6 +21,9 @@
  * with this program; if not, write the Free Software Foundation, Inc., 59
  * Temple Place - Suite 330, Boston MA 02111-1307, USA.
  *
+ * Author: Liu Bo <liubo2009@cn.fujitsu.com>
+ * Author: Garrett Cooper <yanegomi@gmail.com>
+ *
  */
 
 #ifndef __LTP_SIGNAL_H
@@ -34,9 +38,7 @@
 
 #ifdef LTP_RT_SIG_TEST
 
-extern int  expected_signal_number;
-
-#if defined(__x86_64__)
+#ifdef __x86_64__
 
 /*
  * From asm/signal.h -- this value isn't exported anywhere outside of glibc and
@@ -45,108 +47,54 @@ extern int  expected_signal_number;
  * (struct sigaction).sa_flags. HEH.
  *
  * I do #undef though, just in case...
- */
-#undef SA_RESTORER
-#define SA_RESTORER	0x04000000
-
-/* 
- * From .../arch/x86/kernel/signal.c:448 --
+ *
+ * Also, from .../arch/x86/kernel/signal.c:448 for v2.6.30 (something or
+ * other):
  *
  * x86-64 should always use SA_RESTORER.
  *
  * -- thus SA_RESTORER must always be defined along with
- * (struct sigaction).sa_restorer.
+ * (struct sigaction).sa_restorer for this architecture.
  */
-#define ARCH_SPECIFIC_RT_SIGACTION_SETUP(__sa) __sa.sa_flags |= SA_RESTORER; __sa.sa_restorer = dummy_restorer;
-/* defined(__x86_64) */
-#else
-#define ARCH_SPECIFIC_RT_SIGACTION_SETUP(__sa)
-#endif
+#undef SA_RESTORER
+#define HAVE_SA_RESTORER
+#define SA_RESTORER	0x04000000
 
-/*
- * To help direct sigsegv_sigaction_handler in determining whether or not the
- * segfault was valid.
- */
-extern int  expected_signal_number;
+struct kernel_sigaction {
+	__sighandler_t ka_sa_handler;
+	unsigned long sa_flags;
+	void (*sa_restorer) (void);
+	sigset_t sa_mask;
+};
 
-/* 
- * A dummy sa_restorer function, because we have to have it if SA_RESTORER is
- * required.
- */
+void (*restore_rt) (void);
+
 inline void
-dummy_restorer(void) {
-	tst_resm(TINFO, "%s called", __func__);
-}
-
-/* 
- * SIGSEGV will be thrown if an overflow occurs, or some other undesired
- * precondition. Let's catch it so the test will at least proceed and not
- * completely bomb the heck out.
- */
-inline void
-sigsegv_sigaction_handler(int signum, siginfo_t *siginfo, void *ucontext) {
-
-	/*
-	 * Backwards compatibility is a pain; I think that's what's driving
-	 * the implicit sa_restorer BS on x86_64, and the reason why mips* and
-	 * ppc* only has a sigaction handler (not rt_sigaction).
-	 *
-	 * GG for backwards compatibility and lack of documentation on an
-	 * internal syscall...
-	 */
-#ifdef __x86_64__
-	if (expected_signal_number == SIGRTMIN) {
-		tst_resm(TINFO, "SIGRTMIN segfaults on x86_64 (known issue).");
-	} else {
-#endif
-	tst_brkm(TBROK | TERRNO, NULL,
-		"Uncaught SIGSEGV; please validate whether or not test meets "
-		"functional requirements as per do_signal and callees in "
-		"$KERN_SRC/arch/<arch>/kernel/signal.c is concerned");
-
-#ifdef __x86_64__
-	}
-#endif
-}
-
-/* 
- * Catch SIGSEGV just in case one of the rt_sigaction calls tosses up a SIGSEGV
- * at the kernel level because of an -EFAULT was tossed by a caller. What a
- * PITA :].
- */
-inline int
-setup_sigsegv_sigaction_handler()
+restorer_h(int signal)
 {
-	int rc = -1;
-	struct sigaction sa;
-	sigset_t sigset;
-
-	/* 
-	 * Catch SIGSEGV just in case one of the rt_sigaction calls tosses up a
-	 * SIGSEGV at the kernel level because of an -EFAULT was tossed by a
-	 * caller. What a PITA :].
-	 */
-	sa.sa_sigaction = (void *)sigsegv_sigaction_handler;
-	/* We want the handler to persist, so don't do SA_RESETHAND... */
-	sa.sa_flags = SA_SIGINFO;
-
-	if (sigemptyset(&sa.sa_mask) < 0) {
-		tst_brkm(TBROK | TERRNO, CLEANUP,
-			"Failed to call sigemptyset for SIGSEGV");
-	} else if (sigaddset(&sigset, SIGSEGV) < 0) {
-		tst_brkm(TBROK | TERRNO, CLEANUP,
-			"Failed to do sigaddset for SIGSEGV");
-	} else if (sigaction(SIGSEGV, &sa, (struct sigaction *) NULL) < 0) {
-		tst_brkm(TBROK | TERRNO, CLEANUP,
-			"Failed to setup sighandler for SIGSEGV");
-	} else {
-		rc = 0;
-	}
-
-	return rc;
-
+	return;
 }
 
-#endif /* RT_SIG_TEST */
+/* Setup an initial signal handler for signal number = sig for x86_64. */
+inline int
+sig_initial(int sig)
+{
+	struct sigaction act, oact;
+
+	act.sa_handler = handler_h;
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask, sig);
+	/* Set act.sa_restorer via syscall(2) */
+	sigaction(sig, &act, &oact);
+	/* Copy oact.sa_restorer via syscall(2) */
+	sigaction(sig, &act, &oact);
+	/* And voila -- we just tricked the kernel into giving us our restorer
+	 * function! */
+	restore_rt = oact.sa_restorer;
+}
+
+#endif /* __x86_64__ */
+
+#endif /* LTP_RT_SIG_TEST */
 
 #endif
