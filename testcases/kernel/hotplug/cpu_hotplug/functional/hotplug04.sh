08941a3fa41efe7a832ac356833c2c389ab71050
Garrett Cooper <yanegomi@gmail.com>
2010-03-05 01:37:03 -0800

    1. POSIXify. 2. Cleanup space.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/hotplug/cpu_hotplug/functional/hotplug04.sh
+++ testcases/kernel/hotplug/cpu_hotplug/functional/hotplug04.sh
@@ -22,49 +22,49 @@ EOF
 
 cpu=0
 until [ $TST_COUNT -gt $TST_TOTAL ]; do
-    cpustate=1
+	cpustate=1
 
-    # Online all the CPUs' keep track of which were already on
-    for i in $(get_all_cpus); do
-        online_cpu $i
-        RC=$?
-        if [ $RC != 0 ]; then
-            : $(( cpu += 1 ))
-            eval "on_${cpu}=$i"
-            echo $i
-        fi
-        if [ $RC = 0 -a "$i" = "cpu0" ]; then
-            cpustate=0
-        fi
-    done
+	# Online all the CPUs' keep track of which were already on
+	for i in $(get_all_cpus); do
+		online_cpu $i
+		RC=$?
+		if [ $RC -ne 0 ]; then
+			: $(( cpu += 1 ))
+			eval "on_${cpu}=$i"
+			echo $i
+		fi
+		if [ $RC -eq 0 -a "$i" = "cpu0" ]; then
+			cpustate=0
+		fi
+	done
 
-    # Now offline all the CPUs
-    for i in $(get_all_cpus); do
-        offline_cpu $i
-        RC=$?
-        if [ $RC = 1 ]; then
-            if [ "$i" != "cpu0" ]; then
-                tst_resm TFAIL "Could not shutdown CPU : $i (is hotplug available?)"
-            else
-                tst_resm TPASS "Successfully shutdown offlined first CPU, $i"
-            fi
-        fi
-    done
+	# Now offline all the CPUs
+	for i in $(get_all_cpus); do
+		offline_cpu $i
+		RC=$?
+		if [ $RC = 1 ]; then
+			if [ "x$i" != "xcpu0" ]; then
+				tst_resm TFAIL "Did not offline first CPU (offlined $i instead)"
+			else
+				tst_resm TPASS "Successfully offlined first CPU, $i"
+			fi
+		fi
+	done
 
-    # Online the ones that were on initially
-    until [ $cpu = 0 ]; do
-        online_cpu $(eval "echo \$on_${cpu}")
-        : $(( cpu -= 1 ))
-    done
+	# Online the ones that were on initially
+	until [ $cpu = 0 ]; do
+		online_cpu $(eval "echo \$on_${cpu}")
+		: $(( cpu -= 1 ))
+	done
 
-    # Return CPU 0 to its initial state
-    if [ $cpustate = 1 ]; then
-        online_cpu 0
-    else 
-        offline_cpu 0
-    fi
+	# Return CPU 0 to its initial state
+	if [ $cpustate = 1 ]; then
+		online_cpu 0
+	else 
+		offline_cpu 0
+	fi
 
-    : $(( TST_COUNT += 1 ))
+	: $(( TST_COUNT += 1 ))
 
 done
 
