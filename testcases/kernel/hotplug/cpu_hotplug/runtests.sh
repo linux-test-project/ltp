b3fb75df0076a12276071ff06421ee41e415d01d
Garrett Cooper <yanegomi@gmail.com>
2010-03-05 02:41:49 -0800

    Clean up some logic.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/hotplug/cpu_hotplug/runtests.sh
+++ testcases/kernel/hotplug/cpu_hotplug/runtests.sh
@@ -3,7 +3,7 @@
 
 CPU_TO_TEST=${CPU_TO_TEST:=1}
 LOOPS=${LOOPS:=1}
-export LHCS_PATH=`pwd`
+export LHCS_PATH=${LHCS_PATH:-$LTPROOT/testcases/bin/cpu_hotplug}
 
 
 echo "CPU:        $CPU_TO_TEST"
@@ -11,7 +11,6 @@ echo "LOOPS:      $LOOPS"
 echo "LHCS_PATH:  $LHCS_PATH"
 
 
-for case in functional/hotplug??.sh
-do
-  $case $CPU_TO_TEST
+for case in functional/hotplug??.sh; do
+	$case $CPU_TO_TEST
 done
