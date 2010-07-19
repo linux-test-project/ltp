fb777bd059bf2f3610fcd1c233182a572fcaef8d
Garrett Cooper <yanegomi@gmail.com>
2010-07-19 02:13:37 -0700

    Add simple wrapper for running all option groups.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

---
+++ testcases/open_posix_testsuite/bin/run-all-posix-option-group-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# A simple wrapper for executing all of the tests.
+#
+# See COPYING for licensing details.
+#
+# Garrett Cooper, July 2010
+#
+
+FAILED=0
+RPOG_SCRIPT="$(dirname "$0")/run-posix-option-group-test.sh"
+
+for option_group in AIO MEM MSG SEM SIG THR TMR TPS; do
+	if ! $PROG_SCRIPT $option_group; then
+		FAILED=1
+	fi
+done
+
+exit $FAILED
