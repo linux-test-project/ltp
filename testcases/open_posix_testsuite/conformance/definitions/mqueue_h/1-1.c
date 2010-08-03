8c50bcc8fd2d15288934179fb98a75ef28876668
Garrett Cooper <yanegomi@gmail.com>
2010-08-02 23:39:51 -0700

    Add testcase to check contents of mq_attr.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/open_posix_testsuite/conformance/definitions/mqueue_h/1-1.c
+++ testcases/open_posix_testsuite/conformance/definitions/mqueue_h/1-1.c
@@ -1,15 +1,51 @@
-/* 
-   Test for the existance and valid format
-   of the mq_attr structure as specified on 
-   line 9678 of the Base Definitions document
-*/
+/*
+ * Garrett Cooper, August 2010
+ *
+ * Verify that mq_flags, mq_maxmsg, mq_msgsize, and mq_curmsgs exist in the
+ * struct mq_attr structure and are long integers as per the
+ * IEEE Std 1003.1-2008 spec.
+ */
 
+#include <limits.h>
 #include <mqueue.h>
-#include "posixtest.h"
 #include <stdio.h>
+#include "posixtest.h"
+
+#define TEST_LIMITS(structure_and_field) do {			\
+		printf("%s..\n", # structure_and_field);	\
+		printf("\t.. -LONG_MIN\n");			\
+		structure_and_field = -LONG_MIN;		\
+		if (structure_and_field != -LONG_MIN) {		\
+			return (PTS_FAIL);			\
+		}						\
+		printf("\t.. LONG_MAX\n");			\
+		structure_and_field = LONG_MAX;			\
+		if (structure_and_field != LONG_MAX) {		\
+			return (PTS_FAIL);			\
+		}						\
+		printf("\t.. -(LONG_MIN+1)\n");			\
+		structure_and_field = -(LONG_MIN+1);		\
+		if (structure_and_field != LONG_MAX) {		\
+			return (PTS_FAIL);			\
+		}						\
+		structure_and_field = LONG_MAX+1;		\
+		printf("\t.. (LONG_MAX+1)\n");			\
+		if (structure_and_field != LONG_MIN) {		\
+			return (PTS_FAIL);			\
+		}						\
+	} while (0)
 
 int main()
 {
-	fprintf(stderr, "Not Implemented!\n");
-	return PTS_UNTESTED;
+	struct mq_attr mqs;
+
+	TEST_LIMITS(mqs.mq_flags);
+	TEST_LIMITS(mqs.mq_maxmsg);
+	TEST_LIMITS(mqs.mq_msgsize);
+	TEST_LIMITS(mqs.mq_curmsgs);
+
+	printf("PASSED!\n");
+
+	return (PTS_PASS);
+
 }
