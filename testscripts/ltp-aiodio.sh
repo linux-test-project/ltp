c362a1afaeba0f233650a2028d4b22d2c86d7b56
Garrett Cooper <yanegomi@gmail.com>
2010-09-10 09:05:02 -0700

    Again, bad search and replace -_-.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testscripts/ltp-aiodio.sh
+++ testscripts/ltp-aiodio.sh
@@ -11,7 +11,7 @@
 
 cd `dirname $0`
 export LTPROOT=${PWD}
-echo $LTPROOT | grep testscripts > /dev/nulll 2>&1
+echo $LTPROOT | grep testscripts > /dev/null 2>&1
 if [ $? -eq 0 ]; then
  cd ..
  export LTPROOT=${PWD}
