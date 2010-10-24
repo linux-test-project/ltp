9bed07c055f6d3086a93bac936de1494d1d6aaae
Garrett Cooper <yanegomi@gmail.com>
2010-10-23 21:56:44 -0700

    Properly deal with sync_file_range support on arm.
    Add checks and necessary argument shifting with __LITTLE_ENDIAN and __BIG_ENDIAN.
    
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/sync_file_range/sync_file_range01.c
+++ testcases/kernel/syscalls/sync_file_range/sync_file_range01.c
@@ -238,20 +238,19 @@ static inline long syncfilerange(int fd, off64_t offset, off64_t nbytes,
 				 unsigned int flags)
 {
 
-#if (defined(__powerpc64__) || defined(__powerpc__)) && (__WORDSIZE==32)
-
+#if (defined(__arm__) || defined(__powerpc__) || defined(__powerpc64__)) && \
+    (__WORDSIZE == 32)
+#if __BYTE_ORDER == __BIG_ENDIAN
 	return syscall(__NR_sync_file_range2, fd, flags, (int)(offset >> 32),
 		       (int)offset, (int)(nbytes >> 32), (int)nbytes);
-
-#elif (defined(__powerpc64__) || defined(__powerpc__)) && (__WORDSIZE==64)
-
-	return syscall(__NR_sync_file_range2, fd, flags, offset, nbytes);
+#elif __BYTE_ORDER == __LITTLE_ENDIAN
+	return syscall(__NR_sync_file_range2, fd, flags, (int)offset,
+		       (int)(offset >> 32), nbytes, (int)(nbytes >> 32));
+#endif
 #else
-
-	return syscall(__NR_sync_file_range, fd, offset, nbytes, flags);
+	return syscall(__NR_sync_file_range2, fd, flags, offset, nbytes);
 #endif
 
-	return 0;
 }
 
 /******************************************************************************/
