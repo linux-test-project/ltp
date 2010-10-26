3bec2b3716b72e8633e346c465c4280566d878aa
Garrett Cooper <yanegomi@gmail.com>
2010-10-25 23:27:26 -0700

    Remove set -x debug.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/open_posix_testsuite/bin/run-tests.sh
+++ testcases/open_posix_testsuite/bin/run-tests.sh
@@ -46,8 +46,6 @@ run_test() {
 
 	complog=`basename $testname`.log.$$
 
-	set -x
-
 	sh -c "$SCRIPT_DIR/t0 $TIMEOUT_VAL ./$1 $(cat ./$(echo "$1" | sed 's,\.[^\.]*,,').args)" > $complog 2>&1
 
 	set +x
