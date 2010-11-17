66d67b5fcbd3581e2e0e088830d574c4d59597da
Garrett Cooper <yanegomi@gmail.com>
2010-11-16 22:42:48 -0800

    Fix invocation by fixing typo.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- runltplite.sh
+++ runltplite.sh
@@ -237,7 +237,7 @@ main()
             # Insurance to make sure that the first element in the pipe
             # completed successfully.
             cat_ok_sentinel=$TMP/cat_ok.$$
-	    (cat $SCENARIO_FILES && touch "$cat_ok_sentinel") | \
+	    (cat $SCENARIO_LISTS && touch "$cat_ok_sentinel") | \
                 while read scenfile; do
 
                     scenfile=${LTPROOT}/runtest/$scenfile
