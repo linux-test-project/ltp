066aec9504c1f782e3b029dbba536d6b5d6e364c
Garrett Cooper <yanegomi@gmail.com>
2010-11-20 15:33:21 -0800

    Add a testcase for another PID.
    Add a test to see whether or not one could determine the cpu clock ID
    for PID = 1. This won't work except for root (necessarily), but if it
    does fail then mark it UNRESOLVED.
    
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

---
+++ testcases/open_posix_testsuite/conformance/interfaces/clock_getcpuclockid/1-2.c
@@ -0,0 +1,54 @@
+/*   
+ * Copyright (c) 2002, Intel Corporation. All rights reserved.
+ * Created by:  julie.n.fleischer REMOVE-THIS AT intel DOT com
+ * This file is licensed under the GPL license.  For the full content
+ * of this license, see the COPYING file at the top level of this 
+ * source tree.
+ *
+ * @pt:CPT
+ * General test that clock_getcpuclockid() returns CPU-time clock for a 
+ * process.  The process chosen is the current process.
+ *
+ * If the process described by pid exists and the calling process has
+ * permission, the clock ID of this clock shall be returned in clock_id.
+ *
+ */
+#define _XOPEN_SOURCE 600
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include "posixtest.h"
+
+int main(int argc, char *argv[])
+{
+#if !defined(_POSIX_CPUTIME) || _POSIX_CPUTIME == -1
+        printf("_POSIX_CPUTIME unsupported\n");
+        return PTS_UNSUPPORTED;
+#else
+	struct timespec tp1;
+	clockid_t clockid;
+
+	if (sysconf(_SC_CPUTIME) == -1) {
+		printf("_POSIX_CPUTIME unsupported\n");
+		return PTS_UNSUPPORTED;
+	}
+
+	if (clock_getcpuclockid(1, &clockid) != 0) {
+		printf("clock_getcpuclockid() failed\n");
+		return PTS_UNRESOLVED;
+	}
+
+	/* 
+	 * Verify that it returned a valid clockid_t that can be used in other
+	 * functions
+	 */
+	if (clock_gettime(clockid, &tp1) != 0) {
+		printf("clock_getcpuclockid() returned an invalid clockid_t: "
+		    "%d\n", clockid);
+		return PTS_FAIL;
+	}
+
+	printf("Test PASSED\n");
+	return PTS_PASS;
+#endif
+}
