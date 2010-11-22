c5ed3e928ca54d32c5f0df06ed9237a47e7dc894
Garrett Cooper <yanegomi@gmail.com>
2010-11-22 15:13:01 -0800

    Fix more compiler warnings and a test bug.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/tkill/tkill02.c
+++ testcases/kernel/syscalls/tkill/tkill02.c
@@ -1,41 +1,41 @@
 /******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007                                   */
-/*                                                                            */
+/* Copyright (c) Crackerjack Project., 2007				   */
+/*									    */
 /* This program is free software;  you can redistribute it and/or modify      */
 /* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or          */
-/* (at your option) any later version.                                        */
-/*                                                                            */
-/* This program is distributed in the hope that it will be useful,            */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  */
-/* the GNU General Public License for more details.                           */
-/*                                                                            */
-/* You should have received a copy of the GNU General Public License          */
-/* along with this program;  if not, write to the Free Software               */
+/* the Free Software Foundation; either version 2 of the License, or	  */
+/* (at your option) any later version.					*/
+/*									    */
+/* This program is distributed in the hope that it will be useful,	    */
+/* but WITHOUT ANY WARRANTY;  without even the implied warranty of	    */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		  */
+/* the GNU General Public License for more details.			   */
+/*									    */
+/* You should have received a copy of the GNU General Public License	  */
+/* along with this program;  if not, write to the Free Software	       */
 /* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    */
-/*                                                                            */
+/*									    */
 /******************************************************************************/
 /******************************************************************************/
-/*                                                                            */
-/* File:        tkill02.c                                           */
-/*                                                                            */
-/* Description: This tests the tkill() syscall                      */
-/*                                                                            */
-/* Usage:  <for command-line>                                                 */
-/* tkill02 [-c n] [-e][-i n] [-I x] [-p x] [-t]                     */
-/*      where,  -c n : Run n copies concurrently.                             */
-/*              -e   : Turn on errno logging.                                 */
-/*              -i n : Execute test n times.                                  */
-/*              -I x : Execute test for x seconds.                            */
-/*              -P x : Pause for x seconds between iterations.                */
-/*              -t   : Turn on syscall timing.                                */
-/*                                                                            */
-/* Total Tests: 1                                                             */
-/*                                                                            */
-/* Test Name:   tkill02                                             */
-/* History:     Porting from Crackerjack to LTP is done by                    */
-/*              Manas Kumar Nayak maknayak@in.ibm.com>                        */
+/*									    */
+/* File:	tkill02.c					   */
+/*									    */
+/* Description: This tests the tkill() syscall		      */
+/*									    */
+/* Usage:  <for command-line>						 */
+/* tkill02 [-c n] [-e][-i n] [-I x] [-p x] [-t]		     */
+/*      where,  -c n : Run n copies concurrently.			     */
+/*	      -e   : Turn on errno logging.				 */
+/*	      -i n : Execute test n times.				  */
+/*	      -I x : Execute test for x seconds.			    */
+/*	      -P x : Pause for x seconds between iterations.		*/
+/*	      -t   : Turn on syscall timing.				*/
+/*									    */
+/* Total Tests: 1							     */
+/*									    */
+/* Test Name:   tkill02					     */
+/* History:     Porting from Crackerjack to LTP is done by		    */
+/*	      Manas Kumar Nayak maknayak@in.ibm.com>			*/
 /******************************************************************************/
 
 #include <stdio.h>
@@ -52,121 +52,122 @@
 #include "linux_syscall_numbers.h"
 
 /* Extern Global Variables */
-extern int Tst_count;           /* counter for tst_xxx routines.         */
-extern char *TESTDIR;           /* temporary dir created by tst_tmpdir() */
+extern int Tst_count;	   /* counter for tst_xxx routines.	 */
+extern char *TESTDIR;	   /* temporary dir created by tst_tmpdir() */
 
 /* Global Variables */
 char *TCID = "tkill02";  /* Test program identifier.*/
 int  testno;
-int  TST_TOTAL = 2;                   /* total number of tests in this file.   */
+int  TST_TOTAL = 2;		   /* total number of tests in this file.   */
 
 /* Extern Global Functions */
 /******************************************************************************/
-/*                                                                            */
-/* Function:    cleanup                                                       */
-/*                                                                            */
+/*									    */
+/* Function:    cleanup						       */
+/*									    */
 /* Description: Performs all one time clean up for this test on successful    */
-/*              completion,  premature exit or  failure. Closes all temporary */
-/*              files, removes all temporary directories exits the test with  */
-/*              appropriate return code by calling tst_exit() function.       */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
+/*	      completion,  premature exit or  failure. Closes all temporary */
+/*	      files, removes all temporary directories exits the test with  */
+/*	      appropriate return code by calling tst_exit() function.       */
+/*									    */
+/* Input:       None.							 */
+/*									    */
+/* Output:      None.							 */
+/*									    */
 /* Return:      On failure - Exits calling tst_exit(). Non '0' return code.   */
-/*              On success - Exits calling tst_exit(). With '0' return code.  */
-/*                                                                            */
+/*	      On success - Exits calling tst_exit(). With '0' return code.  */
+/*									    */
 /******************************************************************************/
 extern void cleanup() {
-        /* Remove tmp dir and all files in it */
-        TEST_CLEANUP;
-        tst_rmdir();
-
-        /* Exit with appropriate return code. */
-        tst_exit();
+	/* Remove tmp dir and all files in it */
+	TEST_CLEANUP;
+	tst_rmdir();
 }
 
 /* Local  Functions */
 /******************************************************************************/
-/*                                                                            */
-/* Function:    setup                                                         */
-/*                                                                            */
+/*									    */
+/* Function:    setup							 */
+/*									    */
 /* Description: Performs all one time setup for this test. This function is   */
-/*              typically used to capture signals, create temporary dirs      */
-/*              and temporary files that may be used in the course of this    */
-/*              test.                                                         */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits by calling cleanup().                      */
-/*              On success - returns 0.                                       */
-/*                                                                            */
+/*	      typically used to capture signals, create temporary dirs      */
+/*	      and temporary files that may be used in the course of this    */
+/*	      test.							 */
+/*									    */
+/* Input:       None.							 */
+/*									    */
+/* Output:      None.							 */
+/*									    */
+/* Return:      On failure - Exits by calling cleanup().		      */
+/*	      On success - returns 0.				       */
+/*									    */
 /******************************************************************************/
 void setup() {
-        /* Capture signals if any */
-        /* Create temporary directories */
-        TEST_PAUSE;
-        tst_tmpdir();
+	/* Capture signals if any */
+	/* Create temporary directories */
+	TEST_PAUSE;
+	tst_tmpdir();
 }
 
 struct test_case_t {
-        int tid;
-        int exp_errno;
+	int tid;
+	int exp_errno;
 } test_cases[] = {
-        { -1, EINVAL },
-        { 99999, ESRCH }
+	{ -1, EINVAL },
+	{ 99999, ESRCH }
 };
 
 int main(int ac, char **av) {
-        int lc;                 /* loop counter */
-        char *msg;              /* message returned from parse_opts */
+	int lc;		/* loop counter */
+	char *msg;	/* message returned from parse_opts */
 	int i, succeed=0, fail=0;
 	
-        /* parse standard options */
-        if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL){
-             tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-             tst_exit();
-           }
+	/* parse standard options */
+	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
+		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
-        setup();
+	setup();
 
-        /* Check looping state if -i option given */
-        for (lc = 0; TEST_LOOPING(lc); ++lc) {
-                Tst_count = 0;
-                for (testno = 0; testno < TST_TOTAL; ++testno) {
+	/* Check looping state if -i option given */
+	for (lc = 0; TEST_LOOPING(lc); ++lc) {
+		Tst_count = 0;
+		for (testno = 0; testno < TST_TOTAL; ++testno) {
 
 			for(i=0; i<2; i++){
-		                TEST(syscall(__NR_tkill, test_cases[i].tid, SIGUSR1));
-		     		if(TEST_RETURN == -1) {
-			            	if(TEST_ERRNO == test_cases[i].exp_errno){
-						tst_resm(TINFO, "%s() call with tid:%d got expected errno:%d", TCID, test_cases[i].tid,TEST_ERRNO);
+
+				TEST(syscall(__NR_tkill, test_cases[i].tid, SIGUSR1));
+
+				if (TEST_RETURN == -1) {
+				    	if (TEST_ERRNO == test_cases[i].exp_errno) {
+						tst_resm(TINFO|TTERRNO,
+							"tkill call with "
+							"tid=%d got expected "
+							"errno",
+							test_cases[i].tid);
 						succeed++;
-					}
-					else{
-						tst_resm(TINFO, "%s() call with tif:%d got unexpected errno = %d : %s", TCID, test_cases[i].tid, TEST_ERRNO, strerror(TEST_ERRNO));
+					} else {
+						tst_resm(TFAIL|TTERRNO,
+							"tkill call with "
+							"tid=%d got unexpected "
+							"errno",
+							test_cases[i].tid);
 						fail++;
 					}
-				}else{
-					tst_resm(TINFO, "%s() with tid:%d succeed,but should fail", TCID, test_cases[i].tid);
+				} else {
+					tst_resm(TINFO,
+						"tkill with tid=%d succeeded "
+						"unexpectedly",
+						test_cases[i].tid);
 				}
 			}
-		
-		if(fail != 0){
-			tst_resm(TFAIL, "tkill() call FAILED");
-			cleanup();
-			tst_exit();
-        	}
-        	else{
-			tst_resm(TPASS, "tkill() call PASSED");
-	        }
 
-                
-	    }
-     }	
+			if (fail != 0) {
+				tst_brkm(TFAIL|TERRNO, cleanup, "tkill failed");
+			} else {
+				tst_resm(TPASS, "tkill() passed");
+			}
+		}
+	}
 	cleanup();
-        tst_exit();
+	tst_exit();
 }
-
