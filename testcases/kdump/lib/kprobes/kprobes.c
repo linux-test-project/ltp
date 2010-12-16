771ff619ef90f7364197db6fc9753647a1bbf83b
Garrett Cooper <yanegomi@gmail.com>
2010-12-16 03:04:26 -0800

    Fix style and unnecessary 0x before %p.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kdump/lib/kprobes/kprobes.c
+++ testcases/kdump/lib/kprobes/kprobes.c
@@ -12,39 +12,40 @@
 
 /* Proxy routine having the same arguments as actual do_fork() routine */
 long jdo_fork(unsigned long clone_flags, unsigned long stack_start,
-              struct pt_regs *regs, unsigned long stack_size,
-              int __user * parent_tidptr, int __user * child_tidptr)
+    struct pt_regs *regs, unsigned long stack_size,
+    int __user * parent_tidptr, int __user * child_tidptr)
 {
-  printk("jprobe: clone_flags=0x%lx, stack_size=0x%lx, regs=0x%p\n",
-         clone_flags, stack_size, regs);
-  /* Always end with a call to jprobe_return(). */
-  jprobe_return();
+	printk("jprobe: clone_flags=0x%lx, stack_size=0x%lx, regs=%p\n",
+	    clone_flags, stack_size, regs);
+	/* Always end with a call to jprobe_return(). */
+	jprobe_return();
 
-  return 0;
+	return 0;
 }
 
 static struct jprobe my_jprobe = {
-        .entry = jdo_fork
+	.entry = jdo_fork
 };
 
 static int __init jprobe_init(void)
 {
-  int ret;
-  my_jprobe.kp.symbol_name = "do_fork";
+	int ret;
+	my_jprobe.kp.symbol_name = "do_fork";
 
-  if ((ret = register_jprobe(&my_jprobe)) <0) {
-    printk("register_jprobe failed, returned %d\n", ret);
-    return -1;
-  }
-  printk("Planted jprobe at %p, handler addr %p\n",
-         my_jprobe.kp.addr, my_jprobe.entry);
-  return 0;
+	if ((ret = register_jprobe(&my_jprobe)) <0) {
+		printk("register_jprobe failed, returned %d\n", ret);
+		/* XXX: Exit code is wrong. */
+		return -1;
+	}
+	printk("Planted jprobe at %p, handler addr %p\n",
+	    my_jprobe.kp.addr, my_jprobe.entry);
+	return 0;
 }
 
 static void __exit jprobe_exit(void)
 {
-  unregister_jprobe(&my_jprobe);
-  printk("jprobe unregistered\n");
+	unregister_jprobe(&my_jprobe);
+	printk("jprobe unregistered\n");
 }
 
 module_init(jprobe_init)
