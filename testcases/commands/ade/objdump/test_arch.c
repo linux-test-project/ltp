a872124e092678f40ded956532db5b57f4db3549
Garrett Cooper <yanegomi@gmail.com>
2010-12-16 14:37:00 -0800

    Fix overly ambitious addition of tst_exit to source.
    Noticed-by: Many
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/commands/ade/objdump/test_arch.c
+++ testcases/commands/ade/objdump/test_arch.c
@@ -3,17 +3,16 @@
 int main(void)
 {
 
-	int i=0;
+	int i = 0;
 
-	for (i=0; i<10; i++) {
-		if (i %2)
-			printf(" Hello \n ");
-		else
+	for (i = 0; i < 10; i++) {
+		if ((i % 2) == 0)
 			printf("World \n");
+		else
+			printf(" Hello \n ");
 	}
 
 	printf(" the end \n");
 
-	tst_exit();
-
+	return 0;
 }
