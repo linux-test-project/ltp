61ca66978059c21c98d4f10b09ca071b1f6056a8
Garrett Cooper <yanegomi@gmail.com>
2010-12-17 16:42:26 -0800

    Fix the compile and provide useful diags for failures.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/controllers/memcg/control/mem_process.c
+++ testcases/kernel/controllers/memcg/control/mem_process.c
@@ -1,34 +1,38 @@
-/*******************************************************************************/
-/*                                                                             */
-/*  Copyright (c) 2010 Mohamed Naufal Basheer                                  */
-/*                                                                             */
-/*  This program is free software;  you can redistribute it and/or modify      */
-/*  it under the terms of the GNU General Public License as published by       */
-/*  the Free Software Foundation; either version 2 of the License, or          */
-/*  (at your option) any later version.                                        */
-/*                                                                             */
-/*  This program is distributed in the hope that it will be useful,            */
-/*  but WITHOUT ANY WARRANTY;  without even the implied warranty of            */
-/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  */
-/*  the GNU General Public License for more details.                           */
-/*                                                                             */
-/*  You should have received a copy of the GNU General Public License          */
-/*  along with this program;  if not, write to the Free Software               */
-/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA    */
-/*                                                                             */
-/*  File:    mem_process.c                                                     */
-/*                                                                             */
-/*  Purpose: act as a memory hog for the memcg_control tests                   */
-/*                                                                             */
-/*  Author:  Mohamed Naufal Basheer <naufal11@gmail.com >                      */
-/*                                                                             */
-/*******************************************************************************/
-#include <stdio.h>
+/*****************************************************************************/
+/*                                                                           */
+/*  Copyright (c) 2010 Mohamed Naufal Basheer                                */
+/*                                                                           */
+/*  This program is free software;  you can redistribute it and/or modify    */
+/*  it under the terms of the GNU General Public License as published by     */
+/*  the Free Software Foundation; either version 2 of the License, or        */
+/*  (at your option) any later version.                                      */
+/*                                                                           */
+/*  This program is distributed in the hope that it will be useful,          */
+/*  but WITHOUT ANY WARRANTY;  without even the implied warranty of          */
+/*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                */
+/*  the GNU General Public License for more details.                         */
+/*                                                                           */
+/*  You should have received a copy of the GNU General Public License        */
+/*  along with this program;  if not, write to the Free Software             */
+/*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  */
+/*                                                                           */
+/*  File:    mem_process.c                                                   */
+/*                                                                           */
+/*  Purpose: act as a memory hog for the memcg_control tests                 */
+/*                                                                           */
+/*  Author:  Mohamed Naufal Basheer <naufal11@gmail.com >                    */
+/*                                                                           */
+/*****************************************************************************/
+
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <err.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/mman.h>
 
 /*
  * Named pipe to act as a communication channel between
@@ -51,21 +55,21 @@ void process_options(int argc, char **argv)
 	opterr = 0;
 	while ((c = getopt(argc, argv, "pm:")) != -1) {
 		switch(c) {
-			case 'p':
-				printf("%d\n",getpagesize());
-				exit(0);
-			case 'm':
-				memsize = strtoul(optarg, &end, 10);
-				if (*end)
-					errx(2, "Invalid -m usage");
-				break;
-			default:
-				errx(2, "Invalid option specifed");
+		case 'm':
+			memsize = strtoul(optarg, &end, 10);
+			if (*end != '\0')
+				errx(2, "invalid -m usage");
+			break;
+		case 'p':
+			printf("%d\n", getpagesize());
+			exit(0);
+		default:
+			errx(2, "invalid option specifed");
         	}
     	}
 
 	if (memsize <= 0)
-		errx(3, "Invalid usage");
+		errx(3, "invalid usage");
 }
 
 /*
@@ -84,16 +88,17 @@ void mem_map()
 {
 	static char *p;
 
-	if (!flag_allocated) {
-		p = mmap(NULL, memsize, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, 0, 0);
-		if (p == MAP_FAILED)
-			errx(4, "mmap failed");
-		touch_memory(p);
-	}else {
+	if (flag_allocated) {
 		if (munmap(p, memsize) == -1)
-			errx(5, "munmap failed");
+			err(5, "munmap failed");
+	} else {
+		p = mmap(NULL, memsize, PROT_READ|PROT_WRITE,
+		    MAP_SHARED|MAP_ANONYMOUS, 0, 0);
+		if (p == MAP_FAILED)
+			err(4, "mmap failed");
+		touch_memory(p);
 	}
-	flag_allocated ^= 1;
+	flag_allocated = !flag_allocated;
 }
 
 /*
@@ -104,12 +109,11 @@ char action()
 	char ch;
 	int fd;
 
-	fd = open(STATUS_PIPE, O_RDONLY);
-	if (fd < 0)
-		errx(6, "Error opening named pipe");
+	if ((fd = open(STATUS_PIPE, O_RDONLY)) == -1)
+		err(6, "Error opening named pipe");
 
-	if (read(fd, &ch, 1) < 0)
-		errx(7, "Error reading named pipe");
+	if (read(fd, &ch, 1) == -1)
+		err(7, "Error reading named pipe");
 
 	close(fd);
 
@@ -133,9 +137,9 @@ int main(int argc, char **argv)
 
 		if (ch == 'm')
 			mem_map();
-	}while (ch != 'x');
+	} while (ch != 'x');
 
 	remove(STATUS_PIPE);
 
-	tst_exit();
+	return 0;
 }
