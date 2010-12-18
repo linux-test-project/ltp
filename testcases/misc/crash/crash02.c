1ffecc92962736d20f878377773573f43b1f38b5
Garrett Cooper <yanegomi@gmail.com>
2010-12-18 00:03:44 -0800

    Fix test.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/misc/crash/crash02.c
+++ testcases/misc/crash/crash02.c
@@ -12,12 +12,12 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA	02111-1307, USA.
  */
 
 /*
@@ -117,11 +117,11 @@ void setup()
 
 void help()
 {
-	printf("  -x      dry run, hexdump random code instead\n");
-	printf("  -l x    max syscall no\n");
-	printf("  -v x    verbose level\n");
-	printf("  -s x    random seed\n");
-	printf("  -n x    ntries\n");
+	printf("	-x		dry run, hexdump random code instead\n");
+	printf("	-l x		max syscall no\n");
+	printf("	-v x		verbose level\n");
+	printf("	-s x		random seed\n");
+	printf("	-n x		ntries\n");
 }
 
 /*
@@ -146,37 +146,35 @@ void record_errno(unsigned int n);
 int
 main (int argc, char *argv[])
 {
-  char *msg;
-  int lc;
+	char *msg;
+	int lc;
 
-  if ((msg=parse_opts(argc, argv, options, help)) != (char *) NULL)
-	tst_brkm(TBROK, cleanup, "OPTION PARSING ERROR - %s", msg);
+	if ((msg = parse_opts(argc, argv, options, help)) != NULL)
+		tst_brkm(TBROK, cleanup, "OPTION PARSING ERROR - %s", msg);
 
-  if (v_opt)
-	  verbose_level = atoi(v_copt);
+	if (v_opt)
+		verbose_level = atoi(v_copt);
 
-  if (n_opt)
-	  ntries = atoi(n_copt);
+	if (n_opt)
+		ntries = atoi(n_copt);
 
-  if (l_opt)
-	  sysno_max = atoi(l_copt);
+	if (l_opt)
+		sysno_max = atoi(l_copt);
 
-  if (s_opt)
-	  nseed = atoi(s_copt);
-  else
-	  nseed = time(NULL);
+	if (s_opt)
+		nseed = atoi(s_copt);
+	else
+		nseed = time(NULL);
 
-   setup();
+	setup();
 
-   for (lc=0; TEST_LOOPING(lc); lc++)
-    {
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
 		Tst_count=0;
 
-		tst_resm(TINFO, "crashme02 %d %d %d",
-	   		sysno_max, nseed, ntries);
+		tst_resm(TINFO, "crashme02 %d %d %d", sysno_max, nseed, ntries);
 
-  		srand (nseed);
-		badboy_fork ();
+		srand(nseed);
+		badboy_fork();
 
 		/* still there? */
 		tst_resm(TPASS, "we're still here, OS seems to be robust");
@@ -192,60 +190,57 @@ int badboy_pid;
 
 void my_signal (int sig, void (*func) ());
 
-void monitor_fcn (int sig)
+void monitor_fcn(int sig)
 {
-  int status;
+	int status;
 
- if (verbose_level >= 3)
-	    printf ("time limit reached on pid. using kill.\n");
-
-  status = kill (badboy_pid, SIGKILL);
-  if (status < 0)
-    {
 	if (verbose_level >= 3)
-      		printf ("failed to kill process\n");
-    }
+	printf ("time limit reached on pid. using kill.\n");
+
+	status = kill(badboy_pid, SIGKILL);
+	if (status < 0) {
+		if (verbose_level >= 3)
+			printf ("failed to kill process\n");
+	}
 }
 
 void
-badboy_fork ()
+badboy_fork()
 {
 	int status, pid;
-
-      status = fork ();
-      badboy_pid = status;
-      if (status == 0)	/* badboy */
-	{
+	pid_t child;
+	child = fork();
+	badboy_pid = status;
+	switch (child) {
+	case -1:
+		perror("fork");
+	case 0:
 #ifdef DEBUG_LATE_BADBOY
-	  sleep(ntries*MAX_TRY_TIME+10);
+		sleep(ntries*MAX_TRY_TIME+10);
 #else
-	  badboy_loop ();
+		badboy_loop();
 #endif
-	  exit (0);	/* tough guy */
+		exit(0);
+	default:
+		if (verbose_level > 3)
+			printf ("badboy pid = %d\n", badboy_pid);
+
+		/* don't trust the child to return at night */
+		my_signal (SIGALRM, monitor_fcn);
+		alarm (ntries*MAX_TRY_TIME);
+
+		pid = waitpid (-1, &status, WUNTRACED);
+		if (pid <= 0)
+			perror("wait");
+		else {
+			if (verbose_level > 3)
+				printf("pid %d exited with status %d\n",
+				    pid, status);
+#if 0
+			record_status(status);
+#endif
+		}
 	}
-      else if (status < 0)
-	perror ("fork");
-      else			/* parent watching over badboy */
-	{
-	  if (verbose_level > 3)
-	  	printf ("badboy pid = %d\n", badboy_pid);
-
-/* don't trust the child to return at night */
-      my_signal (SIGALRM, monitor_fcn);
-      alarm (ntries*MAX_TRY_TIME);
-
-	  pid = waitpid (-1, &status, WUNTRACED);
-	  if (pid <= 0)
-	  {
-		  perror ("wait");
-	  } else {
-	  	if (verbose_level > 3)
-		  printf ("pid %d exited with status %d\n", pid, status);
-		#if 0
-		record_status(status);
-		#endif
-	   }
-	}	/* parent */
 	alarm(0);
 }
 
@@ -267,17 +262,16 @@ void record_errno(unsigned int n)
 void
 summarize_errno ()
 {
-  int i;
+	int i;
 
-  if (x_opt || verbose_level < 2)
-		  return;
+	if (x_opt || verbose_level < 2)
+			return;
 
-  printf ("errno status ... number of cases\n");
-  for (i = 0; i < STATUS_MAX; i++)
-    {
-      if (errno_table[i])
-      	printf ( "%12d ... %5d\n", i, errno_table[i]);
-    }
+	printf ("errno status ... number of cases\n");
+	for (i = 0; i < STATUS_MAX; i++) {
+		if (errno_table[i])
+			printf ( "%12d ... %5d\n", i, errno_table[i]);
+	}
 }
 
 /* ************* badboy ******************************************* */
@@ -299,121 +293,116 @@ int in_blacklist (int sysno);
 void
 badboy_loop ()
 {
-  int i;
+	int i;
 
-  for (i = 0; i < ntries; ++i)
-    {
-	  /* level 5 */
+	for (i = 0; i < ntries; ++i) {
+		/* level 5 */
 
-  if (!x_opt && verbose_level >= 5) {
-		printf ("try %d\n", i);
-  }
+		if (!x_opt && verbose_level >= 5) {
+			printf("try %d\n", i);
+		}
 
-      if (setjmp (again_buff) == 3)
-	{
-  	  if (verbose_level >= 5)
-	  	printf ("Barfed\n");
+		if (setjmp (again_buff) == 3) {
+			if (verbose_level >= 5)
+					printf("Barfed\n");
+		} else {
+			set_up_signals ();
+			alarm(MAX_TRY_TIME);
+			try_one_crash (i);
+		}
 	}
-      else
-	{
-	  set_up_signals ();
-	  alarm (MAX_TRY_TIME);
-	  try_one_crash (i);
-	}
-  }
-  summarize_errno();
+	summarize_errno();
 }
 
 void again_handler (int sig)
 {
-  char *ss;
+	char *ss;
 
-  switch (sig)
-    {
-    case SIGILL:
-      ss = " illegal instruction";
-      break;
+	switch (sig) {
+	case SIGILL:
+		ss = " illegal instruction";
+		break;
 #ifdef SIGTRAP
-    case SIGTRAP:
-      ss = " trace trap";
-      break;
+	case SIGTRAP:
+		ss = " trace trap";
+		break;
 #endif
-    case SIGFPE:
-      ss = " arithmetic exception";
-      break;
+	case SIGFPE:
+		ss = " arithmetic exception";
+		break;
 #ifdef SIGBUS
-    case SIGBUS:
-      ss = " bus error";
-      break;
+	case SIGBUS:
+		ss = " bus error";
+		break;
 #endif
-    case SIGSEGV:
-      ss = " segmentation violation";
-      break;
+	case SIGSEGV:
+		ss = " segmentation violation";
+		break;
 #ifdef SIGIOT
-    case SIGIOT:
-      ss = " IOT instruction";
-      break;
+	case SIGIOT:
+		ss = " IOT instruction";
+		break;
 #endif
 #ifdef SIGEMT
-    case SIGEMT:
-      ss = " EMT instruction";
-      break;
+	case SIGEMT:
+		ss = " EMT instruction";
+		break;
 #endif
 #ifdef SIGALRM
-    case SIGALRM:
-      ss = " alarm clock";
-      break;
+	case SIGALRM:
+		ss = " alarm clock";
+		break;
 #endif
-    case SIGINT:
-      ss = " interrupt";
-      break;
-    default:
-      ss = "";
-    }
-  if (verbose_level >= 5)
-  	printf ("Got signal %d%s\n", sig, ss);
+	case SIGINT:
+		ss = " interrupt";
+		break;
+	default:
+		ss = "";
+	}
+	if (verbose_level >= 5)
+		printf ("Got signal %d%s\n", sig, ss);
 
-  longjmp (again_buff, 3);
+	longjmp (again_buff, 3);
 }
 
 void my_signal (int sig, void (*func) ())
 {
-  struct sigaction act;
+	struct sigaction act;
 
-  act.sa_handler = func;
-  memset (&act.sa_mask, 0x00, sizeof (sigset_t));
-  act.sa_flags = SA_NOMASK|SA_RESTART;
-  sigaction (sig, &act, 0);
+	act.sa_handler = func;
+	memset (&act.sa_mask, 0x00, sizeof (sigset_t));
+	act.sa_flags = SA_NOMASK|SA_RESTART;
+	sigaction (sig, &act, 0);
 }
 
 void
 set_up_signals ()
 {
-  my_signal (SIGILL, again_handler);
+	my_signal (SIGILL, again_handler);
 #ifdef SIGTRAP
-  my_signal (SIGTRAP, again_handler);
+	my_signal (SIGTRAP, again_handler);
 #endif
-  my_signal (SIGFPE, again_handler);
+	my_signal (SIGFPE, again_handler);
 #ifdef SIGBUS
-  my_signal (SIGBUS, again_handler);
+	my_signal (SIGBUS, again_handler);
 #endif
-  my_signal (SIGSEGV, again_handler);
+	my_signal (SIGSEGV, again_handler);
 #ifdef SIGIOT
-  my_signal (SIGIOT, again_handler);
+	my_signal (SIGIOT, again_handler);
 #endif
 #ifdef SIGEMT
-  my_signal (SIGEMT, again_handler);
+	my_signal (SIGEMT, again_handler);
 #endif
 #ifdef SIGALRM
-  my_signal (SIGALRM, again_handler);
+	my_signal (SIGALRM, again_handler);
 #endif
-  my_signal (SIGINT, again_handler);
+	my_signal (SIGINT, again_handler);
 }
 
 /*
  * NB: rand() (ie. RAND_MAX) might be on 31bits only!
  *
- * FIXME: 64bits systems
+ * FIXME: 64-bit systems
  *
  * TODO: improve arg mixing (16bits and 8bits values, NULLs, etc.).
  *	big values as returned by rand() are no so interresting
@@ -424,89 +413,88 @@ set_up_signals ()
  */
 long int rand_long()
 {
-    int r1, r2;
+		int r1, r2;
 
-    r1 = rand();
-    r2 = rand();
+		r1 = rand();
+		r2 = rand();
 
-    if (r1 & 0x10000L)
-    	r1 = 0;
-    if (!r1 && (r2 & 0x50000L))
-    	r2 = 0;
-    else if (!r1 && (r2 & 0x20000L))
-    	r2 &= 0x00ffL;
+		if (r1 & 0x10000L)
+			r1 = 0;
+		if (!r1 && (r2 & 0x50000L))
+			r2 = 0;
+		else if (!r1 && (r2 & 0x20000L))
+			r2 &= 0x00ffL;
 
-    return (long int) ((r1 & 0xffffL) << 16) | (r2 & 0xffffL);
+		return (long int) ((r1 & 0xffffL) << 16) | (r2 & 0xffffL);
 }
 
 void
 try_one_crash (int try_num)
 {
-  long int sysno, arg1, arg2, arg3, arg4, arg5, arg6, arg7;
+	long int sysno, arg1, arg2, arg3, arg4, arg5, arg6, arg7;
 
-  do {
-    sysno = rand()%sysno_max;
-  } while (in_blacklist(sysno));
+	do {
+		sysno = rand()%sysno_max;
+	} while (in_blacklist(sysno));
 
-  arg1 = rand_long();
-  arg2 = rand_long();
-  arg3 = rand_long();
-  arg4 = rand_long();
-  arg5 = rand_long();
-  arg6 = rand_long();
-  arg7 = rand_long();
+	arg1 = rand_long();
+	arg2 = rand_long();
+	arg3 = rand_long();
+	arg4 = rand_long();
+	arg5 = rand_long();
+	arg6 = rand_long();
+	arg7 = rand_long();
 
-  if (x_opt) {
-      if (verbose_level >= 1)
-  	printf("%04d: syscall(%ld, %#lx, %#lx, %#lx, %#lx, %#lx, %#lx, %#lx)\n",
-		try_num, sysno, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
-  } else {
-    syscall (sysno, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
-    record_errno(errno);
-  }
+	if (x_opt) {
+		if (verbose_level >= 1)
+			printf("%04d: syscall(%ld, %#lx, %#lx, %#lx, %#lx, "
+					"%#lx, %#lx, %#lx)\n",
+					try_num, sysno, arg1, arg2, arg3, arg4, arg5,
+					arg6, arg7);
+	} else {
+		syscall (sysno, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+		record_errno(errno);
+	}
 }
 
 /* The following syscalls create new processes which may cause the test
-   unable to finish. */
+	 unable to finish. */
 int
 in_blacklist (int sysno)
 {
-  int i;
-  const list[] =
-    {
+	int i;
+	const int list[] = {
 #if defined(__ia64__)
-      SYS_clone2,
+		SYS_clone2,
 #else
-      /*
-       * No SYS_fork(vfork) on IA-64. Instead, it uses,
-       * clone(child_stack=0, flags=CLONE_VM|CLONE_VFORK|SIGCHLD)
-       * clone2()
-       */
+		/*
+		 * No SYS_fork(vfork) on IA-64. Instead, it uses,
+		 * clone(child_stack=0, flags=CLONE_VM|CLONE_VFORK|SIGCHLD)
+		 * clone2()
+		 */
 
-      /*
-       * NOTE (garrcoop):
-       *  Could not find reference to SYS_fork(vfork) on mips32
-       *  with the Montavista / Octeon toolchain. Need to develop an
-       *  autoconf check for this item.
-       */
+		/*
+		 * NOTE (garrcoop):
+		 * Could not find reference to SYS_fork(vfork) on mips32
+		 * with the Montavista / Octeon toolchain. Need to develop an
+		 * autoconf check for this item.
+		 */
 #if defined(__NR_vfork) && __NR_vfork
-      SYS_vfork,
-#endif
-#if defined(__NR_fork) && __NR_fork
-      SYS_fork,
+		SYS_vfork,
+#elif defined(__NR_fork) && __NR_fork
+		SYS_fork,
 #endif
 #endif /* __ia64__ */
 #if defined(__NR_clone) && __NR_clone
-      SYS_clone,
+		SYS_clone,
 #endif
-      -1
-    };
+		-1
+	};
 
-  for (i = 0; list[i] != -1; i++)
-    {
-      if (sysno == list[i])
-        return 1;
-    }
+	for (i = 0; list[i] != -1; i++) {
+		if (sysno == list[i])
+			return 1;
+	}
 
-        return 0;
+	return 0;
 }
