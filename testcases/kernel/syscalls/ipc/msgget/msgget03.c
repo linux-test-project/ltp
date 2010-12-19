460095d823ba2f1732a9d0b69b8f136e4c935e9a
Garrett Cooper <yanegomi@gmail.com>
2010-12-19 10:18:06 -0800

    Fix test.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/ipc/msgget/msgget03.c
+++ testcases/kernel/syscalls/ipc/msgget/msgget03.c
@@ -145,7 +145,7 @@ void setup(void)
 
 	maxmsgs = get_max_msgqueues();
 	if (maxmsgs < 0)
-		tst_brkm(TBROK, cleanup, "");
+		tst_brkm(TBROK, cleanup, "get_max_msgqueues failed");
 
 	msg_q_arr = (int *)calloc(maxmsgs, sizeof(int));
 	if (msg_q_arr == NULL) {
