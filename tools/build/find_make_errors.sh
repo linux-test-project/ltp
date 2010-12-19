2d5280c20e0ad4d7788906869ce157993b26864e
Garrett Cooper <yanegomi@gmail.com>
2010-12-19 00:04:16 -0800

    Just capture the errors in make-errors.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- tools/build/find_make_errors.sh
+++ tools/build/find_make_errors.sh
@@ -3,9 +3,8 @@
 MAKE=${MAKE:=gmake}
 
 $MAKE -k clean
-$MAKE -k all >make-errors 2>&1
+$MAKE -k all 2>&1 | grep ' error: ' > make-errors
 
-grep ' error: ' make-errors | cut -f 1 -d : | sort -u | \
-    while read f; do
+cut -f 1 -d : < make-errors | sort -u | while read f; do
         find . -name $f -type f
     done > make-errors-files-only
