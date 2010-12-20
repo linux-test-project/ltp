58dc8e688ed0176fb82047c4e23ae06caa99f276
Garrett Cooper <yanegomi@gmail.com>
2010-12-20 12:25:34 -0800

    Make execve03 testcase 100% conformant and safe.
    This is a good boilerplate example for what one should do to write a
    deterministic, clean testcase.
    
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/execve/execve03.c
+++ testcases/kernel/syscalls/execve/execve03.c
@@ -65,15 +65,17 @@
  *	test #5 will fail with ETXTBSY not EACCES if the test is run as root
  */
 
-#include <stdio.h>
-#include <errno.h>
-#include <test.h>
-#include <usctest.h>
-#include <pwd.h>
-#include <sys/mman.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <pwd.h>
+#include <stdio.h>
+
+#include "test.h"
+#include "usctest.h"
+#include "safe_macros.h"
 
 char *TCID = "execve03";
 int fileHandle = 0;
@@ -100,25 +102,19 @@ struct test_case_t {
 	int error;
 } TC[] = {
 	/* the file name is greater than VFS_MAXNAMELEN - ENAMTOOLONG */
-	{
-	long_file, ENAMETOOLONG},
-	    /* the filename does not exist - ENOENT */
-	{
-	no_dir, ENOENT},
-	    /* the path contains a directory name which doesn't exist - ENOTDIR */
-	{
-	test_name3, ENOTDIR},
+	{ long_file, ENAMETOOLONG},
+	/* the filename does not exist - ENOENT */
+	{ no_dir, ENOENT},
+	/* the path contains a directory name which doesn't exist - ENOTDIR */
+	{ test_name3, ENOTDIR},
 #if !defined(UCLINUX)
-	    /* the filename isn't part of the process address space - EFAULT */
-	{
-	(char *)-1, EFAULT},
+	/* the filename isn't part of the process address space - EFAULT */
+	{ (char *)-1, EFAULT},
 #endif
-	    /* the filename does not have execute permission - EACCES */
-	{
-	test_name5, EACCES},
-	    /* the file is zero length with execute permissions - ENOEXEC */
-	{
-	test_name6, ENOEXEC}
+	/* the filename does not have execute permission - EACCES */
+	{ test_name5, EACCES},
+	/* the file is zero length with execute permissions - ENOEXEC */
+	{ test_name6, ENOEXEC}
 };
 
 int TST_TOTAL = sizeof(TC) / sizeof(*TC);
@@ -129,25 +125,20 @@ int main(int ac, char **av)
 	char *msg;		/* message returned from parse_opts */
 	int i;
 
-	/* parse standard options */
-	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL) {
+	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-	}
 
 	setup();
 
-	/* set up the expected errnos */
 	TEST_EXP_ENOS(exp_enos);
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
 
-		/* reset Tst_count in case we are looping */
 		Tst_count = 0;
 
-		/* loop through the test cases */
 		for (i = 0; i < TST_TOTAL; i++) {
 
-			TEST(execve(TC[i].tname, NULL, NULL));
+			TEST(execve(TC[i].tname, av, NULL));
 
 			if (TEST_RETURN != -1) {
 				tst_resm(TFAIL, "call succeeded unexpectedly");
@@ -156,15 +147,14 @@ int main(int ac, char **av)
 
 			TEST_ERROR_LOG(TEST_ERRNO);
 
-			if (TEST_ERRNO == TC[i].error) {
-				tst_resm(TPASS, "expected failure - "
-					 "errno = %d : %s", TEST_ERRNO,
-					 strerror(TEST_ERRNO));
-			} else {
-				tst_resm(TFAIL, "unexpected error - %d : %s - "
-					 "expected %d", TEST_ERRNO,
-					 strerror(TEST_ERRNO), TC[i].error);
-			}
+			if (TEST_ERRNO == TC[i].error)
+				tst_resm(TPASS|TTERRNO,
+				    "execve failed as expected");
+			else
+				tst_resm(TFAIL|TTERRNO,
+				    "execve failed unexpectedly; expected "
+				    "%d - %s",
+				    TC[i].error, strerror(TC[i].error));
 		}
 	}
 	cleanup();
@@ -186,70 +176,40 @@ void setup()
 
 	TEST_PAUSE;
 
-	ltpuser = getpwnam(nobody_uid);
-	if (setgid(ltpuser->pw_uid) == -1) {
-		tst_resm(TINFO, "setgid failed to "
-			 "to set the gid to %d", ltpuser->pw_uid);
-		perror("setgid");
-	}
-	if (setuid(ltpuser->pw_uid) == -1) {
-		tst_resm(TINFO, "setuid failed to "
-			 "to set the uid to %d", ltpuser->pw_uid);
-		perror("setuid");
-	}
+	ltpuser = SAFE_GETPWNAM(NULL, nobody_uid);
 
-	/* make a temporary directory and cd to it */
+	SAFE_SETGID(NULL, ltpuser->pw_gid);
+
 	tst_tmpdir();
 
-	/*
-	 * set up a name that should generate an ENOTDIR error
-	 */
-	if ((cwdname = getcwd(cwdname, 0)) == NULL) {
-		tst_brkm(TBROK, cleanup, "could not get currect directory");
-	}
+	cwdname = SAFE_GETCWD(cleanup, cwdname, 0);
 
 	sprintf(test_name5, "%s/fake.%d", cwdname, getpid());
 
-	if ((fileHandle = creat(test_name5, 0444)) == -1) {
-		tst_brkm(TBROK, cleanup, "creat(2) FAILED to create temp file");
-	}
+	fileHandle = SAFE_CREAT(cleanup, test_name5, 0444);
 
 	sprintf(test_name3, "%s/fake.%d", test_name5, getpid());
 
 	/* creat() and close a zero length file with executeable permission */
 	sprintf(test_name6, "%s/execve03.%d", cwdname, getpid());
 
-	if ((fd = creat(test_name6, 0755)) == -1) {
-		tst_brkm(TBROK, cleanup, "creat() failed");
-	}
-	if (close(fd) == -1) {
-		tst_brkm(TBROK, cleanup, "close() failed");
-	}
+	fd = SAFE_CREAT(cleanup, test_name6, 0755);
+	fd = SAFE_CLOSE(cleanup, fd);
 #if !defined(UCLINUX)
 	bad_addr = mmap(0, 1, PROT_NONE,
 			MAP_PRIVATE_EXCEPT_UCLINUX | MAP_ANONYMOUS, 0, 0);
-	if (bad_addr == MAP_FAILED) {
+	if (bad_addr == MAP_FAILED)
 		tst_brkm(TBROK, cleanup, "mmap failed");
-	}
 	TC[3].tname = bad_addr;
 #endif
 }
 
-/*
- * cleanup() - performs all ONE TIME cleanup for this test at
- *	       completion or premature exit.
- */
 void cleanup()
 {
-	/*
-	 * print timing stats if that option was specified.
-	 * print errno log if that option was specified.
-	 */
-	close(fileHandle);
+	SAFE_CLOSE(NULL, fileHandle);
 
 	TEST_CLEANUP;
 
-	/* delete the test directory created in setup() */
 	tst_rmdir();
 
 }
