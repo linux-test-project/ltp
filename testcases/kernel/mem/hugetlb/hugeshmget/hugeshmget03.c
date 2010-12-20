45c9708f74e2e267fa2ef6e46d06ac40ef8b6d77
Garrett Cooper <yanegomi@gmail.com>
2010-12-20 07:13:39 -0800

    -Wuninitialized warnings.
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/mem/hugetlb/hugeshmget/hugeshmget03.c
+++ testcases/kernel/mem/hugetlb/hugeshmget/hugeshmget03.c
@@ -76,6 +76,8 @@ int main(int ac, char **av)
 	char *msg;			/* message returned from parse_opts */
         unsigned long huge_pages_shm_to_be_allocated;
 
+	huge_pages_shm_to_be_allocated = 0;
+
 	/* parse standard options */
 	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
 		tst_brkm(TBROK, cleanup, "OPTION PARSING ERROR - %s", msg);
