0bf0ed7130a677b0a5593c832ee4b2399bd8a86f
Garrett Cooper <yanegomi@gmail.com>
2010-12-20 03:37:53 -0800

    Add a macro for __attribute__((warn_unused_result)).
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- include/compiler.h
+++ include/compiler.h
@@ -7,11 +7,12 @@
 #ifndef __LTP_COMPILER_H__
 #define __LTP_COMPILER_H__
 
-#define LTP_ATTRIBUTE_NORETURN __attribute__((noreturn))
-#define LTP_ATTRIBUTE_UNUSED __attribute__((unused))
+#define LTP_ATTRIBUTE_NORETURN		__attribute__((noreturn))
+#define LTP_ATTRIBUTE_UNUSED		__attribute__((unused))
+#define LTP_ATTRIBUTE_UNUSED_RESULT	__attribute__((warn_unused_result))
 
 #ifndef ARRAY_SIZE
-	#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof(arr[0]))
 #endif
 
 #endif /* __LTP_COMPILER_H__ */
