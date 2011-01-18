0db55077ff89a9d765965b436abe0ebc873933d6
Garrett Cooper <yanegomi@gmail.com>
2011-01-18 01:33:07 -0800

    Test OOM functionality.
    v3: use common functions.
    v2: code cleanup and skip 32-bit since there is no guarantee of running
    out of
       virtual memory address space there.
    
    Signed-off-by: CAI Qian <caiqian@redhat.com>

---
+++ testcases/kernel/mem/oom/oom01.c
@@ -0,0 +1,126 @@
+/*
+ * Out Of Memory (OOM)
+ *
+ * The program is designed to cope with unpredictable like amount and
+ * system physical memory, swap size and other VMM technology like KSM,
+ * memcg, memory hotplug and so on which may affect the OOM
+ * behaviours. It simply increase the memory consumption 3G each time
+ * until all the available memory is consumed and OOM is triggered.
+ *
+ * Copyright (C) 2010  Red Hat, Inc.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it would be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Further, this software is distributed without any warranty that it
+ * is free of the rightful claim of any third person regarding
+ * infringement or the like.  Any license provided herein, whether
+ * implied or otherwise, applies only to this software file.  Patent
+ * licenses, if any, provided herein do not apply to combinations of
+ * this program with other software, or any other product whatsoever.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <asm/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "test.h"
+#include "usctest.h"
+#include "lib/oom.h"
+
+char *TCID = "oom01";
+int TST_TOTAL = 1;
+extern int Tst_count;
+
+static void setup(void);
+
+int main(int argc, char *argv[])
+{
+	char *msg;
+	int lc, fd;
+
+	msg = parse_opts(argc, argv, NULL, NULL);
+	if (msg != NULL)
+		tst_brkm(TBROK, tst_exit, "OPTION PARSING ERROR - %s", msg);
+
+#ifdef __i386__
+	tst_brkm(TCONF, tst_exit,
+		"this test is not designed for 32-bit system.");
+#endif /* __i386__ */
+
+	setup();
+
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+		Tst_count = 0;
+		tst_resm(TINFO, "start testing overcommit_memory=2.");
+		fd = open(SYSFS_OVER, O_WRONLY);
+		if (fd == -1)
+			tst_brkm(TBROK|TERRNO, cleanup, "open");
+		if (write(fd, "2", 1) != 1)
+			tst_brkm(TBROK|TERRNO, cleanup, "write");
+		oom(OVERCOMMIT, 0, 0);
+
+		tst_resm(TINFO, "start testing overcommit_memory=0.");
+		if (lseek(fd, SEEK_SET, 0) == -1)
+			tst_brkm(TBROK|TERRNO, cleanup, "lseek");
+		if (write(fd, "0", 1) != 1)
+			tst_brkm(TBROK|TERRNO, cleanup, "write");
+		oom(OVERCOMMIT, 0, 0);
+
+		tst_resm(TINFO, "start normal OOM testing.");
+		if (lseek(fd, SEEK_SET, 0) == -1)
+			tst_brkm(TBROK|TERRNO, cleanup, "lseek");
+		if (write(fd, "1", 1) != 1)
+			tst_brkm(TBROK|TERRNO, cleanup, "write");
+		oom(NORMAL, 0, 0);
+
+		tst_resm(TINFO, "start OOM testing for mlocked pages.");
+		oom(MLOCK, 0, 0);
+
+		tst_resm(TINFO, "start OOM testing for KSM pages.");
+		oom(KSM, 0, 0);
+	}
+	cleanup();
+}
+
+void setup(void)
+{
+	int fd;
+
+	tst_sig(FORK, DEF_HANDLER, cleanup);
+	TEST_PAUSE;
+
+	fd = open(SYSFS_OVER, O_RDONLY);
+	if (fd == -1)
+		tst_brkm(TBROK|TERRNO, cleanup, "open");
+	if (read(fd, &overcommit, 1) != 1)
+		tst_brkm(TBROK|TERRNO, cleanup, "read");
+	close(fd);
+}
+
+void cleanup(void)
+{
+	int fd;
+
+	fd = open(SYSFS_OVER, O_WRONLY);
+	if (fd == -1)
+		tst_brkm(TBROK|TERRNO, cleanup, "open");
+	if (write(fd, &overcommit, 1) != 1)
+		tst_brkm(TBROK|TERRNO, cleanup, "write");
+	close(fd);
+
+	TEST_CLEANUP;
+	tst_exit();
+}
