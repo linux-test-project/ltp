25d422d2169fed26c5c9512a8fc9c3f69ae061fe
Garrett Cooper <yanegomi@gmail.com>
2011-01-20 23:28:54 -0800

    Cleanup testcase and add more sub testcases.
    Credit goes to Cristian Greco (cristian <AT> regolo <DOT> cc) for
    spotting the original permissions.
    
    Signed-off-by: Garrett Cooper <yanegomi@gmail.com>

--- testcases/kernel/syscalls/acct/acct01.c
+++ testcases/kernel/syscalls/acct/acct01.c
@@ -36,139 +36,110 @@
  *	by root.   Use the TERM flag, to clean up files.
  */
 
-#include <stdio.h>		/* needed by testhead.h         */
-#include <errno.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pwd.h>
+#include <stdio.h>		/* needed by testhead.h         */
+#include <stdlib.h>
+#include <unistd.h>
 
-/** LTP Port **/
 #include "test.h"
 #include "usctest.h"
+#include "safe_macros.h"
 
-#define FAILED 0
-#define PASSED 1
+char *TCID = "acct01";
+int TST_TOTAL = 6;
 
-char *TCID = "acct01";		/* Test program identifier.    */
-int TST_TOTAL = 4;		/* Total number of test cases. */
-/**************/
+char tmpbuf[80];
 
-char fname[80];
-
-/*--------------------------------------------------------------*/
-int main(argc, argv)
-int argc;
-char *argv[];
+static void setup(void)
 {
-	register int i;
-	char tmpbuf[100], tmpbuf2[100];
 
-/*--------------------------------------------------------------*/
-
-	/* turn off acct, so we are in a known state
+	/* 
+	 * XXX: FreeBSD says you must always be superuser, but Linux says you
+	 * need to have CAP_SYS_PACCT capability.
+	 *
+	 * Either way, it's better to do this to test out the EPERM
+	 * requirement.
 	 */
-	if (acct(NULL) == -1) {
-		if (errno == ENOSYS) {
-			tst_resm(TCONF,
-				 "BSD process accounting is not configured in this kernel.");
-			tst_resm(TCONF, "Test will not run.");
-			tst_exit();
-		} else {
-			tst_resm(TBROK|TERRNO, "Attempting to disable acct failed");
-			tst_exit();
-		}
-	}
+	tst_require_root(NULL);
 
 	tst_tmpdir();
 
-	/* now try to use a device, and it should fail */
-	if (acct("/dev/null") != -1) {
-		tst_resm(TFAIL,
-			 "attempting to assign acct file to device: expected failure but got okay return");
-		tst_exit();
-	} else
-		tst_resm(TPASS, "Received expected error: -1");
-
-	/* check the errno, for EACCESS */
-	if (errno != EACCES) {
-		tst_resm(TFAIL|TERRNO,
-			 "Attempt to use non-ordinary file didn't receive EACCESS error");
-		tst_exit();
-	} else
-		tst_resm(TPASS, "Received expected error: EACCESS");
-
-	/* check for error on non-existent file name */
-	sprintf(tmpbuf, "./%s.%d", TCID, getpid());
-
-	unlink(tmpbuf);
-
-	if (acct(tmpbuf) != -1) {
-		tst_resm(TBROK,
-			 "attempt to set acct to non-existent file didn't fail as it should");
-		tst_exit();
-	}
-
-	/* check the errno */
-	if (errno != ENOENT) {
-		tst_resm(TFAIL|TERRNO,
-			 "Attempt to set acct to non-existent file failed as we wanted ENOENT");
-	} else
-		tst_resm(TPASS, "Received expected error: ENOENT");
-
-	/* now do a valid acct enable call
-	 */
-	sprintf(tmpbuf, "./%s.%d", TCID, getpid());
-
-	if ((i = creat(tmpbuf, 0777)) == -1) {
-		tst_resm(TBROK|TERRNO, "creat(%s, 0777) failed", tmpbuf);
-		tst_exit();
-	}
-
-	close(i);
-
-	if (acct(tmpbuf) == -1) {
-		tst_resm(TBROK|TERRNO, "acct(%s) failed", tmpbuf);
-		tst_exit();
-	}
-
-	/* now check for busy error return
-	 */
-
-	sprintf(tmpbuf2, "./%s.%d.2", TCID, getpid());
-
-	if (acct(tmpbuf2) != -1) {
-		tst_resm(TBROK,
-			 "Second try on enabling acct did not fail as it should");
-		tst_exit();
-	}
-
-	/*
-	 * acct() confirms to SVr4, but not POSIX in LINUX as of 03 Dec 2002
-	 * In the above case, file doesn't exist and should get ENOENT.
-	 */
-	if (errno != ENOENT) {
-		tst_resm(TFAIL,
-			 "Second try on enabling acct failed but with errno= %d expected= %d",
-			 errno, ENOENT);
-		tst_exit();
-	} else
-		tst_resm(TPASS, "Received expected error: ENOENT");
-
-	/* now disable accting */
+	/* turn off acct, so we are in a known state */
 	if (acct(NULL) == -1) {
-		tst_resm(TBROK|TERRNO,
-			 "Attempt to do final disable of acct failed");
-		tst_exit();
+		if (errno == ENOSYS)
+			tst_brkm(TCONF, cleanup,
+			    "BSD process accounting is not configured in this "
+			    "kernel");
+		else
+			tst_brkm(TBROK|TERRNO, cleanup, "acct(NULL) failed");
 	}
+}
 
-	unlink(tmpbuf);
+static void cleanup(void)
+{
 
-/*--------------------------------------------------------------*/
-/* Clean up any files created by test before call to tst_exit.	*/
+	if (acct(NULL) == -1)
+		tst_resm(TBROK|TERRNO, "acct(NULL) failed");
+
 	tst_rmdir();
-	tst_exit();		/* THIS CALL DOES NOT RETURN - EXITS!!  */
-/*--------------------------------------------------------------*/
+}
+
+int main(int argc, char *argv[])
+{
+	struct passwd *pwent;
+
+	setup();
+
+	if (acct("/") == -1 && errno == EISDIR) 
+		tst_resm(TPASS, "Failed with EISDIR as expected");
+	else
+		tst_brkm(TFAIL|TERRNO, cleanup,
+		    "didn't fail as expected; expected EISDIR");
+
+	if (acct("/dev/null") == -1 && errno == EACCES)
+		tst_resm(TPASS, "Failed with EACCES as expected");
+	else
+		tst_brkm(TFAIL|TERRNO, cleanup,
+		    "didn't fail as expected; expected EACCES");
+
+	if (acct("does/not/exist") == -1 && errno == ENOTDIR)
+		tst_resm(TPASS, "Failed with ENOTDIR as expected");
+	else
+		tst_brkm(TFAIL|TERRNO, cleanup,
+		    "didn't fail as expected; expected ENOTDIR");
+
+	sprintf(tmpbuf, "./%s.%d", TCID, getpid());
+
+	fd = SAFE_CREAT(cleanup, tmpbuf, 0777);
+	SAFE_CLOSE(cleanup, fd);
+
+	if (acct(tmpbuf) == -1)
+		tst_brkm(TBROK|TERRNO, cleanup, "acct failed unexpectedly");
+
+	pwent = SAFE_GETPWNAM("nobody");
+
+	SAFE_SETEUID(pwent->pw_uid);
+
+	if (acct(tmpbuf) == -1 && errno == EPERM)
+		tst_resm(TPASS, "acct failed as expected with EPERM");
+	else
+		tst_brkm(TBROK|TERRNO, cleanup,
+		    "acct didn't fail as expected with EPERM");
+
+	SAFE_SETEGID(0);
+
+	SAFE_UNLINK(cleanup, tmpbuf);
+
+	if (acct(tmpbuf) == -1 && errno == ENOENT)
+		tst_resm(TPASS, "acct failed as expected with ENOENT");
+	else
+		tst_brkm(TBROK|TERRNO, cleanup,
+		    "acct didn't fail as expected with ENOENT");
+
+	cleanup();
 	tst_exit();
 }
