45a0ba15ae38f11410132d8cf6d7a2d75405d432
Peter W Morreale <pmorreale@novell.com>
2011-05-31 15:09:26 -0600

    New test for pthread_cond_init
    Create a new test to ensure that pthread_cond_init returns EBUSY for
    re-initializing a previously used (but not destroyed) condition variable.
    
    Signed-off-by: Peter W. Morreale <pmorreale@novell.com>

---
+++ testcases/open_posix_testsuite/conformance/interfaces/pthread_cond_init/4-3.c
@@ -0,0 +1,59 @@
+/*
+ *
+ *   Copyright (c) Novell Inc. 2011
+ *
+ *   This program is free software;  you can redistribute it and/or modify
+ *   it under the terms in version 2 of the GNU General Public License as
+ *   published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *   the GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program;  if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *   Author:  Peter W. Morreale <pmorreale AT novell DOT com>
+ *
+ *   Date:  31/05/2011
+ *
+ *   Test assertion 4 - EBUSY is returned for re-initializing a
+ *   condition variable.  Note the 0 (zero) may be returned
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+#include <posixtest.h>
+
+#define ERR_MSG(f, rc)	printf("Failed: function: %s status: %s(%u)\n", \
+						f, strerror(rc), rc)
+
+
+int main(void)
+{
+	int status;
+	pthread_cond_t cond;
+	int rc;
+
+	status = PTS_UNRESOLVED;
+
+	rc = pthread_cond_init(&cond, NULL);
+	if (rc) {
+		ERR_MSG("pthread_cond_init()", rc);
+		return status;
+	}
+
+	rc = pthread_cond_init(&cond, NULL);
+	if (rc && rc != EBUSY) {
+		ERR_MSG("pthread_cond_init() 2", rc);
+		return PTS_FAIL;
+	}
+
+	printf("Test PASS\n");
+	return PTS_PASS;
+}
