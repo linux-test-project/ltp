c842487a3ae9fb97fed709976445e40d1bcddfc5
Peter W Morreale <pmorreale@novell.com>
2011-06-21 08:21:56 -0600

    Apply coding style.
    Apply coding style to sigqueue test 9-1.
    
    Signed-off-by: Peter W. Morreale <pmorreale@novell.com>

--- testcases/open_posix_testsuite/conformance/interfaces/sigqueue/9-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/sigqueue/9-1.c
@@ -4,25 +4,26 @@
  * This file is licensed under the GPL license.  For the full content
  * of this license, see the COPYING file at the top level of this
  * source tree.
-
- Steps:
- - Register for myhandler to be called when SIGTOTEST is called, and make
-   sure SA_SIGINFO is set.
- - Block signal SIGTOTEST from the process.
- - Using sysconf(), check to see if there is a limit on number of queued
-   signals that are pending. If there isn't a limit (i.e. sysconf returned
-   -1), then this test is not applicable to the system's implementation,
-   and thus we should pass it.
- - Using sigqueue(), send to the current process a number of instances (of SIGTOTEST)
-   equal to the limit that sysconf() returned.
- - Send one more instance of SIGTOTEST and verify that sigqueue returns -1 and sets errno to
-   [EAGAIN]
+ *
+ * Updated: 21.06.2011  Peter W. Morreale <pmorreale@novell.com>
+ *
+ * Steps:
+ * - Register for myhandler to be called when SIGTOTEST is called, and make
+ *   sure SA_SIGINFO is set.
+ * - Block signal SIGTOTEST from the process.
+ * - Using sysconf(), check to see if there is a limit on number of queued
+ *   signals that are pending. If there isn't a limit (i.e. sysconf returned
+ *   -1), then this test is not applicable to the system's implementation,
+ *   and thus we should pass it.
+ * - Using sigqueue(), send to the current process a number of instances
+ *   (of SIGTOTEST) equal to the limit that sysconf() returned.
+ * - Send one more instance of SIGTOTEST and verify that sigqueue returns
+ *   -1 and sets errno to [EAGAIN]
+ *
  */
 
 #define _XOPEN_SOURCE 600
-#define _XOPEN_REALTIME 1
 #define SIGTOTEST SIGRTMIN
-#define NUMCALLS 5
 
 #include <signal.h>
 #include <stdio.h>
@@ -31,15 +32,17 @@
 #include <errno.h>
 #include "posixtest.h"
 
-void myhandler(int signo, siginfo_t *info, void *context) {
-	printf ("Inside Handler\n");
+void myhandler(int signo, siginfo_t *info, void *context)
+{
+	printf("Inside Handler\n");
 }
 
-int main()
+int main(void)
 {
-
-	int pid, i;
+	int pid = getpid();
+	int i;
 	long syslimit;
+	int rc;
 	union sigval value;
 	struct sigaction act;
 
@@ -53,32 +56,32 @@ int main()
 
 	sighold(SIGTOTEST);
 
+	/*
+	 * Get system limit.  Note that this limit is optional.
+	 */
 	syslimit = sysconf(_SC_SIGQUEUE_MAX);
+	if (syslimit < 0)
+		goto done;
 
-	printf("sigqueuemax %ld\n", syslimit);
-
-	if (syslimit == -1) {
-		printf("Test PASSED: Actually, test is not applicable to this implementation. This system has no defined limit."
-			" Note: it is optional whether an implementation has this limit, so this is"
-			" not a bug.\n");
-		return PTS_PASS;
-	}
-
-	for (i=0; i<syslimit; i++) {
+	for (i = 0; i < syslimit; i++) {
 		if (sigqueue(pid, SIGTOTEST, value) != 0) {
-			printf("Test UNRESOLVED: call to sigqueue did not return success\n");
+			printf("Failed: sigqueue on %d of %d max, errno: %s\n",
+				i, syslimit, strerror(errno));
 			return PTS_UNRESOLVED;
 		}
 	}
 
-	if (sigqueue(pid, SIGTOTEST, value) != -1) {
-		printf("Test FAILED: sigqueue did not return -1 even though the process has already queued {SIGQUEUE_MAX} signals that are still pending.\n");
+	/*
+	 * Enqueue one more, needs to fail with EAGAIN
+	 */
+	rc = sigqueue(pid, SIGTOTEST, value);
+	if (!(rc == -1 && errno == EAGAIN)) {
+		printf("Failed: sigqueue() queued SIGQUEUE_MAX+1 signals\n");
 		return PTS_FAIL;
 	}
 
-	if (errno != EAGAIN) {
-		printf("Test FAILED: errno was not set to [EAGAIN] even though the process has already queued {SIGQUEUE_MAX} signals that are still pending.\n");
-	}
+done:
+	printf("Test PASS\n");
 
 	return PTS_PASS;
 }
