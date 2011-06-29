5e6c18c52372e1d2ea35236120a3fc6bbaa6ba5a
Peter W Morreale <pmorreale@novell.com>
2011-06-29 10:13:22 -0600

    coding style for 3-1
    fix coding style for 3-1
    
    Signed-off-by: Peter W. Morreale <pmorreale@novell.com>

--- testcases/open_posix_testsuite/conformance/interfaces/pthread_cond_init/3-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/pthread_cond_init/3-1.c
@@ -12,45 +12,46 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include "posixtest.h"
 
-int main()
+#define ERR_MSG(f, rc)  printf("Failed: func: %s rc: %s (%u)\n", \
+			f, strerror(rc), rc)
+
+int main(void)
 {
 	pthread_condattr_t condattr;
-	pthread_cond_t  cond;
+	pthread_cond_t cond;
 	int rc;
+	int status = PTS_UNRESOLVED;
+	char *label;
 
-	/* Initialize a cond attributes object */
-	if ((rc=pthread_condattr_init(&condattr)) != 0) {
-		fprintf(stderr,"Error at pthread_condattr_init(), rc=%d\n",rc);
-		return PTS_UNRESOLVED;
-	}
+	label = "pthread_condattr_init()";
+	rc = pthread_condattr_init(&condattr);
+	if (rc)
+		goto done;
 
-	/* Initialize a cond object with the default cond attributes */
-	if ((rc=pthread_cond_init(&cond,&condattr)) == 0) {
-		printf("Test PASSED\n");
-		return PTS_PASS;
-	}
-
-	/* Check if returned values are tolerable */
-	else if (rc == ENOMEM) {
-		fprintf(stderr,"Insufficient memory to initialize the cond\n");
-		return PTS_UNRESOLVED;
-	}
-	else if (rc == EAGAIN) {
-		fprintf(stderr,"Lack of the necessary resources to initilize the cond\n");
-		return PTS_UNRESOLVED;
-	}
-	else if (rc == EBUSY) {
-		fprintf(stderr,"Detected an attemp to reinitilize a previously initilized cond\n");
-		return PTS_UNRESOLVED;
+	label = "pthread_cond_init()";
+	rc = pthread_cond_init(&cond, &condattr);
+	switch (rc) {
+	case 0:
+		break;
+	case ENOMEM:
+	case EINVAL:
+	case EBUSY:
+	case EAGAIN:
+		status = PTS_UNRESOLVED;
+		goto done;
+	default:
+		status = PTS_FAIL;
+		goto done;
 	}
 
-	/* Any other returned value means the test failed */
-	else
-	{
-		printf("Test FAILED\n");
-		return PTS_FAIL;
-	}
+	printf("Test PASS\n");
+	return PTS_PASS;
+
+done:
+	ERR_MSG(label, rc);
+	return status;
 }
