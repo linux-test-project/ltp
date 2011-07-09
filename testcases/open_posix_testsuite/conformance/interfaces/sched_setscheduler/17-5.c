754b695d57b186e6846adf9664a00a57085821bb
Peter W Morreale <pmorreale@novell.com>
2011-07-09 08:24:48 -0600

    Cleanup of sched_setscheduler 17-5 test
    Various cleanups and coding style...
    
    Signed-off-by: Peter W. Morreale <pmorreale@novell.com>

--- testcases/open_posix_testsuite/conformance/interfaces/sched_setscheduler/17-5.c
+++ testcases/open_posix_testsuite/conformance/interfaces/sched_setscheduler/17-5.c
@@ -16,62 +16,78 @@
  *   1. Get the old policy and priority.
  *   2. Call sched_setscheduler with invalid args.
  *   3. Check that the policy and priority have not changed.
+ *
+ * Orignal author unknown
+ * Updated:  Peter W. Morreale <pmorreale AT novell DOT com>
+ * Date:     09.07.2011
  */
+
 #include <sched.h>
 #include <stdio.h>
 #include <limits.h>
 #include <errno.h>
+#include <string.h>
 #include <unistd.h>
 #include "posixtest.h"
 
-int main() {
+#define ERR_MSG(f, rc)  printf("Failed: %s rc: %d errno: %s\n", \
+				f, rc, strerror(errno))
+
+int main(void)
+{
 	int old_priority, old_policy, new_policy;
 	struct sched_param param;
 	int invalid_policy;
+	char *label;
+	pid_t pid = getpid();
+	int rc;
 
 	invalid_policy = INT_MAX;
 
-	if (sched_getparam(getpid(), &param) == -1) {
-		perror("An error occurs when calling sched_getparam()");
-		return PTS_UNRESOLVED;
-	}
+	label = "sched_getparam()";
+	rc = sched_getparam(pid, &param);
+	if (rc)
+		goto unresolved;
 	old_priority = param.sched_priority;
 
-	old_policy = sched_getscheduler(getpid());
-	if (old_policy == -1) {
-		perror("An error occurs when calling sched_getscheduler()");
-		return PTS_UNRESOLVED;
+	label = "sched_getscheduler()";
+	rc = sched_getscheduler(pid);
+	if (rc < 0)
+		goto unresolved;
+	old_policy = rc;
+
+	label = "sched_setscheduler() - invalid policy succeeded?";
+	rc = sched_setscheduler(0, invalid_policy, &param);
+	if (!rc)
+		goto unresolved;
+
+	label = "sched_getparam()";
+	rc = sched_getparam(pid, &param);
+	if (rc)
+		goto unresolved;
+
+	label = "sched_getscheduler()";
+	rc = sched_getscheduler(pid);
+	if (rc < 0)
+		goto unresolved;
+	new_policy = rc;
+
+	if (old_policy != new_policy) {
+		printf("Failed: invalid policy change, old: %u, new %u\n",
+				old_policy, new_policy);
+		exit(PTS_FAIL);
 	}
 
-	sched_setscheduler(0, invalid_policy, &param);
-
-	if (errno == 0) {
-		printf("No error occurs, could %i be a valid value for the scheduling policy ???\n", invalid_policy);
-		return PTS_UNRESOLVED;
+	if (old_priority != param.sched_priority) {
+		printf("Failed: invalid priority change, old: %u, new %u\n",
+				old_priority, param.sched_priority);
+		exit(PTS_FAIL);
 	}
 
-	if (sched_getparam(getpid(), &param) != 0) {
-		perror("An error occurs when calling sched_getparam()");
-		return PTS_UNRESOLVED;
-	}
+	printf("Test PASSED\n");
+	return PTS_PASS;
 
-	new_policy = sched_getscheduler(getpid());
-	if (new_policy == -1) {
-		perror("An error occurs when calling sched_getscheduler()");
-		return PTS_UNRESOLVED;
-	}
-
-	if (old_policy == new_policy &&
-	   old_priority == param.sched_priority) {
-		printf("Test PASSED\n");
-		return PTS_PASS;
-	}
-
-	if (param.sched_priority != old_priority) {
-		printf("The param has changed\n");
-	}
-	if (new_policy != old_policy) {
-		printf("The policy has changed\n");
-	}
-	return PTS_FAIL;
+unresolved:
+	ERR_MSG(label, rc);
+	return PTS_UNRESOLVED;
 }
