1f73c0bdea8299a3b66b83c85bdedf9ee988505e
Yi Zhao <yi.zhao@windriver.com>
2011-07-29 16:33:05 +0800

    run-all-posix-option-group-tests.sh: Fix a spelling mistake
    The script can not work due to this mistake.
    
    Signed-off-by: Yi Zhao <yi.zhao@windriver.com>

--- testcases/open_posix_testsuite/bin/run-all-posix-option-group-tests.sh
+++ testcases/open_posix_testsuite/bin/run-all-posix-option-group-tests.sh
@@ -8,7 +8,7 @@
 #
 
 FAILED=0
-RPOG_SCRIPT="$(dirname "$0")/run-posix-option-group-test.sh"
+PROG_SCRIPT="$(dirname "$0")/run-posix-option-group-test.sh"
 
 for option_group in AIO MEM MSG SEM SIG THR TMR TPS; do
 	if ! $PROG_SCRIPT $option_group; then
