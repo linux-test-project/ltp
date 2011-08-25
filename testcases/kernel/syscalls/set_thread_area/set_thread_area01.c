bde87cb9bb181e67c0917b2e2243c6c19d0566a1
Cyril Hrubis <chrubis@suse.cz>
2011-08-25 16:12:50 +0200

    Rewrite the syscalls/set_thread_area tests
    * Fixed the coding style and incorrectly used LTP test interface
    * Merged these two tests into one
    * The thread_get_area() now uses entry_number initialized by
      thread_set_area() instead of incorrect ad-hoc constant value.
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/set_thread_area/set_thread_area01.c
+++ testcases/kernel/syscalls/set_thread_area/set_thread_area01.c
@@ -1,5 +1,7 @@
-/******************************************************************************
+/*************************************************************************
  * Copyright (c) Crackerjack Project., 2007
+ * Copyright (c) Manas Kumar Nayak <maknayak@in.ibm.com>
+ * Copyright (c) Cyril Hrubis <chrubis@suse.cz> 2011
  *
  * This program is free software;  you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,164 +17,106 @@
  * along with this program;  if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- ******************************************************************************
- * File:	set_thread_area01.c
- * Description: This test two syscalls
- *		1) get__thread_area():
- *		   get_thread_area returns an entry in the current thread's
- *		   Thread Local Storage (TLS) array.get_thread_area returns 0
- *		   on success. Otherwise, it returns -1
- *		2) set_thread_area():
- *		   set_thread_area() sets an entry in the current thread's
- *		   Thread Local.When  set_thread_area() is passed an
- *		   entry_number of -1, it uses a free TLS entry.
- *		   set_thread_area() returns 0 on success, and -1 on failure
- *
- * Usage:  <for command-line>
- * set_tid_address01 [-c n] [-e][-i n] [-I x] [-p x] [-t]
- *      where,  -c n : Run n copies concurrently.
- *		-e   : Turn on errno logging.
- *		-i n : Execute test n times.
- *		-I x : Execute test for x seconds.
- *		-P x : Pause for x seconds between iterations.
- *		-t   : Turn on syscall timing.
- *
- * Total Tests: 2
- * Test Name:   set_thread_area01
- * History:     Porting from Crackerjack to LTP is done by
- *		Manas Kumar Nayak maknayak@in.ibm.com>
- *****************************************************************************/
+ ************************************************************************/
 
 #include "set_thread_area.h"
 
-/* Global Variables */
-char *TCID = "set_thread_area01"; /* Test program identifier.		  */
-int  testno;
-int  TST_TOTAL = 2;		  /* total number of tests in this file.  */
+char *TCID = "set_thread_area_01";
+int  TST_TOTAL = 6;
 
 #if defined(HAVE_ASM_LDT_H) && defined(HAVE_STRUCT_USER_DESC)
-/* Extern Global Variables */
 
-/* Extern Global Functions */
-/************************************************************************/
-/*									*/
-/* Function:    cleanup							*/
-/*									*/
-/* Description: Performs all one time clean up for this test on		*/
-/* successful completion, premature exit or failure. Closes all		*/
-/* temporary files, removes all temporary directories exits the		*/
-/* test with appropriate return code by calling tst_exit() function.	*/
-/*									*/
-/* Input:       None.							*/
-/*									*/
-/* Output:      None.							*/
-/*									*/
-/* Return:      On failure - Exits calling tst_exit(). Non '0' return	*/
-/* 		code. On success - Exits calling tst_exit(). With '0'	*/
-/*  		return code.						*/
-/*									*/
-/************************************************************************/
-extern void cleanup() {
-
+static void cleanup(void)
+{
 	TEST_CLEANUP;
-	tst_rmdir();
-
 }
 
-/* Local  Functions */
-/************************************************************************/
-/*									*/
-/* Function:    setup							*/
-/*									*/
-/* Description: Performs all one time setup for this test.		*/
-/* This function is typically used to capture signals, create		*/
-/* temporary dirs and temporary files that may be used in the		*/
-/* course of this test.							*/
-/*									*/
-/* Input:       None.							*/
-/*									*/
-/* Output:      None.							*/
-/*									*/
-/* Return:      On failure - Exits by calling cleanup().		*/
-/*			  On success - returns 0.			*/
-/*									*/
-/************************************************************************/
-void setup() {
-	/* Capture signals if any */
-	/* Create temporary directories */
+static void setup(void)
+{
 	TEST_PAUSE;
-	tst_tmpdir();
 }
 
-int main(int ac, char **av) {
+struct test {
+	int syscall;
+	thread_area_s *u_info;
+	int exp_ret;
+	int exp_errno;
+};
 
-	thread_area_s u_info;
-	int lc;		/* loop counter */
-	char *msg;	/* message returned from parse_opts */
+/*
+ * The set_thread_area uses a free entry_number if entry number is set to -1
+ * and upon the syscall exit the entry number is set to entry which was used.
+ * So when we call get_thread_area on u_info1, the entry number is initalized
+ * corectly by the previous set_thread_area.
+ */
+static struct user_desc u_info1 = {.entry_number = -1};
+static struct user_desc u_info2 = {.entry_number = -2};
 
-	/* parse standard options */
-	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL) {
-		 tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-		 tst_exit();
+static struct test tests[] = {
+	{__NR_set_thread_area, &u_info1,   0, 0},
+	{__NR_get_thread_area, &u_info1,   0, 0},
+	{__NR_set_thread_area, &u_info2,  -1, EINVAL},
+	{__NR_get_thread_area, &u_info2,  -1, EINVAL},
+	{__NR_set_thread_area, (void*)-9, -1, EFAULT},
+	{__NR_get_thread_area, (void*)-9, -1, EFAULT},
+};
+
+static const char *get_name(int syscall)
+{
+	switch (syscall) {
+	case __NR_set_thread_area:
+		return "set_thread_area()";
+	break;
+	case __NR_get_thread_area:
+		return "get_thread_area()";
+	break;
+	default:
+		return "invalid syscall";
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	int lc, i;
+	char *msg;
+
+	if ((msg = parse_opts(argc, argv, NULL, NULL)) != NULL)
+		 tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
 	setup();
 
-	for (lc = 0; TEST_LOOPING(lc); ++lc) {
-
-		Tst_count = 0;
-
-		for (testno = 0; testno < TST_TOTAL; ++testno) {
-
-			u_info.entry_number = 6;
-
-			/*
-			 * This call to get_thread_area function should be
-			 * successful.
-			 */
-
-			/* Call get_thread_area(). */
-			TEST(syscall(__NR_get_thread_area, &u_info));
-			if (TEST_RETURN == 0) {
-				tst_resm(TPASS, "get_thread_area call succeeded");
-			} else {
-				tst_resm(TFAIL, "failed - errno = %d : %s",
-					 TEST_ERRNO, strerror(TEST_ERRNO));
-				cleanup();
-				tst_exit();
+	for (lc = 0; TEST_LOOPING(lc); lc++) {
+		for (i = 0; i < sizeof(tests) / sizeof(struct test); i++) {
+			TEST(syscall(tests[i].syscall, tests[i].u_info));
+			
+			if (TEST_RETURN != tests[i].exp_ret) {
+				tst_resm(TFAIL, "%s returned %li expected %i",
+				         get_name(tests[i].syscall),
+					 TEST_RETURN, tests[i].exp_ret);
+				continue;
 			}
 
-			u_info.entry_number = -1;
-			/*
-			 * This call to set_thread_area function should be sucessful.
-			 */
-
-			/* Call set_thread_area(). */
-			TEST(syscall(__NR_set_thread_area, &u_info));
-
-			if (TEST_RETURN == 0) {
-				tst_resm(TPASS, "set_thread_area call succeeded");
-			} else {
-			     	tst_resm(TFAIL, "failed - errno = %d : %s",
-					 TEST_ERRNO, strerror(TEST_ERRNO));
-				cleanup();
-	  			tst_exit();
+			if (TEST_ERRNO != tests[i].exp_errno) {
+				tst_resm(TFAIL, "%s failed with %i (%s) expected %i (%s)",
+				         get_name(tests[i].syscall), TEST_ERRNO,
+					 strerror(TEST_ERRNO), tests[i].exp_errno,
+					 strerror(tests[i].exp_errno));
+				continue;
 			}
-
+			
+			tst_resm(TPASS, "%s returned %li errno %i (%s)",
+			         get_name(tests[i].syscall), TEST_RETURN,
+				 TEST_ERRNO, strerror(TEST_ERRNO));
 		}
-
 	}
 
 	cleanup();
-
 	tst_exit();
-
 }
-
 #else
-int main(void) {
-	tst_resm(TCONF, "[gs]et_thread_area is not available on this "
-			"architecture");
+int main(void)
+{
+	tst_brkm(TCONF, NULL, "set_thread_area isn't available for this architecture");
 	tst_exit();
 }
 #endif
