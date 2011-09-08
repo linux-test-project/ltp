bca9a874cc1724aa8d17b0173340c68deda0a20b
Nageswara R Sastry <rnsastry@linux.vnet.ibm.com>
2011-09-08 15:21:03 +0530

    KSM test case fixes - ksm03
    Disable ksm03 test if KSM is not configured
    
    Signed-off-by: Nageswara R Sastry <rnsastry@linux.vnet.ibm.com>

--- testcases/kernel/mem/ksm/ksm03.c
+++ testcases/kernel/mem/ksm/ksm03.c
@@ -107,6 +107,8 @@ void setup(void)
 
 	if (tst_kvercmp(2, 6, 32) < 0)
 		tst_brkm(TCONF, NULL, "2.6.32 or greater kernel required");
+	if (access(PATH_KSM, F_OK) == -1)
+		tst_brkm(TCONF, NULL, "KSM configuration is not enabled");
 
 	mount_mem("memcg", "cgroup", "memory", MEMCG_PATH, MEMCG_PATH_NEW);
 	tst_sig(FORK, DEF_HANDLER, NULL);
