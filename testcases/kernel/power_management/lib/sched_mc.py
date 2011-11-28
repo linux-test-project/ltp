bff601dedcc2000efc0f3f0096a9be83ebecfafb
Xu,Anhua <anhua.xu@intel.com>
2011-11-28 22:42:09 +0800

    Power Management: fix for verify_sched_domain_dmesg
    verify_sched_domain_dmesg() failed to expand cpu_groups: [0-5],[12-17]
    as one group [0-17]. This is totally wrong, since [6-11] is mistakenly
    involved.
    
    Signed-off-by: Xu,Anhua <anhua.xu@intel.com>

--- testcases/kernel/power_management/lib/sched_mc.py
+++ testcases/kernel/power_management/lib/sched_mc.py
@@ -668,7 +668,7 @@ def verify_sched_domain_dmesg(sched_mc_level, sched_smt_level):
                         group_info=group_info.replace\
                             (group_info[openindex:closeindex+1],"")
 
-                    subgroup = group_info.split()
+                    subgroup = group_info.split(",")
                     for j in range(0, len(subgroup)):
                         cpu_group = expand_range(subgroup[j])
                         status = validate_cpugrp_map(cpu_group, sched_mc_level,\
