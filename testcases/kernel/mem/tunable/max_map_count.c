c7c3f75d48affb4a56ff7b17f5fea08c506dc92a
Caspar Zhang <caspar@casparzhang.com>
2012-03-15 01:06:14 +0800

    tunable/max_map_count: avoid oom
    This patch fixes the following issues in max_map_count:
    
    1) add a workaround to avoid triggering oom-killer when testing. Since
    oom-killer is not stable, it is expected to keep this workaround in this
    testcase for long time.
    
    2) move count map procedure to parent since the test might still
    succeed if child fails.
    
    Signed-off-by: Caspar Zhang <caspar@casparzhang.com>

--- testcases/kernel/mem/tunable/max_map_count.c
+++ testcases/kernel/mem/tunable/max_map_count.c
@@ -29,15 +29,6 @@
  * we should except the two special vmas from map_count:
  * map_count -= 2;
  *
- * While most applications need less than a thousand maps, and the tunable
- * default value is 65530.
- * When the value is too large or too little, the system would hang, so I
- * choose four special value to test, which can coverage most situation:
- * a) Default value / 100
- * b) Default value / 10
- * c) Default value
- * d) Default value * 10
- *
  * ********************************************************************
  * Copyright (C) 2012 Red Hat, Inc.
  *
@@ -63,7 +54,7 @@
  *
  * ********************************************************************
  */
-
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
@@ -75,13 +66,14 @@
 #include "usctest.h"
 #include "mem.h"
 
-#define MAX_MAP_COUNT_DEFAULT 65530L
+#define MAX_MAP_COUNT_DEFAULT 64
 
 char *TCID = "max_map_count";
 int TST_TOTAL = 1;
+
 static long old_max_map_count;
 
-static long count_maps(void);
+static long count_maps(pid_t pid);
 static void max_map_count_test(void);
 
 int main(int argc, char *argv[])
@@ -89,11 +81,11 @@ int main(int argc, char *argv[])
 	char *msg;
 	int lc;
 
-	setup();
 	msg = parse_opts(argc, argv, NULL, NULL);
 	if (msg != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR -%s ", msg);
 
+	setup();
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
 		Tst_count = 0;
 		max_map_count_test();
@@ -124,27 +116,26 @@ void cleanup(void)
 	TEST_CLEANUP;
 }
 
-static long count_maps(void)
+static long count_maps(pid_t pid)
 {
 	FILE *fp;
-	int ret;
 	size_t len;
 	char *line = NULL;
 	char buf[BUFSIZ];
 	long map_count = 0;
 
-	while (mmap(NULL, 1, PROT_READ, MAP_SHARED | MAP_ANONYMOUS, -1,
-		    (off_t)NULL) != MAP_FAILED) ;
-
-	ret = sprintf(buf, "/proc/%d/maps", getpid());
-	if (ret < 0)
-		tst_brkm(TBROK | TERRNO, cleanup, "sprintf");
-
-	if ((fp = fopen(buf, "r")) == NULL)
+	snprintf(buf, BUFSIZ, "/proc/%d/maps", pid);
+	fp = fopen(buf, "r");
+	if (fp == NULL)
 		tst_brkm(TBROK | TERRNO, cleanup, "fopen %s", buf);
-	while (getline(&line, &len, fp) != -1)
+	while (getline(&line, &len, fp) != -1) {
+		/* exclude vdso and vsyscall */
+		if (sscanf(line, "%*p-%*p %*4s %*p %*2d:%*2d %*d %s", buf) ==
+			    1 && ((strcmp(buf, "[vdso]") == 0) ||
+				  (strcmp(buf, "[vsyscall]") == 0)))
+			continue;
 		map_count++;
-
+	}
 	fclose(fp);
 
 	return map_count;
@@ -157,41 +148,60 @@ static void max_map_count_test(void)
 	pid_t pid;
 	long max_maps;
 	long map_count;
+	long max_iters;
 
-	tst_resm(TINFO, "Starting max_map_count test...");
-	max_maps = MAX_MAP_COUNT_DEFAULT / 1000;
-	for (i = 0; i < 4; i++) {
-		max_maps *= 10;
+	/*
+	 * XXX Due to a possible kernel bug, oom-killer can be easily
+	 * triggered when doing small piece mmaps in huge amount even if
+	 * many swap is available and/or overcommit_memory set to 2.
+	 * Also oom-killer would kill wrong victims in this situation,
+	 * we only test with all free physical memory for now. After
+	 * oom-killer becomes more stable (which is unlikely to be), we
+	 * can consider to test all available physical mem + swap.
+	 */
+	max_iters = read_meminfo("MemFree:") / sysconf(_SC_PAGESIZE) * 1024;
+
+	max_maps = MAX_MAP_COUNT_DEFAULT;
+	for (i = 0; i < 5; i++) {
+		if (max_maps > max_iters)
+			max_maps = max_iters;
 		set_sys_tune("max_map_count", max_maps, 1);
-		fflush(stdout);
 
 		switch (pid = fork()) {
 		case -1:
 			tst_brkm(TBROK | TERRNO, cleanup, "fork");
 		case 0:
-			map_count = count_maps();
-
-			/* Except VDS and VSYSCALL special maps */
-			map_count -= 2;
-			printf("Child[%d] generated %ld map entries.\n",
-			       getpid(), map_count);
-			if (map_count > max_maps)
-				tst_resm(TFAIL, "The actual map entries"
-					 " > max_map_count");
+			while (mmap(NULL, 1, PROT_READ,
+				    MAP_SHARED|MAP_ANONYMOUS, -1, 0)
+					!= MAP_FAILED)
+				;
+			if (raise(SIGSTOP) != 0)
+				tst_brkm(TBROK|TERRNO, tst_exit, "raise");
 			exit(0);
 		default:
-			if (waitpid(pid, &status, 0) == -1)
-				tst_brkm(TBROK | TERRNO, cleanup, "waitpid");
-
-			if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-				if (WIFSIGNALED(status)
-				    && WTERMSIG(status) == SIGKILL)
-					printf("Child[%d] is killed by "
-					       "SIGKILL signal.\n", pid);
-				else
-					tst_resm(TFAIL, "Child[%d] "
-						 "failed unexpectedly", pid);
-			}
+			break;
 		}
+		/* wait child done mmap and stop */
+		if (waitpid(pid, &status, WUNTRACED) == -1)
+			tst_brkm(TBROK|TERRNO, cleanup, "waitpid");
+		if (!WIFSTOPPED(status))
+			tst_brkm(TBROK, cleanup, "child did not stopped");
+
+		map_count = count_maps(pid);
+		if (map_count == max_maps)
+			tst_resm(TPASS, "%ld map entries in total "
+					"as expected.", max_maps);
+		else
+			tst_resm(TFAIL, "%ld map entries in total, but "
+					"expected %ld entries", map_count,
+					max_maps);
+
+		/* make child continue to exit */
+		if (kill(pid, SIGCONT) != 0)
+			tst_brkm(TBROK|TERRNO, cleanup, "kill");
+		if (waitpid(pid, &status, 0) == -1)
+			tst_brkm(TBROK|TERRNO, cleanup, "waitpid");
+
+		max_maps *= 10;
 	}
 }
