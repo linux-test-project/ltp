19a526eab6a245a6064501d5eb037a94fd97771e
Cyril Hrubis <chrubis@suse.cz>
2012-06-08 14:50:05 +0200

    Cleanup fork/2-1.c test source.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/open_posix_testsuite/conformance/interfaces/fork/2-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/fork/2-1.c
@@ -41,107 +41,55 @@
 #define _XOPEN_SOURCE 600
 #endif
 
-/********************************************************************************************/
-/****************************** standard includes *****************************************/
-/********************************************************************************************/
 #include <pthread.h>
- #include <stdarg.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <string.h>
- #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include <sys/wait.h>
- #include <errno.h>
+#include <errno.h>
 
 #include <signal.h>
 
-/********************************************************************************************/
-/******************************   Test framework   *****************************************/
-/********************************************************************************************/
-#include "../testfrmw/testfrmw.h"
- #include "../testfrmw/testfrmw.c"
-/* This header is responsible for defining the following macros:
- * UNRESOLVED(ret, descr);
- *    where descr is a description of the error and ret is an int (error code for example)
- * FAILED(descr);
- *    where descr is a short text saying why the test has failed.
- * PASSED();
- *    No parameter.
- *
- * Both three macros shall terminate the calling process.
- * The testcase shall not terminate in any other maneer.
- *
- * The other file defines the functions
- * void output_init()
- * void output(char * string, ...)
- *
- * Those may be used to output information.
- */
+#include "posixtest.h"
 
-/********************************************************************************************/
-/********************************** Configuration ******************************************/
-/********************************************************************************************/
-#ifndef VERBOSE
-#define VERBOSE 1
-#endif
-
-/********************************************************************************************/
-/***********************************    Test case   *****************************************/
-/********************************************************************************************/
+struct test_struct {
+	char one;
+	short two;
+	int three;
+	void *four;
+};
 
 void handler(int sig)
 {
-	/* this won't be used */
-	output("Sig %d received\n", sig);
-
-	return ;
+	(void)sig;
 }
 
-/* The main test function. */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	int ret, status;
 	pid_t child, ctl;
-
-	/* structure for structure test */
-
-	struct _thestruct
-	{
-		char one ;
-		short two;
-		int three;
-		void * four;
-	}
-
-	mystruct = {1, 2, 3, (void *) 4};
-
-	/* pointer for malloc'ed memory */
-	void * malloced;
-
-	/* Structure for signal handler test */
-
+	void *malloced;
 	struct sigaction sa_ori, sa_child;
+	struct test_struct mystruct = {1, 2, 3, (void *) 4};
 
-	/* Initialize output */
-	output_init();
+	malloced = malloc(sysconf(_SC_PAGESIZE));
 
-	/* Initialize the memory pointer */
-	malloced = (void *) malloc(sysconf(_SC_PAGESIZE));
-
-	if (malloced == NULL)
-	{
-		UNRESOLVED(errno, "Unable to alloc memory");
+	if (malloced == NULL) {
+		perror("malloc() failed");
+		return PTS_UNRESOLVED;
 	}
 
-	*(double *) malloced = 2.3;
+	*(double *)malloced = 2.3;
 
 	/* Initialize an environment variable */
 	ret = setenv("OPTS_FORK_TC", "2-1.c", 1);
 
-	if (ret != 0)
-	{
-		UNRESOLVED(errno, "Failed to set the environment variable");
+	if (ret != 0) {
+		perror("setenv() failed");
+		return PTS_UNRESOLVED;
 	}
 
 	/* Initialize the signal handler */
@@ -149,134 +97,117 @@ int main(int argc, char * argv[])
 
 	ret = sigemptyset(&sa_ori.sa_mask);
 
-	if (ret != 0)
-	{
-		UNRESOLVED(errno, "sigemptyset failed");
+	if (ret != 0) {
+		perror("sigemptyset() failed");
+		return PTS_UNRESOLVED;
 	}
 
 	ret = sigaddset(&sa_ori.sa_mask, SIGUSR2);
 
-	if (ret != 0)
-	{
-		UNRESOLVED(errno, "sigaddset failed");
+	if (ret != 0) {
+		perror("sigaddset() failed");
+		return PTS_UNRESOLVED;
 	}
 
 	sa_ori.sa_flags = SA_NOCLDSTOP;
 	ret = sigaction(SIGUSR1, &sa_ori, NULL);
 
-	if (ret != 0)
-	{
-		UNRESOLVED(errno, "Failed to set the signal handler");
+	if (ret != 0) {
+		perror("sigaction() failed");
+		return PTS_UNRESOLVED;
 	}
 
 	/* Create the child */
 	child = fork();
 
-	if (child == -1)
-	{
-		UNRESOLVED(errno, "Failed to fork");
+	if (child == -1) {
+		perror("fork() failed");
+		return PTS_UNRESOLVED;
 	}
 
 	/* child */
-	if (child == 0)
-	{
+	if (child == 0) {
 		/* Check the struct was copied */
-
 		if ((mystruct.one != 1) || (mystruct.two != 2) ||
-		    (mystruct.three != 3) || (mystruct.four != (void *) 4))
-		{
-			FAILED("The struct data was not copied to the child process");
+		    (mystruct.three != 3) || (mystruct.four != (void *) 4)) {
+			printf("On-the-stack structure not copied correctly\n");
+			return PTS_FAIL;
 		}
 
 		/* Check the malloc'ed memory is copied */
-		if (*(double *) malloced != 2.3)
-		{
-			FAILED("Malloc'd block not copied in child process");
+		if (*(double *) malloced != 2.3) {
+			printf("Allocated page not copied correctly\n");
+			return PTS_FAIL;
 		}
 
 		/* Free the memory -- this should suceed */
 		free(malloced);
 
 		/* Check the env variable */
-		if (strncmp("2-1.c", getenv("OPTS_FORK_TC"), 6) != 0)
-		{
-			FAILED("The environment is not copied to the child");
+		if (strncmp("2-1.c", getenv("OPTS_FORK_TC"), 6) != 0) {
+			printf("Enviroment variable not copied correctly\n");
+			return PTS_FAIL;
 		}
 
 		/* Check the signal handler stuff */
 		ret = sigaction(SIGUSR1, NULL, &sa_child);
 
-		if (ret != 0)
-		{
-			UNRESOLVED(errno, "Failed to read sigaction information in child");
+		if (ret != 0) {
+			perror("sigaction() failed in child");
+			return PTS_UNRESOLVED;
 		}
 
-		if (sa_child.sa_handler != handler)
-		{
-			FAILED("The child signal handler function is different from the parent's");
+		if (sa_child.sa_handler != handler) {
+			printf("Signal handler function is different\n");
+			return PTS_FAIL;
 		}
 
 		ret = sigismember(&sa_child.sa_mask, SIGUSR2);
 
-		if (ret == 0)
-		{
-			FAILED("The child signal handler mask is different from the parent's");
+		if (ret == 0) {
+			printf("Signal handler mask is different\n");
+			return PTS_FAIL;
 		}
 
-		if (ret != 1)
-		{
-			UNRESOLVED(errno, "Unexpected return code from sigismember");
+		if (ret != 1) {
+			printf("Unexpected return code from sigismember\n");
+			return PTS_UNRESOLVED;
 		}
 
 		if (((sa_child.sa_flags & SA_NOCLDSTOP) != SA_NOCLDSTOP)
 #ifndef WITHOUT_XOPEN
-		        || ((sa_child.sa_flags & SA_ONSTACK) != 0)
-		        || ((sa_child.sa_flags & SA_RESETHAND) != 0)
-		        || ((sa_child.sa_flags & SA_RESTART) != 0)
-		        || ((sa_child.sa_flags & SA_SIGINFO) != 0)
-		        || ((sa_child.sa_flags & SA_NOCLDWAIT) != 0)
-		        || ((sa_child.sa_flags & SA_NODEFER) != 0)
+			|| ((sa_child.sa_flags & SA_ONSTACK) != 0)
+			|| ((sa_child.sa_flags & SA_RESETHAND) != 0)
+			|| ((sa_child.sa_flags & SA_RESTART) != 0)
+			|| ((sa_child.sa_flags & SA_SIGINFO) != 0)
+			|| ((sa_child.sa_flags & SA_NOCLDWAIT) != 0)
+			|| ((sa_child.sa_flags & SA_NODEFER) != 0)
 #endif
-		   )
-		{
-			FAILED("The sigaction flags are different in the child");
+		   ) {
+			printf("The sigaction flags are different\n");
+			return PTS_FAIL;
 		}
 
-		/* The child stops here */
-		exit(PTS_PASS);
+		return PTS_PASS;
 	}
 
 	/* Parent joins the child */
 	ctl = waitpid(child, &status, 0);
 
-	if (ctl != child)
-	{
-		UNRESOLVED(errno, "Waitpid returned the wrong PID");
+	if (ctl != child) {
+		printf("Waitpid returned wrong PID\n");
+		return PTS_UNRESOLVED;
 	}
 
-	if (!WIFEXITED(status))
-	{
-		UNRESOLVED(status, "Child exited abnormally");
+	if (!WIFEXITED(status)) {
+		printf("Child exited abnormally\n");
+		return PTS_UNRESOLVED;
 	}
 
-	if (WEXITSTATUS(status) == PTS_PASS)
-	{
-
-		/* Test passed */
-#if VERBOSE > 0
-		output("Test passed\n");
-#endif
-
-		PASSED;
+	if (WEXITSTATUS(status) == PTS_PASS) {
+		printf("Test PASSED\n");
+		return PTS_PASS;
 	}
 
-	if (WEXITSTATUS(status) == PTS_FAIL)
-	{
-
-		/* Test failed */
-		FAILED("Test failed in child\n");
-	}
-
-	/* Otherwise we've an unexpected return code */
-	UNRESOLVED(WEXITSTATUS(status), "Child returned an unexpected error code");
+	return WEXITSTATUS(status);
 }
