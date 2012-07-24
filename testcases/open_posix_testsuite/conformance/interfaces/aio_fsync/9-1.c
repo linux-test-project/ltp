cc2d0e6ce0bc503dd57e2b85b28debdb395f34bd
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-07-24 14:44:02 +0800

    open_posix:aio_fsync:9-1: code cleanup
    Fix coding style.
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/open_posix_testsuite/conformance/interfaces/aio_fsync/9-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/aio_fsync/9-1.c
@@ -38,8 +38,7 @@ int main()
 	unlink(tmpfname);
 	fd = open(tmpfname, O_CREAT | O_RDWR | O_EXCL,
 		  S_IRUSR | S_IWUSR);
-	if (fd == -1)
-	{
+	if (fd == -1) {
 		printf(TNAME " Error at open(): %s\n",
 		       strerror(errno));
 		exit(PTS_UNRESOLVED);
@@ -52,8 +51,7 @@ int main()
 	aiocb_write.aio_buf = buf;
 	aiocb_write.aio_nbytes = BUF_SIZE;
 
-	if (aio_write(&aiocb_write) == -1)
-	{
+	if (aio_write(&aiocb_write) == -1) {
 		printf(TNAME " Error at aio_write(): %s\n",
 		       strerror(errno));
 		exit(PTS_FAIL);
@@ -62,13 +60,12 @@ int main()
 	memset(&aiocb_fsync, 0, sizeof(aiocb_fsync));
 	aiocb_fsync.aio_fildes = fd;
 
-	if (aio_fsync(O_SYNC, &aiocb_fsync) != 0)
-	{
+	if (aio_fsync(O_SYNC, &aiocb_fsync) != 0) {
 		printf(TNAME " Error at aio_fsync(): %s\n", strerror(errno));
 		exit(PTS_FAIL);
 	}
 
 	close(fd);
-	printf ("Test PASSED\n");
+	printf("Test PASSED\n");
 	return PTS_PASS;
 }
