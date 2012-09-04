b7ba6ec83027a33948b723c6684d708d324e2cb4
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-09-04 13:54:53 +0800

    open_posix:mq_open:25-2:code cleanup
    Fix coding style.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/open_posix_testsuite/conformance/interfaces/mq_open/25-2.c
+++ testcases/open_posix_testsuite/conformance/interfaces/mq_open/25-2.c
@@ -31,80 +31,78 @@ static int invalid_values[NUMTESTS] = { 0, -1, INT32_MIN };
 
 int main()
 {
-        char qname[NAMESIZE];
-        mqd_t queue;
+	char qname[NAMESIZE];
+	mqd_t queue;
 	struct mq_attr attr;
-	int i, failed=0;
+	int i, failed = 0;
 
-        sprintf(qname, "/mq_open_25-1_%d", getpid());
+	sprintf(qname, "/mq_open_25-1_%d", getpid());
 
-	// First, test for invalid maxmsg
 	attr.mq_msgsize = VALIDVAL;
-	for (i=0; i<NUMTESTS; i++) {
+	for (i = 0; i < NUMTESTS; i++) {
 		attr.mq_maxmsg = invalid_values[i];
-        	queue = mq_open(qname, O_CREAT |O_RDWR,
-						S_IRUSR | S_IWUSR, &attr);
-        	if (queue != (mqd_t)-1) {
+		queue = mq_open(qname, O_CREAT | O_RDWR,
+				S_IRUSR | S_IWUSR, &attr);
+		if (queue != (mqd_t) -1) {
 			printf("mq_open() succeeded w/invalid mq_maxmsg %ld\n",
-					attr.mq_maxmsg);
+			       attr.mq_maxmsg);
 			mq_close(queue);
 			mq_unlink(qname);
 			failed++;
 #ifdef DEBUG
-        	} else {
+		} else {
 			printf("mq_maxmsg %ld failed as expected\n",
-					attr.mq_maxmsg);
+			       attr.mq_maxmsg);
 #endif
 		}
 
 		if (errno != EINVAL) {
 			printf("errno != EINVAL for mq_maxmsg %ld\n",
-					attr.mq_maxmsg);
+			       attr.mq_maxmsg);
 			failed++;
 #ifdef DEBUG
-        	} else {
+		} else {
 			printf("mq_maxmsg %ld set errno==EINVAL as expected\n",
-					attr.mq_maxmsg);
+			       attr.mq_maxmsg);
 #endif
 		}
 	}
 
-	// Second, test for invalid msgsize
 	attr.mq_maxmsg = VALIDVAL;
-	for (i=0; i<NUMTESTS; i++) {
+	for (i = 0; i < NUMTESTS; i++) {
 		attr.mq_msgsize = invalid_values[i];
-        	queue = mq_open(qname, O_CREAT |O_RDWR,
-						S_IRUSR | S_IWUSR, &attr);
-        	if (queue != (mqd_t)-1) {
+		queue = mq_open(qname, O_CREAT | O_RDWR,
+				S_IRUSR | S_IWUSR, &attr);
+		if (queue != (mqd_t) -1) {
 			printf("mq_open() succeeded w/invalid mq_msgsize %ld\n",
-					attr.mq_msgsize);
+			       attr.mq_msgsize);
 			mq_close(queue);
 			mq_unlink(qname);
 			failed++;
 #ifdef DEBUG
-        	} else {
+		} else {
 			printf("mq_msgsize %ld failed as expected\n",
-					attr.mq_msgsize);
+			       attr.mq_msgsize);
 #endif
 		}
 
 		if (errno != EINVAL) {
 			printf("errno != EINVAL for mq_msgsize %ld\n",
-					attr.mq_msgsize);
+			       attr.mq_msgsize);
 			failed++;
 #ifdef DEBUG
-        	} else {
+		} else {
 			printf("mq_msgsize %ld set errno==EINVAL as expected\n",
-					attr.mq_msgsize);
+			       attr.mq_msgsize);
 #endif
 		}
 	}
 
-	if (failed>0) {
+	if (failed > 0) {
 		printf("Test FAILED\n");
 		return PTS_FAIL;
 	}
 
-        printf("Test PASSED\n");
-        return PTS_PASS;
+	printf("Test PASSED\n");
+	return PTS_PASS;
 }
