5ba115924bb886f6e469c49f0c1a59004d664f85
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-09-04 13:54:53 +0800

    open_posix:mq_open:19-1:code cleanup
    Fix coding style.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/open_posix_testsuite/conformance/interfaces/mq_open/19-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/mq_open/19-1.c
@@ -37,65 +37,65 @@
 
 int main()
 {
-        char qname[NAMESIZE], msgrcd[BUFFER];
-        const char *msgptr = MSGSTR;
-        mqd_t queue;
+	char qname[NAMESIZE], msgrcd[BUFFER];
+	const char *msgptr = MSGSTR;
+	mqd_t queue;
 	struct mq_attr attr;
-	int failure=0;
+	int failure = 0;
 	unsigned pri;
 
-        sprintf(qname, "/mq_open_19-1_%d", getpid());
+	sprintf(qname, "/mq_open_19-1_%d", getpid());
 
 	attr.mq_msgsize = BUFFER;
 	attr.mq_maxmsg = BUFFER;
 
-        queue = mq_open(qname, O_CREAT |O_RDWR|O_NONBLOCK,
+	queue = mq_open(qname, O_CREAT | O_RDWR | O_NONBLOCK,
 			S_IRUSR | S_IWUSR, &attr);
-        if (queue == (mqd_t)-1) {
-                perror("mq_open() did not return success");
+	if (queue == (mqd_t) -1) {
+		perror("mq_open() did not return success");
 		printf("Test UNRESOLVED\n");
-                return PTS_UNRESOLVED;
-        }
-
-        if (mq_receive(queue, msgrcd, BUFFER, &pri) != -1) {
-		printf("mq_receive() succeded\n");
-		printf("mq_open() may have placed a message in the queue\n");
-		failure=1;
+		return PTS_UNRESOLVED;
 	}
 
-        if (mq_send(queue, msgptr, strlen(msgptr), 1) == -1) {
-                perror("mq_send() did not return success");
+	if (mq_receive(queue, msgrcd, BUFFER, &pri) != -1) {
+		printf("mq_receive() succeded\n");
+		printf("mq_open() may have placed a message in the queue\n");
+		failure = 1;
+	}
+
+	if (mq_send(queue, msgptr, strlen(msgptr), 1) == -1) {
+		perror("mq_send() did not return success");
 		printf("Test UNRESOLVED\n");
 		/* close queue and exit */
 		mq_close(queue);
 		mq_unlink(qname);
-                return PTS_UNRESOLVED;
-        }
+		return PTS_UNRESOLVED;
+	}
 
-        queue = mq_open(qname, O_RDWR|O_NONBLOCK, S_IRUSR | S_IWUSR, NULL);
-        if (queue == (mqd_t)-1) {
-                perror("mq_open() second time did not return success");
+	queue = mq_open(qname, O_RDWR | O_NONBLOCK, S_IRUSR | S_IWUSR, NULL);
+	if (queue == (mqd_t) -1) {
+		perror("mq_open() second time did not return success");
 		printf("Test UNRESOLVED\n");
 		/* close queue and exit */
 		mq_close(queue);
 		mq_unlink(qname);
-                return PTS_UNRESOLVED;
-        }
+		return PTS_UNRESOLVED;
+	}
 
-        if (mq_receive(queue, msgrcd, BUFFER, &pri) == -1) {
+	if (mq_receive(queue, msgrcd, BUFFER, &pri) == -1) {
 		perror("mq_receive() failed");
 		printf("mq_open() may have removed a msg from the queue\n");
-		failure=1;
+		failure = 1;
 	}
 
 	mq_close(queue);
 	mq_unlink(qname);
 
-	if (failure==1) {
+	if (failure == 1) {
 		printf("Test FAILED\n");
 		return PTS_FAIL;
 	}
 
-        printf("Test PASSED\n");
-        return PTS_PASS;
+	printf("Test PASSED\n");
+	return PTS_PASS;
 }
