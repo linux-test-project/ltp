4548c6cf9bcdd96d8303caa4130ab638b61f8a30
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2012-10-19 18:03:36 +0800

    Update FSF address
    find . -type f -exec sed -i 's/675 Mass Ave, Cambridge, MA 02139, USA/51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA/g' {} \;
    find . -type f -exec sed -i 's/59 Temple Place, Suite 330, Boston, MA 02111-1307 USA/51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA/g' {} \;
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/kernel/sched/sched_stress/sched.h
+++ testcases/kernel/sched/sched_stress/sched.h
@@ -13,7 +13,7 @@
  *
  *   You should have received a copy of the GNU General Public License
  *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 /*@(#)63  1.1  testcase/bpsc_str/sched.h, pvt_kernel, pv_kernel 12/11/96 16:43:19*/
 /*---------------------------------------------------------------------+
