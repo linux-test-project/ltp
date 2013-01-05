e04a8ee5668ef13a5670b5c0a0e0150abcf4ed38
Mike Frysinger <vapier@gentoo.org>
2013-01-04 23:23:59 -0500

    cpuid: optimize x86 & fix x86_64 truncation
    Use xchg on x86 to avoid needing to use an intermediate register and
    shave off one instruction.
    
    Skip the whole thing on x86_64 to avoid truncating the top 32bits of
    the rbx reg due to moving things around with ebx/etc...
    
    Signed-off-by: Mike Frysinger <vapier@gentoo.org>

--- include/ltp_cpuid.h
+++ include/ltp_cpuid.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
+ * Copyright (c) 2012-2013 The Chromium OS Authors. All rights reserved.
  *
  * Licensed under the BSD 3-clause.
  */
@@ -13,13 +13,17 @@ static inline void cpuid(unsigned int info, unsigned int *eax, unsigned int *ebx
 #if defined(__i386__) || defined(__x86_64__)
 	unsigned int _eax = info, _ebx, _ecx, _edx;
 	asm volatile(
-		"mov %%ebx, %%edi;" // save ebx (for PIC)
+# ifdef __i386__
+		"xchg %%ebx, %%esi;" /* save ebx (for PIC) */
 		"cpuid;"
-		"mov %%ebx, %%esi;" // pass to caller
-		"mov %%edi, %%ebx;" // restore ebx
-		: "+a" (_eax), "=S" (_ebx), "=c" (_ecx), "=d" (_edx)
-		:       /* inputs: eax is handled above */
-		: "edi" /* clobbers: we hit edi directly */
+		"xchg %%esi, %%ebx;" /* restore ebx & pass to caller */
+		: "=S" (_ebx),
+# else
+		"cpuid;"
+		: "=b" (_ebx),
+# endif
+		  "+a" (_eax), "=c" (_ecx), "=d" (_edx)
+		: /* inputs: eax is handled above */
 	);
 	if (eax) *eax = _eax;
 	if (ebx) *ebx = _ebx;
