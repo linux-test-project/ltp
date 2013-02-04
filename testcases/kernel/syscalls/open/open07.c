78d42779bb87102d5dd1fd2441c9928d543b5839
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2013-02-04 17:11:18 +0800

    open07: code cleanups
    Cleanup the coding style.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/kernel/syscalls/open/open07.c
+++ testcases/kernel/syscalls/open/open07.c
@@ -40,7 +40,7 @@
  *	4. Create a symbolic link to a symbolically linked directory, and call
  *	   open(O_NOFOLLOW). Check that it returns ELOOP.
  *
- * 	5. Create a symbolic link to a directory, and call
+ *	5. Create a symbolic link to a directory, and call
  *         open("link/", O_NOFOLLOW). Check that it succeeds.
  *
  * USAGE:  <for command-line>
@@ -67,21 +67,22 @@
 #include "test.h"
 #include "usctest.h"
 
-void setup(void);
-void cleanup(void);
-void setupfunc_test1();
-void setupfunc_test2();
-void setupfunc_test3();
-void setupfunc_test4();
-void setupfunc_test5();
+static void setup(void);
+static void cleanup(void);
+static void setupfunc_test1();
+static void setupfunc_test2();
+static void setupfunc_test3();
+static void setupfunc_test4();
+static void setupfunc_test5();
 
 char *TCID = "open07";
 int TST_TOTAL = 5;
-int fd1, fd2;
 
-int exp_enos[] = { ELOOP, 0 };
+static int fd1, fd2;
 
-struct test_case_t {
+static int exp_enos[] = { ELOOP, 0 };
+
+static struct test_case_t {
 	char *desc;
 	char filename[100];
 	int flags;
@@ -89,19 +90,17 @@ struct test_case_t {
 	void (*setupfunc) ();
 	int exp_errno;
 } TC[] = {
-	{
-		"Test for ELOOP on f2: f1 -> f2", {
-	}, O_NOFOLLOW, 00700, setupfunc_test1, ELOOP}, {
-		"Test for ELOOP on d2: d1 -> d2", {
-	}, O_NOFOLLOW, 00700, setupfunc_test2, ELOOP}, {
-		"Test for ELOOP on f3: f1 -> f2 -> f3", {
-	}, O_NOFOLLOW, 00700, setupfunc_test3, ELOOP}, {
-		"Test for ELOOP on d3: d1 -> d2 -> d3", {
-	}, O_NOFOLLOW, 00700, setupfunc_test4, ELOOP}, {
-		"Test for success on d2: d1 -> d2", {
-	}, O_NOFOLLOW, 00700, setupfunc_test5, 0}, {
-		NULL, {
-	}, 0, 0, NULL, 0}
+	{"Test for ELOOP on f2: f1 -> f2", {},
+	 O_NOFOLLOW, 00700, setupfunc_test1, ELOOP},
+	{"Test for ELOOP on d2: d1 -> d2", {},
+	 O_NOFOLLOW, 00700, setupfunc_test2, ELOOP},
+	{"Test for ELOOP on f3: f1 -> f2 -> f3", {},
+	 O_NOFOLLOW, 00700, setupfunc_test3, ELOOP},
+	{"Test for ELOOP on d3: d1 -> d2 -> d3", {},
+	 O_NOFOLLOW, 00700, setupfunc_test4, ELOOP},
+	{"Test for success on d2: d1 -> d2", {},
+	 O_NOFOLLOW, 00700, setupfunc_test5, 0},
+	{NULL, {}, 0, 0, NULL, 0}
 };
 
 int main(int ac, char **av)
@@ -110,9 +109,9 @@ int main(int ac, char **av)
 	char *msg;
 	int i;
 
-	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL) {
+	msg = parse_opts(ac, av, NULL, NULL);
+	if (msg != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-	}
 
 	setup();
 
@@ -120,19 +119,14 @@ int main(int ac, char **av)
 
 	/* run the setup routines for the individual tests */
 	for (i = 0; i < TST_TOTAL; i++) {
-		if (TC[i].setupfunc != NULL) {
+		if (TC[i].setupfunc != NULL)
 			TC[i].setupfunc();
-		}
 	}
 
-	/* The following loop checks looping state if -i option given */
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		/* reset Tst_count in case we are looping */
 		Tst_count = 0;
 
 		for (i = 0; TC[i].desc != NULL; ++i) {
-
 			TEST(open(TC[i].filename, TC[i].flags, TC[i].mode));
 
 			if (TC[i].exp_errno != 0) {
@@ -161,102 +155,103 @@ int main(int ac, char **av)
 						 "expected");
 				}
 			}
+
 			if (TEST_RETURN != -1)
 				close(TEST_RETURN);
 		}
 	}
+
 	cleanup();
 	tst_exit();
 }
 
-void setupfunc_test1()
+static void setupfunc_test1()
 {
 	char file1[100], file2[100];
 
 	sprintf(file1, "open03.1.%d", getpid());
 	sprintf(file2, "open03.2.%d", getpid());
-	if ((fd1 = creat(file1, 00700)) < 0) {
+	fd1 = creat(file1, 00700);
+	if (fd1 < 0)
 		tst_brkm(TBROK, cleanup, "creat(2) failed: errno: %d", errno);
-	}
-	if (symlink(file1, file2) < 0) {
+
+	if (symlink(file1, file2) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
+
 	strcpy(TC[0].filename, file2);
 }
 
-void setupfunc_test2()
+static void setupfunc_test2()
 {
 	char file1[100], file2[100];
 
 	sprintf(file1, "open03.3.%d", getpid());
 	sprintf(file2, "open03.4.%d", getpid());
-	if (mkdir(file1, 00700) < 0) {
+	if (mkdir(file1, 00700) < 0)
 		tst_brkm(TBROK, cleanup, "mkdir(2) failed: errno: %d", errno);
-	}
-	if (symlink(file1, file2) < 0) {
+
+	if (symlink(file1, file2) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
+
 	strcpy(TC[1].filename, file2);
 }
 
-void setupfunc_test3()
+static void setupfunc_test3()
 {
 	char file1[100], file2[100], file3[100];
 
 	sprintf(file1, "open03.5.%d", getpid());
 	sprintf(file2, "open03.6.%d", getpid());
 	sprintf(file3, "open03.7.%d", getpid());
-	if ((fd2 = creat(file1, 00700)) < 0) {
+	fd2 = creat(file1, 00700);
+	if (fd2 < 0)
 		tst_brkm(TBROK, cleanup, "creat(2) failed: errno: %d", errno);
-	}
-	if (symlink(file1, file2) < 0) {
+
+	if (symlink(file1, file2) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
-	if (symlink(file2, file3) < 0) {
+
+	if (symlink(file2, file3) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
+
 	strcpy(TC[2].filename, file3);
 }
 
-void setupfunc_test4()
+static void setupfunc_test4()
 {
 	char file1[100], file2[100], file3[100];
 
 	sprintf(file1, "open03.8.%d", getpid());
 	sprintf(file2, "open03.9.%d", getpid());
 	sprintf(file3, "open03.10.%d", getpid());
-	if (mkdir(file1, 00700) < 0) {
+	if (mkdir(file1, 00700) < 0)
 		tst_brkm(TBROK, cleanup, "mkdir(2) failed: errno: %d", errno);
-	}
-	if (symlink(file1, file2) < 0) {
+
+	if (symlink(file1, file2) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
-	if (symlink(file2, file3) < 0) {
+
+	if (symlink(file2, file3) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
+
 	strcpy(TC[3].filename, file3);
 }
 
-void setupfunc_test5()
+static void setupfunc_test5()
 {
 	char file1[100], file2[100];
 
 	sprintf(file1, "open11.3.%d", getpid());
 	sprintf(file2, "open12.4.%d", getpid());
-	if (mkdir(file1, 00700) < 0) {
+	if (mkdir(file1, 00700) < 0)
 		tst_brkm(TBROK, cleanup, "mkdir(2) failed: errno: %d", errno);
-	}
-	if (symlink(file1, file2) < 0) {
+
+	if (symlink(file1, file2) < 0)
 		tst_brkm(TBROK, cleanup, "symlink(2) failed: errno: %d", errno);
-	}
+
 	strcpy(TC[4].filename, file2);
 	strcat(TC[4].filename, "/");
 }
 
-/*
- * setup() - performs all ONE TIME setup for this test
- */
-void setup(void)
+static void setup(void)
 {
 	umask(0);
 
@@ -267,20 +262,11 @@ void setup(void)
 	tst_tmpdir();
 }
 
-/*
- * cleanup() - performs all the ONE TIME cleanup for this test at completion
- *	       or premature exit.
- */
-void cleanup(void)
+static void cleanup(void)
 {
-	/*
-	 * print timing status if that option was specified.
-	 * print errno log if that option was specified
-	 */
 	TEST_CLEANUP;
 	close(fd1);
 	close(fd2);
 
 	tst_rmdir();
-
 }
