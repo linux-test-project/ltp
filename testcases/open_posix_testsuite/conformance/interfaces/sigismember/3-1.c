6d99ffa539eac1b5c679417e554fde9c397c481e
Cyril Hrubis <chrubis@suse.cz>
2013-03-12 13:43:13 +0100

    openposix/.../sigismember: Cleanup.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/open_posix_testsuite/conformance/interfaces/sigismember/3-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/sigismember/3-1.c
@@ -4,18 +4,16 @@
  * This file is licensed under the GPL license.  For the full content
  * of this license, see the COPYING file at the top level of this
  * source tree.
-
+ *
  * Tests assertion 3 by filling a signal set and arbitrarily querying
  * it for a SIGABRT function. Sigmember should return a 1.
-*/
-
+ */
 #include <stdio.h>
 #include <signal.h>
 #include "posixtest.h"
 
-int main()
+int main(void)
 {
-
 	sigset_t signalset;
 
 	if (sigfillset(&signalset) == -1) {
@@ -24,13 +22,10 @@ int main()
 	}
 
 	if (sigismember(&signalset, SIGABRT) != 1) {
-#ifdef DEBUG
-		printf
-		    ("sigismember didn't returned a 1 even though sigfillset was just called\n");
-#endif
+		printf("sigismember doesn't confirm the signal was set\n");
 		return PTS_FAIL;
 	}
 
-	printf("sigismember passed\n");
+	printf("Test PASSED\n");
 	return PTS_PASS;
 }
--- testcases/open_posix_testsuite/conformance/interfaces/sigismember/4-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/sigismember/4-1.c
@@ -4,19 +4,18 @@
  * This file is licensed under the GPL license.  For the full content
  * of this license, see the COPYING file at the top level of this
  * source tree.
-
+ *
  * Tests assertion 4 by emptying a signal set and querying it for
  * a SIGABRT function. Sigmember should return a 0 indicating that
  * the signal is not a member of the set.
-*/
+ */
 
 #include <stdio.h>
 #include <signal.h>
 #include "posixtest.h"
 
-int main()
+int main(void)
 {
-
 	sigset_t signalset;
 
 	if (sigemptyset(&signalset) == -1) {
@@ -25,13 +24,10 @@ int main()
 	}
 
 	if (sigismember(&signalset, SIGABRT) != 0) {
-#ifdef DEBUG
-		printf
-		    ("sigismember did not return a 0 even though sigemptyset was just called\n");
-#endif
+		printf("sigismember dit not return 0");
 		return PTS_FAIL;
 	}
 
-	printf("sigismember passed\n");
+	printf("Test PASSED\n");
 	return PTS_PASS;
 }
