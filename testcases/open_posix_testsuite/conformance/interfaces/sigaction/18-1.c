5f3786942119704d17da876239c71a125985becc
Cyril Hrubis <chrubis@suse.cz>
2013-03-27 16:26:32 +0100

    conformance/interfaces/sigaction/18-*.c: Regenerate.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/open_posix_testsuite/conformance/interfaces/sigaction/18-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/sigaction/18-1.c
@@ -1,7 +1,8 @@
 /*
 * Copyright (c) 2005, Bull S.A..  All rights reserved.
 * Created by: Sebastien Decugis
-
+* Copyright (c) 2013 Cyril Hrubis <chrubis@suse.cz>
+*
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of version 2 of the GNU General Public License as
 * published by the Free Software Foundation.
@@ -13,26 +14,23 @@
 * You should have received a copy of the GNU General Public License along
 * with this program; if not, write the Free Software Foundation, Inc.,
 * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
+*
 * This sample test aims to check the following assertions:
 *
 * If SA_SIGINFO is not set in sa_flags, sa_handler is used as the signal
 * handling function.
-
+*
 * The steps are:
 * -> register a handler for SIGABRT without SA_SIGINFO, and a known function
-*   as saèhandler
+*   as sa_handler
 * -> raise SIGABRT, and check the function has been called.
-
+*
 * The test fails if the function is not called
 */
 
 /* We are testing conformance to IEEE Std 1003.1, 2003 Edition */
 #define _POSIX_C_SOURCE 200112L
 
-/******************************************************************************/
-/*************************** standard includes ********************************/
-/******************************************************************************/
 #include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
@@ -43,58 +41,19 @@
 #include <signal.h>
 #include <errno.h>
 
-/******************************************************************************/
-/***************************   Test framework   *******************************/
-/******************************************************************************/
-#include "../testfrmw/testfrmw.h"
-#include "../testfrmw/testfrmw.c"
-/* This header is responsible for defining the following macros:
- * UNRESOLVED(ret, descr);
- *    where descr is a description of the error and ret is an int
- *   (error code for example)
- * FAILED(descr);
- *    where descr is a short text saying why the test has failed.
- * PASSED();
- *    No parameter.
- *
- * Both three macros shall terminate the calling process.
- * The testcase shall not terminate in any other maneer.
- *
- * The other file defines the functions
- * void output_init()
- * void output(char * string, ...)
- *
- * Those may be used to output information.
- */
+#include "posixtest.h"
 
-/******************************************************************************/
-/**************************** Configuration ***********************************/
-/******************************************************************************/
-#ifndef VERBOSE
-#define VERBOSE 1
-#endif
+static sig_atomic_t called = 0;
 
-#define SIGNAL SIGABRT
-
-/******************************************************************************/
-/***************************    Test case   ***********************************/
-/******************************************************************************/
-
-int called = 0;
-void handler(int sig)
+static void handler()
 {
 	called = 1;
 }
 
-/* main function */
 int main(void)
 {
 	int ret;
-
 	struct sigaction sa;
-
-	/* Initialize output */
-	output_init();
 
 	/* Set the signal handler */
 	sa.sa_flags = 0;
@@ -102,38 +61,37 @@ int main(void)
 	ret = sigemptyset(&sa.sa_mask);
 
 	if (ret != 0) {
-		UNRESOLVED(ret, "Failed to empty signal set");
+		perror("Failed to empty signal set");
+		return PTS_UNRESOLVED;
 	}
 
 	/* Install the signal handler for SIGABRT */
-	ret = sigaction(SIGNAL, &sa, 0);
+	ret = sigaction(SIGABRT, &sa, 0);
 
 	if (ret != 0) {
-		UNRESOLVED(ret, "Failed to set signal handler");
+		perror("Failed to set signal handler");
+		return PTS_UNRESOLVED;
 	}
 
 	if (called) {
-		FAILED
-		    ("The signal handler has been called when no signal was raised");
+		fprintf(stderr,
+			"The signal handler has been called before signal was raised");
+		return PTS_FAIL;
 	}
 
-	ret = raise(SIGNAL);
+	ret = raise(SIGABRT);
 
 	if (ret != 0) {
-		UNRESOLVED(ret, "Failed to raise SIGABRT");
+		perror("Failed to raise SIGABRT");
+		return PTS_UNRESOLVED;
 	}
 
 	if (!called) {
-		FAILED
-		    ("the sa_handler was not called whereas SA_SIGINFO was not set");
+		fprintf(stderr, "The sa_handler was not called\n");
+		return PTS_FAIL;
 	}
 
-	/* Test passed */
-#if VERBOSE > 0
+	printf("Test PASSED\n");
 
-	output("Test passed\n");
-
-#endif
-
-	PASSED;
+	return PTS_PASS;
 }
