261e997abb5e29b85038fcc3ea21d2a6d4b4a520
Cyril Hrubis <chrubis@suse.cz>
2013-04-02 15:36:59 +0200

    open_posix_testsuite/.../aio_suspend/9-1.c
    Fix the testcase to adjust aio request sizes as runtime.
    
    This testcase fails on machines that are fast enough to complete
    the hardcoded request size before we get to the aio_suspend(). This
    patch changes the testcase to do several tries with increasing buffer
    sizes in order to cover both small embedded systems and large
    mainframes.
    
    There were several race conditions in the original code that had to be
    fixed in order to be able to run the test repeatedly.
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/open_posix_testsuite/conformance/interfaces/aio_suspend/9-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/aio_suspend/9-1.c
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2004, Bull SA. All rights reserved.
- * Created by:  Laurent.Vivier@bull.net
+ *  Created by:  Laurent.Vivier@bull.net
+ * Copyright (c) 2013 Cyril Hrubis <chrubis@suse.cz>
+ *
  * This file is licensed under the GPL license.  For the full content
  * of this license, see the COPYING file at the top level of this
  * source tree.
@@ -10,7 +12,7 @@
  * assertion:
  *
  *	aio_suspend() shall fail if:
-	[EAGAIN] No AIO indicated in the list completed before timeout
+ *	[EAGAIN] No AIO indicated in the list completed before timeout
  *
  * method:
  *
@@ -35,37 +37,28 @@
 
 #include "posixtest.h"
 
-#define TNAME "aio_suspend/9-1.c"
-
-#define NUM_AIOCBS	10
-#define BUF_SIZE	(1024*1024)
 #define WAIT_FOR_AIOCB	6
 
-static int received_all;
+static sig_atomic_t received_all;
 
-static void sigrt1_handler(int signum, siginfo_t *info, void *context)
+static void sigrt1_handler()
 {
 	received_all = 1;
 }
 
-int main(void)
+static int do_test(int num_aiocbs, size_t buf_size)
 {
 	char tmpfname[256];
 	int fd;
-
-	struct aiocb **aiocbs;
+	struct aiocb *aiocbs[num_aiocbs];
 	struct aiocb *plist[2];
 	char *bufs;
 	struct sigaction action;
 	struct sigevent event;
-	struct timespec ts = { 0, 1000000 };	/* 1 ms */
-	int errors = 0;
-	int ret;
-	int err;
+	struct timespec ts = {0, 1000000};	/* 1 ms */
+	int ret, ret2;
+	int err = PTS_UNRESOLVED;
 	int i;
-
-	if (sysconf(_SC_ASYNCHRONOUS_IO) < 200112L)
-		return PTS_UNSUPPORTED;
 
 	snprintf(tmpfname, sizeof(tmpfname), "/tmp/pts_aio_suspend_9_1_%d",
 		 getpid());
@@ -74,41 +67,47 @@ int main(void)
 	fd = open(tmpfname, O_CREAT | O_RDWR | O_EXCL, S_IRUSR | S_IWUSR);
 
 	if (fd == -1) {
-		printf(TNAME " Error at open(): %s\n", strerror(errno));
-		exit(PTS_UNRESOLVED);
+		printf("Error at open(): %s\n", strerror(errno));
+		goto err0;
 	}
 
 	unlink(tmpfname);
 
-	bufs = malloc(NUM_AIOCBS * BUF_SIZE);
+	int file_size = num_aiocbs * buf_size;
+
+	bufs = malloc(file_size);
 
 	if (bufs == NULL) {
-		printf(TNAME " Error at malloc(): %s\n", strerror(errno));
-		close(fd);
-		exit(PTS_UNRESOLVED);
+		printf("Error at malloc(): %s\n", strerror(errno));
+		goto err1;
 	}
 
-	if (write(fd, bufs, NUM_AIOCBS * BUF_SIZE) != (NUM_AIOCBS * BUF_SIZE)) {
-		printf(TNAME " Error at write(): %s\n", strerror(errno));
-		free(bufs);
-		close(fd);
-		exit(PTS_UNRESOLVED);
-	}
+	ret = write(fd, bufs, file_size);
+	if (ret != file_size) {
 
-	aiocbs = malloc(sizeof(struct aiocb *) * NUM_AIOCBS);
+		if (ret < 0)
+			printf("Error at write(): %s\n", strerror(errno));
+		else
+			printf("Error at write(): %i of %i written\n",
+			       ret, file_size);
+
+		goto err2;
+	}
 
 	/* Queue up a bunch of aio reads */
-	for (i = 0; i < NUM_AIOCBS; i++) {
-
+	for (i = 0; i < num_aiocbs; i++) {
 		aiocbs[i] = malloc(sizeof(struct aiocb));
 		memset(aiocbs[i], 0, sizeof(struct aiocb));
 
 		aiocbs[i]->aio_fildes = fd;
-		aiocbs[i]->aio_offset = i * BUF_SIZE;
-		aiocbs[i]->aio_buf = &bufs[i * BUF_SIZE];
-		aiocbs[i]->aio_nbytes = BUF_SIZE;
+		aiocbs[i]->aio_offset = i * buf_size;
+		aiocbs[i]->aio_buf = &bufs[i * buf_size];
+		aiocbs[i]->aio_nbytes = buf_size;
 		aiocbs[i]->aio_lio_opcode = LIO_READ;
 	}
+
+	/* reset the completion flag */
+	received_all = 0;
 
 	/* Use SIGRTMIN+1 for list completion */
 	event.sigev_notify = SIGEV_SIGNAL;
@@ -125,82 +124,91 @@ int main(void)
 	plist[1] = aiocbs[WAIT_FOR_AIOCB];
 
 	/* Submit request list */
-	ret = lio_listio(LIO_NOWAIT, aiocbs, NUM_AIOCBS, &event);
+	ret = lio_listio(LIO_NOWAIT, aiocbs, num_aiocbs, &event);
 
 	if (ret) {
-		printf(TNAME " Error at lio_listio() %d: %s\n",
+		printf(" Error at lio_listio() %d: %s\n",
 		       errno, strerror(errno));
-		for (i = 0; i < NUM_AIOCBS; i++)
-			free(aiocbs[i]);
-		free(bufs);
-		free(aiocbs);
-		close(fd);
-		exit(PTS_UNRESOLVED);
-	}
-
-	/* Check selected request has not completed yet */
-	err = aio_error(aiocbs[WAIT_FOR_AIOCB]);
-	if (err != EINPROGRESS) {
-		printf(TNAME " Error : AIOCB %d already completed"
-		       " before suspend\n", WAIT_FOR_AIOCB);
-		for (i = 0; i < NUM_AIOCBS; i++)
-			free(aiocbs[i]);
-		free(bufs);
-		free(aiocbs);
-		close(fd);
-		exit(PTS_FAIL);
+		goto err3;
 	}
 
 	/* Suspend on selected request */
 	ret = aio_suspend((const struct aiocb **)plist, 2, &ts);
+
+	/* Check selected request has not completed yet */
+	ret2 = aio_error(aiocbs[WAIT_FOR_AIOCB]);
+	if (ret2 != EINPROGRESS) {
+		/*
+		 * The operation was too fast, wait for completion
+		 * and redo it with larger buffers.
+		 */
+		err = -1;
+		goto err4;
+	}
+
 	/* timed out aio_suspend should return -1 and set errno to EAGAIN */
 	if (ret != -1) {
-		printf(TNAME " aio_suspend() should return -1\n");
-		for (i = 0; i < NUM_AIOCBS; i++)
-			free(aiocbs[i]);
-		free(bufs);
-		free(aiocbs);
-		close(fd);
-		exit(PTS_FAIL);
+		printf("aio_suspend() should return -1\n");
+		err = PTS_FAIL;
+		goto err4;
 	}
 	if (errno != EAGAIN) {
-		printf(TNAME " aio_suspend() should set errno to EAGAIN:"
-		       " %d (%s)\n", errno, strerror(errno));
-		for (i = 0; i < NUM_AIOCBS; i++)
-			free(aiocbs[i]);
-		free(bufs);
-		free(aiocbs);
-		close(fd);
-		exit(PTS_FAIL);
+		printf("aio_suspend() should set errno to EAGAIN: %d (%s)\n",
+		       errno, strerror(errno));
+		err = PTS_FAIL;
+		goto err4;
 	}
 
 	/* Wait for list processing completion */
 	while (!received_all)
-		sleep(1);
+		usleep(10000);
 
-	/* Check return code and free things */
-	for (i = 0; i < NUM_AIOCBS; i++) {
+	/* Check return values and errors */
+	err = PTS_PASS;
+
+	for (i = 0; i < num_aiocbs; i++) {
 		err = aio_error(aiocbs[i]);
 		ret = aio_return(aiocbs[i]);
 
-		if ((err != 0) && (ret != BUF_SIZE)) {
-			printf(TNAME " req %d: error = %d - return = %d\n",
+		if ((err != 0) && ((size_t)ret != buf_size)) {
+			printf("req %d: error = %d - return = %d\n",
 			       i, err, ret);
-			errors++;
+			err = PTS_FAIL;
 		}
-
-		free(aiocbs[i]);
 	}
 
+err4:
+	while (!received_all)
+		usleep(10000);
+err3:
+	for (i = 0; i < num_aiocbs; i++)
+		free(aiocbs[i]);
+err2:
 	free(bufs);
-	free(aiocbs);
-
+err1:
 	close(fd);
+err0:
+	return err;
+}
 
-	if (errors != 0)
-		exit(PTS_FAIL);
+int main(void)
+{
+	int aio_cbs = 10;
+	int buf_size = 1024 * 64;
+	int ret;
 
-	printf(TNAME " PASSED\n");
+	if (sysconf(_SC_ASYNCHRONOUS_IO) < 200112L)
+		return PTS_UNSUPPORTED;
 
+	/* Repeat the test with increasing buffer size */
+	do {
+		ret = do_test(aio_cbs, buf_size);
+		buf_size += buf_size / 4;
+	} while (ret == -1);
+
+	if (ret != 0)
+		return ret;
+
+	printf("(buf_size = %i)\nTest PASSED\n", buf_size);
 	return PTS_PASS;
 }
