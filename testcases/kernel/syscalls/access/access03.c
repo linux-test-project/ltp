41dae8917c820defbc06fcaf61c807072ed13549
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2013-04-26 14:55:25 +0800

    access03: code cleanups
    Cleanup the code.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/kernel/syscalls/access/access03.c
+++ testcases/kernel/syscalls/access/access03.c
@@ -20,82 +20,11 @@
  * with this program; if not, write the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
  */
-/* $Id: access03.c,v 1.8 2009/03/23 13:35:39 subrata_modak Exp $ */
-/**********************************************************
- *
- *	OS Test - Silicon Graphics, Inc.
- *
- *	TEST IDENTIFIER	: access03
- *
- *	EXECUTED BY	: anyone
- *
- *	TEST TITLE	: EFAULT error testing for access(2)
- *
- *	PARENT DOCUMENT	: acstds01
- *
- *	TEST CASE TOTAL	: 8
- *
- *	WALL CLOCK TIME	: 1
- *
- *	CPU TYPES		: ALL
- *
- *	AUTHOR		: Kathy Olmsted
- *
- *	CO-PILOT		: Tom Hampson
- *
- *	DATE STARTED	: 05/13/92
- *
- *	INITIAL RELEASE	: UNICOS 7.0
- *
- *	TEST CASES
- *
- *	access(2) test for errno(s) EFAULT.
- *
- *	INPUT SPECIFICATIONS
- *	The standard options for system call tests are accepted.
- *	(See the parse_opts(3) man page).
- *
- *	DURATION
- *	Terminates - with frequency and infinite modes.
- *
- *	SIGNALS
- *	Uses SIGUSR1 to pause before test if option set.
- *	(See the parse_opts(3) man page).
- *
- *	ENVIRONMENTAL NEEDS
- *	  No run-time environmental needs.
- *
- *	DETAILED DESCRIPTION
- *
- *	Setup:
- *	  Setup signal handling.
- *		Make and change to a temporary directory.
- *	  Pause for SIGUSR1 if option specified.
- *
- *	Test:
- *	 Loop if the proper options are given.
- *	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno.
- *		If doing functional test
- *			check the errno returned and print result message
- *
- *	Cleanup:
- *	  Print errno log and/or timing stats if options given
- *		Remove the temporary directory and exit.
- *
- *
- *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#**/
+
+/*
+ * access(2) test for errno(s) EFAULT.
+ */
 
 #include <errno.h>
 #include <string.h>
@@ -115,8 +44,8 @@ int TST_TOTAL = 8;
 int exp_enos[] = { EFAULT, 0 };	/* List must end with 0 */
 
 /* XXX (garrcoop): uh, this isn't a bad address yo. */
-void *low_addr;
-void *high_addr;
+static void *low_addr;
+static void *high_addr;
 
 #if !defined(UCLINUX)
 
@@ -125,7 +54,8 @@ int main(int ac, char **av)
 	int lc;
 	char *msg;
 
-	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
+	msg = parse_opts(ac, av, NULL, NULL);
+	if (msg != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
 	setup();
@@ -134,6 +64,7 @@ int main(int ac, char **av)
 	TEST_EXP_ENOS(exp_enos);
 
 #define TEST_ACCESS(addr, mode) \
+{ \
 	if (access(low_addr, mode) == -1) { \
 		if (errno == EFAULT) { \
 			tst_resm(TPASS, \
@@ -147,10 +78,10 @@ int main(int ac, char **av)
 	} else { \
 		tst_resm(TFAIL, \
 		    "access(%p, %s) succeeded unexpectedly", addr, #mode); \
-	}
+	} \
+}
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
 		tst_count = 0;
 
 		TEST_ACCESS(low_addr, R_OK);
@@ -162,25 +93,22 @@ int main(int ac, char **av)
 		TEST_ACCESS(high_addr, W_OK);
 		TEST_ACCESS(high_addr, X_OK);
 		TEST_ACCESS(high_addr, F_OK);
-
 	}
 
 	cleanup();
 	tst_exit();
-
 }
 
-void setup()
+static void setup(void)
 {
-
 	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
 	TEST_PAUSE;
 
 	low_addr = mmap(0, 1, PROT_NONE,
 			MAP_PRIVATE_EXCEPT_UCLINUX | MAP_ANONYMOUS, 0, 0);
 	if (low_addr == MAP_FAILED)
 		tst_brkm(TBROK | TERRNO, NULL, "mmap failed");
+
 	high_addr = get_high_address();
 	if (high_addr == NULL)
 		tst_brkm(TBROK | TERRNO, NULL, "get_high_address failed");
@@ -189,14 +117,9 @@ void setup()
 	tst_tmpdir();
 }
 
-/***************************************************************
- * cleanup() - performs all ONE TIME cleanup for this test at
- *		completion or premature exit.
- ***************************************************************/
-void cleanup()
+static void cleanup(void)
 {
 	TEST_CLEANUP;
-
 	tst_rmdir();
 }
 
