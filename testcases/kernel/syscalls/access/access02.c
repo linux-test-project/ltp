332fe2d0634d9ed972169bdb8bbd080fd00817eb
Wanlong Gao <gaowanlong@cn.fujitsu.com>
2013-04-26 14:55:25 +0800

    access02: code cleanups
    Cleanup the code.
    
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/kernel/syscalls/access/access02.c
+++ testcases/kernel/syscalls/access/access02.c
@@ -18,8 +18,6 @@
  */
 
 /*
- * Test Name: access02
- *
  * Test Description:
  *  Verify that access() succeeds to check the read/write/execute permissions
  *  on a file if the mode argument passed was R_OK/W_OK/X_OK.
@@ -27,46 +25,7 @@
  *  Also verify that, access() succeeds to test the accessibility of the file
  *  referred to by symbolic link if the pathname points to a symbolic link.
  *
- * Expected Result:
- *  access() should return 0 value and the specified file should have
- *  respective permissions set on it.
- *
- * Algorithm:
- *  Setup:
- *   Setup signal handling.
- *   Create temporary directory.
- *   Pause for SIGUSR1 if option specified.
- *
- *  Test:
- *   Loop if the proper options are given.
- *   Execute system call
- *   Check return code, if system call failed (return=-1)
- *	Log the errno and Issue a FAIL message.
- *   Otherwise,
- *	Verify the Functionality of system call
- *      if successful,
- *		Issue Functionality-Pass message.
- *      Otherwise,
- *		Issue Functionality-Fail message.
- *  Cleanup:
- *   Print errno log and/or timing stats if options given
- *   Delete the temporary directory created.
- *
- * Usage:  <for command-line>
- *  access02 [-c n] [-f] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -f   : Turn off functionality Testing.
- *	       -i n : Execute test n times.
- *	       -I x : Execute test for x seconds.
- *	       -P x : Pause for x seconds between iterations.
- *	       -t   : Turn on syscall timing.
- *
- * HISTORY
  *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS:
- *  None.
- *
  */
 
 #include <sys/types.h>
@@ -92,54 +51,47 @@
 
 char *TCID = "access02";
 int TST_TOTAL = 4;
-int fd1, fd2, fd4;		/* file descriptor for testfile(s) */
-char nobody_uid[] = "nobody";
-struct passwd *ltpuser;
 
-int setup1();			/* setup() to test access() for R_OK */
-int setup2();			/* setup() to test access() for W_OK */
-int setup3();			/* setup() to test access() for X_OK */
-int setup4();			/* setup() to test access() on symlink file */
+static int fd1, fd2, fd4;
+static const char nobody_uid[] = "nobody";
+static struct passwd *ltpuser;
 
-struct test_case_t {		/* test case structure */
+static int setup1(void);
+static int setup2(void);
+static int setup3(void);
+static int setup4(void);
+
+static struct test_case_t {
 	char *pathname;
 	mode_t a_mode;
-	int (*setupfunc) ();
+	int (*setupfunc) (void);
 } test_cases[] = {
-	/* Read access */
-	{
-	TEST_FILE1, R_OK, setup1},
-	    /* Write access */
-	{
-	TEST_FILE2, W_OK, setup2},
-	    /* Execute access */
-	{
-	TEST_FILE3, X_OK, setup3},
-	    /* Symlink */
-	{
-SYM_FILE, W_OK, setup4},};
+	{TEST_FILE1, R_OK, setup1},
+	{TEST_FILE2, W_OK, setup2},
+	{TEST_FILE3, X_OK, setup3},
+	{SYM_FILE, W_OK, setup4},
+};
 
-void setup();
-void cleanup();
-int access_verify(int);
+static void setup(void);
+static void cleanup(void);
+static int access_verify(int);
 
 int main(int ac, char **av)
 {
 	int lc;
 	int i;
 	char *msg;
-	mode_t access_mode;	/* specified access mode for testfile */
-	char *file_name;	/* name of the testfile */
+	mode_t access_mode;
+	char *file_name;
 
-	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
+	msg = parse_opts(ac, av, NULL, NULL);
+	if (msg != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
 		tst_count = 0;
-
 		for (i = 0; i < TST_TOTAL; i++) {
 			file_name = test_cases[i].pathname;
 			access_mode = test_cases[i].a_mode;
@@ -157,24 +109,22 @@ int main(int ac, char **av)
 				continue;
 			}
 
-			if (STD_FUNCTIONAL_TEST) {
+			if (STD_FUNCTIONAL_TEST)
 				access_verify(i);
-			} else
+			else
 				tst_resm(TPASS, "call succeeded");
 		}
 	}
 
 	cleanup();
-
 	tst_exit();
 }
 
-void setup()
+static void setup(void)
 {
 	int i;
 
 	tst_require_root(NULL);
-
 	tst_sig(FORK, DEF_HANDLER, cleanup);
 
 	ltpuser = getpwnam(nobody_uid);
@@ -184,7 +134,6 @@ void setup()
 		tst_brkm(TINFO | TERRNO, NULL, "setuid failed");
 
 	TEST_PAUSE;
-
 	tst_tmpdir();
 
 	for (i = 0; i < TST_TOTAL; i++)
@@ -198,12 +147,12 @@ void setup()
  *   Creat/open a testfile and write some data into it.
  *   This function returns 0.
  */
-int setup1()
+static int setup1(void)
 {
 	char write_buf[] = "abc";
 
-	/* Creat a test file under above directory created */
-	if ((fd1 = open(TEST_FILE1, O_RDWR | O_CREAT, FILE_MODE)) == -1) {
+	fd1 = open(TEST_FILE1, O_RDWR | O_CREAT, FILE_MODE);
+	if (fd1 == -1) {
 		tst_brkm(TBROK | TERRNO, cleanup,
 			 "open(%s, O_RDWR|O_CREAT, %#o) failed",
 			 TEST_FILE1, FILE_MODE);
@@ -225,10 +174,10 @@ int setup1()
  *   Creat/open a testfile for writing under temporary directory.
  *   This function returns 0.
  */
-int setup2()
+static int setup2(void)
 {
-	/* Creat a test file under temporary directory */
-	if ((fd2 = open(TEST_FILE2, O_RDWR | O_CREAT, FILE_MODE)) == -1) {
+	fd2 = open(TEST_FILE2, O_RDWR | O_CREAT, FILE_MODE);
+	if (fd1 == -1) {
 		tst_brkm(TBROK | TERRNO, cleanup,
 			 "open(%s, O_RDWR|O_CREAT, %#o) failed",
 			 TEST_FILE2, FILE_MODE);
@@ -244,15 +193,15 @@ int setup2()
  *   Creat/open a testfile and provide execute permissions to it.
  *   This function returns 0.
  */
-int setup3()
+static int setup3(void)
 {
-	int fd3;		/* File handle for test file */
+	int fd3;
 #ifdef UCLINUX
 	char exechead[] = "#!/bin/sh\n";
 #endif
 
-	/* Creat a test file under temporary directory */
-	if ((fd3 = open(TEST_FILE3, O_RDWR | O_CREAT, FILE_MODE)) == -1) {
+	fd3 = open(TEST_FILE3, O_RDWR | O_CREAT, FILE_MODE);
+	if (fd3 == -1) {
 		tst_brkm(TBROK | TERRNO, cleanup,
 			 "open(%s, O_RDWR|O_CREAT, %#o) failed",
 			 TEST_FILE3, FILE_MODE);
@@ -287,10 +236,10 @@ int setup3()
  *   Creat a symbolic link of temporary file.
  *   This function returns 0.
  */
-int setup4()
+static int setup4(void)
 {
-	/* Creat a temporary  file under temporary directory */
-	if ((fd4 = open(TEMP_FILE, O_RDWR | O_CREAT, FILE_MODE)) == -1) {
+	fd4 = open(TEMP_FILE, O_RDWR | O_CREAT, FILE_MODE);
+	if (fd4 == -1) {
 		tst_brkm(TBROK | TERRNO, cleanup,
 			 "open(%s, O_RDWR|O_CREAT, %#o) failed",
 			 TEMP_FILE, FILE_MODE);
@@ -311,7 +260,7 @@ int setup4()
  *	This function verify the accessibility of the
  *	the testfile with the one verified by access().
  */
-int access_verify(int i)
+static int access_verify(int i)
 {
 	char write_buf[] = "abc";
 	char read_buf[BUFSIZ];
@@ -320,40 +269,48 @@ int access_verify(int i)
 	rval = 0;
 
 	switch (i) {
-	case 0:		/*
-				 * The specified file has read access.
-				 * Attempt to read some data from the testfile
-				 * and if successful, access() behaviour is
-				 * correct.
-				 */
-		if ((rval = read(fd1, &read_buf, sizeof(read_buf))) == -1)
+	case 0:
+		/*
+		 * The specified file has read access.
+		 * Attempt to read some data from the testfile
+		 * and if successful, access() behaviour is
+		 * correct.
+		 */
+		rval = read(fd1, &read_buf, sizeof(read_buf));
+		if (rval == -1)
 			tst_resm(TFAIL | TERRNO, "read(%s) failed", TEST_FILE1);
 		break;
-	case 1:		/*
-				 * The specified file has write access.
-				 * Attempt to write some data to the testfile
-				 * and if successful, access() behaviour is correct.
-				 */
-		if ((rval = write(fd2, write_buf, strlen(write_buf))) == -1)
+	case 1:
+		/*
+		 * The specified file has write access.
+		 * Attempt to write some data to the testfile
+		 * and if successful, access() behaviour is correct.
+		 */
+		rval = write(fd2, write_buf, strlen(write_buf));
+		if (rval == -1)
 			tst_resm(TFAIL | TERRNO, "write(%s) failed",
 				 TEST_FILE2);
 		break;
-	case 2:		/*
-				 * The specified file has execute access.
-				 * Attempt to execute the specified executable
-				 * file, if successful, access() behaviour is correct.
-				 */
-		if ((rval = system("./" TEST_FILE3)) != 0)
+	case 2:
+		/*
+		 * The specified file has execute access.
+		 * Attempt to execute the specified executable
+		 * file, if successful, access() behaviour is correct.
+		 */
+		rval = system("./" TEST_FILE3);
+		if (rval != 0)
 			tst_resm(TFAIL, "Fail to execute the %s", TEST_FILE3);
 		break;
-	case 3:		/*
-				 * The file pointed to by symbolic link has
-				 * write access.
-				 * Attempt to write some data to this temporary file
-				 * pointed to by symlink. if successful, access() bahaviour
-				 * is correct.
-				 */
-		if ((rval = write(fd4, write_buf, strlen(write_buf))) == -1)
+	case 3:
+		/*
+		 * The file pointed to by symbolic link has
+		 * write access.
+		 * Attempt to write some data to this temporary file
+		 * pointed to by symlink. if successful, access() bahaviour
+		 * is correct.
+		 */
+		rval = write(fd4, write_buf, strlen(write_buf));
+		if (rval == -1)
 			tst_resm(TFAIL | TERRNO, "write(%s) failed", TEMP_FILE);
 		break;
 	default:
@@ -363,18 +320,8 @@ int access_verify(int i)
 	return rval;
 }
 
-/*
- * cleanup() - performs all ONE TIME cleanup for this test at
- *             completion or premature exit.
- *
- *  Remove the test directory and testfile created in the setup.
- */
-void cleanup()
+static void cleanup(void)
 {
-	/*
-	 * print timing stats if that option was specified.
-	 * print errno log if that option was specified.
-	 */
 	TEST_CLEANUP;
 
 	/* Close the testfile(s) created in the setup()s */
@@ -385,10 +332,5 @@ void cleanup()
 	if (close(fd4) == -1)
 		tst_brkm(TFAIL | TERRNO, NULL, "close(%s) failed", TEMP_FILE);
 
-	/*
-	 * Delete the test directory/file and temporary directory
-	 * created in the setup.
-	 */
 	tst_rmdir();
-
 }
