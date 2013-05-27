c055410d2cc0cb94a017da82f6db3d726870432d
Shuang Qiu <shuang.qiu@oracle.com>
2013-05-27 14:29:42 +0800

    Unmap the mapped address region
    It does not unmap the mapped address region,so we sometimes still get
    such
    warning when cleanup with nfs as TMPDIR although the file descriptor is
    closed:
    0  TWARN  :  tst_rmdir: rmobj(/mnt/nfsv3/ltp-MOeIFH7102/vma4FwRqb)
    failed:
    unlink(/mnt/nfsv3/ltp-MOeIFH7102/vma4FwRqb/.nfs000000000110cb3600000001)
    failed; errno=16: Device or resource busy
    
    Signed-off-by: Shuang Qiu <shuang.qiu@oracle.com>
    Reviewed-by: Jan Stancek <jstancek@redhat.com>
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

--- testcases/kernel/mem/vma/vma03.c
+++ testcases/kernel/mem/vma/vma03.c
@@ -101,8 +101,10 @@ int main(int argc, char *argv[])
 				tst_resm(TPASS, "mremap failed as expected.");
 			else
 				tst_resm(TFAIL | TERRNO, "mremap");
+			munmap(map, pgsz);
 		} else {
 			tst_resm(TFAIL, "mremap succeeded unexpectedly.");
+			munmap(remap, 2 * pgsz);
 		}
 
 		close(fd);
