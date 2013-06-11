208669109e427ee358285eba4073b5b07d475560
Alexey Kodanev <alexey.kodanev@oracle.com>
2013-06-11 13:56:10 +0400

    prot_hsymlinks & cgroup_xattr: fix copyright header
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/kernel/security/prot_hsymlinks/prot_hsymlinks.c
+++ testcases/kernel/security/prot_hsymlinks/prot_hsymlinks.c
@@ -3,7 +3,8 @@
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it would be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
