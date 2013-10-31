e052e107bef9ff2b32a79d636d14ae5cf4194f23
Zeng Linggang <zenglg.jy@cn.fujitsu.com>
2013-10-31 16:47:28 +0800

    acct/acct01.c: Add ELOOP ENAMETOOLONG EROFS
    Add ELOOP, ENAMETOOLONG, EROFS error number test in acct01.c for acct(2)
    
    Signed-off-by: Zeng Linggang <zenglg.jy@cn.fujitsu.com>

--- testcases/kernel/syscalls/acct/acct01.c
+++ testcases/kernel/syscalls/acct/acct01.c
@@ -34,22 +34,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/mount.h>
 
 #include "test.h"
 #include "usctest.h"
 #include "safe_macros.h"
 
+#define DIR_MODE	(S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP| \
+			 S_IXGRP|S_IROTH|S_IXOTH)
 #define TEST_FILE1	"/"
 #define TEST_FILE2	"/dev/null"
 #define TEST_FILE3	"/tmp/does/not/exist"
 #define TEST_FILE4	"/etc/fstab/"
 #define TEST_FILE5	"./tmpfile"
+#define TEST_FILE6	"test_file_eloop1"
+#define TEST_FILE7	nametoolong
+#define TEST_FILE8	"mntpoint/tmp"
+
+static char nametoolong[PATH_MAX+2];
+static char *fstype = "ext2";
+static char *device;
+static int dflag;
+static int mount_flag;
 
 static void setup(void);
 static void cleanup(void);
 static void setup2(void);
 static void cleanup2(void);
 static void acct_verify(int);
+static void help(void);
+
+static option_t options[] = {
+	{"T:", NULL, &fstype},
+	{"D:", &dflag, &device},
+	{NULL, NULL, NULL}
+};
 
 static struct test_case_t {
 	char *filename;
@@ -64,17 +83,32 @@ static struct test_case_t {
 	{TEST_FILE4, "ENOTDIR", ENOTDIR, NULL,   NULL},
 	{TEST_FILE5, "EPERM",   EPERM,   setup2, cleanup2},
 	{NULL,       "EPERM",   EPERM,   setup2, cleanup2},
+	{TEST_FILE6, "ELOOP",        ELOOP,        NULL, NULL},
+	{TEST_FILE7, "ENAMETOOLONG", ENAMETOOLONG, NULL, NULL},
+	{TEST_FILE8, "EROFS",        EROFS,        NULL, NULL},
 };
 
 char *TCID = "acct01";
 int TST_TOTAL = ARRAY_SIZE(test_cases);
 static struct passwd *ltpuser;
-static int exp_enos[] = { EISDIR, EACCES, ENOENT, ENOTDIR, EPERM, 0 };
+static int exp_enos[] = { EISDIR, EACCES, ENOENT, ENOTDIR, EPERM,
+			  ELOOP, ENAMETOOLONG, EROFS, 0 };
 
 int main(int argc, char *argv[])
 {
 	int lc;
+	char *msg;
 	int i;
+
+	msg = parse_opts(argc, argv, options, help);
+	if (msg != NULL)
+		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
+
+	/* Check for mandatory option of the testcase */
+	if (!dflag) {
+		tst_brkm(TBROK, NULL, "you must specify the device used for "
+			 "mounting with -D option");
+	}
 
 	setup();
 
@@ -123,6 +157,31 @@ static void setup(void)
 			tst_brkm(TBROK | TERRNO, cleanup, "acct(NULL) failed");
 		}
 	}
+
+	/* ELOOP SETTING */
+	SAFE_SYMLINK(cleanup, TEST_FILE6, "test_file_eloop2");
+	SAFE_SYMLINK(cleanup, "test_file_eloop2", TEST_FILE6);
+
+	/* ENAMETOOLONG SETTING */
+	memset(nametoolong, 'a', PATH_MAX+1);
+
+	/* EROFS SETTING */
+	tst_mkfs(NULL, device, fstype, NULL);
+	SAFE_MKDIR(cleanup, "mntpoint", DIR_MODE);
+	if (mount(device, "mntpoint", fstype, 0, NULL) < 0) {
+		tst_brkm(TBROK | TERRNO, cleanup,
+			 "mount device:%s failed", device);
+	}
+	mount_flag = 1;
+	/* Create a file in the file system, then remount it as read-only */
+	fd = SAFE_CREAT(cleanup, TEST_FILE8, 0644);
+	SAFE_CLOSE(cleanup, fd);
+	if (mount(device, "mntpoint", fstype,
+		  MS_REMOUNT | MS_RDONLY, NULL) < 0) {
+		tst_brkm(TBROK | TERRNO, cleanup,
+			 "mount device:%s failed", device);
+	}
+	mount_flag = 1;
 }
 
 static void acct_verify(int i)
@@ -141,6 +200,7 @@ static void acct_verify(int i)
 			 test_cases[i].filename);
 		return;
 	}
+
 	if (TEST_ERRNO == test_cases[i].exp_errno) {
 		tst_resm(TPASS | TTERRNO, "acct failed as expected");
 	} else {
@@ -167,6 +227,17 @@ static void cleanup(void)
 
 	if (acct(NULL) == -1)
 		tst_resm(TBROK | TERRNO, "acct(NULL) failed");
+	if (mount_flag && umount("mntpoint") < 0) {
+		tst_brkm(TBROK | TERRNO, NULL,
+			 "umount device:%s failed", device);
+	}
 
 	tst_rmdir();
+}
+
+static void help(void)
+{
+	printf("-T type   : specifies the type of filesystem to be mounted. "
+	       "Default ext2.\n");
+	printf("-D device : device used for mounting.\n");
 }
