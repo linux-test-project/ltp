e4a85db28bf0adcc5335c5f6073c12d3fda5bfd1
Cyril Hrubis <chrubis@suse.cz>
2013-11-21 11:23:34 +0100

    clock_getres/clock_getres01.c: Finish the cleanup
    * Cleanup the GPL headers
    
    * Add standard option parsing code
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/clock_getres/clock_getres01.c
+++ testcases/kernel/syscalls/clock_getres/clock_getres01.c
@@ -1,48 +1,32 @@
-/******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007-2008 ,Hitachi, Ltd		*/
-/*	  Author(s): Takahiro Yasui <takahiro.yasui.mp@hitachi.com>,	      */
-/*		       Yumiko Sugita <yumiko.sugita.yf@hitachi.com>, 	      */
-/*		       Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>	      */
-/*								  	      */
-/* This program is free software;  you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or	  */
-/* (at your option) any later version.					*/
-/*									    */
-/* This program is distributed in the hope that it will be useful,	    */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of	    */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		  */
-/* the GNU General Public License for more details.			   */
-/*									    */
-/* You should have received a copy of the GNU General Public License	  */
-/* along with this program;  if not, write to the Free Software Foundation, */
-/* Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA         */
-/*									    */
-/******************************************************************************/
-/******************************************************************************/
-/*									    */
-/* File:	clock_getres01.c					      */
-/*									    */
-/* Description: This tests the clock_getres() syscall			 */
-/*									      */
-/* 									      */
-/*									      */
-/*									      */
-/*									      */
-/*									    */
-/* Usage:  <for command-line>						 */
-/* clock_getres01 [-c n] [-e][-i n] [-I x] [-p x] [-t]			*/
-/*      where,  -c n : Run n copies concurrently.			     */
-/*	      -e   : Turn on errno logging.				 */
-/*	      -i n : Execute test n times.				  */
-/*	      -I x : Execute test for x seconds.			    */
-/*	      -P x : Pause for x seconds between iterations.		*/
-/*	      -t   : Turn on syscall timing.				*/
-/*									    */
-/* Test Name:   clock_getres01						*/
-/* History:     Porting from Crackerjack to LTP is done by		    */
-/*	      Manas Kumar Nayak maknayak@in.ibm.com>			*/
-/******************************************************************************/
+/******************************************************************************
+ * Copyright (c) Crackerjack Project., 2007-2008 ,Hitachi, Ltd                *
+ *   Author(s): Takahiro Yasui <takahiro.yasui.mp@hitachi.com>,               *
+ *              Yumiko Sugita <yumiko.sugita.yf@hitachi.com>,                 *
+ *              Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>                  *
+ *   LTP authors:                                                             *
+ *              Manas Kumar Nayak maknayak@in.ibm.com>                        *
+ *              Zeng Linggang <zenglg.jy@cn.fujitsu.com>                      *
+ *              Cyril Hrubis <chrubis@suse.cz>                                *
+ *                                                                            *
+ * This program is free software;  you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  *
+ * the GNU General Public License for more details.                           *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program;  if not, write to the Free Software Foundation,   *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA           *
+ *                                                                            *
+ ******************************************************************************/
+/*
+ * Description: This tests the clock_getres() syscall
+ */
+
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <getopt.h>
@@ -94,6 +78,10 @@ int main(int ac, char **av)
 	int i;
 	int lc;
 	struct timespec res;
+	const char *msg;
+
+	if ((msg = parse_opts(ac, av, NULL, NULL)))
+		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
 	setup();
 
@@ -134,7 +122,6 @@ int main(int ac, char **av)
 	}
 
 	cleanup();
-
 	tst_exit();
 }
 
