9317d9c2848b93e1d138ec9cae9cee3208540a4d
Cyril Hrubis <chrubis@suse.cz>
2014-01-09 14:42:23 +0100

    lib/cloner.c: Use clone() on s390 and s390x too
    There is no reason to use undocumented __clone() on s390 and s390x
    as I haven't been able to find a distribution where clone() is not
    defined on these. I suppose that this is a leftover from early clone
    implementation days.
    
    Moreover it fixes build failure on recent enough s390 and s390x where
    clone accepts seven aruments. (i.e. SLES11 and newer)
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- lib/cloner.c
+++ lib/cloner.c
@@ -32,12 +32,8 @@
 
 #undef clone			/* we want to use clone() */
 
-#if defined (__s390__) || defined(__s390x__)
-#define clone __clone
-extern int __clone(int (void *), void *, int, void *);
-#elif defined(__ia64__)
+#if defined(__ia64__)
 #define clone2 __clone2
-
 extern int __clone2(int (*fn) (void *arg), void *child_stack_base,
 		    size_t child_stack_size, int flags, void *arg,
 		    pid_t *parent_tid, void *tls, pid_t *child_tid);
