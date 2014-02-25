d9d03d442b3cc94ee07169ce22715627d367bc47
Peng Haitao <penght@cn.fujitsu.com>
2014-02-25 09:34:15 +0800

    sigaction:16-1: leave it untested on the Linux kernel less than 2.6.22
    As the Linux signal(7) said:
    POSIX semaphore interfaces: sem_wait(3) and sem_timedwait(3) (since Linux 2.6.22; beforehand, always failed with EINTR).
    
    So, on Linux, we just leave it untested when the kernel version
    is less than 2.6.22 and user wants to filter it.
    
    Signed-off-by: Peng Haitao <penght@cn.fujitsu.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>
    Signed-off-by: Wanlong Gao <gaowanlong@cn.fujitsu.com>

---
+++ testcases/open_posix_testsuite/scripts/tst_kvercmp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+	echo "Usgae: ./tst_kvercmp.sh r1 r2 r3"
+	exit 1
+fi
+
+ker_ver=$(uname -r)
+r1=$(echo ${ker_ver} | awk -F. '{print $1}')
+r2=$(echo ${ker_ver} | awk -F. '{print $2}')
+r3=$(echo ${ker_ver} | awk -F. '{print $3}')
+r3=${r3%-*}
+
+test_ver=$(($1 * 65536 + $2 * 256 + $3))
+curr_ver=$((${r1} * 65536 + ${r2} * 256 + ${r3}))
+if [ ${curr_ver} -ge ${test_ver} ]; then
+	echo 0
+else
+	echo 1
+fi
