be8b8787369ebe8102632e71e83b6fa94e650b92
Cyril Hrubis <chrubis@suse.cz>
2014-03-18 17:55:52 +0100

    syscalls/mq_notify01: Cleanup.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/mq_notify/mq_notify01.c
+++ testcases/kernel/syscalls/mq_notify/mq_notify01.c
@@ -1,44 +1,30 @@
 /******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007-2008 ,Hitachi, Ltd		*/
+/* Copyright (c) Crackerjack Project., 2007-2008 ,Hitachi, Ltd		      */
 /*	  Author(s): Takahiro Yasui <takahiro.yasui.mp@hitachi.com>,	      */
-/*		       Yumiko Sugita <yumiko.sugita.yf@hitachi.com>, 	      */
+/*		       Yumiko Sugita <yumiko.sugita.yf@hitachi.com>,          */
 /*		       Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>	      */
-/*								  	      */
+/* Porting from Crackerjack to LTP is done by		                      */
+/*         Manas Kumar Nayak maknayak@in.ibm.com>			      */
+/*								              */
 /* This program is free software;  you can redistribute it and/or modify      */
 /* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or	  */
-/* (at your option) any later version.					*/
-/*									    */
-/* This program is distributed in the hope that it will be useful,	    */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of	    */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		  */
-/* the GNU General Public License for more details.			   */
-/*									    */
-/* You should have received a copy of the GNU General Public License	  */
-/* along with this program;  if not, write to the Free Software	       */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
-/*									    */
+/* the Free Software Foundation; either version 2 of the License, or	      */
+/* (at your option) any later version.					      */
+/*									      */
+/* This program is distributed in the hope that it will be useful,	      */
+/* but WITHOUT ANY WARRANTY;  without even the implied warranty of	      */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		      */
+/* the GNU General Public License for more details.			      */
+/*									      */
+/* You should have received a copy of the GNU General Public License	      */
+/* along with this program;  if not, write to the Free Software Foundation,   */
+/* Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA           */
+/*									      */
 /******************************************************************************/
 /******************************************************************************/
-/*									    */
-/* File:	mq_notify01.c						 */
-/*									    */
-/* Description: This tests the mq_notify() syscall			    */
-/*									    */
-/* Usage:  <for command-line>						 */
-/* mq_notify01 [-c n] [-e][-i n] [-I x] [-p x] [-t]			   */
-/*      where,  -c n : Run n copies concurrently.			     */
-/*	      -e   : Turn on errno logging.				 */
-/*	      -i n : Execute test n times.				  */
-/*	      -I x : Execute test for x seconds.			    */
-/*	      -P x : Pause for x seconds between iterations.		*/
-/*	      -t   : Turn on syscall timing.				*/
-/*									    */
-/* Total Tests: 1							     */
-/*									    */
-/* Test Name:   mq_notify01						   */
-/* History:     Porting from Crackerjack to LTP is done by		    */
-/*	      Manas Kumar Nayak maknayak@in.ibm.com>			*/
+/*									      */
+/* Description: This tests the mq_notify() syscall			      */
+/*									      */
 /******************************************************************************/
 #define _XOPEN_SOURCE 600
 #include <sys/syscall.h>
@@ -66,66 +52,20 @@ char *TCID = "mq_notify01";
 int testno;
 int TST_TOTAL = 1;
 
-/* Extern Global Functions */
-/******************************************************************************/
-/*									    */
-/* Function:    cleanup						       */
-/*									    */
-/* Description: Performs all one time clean up for this test on successful    */
-/*	      completion,  premature exit or  failure. Closes all temporary */
-/*	      files, removes all temporary directories exits the test with  */
-/*	      appropriate return code by calling tst_exit() function.       */
-/*									    */
-/* Input:       None.							 */
-/*									    */
-/* Output:      None.							 */
-/*									    */
-/* Return:      On failure - Exits calling tst_exit(). Non '0' return code.   */
-/*	      On success - Exits calling tst_exit(). With '0' return code.  */
-/*									    */
-/******************************************************************************/
-extern void cleanup()
+static void cleanup(void)
 {
-
 	TEST_CLEANUP;
 	tst_rmdir();
-
 }
 
-/* Local  Functions */
-/******************************************************************************/
-/*									    */
-/* Function:    setup							 */
-/*									    */
-/* Description: Performs all one time setup for this test. This function is   */
-/*	      typically used to capture signals, create temporary dirs      */
-/*	      and temporary files that may be used in the course of this    */
-/*	      test.							 */
-/*									    */
-/* Input:       None.							 */
-/*									    */
-/* Output:      None.							 */
-/*									    */
-/* Return:      On failure - Exits by calling cleanup().		      */
-/*	      On success - returns 0.				       */
-/*									    */
-/******************************************************************************/
-void setup()
+static void setup(void)
 {
-	/* Capture signals if any */
-	/* Create temporary directories */
 	TEST_PAUSE;
 	tst_tmpdir();
 }
 
-/*
- * Macros
- */
 #define SYSCALL_NAME    "mq_notify"
 
-/*
- * Global variables
- */
 static int opt_debug;
 static char *progname;
 static int notified;
@@ -139,9 +79,6 @@ enum test_type {
 	ALREADY_REGISTERED,
 };
 
-/*
- * Data Structure
- */
 struct test_case {
 	int notify;
 	int ttype;
@@ -152,14 +89,6 @@ struct test_case {
 #define MAX_MSGSIZE     8192
 #define MSG_SIZE	16
 #define USER_DATA       0x12345678
-
-/* Test cases
- *
- *   test status of errors on man page
- *
- *   EBADF	      v (not a valid descriptor)
- *   EBUSY	      v (process is already registered for notification)
-*/
 
 static struct test_case tcase[] = {
 	{			// case00
@@ -230,14 +159,6 @@ static void tfunc(union sigval sv)
 	cmp_ok = sv.sival_int == USER_DATA;
 	notified = 1;
 }
-
-/*
- * do_test()
- *
- *   Input  : TestCase Data
- *   Return : RESULT_OK(0), RESULT_NG(1)
- *
- */
 
 static int do_test(struct test_case *tc)
 {
@@ -364,10 +285,6 @@ EXIT:
 	return result;
 }
 
-/*
- * usage()
- */
-
 static void usage(const char *progname)
 {
 	tst_resm(TINFO, "usage: %s [options]", progname);
@@ -377,10 +294,6 @@ static void usage(const char *progname)
 	tst_resm(TINFO, "    -d --debug	   Show debug messages");
 	tst_resm(TINFO, "    -h --help	    Show this message");
 }
-
-/*
- * main()
- */
 
 int main(int ac, char **av)
 {
