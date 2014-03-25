9baa5e0c82407270d2349770e321259f0f42b890
Stanislav Kholmanskikh <stanislav.kholmanskikh@oracle.com>
2014-03-25 18:24:33 +0400

    rpc-tirpc: fixed the cleaner binaries
    Renamed cleaner.c.src to tirpc_cleaner.c (to make it build).
    
    For consistency added rpc_cleaner.c, which utilise only TS-RPC
    functions.
    
    Signed-off-by: Stanislav Kholmanskikh <stanislav.kholmanskikh@oracle.com>

---
+++ testcases/network/rpc/rpc-tirpc/tests_pack/rpc_cleaner.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2014 Oracle and/or its affiliates. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it would be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write the Free Software Foundation,
+ * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include <stdio.h>
+#include <rpc/rpc.h>
+
+#define VERSNUM 1 /* Default version used in the test binaries */
+
+int main(int argc, char *argv[])
+{
+	/* Server program number */
+	int prog_num = atoi(argv[1]);
+
+	pmap_unset(prog_num, VERSNUM);
+	svc_unregister(prog_num, VERSNUM);
+
+	return 0;
+}
