a0e86116da368d8815f86956ed8e7e3e07805dde
Cyril Hrubis <chrubis@suse.cz>
2014-05-15 13:21:05 +0200

    syscalls/openat01: Fixes.
    * Cleanup
    
    * Use tst_exit() instead of return 0
      (the test failure wasn't propagated out at all!)
    
    * Make use of tst_tmpdir()
    
    * Make use of SAFE_MACROS()
    
    * ...
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/openat/openat01.c
+++ testcases/kernel/syscalls/openat/openat01.c
@@ -1,44 +1,26 @@
 /******************************************************************************
  *
- *   Copyright (c) International Business Machines  Corp., 2006
+ * Copyright (c) International Business Machines  Corp., 2006
+ *  Author: Yi Yang <yyangcdl@cn.ibm.com>
+ * Copyright (c) Cyril Hrubis 2014 <chrubis@suse.cz>
  *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This program is free software;  you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * NAME
- *      openat01.c
+ * You should have received a copy of the GNU General Public License
+ * along with this program;  if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * DESCRIPTION
- *	This test case will verify basic function of openat
- *	added by kernel 2.6.16 or up.
- *
- * USAGE:  <for command-line>
- * openat01 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-p]
- * where:
- *      -c n : Run n copies simultaneously.
- *      -e   : Turn on errno logging.
- *      -i n : Execute test n times.
- *      -I x : Execute test for x seconds.
- *      -p   : Pause for SIGUSR1 before starting
- *      -P x : Pause for x seconds between iterations.
- *      -t   : Turn on syscall timing.
- *
- * Author
- *	Yi Yang <yyangcdl@cn.ibm.com>
- *
- * History
- *      08/22/2006      Created first by Yi Yang <yyangcdl@cn.ibm.com>
+ *  This test case will verify basic function of openat
+ *  added by kernel 2.6.16 or up.
  *
  *****************************************************************************/
 
@@ -52,28 +34,66 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+
 #include "test.h"
 #include "usctest.h"
-#include "linux_syscall_numbers.h"
+#include "safe_macros.h"
 #include "lapi/fcntl.h"
 #include "openat.h"
 
-#define TEST_CASES 5
-
-void setup();
-void cleanup();
-void setup_every_copy();
+static void setup(void);
+static void cleanup(void);
 
 char *TCID = "openat01";
-int TST_TOTAL = TEST_CASES;
-char pathname[256];
-char testfile[256];
-char testfile2[256];
-char testfile3[256];
-int dirfd, fd, ret;
-int fds[TEST_CASES];
-char *filenames[TEST_CASES];
-int expected_errno[TEST_CASES] = { 0, 0, ENOTDIR, EBADF, 0 };
+
+static int dirfd, fd;
+static int fd_invalid = 100;
+static int fd_atcwd = AT_FDCWD;
+
+#define TEST_FILE "test_file"
+#define TEST_DIR "test_dir/"
+
+static char glob_path[256];
+
+static struct test_case {
+	int *dirfd;
+	const char *pathname;
+	int exp_ret;
+	int exp_errno;
+} test_cases[] = {
+	{&dirfd, TEST_FILE, 0, 0},
+	{&dirfd, glob_path, 0, 0},
+	{&fd, TEST_FILE, -1, ENOTDIR},
+	{&fd_invalid, TEST_FILE, -1, EBADF},
+	{&fd_atcwd, TEST_DIR TEST_FILE, 0, 0}
+};
+
+int TST_TOTAL = ARRAY_SIZE(test_cases);
+
+static void verify_openat(struct test_case *test)
+{
+	TEST(openat(*test->dirfd, test->pathname, O_RDWR, 0600));
+
+	if ((test->exp_ret == -1 && TEST_RETURN != -1) ||
+	    (test->exp_ret == 0 && TEST_RETURN < 0)) {
+		tst_resm(TFAIL | TTERRNO,
+		         "openat() returned %ldl, expected %d",
+			 TEST_RETURN, test->exp_ret);
+		return;
+	}
+
+	if (TEST_RETURN > 0)
+		SAFE_CLOSE(cleanup, TEST_RETURN);
+
+	if (TEST_ERRNO != test->exp_errno) {
+		tst_resm(TFAIL | TTERRNO,
+		         "openat() returned wrong errno, expected %s(%d)",
+			 tst_strerrno(test->exp_errno), test->exp_errno);
+		return;
+	}
+
+	tst_resm(TPASS | TTERRNO, "openat() returned %ld", TEST_RETURN);
+}
 
 int main(int ac, char **av)
 {
@@ -81,137 +101,51 @@ int main(int ac, char **av)
 	char *msg;
 	int i;
 
-	/* Disable test if the version of the kernel is less than 2.6.16 */
-	if ((tst_kvercmp(2, 6, 16)) < 0) {
-		tst_resm(TWARN, "This test can only run on kernels that are ");
-		tst_resm(TWARN, "2.6.16 and higher");
-		exit(0);
-	}
-
-	/***************************************************************
-	 * parse standard options
-	 ***************************************************************/
 	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
-	/***************************************************************
-	 * perform global setup for test
-	 ***************************************************************/
 	setup();
 
-	/***************************************************************
-	 * check looping state if -c option given
-	 ***************************************************************/
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		setup_every_copy();
-
 		tst_count = 0;
 
-		/*
-		 * Call openat
-		 */
-		for (i = 0; i < TST_TOTAL; i++) {
-			TEST(openat
-			     (fds[i], filenames[i], O_CREAT | O_WRONLY, 0600));
-
-			/* check return code */
-			if (TEST_ERRNO == expected_errno[i]) {
-
-				/***************************************************************
-				 * only perform functional verification if flag set (-f not given)
-				 ***************************************************************/
-				if (STD_FUNCTIONAL_TEST) {
-					/* No Verification test, yet... */
-					tst_resm(TPASS,
-						 "openat() returned the expected errno %d: %s",
-						 TEST_ERRNO,
-						 strerror(TEST_ERRNO));
-				}
-			} else {
-				TEST_ERROR_LOG(TEST_ERRNO);
-				tst_resm(TFAIL,
-					 "openat() Failed, errno=%d : %s",
-					 TEST_ERRNO, strerror(TEST_ERRNO));
-			}
-		}
-
+		for (i = 0; i < TST_TOTAL; i++)
+			verify_openat(test_cases + i);
 	}
 
-	/***************************************************************
-	 * cleanup and exit
-	 ***************************************************************/
 	cleanup();
-
-	return (0);
+	tst_exit();
 }
 
-void setup_every_copy(void)
+static void setup(void)
 {
-	/* Initialize test dir and file names */
-	sprintf(pathname, "openattestdir%d", getpid());
-	sprintf(testfile, "openattestfile%d.txt", getpid());
-	sprintf(testfile2, "openattestfile%d_2.txt", getpid());
-	sprintf(testfile3, "/tmp/openattestfile%d.txt", getpid());
-
-	ret = mkdir(pathname, 0600);
-	if (ret < 0) {
-		perror("mkdir: ");
-		exit(-1);
-	}
-
-	dirfd = open(pathname, O_DIRECTORY);
-	if (dirfd < 0) {
-		perror("open: ");
-		exit(-1);
-	}
-
-	fd = open(testfile2, O_CREAT | O_RDWR, 0600);
-	if (fd < 0) {
-		perror("open: ");
-		exit(-1);
-	}
-	fds[0] = fds[1] = dirfd;
-	fds[2] = fd;
-	fds[3] = 100;
-	fds[4] = AT_FDCWD;
-
-	filenames[0] = filenames[3] = filenames[4] = testfile;
-	filenames[1] = testfile3;
-	filenames[2] = testfile2;
-}
-
-/***************************************************************
- * setup() - performs all ONE TIME setup for this test.
- ***************************************************************/
-void setup(void)
-{
+	char *tmpdir;
 
 	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+
+	tst_tmpdir();
+
+	SAFE_MKDIR(cleanup, TEST_DIR, 0700);
+	dirfd = SAFE_OPEN(cleanup, TEST_DIR, O_DIRECTORY);
+	fd = SAFE_OPEN(cleanup, TEST_DIR TEST_FILE, O_CREAT | O_RDWR, 0600);
+
+	tmpdir = tst_get_tmpdir();
+	snprintf(glob_path, sizeof(glob_path), "%s/" TEST_DIR TEST_FILE,
+	         tmpdir);
+	free(tmpdir);
 
 	TEST_PAUSE;
 }
 
-/***************************************************************
- * cleanup() - performs all ONE TIME cleanup for this test at
- *		completion or premature exit.
- ***************************************************************/
-void cleanup(void)
+static void cleanup(void)
 {
-	/* Remove them */
-	char tmppathname[256];
-	strcpy(tmppathname, pathname);
-	unlink(strcat(strcat(tmppathname, "/"), testfile));
-	unlink(testfile);
-	strcpy(tmppathname, pathname);
-	unlink(strcat(strcat(tmppathname, "/"), testfile2));
-	unlink(testfile2);
-	unlink(testfile3);
-	rmdir(pathname);
+	if (fd > 0 && close(fd))
+		tst_resm(TWARN | TERRNO, "close(fd) failed");
 
-	/*
-	 * print timing stats if that option was specified.
-	 * print errno log if that option was specified.
-	 */
+	if (dirfd > 0 && close(dirfd))
+		tst_resm(TWARN | TERRNO, "close(dirfd) failed");
+
+	tst_rmdir();
+
 	TEST_CLEANUP;
-
 }
