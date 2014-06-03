d458410d518369400bba9e676cc9b16d7592cdc6
Cyril Hrubis <chrubis@suse.cz>
2014-06-03 12:27:03 +0200

    syscalls/vmsplice01: Cleanup & fixes.
    * Use tst_exit() instead of return (0)
      (the possible test failure is propagated now)
    
    * Use SAFE_MACROS()
    
    * Remove useless comments
    
    * ...
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/vmsplice/vmsplice01.c
+++ testcases/kernel/syscalls/vmsplice/vmsplice01.c
@@ -1,275 +1,181 @@
 /******************************************************************************
  *
- *   Copyright (c) International Business Machines  Corp., 2006
+ * Copyright (c) International Business Machines  Corp., 2006
+ *   Author Yi Yang <yyangcdl@cn.ibm.com>
+ * Copyright (c) 2014 Cyril Hrubis <chrubis@suse.cz>
  *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This program is free software;  you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * NAME
- *      vmsplice01.c
+ * You should have received a copy of the GNU General Public License
+ * along with this program;  if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  *
  * DESCRIPTION
  *	This test case will verify basic function of vmsplice
  *	added by kernel 2.6.17 or up.
  *
- * USAGE:  <for command-line>
- * vmsplice01 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-p]
- * where:
- *      -c n : Run n copies simultaneously.
- *      -e   : Turn on errno logging.
- *      -i n : Execute test n times.
- *      -I x : Execute test for x seconds.
- *      -p   : Pause for SIGUSR1 before starting
- *      -P x : Pause for x seconds between iterations.
- *      -t   : Turn on syscall timing.
- *
- * Author
- *	Yi Yang <yyangcdl@cn.ibm.com>
- *
- * History
- *      09/01/2006      Created first by Yi Yang <yyangcdl@cn.ibm.com>
- *                      Thanks for some help from Jens Axboe <axboe@kernel.dk>
- *
  *****************************************************************************/
+
+#define _GNU_SOURCE
 
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <fcntl.h>
-#include <linux/unistd.h>
-#include <linux/uio.h>
 #include <sys/poll.h>
+
 #include "test.h"
 #include "usctest.h"
 #include "linux_syscall_numbers.h"
 #include "tst_fs_type.h"
+#include "safe_macros.h"
+#include "lapi/fcntl.h"
 
-#define SPLICE_TEST_BLOCK_SIZE (1<<17)	/* 128K */
+#define TEST_BLOCK_SIZE (1<<17)	/* 128K */
 
-static int vmsplice_test(void);
-void setup();
-void cleanup();
+static void vmsplice_test(void);
+static void setup(void);
+static void cleanup(void);
+
+#define TESTFILE "vmsplice_test_file"
+
+static int fd_out;
+static char buffer[TEST_BLOCK_SIZE];
 
 char *TCID = "vmsplice01";
 int TST_TOTAL = 1;
-char testfile[256];
-
-static long mysplice(int fd_in, loff_t * off_in,
-		     int fd_out, loff_t * off_out,
-		     size_t len, unsigned int flags)
-{
-	return ltp_syscall(__NR_splice, fd_in, off_in, fd_out, off_out,
-		len, flags);
-}
-
-static long myvmsplice(int fd, struct iovec *v, unsigned long nregs,
-		       unsigned int flags)
-{
-	return ltp_syscall(__NR_vmsplice, fd, v, nregs, flags);
-}
-
-static void setup_every_copy(void)
-{
-	/* Initialize test file names */
-	sprintf(testfile, "vmsplicetest%d.txt", getpid());
-}
 
 int main(int ac, char **av)
 {
 	int lc;
 	const char *msg;
 
-	/* Disable test if the version of the kernel is less than 2.6.17 */
-	if ((tst_kvercmp(2, 6, 17)) < 0) {
-		tst_resm(TWARN, "This test can only run on kernels that are ");
-		tst_resm(TWARN, "2.6.17 and higher");
-		exit(0);
-	}
-
-	/***************************************************************
-	 * parse standard options
-	 ***************************************************************/
 	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
 
-	/***************************************************************
-	 * perform global setup for test
-	 ***************************************************************/
 	setup();
 
-	/***************************************************************
-	 * check looping state if -c option given
-	 ***************************************************************/
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		setup_every_copy();
-
-		tst_count = 0;
-
-		/*
-		 * Call vmsplice_test
-		 */
-		TEST(vmsplice_test());
-
-		/* check return code */
-		if (TEST_RETURN < 0) {
-			if (TEST_RETURN != -1) {
-				TEST_ERRNO = -TEST_RETURN;
-			}
-			TEST_ERROR_LOG(TEST_ERRNO);
-			tst_resm(TFAIL, "vmsplice() Failed, errno=%d : %s",
-				 TEST_ERRNO, strerror(TEST_ERRNO));
-		} else {
-
-			tst_resm(TPASS, "vmsplice() returned %ld",
-				 TEST_RETURN);
-		}
-
-	}
+	for (lc = 0; TEST_LOOPING(lc); lc++)
+		vmsplice_test();
 
 	cleanup();
-
-	return (0);
+	tst_exit();
 }
 
-static int vmsplice_test(void)
+static void check_file(void)
 {
-	char buffer[SPLICE_TEST_BLOCK_SIZE];
-	char vmsplicebuffer[SPLICE_TEST_BLOCK_SIZE];
+	int i;
+	char vmsplicebuffer[TEST_BLOCK_SIZE];
+
+	fd_out = SAFE_OPEN(cleanup, TESTFILE, O_RDONLY);
+	SAFE_READ(cleanup, 1, fd_out, vmsplicebuffer, TEST_BLOCK_SIZE);
+
+	for (i = 0; i < TEST_BLOCK_SIZE; i++) {
+		if (buffer[i] != vmsplicebuffer[i])
+			break;
+	}
+
+	if (i < TEST_BLOCK_SIZE)
+		tst_resm(TFAIL, "Wrong data read from the buffer at %i", i);
+	else
+		tst_resm(TPASS, "Written data has been read back correctly");
+
+	close(fd_out);
+	fd_out = 0;
+}
+
+static void vmsplice_test(void)
+{
 	int pipes[2];
 	long written;
-	int i, ret, flag = 0;
+	int ret;
 	int fd_out;
 	struct iovec v;
-	struct pollfd pfd;
 	loff_t offset;
 
-	for (i = 0; i < SPLICE_TEST_BLOCK_SIZE; i++) {
-		buffer[i] = i & 0xff;
-	}
 	v.iov_base = buffer;
-	v.iov_len = SPLICE_TEST_BLOCK_SIZE;
+	v.iov_len = TEST_BLOCK_SIZE;
 
-	ret = pipe(pipes);
-	if (ret < 0) {
-		perror("pipe: ");
-		return -1;
-	}
+	SAFE_PIPE(cleanup, pipes);
+	fd_out = SAFE_OPEN(cleanup, TESTFILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-	fd_out = open(testfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd_out < 0) {
-		close(pipes[0]);
-		close(pipes[1]);
-		perror("open: ");
-		return -1;
-	}
-
-	pfd.fd = pipes[1];
-	pfd.events = POLLOUT;
+	struct pollfd pfd = {.fd = pipes[1], .events = POLLOUT};
 	offset = 0;
+
 	while (v.iov_len) {
 		/*
 		 * in a real app you'd be more clever with poll of course,
 		 * here we are basically just blocking on output room and
 		 * not using the free time for anything interesting.
 		 */
-		if (poll(&pfd, 1, -1) < 0) {
-			perror("poll: ");
-			return -1;
-		}
+		if (poll(&pfd, 1, -1) < 0)
+			tst_brkm(TBROK | TERRNO, cleanup, "poll() failed");
 
-		written = myvmsplice(pipes[1], &v, 1, 0);
+		written = vmsplice(pipes[1], &v, 1, 0);
 		if (written < 0) {
-			ret = -errno;
-			close(fd_out);
-			close(pipes[0]);
-			close(pipes[1]);
-			return ret;
-		} else if (written == 0)
-			break;
-		else {
-			v.iov_base += written;
-			v.iov_len -= written;
-			flag = 1;
+			tst_brkm(TBROK | TERRNO, cleanup, "vmsplice() failed");
+		} else {
+			if (written == 0) {
+				break;
+			} else {
+				v.iov_base += written;
+				v.iov_len -= written;
+			}
 		}
 
-		/*
-		 * check if the current filesystem is nfs
-		 */
-		if (tst_fs_type(cleanup, ".") == TST_NFS_MAGIC) {
-			if (flag == 1)
-				tst_resm(TINFO, "vmplice() passes");
-			tst_brkm(TCONF, cleanup, "Cannot do splice() "
-				 "on a file located on an NFS filesystem");
-		}
-
-		ret = mysplice(pipes[0], NULL, fd_out, &offset, written, 0);
-		if (ret < 0) {
-			ret = -errno;
-			close(fd_out);
-			close(pipes[0]);
-			close(pipes[1]);
-			return ret;
-		}
+		ret = splice(pipes[0], NULL, fd_out, &offset, written, 0);
+		if (ret < 0)
+			tst_brkm(TBROK | TERRNO, cleanup, "splice() failed");
 		//printf("offset = %lld\n", (long long)offset);
-
 	}
 
-	close(fd_out);
 	close(pipes[0]);
 	close(pipes[1]);
+	close(fd_out);
+	fd_out = 0;
 
-	fd_out = open(testfile, O_RDONLY);
-	if (fd_out < 0) {
-		perror("open: ");
-		return -1;
-	}
-	read(fd_out, vmsplicebuffer, SPLICE_TEST_BLOCK_SIZE);
-	for (i = 0; i < SPLICE_TEST_BLOCK_SIZE; i++) {
-		if (buffer[i] != vmsplicebuffer[i])
-			break;
-	}
-	if (i < SPLICE_TEST_BLOCK_SIZE) {
-		return -1;
-	}
-	return 0;
+	check_file();
 }
 
-/***************************************************************
- * setup() - performs all ONE TIME setup for this test.
- ***************************************************************/
-void setup(void)
+static void setup(void)
 {
+	int i;
+
+	if ((tst_kvercmp(2, 6, 17)) < 0) {
+		tst_brkm(TCONF, NULL,
+		         "The vmsplice is supported 2.6.17 and newer");
+	}
 
 	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+
+	tst_tmpdir();
+
+	if (tst_fs_type(cleanup, ".") == TST_NFS_MAGIC) {
+		tst_brkm(TCONF, cleanup, "Cannot do splice() "
+			 "on a file located on an NFS filesystem");
+	}
+
+	for (i = 0; i < TEST_BLOCK_SIZE; i++)
+		buffer[i] = i & 0xff;
 
 	TEST_PAUSE;
 }
 
-/***************************************************************
- * cleanup() - performs all ONE TIME cleanup for this test at
- *             completion or premature exit.
- ***************************************************************/
-void cleanup(void)
+static void cleanup(void)
 {
-	/* Remove them */
-	unlink(testfile);
-
-	/*
-	 * print timing stats if that option was specified.
-	 * print errno log if that option was specified.
-	 */
 	TEST_CLEANUP;
 
+	if (fd_out > 0 && close(fd_out))
+		tst_resm(TWARN, "Failed to close fd_out");
+
+	tst_rmdir();
 }
