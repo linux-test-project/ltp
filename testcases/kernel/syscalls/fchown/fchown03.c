369bb1c9c78718355e4b23cb58408f01ecd91abb
Cyril Hrubis <chrubis@suse.cz>
2014-06-04 16:00:01 +0200

    syscalls/fchown03: Cleanup & fixes
    * Use tst_exit() instead of return (0)
      (the possible test failure is propagated now)
    
    * Use tst_require_root()
    
    * Use SAFE_MACROS()
    
    * Remove useless comments
    
    * ...
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/fchown/fchown03.c
+++ testcases/kernel/syscalls/fchown/fchown03.c
@@ -1,25 +1,23 @@
 /*
+ * Copyright (c) International Business Machines  Corp., 2001
+ *  07/2001 Ported by Wayne Boyer
  *
- *   Copyright (c) International Business Machines  Corp., 2001
+ * This program is free software;  you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License for more details.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program;  if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 /*
- * Test Name: fchown03
- *
  * Test Description:
  *  Verify that, fchown(2) succeeds to change the group of a file specified
  *  by path when called by non-root user with the following constraints,
@@ -27,46 +25,6 @@
  *	- the intended gid is either egid, or one of the supplementary gids
  *	  of the process.
  *  Also, verify that fchown() clears the setuid/setgid bits set on the file.
- *
- * Expected Result:
- *  fchown(2) should return 0 and the ownership set on the file should match
- *  the numeric values contained in owner and group respectively.
- *
- * Algorithm:
- *  Setup:
- *   Setup signal handling.
- *   Create temporary directory.
- *   Pause for SIGUSR1 if option specified.
- *
- *  Test:
- *   Loop if the proper options are given.
- *   Execute system call
- *   Check return code, if system call failed (return=-1)
- *	Log the errno and Issue a FAIL message.
- *   Otherwise,
- *	Verify the Functionality of system call
- *      if successful,
- *		Issue Functionality-Pass message.
- *      Otherwise,
- *		Issue Functionality-Fail message.
- *  Cleanup:
- *   Print errno log and/or timing stats if options given
- *   Delete the temporary directory created.
- *
- * Usage:  <for command-line>
- *  fchown03 [-c n] [-f] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -f   : Turn off functionality Testing.
- *	       -i n : Execute test n times.
- *	       -I x : Execute test for x seconds.
- *	       -P x : Pause for x seconds between iterations.
- *	       -t   : Turn on syscall timing.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS:
- *  This test should be run by 'non-super-user' (uid != 0) only.
  */
 
 #include <stdio.h>
@@ -82,50 +40,43 @@
 
 #include "test.h"
 #include "usctest.h"
+#include "safe_macros.h"
 
 #define FILE_MODE (mode_t)(S_IFREG | S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 #define NEW_PERMS (mode_t)(S_IFREG | S_IRWXU | S_IRWXG | S_ISUID | S_ISGID)
 #define FCHOWN_PERMS	(mode_t)(NEW_PERMS & ~(S_ISUID | S_ISGID))
 #define TESTFILE	"testfile"
 
-int fildes;			/* File descriptor for test file */
 char *TCID = "fchown03";
-int TST_TOTAL = 1;		/* Total number of test conditions */
-char nobody_uid[] = "nobody";
-struct passwd *ltpuser;
+int TST_TOTAL = 1;
 
-void setup();			/* setup function for the test */
-void cleanup();			/* cleanup function for the test */
+static int fildes;
+char nobody_uid[] = "nobody";
+static struct passwd *ltpuser;
+
+static void setup(void);
+static void cleanup(void);
 
 int main(int ac, char **av)
 {
-	struct stat stat_buf;	/* stat(2) struct contents */
+	struct stat stat_buf;
 	int lc;
 	const char *msg;
-	uid_t user_id;		/* Owner id of the test file. */
-	gid_t group_id;		/* Group id of the test file. */
+	uid_t user_id;
+	gid_t group_id;
 
 	msg = parse_opts(ac, av, NULL, NULL);
-	if (msg != NULL) {
+	if (msg != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-
-	}
 
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
 		tst_count = 0;
 
-		/* Get the euid/egid of the process */
 		user_id = geteuid();
 		group_id = getegid();
 
-		/*
-		 * Call fchwon(2) with different user id and
-		 * group id (numeric values) to set it on
-		 * testfile.
-		 */
 		TEST(fchown(fildes, -1, group_id));
 
 		if (TEST_RETURN == -1) {
@@ -134,32 +85,16 @@ int main(int ac, char **av)
 			continue;
 		}
 
-		/*
-		 * Get the testfile information using
-		 * fstat(2).
-		 */
-		if (fstat(fildes, &stat_buf) < 0) {
-			tst_brkm(TFAIL, cleanup, "fstat(2) of %s "
-				 "failed, errno:%d",
-				 TESTFILE, TEST_ERRNO);
-		}
+		SAFE_FSTAT(cleanup, fildes, &stat_buf);
 
-		/*
-		 * Check for expected Ownership ids
-		 * set on testfile.
-		 */
 		if ((stat_buf.st_uid != user_id) ||
 		    (stat_buf.st_gid != group_id)) {
 			tst_resm(TFAIL, "%s: Incorrect "
 				 "ownership set, Expected %d %d",
 				 TESTFILE, user_id, group_id);
+			continue;
 		}
 
-		/*
-		 * Verify that setuid/setgid bits
-		 * set on the testfile in setup() are
-		 * cleared by fchown()
-		 */
 		if (stat_buf.st_mode != FCHOWN_PERMS) {
 			tst_resm(TFAIL, "%s: Incorrect mode permissions"
 				 " %#o, Expected %#o", TESTFILE,
@@ -171,74 +106,39 @@ int main(int ac, char **av)
 	}
 
 	cleanup();
-	return (0);
+	tst_exit();
 }
 
-/*
- * setup() - performs all ONE TIME setup for this test.
- *	     Create a temporary directory and change directory to it.
- *	     Create a test file under temporary directory and close it
- *	     Change the ownership on testfile.
- */
-void setup(void)
+static void setup(void)
 {
-
 	tst_sig(FORK, DEF_HANDLER, cleanup);
 
-	/* Switch to nobody user for correct error code collection */
-	if (geteuid() != 0) {
-		tst_brkm(TBROK, NULL, "Test must be run as root");
-	}
-	ltpuser = getpwnam(nobody_uid);
-	if (seteuid(ltpuser->pw_uid) == -1) {
-		tst_brkm(TBROK, cleanup, "seteuid failed to "
-			 "to set the effective uid to %d: %s", ltpuser->pw_uid,
-			 strerror(errno));
-	}
+	tst_require_root(NULL);
+
+	ltpuser = SAFE_GETPWNAM(cleanup, nobody_uid);
+	SAFE_SETEUID(NULL, ltpuser->pw_uid);
 
 	TEST_PAUSE;
 
 	tst_tmpdir();
 
-	/* Create a test file under temporary directory */
-	if ((fildes = open(TESTFILE, O_RDWR | O_CREAT, FILE_MODE)) == -1) {
-		tst_brkm(TBROK, cleanup,
-			 "open(%s, O_RDWR|O_CREAT, %o) Failed, errno=%d : %s",
-			 TESTFILE, FILE_MODE, errno, strerror(errno));
-	}
-	seteuid(0);
-	if (fchown(fildes, -1, 0) < 0)
-		tst_brkm(TBROK, cleanup, "Fail to modify Ownership of %s: %s",
-			 TESTFILE, strerror(errno));
+	fildes = SAFE_OPEN(cleanup, TESTFILE, O_RDWR | O_CREAT, FILE_MODE);
 
-	if (fchmod(fildes, NEW_PERMS) < 0)
-		tst_brkm(TBROK, cleanup, "Fail to modify Mode of %s: %s",
-			 TESTFILE, strerror(errno));
+	SAFE_SETEUID(cleanup, 0);
 
-	setegid(ltpuser->pw_gid);
-	seteuid(ltpuser->pw_uid);
+	SAFE_FCHOWN(cleanup, fildes, -1, 0);
+	SAFE_FCHMOD(cleanup, fildes, NEW_PERMS);
 
+	SAFE_SETEGID(cleanup, ltpuser->pw_gid);
+	SAFE_SETEUID(cleanup, ltpuser->pw_uid);
 }
 
-/*
- * cleanup() - performs all ONE TIME cleanup for this test at
- *	       completion or premature exit.
- *	       Close the temporary file.
- *	       Remove the test directory and testfile created in the setup.
- */
-void cleanup(void)
+static void cleanup(void)
 {
-	/*
-	 * print timing stats if that option was specified.
-	 */
 	TEST_CLEANUP;
 
-	/* Close the test file created above */
-	if (close(fildes) == -1) {
-		tst_resm(TBROK, "close(%s) Failed, errno=%d : %s",
-			 TESTFILE, errno, strerror(errno));
-	}
+	if (fildes > 0 && close(fildes))
+		tst_resm(TWARN | TERRNO, "close(%s) Failed", TESTFILE);
 
 	tst_rmdir();
-
 }
