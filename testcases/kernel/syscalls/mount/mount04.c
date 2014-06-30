1c9f529dce08c061a498a0ff4ba3154509be5ea1
Cyril Hrubis <chrubis@suse.cz>
2014-06-30 14:00:31 +0200

    syscalls/mount04: Cleanup & convert to tst_acquire_device()
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/mount/mount04.c
+++ testcases/kernel/syscalls/mount/mount04.c
@@ -16,11 +16,6 @@
  * DESCRIPTION
  *	Verify that mount(2) returns -1 and sets errno to  EPERM if the user
  *	is not the super-user.
- *
- * RESTRICTIONS
- *	test must be run with the -D option
- *	test doesn't support -c option to run it in parallel, as mount
- *	syscall is not supposed to run in parallel.
  */
 
 #include <errno.h>
@@ -31,8 +26,8 @@
 #include <pwd.h>
 #include "test.h"
 #include "usctest.h"
+#include "safe_macros.h"
 
-static void help(void);
 static void setup(void);
 static void cleanup(void);
 
@@ -41,81 +36,54 @@ char *TCID = "mount04";
 #define DIR_MODE	S_IRWXU | S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP
 
 static char *mntpoint = "mntpoint";
-static char *fstype = "ext2";
-static char *device;
-static int Dflag = 0;
+static const char *fs_type;
+static const char *device;
 
-static struct test_case_t {
-	char *err_desc;		/* error description            */
-	int exp_errno;		/* Expected error no            */
-	char *exp_errval;	/* Expected error value string  */
-} testcases[] = {
-	{
-	"User not Super User/root", EPERM, "EPERM"}
-};
-
-int TST_TOTAL = sizeof(testcases) / sizeof(testcases[0]);
+int TST_TOTAL = 1;
 
 static int exp_enos[] = { EPERM, 0 };
 
-static option_t options[] = {
-	{"T:", NULL, &fstype},
-	{"D:", &Dflag, &device},
-	{NULL, NULL, NULL}
-};
+static void verify_mount(void)
+{
+
+	TEST(mount(device, mntpoint, fs_type, 0, NULL));
+
+	if (TEST_RETURN == -1) {
+		if (TEST_ERRNO == EPERM)
+			tst_resm(TPASS | TTERRNO, "mount() failed expectedly");
+		else
+			tst_resm(TFAIL | TTERRNO,
+			         "mount() was expected to fail with EPERM");
+		return;
+	}
+
+	if (TEST_RETURN == 0) {
+		tst_resm(TFAIL, "mount() succeeded unexpectedly");
+		if (umount(mntpoint))
+			tst_brkm(TBROK, cleanup, "umount() failed");
+		return;
+	}
+
+	tst_resm(TFAIL | TTERRNO, "mount() returned %li", TEST_RETURN);
+}
 
 int main(int ac, char **av)
 {
-	int lc, i;
+	int lc;
 	const char *msg;
 
-	if ((msg = parse_opts(ac, av, options, &help)) != NULL)
+	if ((msg = parse_opts(ac, av, NULL, NULL)))
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-
-	if (Dflag == 0) {
-		tst_brkm(TBROK, NULL, "You must specifiy the device used for "
-			 " mounting with -D option.");
-		tst_exit();
-	}
 
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
 		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; ++i) {
-
-			TEST(mount(device, mntpoint, fstype, 0, NULL));
-
-			/* check return code */
-			if ((TEST_RETURN == -1) &&
-			    (TEST_ERRNO == testcases[i].exp_errno)) {
-				tst_resm(TPASS, "mount(2) expected failure; "
-					 "Got errno - %s : %s",
-					 testcases[i].exp_errval,
-					 testcases[i].err_desc);
-			} else {
-				if (umount(mntpoint) == -1) {
-					tst_brkm(TBROK, cleanup, "umount(2) "
-						 "failed to umount mntpoint %s "
-						 "errno - %d : %s", mntpoint,
-						 TEST_ERRNO,
-						 strerror(TEST_ERRNO));
-				}
-				tst_resm(TFAIL, "mount(2) failed to produce "
-					 "expected error; %d, errno:%s got %d",
-					 testcases[i].exp_errno,
-					 testcases[i].exp_errval, TEST_ERRNO);
-			}
-
-			TEST_ERROR_LOG(TEST_ERRNO);
-		}
+		verify_mount();
 	}
 
 	cleanup();
 	tst_exit();
-
 }
 
 static void setup(void)
@@ -126,49 +94,35 @@ static void setup(void)
 	tst_sig(FORK, DEF_HANDLER, cleanup);
 
 	tst_require_root(NULL);
-	tst_mkfs(NULL, device, fstype, NULL);
-
-	ltpuser = getpwnam(nobody_uid);
-	if (seteuid(ltpuser->pw_uid) == -1) {
-		tst_brkm(TBROK, cleanup,
-			 "seteuid() failed to change euid to %d "
-			 "errno = %d : %s", ltpuser->pw_uid, TEST_ERRNO,
-			 strerror(TEST_ERRNO));
-	}
 
 	tst_tmpdir();
 
-	if (mkdir(mntpoint, DIR_MODE)) {
-		tst_brkm(TBROK, cleanup, "mkdir(%s, %#o) failed; "
-			 "errno = %d: %s", mntpoint, DIR_MODE, errno,
-			 strerror(errno));
-	}
+	fs_type = tst_dev_fs_type();
+	device = tst_acquire_device(cleanup);
+
+	if (!device)
+		tst_brkm(TCONF, cleanup, "Failed to obtain block device");
+
+	tst_mkfs(cleanup, device, fs_type, NULL);
+
+	ltpuser = SAFE_GETPWNAM(cleanup, nobody_uid);
+	SAFE_SETEUID(cleanup, ltpuser->pw_uid);
+	SAFE_MKDIR(cleanup, mntpoint, DIR_MODE);
 
 	TEST_EXP_ENOS(exp_enos);
 
 	TEST_PAUSE;
-
-	return;
 }
 
 static void cleanup(void)
 {
 	TEST_CLEANUP;
+
+	if (seteuid(0))
+		tst_resm(TWARN | TERRNO, "seteuid(0) failed");
+
+	if (device)
+		tst_release_device(NULL, device);
+
 	tst_rmdir();
-
-	/* Set effective user id back to root */
-	if (seteuid(0) == -1) {
-		tst_resm(TINFO, "seteuid failed to "
-			 "to set the effective uid to root");
-		perror("seteuid");
-	}
-
-	return;
-}
-
-static void help(void)
-{
-	printf("-T type	  : specifies the type of filesystem to be mounted."
-	       " Default ext2. \n");
-	printf("-D device : device used for mounting \n");
 }
