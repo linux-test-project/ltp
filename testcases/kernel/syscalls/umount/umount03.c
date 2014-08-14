22c772bf09dc3bb3044bc01f13779e06d4c2bde5
Cyril Hrubis <chrubis@suse.cz>
2014-08-14 11:42:35 +0200

    syscalls/umount03: Cleanup.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/umount/umount03.c
+++ testcases/kernel/syscalls/umount/umount03.c
@@ -32,30 +32,25 @@
 #include <sys/fcntl.h>
 #include <sys/wait.h>
 #include <pwd.h>
+
 #include "test.h"
 #include "usctest.h"
+#include "safe_macros.h"
 
 static void help(void);
 static void setup(void);
 static void cleanup(void);
-static void cleanup1(void);
 
 char *TCID = "umount03";
 
 #define FSTYPE_LEN	20
 #define DIR_MODE	S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH
+#define MNTPOINT	"mntpoint"
 
-static char *mntpoint = "mntpoint";
 static char *fstype = "ext2";
 static char *device;
+static int mount_flag;
 static int Dflag = 0;
-
-static struct test_case_t {
-	char *err_desc;		/* error description            */
-	int exp_errno;		/* Expected error no            */
-	char *exp_errval;	/* Expected error value string  */
-} testcases = {
-"User not Super User/root", EPERM, "EPERM"};
 
 int TST_TOTAL = 1;
 
@@ -71,9 +66,6 @@ int main(int ac, char **av)
 {
 	int lc;
 	const char *msg;
-	char nobody_uid[] = "nobody";
-	struct passwd *ltpuser;
-	int status;
 
 	if ((msg = parse_opts(ac, av, options, &help)) != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
@@ -87,117 +79,59 @@ int main(int ac, char **av)
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
 		tst_count = 0;
 
-		switch (fork()) {
+		TEST(umount(MNTPOINT));
 
-		case -1:
-			tst_resm(TWARN, "fork() failed");
+		if (TEST_RETURN != -1) {
+			tst_resm(TFAIL, "umout() didn't fail (ret=%ld)",
+				 TEST_RETURN);
 			continue;
-
-		case 0:
-			if ((ltpuser = getpwnam(nobody_uid)) == NULL) {
-				tst_brkm(TBROK, NULL, "\"nobody\" user"
-					 "not present");
-			}
-			if (setuid(ltpuser->pw_uid) == -1) {
-				tst_resm(TWARN, "setuid failed "
-					 "to set the effective uid to %d",
-					 ltpuser->pw_uid);
-				exit(1);
-			}
-			TEST(umount(mntpoint));
-
-			if ((TEST_RETURN == -1) &&
-			    (TEST_ERRNO == testcases.exp_errno)) {
-				tst_resm(TPASS, "umount(2) expected failure "
-					 "Got errno - %s : %s",
-					 testcases.exp_errval,
-					 testcases.err_desc);
-			} else {
-				tst_resm(TFAIL, "umount(2) failed to produce "
-					 "expected error; %d, errno:%s got %d",
-					 testcases.exp_errno,
-					 testcases.exp_errval, TEST_ERRNO);
-			}
-
-			TEST_ERROR_LOG(TEST_ERRNO);
-			exit(1);
-
-		default:
-			if ((wait(&status)) < 0) {
-				tst_resm(TFAIL, "wait() failed");
-			}
 		}
 
+		if (TEST_ERRNO == EPERM) {
+			tst_resm(TPASS | TTERRNO, "umount() failed with EPERM");
+		} else {
+			tst_resm(TFAIL | TTERRNO,
+			         "umount() did not fail with EPERM(%d)", EPERM);
+		}
 	}
 
 	cleanup();
 	tst_exit();
-
 }
 
 static void setup(void)
 {
-	char nobody_uid[] = "nobody";
 	struct passwd *ltpuser;
 
 	tst_sig(FORK, DEF_HANDLER, cleanup);
+	TEST_EXP_ENOS(exp_enos);
 
 	tst_require_root(NULL);
 	tst_mkfs(NULL, device, fstype, NULL);
 
-	if ((ltpuser = getpwnam(nobody_uid)) == NULL)
-		tst_brkm(TBROK, NULL, "\"nobody\" user not present");
-
-	if (seteuid(ltpuser->pw_uid) == -1) {
-		tst_brkm(TBROK, NULL, "setuid failed to set the "
-			 "effective uid to %d", ltpuser->pw_uid);
-	}
+	ltpuser = SAFE_GETPWNAM(cleanup, "nobody");
 
 	tst_tmpdir();
 
-	if (mkdir(mntpoint, DIR_MODE)) {
-		tst_brkm(TBROK, cleanup1, "mkdir(%s, %#o) failed; errno = %d:"
-			 " %s", mntpoint, DIR_MODE, errno, strerror(errno));
-	}
+	SAFE_MKDIR(cleanup, MNTPOINT, DIR_MODE);
 
-	if (seteuid(0) == -1) {
-		tst_brkm(TBROK, cleanup1, "setuid failed to set the effective"
-			 " uid to %d", ltpuser->pw_uid);
-	}
-	TEST_EXP_ENOS(exp_enos);
+	if (mount(device, MNTPOINT, fstype, 0, NULL))
+		tst_brkm(TBROK | TERRNO, cleanup, "mount() failed");
+	mount_flag = 1;
 
-	if (access(device, F_OK))
-		tst_brkm(TBROK, cleanup1, "Device '%s' does not exist", device);
-
-	TEST(mount(device, mntpoint, fstype, 0, NULL));
-
-	if (TEST_RETURN != 0) {
-		tst_brkm(TBROK, cleanup1, "mount(2) failed to mount device %s "
-			 "errno = %d : %s", device, TEST_ERRNO,
-			 strerror(TEST_ERRNO));
-	}
-
+	SAFE_SETEUID(cleanup, ltpuser->pw_uid);
 	TEST_PAUSE;
-}
-
-static void cleanup1(void)
-{
-	TEST_CLEANUP;
-	tst_rmdir();
-	tst_exit();
 }
 
 static void cleanup(void)
 {
-	TEST(umount(mntpoint));
-	if (TEST_RETURN != 0) {
-		tst_resm(TWARN, "umount(2) failed to umount device %s while"
-			 " cleanuo errno = %d : %s", mntpoint, TEST_ERRNO,
-			 strerror(TEST_ERRNO));
-	}
+	if (seteuid(0))
+		tst_resm(TWARN | TERRNO, "seteuid(0) failed");
+
+	if (mount_flag && umount(MNTPOINT))
+		tst_resm(TWARN | TERRNO, "umount() failed");
 
 	TEST_CLEANUP;
 	tst_rmdir();
