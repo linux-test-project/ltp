da7d01b0afd7b89edfca1fe0344d8d376f669b3a
Cyril Hrubis <chrubis@suse.cz>
2014-08-20 16:47:53 +0200

    syscalls/kectl01: Cleanup.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/keyctl/keyctl01.c
+++ testcases/kernel/syscalls/keyctl/keyctl01.c
@@ -1,42 +1,28 @@
-/******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007				   */
-/*									    */
-/* This program is free software;  you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or	  */
-/* (at your option) any later version.					*/
-/*									    */
-/* This program is distributed in the hope that it will be useful,	    */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of	    */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		  */
-/* the GNU General Public License for more details.			   */
-/*									    */
-/* You should have received a copy of the GNU General Public License	  */
-/* along with this program;  if not, write to the Free Software	       */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
-/*									    */
-/******************************************************************************/
-/******************************************************************************/
-/*									    */
-/* File:	keyctl01.c				   		      */
-/*									    */
-/* Description: This tests the keyctl() syscall				      */
-/*		Manipulate the kernel's key management facility	       */
-/* Usage:  <for command-line>						 */
-/* keyctl01 [-c n] [-e][-i n] [-I x] [-p x] [-t]		     	      */
-/*      where,  -c n : Run n copies concurrently.			     */
-/*	      -e   : Turn on errno logging.				 */
-/*	      -i n : Execute test n times.				  */
-/*	      -I x : Execute test for x seconds.			    */
-/*	      -P x : Pause for x seconds between iterations.		*/
-/*	      -t   : Turn on syscall timing.				*/
-/*									    */
-/* Total Tests: 2							     */
-/*									    */
-/* Test Name:   keyctl01					     	      */
-/* History:     Porting from Crackerjack to LTP is done by		    */
-/*	      Manas Kumar Nayak maknayak@in.ibm.com>			*/
-/******************************************************************************/
+/******************************************************************************
+ * Copyright (c) Crackerjack Project., 2007				      *
+ *									      *
+ * This program is free software;  you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or	      *
+ * (at your option) any later version.					      *
+ *									      *
+ * This program is distributed in the hope that it will be useful,	      *
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of	      *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		      *
+ * the GNU General Public License for more details.			      *
+ *									      *
+ * You should have received a copy of the GNU General Public License	      *
+ * along with this program;  if not, write to the Free Software Foundation,   *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA           *
+ *                                                                            *
+ ******************************************************************************/
+/*
+ * Description: This tests the keyctl() syscall
+ *		Manipulate the kernel's key management facility
+ *
+ * History:     Porting from Crackerjack to LTP is done by
+ *	      Manas Kumar Nayak maknayak@in.ibm.com>
+ */
 
 #include "config.h"
 #include <sys/types.h>
@@ -59,54 +45,15 @@ int testno;
 int TST_TOTAL = 2;
 
 #if HAVE_KEYCTL_SYSCALL
-/* Extern Global Functions */
-/******************************************************************************/
-/*									    */
-/* Function:    cleanup						       */
-/*									    */
-/* Description: Performs all one time clean up for this test on successful    */
-/*	      completion,  premature exit or  failure. Closes all temporary */
-/*	      files, removes all temporary directories exits the test with  */
-/*	      appropriate return code by calling tst_exit() function.       */
-/*									    */
-/* Input:       None.							 */
-/*									    */
-/* Output:      None.							 */
-/*									    */
-/* Return:      On failure - Exits calling tst_exit(). Non '0' return code.   */
-/*	      On success - Exits calling tst_exit(). With '0' return code.  */
-/*									    */
-/******************************************************************************/
-void cleanup(void)
-{
 
+static void cleanup(void)
+{
 	TEST_CLEANUP;
 	tst_rmdir();
-
 }
 
-/* Local  Functions */
-/******************************************************************************/
-/*									    */
-/* Function:    setup							 */
-/*									    */
-/* Description: Performs all one time setup for this test. This function is   */
-/*	      typically used to capture signals, create temporary dirs      */
-/*	      and temporary files that may be used in the course of this    */
-/*	      test.							 */
-/*									    */
-/* Input:       None.							 */
-/*									    */
-/* Output:      None.							 */
-/*									    */
-/* Return:      On failure - Exits by calling cleanup().		      */
-/*	      On success - returns 0.				       */
-/*									    */
-/******************************************************************************/
-void setup(void)
+static void setup(void)
 {
-	/* Capture signals if any */
-	/* Create temporary directories */
 	TEST_PAUSE;
 	tst_tmpdir();
 }
@@ -118,10 +65,8 @@ int main(int ac, char **av)
 	key_serial_t ne_key;
 	const char *msg;
 
-	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL) {
+	if ((msg = parse_opts(ac, av, NULL, NULL)) != NULL)
 		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
-		tst_exit();
-	}
 
 	setup();
 
@@ -172,13 +117,11 @@ int main(int ac, char **av)
 
 	}
 	cleanup();
-
-	return (1);
+	tst_exit();
 }
 #else
 int main(void)
 {
-	tst_resm(TCONF, "keyctl syscall support not available on system");
-	tst_exit();
+	tst_brkm(TCONF, NULL, "keyctl syscall support not available on system");
 }
 #endif
