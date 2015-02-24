58be22032417605993706988ee2daafc837f3f50
Cyril Hrubis <chrubis@suse.cz>
2015-02-24 15:48:51 +0100

    syscalls/nanosleep01: Cleanup and fix.
    * Get rid of the fork, there is no reason to do the test in child
    
    * Use tst_timer with CLOCK_MONOTONIC, because the testcases could
      fail if system wall time was changed during the test
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/nanosleep/nanosleep01.c
+++ testcases/kernel/syscalls/nanosleep/nanosleep01.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) International Business Machines  Corp., 2001
  *  07/2001 Ported by Wayne Boyer
+ * Copyright (C) 2015 Cyril Hrubis <chrubis@suse.cz>
  *
  * This program is free software;  you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,22 +22,11 @@
  * Test Name: nanosleep01
  *
  * Test Description:
- *  Verify that nanosleep() will be successful to suspend the execution
- *  of a process for a specified time.
- *
- * Expected Result:
  *  nanosleep() should return with value 0 and the process should be
  *  suspended for time specified by timespec structure.
  */
 
 #include <errno.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <sys/time.h>
-#include <sys/wait.h>
-#include <stdint.h>
-
 #include "test.h"
 
 char *TCID = "nanosleep01";
@@ -48,10 +38,6 @@ int main(int ac, char **av)
 {
 	int lc;
 	const char *msg;
-	pid_t cpid;
-	struct timeval otime;
-	struct timeval ntime;
-	int retval = 0, e_code, status;
 	struct timespec timereq = {.tv_sec = 2, .tv_nsec = 9999};
 
 	msg = parse_opts(ac, av, NULL, NULL);
@@ -61,68 +47,22 @@ int main(int ac, char **av)
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
+		tst_timer_start(CLOCK_MONOTONIC);
+		TEST(nanosleep(&timereq, NULL));
+		tst_timer_stop();
 
-		/*
-		 * Creat a child process and suspend it till
-		 * time specified by timespec struct element
-		 * time_t tv_sec.
-		 */
-		cpid = FORK_OR_VFORK();
-		if (cpid == -1)
-			tst_brkm(TBROK, NULL, "fork() failed");
+		if (TEST_RETURN == -1) {
+			tst_resm(TFAIL | TERRNO, "nanosleep() failed");
+			continue;
+		}
 
-		if (cpid == 0) {
-			/* Note down the current time */
-			gettimeofday(&otime, 0);
-			/*
-			 * Call nanosleep() to suspend child process
-			 * for specified time.
-			 */
-			TEST(nanosleep(&timereq, NULL));
-
-			/* time after child resumes execution */
-			gettimeofday(&ntime, 0);
-
-			if (TEST_RETURN == -1) {
-				retval = 1;
-				tst_resm(TFAIL,
-					 "nanosleep() failed, errno=%d : %s",
-					 TEST_ERRNO, strerror(TEST_ERRNO));
-				continue;
-			}
-
-			/*
-			 * Verify whether child execution was
-			 * actually suspended to desired interval.
-			 */
-			long want_ms, got_ms;
-			want_ms =
-			    timereq.tv_sec * 1000 +
-			    timereq.tv_nsec / 1000000;
-			got_ms =
-			    ntime.tv_sec * 1000 + ntime.tv_usec / 1000;
-			got_ms -=
-			    otime.tv_sec * 1000 + otime.tv_usec / 1000;
-			if (got_ms < want_ms) {
-				retval = 1;
-				tst_resm(TFAIL, "Child execution not "
-					 "suspended for %jd seconds.  (Wanted %ld ms, got %ld ms)",
-					 (intmax_t) timereq.tv_sec,
-					 want_ms, got_ms);
-			} else {
-				tst_resm(TPASS, "nanosleep "
-					 "functionality is correct");
-			}
-			exit(retval);
+		if (tst_timespec_lt(tst_timer_elapsed(), timereq)) {
+			tst_resm(TFAIL,
+			         "nanosleep() suspended for %lli us, expected %lli",
+				 tst_timer_elapsed_us(), tst_timespec_to_us(timereq));
 		} else {
-			/* wait for the child to finish */
-			wait(&status);
-			/* make sure the child returned a good exit status */
-			e_code = status >> 8;
-			if (e_code != 0) {
-				tst_resm(TFAIL, "Failures reported above");
-			}
+			tst_resm(TPASS, "nanosleep() suspended for %lli us",
+			         tst_timer_elapsed_us());
 		}
 	}
 
@@ -132,6 +72,6 @@ int main(int ac, char **av)
 static void setup(void)
 {
 	tst_sig(FORK, DEF_HANDLER, NULL);
-
+	tst_timer_check(CLOCK_MONOTONIC);
 	TEST_PAUSE;
 }
