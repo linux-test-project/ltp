de9390015ad7bf6d548c2c59ecb124815fa66ae6
Cyril Hrubis <chrubis@suse.cz>
2015-03-05 14:34:15 +0100

    syscalls/exit_group01: Cleanup.
    * Cleanup
    
    * Make use of parse_opts() and TEST_LOOPING()
    
    * Remove useless sleep(5)
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/exit_group/exit_group01.c
+++ testcases/kernel/syscalls/exit_group/exit_group01.c
@@ -1,42 +1,23 @@
-/******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007                                   */
-/*                                                                            */
-/* This program is free software;  you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or          */
-/* (at your option) any later version.                                        */
-/*                                                                            */
-/* This program is distributed in the hope that it will be useful,            */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  */
-/* the GNU General Public License for more details.                           */
-/*                                                                            */
-/* You should have received a copy of the GNU General Public License          */
-/* along with this program;  if not, write to the Free Software               */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
-/*                                                                            */
-/******************************************************************************/
-/******************************************************************************/
-/*                                                                            */
-/* File:        exit_group01.c                                           */
-/*                                                                            */
-/* Description: This tests the exit_group() syscall                      */
-/*                                                                            */
-/* Usage:  <for command-line>                                                 */
-/* exit_group01 [-c n] [-e][-i n] [-I x] [-p x] [-t]                     */
-/*      where,  -c n : Run n copies concurrently.                             */
-/*              -e   : Turn on errno logging.                                 */
-/*              -i n : Execute test n times.                                  */
-/*              -I x : Execute test for x seconds.                            */
-/*              -P x : Pause for x seconds between iterations.                */
-/*              -t   : Turn on syscall timing.                                */
-/*                                                                            */
-/* Total Tests: 1                                                             */
-/*                                                                            */
-/* Test Name:   exit_group01                                             */
-/* History:     Porting from Crackerjack to LTP is done by                    */
-/*              Manas Kumar Nayak maknayak@in.ibm.com>                        */
-/******************************************************************************/
+/******************************************************************************
+ * Copyright (c) Crackerjack Project., 2007                                   *
+ * Ported to LTP by Manas Kumar Nayak <maknayak@in.ibm.com>                   *
+ * Copyright (C) 2015 Cyril Hrubis <chrubis@suse.cz>                          *
+ *                                                                            *
+ * This program is free software;  you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation; either version 2 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  *
+ * the GNU General Public License for more details.                           *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program;  if not, write to the Free Software Foundation,   *
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA           *
+ *                                                                            *
+ ******************************************************************************/
 
 #include <stdio.h>
 #include <errno.h>
@@ -50,80 +31,42 @@ char *TCID = "exit_group01";
 int testno;
 int TST_TOTAL = 1;
 
-/* Extern Global Functions */
-/******************************************************************************/
-/*                                                                            */
-/* Function:    cleanup                                                       */
-/*                                                                            */
-/* Description: Performs all one time clean up for this test on successful    */
-/*              completion,  premature exit or  failure. Closes all temporary */
-/*              files, removes all temporary directories exits the test with  */
-/*              appropriate return code by calling tst_exit() function.       */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits calling tst_exit(). Non '0' return code.   */
-/*              On success - Exits calling tst_exit(). With '0' return code.  */
-/*                                                                            */
-/******************************************************************************/
-void cleanup(void)
+static void verify_exit_group(void)
 {
-
-	tst_rmdir();
-}
-
-/* Local  Functions */
-/******************************************************************************/
-/*                                                                            */
-/* Function:    setup                                                         */
-/*                                                                            */
-/* Description: Performs all one time setup for this test. This function is   */
-/*              typically used to capture signals, create temporary dirs      */
-/*              and temporary files that may be used in the course of this    */
-/*              test.                                                         */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits by calling cleanup().                      */
-/*              On success - returns 0.                                       */
-/*                                                                            */
-/******************************************************************************/
-void setup(void)
-{
-	/* Capture signals if any */
-	/* Create temporary directories */
-	TEST_PAUSE;
-	tst_tmpdir();
-}
-
-int main(int ac, char **av)
-{
-	int status;
 	pid_t cpid, w;
+	int status;
 
-	setup();
+	cpid = fork();
+	if (cpid == -1)
+		tst_brkm(TFAIL | TERRNO, NULL, "fork failed");
 
-	cpid = fork();		//call exit_group()
-	if (cpid == -1) {
-		tst_brkm(TFAIL | TERRNO, cleanup, "fork failed");
-	} else if (cpid == 0) {
-		sleep(5);
+	if (cpid == 0) {
 		TEST(ltp_syscall(__NR_exit_group, 4));
 	} else {
 		w = wait(&status);
 		if (w == -1)
-			tst_brkm(TBROK | TERRNO, cleanup, "wait failed");
-		if (WIFEXITED(status) && (WEXITSTATUS(status) == 4))
-			tst_resm(TPASS, "exit_group succeeded");
-		else {
+			tst_brkm(TBROK | TERRNO, NULL, "wait() failed");
+
+		if (WIFEXITED(status) && (WEXITSTATUS(status) == 4)) {
+			tst_resm(TPASS, "exit_group() succeeded");
+		} else {
 			tst_resm(TFAIL | TERRNO,
-				 "exit_group failed (wait status = %d)", w);
+				 "exit_group() failed (wait status = %d)", w);
 		}
 	}
-	cleanup();
+}
+
+int main(int ac, char **av)
+{
+	int lc;
+	const char *msg;
+
+	msg = parse_opts(ac, av, NULL, NULL);
+	if (msg)
+		tst_brkm(TBROK, NULL, "OPTION PARSING ERROR - %s", msg);
+
+	for (lc = 0; TEST_LOOPING(lc); lc++)
+		verify_exit_group();
+
 	tst_exit();
 }
