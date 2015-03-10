9dd5bc6f4c439515c0f32a7d34463039878ccb0c
Cyril Hrubis <chrubis@suse.cz>
2015-03-10 17:39:51 +0100

    syscalls/execl01: Cleanup.
    Get rid of STD_TIMING_ON.
    
    Get rid of useless calls to tst_tmpdir() and tst_rmdir()
    
    Actually fail the test when execl() fails.
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/execl/execl01.c
+++ testcases/kernel/syscalls/execl/execl01.c
@@ -1,5 +1,9 @@
 /*
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
+ *    AUTHOR		: William Roske
+ *    CO-PILOT		: Dave Fenner
+ *    DATE STARTED	: 06/01/02
+ * Copyright (C) 2015 Cyril Hrubis <chrubis@suse.cz>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -28,87 +32,7 @@
  * For further information regarding this notice, see:
  *
  * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
  */
-/* $Id: execl01.c,v 1.6 2009/03/23 13:35:40 subrata_modak Exp $ */
-/**********************************************************
- *
- *    OS Test - Silicon Graphics, Inc.
- *
- *    TEST IDENTIFIER	: execl01
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for execl(2)
- *
- *    PARENT DOCUMENT	: usctpl01
- *
- *    TEST CASE TOTAL	: 1
- *
- *    WALL CLOCK TIME	: 1
- *
- *    CPU TYPES		: ALL
- *
- *    AUTHOR		: William Roske
- *
- *    CO-PILOT		: Dave Fenner
- *
- *    DATE STARTED	: 06/01/02
- *
- *    INITIAL RELEASE	: UNICOS 7.0
- *
- *    TEST CASES
- *
- * 	1.) execl(2) returns...(See Description)
- *
- *    INPUT SPECIFICATIONS
- * 	The standard options for system call tests are accepted.
- *	(See the parse_opts(3) man page).
- *
- *    OUTPUT SPECIFICATIONS
- *$
- *    DURATION
- * 	Terminates - with frequency and infinite modes.
- *
- *    SIGNALS
- * 	Uses SIGUSR1 to pause before test if option set.
- * 	(See the parse_opts(3) man page).
- *
- *    RESOURCES
- * 	None
- *
- *    ENVIRONMENTAL NEEDS
- *      No run-time environmental needs.
- *
- *    SPECIAL PROCEDURAL REQUIREMENTS
- * 	None
- *
- *    INTERCASE DEPENDENCIES
- * 	None
- *
- *    DETAILED DESCRIPTION
- *	This is a Phase I test for the execl(2) system call.  It is intended
- *	to provide a limited exposure of the system call, for now.  It
- *	should/will be extended when full functional tests are written for
- *	execl(2).
- *
- * 	Setup:
- * 	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- * 	Test:
- *	 Loop if the proper options are given.
- * 	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- * 	Cleanup:
- * 	  Print errno log and/or timing stats if options given
- *
- *
- *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#**/
-
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -118,55 +42,30 @@
 
 #include "test.h"
 
-void setup();
-void cleanup();
+static void setup(void);
 
 char *TCID = "execl01";
 int TST_TOTAL = 1;
 
-pid_t pid;			/* process id from fork */
-int status;			/* status returned from waitpid */
-
 int main(int ac, char **av)
 {
 	int lc;
+	pid_t pid;
 
 	tst_parse_opts(ac, av, NULL, NULL);
 
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		/*
-		 * TEST CASE:
-		 *   fork, then call execl from child
-		 */
 		switch (pid = FORK_OR_VFORK()) {
-		case 0:	/* CHILD - Call execl(2) */
-			execl("test", "test", NULL);
-			/*
-			 * should not get here!! if we do, the parent will fail
-			 * the Test Case
-			 */
-			exit(errno);
+		case 0:
+			execl("execl01_child", "execl01_child", "canary", NULL);
+			tst_brkm(TFAIL | TERRNO, NULL,
+			         "Failed to execute execl01_child");
 		case -1:
-			tst_brkm(TBROK | TERRNO, cleanup, "fork failed");
-			break;
+			tst_brkm(TBROK | TERRNO, NULL, "fork failed");
 		default:
-			if (waitpid(pid, &status, 0) == -1)
-				tst_brkm(TBROK | TERRNO, cleanup,
-					 "waitpid failed");
-			if (WIFEXITED(status)) {
-				tst_resm(TPASS,
-					 "execl - properly exec's a simple "
-					 "program..");
-			} else {
-				tst_resm(TFAIL,
-					 "child exited abnormally; wait status=%d",
-					 status);
-			}
+			tst_record_childstatus(NULL, pid);
 		}
 
 	}
@@ -174,24 +73,9 @@ int main(int ac, char **av)
 	tst_exit();
 }
 
-void setup(void)
+static void setup(void)
 {
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	if (STD_TIMING_ON)
-		tst_resm(TINFO,
-			 "There are NO timing statistics produced by this test.\n"
-			 "This is because the test forks to create a child process "
-			 "which then calls execl.\nThe TEST macro is NOT used.");
+	tst_sig(FORK, DEF_HANDLER, NULL);
 
 	TEST_PAUSE;
-
-	tst_tmpdir();
-
-}
-
-void cleanup(void)
-{
-	tst_rmdir();
 }
