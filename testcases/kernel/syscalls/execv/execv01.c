8de536548569eccbb391a1ba00540d391d95e4c4
Cyril Hrubis <chrubis@suse.cz>
2015-03-11 13:48:25 +0100

    syscalls/execv01: Cleanup.
    Get rid of STD_TIMING_ON.
    
    Get rid of useless calls to tst_tmpdir() and tst_rmdir()
    
    Actually fail the test when execv() fails.
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/execv/execv01.c
+++ testcases/kernel/syscalls/execv/execv01.c
@@ -1,5 +1,9 @@
 /*
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
+ *    AUTHOR		: William Roske
+ *    CO-PILOT		: Dave Fenner
+ *    DATE STARTED	: 06/01/02
+ * Copyright (C) 2015 Cyril Hrubis <chrubis@suse.cz>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of version 2 of the GNU General Public License as
@@ -28,90 +32,9 @@
  * For further information regarding this notice, see:
  *
  * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
  */
-/* $Id: execv01.c,v 1.6 2009/03/23 13:35:40 subrata_modak Exp $ */
-/**********************************************************
- *
- *    OS Test - Silicon Graphics, Inc.
- *
- *    TEST IDENTIFIER	: execv01
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for execv(2)
- *
- *    PARENT DOCUMENT	: usctpl01
- *
- *    TEST CASE TOTAL	: 1
- *
- *    WALL CLOCK TIME	: 1
- *
- *    CPU TYPES		: ALL
- *
- *    AUTHOR		: William Roske
- *
- *    CO-PILOT		: Dave Fenner
- *
- *    DATE STARTED	: 06/01/02
- *
- *    INITIAL RELEASE	: UNICOS 7.0
- *
- *    TEST CASES
- *
- * 	1.) execv(2) returns...(See Description)
- *
- *    INPUT SPECIFICATIONS
- * 	The standard options for system call tests are accepted.
- *	(See the parse_opts(3) man page).
- *
- *    OUTPUT SPECIFICATIONS
- *$
- *    DURATION
- * 	Terminates - with frequency and infinite modes.
- *
- *    SIGNALS
- * 	Uses SIGUSR1 to pause before test if option set.
- * 	(See the parse_opts(3) man page).
- *
- *    RESOURCES
- * 	None
- *
- *    ENVIRONMENTAL NEEDS
- *      No run-time environmental needs.
- *
- *    SPECIAL PROCEDURAL REQUIREMENTS
- * 	None
- *
- *    INTERCASE DEPENDENCIES
- * 	None
- *
- *    DETAILED DESCRIPTION
- *	This is a Phase I test for the execv(2) system call.  It is intended
- *	to provide a limited exposure of the system call, for now.  It
- *	should/will be extended when full functional tests are written for
- *	execv(2).
- *
- * 	Setup:
- * 	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- * 	Test:
- *	 Loop if the proper options are given.
- * 	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- * 	Cleanup:
- * 	  Print errno log and/or timing stats if options given
- *
- *
- *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#**/
-
 #include <sys/types.h>
 #include <sys/wait.h>
-
 #include <errno.h>
 #include <signal.h>
 #include <stdlib.h>
@@ -119,73 +42,40 @@
 
 #include "test.h"
 
-void setup();
-void cleanup();
+static void setup(void);
 
 char *TCID = "execv01";
 int TST_TOTAL = 1;
 
-pid_t pid;			/* process id from fork */
-int status;			/* status returned from waitpid */
-char *args[2] = { "/bin/test", 0 };	/* argument list for execv call */
-
 int main(int ac, char **av)
 {
 	int lc;
+	pid_t pid;
+	char *const args[] = { "execv01_child", "canary", NULL};
 
 	tst_parse_opts(ac, av, NULL, NULL);
 
 	setup();
 
 	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
 		switch (pid = FORK_OR_VFORK()) {
 		case 0:
-			execv("/bin/true", args);
-			exit(errno);
+			execv("execv01_child", args);
+			tst_brkm(TFAIL | TERRNO, NULL,
+			         "Failed to execute execv01_child");
 		case -1:
-			tst_brkm(TBROK, cleanup, "fork failed");
-			break;
+			tst_brkm(TBROK, NULL, "fork failed");
 		default:
-			waitpid(pid, &status, 0);
-			if (WIFEXITED(status)) {
-				tst_resm(TPASS,
-					 "execv - properly exec's a simple program..");
-			} else {
-				tst_resm(TFAIL,
-					 "Child process did not terminate properly, status=%d",
-					 status);
-			}
-			break;
-		}		/* switch */
-
+			tst_record_childstatus(NULL, pid);
+		}
 	}
 
-	cleanup();
-
 	tst_exit();
-
 }
 
-void setup(void)
+static void setup(void)
 {
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	if (STD_TIMING_ON)
-		tst_resm(TINFO,
-			 "There are NO timing statistics produced by this test.\n\
-This is because the test forks to create a child process which then calls execv.\n\
-The TEST macro is NOT used.");
+	tst_sig(FORK, DEF_HANDLER, NULL);
 
 	TEST_PAUSE;
-
-	tst_tmpdir();
-}
-
-void cleanup(void)
-{
-	tst_rmdir();
 }
