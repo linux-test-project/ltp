9433d53924a128a37b850842efb1783e1dffb8c7
Manjeet Pawar <manjeet.p@samsung.com>
2015-05-28 04:42:57 +0000

    device-drivers: tusb.c: Fix possible NULL pointer derefrence
    pdev was accessed before its NULL check, this patch fixes this.
    
    Signed-off-by: Manjeet Pawar <manjeet.p@samsung.com>
    Signed-off-by: Yogesh Gaur <yn.gaur@samsung.com>

--- testcases/kernel/device-drivers/usb/tusb/tusb.c
+++ testcases/kernel/device-drivers/usb/tusb/tusb.c
@@ -386,13 +386,14 @@ static int test_hcd_probe()
 	int rc;
 	struct usb_hcd *hcd = NULL;
 	struct pci_dev *pdev = ltp_usb.pdev;
-	struct pci_device_id *id =
-	    (struct pci_device_id *)pdev->driver->id_table;
+	struct pci_device_id *id = NULL;
 
 	if (!pdev) {
 		printk("tusb: pdev pointer not set\n");
 		return 1;
 	}
+
+	id = (struct pci_device_id *)pdev->driver->id_table;
 
 	if (!id || !id->driver_data) {
 		printk("tusb: id_table not set\n");
