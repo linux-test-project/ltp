2e54c0080cfcdf358bb3b3e56977925e49910136
Cyril Hrubis <chrubis@suse.cz>
2016-02-29 16:48:03 +0100

    syscalls/llistxattr02: Convert to new API
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/llistxattr/llistxattr02.c
+++ testcases/kernel/syscalls/llistxattr/llistxattr02.c
@@ -34,15 +34,13 @@
 #include <sys/types.h>
 
 #ifdef HAVE_ATTR_XATTR_H
-#include <attr/xattr.h>
+# include <attr/xattr.h>
 #endif
 
-#include "test.h"
-#include "safe_macros.h"
-
-char *TCID = "llistxattr02";
+#include "tst_test.h"
 
 #ifdef HAVE_ATTR_XATTR_H
+
 #define SECURITY_KEY	"security.ltptest"
 #define VALUE	"test"
 #define VALUE_SIZE	4
@@ -60,45 +58,21 @@ static struct test_case {
 	{(char *)-1, 20, EFAULT}
 };
 
-static void verify_llistxattr(struct test_case *tc);
-static void setup(void);
-static void cleanup(void);
-
-int TST_TOTAL = ARRAY_SIZE(tc);
-
-int main(int ac, char **av)
+static void verify_llistxattr(unsigned int n)
 {
-	int lc;
-	int i;
+	struct test_case *t = tc + n;
+	char buf[t->size];
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-		for (i = 0; i < TST_TOTAL; i++)
-			verify_llistxattr(&tc[i]);
-	}
-
-	cleanup();
-	tst_exit();
-}
-
-static void verify_llistxattr(struct test_case *tc)
-{
-	char buf[tc->size];
-
-	TEST(llistxattr(tc->path, buf, tc->size));
+	TEST(llistxattr(t->path, buf, t->size));
 	if (TEST_RETURN != -1) {
-		tst_resm(TFAIL, "llistxattr() succeeded unexpectedly");
+		tst_res(TFAIL, "llistxattr() succeeded unexpectedly");
 	} else {
-		if (TEST_ERRNO != tc->exp_err) {
-			tst_resm(TFAIL | TTERRNO, "llistxattr() failed "
+		if (TEST_ERRNO != t->exp_err) {
+			tst_res(TFAIL | TTERRNO, "llistxattr() failed "
 				 "unexpectedlly, expected %s",
-				 tst_strerrno(tc->exp_err));
+				 tst_strerrno(t->exp_err));
 		} else {
-			tst_resm(TPASS | TTERRNO,
+			tst_res(TPASS | TTERRNO,
 				 "llistxattr() failed as expected");
 		}
 	}
@@ -108,37 +82,30 @@ static void setup(void)
 {
 	int n;
 
-	tst_require_root();
+	SAFE_TOUCH("testfile", 0644, NULL);
 
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	SAFE_TOUCH(cleanup, "testfile", 0644, NULL);
-
-	SAFE_SYMLINK(cleanup, "testfile", "symlink");
+	SAFE_SYMLINK("testfile", "symlink");
 
 	n = lsetxattr("symlink", SECURITY_KEY, VALUE, VALUE_SIZE, XATTR_CREATE);
 	if (n == -1) {
 		if (errno == ENOTSUP) {
-			tst_brkm(TCONF, cleanup, "no xattr support in fs or "
+			tst_brk(TCONF, "no xattr support in fs or "
 				 "mounted without user_xattr option");
 		} else {
-			tst_brkm(TBROK | TERRNO, cleanup, "lsetxattr() failed");
+			tst_brk(TBROK | TERRNO, "lsetxattr() failed");
 		}
 	}
 }
 
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.tid = "llistxattr02",
+	.needs_tmpdir = 1,
+	.needs_root = 1,
+	.test = verify_llistxattr,
+	.tcnt = ARRAY_SIZE(tc),
+	.setup = setup,
+};
 
 #else /* HAVE_ATTR_XATTR_H */
-int main(int ac, char **av)
-{
-	tst_brkm(TCONF, NULL, "<attr/xattr.h> does not exist.");
-}
+	TST_TEST_TCONF("<attr/xattr.h> does not exist.");
 #endif
