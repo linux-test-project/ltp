0b1a91b28752a98df02e6210e3fb06d65806a449
Xiao Yang <yangx.jy@cn.fujitsu.com>
2016-03-02 19:01:27 +0800

    lapi/socket.h: fix typo
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>

--- include/lapi/socket.h
+++ include/lapi/socket.h
@@ -1,6 +1,6 @@
 /*
-* Copyright (c) 20156 Fujitsu Ltd.
-* Author: Xiao Yang <fenggw-fnst@cn.fujitsu.com>
+* Copyright (c) 2016 Fujitsu Ltd.
+* Author: Xiao Yang <yangx.jy@cn.fujitsu.com>
 *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License as
