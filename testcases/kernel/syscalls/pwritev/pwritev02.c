8f73e2f3029488ed6592e27eaf0c59f7393d2e05
Xiao Yang <yangx.jy@cn.fujitsu.com>
2016-04-01 13:14:40 +0800

    pwritev/pwritev02.c: add specific error for pwritev()
    1) pwritev(2) fails when attempts to write from a invalid address
       and set errno to EFAULT.
    2) pwritev(2) fails if file descriptor is invalid and set errno
       to EBADF.
    3) pwritev(2) fails if file descriptor is not open for writing
       and set errno to EBADF.
    
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>

--- testcases/kernel/syscalls/pwritev/pwritev02.c
+++ testcases/kernel/syscalls/pwritev/pwritev02.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2015 Fujitsu Ltd.
+* Copyright (c) 2015-2016 Fujitsu Ltd.
 * Author: Xiao Yang <yangx.jy@cn.fujitsu.com>
 *
 * This program is free software; you can redistribute it and/or modify it
@@ -21,14 +21,21 @@
 * 1) pwritev(2) fails if iov_len is invalid.
 * 2) pwritev(2) fails if the vector count iovcnt is less than zero.
 * 3) pwritev(2) fails if offset is negative.
+* 4) pwritev(2) fails when attempts to write from a invalid address
+* 5) pwritev(2) fails if file descriptor is invalid.
+* 6) pwritev(2) fails if file descriptor is not open for writing.
 *
 * Expected Result:
 * 1) pwritev(2) should return -1 and set errno to EINVAL.
 * 2) pwritev(2) should return -1 and set errno to EINVAL.
 * 3) pwritev(2) should return -1 and set errno to EINVAL.
+* 4) pwritev(2) should return -1 and set errno to EFAULT.
+* 5) pwritev(2) should return -1 and set errno to EBADF.
+* 6) pwritev(2) should return -1 and set errno to EBADF.
 */
 
 #include <errno.h>
+#include <sys/uio.h>
 
 #include "test.h"
 #include "pwritev.h"
@@ -36,7 +43,9 @@
 
 #define CHUNK           64
 
-static int fd;
+static int fd1;
+static int fd2;
+static int fd3 = -1;
 static char buf[CHUNK];
 
 static struct iovec wr_iovec1[] = {
@@ -45,24 +54,33 @@ static struct iovec wr_iovec1[] = {
 
 static struct iovec wr_iovec2[] = {
 	{buf, CHUNK},
+	{(char *)-1, CHUNK},
 };
 
 static struct test_case_t {
+	int *fd;
 	struct iovec *name;
 	int count;
 	off_t offset;
+	int exp_err;
 } tc[] = {
 	/* test1 */
-	{wr_iovec1, 1, 0},
+	{&fd1, wr_iovec1, 1, 0, EINVAL},
 	/* test2 */
-	{wr_iovec2, -1, 0},
+	{&fd1, wr_iovec2, -1, 0, EINVAL},
 	/* test3 */
-	{wr_iovec2, 1, -1}
+	{&fd1, wr_iovec2, 1, -1, EINVAL},
+	/* test4 */
+	{&fd1, wr_iovec2, 2, 0, EFAULT},
+	/* test5 */
+	{&fd3, wr_iovec2, 1, 0, EBADF},
+	/* test6 */
+	{&fd2, wr_iovec2, 1, 0, EBADF}
 };
 
-void verify_pwritev(struct test_case_t *tc);
-void setup(void);
-void cleanup(void);
+static void verify_pwritev(struct test_case_t *tc);
+static void setup(void);
+static void cleanup(void);
 
 char *TCID = "pwritev02";
 int TST_TOTAL = ARRAY_SIZE(tc);
@@ -86,22 +104,24 @@ int main(int ac, char **av)
 	tst_exit();
 }
 
-void verify_pwritev(struct test_case_t *tc)
+static void verify_pwritev(struct test_case_t *tc)
 {
-	TEST(pwritev(fd, tc->name, tc->count, tc->offset));
+	TEST(pwritev(*tc->fd, tc->name, tc->count, tc->offset));
 	if (TEST_RETURN == 0) {
-		tst_resm(TFAIL, "pwritev(2) succeed unexpectedly");
+		tst_resm(TFAIL, "pwritev() succeeded unexpectedly");
 	} else {
-		if (TEST_ERRNO == EINVAL) {
-			tst_resm(TPASS | TTERRNO, "pwritev(2) fails as expected");
+		if (TEST_ERRNO == tc->exp_err) {
+			tst_resm(TPASS | TTERRNO,
+				 "pwritev() failed as expected");
 		} else {
-			tst_resm(TFAIL | TTERRNO, "pwritev(2) fails unexpectedly,"
-				 " expected errno is EINVAL");
+			tst_resm(TFAIL | TTERRNO,
+				 "pwritev() failed unexpectedly, expected"
+				 " errno is %s", tst_strerrno(tc->exp_err));
 		}
 	}
 }
 
-void setup(void)
+static void setup(void)
 {
 	if ((tst_kvercmp(2, 6, 30)) < 0) {
 		tst_brkm(TCONF, NULL, "This test can only run on kernels"
@@ -114,12 +134,17 @@ void setup(void)
 
 	tst_tmpdir();
 
-	fd = SAFE_OPEN(cleanup, "file", O_RDWR | O_CREAT, 0644);
+	fd1 = SAFE_OPEN(cleanup, "file", O_RDWR | O_CREAT, 0644);
+
+	fd2 = SAFE_OPEN(cleanup, "file", O_RDONLY | O_CREAT, 0644);
 }
 
-void cleanup(void)
+static void cleanup(void)
 {
-	if (fd > 0 && close(fd))
+	if (fd1 > 0 && close(fd1))
+		tst_resm(TWARN | TERRNO, "failed to close file");
+
+	if (fd2 > 0 && close(fd2))
 		tst_resm(TWARN | TERRNO, "failed to close file");
 
 	tst_rmdir();
