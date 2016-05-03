cb82d90c32f85001cc53290f765272678a628a7f
Cyril Hrubis <chrubis@suse.cz>
2016-05-03 16:20:40 +0200

    syscalls/syscall01: Cleanup && convert to newlib.
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/syscall/syscall01.c
+++ testcases/kernel/syscalls/syscall/syscall01.c
@@ -1,204 +1,95 @@
 /*
+ * Copyright (c) International Business Machines  Corp., 2002
+ * 01/02/2003	Port to LTP avenkat@us.ibm.com
+ * 06/30/2001	Port to Linux nsharoff@us.ibm.com
  *
- *   Copyright (c) International Business Machines  Corp., 2002
+ * This program is free software;  you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License for more details.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program;  if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-
-/* 01/02/2003	Port to LTP avenkat@us.ibm.com*/
-/* 06/30/2001	Port to Linux	nsharoff@us.ibm.com */
 
 /*
- * NAME
- *	syscall1.c -- test syscall
- *
- * CALLS
- *	syscall
- *
- * ALGORITHM
- *	Use syscall to simulate some section 2 calls and make sure
- *	things work as expected.  Pretty simple, but if it works
- *	for a few it should work for all.
- *
- * RESTRICTIONS
- *	The syscall numbers are system dependent!!  They represent
- *	entries in a table and can be changed from kernel to kernel.
- *	They ARE differnet between vax 4.2BSD and our ported system.
+ * Basic test for syscall().
  */
 
-#include <stdio.h>		/* needed by testhead.h         */
-#include <syscall.h>
-#include <errno.h>
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 
-/*****	LTP Port	*****/
-#include "test.h"
+#include "tst_test.h"
+#include "linux_syscall_numbers.h"
 
-#define FAILED 0
-#define PASSED 1
-
-char *TCID = "syscall01";
-int local_flag = PASSED;
-int block_number;
-FILE *temp;
-int TST_TOTAL = 1;
-/*****	**	**	*****/
-
-//char progname[]= "syscall1()";
-
-#define ITER		500
-
-int t_flag;
-
-/*****	LTP Port	*****/
-void setup();
-int blenter();
-int blexit();
-int anyfail();
-void cleanup();
-void do_setpg();
-void fail_exit();
-/*****	**	**	*****/
-
-/*--------------------------------------------------------------*/
-int main(int argc, char *argv[])
+static void verify_getpid(void)
 {
-	register int i;
-	int v1, v2;
+	pid_t p1, p2;
 
-	setup();		/* temp file is now open        */
-/*--------------------------------------------------------------*/
-	blenter();
+	p1 = getpid();
+	p2 = syscall(SYS_getpid);
 
-#if defined(SYS_getpid)
-	for (i = 0; i < ITER; i++) {
-		v1 = getpid();
-		v2 = syscall(SYS_getpid);
-		if (v1 != v2) {
-			fprintf(temp, "\tgetpid syscall failed.\n");
-			fprintf(temp, "\t  iteration %d\n", i);
-			local_flag = FAILED;
-			break;
-		}
+	if (p1 == p2) {
+		tst_res(TPASS, "getpid() == syscall(SYS_getpid)");
+	} else {
+		tst_res(TFAIL, "getpid() = %i, syscall(SYS_getpid) = %i",
+			p1, p2);
 	}
-#else
-	fprintf(temp, "\tgetpid syscall failed.\n");
-	fprintf(temp, "\tSYS_getpid not defined\n");
-	local_flag = FAILED;
-#endif
-	blexit();
-/*--------------------------------------------------------------*/
-	blenter();
+}
 
-#if defined(SYS_getuid) || defined(SYS_getuid32)
-	for (i = 0; i < ITER; i++) {
-		v1 = getuid();
-#if defined(SYS_getuid)
-		v2 = syscall(SYS_getuid);
-#else
-		v2 = syscall(SYS_getuid32);
-#endif
-		if (v1 != v2) {
-			fprintf(temp, "\tgetuid syscall failed.\n");
-			fprintf(temp, "\t  iteration %d\n", i);
-			local_flag = FAILED;
-			break;
-		}
+static void verify_getuid(void)
+{
+	uid_t u1, u2;
+
+	u1 = getuid();
+	u2 = syscall(SYS_getuid);
+
+	if (u1 == u2) {
+		tst_res(TPASS, "getuid() == syscall(SYS_getuid)");
+	} else {
+		tst_res(TFAIL, "getuid() = %i, syscall(SYS_getuid) = %i",
+			u1, u2);
 	}
-#else
-	fprintf(temp, "\tgetuid syscall failed.\n");
-	fprintf(temp, "\tSYS_getuid and SYS_getuid32 not defined\n");
-	local_flag = FAILED;
-#endif
-	blexit();
-/*--------------------------------------------------------------*/
-	blenter();
+}
 
-#if defined(SYS_getgid) || defined(SYS_getgid32)
-	for (i = 0; i < ITER; i++) {
-		v1 = getgid();
-#if defined(SYS_getgid)
-		v2 = syscall(SYS_getgid);
-#else
-		v2 = syscall(SYS_getgid32);
-#endif
-		if (v1 != v2) {
-			fprintf(temp, "\tgetgid syscall failed.\n");
-			fprintf(temp, "\t  iteration %d\n", i);
-			local_flag = FAILED;
-			break;
-		}
+static void verify_getgid(void)
+{
+	gid_t g1, g2;
+
+	g1 = getgid();
+	g2 = syscall(SYS_getgid);
+
+	if (g1 == g2) {
+		tst_res(TPASS, "getgid() == syscall(SYS_getgid)");
+	} else {
+		tst_res(TFAIL, "getgid() = %i, syscall(SYS_getgid) = %i",
+			g1, g2);
 	}
-#else
-	fprintf(temp, "\tgetgid syscall failed.\n");
-	fprintf(temp, "\tSYS_getgid and SYS_getgid32 not defined\n");
-	local_flag = FAILED;
-#endif
-
-	blexit();
-/*--------------------------------------------------------------*/
-
-    /***************************************************************
-     * cleanup and exit
-     ***************************************************************/
-	cleanup();
-	tst_exit();
-
-	anyfail();		/* THIS CALL DOES NOT RETURN - EXITS!!  */
-
 }
 
-/*--------------------------------------------------------------*/
 
-/*****	LTP Port	*****/
-/* functions */
+static void (*tcases[])(void) = {
+	verify_getpid,
+	verify_getuid,
+	verify_getgid,
+};
 
-void cleanup(void)
+static void verify_syscall(unsigned int n)
 {
-
+	tcases[n]();
 }
 
-void setup(void)
-{
+static struct tst_test test = {
+	.tid = "syscall01",
+	.test = verify_syscall,
+	.tcnt = ARRAY_SIZE(tcases),
+};
 
-	temp = stderr;
-
-}
-
-int blenter(void)
-{
-	local_flag = PASSED;
-	return 0;
-}
-
-int blexit(void)
-{
-	(local_flag == PASSED) ? tst_resm(TPASS, "Test passed")
-	    : tst_resm(TFAIL, "Test failed");
-	return 0;
-}
-
-int anyfail(void)
-{
-	tst_exit();
-}
-
-void fail_exit(void)
-{
-	local_flag = FAILED;
-	blexit();
-	anyfail();
-}
-
-/*****	**	**	*****/
