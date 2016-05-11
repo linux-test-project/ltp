296b5218bd592b274e54171866ca9be6fbb4c501
Chunyu Hu <chuhu@redhat.com>
2016-05-11 19:36:29 +0800

    ftrace_stress: update the trace_options test
    The ftrace_trace_options.sh is using hard coded trace options, including
    the outdated 'branch', so let's get the trace_options dynamicly from the
    trace_options file.
    
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/tracing/ftrace_test/ftrace_stress/ftrace_trace_options.sh
+++ testcases/kernel/tracing/ftrace_test/ftrace_stress/ftrace_trace_options.sh
@@ -13,30 +13,46 @@
 #                                                                             #
 ###############################################################################
 
+. test.sh
+
 LOOP=200
 
-trace_options=(print-parent sym-offset sym-addr verbose raw hex bin block trace_printk ftrace_preempt branch annotate userstacktrace sym-userobj printk-msg-only context-info latency-format sleep-time graph-time)
+while true; do
+	j=0;
+	while [ $j -lt $LOOP ]; do
+		trace_options="$(ls $TRACING_PATH/options/)"
+		# enable the nop_test_refuse can cause an
+		# 'write error: Invalid argument'. So don't test it.
+		trace_options="$(echo $trace_options | sed 's/test_nop_refuse//')"
+		nr_trace_options=$(echo "${trace_options}" | wc -w)
 
-NR_TRACE_OPTIONS=19
-
-for ((; ; ))
-{
-	for ((j = 0; j < $LOOP; j++))
-	{
-		num=`date +%N`
-		num=`printf 1%s $num`
-
-		for ((i = 0; i < $NR_TRACE_OPTIONS; i++))
-		{
-			n=$(( ( $num >> $i ) % 2 ))
+		option_index=$(tst_random 1 $nr_trace_options)
+		option=$(echo "$trace_options" | awk "{print \$$option_index}")
+		i=0
+		while [ $i -lt $nr_trace_options ]; do
+			n=$(tst_random 0 1)
+			opt_f="$TRACING_PATH"/options/$option
+			ret_val=0
 			if [ $n -eq 0 ]; then
-				echo 0 > "$TRACING_PATH"/options/${trace_options[$i]}
+				operation="setup"
 			else
-				echo 1 > "$TRACING_PATH"/options/${trace_options[$i]}
+				operation="clear"
 			fi
-		}
-	}
+			# On old kernel, some trace option dirs
+			# won't be made if the option has nothing
+			# to do with the current tracer. But on newer
+			# kernel(4.4-rc1), all option dirs will be made.
+			# So here check it to avoid 'Permision denied'
+			if [ -f $opt_f ]; then
+				echo $n > $opt_f
+				ret_val=$?
+			fi
 
-	sleep 1
-}
-
+			if [ $ret_val -ne 0 ]; then
+				tst_resm TFAIL "$0: $operation trace option $option failed"
+			fi
+			i=$((i + 1))
+		done
+		j=$((j + 1))
+	done
+done
