66cef985087189bfe09a66e0f8561a31717865b7
Dong Zhu <bluezhudong@gmail.com>
2016-06-02 21:58:31 +0800

    dma_thread_diotest: correct the number of running workers
    This test will cause Segmentation fault (core dumped) on a
    system with 20 CPUs and PAGESIZE == 64KB, because each worker
    is given PAGESIZE portion of a 1MB buffer.
    
    This patch is limiting number of running workers to number
    of PAGESIZE chunks that can fit into the buffer.
    
    Signed-off-by: Dong Zhu <bluezhudong@gmail.com>
    Reviewed-by: Jan Stancek <jstancek@redhat.com>

--- testcases/kernel/io/direct_io/dma_thread_diotest.c
+++ testcases/kernel/io/direct_io/dma_thread_diotest.c
@@ -113,10 +113,10 @@
 #define DIR_MODE	(S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP| \
 			 S_IXGRP|S_IROTH|S_IXOTH)
 #define FILECOUNT	100
-#define MIN_WORKERS	2
-#define MAX_WORKERS	256
 #define PATTERN		(0xfa)
 #define PAGE_SIZE	getpagesize()
+#define MIN_WORKERS	2
+#define MAX_WORKERS	(READSIZE/PAGE_SIZE)
 
 char *TCID = "dma_thread_diotest";
 int TST_TOTAL = 1;
@@ -229,6 +229,8 @@ int main(int argc, char *argv[])
 	int i, lc;
 
 	workers = sysconf(_SC_NPROCESSORS_ONLN);
+	if (workers > MAX_WORKERS)
+		workers = MAX_WORKERS;
 	tst_parse_opts(argc, argv, options, help);
 
 	setup();
@@ -439,6 +441,6 @@ static void cleanup(void)
 static void help(void)
 {
 	printf("-a align read buffer to offset <alignment>.\n");
-	printf("-w number of worker threads, 2 (default) to 256,"
-	       " defaults to number of cores.\n");
+	printf("-w number of worker threads, 2 (default) to %d,"
+	       " defaults to number of cores.\n", MAX_WORKERS);
 }
