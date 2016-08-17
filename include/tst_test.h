0aa4a61fb3c6c4993242f9d69bbae54ca782d32e
Xiao Yang <yangx.jy@cn.fujitsu.com>
2016-08-17 17:16:36 +0800

    tst_test.h: fix tst_test->tid for unsupported cases
    If some cases aren't supported by system, they fail with
    the following message:
      "tst_test.c:569: BROK: No tid set in test structure"
    We should return TCONF instead of TBROK.
    
    This has been broken by:
    
    commit f5f208bc067add80da32f6e3fd9a564d3c0ded0b
    Author: Cyril Hrubis <chrubis@suse.cz>
    Date:   Thu Aug 4 16:10:21 2016 +0200
    
        tst_test: Complain on NULL test->tid
    
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- include/tst_test.h
+++ include/tst_test.h
@@ -158,10 +158,9 @@ int main(int argc, char *argv[])
 
 #endif /* TST_NO_DEFAULT_MAIN */
 
-#define TST_TEST_TCONF(message)                                           \
-	static void tst_do_test(void) { tst_brk(TCONF, "%s", message); }; \
-        static struct tst_test test = { .test_all = tst_do_test }         \
-
+#define TST_TEST_TCONF(message)                                              \
+        static void tst_do_test(void) { tst_brk(TCONF, "%s", message); };    \
+        static struct tst_test test = { .test_all = tst_do_test, .tid = "" } \
 /*
  * This is a hack to make the testcases link without defining TCID
  */
