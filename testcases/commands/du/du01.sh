d40cbfb2372f68aef72ebdf167018d7374622ded
Cyril Hrubis <chrubis@suse.cz>
2016-09-26 18:31:03 +0200

    du01.sh: Convert to the new library
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/commands/du/du01.sh
+++ testcases/commands/du/du01.sh
@@ -20,17 +20,16 @@
 # Test du command with some basic options.
 #
 
-TCID=du01
-TST_TOTAL=23
-. test.sh
+TST_ID="du01"
+TST_CNT=23
+TST_SETUP=setup
+TST_TESTFUNC=do_test
+TST_NEEDS_TMPDIR=1
+TST_NEEDS_CMDS="dd du stat"
+. tst_test.sh
 
 setup()
 {
-	tst_check_cmds dd du stat
-
-	tst_tmpdir
-	TST_CLEANUP=cleanup
-
 	ROD_SILENT mkdir basedir
 	ROD_SILENT cd basedir
 
@@ -40,16 +39,17 @@ setup()
 
 	ROD_SILENT ln -s ../testfile testdir/testsymlink
 
+	page_size=$(getconf PAGESIZE)
+	if [ "$page_size" -lt 1024 ]; then
+		tst_brk TBROK "Page size < 1024"
+	fi
+	page_size=$(( page_size / 1024 ))
+
 	# Display values are in units of the first available SIZE
 	# from --block-size, and the DU_BLOCK_SIZE, BLOCK_SIZE and
 	# BLOCKSIZE environment variables. Here we need to
 	# set DU_BLOCK_SIZE to 1024 to ensure the result is expected.
 	export DU_BLOCK_SIZE=1024
-}
-
-cleanup()
-{
-	tst_rmdir
 }
 
 du_test()
@@ -62,32 +62,24 @@ du_test()
 	if [ ${test_return} -ne 0 ]; then
 		grep -q -E "unrecognized option|invalid option" ../temp
 		if [ $? -eq 0 ]; then
-			tst_resm TCONF "'$1' not supported"
+			tst_res TCONF "'$1' not supported"
 		else
-			tst_resm TFAIL "'$1' failed"
+			tst_res TFAIL "'$1' failed"
 		fi
 		return
 	fi
 
 	grep -q $2 ../temp
 	if [ $? -eq 0 ]; then
-		tst_resm TPASS "'$1' passed"
+		tst_res TPASS "'$1' passed"
 	else
-		tst_resm TFAIL "'$1' failed"
-		tst_resm TINFO "Looking for '$2' in:"
+		tst_res TFAIL "'$1' failed"
+		tst_res TINFO "Looking for '$2' in:"
 		cat ../temp
 	fi
 }
 
-setup
-
 block_size=512
-
-page_size=$(getconf PAGESIZE)
-if [ "$page_size" -lt 1024 ]; then
-	tst_brkm TBROK "Page size < 1024"
-fi
-page_size=$(( page_size / 1024 ))
 
 # The output could be different in some systems, if we use du to
 # estimate file space usage with the same filesystem and the same size.
@@ -104,28 +96,33 @@ check16="^10[2-3][0-9][0-9][[:space:]]testdir\/"
 check20="^11M[[:space:]]\."
 check23="^[0-9]\{1,2\}[[:space:]]\."
 
-du_test "du" ${check1}
-du_test "du testfile" ${check2}
-du_test "du -a" ${check3}
-du_test "du --all" ${check3}
-du_test "du -B ${block_size}" ${check5}
-du_test "du --block-size=${block_size}" ${check5}
-du_test "du -b" ${check7}
-du_test "du --bytes" ${check7}
-du_test "du -c" ${check9}
-du_test "du --total" ${check9}
-du_test "du -D testdir/testsymlink" ${check11}
-du_test "du --dereference-args testdir/testsymlink" ${check11}
-du_test "du --max-depth=1" ${check1}
-du_test "du --human-readable" ${check14}
-du_test "du -k" ${check1}
-du_test "du -L testdir/" ${check16}
-du_test "du --dereference testdir/" ${check16}
-du_test "du -P" ${check1}
-du_test "du --no-dereference" ${check1}
-du_test "du --si" ${check20}
-du_test "du -s" ${check1}
-du_test "du --summarize" ${check1}
-du_test "du --exclude=testfile" ${check23}
+do_test()
+{
+	case $1 in
+	1) du_test "du" ${check1};;
+	2) du_test "du testfile" ${check2};;
+	3) du_test "du -a" ${check3};;
+	4) du_test "du --all" ${check3};;
+	5) du_test "du -B ${block_size}" ${check5};;
+	6) du_test "du --block-size=${block_size}" ${check5};;
+	7) du_test "du -b" ${check7};;
+	8) du_test "du --bytes" ${check7};;
+	9) du_test "du -c" ${check9};;
+	10) du_test "du --total" ${check9};;
+	11) du_test "du -D testdir/testsymlink" ${check11};;
+	12) du_test "du --dereference-args testdir/testsymlink" ${check11};;
+	13) du_test "du --max-depth=1" ${check1};;
+	14) du_test "du --human-readable" ${check14};;
+	15) du_test "du -k" ${check1};;
+	16) du_test "du -L testdir/" ${check16};;
+	17) du_test "du --dereference testdir/" ${check16};;
+	18) du_test "du -P" ${check1};;
+	19) du_test "du --no-dereference" ${check1};;
+	20) du_test "du --si" ${check20};;
+	21) du_test "du -s" ${check1};;
+	22) du_test "du --summarize" ${check1};;
+	23) du_test "du --exclude=testfile" ${check23};;
+	esac
+}
 
-tst_exit
+tst_run
