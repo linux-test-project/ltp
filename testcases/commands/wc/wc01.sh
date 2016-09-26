fe19c37a809d82dcd4fe5db866d641a97b5841b2
Cyril Hrubis <chrubis@suse.cz>
2016-09-26 18:29:40 +0200

    wc01.sh: Convert to the new library
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/commands/wc/wc01.sh
+++ testcases/commands/wc/wc01.sh
@@ -16,26 +16,19 @@
 # Test wc command with some basic options.
 #
 
-TCID=wc01.sh
-TST_TOTAL=12
-. test.sh
+TST_ID="wc01"
+TST_CNT=12
+TST_SETUP=setup
+TST_TESTFUNC=do_test
+TST_NEEDS_TMPDIR=1
+TST_NEEDS_CMDS="wc"
+. tst_test.sh
 
 setup()
 {
-	tst_check_cmds wc
-
-	tst_tmpdir
-
-	TST_CLEANUP="cleanup"
-
 	echo "hello world" > ltp_wc
 
 	echo "This is a test" >> ltp_wc
-}
-
-cleanup()
-{
-	tst_rmdir
 }
 
 wc_test()
@@ -50,9 +43,9 @@ wc_test()
 	if [ $? -ne 0 ]; then
 		grep -q -E "unknown option|invalid option" temp
 		if [ $? -eq 0 ]; then
-			tst_resm TCONF "$wc_cmd not supported."
+			tst_res TCONF "$wc_cmd not supported."
 		else
-			tst_resm TFAIL "$wc_cmd failed."
+			tst_res TFAIL "$wc_cmd failed."
 		fi
 		return
 	fi
@@ -60,28 +53,30 @@ wc_test()
 	if [ $# -gt 1 ]; then
 		local act_out=`cat temp | awk '{printf $1}'`
 		if [ $act_out -ne $std_out ]; then
-			tst_resm TFAIL "$wc_cmd got mismatched data."
+			tst_res TFAIL "$wc_cmd got mismatched data."
 			return
 		fi
 	fi
 
-	tst_resm TPASS "wc passed with $wc_opt option."
+	tst_res TPASS "wc passed with $wc_opt option."
 }
 
+do_test()
+{
+	case $1 in
+	1) wc_test "-c" ltp_wc 27;;
+	2) wc_test "--bytes" ltp_wc 27;;
+	3) wc_test "-l" ltp_wc 2;;
+	4) wc_test "--lines" ltp_wc 2;;
+	5) wc_test "-L" ltp_wc 14;;
+	6) wc_test "--max-line-length" ltp_wc 14;;
+	7) wc_test "-w" ltp_wc 6;;
+	8) wc_test "--words" ltp_wc 6;;
+	9) wc_test "-m" ltp_wc 27;;
+	10) wc_test "--chars" ltp_wc 27;;
+	11) wc_test "--help";;
+	12) wc_test "--version";;
+	esac
+}
 
-setup
-
-wc_test "-c" ltp_wc 27
-wc_test "--bytes" ltp_wc 27
-wc_test "-l" ltp_wc 2
-wc_test "--lines" ltp_wc 2
-wc_test "-L" ltp_wc 14
-wc_test "--max-line-length" ltp_wc 14
-wc_test "-w" ltp_wc 6
-wc_test "--words" ltp_wc 6
-wc_test "-m" ltp_wc 27
-wc_test "--chars" ltp_wc 27
-wc_test "--help"
-wc_test "--version"
-
-tst_exit
+tst_run
