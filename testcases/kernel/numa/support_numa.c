ad38c31ee46958c398356da85ad033ce17310668
Li Wang <liwang@redhat.com>
2016-12-14 18:08:43 +0800

    ltp/numa: cleanup work for numa/*
    * Remove useless test.sh
    
    * Remove outdated numa_node_size.c
      Note: abandoned in commit 67045d7530
    
    * Rewrite support_numa.c
      Note: No need to include test.h to get ltp standard
      PASS/FAIL as ordinary testcases, it's just an aided
      process which calling in numa.sh)
    
    * Update README document
    
    Signed-off-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/numa/support_numa.c
+++ testcases/kernel/numa/support_numa.c
@@ -1,33 +1,30 @@
 /******************************************************************************/
 /*                                                                            */
 /* Copyright (c) International Business Machines  Corp., 2007                 */
+/* Copyright (c) Linux Test Project, 2016                                     */
 /*                                                                            */
-/* This program is free software;  you can redistribute it and/or modify      */
+/* This program is free software: you can redistribute it and/or modify       */
 /* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or          */
+/* the Free Software Foundation, either version 3 of the License, or          */
 /* (at your option) any later version.                                        */
 /*                                                                            */
 /* This program is distributed in the hope that it will be useful,            */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  */
-/* the GNU General Public License for more details.                           */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of             */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              */
+/* GNU General Public License for more details.                               */
 /*                                                                            */
 /* You should have received a copy of the GNU General Public License          */
-/* along with this program;  if not, write to the Free Software               */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
+/* along with this program. If not, see <http://www.gnu.org/licenses/>.       */
 /*                                                                            */
 /******************************************************************************/
 
 /******************************************************************************/
 /*                                                                            */
-/* File:        support_numa.c                                                     */
+/* File:        support_numa.c                                                */
 /*                                                                            */
 /* Description: Allocates 1MB of memory and touches it to verify numa         */
 /*                                                                            */
 /* Author:      Sivakumar Chinnaiah  Sivakumar.C@in.ibm.com                   */
-/*                                                                            */
-/* History:     Created - Jul 18 2007 - Sivakumar Chinnaiah                   */
-/*                                                 Sivakumar.C@in.ibm.com     */
 /*                                                                            */
 /******************************************************************************/
 
@@ -38,81 +35,70 @@
 #include <signal.h>
 #include <limits.h>
 #include <string.h>
-#include "test.h"
 
 /* Global Variables */
 #define MB (1<<20)
 #define PAGE_SIZE getpagesize()
 #define barrier() __asm__ __volatile__("": : :"memory")
 
-/* Extern Global Variables */
-extern int tst_count;		/* to avoid compilation errors. */
-extern char *TESTDIR;		/* to avoid compilation errors. */
-
-/* Global Variables */
-char *TCID = "support_numa";	/* to avoid compilation errors. */
-int TST_TOTAL = 1;		/* to avoid compilation errors. */
-
-void sigfunc(int sig)
+static void sigfunc(__attribute__ ((unused)) int sig)
 {
-	tst_resm(TINFO, "#Caught signal signum=%d", sig);
 }
 
-/******************************************************************************/
-/*                                                                            */
-/* Function:    main                                                          */
-/*                                                                            */
-/* Description: Alloctes 1MB of memory and touches it to verify numa behaviour*/
-/*                                                                            */
-/* Input:       Describe input arguments to this program                      */
-/*               argv[1] ==1 then print pagesize                              */
-/*               argv[1] ==2 then allocate 1MB of memory                      */
-/*		 argv[1] ==3 then pause the program to catch sigint	      */
-/*                                                                            */
-/* Exit:       On failure - Exits with non-zero value.                        */
-/*             On success - exits with 0 exit value.                          */
-/*                                                                            */
-/******************************************************************************/
+static void help(void)
+{
+	printf("Input:	Describe input arguments to this program\n");
+	printf("	argv[1] == 1 then print pagesize\n");
+	printf("	argv[1] == 2 then allocate 1MB of memory\n");
+	printf("	argv[1] == 3 then pause the program to catch sigint\n");
+	printf("Exit:	On failure - Exits with non-zero value\n");
+	printf("	On success - exits with 0 exit value\n");
+
+	exit(1);
+}
 
 int main(int argc, char *argv[])
 {
 	int i;
 	char *buf = NULL;
-	int count = 0;
 	struct sigaction sa;
+
+	if (argc != 2) {
+		fprintf(stderr, "Here expect only one number(i.e. 2) as the parameter.\n");
+		exit(1);
+	}
 
 	switch (atoi(argv[1])) {
 	case 1:
 		printf("%d", PAGE_SIZE);
-		tst_exit();
+		return 0;
 	case 2:
 		buf = malloc(MB);
 		if (!buf) {
-			tst_resm(TINFO, "#Memory is not available\n");
-			tst_exit();
+			fprintf(stderr, "Memory is not available\n");
 			exit(2);
 		}
 		for (i = 0; i < MB; i += PAGE_SIZE) {
-			count++;
 			buf[i] = 'a';
 			barrier();
 		}
 		free(buf);
-		tst_exit();
+		return 0;
 	case 3:
 		/* Trap SIGINT */
 		sa.sa_handler = sigfunc;
 		sa.sa_flags = SA_RESTART;
 		sigemptyset(&sa.sa_mask);
 		if (sigaction(SIGINT, &sa, 0) < 0) {
-			tst_brkm(TBROK, NULL, "#Sigaction SIGINT failed\n");
-			tst_exit();
-			exit(1);
+			fprintf(stderr, "Sigaction SIGINT failed\n");
+			exit(3);
 		}
 		/* wait for signat Int */
 		pause();
-		tst_exit();
+		return 0;
 	default:
-		exit(1);
+		help();
 	}
+
+	return 0;
 }
