7eb77fbfd803b3002c82887e69ceed255fa0c3f3
Cyril Hrubis <chrubis@suse.cz>
2017-02-08 12:01:47 +0100

    runltp,tst_device: Bump minimal device size to 256MB
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>
    Acked-by: Jan Stancek <jstancek@redhat.com>

--- lib/tst_device.c
+++ lib/tst_device.c
@@ -41,6 +41,7 @@
 #define LOOP_CONTROL_FILE "/dev/loop-control"
 
 #define DEV_FILE "test_dev.img"
+#define DEV_SIZE_MB 256
 
 static char dev_path[1024];
 static int device_acquired;
@@ -204,7 +205,7 @@ const char *tst_acquire_device__(unsigned int size)
 	unsigned int acq_dev_size;
 	uint64_t ltp_dev_size;
 
-	acq_dev_size = size > 150 ? size : 150;
+	acq_dev_size = size > DEV_SIZE_MB ? size : DEV_SIZE_MB;
 
 	dev = getenv("LTP_DEV");
 
