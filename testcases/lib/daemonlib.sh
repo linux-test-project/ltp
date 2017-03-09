034fc5038d88dc64078609922687ec5e2ea1c26e
Petr Vorel <pvorel@suse.cz>
2017-03-09 14:34:10 +0100

    daemonlib: Use is-active instead of status for systemctl
    'systemctl status service' exit code is broken in old distros. It
    returns 0 even if service is not running. As we use status_daemon for
    detecting whether service is running, it's more reliable to use
    is-active.
    
    systemctl bug was introduced in systemd v209 (f74294c1d), fixed in v213
    (5bb75bc74).
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/lib/daemonlib.sh
+++ testcases/lib/daemonlib.sh
@@ -69,7 +69,7 @@ stop_daemon()
 status_daemon()
 {
 	if [ $HAVE_SYSTEMCTL -eq 1 ]; then
-		systemctl status $1.service > /dev/null 2>&1
+		systemctl is-active $1.service > /dev/null 2>&1
 	elif command -v service >/dev/null 2>&1; then
 		service $1 status > /dev/null 2>&1
 	else
