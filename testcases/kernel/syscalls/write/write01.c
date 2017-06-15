c0227c25f2490f1b1da335f0f2ff9935bdc5587e
Jinhui Huang <huangjh.jy@cn.fujitsu.com>
2017-06-15 17:26:43 +0800

    syscalls/write01.c: Add test with different buffer sizes && Remove write02
    Signed-off-by: Jinhui Huang <huangjh.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/write/write01.c
+++ testcases/kernel/syscalls/write/write01.c
@@ -20,17 +20,10 @@
  * You should have received a copy of the GNU General Public License along
  * with this program; if not, write the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
  */
 
+#include <stdio.h>
+#include <string.h>
 #include <errno.h>
 #include "tst_test.h"
 
@@ -38,17 +31,36 @@ static int fd;
 
 static void verify_write(void)
 {
-	TEST(write(fd, "w", 1));
+	int i, badcount = 0;
+	char buf[BUFSIZ];
 
-	if (TEST_RETURN == -1)
-		tst_res(TFAIL | TTERRNO, "write(2) failed");
+	memset(buf, 'w', BUFSIZ);
+
+	SAFE_LSEEK(fd, 0, SEEK_SET);
+
+	for (i = BUFSIZ; i > 0; i--) {
+		TEST(write(fd, buf, i));
+		if (TEST_RETURN == -1) {
+			tst_res(TFAIL | TTERRNO, "write failed");
+			return;
+		}
+
+		if (TEST_RETURN != i) {
+			badcount++;
+			tst_res(TINFO, "write() returned %ld, expected %d",
+				TEST_RETURN, i);
+		}
+	}
+
+	if (badcount != 0)
+		tst_res(TFAIL, "write() failed to return proper count");
 	else
-		tst_res(TPASS, "write(2) returned %ld", TEST_RETURN);
+		tst_res(TPASS, "write() passed");
 }
 
 static void setup(void)
 {
-	fd = SAFE_OPEN("testfile", O_RDWR | O_CREAT, 0700);
+	fd = SAFE_OPEN("test_file", O_RDWR | O_CREAT, 0700);
 }
 
 static void cleanup(void)
@@ -58,8 +70,8 @@ static void cleanup(void)
 }
 
 static struct tst_test test = {
-	.test_all = verify_write,
 	.setup = setup,
 	.cleanup = cleanup,
+	.test_all = verify_write,
 	.needs_tmpdir = 1,
 };
