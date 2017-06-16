512cb343ef35cb952b87405787f2325106b5f48c
Helge Deller <deller@gmx.de>
2017-06-16 15:55:23 +0200

    mmap01: fix testcase on hppa architecture
    The mmap() syscall to map a file at any given page-aligned address (via the
    MAP_FIXED flag) can fail on the hppa architecture due to cache colouring
    requirements of the architecture.
    
    This patch drops the MAP_FIXED flag and thus maps the file instead at some
    convenient address based on the hint as given by the addr parameter. The tests
    covered by mmap01 check the *end* of the mmaped file for consistency, so
    mapping the beginning of the file at any address is okay without breaking the
    main intention of the mmap01 testcase.
    
    Since we now map the file at some random address as provided by the kernel drop
    the initialization of the to-be-mapped address as well.
    
    Signed-off-by: Helge Deller <deller@gmx.de>
    Acked-by: Jan Stancek <jstancek@redhat.com>

--- testcases/kernel/syscalls/mmap/mmap01.c
+++ testcases/kernel/syscalls/mmap/mmap01.c
@@ -80,8 +80,8 @@ int main(int ac, char **av)
 		 * with write access.
 		 */
 		errno = 0;
-		addr = mmap(addr, page_sz, PROT_READ | PROT_WRITE,
-			    MAP_FILE | MAP_SHARED | MAP_FIXED, fildes, 0);
+		addr = mmap(NULL, page_sz, PROT_READ | PROT_WRITE,
+			    MAP_FILE | MAP_SHARED, fildes, 0);
 
 		/* Check for the return value of mmap() */
 		if (addr == MAP_FAILED) {
@@ -181,23 +181,6 @@ static void setup(void)
 	if ((dummy = calloc(page_sz, sizeof(char))) == NULL) {
 		tst_brkm(TFAIL, cleanup, "calloc failed (dummy)");
 	}
-
-	/*
-	 * Initialize addr to align with the first segment boundary address
-	 * above the break address of the process.
-	 */
-	addr = (void *)(((intptr_t) sbrk(0) + (SHMLBA - 1)) & ~(SHMLBA - 1));
-
-	/* Set the break address of the process to the addr plus one
-	 * page size.
-	 */
-	if ((intptr_t) sbrk(SHMLBA + page_sz) == -1) {
-		tst_brkm(TFAIL | TERRNO, cleanup,
-			 "sbrk(SHMLBA + page_sz) failed");
-	}
-
-	/* Initialize one page region from addr with 'A' */
-	memset(addr, 'A', page_sz);
 
 	/* Create the command which will be executed in the test */
 	sprintf(cmd_buffer, "grep XYZ %s/%s > /dev/null", Path_name, TEMPFILE);
