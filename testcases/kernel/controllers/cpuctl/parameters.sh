ec1918b7cacdf1b57af88b6247b2da771d3525e7
Marcin Wolcendorf <mwolcendorf@suse.com>
2017-07-04 16:51:27 +0200

    Fix indentation

--- testcases/kernel/controllers/cpuctl/parameters.sh
+++ testcases/kernel/controllers/cpuctl/parameters.sh
@@ -16,17 +16,17 @@
 #                                                                               #
 #  You should have received a copy of the GNU General Public License            #
 #  along with this program;  if not, write to the Free Software                 #
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA      #
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA #
 #                                                                               #
 #################################################################################
 # Name Of File: parameters.sh                                                   #
 #                                                                               #
-# Description: 	This file has functions for the setup for testing cpucontroller #
+# Description:  This file has functions for the setup for testing cpucontroller #
 #               setup includes creating controller device, mounting it with     #
 #               cgroup filesystem with option cpu and creating groups in it.    #
 #                                                                               #
-# Functions:    get_num_groups(): decides number of groups based on num of cpus	#
-#               setup(): creaes /dev/cpuctl, mounts cgroup fs on it, creates 	#
+# Functions:    get_num_groups(): decides number of groups based on num of cpus #
+#               setup(): creaes /dev/cpuctl, mounts cgroup fs on it, creates    #
 #               groups in that, creates fifo to fire tasks at one time.         #
 #               cleanup(): Does full system cleanup                             #
 #                                                                               #
@@ -59,78 +59,78 @@ set_def_group() #default group spinning a task to create ideal scenario
 	done
 }
 
-get_num_groups()        # Number of tasks should be >= number of cpu's (to check scheduling fairness)
+get_num_groups()	# Number of tasks should be >= number of cpu's (to check scheduling fairness)
 {
-        NUM_GROUPS=$(( (NUM_CPUS*3 + 1)/2 ))
+	NUM_GROUPS=$(( (NUM_CPUS*3 + 1)/2 ))
 }
 
 	# Write the cleanup function
 cleanup ()
 {
-        echo "Cleanup called";
+	echo "Cleanup called";
 	killall cpuctl_def_task01 1>/dev/null 2>&1;
 	killall cpuctl_def_task02 1>/dev/null 2>&1;
 	killall cpuctl_task_* 1>/dev/null 2>&1;
 	sleep 1
-        rm -f cpuctl_task_* 2>/dev/null
+	rm -f cpuctl_task_* 2>/dev/null
 	for task in `cat /dev/cpuctl/group_def/tasks`; do
 		echo $task > /dev/cpuctl/tasks 2>/dev/null 1>&2;
 	done
-        rmdir /dev/cpuctl/group* 2> /dev/null
-        umount /dev/cpuctl 2> /dev/null
-        rmdir /dev/cpuctl 2> /dev/null
-        rm -f myfifo 2>/dev/null
+	rmdir /dev/cpuctl/group* 2> /dev/null
+	umount /dev/cpuctl 2> /dev/null
+	rmdir /dev/cpuctl 2> /dev/null
+	rm -f myfifo 2>/dev/null
 
 }
-        # Create /dev/cpuctl &  mount the cgroup file system with cpu controller
-        #clean any group created eralier (if any)
+	# Create /dev/cpuctl &	mount the cgroup file system with cpu controller
+	#clean any group created eralier (if any)
 
 do_setup ()
 {
-        if [ -e /dev/cpuctl ]
-        then
-                echo "WARN:/dev/cpuctl already exist..overwriting"; # or a warning ?
-                cleanup;
-                mkdir /dev/cpuctl;
-        else
-                mkdir /dev/cpuctl
-        fi
-        mount -t cgroup -ocpu cgroup /dev/cpuctl 2> /dev/null
-        if [ $? -ne 0 ]
-        then
-                echo "ERROR: Could not mount cgroup filesystem on /dev/cpuctl..Exiting test";
-                cleanup;
-                exit -1;
-        fi
+	if [ -e /dev/cpuctl ]
+	then
+		echo "WARN:/dev/cpuctl already exist..overwriting"; # or a warning ?
+		cleanup;
+		mkdir /dev/cpuctl;
+	else
+		mkdir /dev/cpuctl
+	fi
+	mount -t cgroup -ocpu cgroup /dev/cpuctl 2> /dev/null
+	if [ $? -ne 0 ]
+	then
+		echo "ERROR: Could not mount cgroup filesystem on /dev/cpuctl..Exiting test";
+		cleanup;
+		exit -1;
+	fi
 
-        # Group created earlier may again be visible if not cleaned properly...so clean them
+	# Group created earlier may again be visible if not cleaned properly...so clean them
 	groups=/dev/cpuctl/group*
-        if [ -z "$groups" ]
-        then
-                rmdir /dev/cpuctl/group*
-                echo "WARN: Earlier groups found and removed...";
-        fi
+	if [ -z "$groups" ]
+	then
+		rmdir /dev/cpuctl/group*
+		echo "WARN: Earlier groups found and removed...";
+	fi
 
-        #Create a fifo to make all tasks wait on it
-        mkfifo myfifo 2> /dev/null;
-        if [ $? -ne 0 ]
-        then
-                echo "ERROR: Can't create fifo...Check your permissions..Exiting test";
-                cleanup;
-                exit -1;
-        fi
+	#Create a fifo to make all tasks wait on it
+	mkfifo myfifo 2> /dev/null;
+	if [ $? -ne 0 ]
+	then
+		echo "ERROR: Can't create fifo...Check your permissions..Exiting test";
+		cleanup;
+		exit -1;
+	fi
 
-        # Create different groups
-        for i in $(seq 1 $NUM_GROUPS)
-        do
-                group=group_$i;
-                mkdir /dev/cpuctl/$group;# 2>/dev/null
-                if [ $? -ne 0 ]
-                then
-                        echo "ERROR: Can't create $group...Check your permissions..Exiting test";
-                        cleanup;
-                        exit -1;
-                fi
-        done
+	# Create different groups
+	for i in $(seq 1 $NUM_GROUPS)
+	do
+		group=group_$i;
+		mkdir /dev/cpuctl/$group;# 2>/dev/null
+		if [ $? -ne 0 ]
+		then
+			echo "ERROR: Can't create $group...Check your permissions..Exiting test";
+			cleanup;
+			exit -1;
+		fi
+	done
 }
 
