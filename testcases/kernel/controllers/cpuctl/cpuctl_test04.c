5143ee35a90e68112e6b88cc2f228f8e3b463c67
Michael Moese <mmoese@suse.de>
2018-01-24 14:03:04 +0100

    fix old style definition in cpuctl_test04.c
    Signed-off-by: Michael Moese <mmoese@suse.de>

--- testcases/kernel/controllers/cpuctl/cpuctl_test04.c
+++ testcases/kernel/controllers/cpuctl/cpuctl_test04.c
@@ -71,7 +71,7 @@ int TST_TOTAL = 2;
 pid_t scriptpid;
 char path[] = "/dev/cpuctl";
 
-extern void cleanup()
+extern void cleanup(void)
 {
 	kill(scriptpid, SIGUSR1);	/* Inform the shell to do cleanup */
 	tst_exit();		/* Report exit status */
