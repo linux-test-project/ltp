373a8bc480c3ff352b5480112dc1265826e7ffc2
Petr Vorel <pvorel@suse.cz>
2018-05-07 15:21:59 +0200

    net/{stress,virt}: Use SPDX-License-Identifier GPL-2.0-or-later
    to save space
    + add author in few cases.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/network/stress/ipsec/ipsec_lib.sh
+++ testcases/network/stress/ipsec/ipsec_lib.sh
@@ -1,24 +1,9 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
 # Copyright (c) 2016 Red Hat Inc.,  All Rights Reserved.
 # Copyright (c) 2016 Oracle and/or its affiliates. All Rights Reserved.
-# Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, see <http://www.gnu.org/licenses/>.
-#
 # Author: Hangbin Liu <haliu@redhat.com>
-#
-#######################################################################
 
 # Authenticated encryption with associated data
 AEALGO="rfc4106_128"
