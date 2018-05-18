3ff1bacb002189f4ccf372bf06bf11c1f5eea8c9
Xiao Yang <yangx.jy@cn.fujitsu.com>
2018-05-18 09:30:36 +0800

    syscalls/prctl01: Rewrite to the new library
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/prctl/prctl01.c
+++ testcases/kernel/syscalls/prctl/prctl01.c
@@ -1,174 +1,44 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * This is a Phase I test for the prctl(2) system call.
+ * It is intended to provide a limited exposure of the system call.
  */
-/**********************************************************
- *
- *    TEST IDENTIFIER	: prctl01
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for prctl(2)
- *
- *    TEST CASE TOTAL	: 2
- *
- *    AUTHOR		: Saji Kumar.V.R <saji.kumar@wipro.com>
- *
- *    SIGNALS
- * 	Uses SIGUSR1 to pause before test if option set.
- * 	(See the parse_opts(3) man page).
- *
- *    DESCRIPTION
- *	This is a Phase I test for the prctl(2) system call.
- *	It is intended to provide a limited exposure of the system call.
- *
- * 	Setup:
- * 	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- * 	Test:
- *	 Loop if the proper options are given.
- *	 fork a child
- *
- *	 CHILD:
- *		call prctl() with proper arguments
- *		If call succeeds,
- *			exit with 0
- *		else
- *			exit with 1
- *	 PARENT:
- *		wait() for child.
- *		If child exits with exit value 0,
- *			Test passed
- *		else
- *			Test Failed
- *
- * 	Cleanup:
- * 	  Print errno log and/or timing stats if options given
- *
- * USAGE:  <for command-line>
- *  prctl01 [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-h] [-f] [-p]
- *			where,  -c n : Run n copies concurrently.
- *				-e   : Turn on errno logging.
- *				-h   : Show help screen
- *				-f   : Turn off functional testing
- *				-i n : Execute test n times.
- *				-I x : Execute test for x seconds.
- *				-p   : Pause for SIGUSR1 before starting
- *				-P x : Pause for x seconds between iterations.
- *				-t   : Turn on syscall timing.
- *
- ****************************************************************/
 
 #include <errno.h>
 #include <signal.h>
 #include <sys/prctl.h>
-#include <sys/wait.h>
 
-#include "test.h"
+#include "tst_test.h"
 
-static void setup(void);
-static void cleanup(void);
-
-char *TCID = "prctl01";
-
-int option[2] = { PR_GET_PDEATHSIG, PR_SET_PDEATHSIG };
-
-int TST_TOTAL = 2;
-
-int main(int ac, char **av)
+static void verify_prctl(void)
 {
+	int get_sig = 0;
 
-	int lc, i;
-	pid_t child_pid;
-	int status, sig;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; ++i) {
-
-			switch (child_pid = FORK_OR_VFORK()) {
-
-			case -1:
-				/* fork() failed */
-				tst_resm(TFAIL, "fork() failed");
-				continue;
-
-			case 0:
-				/* Child */
-				if (i == 1) {
-					sig = SIGUSR2;
-					TEST(prctl(option[i], sig));
-				} else {
-					TEST(prctl(option[i], &sig));
-				}
-
-				if (TEST_RETURN == 0) {
-					exit(0);
-				} else {
-					tst_resm(TWARN | TTERRNO,
-						 "prctl() returned %ld",
-						 TEST_RETURN);
-					exit(1);
-				}
-
-			default:
-				/* Parent */
-				if ((waitpid(child_pid, &status, 0)) < 0) {
-					tst_resm(TFAIL, "waitpid() failed");
-					continue;
-				}
-
-				if ((WIFEXITED(status)) &&
-				    (WEXITSTATUS(status) == 0)) {
-					tst_resm(TPASS, "Test Passed");
-				} else {
-					tst_resm(TFAIL, "Test Failed");
-				}
-
-			}
-		}
+	TEST(prctl(PR_SET_PDEATHSIG, SIGUSR2));
+	if (TEST_RETURN == -1) {
+		tst_res(TFAIL | TTERRNO, "prctl(PR_SET_PDEATHSIG) failed");
+		return;
 	}
 
-	/* cleanup and exit */
-	cleanup();
-	tst_exit();
+	tst_res(TPASS, "prctl(PR_SET_PDEATHSIG) succeeded");
 
+	TEST(prctl(PR_GET_PDEATHSIG, &get_sig));
+	if (TEST_RETURN == -1) {
+		tst_res(TFAIL | TTERRNO, "prctl(PR_GET_PDEATHSIG) failed");
+		return;
+	}
+
+	if (get_sig == SIGUSR2) {
+		tst_res(TPASS,
+			"prctl(PR_GET_PDEATHSIG) got expected death signal");
+	} else {
+		tst_res(TFAIL, "prctl(PR_GET_PDEATHSIG) got death signal %d, expected %d",
+			get_sig, SIGUSR2);
+	}
 }
 
-/* setup() - performs all ONE TIME setup for this test */
-void setup(void)
-{
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-}
-
-/*
- *cleanup() -  performs all ONE TIME cleanup for this test at
- *		completion or premature exit.
- */
-void cleanup(void)
-{
-
-}
+static struct tst_test test = {
+	.test_all = verify_prctl,
+};
