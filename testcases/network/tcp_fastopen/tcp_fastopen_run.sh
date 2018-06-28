a0092f6e98aacd72deef5a424b695971a5f9a3a6
Alexey Kodanev <alexey.kodanev@oracle.com>
2018-06-28 16:54:56 +0300

    network/tcp_fastopen: convert to new API
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/network/tcp_fastopen/tcp_fastopen_run.sh
+++ testcases/network/tcp_fastopen/tcp_fastopen_run.sh
@@ -1,53 +1,39 @@
 #!/bin/sh
-# Copyright (c) 2014-2017 Oracle and/or its affiliates. All Rights Reserved.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write the Free Software Foundation,
-# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2014-2018 Oracle and/or its affiliates. All Rights Reserved.
 #
 # Author: Alexey Kodanev <alexey.kodanev@oracle.com>
-#
 
-TST_NETLOAD_MAX_SRV_REPLIES=3
+TST_SETUP="setup"
+TST_TESTFUNC="test"
+TST_CNT=2
+TST_CLEANUP="cleanup"
+TST_MIN_KVER="3.7"
+TST_NEEDS_TMPDIR=1
+TST_NEEDS_ROOT=1
+TST_NEEDS_CMDS="tc"
+TST_OPTS="R:"
+TST_USAGE=tcp_fastopen_usage
+TST_PARSE_ARGS=tcp_fastopen_parse_args
+
+srv_replies=3
+
+tcp_fastopen_usage()
+{
+	echo "-R x    Number of requests, after which connection is closed"
+}
 
 tcp_fastopen_parse_args()
 {
 	case "$1" in
-	h)
-		echo "Usage:"
-		echo "h        help"
-		echo "R x      num of requests, after which conn. closed"
-		echo "6        run over IPv6"
-		exit 0
-	;;
-	R) TST_NETLOAD_MAX_SRV_REPLIES=$2 ;;
-	*) tst_brkm TBROK "unknown option: $1" ;;
+	R) srv_replies=$2 ;;
 	esac
 }
 
-TST_TOTAL=1
-TCID="tcp_fastopen"
-TST_NEEDS_TMPDIR=1
-
-TST_OPTS="hr:n:R:"
-TST_PARSE_ARGS=tcp_fastopen_parse_args
-
-TST_USE_LEGACY_API=1
 . tst_net.sh
 
 cleanup()
 {
-	tst_rmdir
 	tc qdisc del dev $(tst_iface) root netem delay 100 >/dev/null
 }
 
@@ -56,44 +42,44 @@ compare()
 	tfo_cmp=$(( 100 - ($time_tfo_on * 100) / $time_tfo_off ))
 
 	if [ "$tfo_cmp" -lt 3 ]; then
-		tst_resm TFAIL "$1 perf result is '$tfo_cmp' percent"
+		tst_res TFAIL "$1 perf result is '$tfo_cmp' percent"
 	else
-		tst_resm TPASS "$1 perf result is '$tfo_cmp' percent"
+		tst_res TPASS "$1 perf result is '$tfo_cmp' percent"
 	fi
 }
 
-tst_require_root
+setup()
+{
+	if tst_kvcmp -lt "3.16" && [ "$TST_IPV6" ]; then
+		tst_brk TCONF "test must be run with kernel 3.16 or newer"
+	fi
 
-if tst_kvcmp -lt "3.7"; then
-	tst_brkm TCONF "test must be run with kernel 3.7 or newer"
-fi
+	ROD tc qdisc add dev $(tst_iface) root netem delay 100
+}
 
-if tst_kvcmp -lt "3.16" && [ "$TST_IPV6" ]; then
-	tst_brkm TCONF "test must be run with kernel 3.16 or newer"
-fi
+test1()
+{
+	tst_res TINFO "using old TCP API and set tcp_fastopen to '0'"
+	tst_netload -H $(tst_ipaddr rhost) -t 0 -R $srv_replies
+	time_tfo_off=$(cat tst_netload.res)
 
-trap "tst_brkm TBROK 'test interrupted'" INT
-TST_CLEANUP="cleanup"
+	tst_res TINFO "using new TCP API and set tcp_fastopen to '3'"
+	tst_netload -H $(tst_ipaddr rhost) -f -t 3 -R $srv_replies
+	time_tfo_on=$(cat tst_netload.res)
 
-ROD tc qdisc add dev $(tst_iface) root netem delay 100
+	compare
+}
 
-tst_resm TINFO "using old TCP API and set tcp_fastopen to '0'"
-tst_netload -H $(tst_ipaddr rhost) -t 0
-time_tfo_off=$(cat tst_netload.res)
+test2()
+{
+	tst_kvcmp -lt "4.11" && \
+		tst_brk TCONF "next test must be run with kernel 4.11 or newer"
 
-tst_resm TINFO "using new TCP API and set tcp_fastopen to '3'"
-tst_netload -H $(tst_ipaddr rhost) -f -t 3
-time_tfo_on=$(cat tst_netload.res)
+	tst_res TINFO "using connect() and TCP_FASTOPEN_CONNECT socket option"
+	tst_netload -H $(tst_ipaddr rhost) -F -t 3 -R $srv_replies
+	time_tfo_on=$(cat tst_netload.res)
 
-compare
+	compare
+}
 
-tst_kvcmp -lt "4.11" && \
-	tst_brkm TCONF "next test must be run with kernel 4.11 or newer"
-
-tst_resm TINFO "using connect() and TCP_FASTOPEN_CONNECT socket option"
-tst_netload -H $(tst_ipaddr rhost) -F -t 3
-time_tfo_on=$(cat tst_netload.res)
-
-compare
-
-tst_exit
+tst_run
