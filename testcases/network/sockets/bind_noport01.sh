93ad6db8413ec3d0253b89316b09c084b738d413
Alexey Kodanev <alexey.kodanev@oracle.com>
2018-07-03 10:12:29 +0300

    network/bind_noport01: convert to new API
    Also add the comment about requiring '-S' source parameter
    for this test.
    
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/network/sockets/bind_noport01.sh
+++ testcases/network/sockets/bind_noport01.sh
@@ -1,51 +1,21 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (c) 2018 Oracle and/or its affiliates.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-TST_TOTAL=1
-TCID="bind_noport"
+TST_TESTFUNC="test"
+TST_MIN_KVER="4.2"
 TST_NEEDS_TMPDIR=1
+TST_TEST_DATA="tcp udp udp_lite dccp"
 
-TST_USE_LEGACY_API=1
 . tst_net.sh
 
-cleanup()
+test()
 {
-	tst_rmdir
+	local type="$2"
+
+	tst_res TINFO "test IP_BIND_ADDRESS_NO_PORT with $type socket"
+	# when using '-S' parameter, netstress sets IP_BIND_ADDRESS_NO_PORT
+	tst_netload -T $type -S $(tst_ipaddr) -H $(tst_ipaddr rhost)
 }
 
-if tst_kvcmp -lt "4.2"; then
-	tst_brkm TCONF "test must be run with kernel 4.2+"
-fi
-
-trap "tst_brkm TBROK 'test interrupted'" INT
-TST_CLEANUP="cleanup"
-
-do_test()
-{
-	local types="tcp udp udp_lite dccp"
-	TST_NETLOAD_CLN_NUMBER=10
-	TST_NETLOAD_CLN_REQUESTS=1000
-
-	tst_resm TINFO "test IP_BIND_ADDRESS_NO_PORT with $types sockets"
-
-	for t in $types; do
-		tst_netload -T $t -S $(tst_ipaddr) -H $(tst_ipaddr rhost)
-	done
-}
-
-do_test
-
-tst_exit
+tst_run
