579f01298b501dccf23db8efd86fc25d2ffe1321
Alexey Kodanev <alexey.kodanev@oracle.com>
2018-07-03 10:27:15 +0300

    network/sctp01: convert to new API
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/network/sctp/sctp01.sh
+++ testcases/network/sctp/sctp01.sh
@@ -1,65 +1,35 @@
 #!/bin/sh
-# Copyright (c) 2017 Oracle and/or its affiliates. All Rights Reserved.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2017-2018 Oracle and/or its affiliates. All Rights Reserved.
 # Author: Alexey Kodanev <alexey.kodanev@oracle.com>
 
-TCID=sctp01
-TST_TOTAL=4
-TST_CLEANUP="cleanup"
+TST_TESTFUNC="test"
 TST_NEEDS_TMPDIR=1
+TST_NEEDS_ROOT=1
+TST_TEST_DATA=",-A 65000"
+TST_TEST_DATA_IFS=","
 
-TST_USE_LEGACY_API=1
 . tst_net.sh
 
-cleanup()
+test()
 {
-	tst_rmdir
-}
+	local opts="$2"
 
-setup()
-{
-	tst_require_root
-	TST_NETLOAD_MAX_SRV_REPLIES=3
-}
+	tst_res TINFO "compare TCP/SCTP performance"
 
-test_run()
-{
-	local opts
-
-	opts="$@"
-
-	tst_resm TINFO "compare TCP/SCTP performance"
-
-	tst_netload -H $(tst_ipaddr rhost) -T tcp $opts
+	tst_netload -H $(tst_ipaddr rhost) -T tcp -R 3 $opts
 	local res0="$(cat tst_netload.res)"
 
-	tst_netload -S $(tst_ipaddr) -H $(tst_ipaddr rhost) -T sctp $opts
+	tst_netload -S $(tst_ipaddr) -H $(tst_ipaddr rhost) -T sctp -R 3 $opts
 	local res1="$(cat tst_netload.res)"
 
 	local per=$(( $res0 * 100 / $res1 - 100 ))
 
 	if [ "$per" -gt "100" -o "$per" -lt "-100" ]; then
-		tst_resm TFAIL "sctp performance $per %"
+		tst_res TFAIL "sctp performance $per %"
 	else
-		tst_resm TPASS "sctp performance $per % in range -100 ... 100 %"
+		tst_res TPASS "sctp performance $per % in range -100 ... 100 %"
 	fi
 }
 
-setup
-test_run
-test_run -A 65000
-
-tst_exit
+tst_run
