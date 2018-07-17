f76b8c7e29f83bb597bcbfd8a286f3bdec5f011e
Mandri, Padmashree <padmashree.mandri@intel.com>
2018-07-17 14:29:19 +0530

    Add strlen() conformance tests and assertion.xml
    Adds
    	1> 1-1 test
    	2> assertions.xml
    	3> coverage.txt
    
    Change-Id: I382bb7f9dc4d9c8d1c7622bf4a126a374a772560
    Signed-off-by: Mandri, Padmashree <padmashree.mandri@intel.com>

---
+++ testcases/open_posix_testsuite/conformance/interfaces/strlen/1-1.c
@@ -0,0 +1,65 @@
+/*
+ * This file is licensed under the GPL license. For the full content
+ * of this license, see the COPYING file at the top level of this
+ * source tree.
+ */
+
+/*
+ * assertion:
+ *	The function will compute the number of bytes in the string to which s points,
+ *	not including the terminating NUL character. and will return the length of s.
+ *
+ * method:
+ *	-Generate sample string s1 of variable lengths in a loop.
+ *	-Use strlen() for generated strings and check whether return value is as expected.
+ *
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include "posixtest.h"
+
+#define STRING_MAX_LEN 50000
+#define STEP_COUNT 2000
+#define TNAME "strlen/1-1.c"
+
+char *random_string(int len)
+{
+	int i;
+	char *output_string;
+	output_string = malloc(len + 1);
+	if (output_string == NULL) {
+		printf(" Failed to allocate memory\n");
+		exit(PTS_UNRESOLVED);
+	}
+	for (i = 0; i < len; i++)
+		output_string[i] = rand() % 254 + 1;
+	output_string[len] = '\0';
+	return output_string;
+}
+
+int main(void)
+{
+	char *ret_str;
+	int i;
+	size_t obtained_len;
+
+	for (i = 0; i < STRING_MAX_LEN; i += STEP_COUNT) {
+		ret_str = random_string(i);
+		obtained_len = strlen(ret_str);
+
+		if (obtained_len != i) {
+			printf(TNAME " Test Failed, return value is not as expected, "
+					"for the given string. Expected string length: %d,"
+					" But got: %zu.\n", i, obtained_len);
+			exit(PTS_FAIL);
+		}
+		free(ret_str);
+	}
+	printf(TNAME " Test Passed, strlen() return value is as expected, "
+			"for the given strings.\n");
+	exit(PTS_PASS);
+}
