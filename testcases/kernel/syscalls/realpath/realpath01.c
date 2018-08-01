2617a059e8b8d3cbc8a79fff00a99df355e17ca7
Michael Moese <mmoese@suse.de>
2018-08-01 15:47:17 +0200

    Move getcwd05 to realpath01 and fix for old distro
    The testcase getcwd05 is a regression test for cve-2018-1000001 [1].
    However, there were changes in the behavior of libc functions, that some older
    distributions refused to backport.
    The testcase was two testcases, one for getcwd() and one for realpath().
    While the behavior of getcwd() changed, it is totally independent from
    the thestcase for the vulnerability in realpath. So, this test should be
    moved to realpath/realpath01.c. In addition, the test of getcwd() is
    totally unneeded here to test realpath() for the fix.
    
    [1] https://sourceware.org/git/gitweb.cgi?p=glibc.git;a=commitdiff;h=52a713fdd0a30e1bd79818e2e3c4ab44ddca1a94
    
    Signed-off-by: Michael Moese <mmoese@suse.de>
    Acked-by: Jan Stancek <jstancek@redhat.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/realpath/realpath01.c
+++ testcases/kernel/syscalls/realpath/realpath01.c
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (C) 2018 Petr Vorel <pvorel@suse.cz>
- * Based on the reproducer posted upstream so other copyrights may apply.
+ * Copyright (C) 2018 Michael Moese <mmoese@suse.de>
  *
+ * cve-2018-1000001 realpath buffer underflow
+ * Based on the reproducer posted upstream so other copyrights may apply.
  * Author: Dmitry V. Levin <ldv@altlinux.org>
  * LTP conversion from glibc source: Petr Vorel <pvorel@suse.cz>
  */
@@ -20,36 +22,22 @@ static void setup(void)
 	SAFE_CHROOT(CHROOT_DIR);
 }
 
-static void run(unsigned int i)
+static void run(void)
 {
-	int fail = 0;
-
-	if (i) {
-		tst_res(TINFO, "testing realpath()");
-		TESTPTR(realpath(".", NULL));
-	} else {
-		tst_res(TINFO, "testing getcwd()");
-		TESTPTR(getcwd(NULL, 0));
-	}
+	TESTPTR(realpath(".", NULL));
 
 	if (TST_ERR != ENOENT) {
 		tst_res(TFAIL | TTERRNO, "returned unexpected errno");
-		fail = 1;
-	}
-
-	if (TST_RET_PTR != NULL) {
+	} else	if (TST_RET_PTR != NULL) {
 		tst_res(TFAIL, "syscall didn't return NULL: '%s'",
 				(char *)TST_RET_PTR);
-		fail = 1;
-	}
-
-	if (!fail)
+	} else {
 		tst_res(TPASS, "bug not reproduced");
+	}
 }
 
 static struct tst_test test = {
-	.test = run,
-	.tcnt = 2,
+	.test_all = run,
 	.setup = setup,
 	.needs_root = 1,
 	.needs_tmpdir = 1,
