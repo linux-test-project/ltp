d0ce06660aa701f5345de85149a0bc70a3fd804f
Alexey Kodanev <alexey.kodanev@oracle.com>
2018-08-21 14:20:20 +0300

    network/dhcp: convert to new API
    Note about dnsmasq_test: removed $(pwd) from the files paths as it
    doesn't point to the temporary directory yet, also --log-facility
    path has to be with '/', so dnsmasqd interprets it as a file.
    
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/network/dhcp/dhcpd_tests.sh
+++ testcases/network/dhcp/dhcpd_tests.sh
@@ -1,31 +1,13 @@
 #!/bin/sh
-# Copyright (c) 2014-2015 Oracle and/or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2014-2018 Oracle and/or its affiliates. All Rights Reserved.
 # Copyright (c) International Business Machines  Corp., 2001
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write the Free Software Foundation,
-# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # Author:       Manoj Iyer, manjo@mail.utexas.edu
 # Author:       Alexey Kodanev alexey.kodanev@oracle.com
 
-TST_CLEANUP=cleanup
-TST_TOTAL=1
-TCID="dhcpd"
 dhcp_name="dhcpd"
 
-TST_USE_LEGACY_API=1
-. tst_net.sh
 . dhcp_lib.sh
 
 setup_dhcpd_conf()
@@ -35,22 +17,22 @@ setup_dhcpd_conf()
 	elif [ -f /etc/dhcp/dhcpd.conf ]; then
 		DHCPD_CONF="/etc/dhcp/dhcpd.conf"
 	else
-		tst_brkm TBROK "failed to find dhcpd.conf"
+		tst_brk TBROK "failed to find dhcpd.conf"
 	fi
 
 	mv $DHCPD_CONF dhcpd.conf
-	[ $? -ne 0 ] && tst_brkm TBROK "failed to backup dhcpd.conf"
+	[ $? -ne 0 ] && tst_brk TBROK "failed to backup dhcpd.conf"
 
 	mv tst_dhcpd.conf $DHCPD_CONF
-	[ $? -ne 0 ] && tst_brkm TBROK "failed to create dhcpd.conf"
+	[ $? -ne 0 ] && tst_brk TBROK "failed to create dhcpd.conf"
 }
 
 start_dhcpd()
 {
-	dhcpd -$ipv $iface0 > tst_dhcpd.err 2>&1
+	dhcpd -$TST_IPVER $iface0 > tst_dhcpd.err 2>&1
 	if [ $? -ne 0 ]; then
 		cat tst_dhcpd.err
-		tst_brkm TBROK "Failed to start dhcpd"
+		tst_brk TBROK "Failed to start dhcpd"
 	fi
 
 }
@@ -95,6 +77,4 @@ print_dhcp_log()
 	cat tst_dhcpd.err
 }
 
-init
-test01
-tst_exit
+tst_run
--- testcases/network/dhcp/dnsmasq_tests.sh
+++ testcases/network/dhcp/dnsmasq_tests.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
-# Copyright (c) 2014-2015 Oracle and/or its affiliates. All Rights Reserved.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write the Free Software Foundation,
-# Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2014-2018 Oracle and/or its affiliates. All Rights Reserved.
 #
 # Author: Alexey Kodanev alexey.kodanev@oracle.com
 
-TST_CLEANUP=cleanup
-TST_TOTAL=1
-TCID="dnsmasq"
 dhcp_name="dnsmasq"
 
-TST_USE_LEGACY_API=1
-. tst_net.sh
 . dhcp_lib.sh
+
+common_opt="--no-hosts --no-resolv --dhcp-authoritative \
+	--log-facility=./tst_dnsmasq.log --interface=$iface0 \
+	--dhcp-leasefile=tst_dnsmasq.lease --conf-file= "
 
 start_dhcp()
 {
@@ -37,7 +23,7 @@ start_dhcp6()
 {
 	# check that dnsmasq supports IPv6
 	dnsmasq --dhcp-range=fd00::1,fd00::1 --test > /dev/null 2>&1 || \
-		tst_brkm TCONF "dnsmasq doesn't support DHCPv6"
+		tst_brk TCONF "dnsmasq doesn't support DHCPv6"
 
 	dnsmasq $common_opt \
 	        --dhcp-range=fd00:1:1:2::100,fd00:1:1:2::100 --enable-ra \
@@ -54,11 +40,4 @@ print_dhcp_log()
 	cat tst_dnsmasq.log
 }
 
-init
-
-common_opt="--no-hosts --no-resolv --dhcp-authoritative \
-	--log-facility=$(pwd)/tst_dnsmasq.log --interface=$iface0 \
-	--dhcp-leasefile=$(pwd)/tst_dnsmasq.lease --conf-file= "
-
-test01
-tst_exit
+tst_run
