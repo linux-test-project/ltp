d93150bc9344bd1d7af8140bfb71fb79d9113a38
Rafael David Tinoco <rafael.tinoco@linaro.org>
2018-10-08 09:52:24 -0300

    syscalls/membarrier: Add membarrier() initial test
    Fixes: #265
    
    Initial test for membarrier() syscall. It tests all existing membarrier
    "commands" (or features), including the need (or not) for previous
    registration for the call to work.
    
    Some features did not exist in older kernels and that is covered by
    skipping some calls, flagging test as skipped & okay, and forcing
    others, making sure that return codes and errno are set right in those
    cases.
    
    Tests are done in a child process due to inexistent kernel interface to
    "unregister" the process from being affected by membarrier() call.
    
    Signed-off-by: Rafael David Tinoco <rafael.tinoco@linaro.org>

---
+++ include/lapi/membarrier.h
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2018 Linaro Limited. All rights reserved.
+ * Author: Rafael David Tinoco <rafael.tinoco@linaro.org>
+ */
+
+#ifndef LAPI_MEMBARRIER_H
+#define LAPI_MEMBARRIER_H
+
+/*
+ * Having <linux/membarrier.h> is enough to know if the test should run or
+ * not, but it might not define all needed MEMBARRIER_CMD_* being tested,
+ * since its first versions included just a few commands.
+ */
+
+enum membarrier_cmd {
+	MEMBARRIER_CMD_QUERY					= 0,
+	MEMBARRIER_CMD_GLOBAL					= (1 << 0),
+	MEMBARRIER_CMD_GLOBAL_EXPEDITED				= (1 << 1),
+	MEMBARRIER_CMD_REGISTER_GLOBAL_EXPEDITED		= (1 << 2),
+	MEMBARRIER_CMD_PRIVATE_EXPEDITED			= (1 << 3),
+	MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED		= (1 << 4),
+	MEMBARRIER_CMD_PRIVATE_EXPEDITED_SYNC_CORE		= (1 << 5),
+	MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_SYNC_CORE	= (1 << 6),
+
+	/* Alias for header backward compatibility. */
+	MEMBARRIER_CMD_SHARED			= MEMBARRIER_CMD_GLOBAL,
+};
+
+#endif
