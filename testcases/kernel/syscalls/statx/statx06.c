31f8fc08ae2a6213e458b7ffedd6147c1a83dbbc
Xiao Yang <yangx.jy@cn.fujitsu.com>
2018-11-21 11:05:32 +0800

    syscalls/statx06.c: Fix compiler errors on old distros
    Compilation fails on old distros due to undefined CLOCK_REALTIME_COARSE,
    AT_FDCWD or MS_STRICTATIME.
    
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/statx/statx06.c
+++ testcases/kernel/syscalls/statx/statx06.c
@@ -34,13 +34,17 @@
  */
 
 #include <stdio.h>
+#include <sys/mount.h>
+#include <time.h>
+
 #include "tst_test.h"
-#include "lapi/stat.h"
 #include "tst_safe_clocks.h"
 #include "tst_safe_macros.h"
 #include "tst_timer.h"
-#include <sys/mount.h>
-#include <time.h>
+#include "lapi/stat.h"
+#include "lapi/mount.h"
+#include "lapi/fcntl.h"
+#include "lapi/posix_clocks.h"
 
 #define MOUNT_POINT "mount_ext"
 #define TEST_FILE MOUNT_POINT"/test_file.txt"
