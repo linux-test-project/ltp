29466549991bf002c802b7e53a45bc9e9937de7b
Alexey Kodanev <alexey.kodanev@oracle.com>
2018-12-04 15:44:44 +0300

    network/dccp01: convert to the new API
    Use the SPDX-License-Identifier in the script and Makefile.
    
    Signed-off-by: Alexey Kodanev <alexey.kodanev@oracle.com>

--- testcases/network/dccp/dccp01.sh
+++ testcases/network/dccp/dccp01.sh
@@ -1,67 +1,44 @@
 #!/bin/sh
-# Copyright (c) 2017 Oracle and/or its affiliates. All Rights Reserved.
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of
-# the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it would be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-# Author: Alexey Kodanev <alexey.kodanev@oracle.com>
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2017-2018 Oracle and/or its affiliates. All Rights Reserved.
 
-TCID=dccp01
-TST_TOTAL=3
-TST_CLEANUP="cleanup"
+TST_CNT=3
+TST_TESTFUNC="test"
 TST_NEEDS_TMPDIR=1
+TST_NEEDS_ROOT=1
 
-TST_USE_LEGACY_API=1
 . tst_net.sh
-
-cleanup()
-{
-	tst_rmdir
-}
-
-setup()
-{
-	tst_require_root
-}
 
 compare()
 {
 	local per=$(( $res0 * 100 / $res1 - 100 ))
 
 	if [ "$per" -gt "100" -o "$per" -lt "-100" ]; then
-		tst_resm TFAIL "$1 performance $per %"
+		tst_res TFAIL "$1 performance $per %"
 	else
-		tst_resm TPASS "$1 performance $per % in range -100 ... 100 %"
+		tst_res TPASS "$1 performance $per % in range -100 ... 100 %"
 	fi
 }
 
-test_run()
+test1()
 {
-	tst_resm TINFO "compare UDP/DCCP/UDP-Lite performance"
-
+	tst_res TINFO "run UDP"
 	tst_netload -H $(tst_ipaddr rhost) -T udp
 	res0="$(cat tst_netload.res)"
-
+}
+test2()
+{
+	tst_res TINFO "compare UDP/DCCP performance"
 	tst_netload -H $(tst_ipaddr rhost) -T dccp
 	res1="$(cat tst_netload.res)"
 	compare DCCP
-
+}
+test3()
+{
+	tst_res TINFO "compare UDP/UDP-Lite performance"
 	tst_netload -H $(tst_ipaddr rhost) -T udp_lite
 	res1="$(cat tst_netload.res)"
 	compare UDP-Lite
 }
 
-setup
-test_run
-
-tst_exit
+tst_run
