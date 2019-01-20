7571d7398df861d37ce56118d4ca194ea6952daf
Enji Cooper <yaneurabeya@gmail.com>
2019-01-20 10:31:48 -0800

    Fix the dummy declaration for `lio_listio`
    The first parameter should be of `struct aiocb *restrict const[restrict]` type,
    not `struct aiocb *const[]` type, per the POSIX spec.
    
    Unfortunately this still doesn't cure compilation with gcc 4.8.5 (it claims
    there's an issue with `-Wincompatible-pointer-types`), so compiling this with
    -Werror seems like a non-starter until the underlying problem is
    resolved.
    
    While here, fix whitespace between types and `*` for pointers.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- testcases/open_posix_testsuite/conformance/definitions/aio_h/4-1.c
+++ testcases/open_posix_testsuite/conformance/definitions/aio_h/4-1.c
@@ -8,18 +8,18 @@
 
 #include <aio.h>
 
-int (*dummy0) (int, struct aiocb *) = aio_cancel;
-int (*dummy1) (const struct aiocb *) = aio_error;
-int (*dummy2) (int, struct aiocb *) = aio_fsync;
-int (*dummy3) (struct aiocb *) = aio_read;
-ssize_t(*dummy4) (struct aiocb *) = aio_return;
-int (*dummy5) (const struct aiocb * const[], int,
+int (*dummy0) (int, struct aiocb*) = aio_cancel;
+int (*dummy1) (const struct aiocb*) = aio_error;
+int (*dummy2) (int, struct aiocb*) = aio_fsync;
+int (*dummy3) (struct aiocb*) = aio_read;
+ssize_t (*dummy4) (struct aiocb*) = aio_return;
+int (*dummy5) (const struct aiocb* const[], int,
 	       const struct timespec *) = aio_suspend;
 int (*dummy6) (struct aiocb *) = aio_write;
-int (*dummy7) (int, struct aiocb * const[],
-	       int, struct sigevent * restrict) = lio_listio;
+int (*dummy7) (int, struct aiocb *restrict const [restrict],
+	       int, struct sigevent *restrict) = lio_listio;
 
-int main()
+int main(void)
 {
 	return 0;
 }
