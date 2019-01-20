f503d92f114160a529077ee4ef181e7e62d7c3e5
Enji Cooper <yaneurabeya@gmail.com>
2019-01-20 10:41:02 -0800

    Fix warnings
    - Annotate unused attributes with `LTP_ATTRIBUTE_UNUSED` to fix -Wunused
      warnings.
    - Remove an unused global variable, `received_selected`.
    
    While here, annotate `received_all` with `volatile` as the value is set from a
    signal handler.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- testcases/open_posix_testsuite/conformance/interfaces/aio_suspend/1-1.c
+++ testcases/open_posix_testsuite/conformance/interfaces/aio_suspend/1-1.c
@@ -50,10 +50,10 @@
 #define BUF_SIZE	(1024*1024)
 #define WAIT_FOR_AIOCB	6
 
-static int received_selected;
-static int received_all;
+static volatile int received_all;
 
-static void sigrt1_handler(int signum, siginfo_t *info, void *context)
+static void sigrt1_handler(int signum LTP_ATTRIBUTE_UNUSED,
+	siginfo_t *info LTP_ATTRIBUTE_UNUSED, void *context LTP_ATTRIBUTE_UNUSED)
 {
 	received_all = 1;
 }
@@ -220,3 +220,5 @@ int main(void)
 
 	return PTS_PASS;
 }
+
+
