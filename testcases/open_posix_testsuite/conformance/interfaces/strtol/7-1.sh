f206659fe57c6601868f6fe5baf872162a6b36c8
Mandri, Padmashree <padmashree.mandri@intel.com>
2019-01-25 12:04:48 +0530

    Add strtol conformance tests
    Adds
    1> 1-1, 2-1, 3-1, 4-1, 5-1, 6-1, 7-1, 8-1, 9-1, 10-1, 11-1, 12-1
    2> assertions.xml
    3> coverage.txt
    
    Signed-off-by: Mandri, Padmashree <padmashree.mandri@intel.com>

---
+++ testcases/open_posix_testsuite/conformance/interfaces/strtol/7-1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#   This file is licensed under the GPL license. For the full content
+#   of this license, see the COPYING file at the top level of this
+#   source tree.
+
+#   assertion:
+#	If the subject sequence is empty or does not have the expected form:
+#		No conversion is performed and the value of nptr will be stored in
+#		the object pointed to by endptr, provided that endptr is not a
+#		null pointer.
+
+#	This is tested implicitly via assertion 6.
+
+	echo "Tested implicitly via assertion 6."
+	exit 0
+
---
+++ testcases/open_posix_testsuite/conformance/interfaces/strtol/9-1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#   This file is licensed under the GPL license. For the full content
+#   of this license, see the COPYING file at the top level of this
+#   source tree.
+
+#   assertion:
+#	Upon successful completion, this function will return the converted value, if any.
+
+#   This is tested implicitly via assertion 1.
+
+    echo "Tested implicitly via assertion 1."
+    exit 0
+
