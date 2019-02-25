01fd8299716327174439b77809d2e69b10a605a4
Petr Vorel <pvorel@suse.cz>
2019-02-26 00:39:04 +0100

    lapi/posix_clocks.h: Include <time.h> before definitions
    + minor formatting fixes (include guards, copyright).
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Acked-by: Jan Stancek <jstancek@redhat.com>

--- include/lapi/posix_clocks.h
+++ include/lapi/posix_clocks.h
@@ -1,23 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
+ * Copyright (c) 2019, Linux Test Project
  * Copyright (c) 2013 Cyril Hrubis <chrubis@suse.cz>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write the Free Software Foundation,
- * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef __POSIX_CLOCK_IDS_H__
-#define __POSIX_CLOCK_IDS_H__
+#include <time.h>
+
+#ifndef POSIX_CLOCKS_H__
+#define POSIX_CLOCKS_H__
 
 #ifndef CLOCK_MONOTONIC_RAW
 # define CLOCK_MONOTONIC_RAW 4
@@ -43,4 +33,4 @@
 # define CLOCK_BOOTTIME_ALARM 9
 #endif
 
-#endif /* __POSIX_CLOCK_IDS_H__ */
+#endif /* POSIX_CLOCKS_H__ */
