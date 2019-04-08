d7ba1503a9bfe3a7151cbd886579708f058131b0
Yang Xu <xuyang2018.jy@cn.fujitsu.com>
2019-04-08 14:52:26 +0800

    syscalls/stat01, 02, 05: Cleanup && Convert to new API
    1) Take use of some safe macros
    2) Remove duplicate tests stat02,stat05
    
    Signed-off-by: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/stat/stat01.c
+++ testcases/kernel/syscalls/stat/stat01.c
@@ -1,152 +1,102 @@
-/*
- * Copyright (c) International Business Machines  Corp., 2001
- *  07/2001 John George
+// SPDX-License-Identifier: GPL-2.0-or-later
+/* Copyright (c) International Business Machines  Corp., 2001
+ *	07/2001 John George
+ *		-Ported
  *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-/*
  *  Verify that, stat(2) succeeds to get the status of a file and fills the
- *  stat structure elements.
+ *  stat structure elements regardless of whether process has or doesn't
+ *  have read access to the file.
  */
-#include <stdio.h>
+
 #include <sys/types.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <errno.h>
-#include <string.h>
-#include <signal.h>
 #include <pwd.h>
+#include "tst_test.h"
 
-#include "test.h"
-#include "safe_macros.h"
+#define FILE_SIZE	 1024
+#define TST_FILEREAD     "test_fileread"
+#define TST_FILENOREAD   "test_filenoread"
+#define READ_MODE        0666
+#define NEW_MODE         0222
+#define MASK             0777
 
-#define FILE_MODE	0644
-#define TESTFILE	"testfile"
-#define FILE_SIZE       1024
-#define BUF_SIZE	256
-#define MASK		0777
+uid_t user_id;
+gid_t group_id;
+struct passwd *ltpuser;
 
-char *TCID = "stat01";
-int TST_TOTAL = 1;
+static struct tcase{
+	char *pathname;
+	unsigned int mode;
+} TC[] = {
+	{TST_FILEREAD, READ_MODE},
+	{TST_FILENOREAD, NEW_MODE}
+};
 
-static uid_t user_id;
-static gid_t group_id;
-
-static void setup(void);
-static void cleanup(void);
-
-static void verify(void)
+static void verify_stat(unsigned int n)
 {
+	struct tcase *tc = TC + n;
 	struct stat stat_buf;
 	int fail = 0;
 
-	TEST(stat(TESTFILE, &stat_buf));
+	TEST(stat(tc->pathname, &stat_buf));
 
-	if (TEST_RETURN == -1) {
-		tst_resm(TFAIL | TTERRNO, "fstat(%s) failed", TESTFILE);
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TTERRNO, "stat(%s) failed", tc->pathname);
 		return;
 	}
 
 	if (stat_buf.st_uid != user_id) {
-		tst_resm(TINFO, "stat_buf.st_uid = %i expected %i",
-		         stat_buf.st_uid, user_id);
+		tst_res(TFAIL, "stat_buf.st_uid = %i expected %i",
+			stat_buf.st_uid, user_id);
 		fail++;
 	}
 
 	if (stat_buf.st_gid != group_id) {
-		tst_resm(TINFO, "stat_buf.st_gid = %i expected %i",
-		         stat_buf.st_gid, group_id);
+		tst_res(TFAIL, "stat_buf.st_gid = %i expected %i",
+			stat_buf.st_gid, group_id);
 		fail++;
 	}
 
 	if (stat_buf.st_size != FILE_SIZE) {
-		tst_resm(TINFO, "stat_buf.st_size = %li expected %i",
-		         (long)stat_buf.st_size, FILE_SIZE);
+		tst_res(TFAIL, "stat_buf.st_size = %li expected %i",
+			(long)stat_buf.st_size, FILE_SIZE);
 		fail++;
 	}
 
-        if ((stat_buf.st_mode & MASK) != FILE_MODE) {
-		tst_resm(TINFO, "stat_buf.st_mode = %o expected %o",
-		         (stat_buf.st_mode & MASK), FILE_MODE);
+	if ((stat_buf.st_mode & MASK) != tc->mode) {
+		tst_res(TFAIL, "stat_buf.st_mode = %o expected %o",
+			(stat_buf.st_mode & MASK), tc->mode);
 		fail++;
 	}
 
-	if (fail) {
-		tst_resm(TFAIL, "functionality of fstat incorrect");
-		return;
-	}
-
-	tst_resm(TPASS, "functionality of fstat correct");
+	if (!fail)
+		tst_res(TPASS, "stat(%s)", tc->pathname);
 }
 
-int main(int ac, char **av)
+void setup(void)
 {
-	int lc;
+	unsigned int i;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	ltpuser = SAFE_GETPWNAM("nobody");
+	SAFE_SETUID(ltpuser->pw_uid);
 
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++)
-		verify();
-
-	cleanup();
-	tst_exit();
-}
-
-static void setup(void)
-{
-	struct passwd *ltpuser;
-	char tst_buff[BUF_SIZE];
-	int wbytes;
-	int write_len = 0;
-	int fd;
-
-	tst_require_root();
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	ltpuser = SAFE_GETPWNAM(NULL, "nobody");
-	SAFE_SETUID(NULL, ltpuser->pw_uid);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	umask(022);
-
-	fd = SAFE_OPEN(tst_rmdir, TESTFILE, O_WRONLY | O_CREAT, FILE_MODE);
-
-	/* Fill the test buffer with the known data */
-	memset(tst_buff, 'a', BUF_SIZE - 1);
-
-	/* Write to the file 1k data from the buffer */
-	while (write_len < FILE_SIZE) {
-		if ((wbytes = write(fd, tst_buff, sizeof(tst_buff))) <= 0)
-			tst_brkm(TBROK | TERRNO, cleanup, "write failed");
-		else
-			write_len += wbytes;
+	for (i = 0; i < ARRAY_SIZE(TC); i++) {
+		if (tst_fill_file(TC[i].pathname, 'a', 256, 4))
+			tst_brk(TBROK, "Failed to create tst file %s",
+				TC[i].pathname);
+		SAFE_CHMOD(TC[i].pathname, TC[i].mode);
 	}
-
-	SAFE_CLOSE(tst_rmdir, fd);
 
 	user_id = getuid();
 	group_id = getgid();
 }
 
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(TC),
+	.setup = setup,
+	.test = verify_stat,
+	.needs_root = 1,
+	.needs_tmpdir = 1,
+};
