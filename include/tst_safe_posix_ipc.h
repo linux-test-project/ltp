e6ffd0c24b5f85a029dd35731708c176ccc09e7a
Petr Vorel <petr.vorel@gmail.com>
2019-04-14 12:07:26 +0200

    tst_safe_posix_ipc.h: Replace definition ANDROID with __ANDROID__
    Based on previous discussion on commit be944db1f and [1].
    
    + use SPDX-License-Identifier.
    
    [1] https://groups.google.com/forum/#!topic/android-ndk/cf9_f1SLXls
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- include/tst_safe_posix_ipc.h
+++ include/tst_safe_posix_ipc.h
@@ -1,24 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (C) 2017 Petr Vorel pvorel@suse.cz
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * Copyright (C) 2017-2019 Petr Vorel pvorel@suse.cz
  */
 
 #ifndef TST_SAFE_POSIX_IPC_H__
@@ -43,7 +25,7 @@ static inline int safe_mq_open(const char *file, const int lineno,
 	/* Android's NDK's mode_t is smaller than an int, which results in
 	 * SIGILL here when passing the mode_t type.
 	 */
-#ifndef ANDROID
+#ifndef __ANDROID__
 	mode = va_arg(ap, mode_t);
 #else
 	mode = va_arg(ap, int);
