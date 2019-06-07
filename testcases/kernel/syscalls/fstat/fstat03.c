9d4144869395a2863041c1e21fbe6a05dd778361
Christian Amann <camann@suse.com>
2019-06-07 11:19:23 +0200

    syscalls/fstat{03,05}: rewrote testcases
    Merged fstat03 and fstat05:
    	Both testcases had lots of duplicated code which
    	could be removed by creating a single testcase
    	for both scenarios.
    Rewrote testcase:
    	Cleaned up and ported to new library.
    
    Signed-off-by: Christian Amann <camann@suse.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/fstat/fstat03.c
+++ testcases/kernel/syscalls/fstat/fstat03.c
@@ -1,160 +1,103 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Test Name: fstat03
+ * Copyright (c) International Business Machines  Corp., 2001
+ *  07/2001 Ported by Wayne Boyer
+ *  05/2019 Ported to new library: Christian Amann <camann@suse.com>
+ */
+/*
+ * Tests different error scenarios:
  *
- * Test Description:
- *   Verify that, fstat(2) returns -1 and sets errno to EBADF if the file
- *   pointed to by file descriptor is not valid.
- *
- * Expected Result:
- *  fstat() should fail with return value -1 and set expected errno.
- *
- * Algorithm:
- *  Setup:
- *   Setup signal handling.
- *   Create temporary directory.
- *   Pause for SIGUSR1 if option specified.
- *
- *  Test:
- *   Loop if the proper options are given.
- *   Execute system call
- *   Check return code, if system call failed (return=-1)
- *	if errno set == expected errno
- *		Issue sys call fails with expected return value and errno.
- *	Otherwise,
- *		Issue sys call fails with unexpected errno.
- *   Otherwise,
- *	Issue sys call returns unexpected value.
- *
- *  Cleanup:
- *   Print errno log and/or timing stats if options given
- *   Delete the temporary directory(s)/file(s) created.
- *
- * Usage:  <for command-line>
- *  fstat03 [-c n] [-e] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -e   : Turn on errno logging.
- *	       -i n : Execute test n times.
- *	       -I x : Execute test for x seconds.
- *	       -P x : Pause for x seconds between iterations.
- *	       -t   : Turn on syscall timing.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS:
- *  This test should be executed by 'non-super-user' only.
- *
+ * 1) Calls fstat() with closed file descriptor
+ *    -> EBADF
+ * 2) Calls fstat() with an invalid address for stat structure
+ *    -> EFAULT (or receive signal SIGSEGV)
  */
 
-#include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
+#include <wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include "tst_test.h"
+#include "tst_safe_macros.h"
 
-#include "test.h"
-#include "safe_macros.h"
+#define TESTFILE	"test_file"
 
-#define FILE_MODE	S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
-#define TEST_FILE	"testfile"
+static int fd_ok;
+static int fd_ebadf;
+static struct stat stat_buf;
 
-char *TCID = "fstat03";
-int TST_TOTAL = 1;
+static struct tcase {
+	int *fd;
+	struct stat *stat_buf;
+	int exp_err;
+} tcases[] = {
+	{&fd_ebadf, &stat_buf, EBADF},
+	{&fd_ok, NULL, EFAULT},
+};
 
-int fildes;			/* testfile descriptor */
-
-void setup();			/* Main setup function for the tests */
-void cleanup();			/* cleanup function for the test */
-
-int main(int ac, char **av)
+static void check_fstat(unsigned int tc_num)
 {
-	struct stat stat_buf;	/* stat structure buffer */
-	int lc;
+	struct tcase *tc = &tcases[tc_num];
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	/*
-	 * Invoke setup function to create a testfile under temporary
-	 * directory.
-	 */
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-		/*
-		 * Call fstat(2) to get the status information
-		 * of a closed testfile pointed to by 'fd'.
-		 * verify that fstat fails with -1 return value and
-		 * sets appropriate errno.
-		 */
-		TEST(fstat(fildes, &stat_buf));
-
-		/* Check return code from fstat(2) */
-		if (TEST_RETURN == -1) {
-			if (TEST_ERRNO == EBADF) {
-				tst_resm(TPASS,
-					 "fstat() fails with expected error EBADF");
-			} else {
-				tst_resm(TFAIL | TTERRNO,
-					 "fstat() did not fail with EBADF");
-			}
+	TEST(fstat(*tc->fd, tc->stat_buf));
+	if (TST_RET == -1) {
+		if (tc->exp_err == TST_ERR) {
+			tst_res(TPASS,
+				 "fstat() fails with expected error %s",
+				 tst_strerrno(tc->exp_err));
 		} else {
-			tst_resm(TFAIL, "fstat() returned %ld, expected -1",
-				 TEST_RETURN);
+			tst_res(TFAIL | TTERRNO,
+				 "fstat() did not fail with %s, but with",
+				 tst_strerrno(tc->exp_err));
 		}
+	} else {
+		tst_res(TFAIL, "fstat() returned %ld, expected -1",
+			 TST_RET);
+	}
+}
+
+static void run(unsigned int tc_num)
+{
+	pid_t pid;
+	int status;
+
+	pid = SAFE_FORK();
+	if (pid == 0) {
+		check_fstat(tc_num);
+		return;
+	}
+	SAFE_WAITPID(pid, &status, 0);
+
+	if (tcases[tc_num].exp_err == EFAULT && WTERMSIG(status) == SIGSEGV) {
+		tst_res(TPASS, "fstat() failed as expected with SIGSEGV");
+		return;
 	}
 
-	/*
-	 * Invoke cleanup() to delete the test directory/file(s) created
-	 * in the setup().
-	 */
-	cleanup();
+	if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+		return;
 
-	tst_exit();
+	tst_res(TBROK, "child %s", tst_strstatus(status));
 }
 
-/*
- * void
- * setup(void) - performs all ONE TIME setup for this test.
- *	Exit the test program on receipt of unexpected signals.
- *	Create a temporary directory and change directory to it.
- *      Create a testfile under temporary directory.
- *      Close the testfile.
- */
-void setup(void)
+static void setup(void)
 {
-	/* Capture unexpected signals */
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	/* Make a temp dir and cd to it */
-	tst_tmpdir();
-
-	/* Create a testfile under temporary directory */
-	fildes = SAFE_OPEN(cleanup, TEST_FILE, O_RDWR | O_CREAT, 0666);
-
-	SAFE_CLOSE(cleanup, fildes);
+	fd_ebadf = tst_get_unused_pid();
+	fd_ok = SAFE_OPEN(TESTFILE, O_RDWR | O_CREAT, 0644);
 }
 
-/*
- * void
- * cleanup() - Performs all ONE TIME cleanup for this test at
- *             completion or premature exit.
- *	Print test timing stats and errno log if test executed with options.
- *	Close the testfile if still opened.
- *	Remove temporary directory and sub-directories/files under it
- *	created during setup().
- *	Exit the test program with normal exit code.
- */
-void cleanup(void)
+static void cleanup(void)
 {
-
-	tst_rmdir();
-
+	if (fd_ok > 0)
+		SAFE_CLOSE(fd_ok);
 }
+
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
+	.setup = setup,
+	.cleanup = cleanup,
+	.needs_tmpdir = 1,
+	.forks_child = 1,
+};
