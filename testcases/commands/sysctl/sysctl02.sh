48f1411837ad49de3f11eaeb924bdadb5c7287bf
Yang Xu <xuyang2018.jy@cn.fujitsu.com>
2019-06-10 16:51:46 +0800

    sysctl/sysctl02: Add new regression test for overflow file-max
    On upstream kernel before commit 7f2923c4f73f ("sysctl: handle overflow
    in proc_get_long") max value in proc_get_long is based on the number of
    chars(21). It rejects values such as 184467440737095516160 (21 chars)
    but accepts values such as 18446744073709551616 (20 chars). But both
    should be rejected as both overflows. 7f2923c4f73f sets max allowed
    value to 2^64-1.
    
    Before commit 32a5ad9c2285 ("sysctl: handle overflow for file-max")
    writing 18446744073709551616 to /proc/sys/fs/file-max overflows and
    value is set 0, and this may crash the system. 32a5ad9c2285 sets max
    allowed value 2^63-1.
    Unfortunately 32a5ad9c2285 introduced a regression. The minimum value
    points at the global 'zero' variable which is an int. This results in a
    KASAN splat when accessed as a long by proc_doulongvec_minmax on 64-bit
    architectures. This bug has been fixed in 9002b21465fa
    ("kernel/sysctl.c: fix out-of-bounds access when setting file-max").
    
    We test in sysctl02.sh setting 2^64, 2^64-1, 2^63 and 0.
    
    Signed-off-by: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
    [ pvorel: partly rewritten to improve shell code, commit message and formatting ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ testcases/commands/sysctl/sysctl02.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2019 FUJITSU LIMITED. All rights reserved.
+# Copyright (c) 2019 Petr Vorel <pvorel@suse.cz>
+# Author: Yang Xu<xuyang2018.jy@cn.fujitsu.com>
+#
+# Test for these regressions causing buffer overflow when writing into
+# /proc/sys/fs/file-max:
+# 7f2923c4f73f ("sysctl: handle overflow in proc_get_long")
+# 32a5ad9c2285 ("sysctl: handle overflow for file-max")
+#
+# This bug has been fixed in 9002b21465fa ("kernel/sysctl.c: fix
+# out-of-bounds access when setting file-max")
+#
+# We test in sysctl02.sh setting 2^64, 2^64-1, 2^63 and 0.
+
+TST_TESTFUNC=do_test
+TST_SETUP=setup
+TST_CLEANUP=cleanup
+TST_CNT=4
+TST_NEEDS_ROOT=1
+TST_NEEDS_CMDS="sysctl"
+sys_name="fs.file-max"
+sys_file="/proc/sys/fs/file-max"
+syms_file="/proc/kallsyms"
+
+. tst_test.sh
+
+setup()
+{
+	[ ! -f "$sys_file" ] && tst_brk TCONF "$sys_file not enabled"
+	orig_value=$(cat "$sys_file")
+}
+
+do_test()
+{
+	case $1 in
+	1) sysctl_test_overflow 18446744073709551616 ;;
+	2) sysctl_test_overflow 18446744073709551615 ;;
+	3) sysctl_test_overflow 9223372036854775808 ;;
+	4) sysctl_test_zero ;;
+	esac
+}
+
+sysctl_test_overflow()
+{
+	local old_value="$(cat $sys_file)"
+	sysctl -w -q $sys_name=$1 2>/dev/null
+	local test_value="$(cat $sys_file)"
+
+	if echo $test_value | grep -q $old_value; then
+		tst_res TPASS "$sys_file overflows, reject it and keep old value"
+	else
+		tst_res TFAIL "$sys_file overflows and set to $test_value"
+	fi
+	cleanup
+}
+
+sysctl_test_zero()
+{
+	[ ! -f "$syms_file" ] && tst_brk TCONF "$syms_file not enabled"
+	ROD sysctl -w -q $sys_name=0
+
+	if grep -q kasan_report $syms_file; then
+		if dmesg | grep -q "KASAN: global-out-of-bounds in __do_proc_doulongvec_minmax"; then
+			tst_res TFAIL "$sys_file is set 0 and trigger a KASAN error"
+		else
+			tst_res TPASS "$sys_file is set 0 and doesn't trigger a KASAN error"
+		fi
+	else
+		tst_res TCONF "kernel doesn't support KASAN"
+	fi
+}
+
+cleanup()
+{
+	[ -n "$orig_value" ] && sysctl -w -q $sys_name=$orig_value
+}
+
+tst_run
