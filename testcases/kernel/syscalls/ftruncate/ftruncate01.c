dfe2b3096148dc16e113f3fdc5939ef2131b3b58
Jinhui huang <huangjh.jy@cn.fujitsu.com>
2019-08-01 15:42:27 +0800

    syscalls/ftruncate: Rewrite ftruncate01 and merge ftruncate02
    Signed-off-by: Jinhui huang <huangjh.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/ftruncate/ftruncate01.c
+++ testcases/kernel/syscalls/ftruncate/ftruncate01.c
@@ -1,203 +1,103 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) 2019 FUJITSU LIMITED. All rights reserved.
+ * Author: Wayne Boyer
  */
-
 /*
- * Test Name: ftruncate01
- *
  * Test Description:
- *  Verify that, ftruncate(2) succeeds to truncate a file to a specified
- *  length if the file indicated by file descriptor opened for writing.
- *
- * Expected Result:
- *  ftruncate(2) should return a value 0 and the length of the file after
- *  truncation should be equal to the length it is truncated to.
- *
- * Algorithm:
- *  Setup:
- *   Setup signal handling.
- *   Create temporary directory.
- *   Pause for SIGUSR1 if option specified.
- *
- *  Test:
- *   Loop if the proper options are given.
- *   Execute system call
- *   Check return code, if system call failed (return=-1)
- *	Log the errno and Issue a FAIL message.
- *   Otherwise,
- *	Verify the Functionality of system call
- *      if successful,
- *		Issue Functionality-Pass message.
- *      Otherwise,
- *		Issue Functionality-Fail message.
- *  Cleanup:
- *   Print errno log and/or timing stats if options given
- *   Delete the temporary directory created.
- *
- * Usage:  <for command-line>
- *  ftruncate01 [-c n] [-f] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -f   : Turn off functionality Testing.
- *	       -i n : Execute test n times.
- *	       -I x : Execute test for x seconds.
- *	       -P x : Pause for x seconds between iterations.
- *	       -t   : Turn on syscall timing.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS:
- *  This test should be run by 'non-super-user' only.
- *
+ *  Verify that, ftruncate() succeeds to truncate a file to a certain length,
+ *  if the file previously is smaller than the truncated size, ftruncate()
+ *  shall increase the size of the file.
  */
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-#include <signal.h>
-#include <inttypes.h>
 
-#include "test.h"
-#include "safe_macros.h"
+#include "tst_test.h"
 
-#define TESTFILE	"testfile"	/* file under test */
-#define FILE_MODE	S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
-#define BUF_SIZE	256	/* buffer size */
-#define FILE_SIZE	1024	/* test file size */
-#define TRUNC_LEN	256	/* truncation length */
+#define TESTFILE	"testfile"
 
-TCID_DEFINE(ftruncate01);
-int TST_TOTAL = 1;		/* Total number of test conditions */
-int fildes;			/* file descriptor for test file */
+#define TRUNC_LEN1	256
+#define TRUNC_LEN2	512
+#define FILE_SIZE	1024
 
-void setup();			/* setup function for the test */
-void cleanup();			/* cleanup function for the test */
+static int fd;
 
-int main(int ac, char **av)
+static void check_and_report(off_t offset, char data, off_t trunc_len)
 {
-	struct stat stat_buf;	/* stat(2) struct contents */
-	int lc;
-	off_t file_length;	/* test file length */
+	int i, file_length;
+	char buf[FILE_SIZE];
+	struct stat stat_buf;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	memset(buf, '*', sizeof(buf));
 
-	setup();
+	SAFE_FSTAT(fd, &stat_buf);
+	file_length = stat_buf.st_size;
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
+	if (file_length != trunc_len) {
+		tst_res(TFAIL, "ftruncate() got incorrected size: %d",
+			file_length);
+		return;
+	}
 
-		tst_count = 0;
+	SAFE_LSEEK(fd, offset, SEEK_SET);
+	SAFE_READ(0, fd, buf, sizeof(buf));
 
-		/*
-		 * Call ftruncate(2) to truncate a test file to a
-		 * specified length.
-		 */
-		TEST(ftruncate(fildes, TRUNC_LEN));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO, "ftruncate(%s) failed",
-				 TESTFILE);
-			continue;
-		}
-		/*
-		 * Get the testfile information using
-		 * fstat(2).
-		 */
-		if (fstat(fildes, &stat_buf) < 0) {
-			tst_brkm(TFAIL, cleanup,
-				 "stat(2) of %s failed, error:%d",
-				 TESTFILE, errno);
-		}
-		stat_buf.st_mode &= ~S_IFREG;
-		file_length = stat_buf.st_size;
-
-		/*
-		 * Check for expected size of testfile after
-		 * truncate(2) on it.
-		 */
-		if (file_length != TRUNC_LEN) {
-			tst_resm(TFAIL,
-				 "%s: Incorrect file size %" PRId64 ", "
-				 "Expected %d", TESTFILE,
-				 (int64_t) file_length, TRUNC_LEN);
-		} else {
-			tst_resm(TPASS, "Functionality of ftruncate() "
-				 "on %s successful", TESTFILE);
+	for (i = 0; i < TRUNC_LEN1; i++) {
+		if (buf[i] != data) {
+			tst_res(TFAIL,
+				"ftruncate() got incorrect data %i, expected %i",
+				buf[i], data);
+			return;
 		}
 	}
 
-	cleanup();
-	tst_exit();
+	tst_res(TPASS, "ftruncate() succeeded");
 }
 
-/*
- * void
- * setup() - performs all ONE TIME setup for this test.
- *  Create a temporary directory and change directory to it.
- *  Create a test file under temporary directory and write some
- *  data into it.
- */
-void setup(void)
+static void verify_ftruncate(void)
 {
-	int i;
-	int c, c_total = 0;	/* bytes to be written to file */
-	char tst_buff[BUF_SIZE];	/* buffer to hold data */
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	/* Fill the test buffer with the known data */
-	for (i = 0; i < BUF_SIZE; i++) {
-		tst_buff[i] = 'a';
+	tst_res(TINFO, "Truncated length smaller than file size");
+	TEST(ftruncate(fd, TRUNC_LEN1));
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TTERRNO, "ftruncate() failed");
+		return;
 	}
 
-	/* open a file for reading/writing */
-	fildes = SAFE_OPEN(cleanup, TESTFILE, O_RDWR | O_CREAT, FILE_MODE);
+	check_and_report(0, 'a', TRUNC_LEN1);
 
-	/* Write to the file 1k data from the buffer */
-	while (c_total < FILE_SIZE) {
-		if ((c = write(fildes, tst_buff, sizeof(tst_buff))) <= 0) {
-			tst_brkm(TBROK | TERRNO, cleanup, "write(%s) failed",
-				 TESTFILE);
-		} else {
-			c_total += c;
-		}
+	tst_res(TINFO, "Truncated length exceeds file size");
+	TEST(ftruncate(fd, TRUNC_LEN2));
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TTERRNO, "ftruncate() failed");
+		return;
 	}
+
+	check_and_report(TRUNC_LEN1, 0, TRUNC_LEN2);
 }
 
-/*
- * void
- * cleanup() - performs all ONE TIME cleanup for this test at
- *	       completion or premature exit.
- *  Close the temporary file.
- *  Remove the test directory and testfile created in the setup.
- */
-void cleanup(void)
+static void setup(void)
 {
+	if (tst_fill_file(TESTFILE, 'a', FILE_SIZE, 1))
+		tst_brk(TBROK, "Failed to create test file");
 
-	/* Close the testfile after writing data into it */
-	if (close(fildes) == -1)
-		tst_brkm(TFAIL | TERRNO, NULL, "close(%s) failed", TESTFILE);
-
-	tst_rmdir();
-
+	fd = SAFE_OPEN(TESTFILE, O_RDWR);
 }
+
+static void cleanup(void)
+{
+	if (fd > 0)
+		SAFE_CLOSE(fd);
+}
+
+static struct tst_test test = {
+	.test_all = verify_ftruncate,
+	.setup = setup,
+	.cleanup = cleanup,
+	.needs_tmpdir = 1,
+};
