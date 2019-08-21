c2049b5c874bc071f8185bffb5fd7dcb042d9ec8
He Zhe <zhe.he@windriver.com>
2019-08-21 16:27:24 +0800

    cve/meltdown.c: Fix kernel symbol finding
    meltdown case fails as below.
    safe_file_ops.c:219: BROK: Expected 3 conversions got 2 at meltdown.c:272
    
    find_kernel_symbol is defined to try twice with each of /proc/kallsyms and
    /boot/System.map-%s. Currently if the symbol is not found in /proc/kallsyms,
    when kernel option CONFIG_KALLSYMS_ALL is disabled, it would stop the case
    immediately due to SAFE_FILE_LINES_SCANF.
    
    This patch turns to use FILE_LINES_SCANF to give find_kernel_symbol second
    chance.
    
    [jstancek] Fix also condition introduced in 7709d2ae92ea, even if
               /proc/kallsyms is disabled, we still want to try System.map.
    
    Signed-off-by: He Zhe <zhe.he@windriver.com>
    Signed-off-by: Jan Stancek <jstancek@redhat.com>

--- testcases/cve/meltdown.c
+++ testcases/cve/meltdown.c
@@ -253,9 +253,15 @@ find_symbol_in_file(const char *filename, const char *symname)
 	int ret, read;
 	char fmt[strlen(symname) + 64];
 
+	tst_res(TINFO, "Looking for %s in %s", symname, filename);
+	if (access(filename, F_OK) == -1) {
+		tst_res(TINFO, "%s not available", filename);
+		return 0;
+	}
+
 	sprintf(fmt, "%%lx %%c %s%%c", symname);
 
-	ret = SAFE_FILE_LINES_SCANF(filename, fmt, &addr, &type, &read);
+	ret = FILE_LINES_SCANF(filename, fmt, &addr, &type, &read);
 	if (ret)
 		return 0;
 
@@ -269,20 +275,13 @@ find_kernel_symbol(const char *name)
 	struct utsname utsname;
 	unsigned long addr;
 
-	if (access("/proc/kallsyms", F_OK) == -1)
-		tst_brk(TCONF, "/proc/kallsyms not enabled");
-
 	addr = find_symbol_in_file("/proc/kallsyms", name);
 	if (addr)
 		return addr;
 
-	tst_res(TINFO, "not found '%s' in /proc/kallsyms", name);
 	if (uname(&utsname) < 0)
 		tst_brk(TBROK | TERRNO, "uname");
-
 	sprintf(systemmap, "/boot/System.map-%s", utsname.release);
-
-	tst_res(TINFO, "looking in '%s'\n", systemmap);
 	addr = find_symbol_in_file(systemmap, name);
 	return addr;
 }
@@ -296,6 +295,9 @@ static void setup(void)
 
 	saved_cmdline_addr = find_kernel_symbol("saved_command_line");
 	tst_res(TINFO, "&saved_command_line == 0x%lx", saved_cmdline_addr);
+
+	if (!saved_cmdline_addr)
+		tst_brk(TCONF, "saved_command_line not found");
 
 	spec_fd = SAFE_OPEN("/proc/cmdline", O_RDONLY);
 
