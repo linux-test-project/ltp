2f6cd52420a93852a4aac35230b04d05d3627e0a
Petr Vorel <pvorel@suse.cz>
2019-09-30 16:59:13 +0200

    shell: Introduce TST_TIMEOUT variable, add checks
    to unify shell API with C API.
    
    TST_TIMEOUT should be set in tests only, it's equivalent
    of tst_test->timeout in C API.
    
    Add checks requiring both TST_TIMEOUT and LTP_TIMEOUT_MUL >= 1,
    that allow to set TST_TIMEOUT lower than the default 300 sec
    (might be useful for some case).
    
    LTP_TIMEOUT_MUL can be float, but will be ceiled to int.
    Using float would require awk/bc, which is unnecessary dependency
    and code complication (we do not care that much if it's multiplied
    precisely as far as the resulting timeout is never smaller than
    the precise calculation).
    
    Also added cut dependency to _tst_setup_timer(), but that's not
    a problem as it was already required for shell API in tst_run().
    
    Suggested-by: Clemens Famulla-Conrad <cfamullaconrad@suse.de>
    Reviewed-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Clemens Famulla-Conrad <cfamullaconrad@suse.de>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/lib/tst_test.sh
+++ testcases/lib/tst_test.sh
@@ -379,9 +379,30 @@ _tst_rescmp()
 
 _tst_setup_timer()
 {
+	TST_TIMEOUT=${TST_TIMEOUT:-300}
 	LTP_TIMEOUT_MUL=${LTP_TIMEOUT_MUL:-1}
 
-	local sec=$((300 * LTP_TIMEOUT_MUL))
+	if [ "$TST_TIMEOUT" = -1 ]; then
+		tst_res TINFO "Timeout per run is disabled"
+		return
+	fi
+
+	local err="LTP_TIMEOUT_MUL must be number >= 1!"
+
+	tst_is_num "$LTP_TIMEOUT_MUL" || tst_brk TBROK "$err ($LTP_TIMEOUT_MUL)"
+
+	if ! tst_is_int "$LTP_TIMEOUT_MUL"; then
+		LTP_TIMEOUT_MUL=$(echo "$LTP_TIMEOUT_MUL" | cut -d. -f1)
+		LTP_TIMEOUT_MUL=$((LTP_TIMEOUT_MUL+1))
+		tst_res TINFO "ceiling LTP_TIMEOUT_MUL to $LTP_TIMEOUT_MUL"
+	fi
+	[ "$LTP_TIMEOUT_MUL" -ge 1 ] || tst_brk TBROK "$err ($LTP_TIMEOUT_MUL)"
+
+	if ! tst_is_int "$TST_TIMEOUT" || [ "$TST_TIMEOUT" -lt 1 ]; then
+		tst_brk TBROK "TST_TIMEOUT must be int >= 1! ($TST_TIMEOUT)"
+	fi
+
+	local sec=$((TST_TIMEOUT * LTP_TIMEOUT_MUL))
 	local h=$((sec / 3600))
 	local m=$((sec / 60 % 60))
 	local s=$((sec % 60))
@@ -418,7 +439,7 @@ tst_run()
 			NEEDS_CMDS|NEEDS_MODULE|MODPATH|DATAROOT);;
 			NEEDS_DRIVERS|FS_TYPE|MNTPOINT|MNT_PARAMS);;
 			IPV6|IPVER|TEST_DATA|TEST_DATA_IFS);;
-			RETRY_FUNC|RETRY_FN_EXP_BACKOFF);;
+			RETRY_FUNC|RETRY_FN_EXP_BACKOFF|TIMEOUT);;
 			NET_MAX_PKT);;
 			*) tst_res TWARN "Reserved variable TST_$_tst_i used!";;
 			esac
