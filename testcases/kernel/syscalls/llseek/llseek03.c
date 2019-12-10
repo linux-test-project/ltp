21af8ca196d8d64ddd3dc0a97f20e524d339281e
Feiyu Zhu <zhufy.jy@cn.fujitsu.com>
2019-12-10 16:14:18 -0500

    syscalls/llseek03: Rewrite the test for new library
    Signed-off-by: Feiyu Zhu <zhufy.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/llseek/llseek03.c
+++ testcases/kernel/syscalls/llseek/llseek03.c
@@ -1,178 +1,126 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2014 Fujitsu Ltd.
  * Author: Xiaoguang Wang <wangxg.fnst@cn.fujitsu.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 /*
  * Description:
- * Verify that,
- *    1. llseek() succeeds to set the file pointer position to the current
- *  specified location, when 'whence' value is set to SEEK_CUR and the data
- *  read from the specified location should match the expected data.
- *    2. llseek() succeeds to set the file pointer position to the end of
- *  the file when 'whence' value set to SEEK_END and any attempts to read
- *  from that position should return 0.
  *
+ * For each of SEEK_SET, SEEK_CUR and SEEK_END verify that,
+ *
+ *   1. llseek() succeeds to set file position in the middle of the data. The
+ *      file offset is checked by reading from a file and comparing the data.
+ *
+ *   2. llseek() succeeds to set file postion to the end of the data, reading
+ *      this postion returns 0.
+ *
+ *   3. llseek() succeeds to set file position after the end of the data,
+ *      reading from this postion returns 0 as well.
  */
-
 #define _GNU_SOURCE
 
-#include <unistd.h>
-#include <errno.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <utime.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/resource.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <inttypes.h>
-
-#include "test.h"
-#include "safe_macros.h"
+#include "tst_test.h"
 
 #define TEST_FILE "testfile"
-
-static void setup(void);
-static void cleanup(void);
-
-static void testfunc_seekcur(void);
-static void testfunc_seekend(void);
-
-static void (*testfunc[])(void) = { testfunc_seekcur, testfunc_seekend };
-
-char *TCID = "llseek03";
-int TST_TOTAL = 2;
-
-static size_t file_size;
-
-int main(int ac, char **av)
-{
-	int i, lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++)
-			(*testfunc[i])();
-	}
-
-	cleanup();
-	tst_exit();
-}
+#define STR "abcdefgh"
 
 static void setup(void)
 {
 	int fd;
-	struct stat stat_buf;
 
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+	fd = SAFE_CREAT(TEST_FILE, 0644);
 
-	tst_tmpdir();
+	SAFE_WRITE(1, fd, STR, sizeof(STR) - 1);
 
-	TEST_PAUSE;
-
-	fd = SAFE_CREAT(cleanup, TEST_FILE, 0644);
-
-	#define STR "abcdefgh"
-	SAFE_WRITE(cleanup, 1, fd, STR, sizeof(STR) - 1);
-
-	SAFE_FSTAT(cleanup, fd, &stat_buf);
-
-	SAFE_CLOSE(cleanup, fd);
-
-	file_size = stat_buf.st_size;
+	SAFE_CLOSE(fd);
 }
 
-static void testfunc_seekcur(void)
+static struct tcase {
+	int whence;
+	int off;
+	int ret;
+	int read;
+	const char *str;
+} tcases[] = {
+	/* Seek somewhere in the middle of data */
+	{SEEK_SET, 1, 1, 3, "bcd"},
+	{SEEK_CUR, 1, 5, 3, "fgh"},
+	{SEEK_END, -1, 7, 1, "h"},
+
+	/* Seek to the end of data */
+	{SEEK_SET, 8, 8, 0, NULL},
+	{SEEK_CUR, 4, 8, 0, NULL},
+	{SEEK_END, 0, 8, 0, NULL},
+
+	/* Seek after the end of data */
+	{SEEK_SET, 10, 10, 0, NULL},
+	{SEEK_CUR, 8, 12, 0, NULL},
+	{SEEK_END, 4, 12, 0, NULL},
+};
+
+static const char *str_whence(int whence)
 {
-	int fd;
-	static char read_buf[BUFSIZ];
-
-	/* reopen TEST_FILE and file offset will be 0 */
-	fd = SAFE_OPEN(cleanup, TEST_FILE, O_RDONLY);
-
-	/* after read, file offset will be 4 */
-	SAFE_READ(cleanup, 1, fd, read_buf, 4);
-
-	TEST(lseek64(fd, (loff_t) 1, SEEK_CUR));
-
-	if (TEST_RETURN == (loff_t) -1) {
-		tst_resm(TFAIL | TTERRNO, "llseek failed on %s ", TEST_FILE);
-		goto cleanup_seekcur;
+	switch (whence) {
+	case SEEK_SET:
+		return "SEEK_SET";
+	case SEEK_CUR:
+		return "SEEK_CUR";
+	case SEEK_END:
+		return "SEEK_END";
+	default:
+		return "INVALID";
 	}
-
-	if (TEST_RETURN != 5) {
-		tst_resm(TFAIL, "llseek return a incorrect file offset");
-		goto cleanup_seekcur;
-	}
-
-	memset(read_buf, 0, sizeof(read_buf));
-
-	/* the expected characters are "fgh" */
-	SAFE_READ(cleanup, 1, fd, read_buf, 3);
-
-	if (strcmp(read_buf, "fgh"))
-		tst_resm(TFAIL, "Read wrong bytes after llseek");
-	else
-		tst_resm(TPASS, "test SEEK_SET for llseek success");
-
-cleanup_seekcur:
-	SAFE_CLOSE(cleanup, fd);
 }
 
-
-static void testfunc_seekend(void)
+static void verify_lseek64(unsigned int n)
 {
-	int fd;
-	ssize_t nread;
-	static char read_buf[BUFSIZ];
+	struct tcase *tc = &tcases[n];
+	char read_buf[128];
+	int fd, ret;
 
-	/* reopen TEST_FILE and file offset will be 0 */
-	fd = SAFE_OPEN(cleanup, TEST_FILE, O_RDONLY);
+	fd = SAFE_OPEN(TEST_FILE, O_RDONLY);
 
-	TEST(lseek64(fd, (loff_t) 0, SEEK_END));
+	SAFE_READ(1, fd, read_buf, 4);
 
-	if (TEST_RETURN == (loff_t) -1) {
-		tst_resm(TFAIL | TTERRNO, "llseek failed on %s ", TEST_FILE);
-		goto cleanup_seekend;
+	TEST(lseek64(fd, tc->off, tc->whence));
+
+	if (TST_RET == -1) {
+                tst_res(TFAIL | TTERRNO, "llseek failed on %s ", TEST_FILE);
+                goto exit;
+        }
+
+	if (TST_RET != tc->ret) {
+                tst_res(TFAIL, "llseek returned %li expected %i", TST_RET, tc->ret);
+                goto exit;
+        } else {
+		tst_res(TPASS, "llseek returned %i", tc->ret);
 	}
 
-	if (TEST_RETURN != (long)file_size) {
-		tst_resm(TFAIL, "llseek return a incorrect file offset");
-		goto cleanup_seekend;
+        memset(read_buf, 0, sizeof(read_buf));
+
+        ret = SAFE_READ(0, fd, read_buf, tc->read);
+
+	if (!tc->read) {
+		if (ret != 0)
+			tst_res(TFAIL, "Read bytes after llseek to end of file");
+		else
+			tst_res(TPASS, "%s read returned 0", str_whence(tc->whence));
+
+		goto exit;
 	}
 
-	memset(read_buf, 0, sizeof(read_buf));
+        if (strcmp(read_buf, tc->str))
+                tst_res(TFAIL, "Read wrong bytes after llseek");
+        else
+                tst_res(TPASS, "%s for llseek", str_whence(tc->whence));
 
-	nread = SAFE_READ(cleanup, 0, fd, read_buf, file_size);
-	if (nread > 0)
-		tst_resm(TFAIL, "Read bytes after llseek to end of file");
-	else
-		tst_resm(TPASS, "test SEEK_END for llseek success");
-
-cleanup_seekend:
-	SAFE_CLOSE(cleanup, fd);
+exit:
+        SAFE_CLOSE(fd);
 }
 
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.needs_tmpdir = 1,
+	.setup = setup,
+	.test = verify_lseek64,
+	.tcnt = ARRAY_SIZE(tcases),
+};
