1dfd30c73f5d2a9af1d8ef4eb69d23ac41855344
Petr Vorel <pvorel@suse.cz>
2019-12-23 08:14:48 +0100

    linktest.sh: Convert to use getopts + set default values
    Main motivation for getopts is to allow -h (print help) to be working
    without too much tweaking. Although other default options (-i n) were
    working, there was no way to print help.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/fs/linktest/linktest.sh
+++ testcases/kernel/fs/linktest/linktest.sh
@@ -3,36 +3,36 @@
 # Copyright (c) International Business Machines Corp., 2000
 # Copyright (c) Linux Test Project, 2012-2019
 # Regression test for max links per file
-# linktest.sh <number of symlinks> <number of hardlinks>
 # Author: Ngie Cooper <yaneurabeya@gmail.com>
 
 TST_NEEDS_TMPDIR=1
-TST_POS_ARGS=2
 TST_TESTFUNC=do_test
+TST_OPTS="a:s:"
+TST_PARSE_ARGS=parse_args
+TST_USAGE=usage
 
 . tst_test.sh
 
-if [ $# -ne 2 ]; then
-	tst_res TBROK "usage: $0 {softlink count} {hardlink count}"
-	exit 1
-fi
+hard_links=1000
+soft_links=1000
 
-validate_parameter()
+usage()
 {
-	if ! tst_is_int "$2"; then
-		tst_brk TBROK "$1 must be integer"
-	fi
-
-	if [ "$2" -lt 0 ]; then
-		tst_brk TBROK "$1 must be >= 0"
-	fi
+	echo "Usage: linktest.sh {-a n} {-s n}"
+	echo "-a n    Hard link count"
+	echo "-s n    Soft link count"
 }
 
-validate_parameter "softlink count" $1
-validate_parameter "hardlink count" $2
+parse_args()
+{
+	tst_is_int "$2" || tst_brk TBROK "-$1 must be integer ($2)"
+	[ "$2" -ge 0 ] || tst_brk TBROK "-$1 must be >= 0 ($2)"
 
-soft_links=$1
-hard_links=$2
+	case $1 in
+	a) hard_links=$2;;
+	s) soft_links=$2;;
+	esac
+}
 
 do_link()
 {
@@ -45,20 +45,20 @@ do_link()
 	local i=0
 	local rtype="TFAIL"
 
+	tst_res TINFO "test $prefix_msg link, limit: $limit"
+
 	cd "${prefix}link.$$"
 	while [ $i -lt $limit ]; do
-		if ! ln ${ln_opts} "$PWD/${prefix}file" ${prefix}file${i}; then
+		if ! ln $ln_opts "$PWD/${prefix}file" ${prefix}file${i}; then
 			lerrors=$((lerrors + 1))
 		fi
 		i=$((i + 1))
 	done
 	cd ..
 
-	if [ $lerrors -eq 0 ]; then
-		rtype=TPASS
-	fi
+	[ $lerrors -eq 0 ] && rtype="TPASS"
 
-	tst_res $rtype "$prefix_msg link errors: $lerrors"
+	tst_res $rtype "errors: $lerrors"
 }
 
 do_test()
@@ -66,8 +66,8 @@ do_test()
 	mkdir hlink.$$ slink.$$
 	touch hlink.$$/hfile slink.$$/sfile
 
-	do_link s "-s" $soft_links "symbolic"
-	do_link h   "" $hard_links "hard"
+	do_link "s" "-s" $soft_links "symbolic"
+	do_link "h"   "" $hard_links "hard"
 
 	rm -rf hlink.$$ slink.$$
 }
