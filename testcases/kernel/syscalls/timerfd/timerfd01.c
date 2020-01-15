01f8d4129acf24354c184d68c85386ce3e2008c9
Jorik Cronenberg <jcronenberg@suse.de>
2020-01-15 16:51:56 +0100

    syscalls/timerfd01: Port to new library & cleanup
    Port timerfd01 to new test library and clean it up.
    Also make it use the LTP framework properly.
    
    Signed-off-by: Jorik Cronenberg <jcronenberg@suse.de>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/timerfd/timerfd01.c
+++ testcases/kernel/syscalls/timerfd/timerfd01.c
@@ -1,282 +1,142 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *  timerfd() test by Davide Libenzi (test app for timerfd)
  *  Copyright (C) 2007  Davide Libenzi
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  *  Davide Libenzi <davidel@xmailserver.org>
  *
+ *  Description:
+ *	Test timerfd with the flags:
+ *		1) CLOCK_MONOTONIC
+ *		2) CLOCK_REALTIME
  *
- *     $ gcc -o timerfd-test2 timerfd-test2.c -lrt
- *
- * NAME
- *	timerfd01.c
  * HISTORY
- *	28/05/2008 Initial contribution by Davide Libenzi <davidel@xmailserver.org>
- *      28/05/2008 Integrated to LTP by Subrata Modak <subrata@linux.vnet.ibm.com>
+ *  28/05/2008 Initial contribution by Davide Libenzi <davidel@xmailserver.org>
+ *  28/05/2008 Integrated to LTP by Subrata Modak <subrata@linux.vnet.ibm.com>
  */
 
 #define _GNU_SOURCE
-#include <sys/syscall.h>
-#include <sys/types.h>
-#include <sys/signal.h>
-#include <sys/time.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <signal.h>
 #include <poll.h>
-#include <fcntl.h>
-#include <time.h>
-#include <errno.h>
-#include "test.h"
-#include "lapi/syscalls.h"
+#include "tst_test.h"
+#include "tst_timer.h"
+#include "lapi/timerfd.h"
 
-#define cleanup tst_exit
-
-char *TCID = "timerfd01";
-
-/*
- * This were good at the time of 2.6.23-rc7 ...
- *
- * #ifdef __NR_timerfd
- *
- * ... but is not now with 2.6.25
- */
-#ifdef __NR_timerfd_create
-
-/* Definitions from include/linux/timerfd.h */
-#define TFD_TIMER_ABSTIME (1 << 0)
-
-struct tmr_type {
+static struct tcase {
 	int id;
 	char const *name;
+} tcases[] = {
+	{CLOCK_MONOTONIC, "CLOCK MONOTONIC"},
+	{CLOCK_REALTIME, "CLOCK REALTIME"},
 };
 
-unsigned long long getustime(int clockid)
+static unsigned long long getustime(int clockid)
 {
 	struct timespec tp;
 
 	if (clock_gettime((clockid_t) clockid, &tp)) {
-		perror("clock_gettime");
+		tst_res(TFAIL | TERRNO, "clock_gettime() failed");
 		return 0;
 	}
 
 	return 1000000ULL * tp.tv_sec + tp.tv_nsec / 1000;
 }
 
-void set_timespec(struct timespec *tmr, unsigned long long ustime)
+static void settime(int tfd, struct itimerspec *tmr, int tflags,
+                    unsigned long long tvalue, int tinterval)
 {
+	tmr->it_value = tst_us_to_timespec(tvalue);
+	tmr->it_interval = tst_us_to_timespec(tinterval);
 
-	tmr->tv_sec = (time_t) (ustime / 1000000ULL);
-	tmr->tv_nsec = (long)(1000ULL * (ustime % 1000000ULL));
+	if (timerfd_settime(tfd, tflags, tmr, NULL))
+		tst_brk(TBROK | TERRNO, "timerfd_settime() failed");
 }
 
-int timerfd_create(int clockid, int flags)
+static void waittmr(int tfd, unsigned int exp_ticks)
 {
-
-	return ltp_syscall(__NR_timerfd_create, clockid, flags);
-}
-
-int timerfd_settime(int ufc, int flags, const struct itimerspec *utmr,
-		    struct itimerspec *otmr)
-{
-
-	return ltp_syscall(__NR_timerfd_settime, ufc, flags, utmr, otmr);
-}
-
-int timerfd_gettime(int ufc, struct itimerspec *otmr)
-{
-
-	return ltp_syscall(__NR_timerfd_gettime, ufc, otmr);
-}
-
-long waittmr(int tfd, int timeo)
-{
-	u_int64_t ticks;
+	uint64_t ticks;
 	struct pollfd pfd;
 
 	pfd.fd = tfd;
 	pfd.events = POLLIN;
 	pfd.revents = 0;
-	if (poll(&pfd, 1, timeo) < 0) {
-		perror("poll");
-		return -1;
+	if (poll(&pfd, 1, -1) < 0) {
+		tst_res(TFAIL | TERRNO, "poll() failed");
+		return;
 	}
 	if ((pfd.revents & POLLIN) == 0) {
-		fprintf(stdout, "no ticks happened\n");
-		return -1;
+		tst_res(TFAIL, "no ticks happened");
+		return;
 	}
-	if (read(tfd, &ticks, sizeof(ticks)) != sizeof(ticks)) {
-		perror("timerfd read");
-		return -1;
-	}
+	SAFE_READ(0, tfd, &ticks, sizeof(ticks));
 
-	return ticks;
+	if (ticks != exp_ticks) {
+		tst_res(TFAIL, "got %u tick(s) expected %u",
+		        (unsigned int)ticks, exp_ticks);
+	} else {
+		tst_res(TPASS, "got %u tick(s)", exp_ticks);
+	}
 }
 
-int TST_TOTAL = 3;
-
-int main(int ac, char **av)
+static void run(unsigned int n)
 {
 	int tfd;
-	unsigned int i;
-	long ticks;
-	unsigned long long tnow, ttmr;
-	u_int64_t uticks;
+	unsigned long long tnow;
+	uint64_t uticks;
 	struct itimerspec tmr;
-	struct tmr_type clks[] = {
-		{CLOCK_MONOTONIC, "CLOCK MONOTONIC"},
-		{CLOCK_REALTIME, "CLOCK REALTIME"},
-	};
+	struct tcase *clks = &tcases[n];
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	tst_res(TINFO, "testing %s", clks->name);
 
-	if ((tst_kvercmp(2, 6, 25)) < 0) {
-		tst_resm(TCONF, "This test can only run on kernels that are ");
-		tst_resm(TCONF, "2.6.25 and higher");
-		exit(0);
-	}
+	tfd = timerfd_create(clks->id, 0);
+	if (tfd == -1)
+		tst_brk(TFAIL | TERRNO, "timerfd_create() failed");
 
-	for (i = 0; i < sizeof(clks) / sizeof(clks[0]); i++) {
-		fprintf(stdout,
-			"\n\n---------------------------------------\n");
-		fprintf(stdout, "| testing %s\n", clks[i].name);
-		fprintf(stdout, "---------------------------------------\n\n");
+	tst_res(TINFO, "relative timer (100 ms)");
+	settime(tfd, &tmr, 0, 100 * 1000, 0);
+	waittmr(tfd, 1);
 
-		fprintf(stdout, "relative timer test (at 500 ms) ...\n");
-		set_timespec(&tmr.it_value, 500 * 1000);
-		set_timespec(&tmr.it_interval, 0);
-		tnow = getustime(clks[i].id);
-		if ((tfd = timerfd_create(clks[i].id, 0)) == -1) {
-			perror("timerfd");
-			return 1;
-		}
-		fprintf(stdout, "timerfd = %d\n", tfd);
+	tst_res(TINFO, "absolute timer (100 ms)");
+	tnow = getustime(clks->id);
+	settime(tfd, &tmr, TFD_TIMER_ABSTIME, tnow + 100 * 1000, 0);
+	waittmr(tfd, 1);
 
-		if (timerfd_settime(tfd, 0, &tmr, NULL)) {
-			perror("timerfd_settime");
-			return 1;
-		}
+	tst_res(TINFO, "sequential timer (50 ms)");
+	tnow = getustime(clks->id);
+	settime(tfd, &tmr, TFD_TIMER_ABSTIME, tnow + 50 * 1000, 50 * 1000);
 
-		fprintf(stdout, "wating timer ...\n");
-		ticks = waittmr(tfd, -1);
-		ttmr = getustime(clks[i].id);
-		if (ticks <= 0)
-			fprintf(stdout, "whooops! no timer showed up!\n");
-		else
-			fprintf(stdout, "got timer ticks (%ld) after %llu ms\n",
-				ticks, (ttmr - tnow) / 1000);
+	memset(&tmr, 0, sizeof(tmr));
+	if (timerfd_gettime(tfd, &tmr))
+		tst_res(TFAIL | TERRNO, "timerfd_gettime() failed");
 
-		fprintf(stdout, "absolute timer test (at 500 ms) ...\n");
-		tnow = getustime(clks[i].id);
-		set_timespec(&tmr.it_value, tnow + 500 * 1000);
-		set_timespec(&tmr.it_interval, 0);
-		if (timerfd_settime(tfd, TFD_TIMER_ABSTIME, &tmr, NULL)) {
-			perror("timerfd_settime");
-			return 1;
-		}
 
-		fprintf(stdout, "wating timer ...\n");
-		ticks = waittmr(tfd, -1);
-		ttmr = getustime(clks[i].id);
-		if (ticks <= 0)
-			fprintf(stdout, "whooops! no timer showed up!\n");
-		else
-			fprintf(stdout, "got timer ticks (%ld) after %llu ms\n",
-				ticks, (ttmr - tnow) / 1000);
+	if (tmr.it_value.tv_sec != 0 || tmr.it_value.tv_nsec > 50 * 1000000)
+		tst_res(TFAIL, "Timer read back value not relative");
+	else
+		tst_res(TPASS, "Timer read back value is relative");
 
-		fprintf(stdout, "sequential timer test (100 ms clock) ...\n");
-		tnow = getustime(clks[i].id);
-		set_timespec(&tmr.it_value, tnow + 100 * 1000);
-		set_timespec(&tmr.it_interval, 100 * 1000);
-		if (timerfd_settime(tfd, TFD_TIMER_ABSTIME, &tmr, NULL)) {
-			perror("timerfd_settime");
-			return 1;
-		}
+	usleep(160000);
 
-		fprintf(stdout, "sleeping 1 second ...\n");
-		sleep(1);
-		if (timerfd_gettime(tfd, &tmr)) {
-			perror("timerfd_gettime");
-			return 1;
-		}
-		fprintf(stdout, "timerfd_gettime returned:\n"
-			"\tit_value = { %ld, %ld } it_interval = { %ld, %ld }\n",
-			(long)tmr.it_value.tv_sec, (long)tmr.it_value.tv_nsec,
-			(long)tmr.it_interval.tv_sec,
-			(long)tmr.it_interval.tv_nsec);
-		fprintf(stdout, "sleeping 1 second ...\n");
-		sleep(1);
+	waittmr(tfd, 3);
 
-		fprintf(stdout, "wating timer ...\n");
-		ticks = waittmr(tfd, -1);
-		ttmr = getustime(clks[i].id);
-		if (ticks <= 0)
-			fprintf(stdout, "whooops! no timer showed up!\n");
-		else
-			fprintf(stdout, "got timer ticks (%ld) after %llu ms\n",
-				ticks, (ttmr - tnow) / 1000);
+	tst_res(TINFO, "testing with O_NONBLOCK");
+	settime(tfd, &tmr, 0, 100 * 1000, 0);
+	waittmr(tfd, 1);
 
-		fprintf(stdout, "O_NONBLOCK test ...\n");
-		tnow = getustime(clks[i].id);
-		set_timespec(&tmr.it_value, 100 * 1000);
-		set_timespec(&tmr.it_interval, 0);
-		if (timerfd_settime(tfd, 0, &tmr, NULL)) {
-			perror("timerfd_settime");
-			return 1;
-		}
-		fprintf(stdout, "timerfd = %d\n", tfd);
+	SAFE_FCNTL(tfd, F_SETFL, fcntl(tfd, F_GETFL, 0) | O_NONBLOCK);
 
-		fprintf(stdout, "wating timer (flush the single tick) ...\n");
-		ticks = waittmr(tfd, -1);
-		ttmr = getustime(clks[i].id);
-		if (ticks <= 0)
-			fprintf(stdout, "whooops! no timer showed up!\n");
-		else
-			fprintf(stdout, "got timer ticks (%ld) after %llu ms\n",
-				ticks, (ttmr - tnow) / 1000);
+	TEST(read(tfd, &uticks, sizeof(uticks)));
+	if (TST_RET > 0)
+		tst_res(TFAIL, "timer ticks not zero");
+	else if (TST_ERR != EAGAIN)
+		tst_res(TFAIL | TERRNO, "read should fail with EAGAIN got");
+	else
+		tst_res(TPASS | TERRNO, "read failed with");
 
-		fcntl(tfd, F_SETFL, fcntl(tfd, F_GETFL, 0) | O_NONBLOCK);
-
-		if (read(tfd, &uticks, sizeof(uticks)) > 0)
-			fprintf(stdout,
-				"whooops! timer ticks not zero when should have been\n");
-		else if (errno != EAGAIN)
-			fprintf(stdout,
-				"whooops! bad errno value (%d = '%s')!\n",
-				errno, strerror(errno));
-		else
-			fprintf(stdout, "success\n");
-
-		fcntl(tfd, F_SETFL, fcntl(tfd, F_GETFL, 0) & ~O_NONBLOCK);
-
-		close(tfd);
-	}
-
-	tst_exit();
+	SAFE_CLOSE(tfd);
 }
 
-#else
-int TST_TOTAL = 0;
-
-int main(void)
-{
-
-	tst_brkm(TCONF, NULL,
-		 "This test needs a kernel that has timerfd syscall.");
-}
-#endif
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
+	.min_kver = "2.6.25",
+};
