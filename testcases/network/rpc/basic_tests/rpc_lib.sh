8407ce72cfc18742ae48bb75c9ef8480a95dfde5
Petr Vorel <pvorel@suse.cz>
2020-02-28 18:35:12 +0100

    rpc_lib.sh: Print used portmap or rpcbind
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/rpc/basic_tests/rpc_lib.sh
+++ testcases/network/rpc/basic_tests/rpc_lib.sh
@@ -5,12 +5,11 @@
 
 check_portmap_rpcbind()
 {
-	tst_res TINFO "Checking for portmap or rpcbind"
-
 	if pgrep portmap > /dev/null; then
 		PORTMAPPER="portmap"
 	else
 		pgrep rpcbind > /dev/null && PORTMAPPER="rpcbind" || \
 			tst_brk TCONF "portmap or rpcbind is not running"
 	fi
+	tst_res TINFO "Using $PORTMAPPER"
 }
