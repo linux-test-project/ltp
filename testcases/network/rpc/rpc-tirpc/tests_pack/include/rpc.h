linux-test-project/ltp

commit 7fe1a8bf894209fe3b68874980ce5721a6e6f45a

rpc: Use libtirpc for all RPC tests + detect headers location
RPC implementation in glibc has been deprecated since glibc 2.16
(headers + bindings were available via --enable-obsolete-rpc) and other
libc ususaly don't implement rpc anyway, it's time to move on and link
against libtirpc. While this fixes problem on these libc
implementations, without libtirpc we obviously end up with no RPC tests.

openSUSE recently moved /usr/include/tirpc to /usr/include as part of
upgrade to glibc 2.27 [1]. Fedora is planning something similar [2].
That's why is needed to detect the correct location and use lapi header.

The reason doing it in one commit is not to break systems using still
/usr/include/tirpc for libtirpc headers:
In file included from ../../../../../../include/lapi/rpc.h:31:0,
                 from librpc01.c:19:
/usr/include/tirpc/rpc/rpc.h:74:10: fatal error: rpc/rpcb_clnt.h: No such file or directory
 #include <rpc/rpcb_clnt.h> /* rpcbind interface functions */

[1] https://build.opensuse.org/package/view_file/Base:System/libtirpc/libtirpc.changes?expand=1
[2] https://bugzilla.redhat.com/show_bug.cgi?id=1537432

Signed-off-by: Petr Vorel <pvorel@suse.cz>
Acked-by: Cyril Hrubis <chrubis@suse.cz>

m4/ltp-tirpc.m4
@@ -24,7 +24,7 @@ AC_DEFUN([LTP_CHECK_TIRPC],[
 	TIRPC_CPPFLAGS=""
 	TIRPC_LIBS=""
 
-	AC_CHECK_HEADER(tirpc/netconfig.h,[
+	AC_CHECK_HEADERS([tirpc/netconfig.h netconfig.h], [
 		TIRPC_CPPFLAGS="-I${SYSROOT}/usr/include/tirpc"
 		AC_DEFINE(HAVE_LIBTIRPC, 1, [Define to 1 if you have libtirpc headers installed])
 		AC_CHECK_LIB(tirpc, rpcb_set, [TIRPC_LIBS="-ltirpc"])])

testcases/network/Makefile
@@ -39,6 +39,10 @@ ifeq ($(ANDROID),1)
 FILTER_OUT_DIRS		+= lib6 rpc sockets
 endif
 
+ifeq ($(TIRPC_LIBS),)
+FILTER_OUT_DIRS		+= rpc
+endif
+
 $(INSTALL_TARGETS): | generate
 
 .PHONY: generate

include/lapi/rpc.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (c) 2018 Petr Vorel <pvorel@suse.cz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LAPI_RPC_H__
+#define LAPI_RPC_H__
+
+#include "config.h"
+
+#ifdef HAVE_NETCONFIG_H
+# include <netconfig.h>
+# include <rpc/rpc.h>
+# include <rpc/types.h>
+# include <rpc/xdr.h>
+# include <rpc/svc.h>
+#elif defined(HAVE_TIRPC_NETCONFIG_H)
+# include <tirpc/netconfig.h>
+# include <tirpc/rpc/rpc.h>
+# include <tirpc/rpc/types.h>
+# include <tirpc/rpc/xdr.h>
+# include <tirpc/rpc/svc.h>
+#else
+# error Missing rpc headers!
+#endif
+
+#endif	/* LAPI_RPC_H__ */

testcases/network/rpc/Makefile
@@ -26,8 +26,4 @@ include $(top_srcdir)/include/mk/env_pre.mk
 
 INSTALL_TARGETS		:= *.sh
 
-ifeq ($(TIRPC_LIBS),)
-FILTER_OUT_DIRS		+= rpc-tirpc
-endif
-
 include $(top_srcdir)/include/mk/generic_trunk_target.mk

testcases/network/rpc/basic_tests/rpc01/Makefile
@@ -49,3 +49,4 @@ MAKE_TARGETS		:= rpc1 rpc_server
 MAKE_DEPS		:= $(LIB)
 
 include $(top_srcdir)/include/mk/generic_trunk_target.mk
+include $(top_srcdir)/testcases/network/rpc/rpc-tirpc/tests_pack/Makefile.inc

testcases/network/rpc/basic_tests/rpc01/lib/Makefile
@@ -22,4 +22,5 @@ include $(top_srcdir)/include/mk/env_pre.mk
 
 INTERNAL_LIB		:= librpc01.a
 
+include $(top_srcdir)/testcases/network/rpc/rpc-tirpc/tests_pack/Makefile.inc
 include $(top_srcdir)/include/mk/lib.mk

commit 60b20c4280f471bd25131a4a37e7451037b4f65a

rpc: Enable and fix build basic RPC tests with glibc SunRPC
Although glibc deprecated SunRPC (optional in glibc 2.25 and earlier,
disabled by default in 2.26, build requires --enable-obsolete-rpc) and
people are advised to use libtirpc enable also for it. Motivation for it
is that nfs-utils allow compiling with glibc SunRPC.

We still need to restrict TI-RPC tests
(testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/tirpc/) to
libtirpc only as they use libtirpc only API (svc_reg, svc_unreg,
pmap_unset, clnt_dg_create).

NOTE: IMHO from long term perspective these tests should be cleaned and
moved to upstream (libtirpc and ntirpc) and glibc testing probably
abandoned.

Fixes: 7fe1a8bf8 ("rpc: Use libtirpc for all RPC tests + detect headers location")

Acked-by: Cyril Hrubis <chrubis@suse.cz>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/mk/config.mk.in
@@ -38,6 +38,7 @@ CRYPTO_LIBS		:= @CRYPTO_LIBS@
 LEXLIB			:= @LEXLIB@
 NUMA_LIBS		:= @NUMA_LIBS@
 SELINUX_LIBS		:= @SELINUX_LIBS@
+HAVE_RPC		:= @HAVE_RPC@
 TIRPC_CFLAGS		:= @TIRPC_CFLAGS@
 TIRPC_LIBS		:= @TIRPC_LIBS@
 KEYUTILS_LIBS		:= @KEYUTILS_LIBS@

m4/ltp-tirpc.m4
@@ -5,9 +5,23 @@ dnl Copyright (c) 2014 Oracle and/or its affiliates. All Rights Reserved.
 AC_DEFUN([LTP_CHECK_TIRPC], [
 	dnl libtirpc library and headers
 	PKG_CHECK_MODULES([LIBTIRPC], [libtirpc], [
+		have_libtirpc=yes
 		TIRPC_CFLAGS=$LIBTIRPC_CFLAGS
 		TIRPC_LIBS=$LIBTIRPC_LIBS
 	], [have_libtirpc=no])
+
+	dnl TI-RPC headers (in glibc, since 2.26 installed only when configured
+	dnl with --enable-obsolete-rpc)
+	dnl NOTE: To port tests for ntirpc would require use non-deprecated
+	dnl functions as it does not have the deprecated ones any more (e.g. use
+	dnl rpc_broadcast() instead of clnt_broadcast()), but glibc implementation
+	dnl does not have the new ones. We could either provide the deprecated
+	dnl functions (copy from libtirpc src/rpc_soc.c) or drop glibc tests.
+	AC_CHECK_HEADERS([rpc/rpc.h], [have_rpc_headers=yes])
+
+	if test "x$have_libtirpc" = "xyes" -o "x$have_rpc_headers" = "xyes"; then
+		AC_SUBST(HAVE_RPC, 1)
+	fi
 
 	dnl fix for old pkg-config (< 0.24)
 	dnl https://autotools.io/pkgconfig/pkg_check_modules.html

testcases/network/Makefile
@@ -1,24 +1,6 @@
-#
-#    network test suite Makefile.
-#
-#    Copyright (C) 2009, Cisco Systems Inc.
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
+# Copyright (C) 2009, Cisco Systems Inc.
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
 # Ngie Cooper, July 2009
-#
 
 top_srcdir		?= ../..
 
@@ -39,7 +21,7 @@ ifeq ($(ANDROID),1)
 FILTER_OUT_DIRS		+= lib6 rpc sockets
 endif
 
-ifeq ($(TIRPC_LIBS),)
+ifeq ($(HAVE_RPC),)
 FILTER_OUT_DIRS		+= rpc
 endif
 

testcases/network/rpc/basic_tests/rpc01/rpc_server.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 #include "librpc01.h"
 
 int debug = 0;

testcases/network/rpc/rpc-tirpc/tests_pack/Makefile
@@ -1,20 +1,5 @@
-#
-#    Copyright (C) 2014, Oracle and/or its affiliates. All Rights Reserved.
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
+# Copyright (C) 2014, Oracle and/or its affiliates. All Rights Reserved.
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
 
 top_srcdir		?= ../../../../..
 
@@ -23,6 +8,13 @@ include	$(top_srcdir)/include/mk/env_pre.mk
 LIBDIR			:= lib
 FILTER_OUT_DIRS		:= $(LIBDIR)
 LIB			:= $(LIBDIR)/librpc-tirpc.a
+
+# keep only rpc_*
+ifeq ($(TIRPC_LIBS),)
+FILTER_OUT_MAKE_TARGETS	+= tirpc_cleaner
+FILTER_OUT_DIRS	+= tirpc_svc_1 tirpc_svc_2 tirpc_svc_3 tirpc_svc_4 tirpc_svc_5 tirpc_svc_6 tirpc_svc_7 tirpc_svc_8 tirpc_svc_9 tirpc_svc_11
+endif
+
 
 $(LIBDIR):
 	mkdir -p "$@"

testcases/network/rpc/rpc-tirpc/tests_pack/include/rpc.h
@@ -13,4 +13,10 @@
 #include <rpc/xdr.h>
 #include <rpc/svc.h>
 
+/*
+ * For pmap_unset() and clnt_broadcast().
+ * Needed for glibc, which does not include <rpc/pmap_clnt.h> in <rpc/rpc.h>.
+ */
+#include <rpc/pmap_clnt.h>
+
 #endif	/* RPC_H__ */

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_cleaner.c
@@ -18,7 +18,7 @@
  */
 
 #include <stdio.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 #define VERSNUM 1 /* Default version used in the test binaries */
 

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/Makefile
@@ -1,22 +1,13 @@
-#
-#    Copyright (C) 2014, Oracle and/or its affiliates. All Rights Reserved.
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License along
-#    with this program; if not, write to the Free Software Foundation, Inc.,
-#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#
+# Copyright (C) 2014, Oracle and/or its affiliates. All Rights Reserved.
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
 
 top_srcdir		?= ../../../../../../..
+
+ifeq ($(TIRPC_LIBS),)
+# glibc does not define registerrpc() in <rpc/rpc.h>
+# registerrpc() is deprecated in libtirpc, it should be replaced by rpc_reg()
+FILTER_OUT_DIRS	+= rpc_regunreg_registerrpc
+endif
 
 include	$(top_srcdir)/include/mk/env_pre.mk
 include $(top_srcdir)/include/mk/generic_trunk_target.mk

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_addrmanagmt_pmap_getport/rpc_pmap_getport.c
@@ -29,8 +29,8 @@
 #include <stdio.h>
 #include <rpc/rpc.h>
 #include <netinet/in.h>
-#include <rpc/pmap_clnt.h>
 #include <netdb.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_addrmanagmt_pmap_rmtcall/rpc_pmap_rmtcall.c
@@ -27,9 +27,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <rpc/rpc.h>
 #include <errno.h>
 #include <netdb.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_addrmanagmt_pmap_rmtcall/rpc_pmap_rmtcall_performance.c
@@ -28,9 +28,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <rpc/rpc.h>
 #include <errno.h>
 #include <netdb.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_addrmanagmt_pmap_rmtcall/rpc_pmap_rmtcall_stress.c
@@ -27,9 +27,9 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <rpc/rpc.h>
 #include <errno.h>
 #include <netdb.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_addrmanagmt_pmap_set/rpc_pmap_set.c
@@ -27,7 +27,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_addrmanagmt_pmap_unset/rpc_pmap_unset.c
@@ -27,7 +27,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_broadc_clnt_broadcast/rpc_clnt_broadcast.c
@@ -28,7 +28,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_broadc_clnt_broadcast/rpc_clnt_broadcast_complex.c
@@ -28,10 +28,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rpc/rpc.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <rpc/pmap_clnt.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1000

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_broadc_clnt_broadcast/rpc_clnt_broadcast_dataint.c
@@ -29,7 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define INTPROCNUM 1000

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_broadc_clnt_broadcast/rpc_clnt_broadcast_performance.c
@@ -28,7 +28,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_broadc_clnt_broadcast/rpc_clnt_broadcast_scalability.c
@@ -28,7 +28,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_broadc_clnt_broadcast/rpc_clnt_broadcast_stress.c
@@ -28,7 +28,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_regunreg_registerrpc/rpc_registerrpc.c
@@ -28,10 +28,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rpc/rpc.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <utmp.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_regunreg_svc_register/rpc_svc_register.c
@@ -28,9 +28,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rpc/rpc.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_regunreg_svc_unregister/rpc_svc_unregister.c
@@ -28,9 +28,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rpc/rpc.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include "rpc.h"
 
 //Standard define
 #define PROCNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_stdcall_svc_freeargs/rpc_svc_freeargs_svc.c
@@ -29,7 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define VERSNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_stdcall_svc_getargs/rpc_svc_getargs.c
@@ -29,7 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define VERSNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/rpc/rpc_stdcall_svc_sendreply/rpc_svc_sendreply.c
@@ -29,7 +29,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <rpc/rpc.h>
+#include "rpc.h"
 
 //Standard define
 #define VERSNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_svc_1/rpc_svc_1.c
@@ -27,8 +27,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <rpc/rpc.h>
 #include <utmp.h>
+#include "rpc.h"
 
 //Standard define
 #define VERSNUM 1

testcases/network/rpc/rpc-tirpc/tests_pack/rpc_svc_2/rpc_svc_2.c
@@ -27,8 +27,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <rpc/rpc.h>
 #include <utmp.h>
+#include "rpc.h"
 
 //Standard define
 #define VERSNUM 1