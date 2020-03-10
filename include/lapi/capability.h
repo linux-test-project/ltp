3f9e6be934348999d49025360cfea4a9ff8fcedd
Ronald Monthero <rhmcruiser@gmail.com>
2020-03-10 13:04:30 +1000

    Modifying based on review for reorder of the macro
    Signed-off-by: Ronald Monthero <rmonther@redhat.com>

--- include/lapi/capability.h
+++ include/lapi/capability.h
@@ -24,12 +24,12 @@
 # define CAP_NET_RAW          13
 #endif
 
-#ifndef CAP_SYS_TIME
-#define CAP_SYS_TIME          25
-#endif
-
 #ifndef CAP_SYS_ADMIN
 # define CAP_SYS_ADMIN        21
+#endif
+
+#ifndef CAP_SYS_TIME
+#define CAP_SYS_TIME          25
 #endif
 
 #ifndef CAP_AUDIT_READ
