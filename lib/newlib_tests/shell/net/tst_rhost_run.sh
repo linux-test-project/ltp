9db7ee74d0b58b69afd153192dc4de84c870d4ed
Petr Vorel <pvorel@suse.cz>
2020-06-17 19:51:37 +0200

    net/test: Add basic testing for tst_rhost_run
    Regression test for use of nested single quotes
    (fixed in c1a2d53f6 "network/nfs_lib.sh: Use double quotes for grep
    pattern", which could be now reverted).
    
    Acked-by: Alexey Kodanev <alexey.kodanev@oracle.com>
    Reviewed-by: Xiao Yang <yangx.jy@cn.fujitsu.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ lib/newlib_tests/shell/net/tst_rhost_run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
+
+TST_TESTFUNC=do_test
+PATH="$(dirname $0)/../../../../testcases/lib/:$PATH"
+. tst_net.sh
+
+do_test()
+{
+	local file="/etc/fstab"
+
+	tst_rhost_run -c 'which grep' > /dev/null || \
+		tst_brk TCONF "grep not found on rhost"
+
+	tst_rhost_run -c "[ -f $file ]" || \
+		tst_brk TCONF "$file not found on rhost"
+
+	tst_rhost_run -s -c "grep -q \"[^ ]\" $file"
+	tst_rhost_run -s -c "grep -q '[^ ]' $file"
+
+	tst_res TPASS "tst_rhost_run is working"
+}
+
+tst_run
