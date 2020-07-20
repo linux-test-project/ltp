1b6ad5c95fb913b7657e22356e1ec0cdd64d22be
Xiao Yang <yangx.jy@cn.fujitsu.com>
2020-07-20 13:27:55 +0800

    syscalls/pidfd_open01.c: Add check for close-on-exec flag
    pidfd_open(2) will set close-on-exec flag on the file descriptor as
    it manpage states, so check close-on-exec flag by fcntl(2).
    
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>

--- testcases/kernel/syscalls/pidfd_open/pidfd_open01.c
+++ testcases/kernel/syscalls/pidfd_open/pidfd_open01.c
@@ -3,20 +3,34 @@
  * Copyright (c) 2020 Viresh Kumar <viresh.kumar@linaro.org>
  *
  * Description:
- * Basic pidfd_open() test, fetches the PID of the current process and tries to
- * get its file descriptor.
+ * Basic pidfd_open() test:
+ * 1) Fetch the PID of the current process and try to get its file descriptor.
+ * 2) Check that the close-on-exec flag is set on the file descriptor.
  */
+
+#include <unistd.h>
+#include <fcntl.h>
 #include "tst_test.h"
 #include "lapi/pidfd_open.h"
 
 static void run(void)
 {
+	int flag;
+
 	TEST(pidfd_open(getpid(), 0));
 
 	if (TST_RET == -1)
 		tst_brk(TFAIL | TTERRNO, "pidfd_open(getpid(), 0) failed");
 
+	flag = fcntl(TST_RET, F_GETFD);
+
 	SAFE_CLOSE(TST_RET);
+
+	if (flag == -1)
+		tst_brk(TFAIL | TERRNO, "fcntl(F_GETFD) failed");
+
+	if (!(flag & FD_CLOEXEC))
+		tst_brk(TFAIL, "pidfd_open(getpid(), 0) didn't set close-on-exec flag");
 
 	tst_res(TPASS, "pidfd_open(getpid(), 0) passed");
 }
