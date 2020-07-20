42d3631ffe5c9c1a706be53194e4adaf8ca77b92
Yang Xu <xuyang2018.jy@cn.fujitsu.com>
2020-07-20 10:16:32 +0800

    syscalls/msgsnd01: Add check for msg_lspid and msg_stime
    Signed-off-by: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/ipc/msgsnd/msgsnd01.c
+++ testcases/kernel/syscalls/ipc/msgsnd/msgsnd01.c
@@ -18,7 +18,7 @@
 #include "libnewipc.h"
 
 static key_t msgkey;
-static int queue_id = -1;
+static int queue_id = -1, pid;
 static struct buf {
 	long type;
 	char text[MSGSIZE];
@@ -27,12 +27,15 @@ static struct buf {
 static void verify_msgsnd(void)
 {
 	struct msqid_ds qs_buf;
+	time_t before_snd, after_snd;
 
+	time(&before_snd);
 	TEST(msgsnd(queue_id, &snd_buf, MSGSIZE, 0));
 	if (TST_RET == -1) {
 		tst_res(TFAIL | TTERRNO, "msgsnd() failed");
 		return;
 	}
+	time(&after_snd);
 
 	SAFE_MSGCTL(queue_id, IPC_STAT, &qs_buf);
 
@@ -41,14 +44,29 @@ static void verify_msgsnd(void)
 	else
 		tst_res(TFAIL, "queue bytes or number of queues mismatched");
 
+	if (qs_buf.msg_lspid == pid)
+		tst_res(TPASS, "PID of last msgsnd(2) matched");
+	else
+		tst_res(TFAIL, "PID of last msgsnd(2) mismatched");
+
+	if (qs_buf.msg_stime >= before_snd && qs_buf.msg_stime <= after_snd) {
+		tst_res(TPASS, "msg_stime = %lu in [%lu, %lu]",
+			(unsigned long)qs_buf.msg_stime,
+			(unsigned long)before_snd, (unsigned long)after_snd);
+	} else {
+		tst_res(TFAIL, "msg_stime = %lu out of [%lu, %lu]",
+			(unsigned long)qs_buf.msg_stime,
+			(unsigned long)before_snd, (unsigned long)after_snd);
+	}
+
 	SAFE_MSGRCV(queue_id, &rcv_buf, MSGSIZE, 1, 0);
 }
 
 static void setup(void)
 {
 	msgkey = GETIPCKEY();
-
 	queue_id = SAFE_MSGGET(msgkey, IPC_CREAT | IPC_EXCL | MSG_RW);
+	pid = getpid();
 }
 
 static void cleanup(void)
