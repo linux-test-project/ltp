155b06d4b6207cfc86f81ecf74a5d132f919adeb
hukoyu <hukoyu@microsoft.com>
2020-07-28 00:21:24 +0000

    LTP test patch for symlink->symlink03

--- testcases/kernel/syscalls/symlink/symlink03.c
+++ testcases/kernel/syscalls/symlink/symlink03.c
@@ -122,8 +122,8 @@ struct test_case_t {		/* test case struct. to hold ref. test cond's */
 	{TEST_FILE1, SYM_FILE1, "No Search permissions to process",
 		    EACCES, setup1}, {
 	TEST_FILE2, SYM_FILE2, "Specified symlink already exists",
-		    EEXIST, setup2}, {
-	TESTFILE, NULL, "Invalid address", EFAULT, no_setup}, {
+                   EEXIST, setup2},/* { TODO: Enable once git issue 297 is fixed
+       TESTFILE, NULL, "Invalid address", EFAULT, no_setup},*/ {
 	TESTFILE, Longpathname, "Symlink path too long", ENAMETOOLONG,
 		    longpath_setup}, {
 	TESTFILE, "", "Symlink Pathname is empty", ENOENT, no_setup}, {
@@ -211,6 +211,7 @@ void setup(void)
 	int ind;
 
 	tst_require_root();
+       tst_tmpdir();
 
 	tst_sig(NOFORK, DEF_HANDLER, cleanup);
 
@@ -225,8 +226,6 @@ void setup(void)
 	ltpuser = getpwnam(nobody_uid);
 	if (setuid(ltpuser->pw_uid) == -1)
 		tst_resm(TINFO | TERRNO, "setuid(%d) failed", ltpuser->pw_uid);
-
-	tst_tmpdir();
 
 	/* call individual setup functions */
 	for (ind = 0; Test_cases[ind].desc != NULL; ind++) {
@@ -346,7 +345,7 @@ void cleanup(void)
 
 	/* Restore mode permissions on test directory created in setup2() */
 	SAFE_CHMOD(NULL, DIR_TEMP, MODE_RWX);
-
+       tst_require_root();
 	tst_rmdir();
 
 }
