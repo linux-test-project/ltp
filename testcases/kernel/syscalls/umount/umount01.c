e2cd02961399a632e82fbcced1abc5c29c150ad3
hukoyu <hukoyu@microsoft.com>
2020-07-28 00:21:24 +0000

    LTP test patch for umount->umount01

--- testcases/kernel/syscalls/umount/umount01.c
+++ testcases/kernel/syscalls/umount/umount01.c
@@ -6,23 +6,19 @@
  * Phase I test for the umount(2) system call.
  * It is intended to provide a limited exposure of the system call.
  */
-
+/* Patching for lkl to use root file system as default LKL memory
+ * is set to 32MB.
+ */
 #include <errno.h>
 #include <sys/mount.h>
 #include "tst_test.h"
 
 #define MNTPOINT	"mntpoint"
 
-static int mount_flag;
+static int mount_flag = 0;
 
 static void verify_umount(void)
 {
-	if (mount_flag != 1) {
-		SAFE_MOUNT(tst_device->dev, MNTPOINT,
-			tst_device->fs_type, 0, NULL);
-		mount_flag = 1;
-	}
-
 	TEST(umount(MNTPOINT));
 
 	if (TST_RET != 0 && TST_ERR == EBUSY) {
@@ -42,19 +38,33 @@ static void verify_umount(void)
 
 static void setup(void)
 {
+	rmdir(MNTPOINT);
 	SAFE_MKDIR(MNTPOINT, 0775);
+
+	const char* src  = "/dev/vda";
+	const char* type = "ext4";
+	const unsigned long mntflags = 0;
+	const char* opts = "mode=0777";   
+	int result = mount(src, MNTPOINT, type, mntflags, opts);
+
+	if (result != 0) {
+		TST_RET = result;
+		tst_res(TFAIL, "umount()- setup Failed");
+		return;
+	}
 }
 
 static void cleanup(void)
 {
 	if (mount_flag)
 		tst_umount(MNTPOINT);
+	SAFE_RMDIR(MNTPOINT);
 }
 
 static struct tst_test test = {
 	.needs_root = 1,
-	.needs_tmpdir = 1,
-	.format_device = 1,
+	.needs_tmpdir = 0,
+	.format_device = 0,
 	.setup = setup,
 	.cleanup = cleanup,
 	.test_all = verify_umount,
