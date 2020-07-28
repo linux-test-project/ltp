810f38c92420477c90ac43bdfd65e336f5cc194f
hukoyu <hukoyu@microsoft.com>
2020-07-28 00:21:21 +0000

    LTP test patch for flistxattr->flistxattr02

--- testcases/kernel/syscalls/flistxattr/flistxattr02.c
+++ testcases/kernel/syscalls/flistxattr/flistxattr02.c
@@ -17,6 +17,11 @@
 * 2) flistxattr(2) should return -1 and set errno to EBADF.
 */
 
+/* Currently xattr is not enabled while mounting root file system. Patch is
+ * to mount root file system with xattr enabled and then use it for the test.
+*/
+
+#include <stdio.h>
 #include "config.h"
 #include <errno.h>
 #include <sys/types.h>
@@ -32,6 +37,13 @@
 #define SECURITY_KEY	"security.ltptest"
 #define VALUE	"test"
 #define VALUE_SIZE	(sizeof(VALUE) - 1)
+
+#define MNTPOINT        "mntpoint"
+#define DIR_MODE        (S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH)
+#define FILE_MODE       (S_IRWXU | S_IRWXG | S_IRWXO | S_ISUID | S_ISGID)
+#define TESTFILE "mntpoint/flistxattr02testfile"
+static const char *device = "/dev/vda";
+static const char *fs_type = "ext4";
 
 static int fd1;
 static int fd2 = -1;
@@ -70,7 +82,11 @@ static void verify_flistxattr(unsigned int n)
 
 static void setup(void)
 {
-	fd1 = SAFE_OPEN("testfile", O_RDWR | O_CREAT, 0644);
+	rmdir(MNTPOINT);
+	SAFE_MKDIR(MNTPOINT, DIR_MODE);
+	SAFE_MOUNT(device, MNTPOINT, fs_type, 0, "user_xattr");
+
+	fd1 = SAFE_OPEN(TESTFILE, O_RDWR | O_CREAT, 0644);
 
 	SAFE_FSETXATTR(fd1, SECURITY_KEY, VALUE, VALUE_SIZE, XATTR_CREATE);
 }
@@ -79,6 +95,9 @@ static void cleanup(void)
 {
 	if (fd1 > 0)
 		SAFE_CLOSE(fd1);
+	remove(TESTFILE);
+	SAFE_UMOUNT(MNTPOINT);
+	SAFE_RMDIR(MNTPOINT);
 }
 
 static struct tst_test test = {
