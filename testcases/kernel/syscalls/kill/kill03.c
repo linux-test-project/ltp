14582115ca3166e50f8f6d667c7fe89de74abfa7
Feiyu Zhu <zhufy.jy@cn.fujitsu.com>
2020-08-21 05:47:09 -0400

    syscalls/kill03, 04: Cleanup && Convert to new library
    1) Remove uclinux code
    2) Take use of safe macro
    3) Merge kill04 into kill03
    4) Add error test that pid is INT_MIN
    
    Reviewed-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
    Signed-off-by: Feiyu Zhu <zhufy.jy@cn.fujitsu.com>

--- testcases/kernel/syscalls/kill/kill03.c
+++ testcases/kernel/syscalls/kill/kill03.c
@@ -1,165 +1,58 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
+ * Copyright (c) International Business Machines  Corp., 2001
  *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
-
-/*
- * NAME
- *	kill03.c
- *
- * DESCRIPTION
- *	Test case to check that kill fails when given an invalid signal.
- *
- * ALGORITHM
- *	call setup
- *	loop if the -i option was given
- *	fork a child
- *	execute the kill system call with an invalid signal
- *	check the return value
- *	if return value is not -1
- *		issue a FAIL message, break remaining tests and cleanup
- *	if we are doing functional testing
- *		if the errno was set to 22 (invalid argument).
- *			issue a PASS message
- *		otherwise
- *			issue a FAIL message
- *	call cleanup
- *
- * USAGE
- *  kill03 [-c n] [-f] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -f   : Turn off functionality Testing.
- *             -i n : Execute test n times.
- *             -I x : Execute test for x seconds.
- *             -P x : Pause for x seconds between iterations.
- *             -t   : Turn on syscall timing.
+ * 1) kill() fails with errno set to EINVAL if given an invalid signal.
+ * 2) kill() fails with errno set to ESRCH if given a non-existent pid.
+ * 3) kill() fails with errno set to ESRCH if the given pid is INT_MIN.
  *
  * HISTORY
  *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS
- *	none
  */
 
-#include "test.h"
-
+#include <sys/types.h>
 #include <signal.h>
-#include <errno.h>
-#include <sys/wait.h>
+#include <unistd.h>
+#include "tst_test.h"
 
-void cleanup(void);
-void setup(void);
-void do_child(void);
+static pid_t real_pid, fake_pid, int_min_pid;
 
-char *TCID = "kill03";
-int TST_TOTAL = 1;
+static struct tcase {
+	int test_sig;
+	int exp_errno;
+	pid_t *pid;
+} tcases[] = {
+	{2000, EINVAL, &real_pid},
+	{SIGKILL, ESRCH, &fake_pid},
+	{SIGKILL, ESRCH, &int_min_pid}
+};
 
-#define TEST_SIG 2000
-
-int main(int ac, char **av)
+static void verify_kill(unsigned int n)
 {
-	int lc;
-	pid_t pid;
-	int exno, status;
+	struct tcase *tc = &tcases[n];
 
-	tst_parse_opts(ac, av, NULL, NULL);
-#ifdef UCLINUX
-	maybe_run_child(&do_child, "");
-#endif
-
-	setup();
-
-	/* The following loop checks looping state if -i option given */
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		/* reset tst_count in case we are looping */
-		tst_count = 0;
-		status = 1;
-		exno = 1;
-		pid = FORK_OR_VFORK();
-		if (pid < 0) {
-			tst_brkm(TBROK, cleanup, "Fork of child failed");
-		} else if (pid == 0) {
-#ifdef UCLINUX
-			if (self_exec(av[0], "") < 0) {
-				tst_brkm(TBROK, cleanup,
-					 "self_exec of child failed");
-			}
-#else
-			do_child();
-#endif
-		} else {
-			TEST(kill(pid, TEST_SIG));
-			kill(pid, SIGKILL);
-			waitpid(pid, &status, 0);
-		}
-
-		if (TEST_RETURN != -1) {
-			tst_brkm(TFAIL, cleanup, "%s failed - errno = %d : %s "
-				 "Expected a return value of -1 got %ld",
-				 TCID, TEST_ERRNO, strerror(TEST_ERRNO),
-				 TEST_RETURN);
-		}
-
-		/*
-		 * Check to see if the errno was set to the expected
-		 * value of 22 : EINVAL.
-		 */
-		if (TEST_ERRNO == EINVAL) {
-			tst_resm(TPASS, "errno set to %d : %s, as "
-				 "expected", TEST_ERRNO,
-				 strerror(TEST_ERRNO));
-		} else {
-			tst_resm(TFAIL, "errno set to %d : %s expected "
-				 "%d : %s", TEST_ERRNO,
-				 strerror(TEST_ERRNO), 22,
-				 strerror(22));
-		}
+	TEST(kill(*tc->pid, tc->test_sig));
+	if (TST_RET != -1) {
+		tst_res(TFAIL, "kill should fail but not, return %ld", TST_RET);
+		return;
 	}
 
-	cleanup();
-	tst_exit();
+	if (tc->exp_errno == TST_ERR)
+		tst_res(TPASS | TTERRNO, "kill failed as expected");
+	else
+		tst_res(TFAIL | TTERRNO, "kill expected %s but got",
+			tst_strerrno(tc->exp_errno));
 }
 
-/*
- * do_child()
- */
-void do_child(void)
+static void setup(void)
 {
-	int exno = 1;
-
-	pause();
-	exit(exno);
+	real_pid = getpid();
+	fake_pid = tst_get_unused_pid();
+	int_min_pid = INT_MIN;
 }
 
-/*
- * setup() - performs all ONE TIME setup for this test
- */
-void setup(void)
-{
-
-	TEST_PAUSE;
-}
-
-/*
- * cleanup() - performs all the ONE TIME cleanup for this test at completion
- * or premature exit.
- */
-void cleanup(void)
-{
-
-}
+static struct tst_test test = {
+	.test = verify_kill,
+	.tcnt = ARRAY_SIZE(tcases),
+	.setup = setup,
+};
