5ebec407778d7906ca510120481dbd2c3acd45f6
Yang Xu <xuyang2018.jy@cn.fujitsu.com>
2020-09-02 12:20:10 +0800

    security/dirtyc0w: Add CVE tag
    Also use correct fix commit id.
    
    Signed-off-by: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/security/dirtyc0w/dirtyc0w.c
+++ testcases/kernel/security/dirtyc0w/dirtyc0w.c
@@ -4,6 +4,8 @@
  */
 
 /*
+ * CVE-2016-5195
+ *
  * This is a regression test for write race that allows unprivileged programs
  * to change readonly files on the system.
  *
@@ -101,7 +103,8 @@ static struct tst_test test = {
 	.test_all = dirtyc0w_test,
 	.tags = (const struct tst_tag[]) {
 		{"linux-git", "4ceb5db9757a"},
-		{"linux-git", "9be0eaffa3ac"},
+		{"linux-git", "19be0eaffa3a"},
+		{"CVE", "2016-5195"},
 		{}
 	}
 };
