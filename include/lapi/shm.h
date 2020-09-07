9ad3e0b5272bbaecea3fd7abd962455e465d95a8
Cyril Hrubis <chrubis@suse.cz>
2020-09-07 17:01:50 +0200

    lapi/shm.h: Add SHM_STAT_ANY
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>
    Reviewed-by: Li Wang <liwang@redhat.com>

---
+++ include/lapi/shm.h
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2020 Cyril Hrubis <chrubis@suse.cz>
+ */
+
+#ifndef LAPI_SHM_H__
+#define LAPI_SHM_H__
+
+#ifndef SHM_STAT_ANY
+# define SHM_STAT_ANY 15
+#endif
+
+#endif /* LAPI_SHM_H__ */
