eb9908ab30fd9eb438065df798f825eb84c31fc1
Petr Vorel <pvorel@suse.cz>
2020-10-15 13:24:55 +0200

    syscalls/get_mempolicy01: Rewrite to new API
    Test changes:
    * use new LTP NUMA API (tst_get_nodemap())
    * add nodemask verification for get_mempolicy01.c
    * split failure cases into get_mempolicy02.c
    
    Also finally remove common_j_h.c and include_j_h.h,
    which are no longer needed.
    
    Reviewed-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/get_mempolicy/get_mempolicy01.c
+++ testcases/kernel/syscalls/get_mempolicy/get_mempolicy01.c
@@ -1,320 +1,213 @@
-/******************************************************************************/
-/* Copyright (c) Crackerjack Project., 2007-2008 ,Hitachi, Ltd		      */
-/*	Author(s): Takahiro Yasui <takahiro.yasui.mp@hitachi.com>,	      */
-/*		   Yumiko Sugita <yumiko.sugita.yf@hitachi.com>,	      */
-/*		   Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>		      */
-/*									      */
-/* This program is free software;  you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by	      */
-/* the Free Software Foundation; either version 2 of the License, or	      */
-/* (at your option) any later version.					      */
-/*									      */
-/* This program is distributed in the hope that it will be useful,	      */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of	      */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See		      */
-/* the GNU General Public License for more details.			      */
-/*									      */
-/* You should have received a copy of the GNU General Public License	      */
-/* along with this program;  if not, write to the Free Software		      */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
-/*									      */
-/******************************************************************************/
-/******************************************************************************/
-/*									      */
-/* File:	get_mempolicy01.c					      */
-/*									      */
-/* Description: This tests the get_mempolicy() syscall			      */
-/*									      */
-/* Usage:  <for command-line>						      */
-/* get_mempolicy01 [-c n] [-e][-i n] [-I x] [-p x] [-t]			      */
-/*	where,	-c n : Run n copies concurrently.			      */
-/*		-e   : Turn on errno logging.				      */
-/*		-i n : Execute test n times.				      */
-/*		-I x : Execute test for x seconds.			      */
-/*		-P x : Pause for x seconds between iterations.		      */
-/*		-t   : Turn on syscall timing.				      */
-/*									      */
-/* Total Tests: 1							      */
-/*									      */
-/* Test Name:	get_mempolicy01						      */
-/* History:	Porting from Crackerjack to LTP is done by		      */
-/*		Manas Kumar Nayak maknayak@in.ibm.com>			      */
-/******************************************************************************/
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
+ * Copyright (c) Linux Test Project, 2009-2020
+ * Copyright (c) Crackerjack Project, 2007-2008, Hitachi, Ltd
+ *
+ * Authors:
+ * Takahiro Yasui <takahiro.yasui.mp@hitachi.com>
+ * Yumiko Sugita <yumiko.sugita.yf@hitachi.com>
+ * Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>
+ * Manas Kumar Nayak <maknayak@in.ibm.com> (original port to the legacy API)
+ */
+
+/*\
+ * [DESCRIPTION]
+ *
+ * Verify that get_mempolicy() returns a proper return value and errno for various cases.
+\*/
 
 #include "config.h"
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <getopt.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <libgen.h>
-#if HAVE_NUMA_H
-#include <numa.h>
-#endif
-#if HAVE_NUMAIF_H
-#include <numaif.h>
-#endif
-
-#include "test.h"
-#include "lapi/syscalls.h"
-#include "include_j_h.h"
-#include "common_j_h.c"
-#include "numa_helper.h"
-
-char *TCID = "get_mempolicy01";
-int TST_TOTAL = 1;
+#include "tst_test.h"
 
 #ifdef HAVE_NUMA_V2
+#include <numa.h>
+#include <numaif.h>
+#include <errno.h>
+#include "tst_numa.h"
 
 #define MEM_LENGTH	(4 * 1024 * 1024)
+#define PAGES_ALLOCATED 16u
 
-static int testno;
+#define POLICY_DESC(x) .policy = x, .desc = "policy: "#x
+#define POLICY_DESC_NO_TARGET(x) .policy = x, .desc = "policy: "#x", no target"
+#define POLICY_DESC_FLAGS(x, y) .policy = x, .flags = y, .desc = "policy: "#x", flags: "#y
+#define POLICY_DESC_FLAGS_NO_TARGET(x, y) .policy = x, .flags = y, .desc = "policy: "#x", flags: "#y", no target"
 
-enum test_type {
-	DEFAULT,		/* get default policy */
-	ADDR,			/* get policy of memory which include mapped address */
-	INVALID_POINTER,
-	INVALID_FLAGS,
-};
-
-enum from_node {
-	NONE,
-	SELF,
-};
+static struct tst_nodemap *node;
+static struct bitmask *nodemask, *getnodemask, *empty_nodemask;
 
 struct test_case {
-	int ttype;
 	int policy;
-	int from_node;
-	int ret;
-	int err;
+	const char *desc;
+	unsigned int flags;
+	char *addr;
+	int (*pre_test)(struct test_case *tc);
+	int (*alloc)(struct test_case *tc);
+	struct bitmask **exp_nodemask;
 };
 
-/* Test cases
- *
- *   test status of errors on man page
- *
- *   (NONE)	     man page hadn't been completed.
- *
- *   test status of errors NOT on man page
- *
- *   EFAULT	     v (invalid address)
- *   EINVAL	     v (invalid parameters)
- */
+static int test_set_mempolicy_default(struct test_case *tc);
+static int test_set_mempolicy_none(struct test_case *tc);
+static int test_mbind_none(struct test_case *tc);
+static int test_mbind_default(struct test_case *tc);
+
 static struct test_case tcase[] = {
-	{			/* case00 */
-	 .ttype = DEFAULT,
-	 .policy = MPOL_DEFAULT,
-	 .from_node = NONE,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case01 */
-	 .ttype = DEFAULT,
-	 .policy = MPOL_BIND,
-	 .from_node = SELF,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case02 */
-	 .ttype = DEFAULT,
-	 .policy = MPOL_INTERLEAVE,
-	 .from_node = SELF,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case03 */
-	 .ttype = DEFAULT,
-	 .policy = MPOL_PREFERRED,
-	 .from_node = NONE,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case04 */
-	 .ttype = DEFAULT,
-	 .policy = MPOL_PREFERRED,
-	 .from_node = SELF,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case05 */
-	 .ttype = ADDR,
-	 .policy = MPOL_DEFAULT,
-	 .from_node = NONE,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case06 */
-	 .ttype = ADDR,
-	 .policy = MPOL_BIND,
-	 .from_node = SELF,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case07 */
-	 .ttype = ADDR,
-	 .policy = MPOL_INTERLEAVE,
-	 .from_node = SELF,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case08 */
-	 .ttype = ADDR,
-	 .policy = MPOL_PREFERRED,
-	 .from_node = NONE,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case09 */
-	 .ttype = ADDR,
-	 .policy = MPOL_PREFERRED,
-	 .from_node = SELF,
-	 .ret = 0,
-	 .err = 0,
-	 },
-	{			/* case10 */
-	 .ttype = INVALID_POINTER,
-	 .policy = MPOL_DEFAULT,
-	 .from_node = NONE,
-	 .ret = -1,
-	 .err = EFAULT,
-	 },
-	{			/* case11 */
-	 .ttype = INVALID_FLAGS,
-	 .policy = MPOL_DEFAULT,
-	 .from_node = NONE,
-	 .ret = -1,
-	 .err = EINVAL,
-	 },
+	{
+		POLICY_DESC_NO_TARGET(MPOL_DEFAULT),
+		.alloc = test_set_mempolicy_none,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC(MPOL_BIND),
+		.alloc = test_set_mempolicy_default,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC(MPOL_INTERLEAVE),
+		.alloc = test_set_mempolicy_default,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC_NO_TARGET(MPOL_PREFERRED),
+		.alloc = test_set_mempolicy_none,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC(MPOL_PREFERRED),
+		.alloc = test_set_mempolicy_default,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC_FLAGS_NO_TARGET(MPOL_DEFAULT, MPOL_F_ADDR),
+		.pre_test = test_mbind_none,
+		.alloc = test_set_mempolicy_none,
+		.exp_nodemask = &empty_nodemask,
+	},
+	{
+		POLICY_DESC_FLAGS(MPOL_BIND, MPOL_F_ADDR),
+		.pre_test = test_mbind_default,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC_FLAGS(MPOL_INTERLEAVE, MPOL_F_ADDR),
+		.pre_test = test_mbind_default,
+		.alloc = test_set_mempolicy_default,
+		.exp_nodemask = &nodemask,
+	},
+	{
+		POLICY_DESC_FLAGS_NO_TARGET(MPOL_PREFERRED, MPOL_F_ADDR),
+		.pre_test = test_mbind_none,
+		.alloc = test_set_mempolicy_none,
+		.exp_nodemask = &empty_nodemask,
+	},
+	{
+		POLICY_DESC_FLAGS(MPOL_PREFERRED, MPOL_F_ADDR),
+		.pre_test = test_mbind_default,
+		.alloc = test_set_mempolicy_default,
+		.exp_nodemask = &nodemask,
+	},
 };
 
-static int do_test(struct test_case *tc);
-static void setup(void);
-static void cleanup(void);
-
-int main(int argc, char **argv)
+static int test_set_mempolicy_default(struct test_case *tc)
 {
-	int i, ret, lc;
-
-	setup();
-
-	ret = 0;
-	testno = (int)ARRAY_SIZE(tcase);
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		for (i = 0; i < testno; i++) {
-			tst_resm(TINFO, "(case%02d) START", i);
-			ret = do_test(&tcase[i]);
-			tst_resm((ret == 0 ? TPASS : TFAIL | TERRNO),
-				 "(case%02d) END", i);
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	TEST(set_mempolicy(tc->policy, nodemask->maskp, nodemask->size));
+	return TST_RET;
 }
 
-static int do_test(struct test_case *tc)
+static int test_set_mempolicy_none(struct test_case *tc)
 {
-	int ret, err, result, cmp_ok;
-	int policy, flags;
-	struct bitmask *nodemask = numa_allocate_nodemask();
-	struct bitmask *getnodemask = numa_allocate_nodemask();
-	char *p = NULL;
-	unsigned long len = MEM_LENGTH;
-	int test_node = -1;
-
-	ret = get_allowed_nodes(NH_MEMS, 1, &test_node);
-	if (ret < 0)
-		tst_brkm(TBROK | TERRNO, cleanup, "get_allowed_nodes: %d", ret);
-	numa_bitmask_setbit(nodemask, test_node);
-	switch (tc->ttype) {
-	case DEFAULT:
-		flags = 0;
-		p = NULL;
-		if (tc->from_node == NONE)
-			TEST(ltp_syscall(__NR_set_mempolicy, tc->policy,
-				NULL, 0));
-		else
-			TEST(ltp_syscall(__NR_set_mempolicy, tc->policy,
-				nodemask->maskp, nodemask->size));
-		if (TEST_RETURN < 0) {
-			tst_resm(TBROK | TERRNO, "set_mempolicy");
-			return -1;
-		}
-
-		break;
-	default:
-		flags = MPOL_F_ADDR;
-		p = mmap(NULL, len, PROT_READ | PROT_WRITE,
-			 MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-		if (p == MAP_FAILED)
-			tst_brkm(TBROK | TERRNO, cleanup, "mmap");
-		if (tc->from_node == NONE)
-			TEST(ltp_syscall(__NR_mbind, p, len, tc->policy,
-				NULL, 0, 0));
-		else
-			TEST(ltp_syscall(__NR_mbind, p, len, tc->policy,
-				nodemask->maskp, nodemask->size, 0));
-		if (TEST_RETURN < 0) {
-			tst_resm(TBROK | TERRNO, "mbind");
-			return -1;
-		}
-
-		if (tc->ttype == INVALID_POINTER)
-#ifdef __ia64__
-			p = (char *)0x8000000000000000UL;
-#else
-			p = 0;
-#endif
-
-		if (tc->ttype == INVALID_FLAGS)
-			flags = -1;
-	}
-	errno = 0;
-	cmp_ok = 1;
-	TEST(ret = ltp_syscall(__NR_get_mempolicy, &policy, getnodemask->maskp,
-			   getnodemask->size, p, flags));
-	err = TEST_ERRNO;
-	if (ret < 0)
-		goto TEST_END;
-
-	/* if policy == MPOL_DEFAULT, get_mempolicy doesn't return nodemask */
-	if (tc->policy == MPOL_DEFAULT)
-		numa_bitmask_clearall(nodemask);
-	cmp_ok = (tc->policy == policy && (tc->from_node == NONE ||
-					   numa_bitmask_equal(nodemask,
-							      getnodemask)));
-TEST_END:
-	result = (err != tc->err) || !cmp_ok;
-	PRINT_RESULT_CMP(0, tc->ret, tc->err, ret, err, cmp_ok);
-	return result;
+	TEST(set_mempolicy(tc->policy, NULL, 0));
+	return TST_RET;
 }
 
-static void cleanup(void)
+static int test_mbind(struct test_case *tc, unsigned long *maskp, unsigned long size)
 {
-	tst_rmdir();
+	tc->addr = SAFE_MMAP(NULL, MEM_LENGTH, PROT_READ | PROT_WRITE,
+			     MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+
+	TEST(mbind(tc->addr, MEM_LENGTH, tc->policy, maskp, size, 0));
+	return TST_RET;
+}
+
+static int test_mbind_none(struct test_case *tc)
+{
+	return test_mbind(tc, NULL, 0);
+}
+
+static int test_mbind_default(struct test_case *tc)
+{
+	return test_mbind(tc, nodemask->maskp, nodemask->size);
 }
 
 static void setup(void)
 {
-	/* check syscall availability */
-	ltp_syscall(__NR_get_mempolicy, NULL, NULL, 0, NULL, 0);
+	unsigned int i;
 
-	if (!is_numa(NULL, NH_MEMS, 1))
-		tst_brkm(TCONF, NULL, "requires NUMA with at least 1 node");
+	node = tst_get_nodemap(TST_NUMA_MEM, PAGES_ALLOCATED * getpagesize() / 1024);
+	if (node->cnt < 1)
+		tst_brk(TCONF, "test requires at least one NUMA memory node");
 
-	TEST_PAUSE;
-	tst_tmpdir();
+	nodemask = numa_allocate_nodemask();
+	empty_nodemask = numa_allocate_nodemask();
+	getnodemask = numa_allocate_nodemask();
+	numa_bitmask_setbit(nodemask, node->map[0]);
+
+	for (i = 0; i < ARRAY_SIZE(tcase); i++) {
+		struct test_case *tc = &tcase[i];
+
+		if (tc->pre_test && tc->pre_test(tc))
+			tst_brk(TFAIL | TERRNO, "test #%d: mbind() failed", i+1);
+
+		if (tc->alloc && tc->alloc(tc))
+			tst_brk(TFAIL | TERRNO, "test #%d: set_mempolicy() failed", i+1);
+	}
 }
+
+static void cleanup(void)
+{
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(tcase); i++) {
+		struct test_case *tc = &tcase[i];
+
+		if (tc->pre_test)
+			SAFE_MUNMAP(tc->addr, MEM_LENGTH);
+	}
+
+	numa_free_nodemask(nodemask);
+	numa_free_nodemask(getnodemask);
+	tst_nodemap_free(node);
+}
+
+static void do_test(unsigned int i)
+{
+	struct test_case *tc = &tcase[i];
+	int policy;
+
+	tst_res(TINFO, "test #%d: %s", i+1, tc->desc);
+
+	TST_EXP_PASS(get_mempolicy(&policy, getnodemask->maskp, getnodemask->size,
+			   tc->addr, tc->flags), "%s", tc->desc);
+
+		struct bitmask *exp_mask = *(tc->exp_nodemask);
+
+		if (!numa_bitmask_equal(exp_mask, getnodemask)) {
+			tst_res(TFAIL, "masks are not equal");
+			tst_res_hexd(TINFO, exp_mask->maskp,
+				     exp_mask->size / 8, "expected:");
+			tst_res_hexd(TINFO, getnodemask->maskp,
+				     getnodemask->size / 8, "returned:");
+		}
+}
+
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(tcase),
+	.test = do_test,
+	.setup = setup,
+	.cleanup = cleanup,
+};
 
 #else
-int main(void)
-{
-	tst_brkm(TCONF, NULL, NUMA_ERROR_MSG);
-}
-#endif
+TST_TEST_TCONF(NUMA_ERROR_MSG);
+#endif /* HAVE_NUMA_V2 */
---
+++ testcases/kernel/syscalls/get_mempolicy/get_mempolicy02.c
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
+ * Copyright (c) Linux Test Project, 2009-2020
+ * Copyright (c) Crackerjack Project, 2007-2008, Hitachi, Ltd
+ *
+ * Authors:
+ * Takahiro Yasui <takahiro.yasui.mp@hitachi.com>
+ * Yumiko Sugita <yumiko.sugita.yf@hitachi.com>
+ * Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>
+ * Manas Kumar Nayak <maknayak@in.ibm.com> (original port to the legacy API)
+ */
+
+/*\
+ * [DESCRIPTION]
+ *
+ * Verify that get_mempolicy() returns a proper return errno for failure cases.
+\*/
+
+#include "config.h"
+#include "tst_test.h"
+
+#ifdef HAVE_NUMA_V2
+#include <numa.h>
+#include <numaif.h>
+#include <errno.h>
+#include "tst_numa.h"
+
+#define PAGES_ALLOCATED 16u
+
+#define POLICY_DESC_TEXT(x, y) .policy = x, .desc = "policy: "#x", "y
+
+static struct tst_nodemap *node;
+static struct bitmask *nodemask;
+
+struct test_case {
+	int policy;
+	const char *desc;
+	unsigned int flags;
+	int err;
+	char *addr;
+};
+
+static struct test_case tcase[] = {
+	{
+		POLICY_DESC_TEXT(MPOL_DEFAULT, "invalid address"),
+		.addr = NULL,
+		.err = EFAULT,
+		.flags = MPOL_F_ADDR,
+	},
+	{
+		POLICY_DESC_TEXT(MPOL_DEFAULT, "invalid flags, no target"),
+		.err = EINVAL,
+		.flags = -1,
+	},
+};
+
+static void setup(void)
+{
+	node = tst_get_nodemap(TST_NUMA_MEM, PAGES_ALLOCATED * getpagesize() / 1024);
+	if (node->cnt < 1)
+		tst_brk(TCONF, "test requires at least one NUMA memory node");
+
+	nodemask = numa_allocate_nodemask();
+}
+
+static void cleanup(void)
+{
+	numa_free_nodemask(nodemask);
+	tst_nodemap_free(node);
+}
+
+static void do_test(unsigned int i)
+{
+	struct test_case *tc = &tcase[i];
+	int policy;
+
+	TST_EXP_FAIL(get_mempolicy(&policy, nodemask->maskp, nodemask->size,
+				   tc->addr, tc->flags), tc->err, "%s", tc->desc);
+}
+
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(tcase),
+	.test = do_test,
+	.setup = setup,
+	.cleanup = cleanup,
+};
+
+#else
+TST_TEST_TCONF(NUMA_ERROR_MSG);
+#endif /* HAVE_NUMA_V2 */
