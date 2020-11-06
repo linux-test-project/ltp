c07be3c0d019af9c738c1bc5fc132cc168371ffb
Xiao Yang <yangx.jy@cn.fujitsu.com>
2020-11-06 14:50:13 +0800

    syscalls/uname{01, 03}: Convert to new API and cleanup
    Also merge uname03 into uname01.
    
    Signed-off-by: Xiao Yang <yangx.jy@cn.fujitsu.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/uname/uname01.c
+++ testcases/kernel/syscalls/uname/uname01.c
@@ -1,162 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
+ * Basic test for uname(2):
+ * Calling uname() succeeded and got correct sysname.
  *
  */
-/* $Id: uname01.c,v 1.6 2009/11/02 13:57:19 subrata_modak Exp $ */
-/**********************************************************
- *
- *    OS Test - Silicon Graphics, Inc.
- *
- *    TEST IDENTIFIER	: uname01
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for uname(2)
- *
- *    PARENT DOCUMENT	: usctpl01
- *
- *    TEST CASE TOTAL	: 1
- *
- *    WALL CLOCK TIME	: 1
- *
- *    CPU TYPES		: ALL
- *
- *    AUTHOR		: William Roske
- *
- *    CO-PILOT		: Dave Fenner
- *
- *    DATE STARTED	: 03/30/92
- *
- *    INITIAL RELEASE	: UNICOS 7.0
- *
- *    TEST CASES
- *
- * 	1.) uname(2) returns...(See Description)
- *
- *    INPUT SPECIFICATIONS
- * 	The standard options for system call tests are accepted.
- *	(See the parse_opts(3) man page).
- *
- *    OUTPUT SPECIFICATIONS
- *$
- *    DURATION
- * 	Terminates - with frequency and infinite modes.
- *
- *    SIGNALS
- * 	Uses SIGUSR1 to pause before test if option set.
- * 	(See the parse_opts(3) man page).
- *
- *    RESOURCES
- * 	None
- *
- *    ENVIRONMENTAL NEEDS
- *      No run-time environmental needs.
- *
- *    SPECIAL PROCEDURAL REQUIREMENTS
- * 	None
- *
- *    INTERCASE DEPENDENCIES
- * 	None
- *
- *    DETAILED DESCRIPTION
- *	This is a Phase I test for the uname(2) system call.  It is intended
- *	to provide a limited exposure of the system call, for now.  It
- *	should/will be extended when full functional tests are written for
- *	uname(2).
- *
- * 	Setup:
- * 	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- * 	Test:
- *	 Loop if the proper options are given.
- * 	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- * 	Cleanup:
- * 	  Print errno log and/or timing stats if options given
- *
- *
- *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#**/
 
 #include <sys/utsname.h>
 #include <errno.h>
 #include <string.h>
-#include <signal.h>
-#include "test.h"
+#include "tst_test.h"
 
-void setup();
-void cleanup();
-
-char *TCID = "uname01";
-int TST_TOTAL = 1;
-
-struct utsname un;
-
-int main(int ac, char **av)
+static void verify_uname(void)
 {
-	int lc;
+	struct utsname un;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	memset(&un, 0, sizeof(un));
 
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(uname(&un));
-
-		if (TEST_RETURN == -1)
-			tst_resm(TFAIL | TTERRNO, "uname failed");
-		else
-			tst_resm(TPASS, "uname(&un) returned %ld", TEST_RETURN);
-
+	TEST(uname(&un));
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TTERRNO, "uname() failed");
+		return;
 	}
 
-	cleanup();
-	tst_exit();
+	if (TST_RET != 0) {
+		tst_res(TFAIL | TTERRNO,
+			"uname() returned invalid value %ld", TST_RET);
+		return;
+	}
+
+	if (strcmp(un.sysname, "Linux")) {
+		tst_res(TFAIL, "sysname is not Linux");
+		return;
+	}
+
+	tst_res(TPASS, "uname() succeeded");
 }
 
-void setup(void)
-{
-	void trapper();
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-}
-
-void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test_all = verify_uname,
+};
