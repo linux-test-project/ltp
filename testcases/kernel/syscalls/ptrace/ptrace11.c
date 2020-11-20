338b7094a9dfbaa35f15d937b87f6a90604e9b55
Yang Xu <xuyang2018.jy@cn.fujitsu.com>
2020-11-20 11:16:33 +0800

    syscalls/ptrace11: Add test for tracing init process
    Before kernel 2.6.26, ptrace can't trace init process and will get
    EPERM error. Now, it should succeed when we have enough privileges.
    
    Signed-off-by: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

---
+++ testcases/kernel/syscalls/ptrace/ptrace11.c
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2020 FUJITSU LIMITED. All rights reserved.
+ * Author: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
+ */
+
+/*\
+ * [DESCRIPTION]
+ *
+ * Before kernel 2.6.26 we could not trace init(1) process and ptrace() would
+ * fail with EPERM. This case just checks whether we can trace init(1) process
+ * successfully.
+\*/
+
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <pwd.h>
+#include <config.h>
+#include <stdlib.h>
+#include "ptrace.h"
+#include "tst_test.h"
+
+static void verify_ptrace(void)
+{
+	TEST(ptrace(PTRACE_ATTACH, 1, NULL, NULL));
+	if (TST_RET == 0)
+		tst_res(TPASS, "ptrace() traces init process successfully");
+	else
+		tst_res(TFAIL | TTERRNO,
+			"ptrace() returns %ld, failed unexpectedly", TST_RET);
+
+	/*
+	 * Wait until tracee is stopped by SIGSTOP otherwise detach will fail
+	 * with ESRCH.
+	 */
+	SAFE_WAITPID(1, NULL, 0);
+
+	SAFE_PTRACE(PTRACE_DETACH, 1, NULL, NULL);
+}
+
+static struct tst_test test = {
+	.test_all = verify_ptrace,
+	.needs_root = 1,
+};
