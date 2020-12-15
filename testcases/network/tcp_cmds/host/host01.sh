c6931abb61123bdd5eeb967c87f0ac380b9d971e
Petr Vorel <pvorel@suse.cz>
2020-12-15 21:55:57 +0100

    host01.sh: Fix getting address
    to get only single address for records like this:
    
    $ host openqa.opensuse.org
    openqa.opensuse.org is an alias for proxy-nue.opensuse.org.
    proxy-nue.opensuse.org has address 195.135.221.140
    proxy-nue.opensuse.org has IPv6 address 2001:67c:2178:8::16
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/tcp_cmds/host/host01.sh
+++ testcases/network/tcp_cmds/host/host01.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2020 Petr Vorel <pvorel@suse.cz>
 # Copyright (c) Köry Maincent <kory.maincent@bootlin.com> 2020
 # Copyright (c) Manoj Iyer <manjo@mail.utexas.edu> 2003
 # Copyright (c) Robbie Williamson <robbiew@us.ibm.com> 2001
 # Copyright (c) International Business Machines  Corp., 2000
 
 TST_TESTFUNC="do_test"
-TST_NEEDS_CMDS="awk host hostname"
+TST_NEEDS_CMDS="awk grep host hostname tail"
 
 . tst_net.sh
 
@@ -16,9 +17,13 @@ do_test()
 	local addr
 
 	tst_res TINFO "test basic functionality of the host command"
+	tst_res TINFO "lhost: $lhost"
 
 	if addr=$(host $lhost); then
-		addr=$(echo "$addr" | awk '{print $NF}')
+		addr=$(echo "$addr" | grep address | tail -1 | awk '{print $NF}')
+		if [ -z "$addr" ]; then
+			tst_brk TFAIL "empty address"
+		fi
 		EXPECT_PASS host $addr \>/dev/null
 	else
 		tst_brk TFAIL "host $lhost on local machine failed"
