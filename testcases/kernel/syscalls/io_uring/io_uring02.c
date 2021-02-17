torvalds/linux

commit 907d1df30a51cc1a1d25414a00cde0494b83df7b

io_uring: fix wqe->lock/completion_lock deadlock
Joseph reports following deadlock:

CPU0:
...
io_kill_linked_timeout  // &ctx->completion_lock
io_commit_cqring
__io_queue_deferred
__io_queue_async_work
io_wq_enqueue
io_wqe_enqueue  // &wqe->lock

CPU1:
...
__io_uring_files_cancel
io_wq_cancel_cb
io_wqe_cancel_pending_work  // &wqe->lock
io_cancel_task_cb  // &ctx->completion_lock

Only __io_queue_deferred() calls queue_async_work() while holding
ctx->completion_lock, enqueue drained requests via io_req_task_queue()
instead.

Cc: stable@vger.kernel.org # 5.9+
Reported-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Tested-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

fs/io_uring.c
@@ -1026,6 +1026,7 @@ static int io_setup_async_rw(struct io_kiocb *req, const struct iovec *iovec,
 			     const struct iovec *fast_iov,
 			     struct iov_iter *iter, bool force);
 static void io_req_drop_files(struct io_kiocb *req);
+static void io_req_task_queue(struct io_kiocb *req);
 
 static struct kmem_cache *req_cachep;
 
@@ -1634,18 +1635,11 @@ static void __io_queue_deferred(struct io_ring_ctx *ctx)
 	do {
 		struct io_defer_entry *de = list_first_entry(&ctx->defer_list,
 						struct io_defer_entry, list);
-		struct io_kiocb *link;
 
 		if (req_need_defer(de->req, de->seq))
 			break;
 		list_del_init(&de->list);
-		/* punt-init is done before queueing for defer */
-		link = __io_queue_async_work(de->req);
-		if (link) {
-			__io_queue_linked_timeout(link);
-			/* drop submission reference */
-			io_put_req_deferred(link, 1);
-		}
+		io_req_task_queue(de->req);
 		kfree(de);
 	} while (!list_empty(&ctx->defer_list));
 }

linux-test-project/ltp

commit e3dd5d2a92f6d028730a02d0cb391ec4f0d25785

syscalls/io_uring02: Use IOSQE_ASYNC when available
This is required as test got broken by 907d1df30a51 ("io_uring: fix
wqe->lock/completion_lock deadlock") from v5.11-rc6.

Reviewed-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: Martin Doucha <mdoucha@suse.cz>

testcases/kernel/syscalls/io_uring/io_uring02.c
@@ -79,20 +79,17 @@ static void setup(void)
 	SAFE_CHROOT(CHROOT_DIR);
 }
 
-static void run(void)
+static void drain_fallback(void)
 {
 	uint32_t i, count, tail;
 	int beef_found = 0;
-	struct io_uring_sqe *sqe_ptr;
+	struct io_uring_sqe *sqe_ptr = uring.sqr_entries;
 	const struct io_uring_cqe *cqe_ptr;
 
 	SAFE_SOCKETPAIR(AF_UNIX, SOCK_DGRAM, 0, sockpair);
 	SAFE_SETSOCKOPT_INT(sockpair[0], SOL_SOCKET, SO_SNDBUF,
 		32+sizeof(buf));
 	SAFE_FCNTL(sockpair[0], F_SETFL, O_NONBLOCK);
-
-	SAFE_IO_URING_INIT(512, &params, &uring);
-	sqe_ptr = uring.sqr_entries;
 
 	/* Add spam requests to force async processing of the real test */
 	for (i = 0, tail = *uring.sqr_tail; i < 255; i++, tail++, sqe_ptr++) {
@@ -150,7 +147,7 @@ static void run(void)
 			tst_res(TFAIL | TTERRNO,
 				"Write outside chroot failed unexpectedly");
 		} else {
-			tst_res(TPASS,
+			tst_res(TPASS | TTERRNO,
 				"Write outside chroot failed as expected");
 		}
 	}
@@ -163,10 +160,67 @@ static void run(void)
 	if (count)
 		tst_res(TFAIL, "Wrong number of entries in completion queue");
 
-	/* iteration cleanup */
-	SAFE_IO_URING_CLOSE(&uring);
 	SAFE_CLOSE(sockpair[0]);
 	SAFE_CLOSE(sockpair[1]);
+}
+
+static void check_result(void)
+{
+	const struct io_uring_cqe *cqe_ptr;
+
+	cqe_ptr = uring.cqr_entries + (*uring.cqr_head & *uring.cqr_mask);
+	++*uring.cqr_head;
+	TST_ERR = -cqe_ptr->res;
+
+	if (cqe_ptr->user_data != BEEF_MARK) {
+		tst_res(TFAIL, "Unexpected entry in completion queue");
+		return;
+	}
+
+	if (cqe_ptr->res == -EINVAL) {
+		tst_res(TINFO, "IOSQE_ASYNC is not supported, using fallback");
+		drain_fallback();
+		return;
+	}
+
+	tst_res(TINFO, "IOSQE_ASYNC is supported");
+
+	if (cqe_ptr->res >= 0) {
+		tst_res(TFAIL, "Write outside chroot succeeded.");
+		return;
+	}
+
+	if (cqe_ptr->res != -ENOENT) {
+		tst_res(TFAIL | TTERRNO,
+			"Write outside chroot failed unexpectedly");
+		return;
+	}
+
+	tst_res(TPASS | TTERRNO, "Write outside chroot failed as expected");
+}
+
+static void run(void)
+{
+	uint32_t tail;
+	struct io_uring_sqe *sqe_ptr;
+
+	SAFE_IO_URING_INIT(512, &params, &uring);
+	sqe_ptr = uring.sqr_entries;
+	tail = *uring.sqr_tail;
+
+	memset(sqe_ptr, 0, sizeof(*sqe_ptr));
+	sqe_ptr->opcode = IORING_OP_SENDMSG;
+	sqe_ptr->flags = IOSQE_ASYNC;
+	sqe_ptr->fd = sendsock;
+	sqe_ptr->addr = (__u64)&beef_header;
+	sqe_ptr->user_data = BEEF_MARK;
+	uring.sqr_array[tail & *uring.sqr_mask] = 0;
+	tail++;
+
+	__atomic_store(uring.sqr_tail, &tail, __ATOMIC_RELEASE);
+	SAFE_IO_URING_ENTER(1, uring.fd, 1, 1, IORING_ENTER_GETEVENTS, NULL);
+	check_result();
+	SAFE_IO_URING_CLOSE(&uring);
 }
 
 static void cleanup(void)