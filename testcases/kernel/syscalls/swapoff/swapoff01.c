2fb10db5e9c8ca4643c33bd01d04fff0bb377f43
QI Fuli <qi.fuli@fujitsu.com>
2021-03-27 19:32:25 +0900

    syscalls/swapoff/swapoff01: Convert swapoff01 to the new API
    Signed-off-by: QI Fuli <qi.fuli@fujitsu.com>
    Signed-off-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/swapoff/swapoff01.c
+++ testcases/kernel/syscalls/swapoff/swapoff01.c
@@ -1,100 +1,59 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
  */
 
-/*
+/*\
+ * [Description]
+ *
  * Check that swapoff() succeeds.
  */
 
 #include <unistd.h>
-#include "test.h"
 #include <errno.h>
 #include <stdlib.h>
-#include "config.h"
+
+#include "tst_test.h"
 #include "lapi/syscalls.h"
-#include "libswapon.h"
-
-static void setup(void);
-static void cleanup(void);
-static void verify_swapoff(void);
-
-char *TCID = "swapoff01";
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-		verify_swapoff();
-	}
-
-	cleanup();
-	tst_exit();
-}
+#include "libswap.h"
 
 static void verify_swapoff(void)
 {
-	if (ltp_syscall(__NR_swapon, "./swapfile01", 0) != 0) {
-		tst_resm(TBROK, "Failed to turn on the swap file"
+	if (tst_syscall(__NR_swapon, "./swapfile01", 0) != 0) {
+		tst_res(TFAIL | TERRNO, "Failed to turn on the swap file"
 			 ", skipping test iteration");
 		return;
 	}
 
-	TEST(ltp_syscall(__NR_swapoff, "./swapfile01"));
+	TEST(tst_syscall(__NR_swapoff, "./swapfile01"));
 
-	if (TEST_RETURN == -1) {
-		tst_resm(TFAIL | TTERRNO, "Failed to turn off swapfile,"
-		         " system reboot after execution of LTP "
-			 "test suite is recommended.");
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TTERRNO, "Failed to turn off swapfile,"
+			" system reboot after execution of LTP "
+			"test suite is recommended.");
 	} else {
-		tst_resm(TPASS, "Succeeded to turn off swapfile");
+		tst_res(TPASS, "Succeeded to turn off swapfile");
 	}
 }
 
 static void setup(void)
 {
-	tst_sig(FORK, DEF_HANDLER, cleanup);
+	is_swap_supported("./tstswap");
 
-	tst_require_root();
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	is_swap_supported(cleanup, "./tstswap");
-
-	if (!tst_fs_has_free(NULL, ".", 64, TST_MB)) {
-		tst_brkm(TBROK, cleanup,
-			 "Insufficient disk space to create swap file");
-	}
+	if (!tst_fs_has_free(".", 64, TST_MB))
+		tst_brk(TBROK,
+			"Insufficient disk space to create swap file");
 
 	if (tst_fill_file("swapfile01", 0x00, 1024, 65536))
-		tst_brkm(TBROK, cleanup, "Failed to create file for swap");
+		tst_brk(TBROK, "Failed to create file for swap");
 
 	if (system("mkswap swapfile01 > tmpfile 2>&1") != 0)
-		tst_brkm(TBROK, cleanup, "Failed to make swapfile");
+		tst_brk(TBROK, "Failed to make swapfile");
 }
 
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.needs_root = 1,
+	.needs_tmpdir = 1,
+	.test_all = verify_swapoff,
+	.setup = setup
+};
