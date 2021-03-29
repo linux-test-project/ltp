805ee9f30069d347ecf7aafe07f66ce0ce23f432
Sharan Turlapati <sturlapati@vmware.com>
2021-03-29 11:17:59 -0700

    realtime/scripts: Fix indentation error in parser.py
    An indentation error in parser.py causes certain blocks
    of code to run even before dependent variables are
    initialized when the code in this file is imported
    by parse-testpi1.py or parse-testpi2.py.
    
    Post running tests like testpi-2 or testpi-4, the
    results of these tests are parsed and added to the
    logs/ directory. This bug prevents the results of such
    tests from being parsed and thus masks the results of
    these tests.
    
    Fixes: 5a8408bc3 ("*.py : convert python2 to python3")
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Sharan Turlapati <sturlapati@vmware.com>

--- testcases/realtime/scripts/parser.py
+++ testcases/realtime/scripts/parser.py
@@ -33,15 +33,15 @@ class Log:
 	def __init__(self,filename):
 		if filename:
 			log_file=filename
-	try:
-		self.__log_file = open(log_file, "r")
-	except IOError as errmsg:
-		sys.exit(errmsg)
+		try:
+			self.__log_file = open(log_file, "r")
+		except IOError as errmsg:
+			sys.exit(errmsg)
 
 	def read(self):
 		for line in self.__log_file.read().split("\n"):
 			yield line
-	self.__log_file.close()
+		self.__log_file.close()
 
 	def eval(self):
 		pass
