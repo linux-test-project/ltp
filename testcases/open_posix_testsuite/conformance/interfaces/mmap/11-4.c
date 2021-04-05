552d4f5c1c4c4b0ce5c0d14da2f9cb0bfcc948a3
Bogdan Lezhepekov <bogdan.lezhepekov@suse.com>
2021-04-05 15:23:22 +0300

    openposix/mmap/1-4: correct a typo
    A typo in the name of the temporary file:
         pts_mmap_11_5 -> pts_mmap_11_4
    
    Signed-off-by: Bogdan Lezhepekov <bogdan.lezhepekov@suse.com>
    Acked-by: Li Wang <liwang@redhat.com>

--- testcases/open_posix_testsuite/conformance/interfaces/mmap/11-4.c
+++ testcases/open_posix_testsuite/conformance/interfaces/mmap/11-4.c
@@ -66,7 +66,7 @@ int main(void)
 	/* mmap will create a partial page */
 	len = page_size / 2;
 
-	PTS_GET_TMP_FILENAME(tmpfname, "pts_mmap_11_5");
+	PTS_GET_TMP_FILENAME(tmpfname, "pts_mmap_11_4");
 #ifdef	__linux__
 	struct statfs buf;
 
