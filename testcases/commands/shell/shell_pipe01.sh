cba8a25b6cb92677615cd13875245a5a2fb7385e
Petr Vorel <pvorel@suse.cz>
2021-04-08 19:53:32 +0200

    commands: Add shell pipe test
    This adds basic shell pipe testing, which was removed from splice02
    in 85cebe238 ("splice02: Generate input in C").
    
    Suggested-by: Cyril Hrubis <chrubis@suse.cz>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ testcases/commands/shell/shell_pipe01.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2021 Petr Vorel <pvorel@suse.cz>
+
+TST_TESTFUNC=do_test
+
+. tst_test.sh
+
+do_test()
+{
+	tst_res TINFO "expecting SUCCESS string passed from stdin"
+
+	read line
+	EXPECT_PASS [ "$line" = "SUCCESS" ]
+}
+
+tst_run
