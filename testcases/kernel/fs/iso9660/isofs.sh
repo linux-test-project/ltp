a530e5081987be47e62412dd714aa3359a1d9e9e
zhanglianjie <zhanglianjie@uniontech.com>
2021-04-13 21:34:27 +0200

    isofs.sh: Fallback to genisoimage
    Debian uses genisoimage as mkisofs replacement.
    
    This is needed because although since bullseye (testing) there is
    mkisofs compatibility symlink, older releases does not have it.
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: zhanglianjie <zhanglianjie@uniontech.com>
    [ rewritten commit message ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/fs/iso9660/isofs.sh
+++ testcases/kernel/fs/iso9660/isofs.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (c) International Business Machines Corp., 2003
+# Copyright (c) Linux Test Project, 2016-2021
 # Written by Prakash Narayana (prakashn@us.ibm.com)
 # and Michael Reed (mreed10@us.ibm.com)
-# Copyright (c) Linux Test Project, 2016-2019
 #
 # Test isofs on Linux system.
 # It makes ISO9660 file system with different options and also
 # mounts ISO9660 file system with different mount options.
 
-TST_NEEDS_CMDS="mkisofs"
 TST_NEEDS_TMPDIR=1
+TST_SETUP=setup
 TST_TESTFUNC=do_test
+
 . tst_test.sh
 
 MAX_DEPTH=3
 MAX_DIRS=4
+
+setup()
+{
+	if tst_cmd_available mkisofs; then
+		MKISOFS_CMD="mkisofs"
+	elif tst_cmd_available genisoimage; then
+		MKISOFS_CMD="genisoimage"
+	else
+		tst_brk TCONF "please install mkisofs or genisoimage"
+	fi
+}
 
 gen_fs_tree()
 {
@@ -56,7 +68,7 @@ do_test() {
 		"-allow-lowercase -allow-multidot -iso-level 3 -f -l -D -J -allow-leading-dots -R"
 	do
 		rm -f isofs.iso
-		EXPECT_PASS mkisofs -o isofs.iso -quiet $mkisofs_opt $make_file_sys_dir 2\> /dev/null \
+		EXPECT_PASS $MKISOFS_CMD -o isofs.iso -quiet $mkisofs_opt $make_file_sys_dir 2\> /dev/null \
 			|| continue
 
 		for mount_opt in \
