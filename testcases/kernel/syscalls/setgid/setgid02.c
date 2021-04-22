721720f9dd0f365967964692e4dbff1df7a2c56d
Zhao Gongyi <zhaogongyi@huawei.com>
2021-04-22 11:29:20 +0800

    syscalls/setgid: Update to new API
    For those:
    	testcases/kernel/syscalls/setgid/setgid01.c
    	testcases/kernel/syscalls/setgid/setgid02.c
    	testcases/kernel/syscalls/setgid/setgid03.c
    
    Signed-off-by: Zhao Gongyi <zhaogongyi@huawei.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/setgid/setgid02.c
+++ testcases/kernel/syscalls/setgid/setgid02.c
@@ -1,111 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
  *  Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-/*
- * Testcase to ensure that the setgid() system call sets errno to EPERM
+/*\
+ * [Description]
  *
- * ALGORITHM
- *	Call setgid() to set the gid to that of root. Run this test as
- *	ltpuser1, and expect to get EPERM
+ * Test if setgid() system call sets errno to EPERM correctly.
+ *
+ * [Algorithm]
+ *
+ * Call setgid() to set the gid to that of root. Run this test as
+ * nobody, and expect to get EPERM.
  */
+
 #include <pwd.h>
-#include <errno.h>
+#include "tst_test.h"
+#include "compat_tst_16.h"
 
-#include "test.h"
-#include "compat_16.h"
-
-TCID_DEFINE(setgid02);
-int TST_TOTAL = 1;
-
-static char root[] = "root";
-static char nobody_uid[] = "nobody";
-static char nobody_gid[] = "nobody";
-static struct passwd *ltpuser;
-
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
+static void run(void)
 {
-	struct passwd *getpwnam(), *rootpwent;
-	int lc;
+	struct passwd *rootpwent;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	rootpwent = SAFE_GETPWNAM("root");
 
-	setup();
+	GID16_CHECK(rootpwent->pw_gid, setgid);
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		if ((rootpwent = getpwnam(root)) == NULL) {
-			tst_brkm(TBROK, cleanup, "getpwnam failed for user id "
-				 "%s", root);
-		}
-
-		GID16_CHECK(rootpwent->pw_gid, setgid, cleanup);
-
-		TEST(SETGID(cleanup, rootpwent->pw_gid));
-
-		if (TEST_RETURN != -1) {
-			tst_resm(TFAIL, "call succeeded unexpectedly");
-			continue;
-		}
-
-		if (TEST_ERRNO != EPERM) {
-			tst_resm(TFAIL, "setgid set invalid errno, expected: "
-				 "EPERM, got: %d\n", TEST_ERRNO);
-		} else {
-			tst_resm(TPASS, "setgid returned EPERM");
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_FAIL(SETGID(rootpwent->pw_gid), EPERM);
 }
 
 static void setup(void)
 {
-	tst_require_root();
+	struct passwd *nobody = SAFE_GETPWNAM("nobody");
 
-	/* Switch to nobody user for correct error code collection */
-	ltpuser = getpwnam(nobody_uid);
-	if (ltpuser == NULL)
-		tst_brkm(TBROK, cleanup, "getpwnam failed for user id %s",
-			nobody_uid);
-
-	if (setgid(ltpuser->pw_gid) == -1) {
-		tst_resm(TINFO, "setgid failed to "
-			 "to set the effective gid to %d", ltpuser->pw_gid);
-		perror("setgid");
-	}
-
-	if (setuid(ltpuser->pw_uid) == -1) {
-		tst_resm(TINFO, "setuid failed to "
-			 "to set the effective uid to %d", ltpuser->pw_uid);
-		perror("setuid");
-	}
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
+	SAFE_SETGID(nobody->pw_gid);
+	SAFE_SETUID(nobody->pw_uid);
 }
 
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.needs_root = 1,
+	.setup = setup,
+	.test_all = run,
+};
--- testcases/kernel/syscalls/setgid/setgid03.c
+++ testcases/kernel/syscalls/setgid/setgid03.c
@@ -1,100 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
  *  Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-/*
- * ALGORITHM
- *	As root sets the current group id to ltpuser1, verify the results
+/*\
+ * [Algorithm]
+ *
+ * As a root sets current group id to nobody and expects success.
  */
+
 #include <pwd.h>
-#include <errno.h>
+#include "tst_test.h"
+#include <compat_tst_16.h>
 
-#include "test.h"
-#include <compat_16.h>
+static struct passwd *nobody;
 
-TCID_DEFINE(setgid03);
-int TST_TOTAL = 1;
-
-static char ltpuser1[] = "nobody";
-static char root[] = "root";
-static struct passwd *ltpuser1pwent, *rootpwent;
-static gid_t mygid;
-
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
+static void run(void)
 {
-	int lc;
+	TST_EXP_PASS(SETGID(nobody->pw_gid));
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		TEST(SETGID(cleanup, ltpuser1pwent->pw_gid));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL, "call failed unexpectedly");
-			continue;
-		}
-
-		if (getgid() != ltpuser1pwent->pw_gid) {
-			tst_resm(TFAIL, "setgid failed to set gid to "
-				 "ltpuser1's gid");
-		} else {
-			tst_resm(TPASS, "functionality of getgid() is correct");
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	if (getgid() != nobody->pw_gid)
+		tst_res(TFAIL, "setgid failed to set gid to nobody gid");
+	else
+		tst_res(TPASS, "functionality of getgid() is correct");
 }
 
 static void setup(void)
 {
-	tst_require_root();
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	if ((rootpwent = getpwnam(root)) == NULL) {
-		tst_brkm(TBROK, cleanup, "getpwnam failed for "
-			 "user id %s", root);
-	}
-
-	mygid = getgid();
-
-	if (mygid != rootpwent->pw_gid) {
-		tst_brkm(TBROK, cleanup, "real group id is not root");
-	}
-
-	if ((ltpuser1pwent = getpwnam(ltpuser1)) == NULL) {
-		tst_brkm(TBROK, cleanup, "getpwnam failed for user "
-			 "id %s", ltpuser1);
-	}
-
-	GID16_CHECK(ltpuser1pwent->pw_gid, setgid, cleanup);
+	nobody = SAFE_GETPWNAM("nobody");
+	GID16_CHECK(nobody->pw_gid, setgid);
 }
 
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.needs_root = 1,
+	.setup = setup,
+	.test_all = run,
+};
