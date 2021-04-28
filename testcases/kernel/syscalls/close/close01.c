ecd667899ad1e3d6707d66fb02cbb4811620fe64
Xie Ziyao <xieziyao@huawei.com>
2021-04-28 17:20:06 +0800

    syscalls/close: Delete close08 and convert close01 to the new API
    1. Cleanup and convert close01 to the new API;
    2. Add a testcase for closing a socket fd;
    3. Delete close08 since it does not add any more coverage over close01.
    
    Signed-off-by: Xie Ziyao <xieziyao@huawei.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/close/close01.c
+++ testcases/kernel/syscalls/close/close01.c
@@ -1,124 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
- *  07/2001 Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * 07/2001 Ported by Wayne Boyer
  */
 
-/*
- * DESCRIPTION
- *	Test that closing a regular file and a pipe works correctly
+/*\
+ * [Description]
+ *
+ * Test that closing a file/pipe/socket works correctly.
  */
 
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include "test.h"
-#include "safe_macros.h"
+#include "tst_test.h"
 
-void cleanup(void);
-void setup(void);
+#define FILENAME "close01_testfile"
 
-char *TCID = "close01";
-int TST_TOTAL = 2;
+static int get_fd_file(void)
+{
+	return SAFE_OPEN(FILENAME, O_RDWR | O_CREAT, 0700);
+}
 
-char fname[40] = "";
+static int get_fd_pipe(void)
+{
+	int pipefildes[2];
+	SAFE_PIPE(pipefildes);
+	SAFE_CLOSE(pipefildes[1]);
+	return pipefildes[0];
+}
 
-int fild, newfd, pipefildes[2];
+static int get_fd_socket(void)
+{
+	return SAFE_SOCKET(AF_INET, SOCK_STREAM, 0);
+}
 
 struct test_case_t {
-	int *fd;
+	int (*get_fd) ();
 	char *type;
-} TC[] = {
-	/* file descriptor for a regular file */
-	{
-	&newfd, "file"},
-	    /* file descriptor for a pipe */
-	{
-	&pipefildes[0], "pipe"}
+} tc[] = {
+	{get_fd_file, "file"},
+	{get_fd_pipe, "pipe"},
+	{get_fd_socket, "socket"}
 };
 
-int main(int ac, char **av)
+static void run(unsigned int i)
 {
-
-	int i;
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		if ((fild = creat(fname, 0777)) == -1)
-			tst_brkm(TBROK | TERRNO, cleanup, "can't open file %s",
-				 fname);
-
-		if ((newfd = dup(fild)) == -1)
-			tst_brkm(TBROK | TERRNO, cleanup,
-				 "can't dup the file des");
-
-		SAFE_PIPE(cleanup, pipefildes);
-
-		for (i = 0; i < TST_TOTAL; i++) {
-
-			TEST(close(*TC[i].fd));
-
-			if (TEST_RETURN == -1) {
-				tst_resm(TFAIL, "call failed unexpectedly");
-				continue;
-			}
-
-			if (close(*TC[i].fd) == -1) {
-				tst_resm(TPASS, "%s appears closed",
-					 TC[i].type);
-			} else {
-				tst_resm(TFAIL, "%s close succeeded on"
-					 "second attempt", TC[i].type);
-			}
-		}
-
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_PASS(close(tc[i].get_fd()), "close a %s fd", tc[i].type);
 }
 
-void setup(void)
-{
-	int mypid;
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	umask(0);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	mypid = getpid();
-	sprintf(fname, "fname.%d", mypid);
-}
-
-void cleanup(void)
-{
-	close(fild);
-
-	tst_rmdir();
-
-}
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(tc),
+	.needs_tmpdir = 1,
+	.test = run,
+};
