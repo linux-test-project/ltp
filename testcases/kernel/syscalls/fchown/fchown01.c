477ceed87473a11d9bed7dabd20142c4980fb9a3
Xie Ziyao <xieziyao@huawei.com>
2021-05-06 21:09:48 +0800

    syscalls/fchown: Convert fchown01 to the new API
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Xie Ziyao <xieziyao@huawei.com>

--- testcases/kernel/syscalls/fchown/fchown01.c
+++ testcases/kernel/syscalls/fchown/fchown01.c
@@ -1,100 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
+ * Copyright (c) Linux Test Project, 2006-2021
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
- *    AUTHOR		: William Roske
- *    CO-PILOT		: Dave Fenner
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
+ * Author: William Roske
+ * Ported to LTP: Dave Fenner
  */
-#include <sys/types.h>
-#include <sys/stat.h>
+
+/*\
+ * [Description]
+ *
+ * Basic test for fchown(). Call fchown() on a fd and expect it to pass.
+ */
+
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include "test.h"
-#include "safe_macros.h"
-#include "compat_16.h"
+#include "tst_test.h"
+#include "compat_tst_16.h"
 
-static void setup(void);
-static void cleanup(void);
-
-TCID_DEFINE(fchown01);
-int TST_TOTAL = 1;
+#define FILENAME "fchown01_testfile"
+#define MODE 0700
 
 static int fd;
+static uid_t uid;
+static gid_t gid;
 
-int main(int ac, char **av)
+static void run(void)
 {
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		UID16_CHECK(geteuid(), "fchown", cleanup)
-		GID16_CHECK(getegid(), "fchown", cleanup)
-
-		TEST(FCHOWN(cleanup, fd, geteuid(), getegid()));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO, "fchown failed");
-		} else {
-			tst_resm(TPASS,
-				 "fchown(fd, geteuid(), getegid()) "
-				 "returned %ld", TEST_RETURN);
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_PASS(FCHOWN(fd, uid, gid));
 }
 
 static void setup(void)
 {
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-	fd = SAFE_OPEN(cleanup, "tempfile", O_RDWR | O_CREAT, 0700);
+	UID16_CHECK(uid = geteuid(), "fchown");
+	GID16_CHECK(gid = getegid(), "fchown");
+	fd = SAFE_OPEN(FILENAME, O_RDWR | O_CREAT, MODE);
 }
 
 static void cleanup(void)
 {
-	if (fd > 0 && close(fd))
-		tst_resm(TWARN | TERRNO, "Failed to close fd");
-
-	tst_rmdir();
+	if (fd > 0)
+		SAFE_CLOSE(fd);
 }
+
+static struct tst_test test = {
+	.needs_tmpdir = 1,
+	.setup = setup,
+	.cleanup = cleanup,
+	.test_all = run,
+};
