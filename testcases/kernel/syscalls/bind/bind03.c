b003e0f08fa40a4533b234d311f89e32338088e6
Cyril Hrubis <chrubis@suse.cz>
2021-05-10 16:50:56 +0200

    syscalls/bind03: Bugfix for running with option "-i"
    When run the test with option "-i 2", test will fail and
    report EADDRINUSE.
    
    Signed-off-by: Zhao Gongyi <zhaogongyi@huawei.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/bind/bind03.c
+++ testcases/kernel/syscalls/bind/bind03.c
@@ -21,16 +21,37 @@
 #define SNAME_B "socket.2"
 
 static int sock1, sock2;
+static struct sockaddr_un sun1, sun2;
 
-void run(void)
+static void run(void)
 {
-	struct sockaddr_un sun1;
-	struct sockaddr_un sun2;
+	/*
+	 * Once a STREAM UNIX domain socket has been bound, it can't be
+	 * rebound.
+	 */
+	TST_EXP_FAIL(bind(sock1, (struct sockaddr *)&sun2, sizeof(sun2)),
+	             EINVAL, "re-bind() socket");
 
+	/*
+	 * Since a socket is already bound to the pathname, it can't be bound
+	 * to a second socket. Expected error is EADDRINUSE.
+	 */
+	TST_EXP_FAIL(bind(sock2, (struct sockaddr *)&sun1, sizeof(sun1)),
+	             EADDRINUSE, "bind() with bound pathname");
+
+	/*
+	 * Kernel is buggy since it creates the node in fileystem first, then
+	 * locks the socket and does all the checks and the node is not removed
+	 * in the error path. For now we will unlink the node here so that the
+	 * test works fine when the run() function is executed in a loop.
+	 */
+	unlink(SNAME_B);
+}
+
+static void setup(void)
+{
 	sock1 = SAFE_SOCKET(PF_UNIX, SOCK_STREAM, 0);
-
-	memset(&sun1, 0, sizeof(sun1));
-	memset(&sun2, 0, sizeof(sun2));
+	sock2 = SAFE_SOCKET(PF_UNIX, SOCK_STREAM, 0);
 
 	sun1.sun_family = AF_UNIX;
 	sun2.sun_family = AF_UNIX;
@@ -46,22 +67,6 @@ void run(void)
 	}
 
 	SAFE_BIND(sock1, (struct sockaddr *)&sun1, sizeof(sun1));
-
-	/*
-	 * Once a STREAM UNIX domain socket has been bound, it can't be
-	 * rebound.
-	 */
-	TST_EXP_FAIL(bind(sock1, (struct sockaddr *)&sun2, sizeof(sun2)),
-	             EINVAL, "re-bind() socket");
-
-	sock2 = SAFE_SOCKET(PF_UNIX, SOCK_STREAM, 0);
-
-	/*
-	 * Since a socket is already bound to the pathname, it can't be bound
-	 * to a second socket. Expected error is EADDRINUSE.
-	 */
-	TST_EXP_FAIL(bind(sock2, (struct sockaddr *)&sun1, sizeof(sun1)),
-	             EADDRINUSE, "bind() with bound pathname");
 }
 
 static void cleanup(void)
@@ -71,6 +76,7 @@ static void cleanup(void)
 }
 
 static struct tst_test test = {
+	.setup = setup,
 	.cleanup = cleanup,
 	.test_all = run,
 	.needs_tmpdir = 1,
