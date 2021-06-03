49f8a0812a156abc6b4544774a26a0963c0d260e
Yang Xu <xuyang2018.jy@fujitsu.com>
2021-06-03 17:44:04 +0800

    syscalls/mallinfo2_01: Add test struct mallinfo2 overflow
    when setting 2G size.
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- testcases/kernel/syscalls/mallinfo/mallinfo_common.h
+++ testcases/kernel/syscalls/mallinfo/mallinfo_common.h
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
+ * Copyright (C) 2020 Free Software Foundation, Inc.
  * Copyright (c) 2021 FUJITSU LIMITED. All rights reserved.
  * Author: Yang Xu <xuyang2018.jy@cn.fujitsu.com>
  */
+
 #ifndef MALLINFO_COMMON_H
 #define MALLINFO_COMMON_H
 
@@ -13,7 +15,8 @@
 #ifdef HAVE_MALLINFO
 static inline void print_mallinfo(const char *msg, struct mallinfo *m)
 {
-	tst_res(TINFO, "%s...", msg);
+	tst_res(TINFO, "%s", msg);
+
 #define P(f) tst_res(TINFO, "%s: %d", #f, m->f)
 	P(arena);
 	P(ordblks);
@@ -25,6 +28,25 @@ static inline void print_mallinfo(const char *msg, struct mallinfo *m)
 	P(uordblks);
 	P(fordblks);
 	P(keepcost);
+}
+#endif
+
+#ifdef HAVE_MALLINFO2
+static inline void print_mallinfo2(const char *msg, struct mallinfo2 *m)
+{
+	tst_res(TINFO, "%s", msg);
+
+#define P2(f) tst_res(TINFO, "%s: %ld", #f, m->f)
+	P2(arena);
+	P2(ordblks);
+	P2(smblks);
+	P2(hblks);
+	P2(hblkhd);
+	P2(usmblks);
+	P2(fsmblks);
+	P2(uordblks);
+	P2(fordblks);
+	P2(keepcost);
 }
 #endif
 
---
+++ testcases/kernel/syscalls/mallinfo2/mallinfo2_01.c
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2021 FUJITSU LIMITED. All rights reserved.
+ * Author: Yang Xu <xuyang2018.jy@fujitsu.com>
+ */
+
+/*\
+ * [DESCRIPTION]
+ *
+ * Basic mallinfo2() test.
+ *
+ * Test hblkhd member of struct mallinfo2 whether overflow when setting 2G size.
+ *
+ * Deprecated mallinfo() overflow in this case, that was the point for creating
+ * mallinfo2().
+ */
+
+#include "mallinfo_common.h"
+#include "tst_safe_macros.h"
+
+#ifdef HAVE_MALLINFO2
+
+void test_mallinfo2(void)
+{
+	struct mallinfo2 info;
+	char *buf;
+	size_t size = 2UL * 1024UL * 1024UL * 1024UL;
+
+	buf = malloc(size);
+
+	if (!buf)
+		tst_brk(TCONF, "Current system can not malloc 2G space, skip it");
+
+	info = mallinfo2();
+	if (info.hblkhd < size) {
+		print_mallinfo2("Test malloc 2G", &info);
+		tst_res(TFAIL, "hblkhd member of struct mallinfo2 overflow?");
+	} else {
+		tst_res(TPASS, "hblkhd member of struct mallinfo2 doesn't overflow");
+	}
+
+	free(buf);
+}
+
+static struct tst_test test = {
+	.test_all = test_mallinfo2,
+};
+
+#else
+TST_TEST_TCONF("system doesn't implement non-POSIX mallinfo2()");
+#endif
