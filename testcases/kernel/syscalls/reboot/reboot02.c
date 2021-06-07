4e339079336187a086e09f7204f78285bd96085e
Petr Vorel <pvorel@suse.cz>
2021-06-07 22:45:15 +0200

    reboot02: Improve doc, simplify code
    doc changes:
    * Remove useless [Restrictions] (lib4 and lib5 related doc originated from 2002).
    * Remove useless [Algorithm] list (duplicate with [Description]).
    
    code changes:
    * Use macro.
    * Rename variables.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/reboot/reboot02.c
+++ testcases/kernel/syscalls/reboot/reboot02.c
@@ -1,23 +1,14 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
+ * Copyright (c) Linux Test Project, 2009-2021
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
- * AUTHOR: Aniruddha Marathe <aniruddha.marathe@wipro.com>
+ * Author: Aniruddha Marathe <aniruddha.marathe@wipro.com>
  */
 
 /*\
  * [Description]
- * This test case checks whether reboot(2) system call returns appropriate
- * error number for invalid flag parameter or invalid user.
- *
- * [Algorithm]
- * Execute system call with invaid flag parameter and then for invalid user
- * check return value and errno.
- *
- * [Restrictions]
- * For lib4 and lib5 reboot(2) system call is implemented as
- * int reboot(int magic, int magic2, int flag, void *arg); This test case
- * is written for int reboot(int flag); which is implemented under glibc
- * Therefore this testcase may not work under libc4 and libc5 libraries
+ * Test whether libc wrapper of reboot(2) system call returns appropriate
+ * error number for invalid cmd parameter or invalid user.
  */
 
 #include <unistd.h>
@@ -26,18 +17,19 @@
 #include <pwd.h>
 #include "tst_test.h"
 
-#define INVALID_PARAMETER 100
+#define INVALID_CMD 100
+#define CMD_DESC(x) .cmd = x, .desc = #x
 
 char nobody_uid[] = "nobody";
 struct passwd *ltpuser;
 
 static struct tcase {
-	int flag;
+	int cmd;
+	const char *desc;
 	int exp_errno;
-	const char *option_message;
 } tcases[] = {
-	{INVALID_PARAMETER, EINVAL, "INVALID_PARAMETER"},
-	{LINUX_REBOOT_CMD_CAD_ON, EPERM, "LINUX_REBOOT_CMD_CAD_ON"},
+	{CMD_DESC(INVALID_CMD), EINVAL},
+	{CMD_DESC(LINUX_REBOOT_CMD_CAD_ON), EPERM},
 };
 
 static void run(unsigned int n)
@@ -45,14 +37,14 @@ static void run(unsigned int n)
 	struct tcase *tc = &tcases[n];
 
 	if (n == 0)
-		TST_EXP_FAIL(reboot(tc->flag),
-			tc->exp_errno, "%s", tc->option_message);
+		TST_EXP_FAIL(reboot(tc->cmd),
+			tc->exp_errno, "%s", tc->desc);
 	else {
 		ltpuser = SAFE_GETPWNAM(nobody_uid);
 		SAFE_SETEUID(ltpuser->pw_uid);
 
-		TST_EXP_FAIL(reboot(tc->flag),
-			tc->exp_errno, "%s", tc->option_message);
+		TST_EXP_FAIL(reboot(tc->cmd),
+			tc->exp_errno, "%s", tc->desc);
 
 		SAFE_SETEUID(0);
 	}
