20b7c8b328def390e8bb7f062168584bf979faa5
Petr Vorel <pvorel@suse.cz>
2021-06-07 12:55:35 +0200

    io: Remove stress_floppy
    floppy disc are history, there probably aren't many people using it on
    newer kernel even on VM. Keeping outdated tests does not make much sense.
    
    Link: https://lists.linux.it/pipermail/ltp/2021-June/023060.html
    
    Acked-by: Jan Stancek <jstancek@redhat.com>
    Acked-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testscripts/diskio.sh
+++ testscripts/diskio.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # These tests require the corresponding media to be present:
 #
-#  stress_floppy: Requires a writeable HD floppy disk
-#
 #  stress_cd: Requires ANY data CD with a minimum of 100MB of data.
-#
-# NOTE: The stress_floppy test is easily modifiable for use on any
-#       write device, i.e. tape drive, zip drive....etc
-#
 
 cd `dirname $0`
 
@@ -27,7 +21,7 @@ export PATH="${PATH}:${LTPROOT}/testcases/bin"
 
 ${LTPROOT}/ver_linux
 
-${LTPROOT}/bin/ltp-pan -e -l /tmp/diskiopan.log -S -a ltpdiskio -n ltpdiskio -f ${LTPROOT}/runtest/io_floppy -f ${LTPROOT}/runtest/io_cd
+${LTPROOT}/bin/ltp-pan -e -l /tmp/diskiopan.log -S -a ltpdiskio -n ltpdiskio -f ${LTPROOT}/runtest/io_cd
 
 if [ $? -eq "0" ]; then
   echo ltp-pan reported PASS
