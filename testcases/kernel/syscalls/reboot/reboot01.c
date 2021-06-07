15c30c0dd077f22cdbab4b207bb7281496d58be9
Petr Vorel <pvorel@suse.cz>
2021-06-07 22:26:39 +0200

    reboot01: Improve doc, simplify code
    doc changes:
    * Remove useless [Restrictions] (lib4 and lib5 related doc originated from 2002).
    * Remove useless [Algorithm] list which was also wrongly formatted.
    * Mention tested commands and why just only these two are tested
      (others require reboot which is not yet supported by LTP API).
    
    code changes:
    * Use macro.
    * Rename variables.
    
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/reboot/reboot01.c
+++ testcases/kernel/syscalls/reboot/reboot01.c
@@ -1,26 +1,17 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
+ * Copyright (c) Linux Test Project, 2006-2021
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
- * AUTHOR: Aniruddha Marathe <aniruddha.marathe@wipro.com>
+ * Author: Aniruddha Marathe <aniruddha.marathe@wipro.com>
  */
 
 /*\
  * [Description]
- * This is a Phase I test for the reboot(2) system call.
- * It is intended to provide a limited exposure of the system call.
  *
- * [Algorithm]
- * 1) Two test cases for two flag values
- * 2) Execute system call
- * 3) Check return code, if system call failed (return=-1)
- * 4) Log the errno and Issue a FAIL message
- * 5) Otherwise, Issue a PASS message
+ * Basic test of libc wrapper of reboot(2) system call.
  *
- * [Restrictions]
- * For lib4 and lib5 reboot(2) system call is implemented as
- * int reboot(int magic, int magic2, int flag, void *arg); This test case
- * is written for int reboot(int flag); which is implemented under glibc
- * Therefore this testcase may not work under libc4 and libc5 libraries.
+ * Test LINUX_REBOOT_CMD_CAD_ON, LINUX_REBOOT_CMD_CAD_OFF commands,
+ * which do not perform reboot.
  */
 
 #include <unistd.h>
@@ -28,18 +19,19 @@
 #include <linux/reboot.h>
 #include "tst_test.h"
 
+#define CMD_DESC(x) .cmd = x, .desc = #x
+
 static struct tcase {
-	const char *option_message;
-	int flag;
+	const char *desc;
+	int cmd;
 } tcases[] = {
-	{"LINUX_REBOOT_CMD_CAD_ON", LINUX_REBOOT_CMD_CAD_ON,},
-	{"LINUX_REBOOT_CMD_CAD_OFF", LINUX_REBOOT_CMD_CAD_OFF,},
+	{CMD_DESC(LINUX_REBOOT_CMD_CAD_ON)},
+	{CMD_DESC(LINUX_REBOOT_CMD_CAD_OFF)},
 };
 
 static void run(unsigned int n)
 {
-	TST_EXP_PASS(reboot(tcases[n].flag),
-		"reboot(%s)", tcases[n].option_message);
+	TST_EXP_PASS(reboot(tcases[n].cmd), "reboot(%s)", tcases[n].desc);
 }
 
 static struct tst_test test = {
