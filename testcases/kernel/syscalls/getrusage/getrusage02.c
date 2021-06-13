9c552ccb650cd7d353297071d563525a19f2ff52
Vinay Kumar <vinay.m.engg@gmail.com>
2021-06-13 22:46:42 +0530

    getrusage02: Skipped EFAULT tests for libc variant.
    Tested EFAULT cases only for "__NR_getrusage" syscall.
    
    Tests for bad addresses in LTP cases trigger segment
    fault in libc on a 32bit system.
    
    Signed-off-by: Vinay Kumar <vinay.m.engg@gmail.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/getrusage/getrusage02.c
+++ testcases/kernel/syscalls/getrusage/getrusage02.c
@@ -17,8 +17,19 @@
 #include <sched.h>
 #include <sys/resource.h>
 #include "tst_test.h"
+#include "lapi/syscalls.h"
 
 static struct rusage usage;
+
+static int libc_getrusage(int who, void *usage)
+{
+	return getrusage(who, usage);
+}
+
+static int sys_getrusage(int who, void *usage)
+{
+	return tst_syscall(__NR_getrusage, who, usage);
+}
 
 struct tc_t {
 	int who;
@@ -29,13 +40,41 @@ struct tc_t {
 	{RUSAGE_SELF, (struct rusage *)-1, EFAULT}
 };
 
+static struct test_variants
+{
+	int (*getrusage)(int who, void *usage);
+	char *desc;
+} variants[] = {
+	{ .getrusage = libc_getrusage, .desc = "libc getrusage()"},
+#if (__NR_getrusage != __LTP__NR_INVALID_SYSCALL)
+	{ .getrusage = sys_getrusage,  .desc = "__NR_getrusage syscall"},
+#endif
+};
+
 static void verify_getrusage(unsigned int i)
 {
+	struct test_variants *tv = &variants[tst_variant];
+
+	if (tc[i].exp_errno == EFAULT &&
+	    tv->getrusage == libc_getrusage) {
+		tst_res(TCONF, "EFAULT is skipped for libc variant");
+		return;
+	}
+
 	TST_EXP_FAIL(getrusage(tc[i].who, tc[i].usage), tc[i].exp_errno,
 	             "getrusage(%i, %p)", tc[i].who, tc[i].usage);
 }
 
+static void setup(void)
+{
+	struct test_variants *tv = &variants[tst_variant];
+
+	tst_res(TINFO, "Testing variant: %s", tv->desc);
+}
+
 static struct tst_test test = {
 	.test = verify_getrusage,
+	.setup = setup,
 	.tcnt = ARRAY_SIZE(tc),
+	.test_variants = ARRAY_SIZE(variants),
 };
