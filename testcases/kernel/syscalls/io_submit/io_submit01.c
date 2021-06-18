244091220d7c9f1787e6a7686d9fa775ff47ebf0
Xie Ziyao <xieziyao@huawei.com>
2021-06-18 17:42:10 +0800

    io_submit01: Add docparse formatting and more detailed description
    Add docparse formatting and more detailed description to distinguish it
    from io_submit{02, 03}.
    
    Signed-off-by: Xie Ziyao <xieziyao@huawei.com>
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/io_submit/io_submit01.c
+++ testcases/kernel/syscalls/io_submit/io_submit01.c
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) Crackerjack Project., 2007
+ * Ported from Crackerjack to LTP by Masatake YAMATO <yamato@redhat.com>
  * Copyright (c) 2011-2017 Cyril Hrubis <chrubis@suse.cz>
  */
 
-/* Porting from Crackerjack to LTP is done
-   by Masatake YAMATO <yamato@redhat.com> */
+/*\
+ * [Description]
+ *
+ * Test io_submit() invoked via libaio:
+ *
+ * - io_submit fails and returns -EINVAL if ctx is invalid.
+ * - io_submit fails and returns -EINVAL if nr is invalid.
+ * - io_submit fails and returns -EFAULT if iocbpp pointer is invalid.
+ * - io_submit fails and returns -EBADF if fd is invalid.
+ * - io_submit succeeds and returns the number of iocbs submitted.
+ * - io_submit succeeds and returns 0 if nr is zero.
+ */
 
 #include <errno.h>
 #include <string.h>
