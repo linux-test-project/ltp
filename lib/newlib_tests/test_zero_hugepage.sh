77d7d8d5f3e66c693227c8a1d1555759610b9fd6
Petr Vorel <pvorel@suse.cz>
2021-08-02 17:12:54 +0200

    test/test_zero_hugepage.sh: Skip test on read-only /proc
    or /proc/sys. On GitHub Actions /proc/sys/vm/nr_hugepages is not allowed
    to be changed:
    
    ./test_zero_hugepage.sh: line 9: can't create /proc/sys/vm/nr_hugepages: Read-only file system
    tst_hugepage.c:57: TBROK: Failed to open FILE '/proc/sys/vm/nr_hugepages' for writing: EROFS (30)
    tst_sys_conf.c:102: TWARN: Failed to open FILE '/proc/sys/vm/nr_hugepages'
    
    There are several /proc subdirectories mounted as read only:
    $ grep '^proc /proc.* proc ro' /proc/mounts
    proc /proc/bus proc ro,nosuid,nodev,noexec,relatime 0 0
    proc /proc/fs proc ro,nosuid,nodev,noexec,relatime 0 0
    proc /proc/irq proc ro,nosuid,nodev,noexec,relatime 0 0
    proc /proc/sys proc ro,nosuid,nodev,noexec,relatime 0 0
    proc /proc/sysrq-trigger proc ro,nosuid,nodev,noexec,relatime 0 0
    
    Suggested-by: Cyril Hrubis <chrubis@suse.cz>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/newlib_tests/test_zero_hugepage.sh
+++ lib/newlib_tests/test_zero_hugepage.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (c) 2021 Yang Xu <xuyang2018.jy@fujitsu.com>
+# Copyright (c) 2021 Petr Vorel <pvorel@suse.cz>
 
 echo "Testing .request_hugepages = TST_NO_HUGEPAGES"
 
 orig_value=`cat /proc/sys/vm/nr_hugepages`
+
+if grep -q -E '^proc /proc(/sys)? proc ro' /proc/mounts; then
+	echo "TCONF: /proc or /proc/sys mounted as read-only"
+	exit 32
+fi
 
 echo "128" > /proc/sys/vm/nr_hugepages
 
