ad8524ea700719e2e383796353b88ce63eb2444e
Martin Doucha <mdoucha@suse.cz>
2021-09-15 15:45:02 +0200

    syscalls/setreuid02: Convert to new API
    The original test looks up specific usernames which may not exist on some
    systems. Use any non-root UID instead.
    
    Also add verification that saved UID gets updated according to
    documentation.
    
    Signed-off-by: Martin Doucha <mdoucha@suse.cz>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/setreuid/setreuid02.c
+++ testcases/kernel/syscalls/setreuid/setreuid02.c
@@ -1,145 +1,86 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- *
- * Ported by John George
+ *    Ported by John George
+ * Copyright (C) 2021 SUSE LLC <mdoucha@suse.cz>
  */
 
-/*
+/*\
+ * [Description]
+ *
  * Test setreuid() when executed by root.
  */
 
-#include <errno.h>
+#include <sys/types.h>
 #include <pwd.h>
-#include <stdlib.h>
-#include <string.h>
 
-#include "test.h"
-#include "compat_16.h"
+#include "tst_test.h"
+#include "tst_uid.h"
+#include "compat_tst_16.h"
 
-TCID_DEFINE(setreuid02);
-
-static uid_t neg_one = -1;
-static struct passwd nobody, daemonpw, root, bin;
-
-/*
- * The following structure contains all test data.  Each structure in the array
- * is used for a separate test.  The tests are executed in the for loop below.
- */
+static uid_t root_uid, nobody_uid, other_uid, neg_one = -1;
 
 static struct test_data_t {
 	uid_t *real_uid;
 	uid_t *eff_uid;
-	struct passwd *exp_real_usr;
-	struct passwd *exp_eff_usr;
-	char *test_msg;
+	uid_t *exp_real_uid;
+	uid_t *exp_eff_uid;
+	uid_t *exp_sav_uid;
+	const char *test_msg;
 } test_data[] = {
-	{
-	&neg_one, &neg_one, &root, &root, "After setreuid(-1, -1),"}, {
-	&nobody.pw_uid, &neg_one, &nobody, &root, "After setreuid(nobody, -1)"},
-	{
-	&root.pw_uid, &neg_one, &root, &root, "After setreuid(root,-1),"}, {
-	&neg_one, &daemonpw.pw_uid, &root, &daemonpw,
-		    "After setreuid(-1, daemon)"}, {
-	&neg_one, &root.pw_uid, &root, &root, "After setreuid(-1,root),"}, {
-	&bin.pw_uid, &neg_one, &bin, &root, "After setreuid(bin, -1)"}, {
-&root.pw_uid, &neg_one, &root, &root, "After setreuid(-1, root)"},};
-
-int TST_TOTAL = ARRAY_SIZE(test_data);
-
-static void setup(void);
-static void cleanup(void);
-static void uid_verify(struct passwd *ru, struct passwd *eu, char *when);
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		int i;
-
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++) {
-			/* Set the real or effective user id */
-			TEST(SETREUID(cleanup, *test_data[i].real_uid,
-				      *test_data[i].eff_uid));
-
-			if (TEST_RETURN == -1) {
-				tst_resm(TBROK, "setreuid(%d, %d) failed",
-					 *test_data[i].real_uid,
-					 *test_data[i].eff_uid);
-			} else {
-				uid_verify(test_data[i].exp_real_usr,
-					   test_data[i].exp_eff_usr,
-					   test_data[i].test_msg);
-			}
-		}
-	}
-	cleanup();
-	tst_exit();
-}
+	{&neg_one, &neg_one, &root_uid, &root_uid, &root_uid,
+		"setreuid(-1, -1)"},
+	{&nobody_uid, &neg_one, &nobody_uid, &root_uid, &root_uid,
+		"setreuid(nobody, -1)"},
+	{&root_uid, &neg_one, &root_uid, &root_uid, &root_uid,
+		"setreuid(root, -1)"},
+	{&neg_one, &nobody_uid, &root_uid, &nobody_uid, &nobody_uid,
+		"setreuid(-1, nobody)"},
+	{&neg_one, &root_uid, &root_uid, &root_uid, &nobody_uid,
+		"setreuid(-1, root)"},
+	{&other_uid, &neg_one, &other_uid, &root_uid, &root_uid,
+		"setreuid(other, -1)"},
+	{&root_uid, &neg_one, &root_uid, &root_uid, &root_uid,
+		"setreuid(root, -1)"},
+};
 
 static void setup(void)
 {
-	tst_require_root();
+	uid_t test_users[2];
+	struct passwd *pw;
 
-	tst_sig(FORK, DEF_HANDLER, cleanup);
+	root_uid = getuid();
+	pw = SAFE_GETPWNAM("nobody");
+	nobody_uid = test_users[0] = pw->pw_uid;
+	tst_get_uids(test_users, 1, 2);
+	other_uid = test_users[1];
 
-	if (getpwnam("nobody") == NULL)
-		tst_brkm(TBROK, NULL, "nobody must be a valid user.");
+	UID16_CHECK(root_uid, setreuid);
+	UID16_CHECK(nobody_uid, setreuid);
+	UID16_CHECK(other_uid, setreuid);
 
-	if (getpwnam("daemon") == NULL)
-		tst_brkm(TBROK, NULL, "daemon must be a valid user.");
-
-	if (getpwnam("bin") == NULL)
-		tst_brkm(TBROK, NULL, "bin must be a valid user.");
-
-	root = *(getpwnam("root"));
-	UID16_CHECK(root.pw_uid, setreuid, cleanup);
-
-	nobody = *(getpwnam("nobody"));
-	UID16_CHECK(nobody.pw_uid, setreuid, cleanup);
-
-	daemonpw = *(getpwnam("daemon"));
-	UID16_CHECK(daemonpw.pw_uid, setreuid, cleanup);
-
-	bin = *(getpwnam("bin"));
-	UID16_CHECK(bin.pw_uid, setreuid, cleanup);
-
-	TEST_PAUSE;
+	/* Make sure that saved UID is also set to root */
+	SAFE_SETUID(root_uid);
 }
 
-static void cleanup(void)
+static void run(unsigned int n)
 {
+	const struct test_data_t *tc = test_data + n;
+
+	TST_EXP_PASS_SILENT(SETREUID(*tc->real_uid, *tc->eff_uid), "%s",
+		tc->test_msg);
+
+	if (!TST_PASS)
+		return;
+
+	if (tst_check_resuid(tc->test_msg, *tc->exp_real_uid, *tc->exp_eff_uid,
+		*tc->exp_sav_uid))
+		tst_res(TPASS, "%s works as expected", tc->test_msg);
 }
 
-static void uid_verify(struct passwd *ru, struct passwd *eu, char *when)
-{
-	if ((getuid() != ru->pw_uid) || (geteuid() != eu->pw_uid)) {
-		tst_resm(TFAIL, "ERROR: %s real uid = %d; effective uid = %d",
-			 when, getuid(), geteuid());
-		tst_resm(TFAIL, "Expected: real uid = %d; effective uid = %d",
-			 ru->pw_uid, eu->pw_uid);
-	} else {
-		tst_resm(TPASS, "real or effective uid was modified as "
-			 "expected");
-	}
-}
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(test_data),
+	.setup = setup,
+	.needs_root = 1,
+};
