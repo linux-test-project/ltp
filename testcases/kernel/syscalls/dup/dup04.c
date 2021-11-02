8e9758db00b9464aff9d0935c08448d765dae312
tangmeng <tangmeng@uniontech.com>
2021-11-02 08:38:58 +0800

    dup/dup04: Convert to new API
    Signed-off-by: tangmeng <tangmeng@uniontech.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/dup/dup04.c
+++ testcases/kernel/syscalls/dup/dup04.c
@@ -1,198 +1,62 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
+ * 06/1994 AUTHOR: Richard Logan CO-PILOT: William Roske
  */
-/* $Id: dup04.c,v 1.6 2009/10/13 14:00:46 subrata_modak Exp $ */
-/**********************************************************
+
+/*\
+ * [DESCRIPTION]
  *
- *    OS Test - Silicon Graphics, Inc.
- *
- *    TEST IDENTIFIER	: dup04
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for dup(2) of a system pipe descriptor
- *
- *    PARENT DOCUMENT	: usctpl01
- *
- *    TEST CASE TOTAL	: 2
- *
- *    WALL CLOCK TIME	: 1
- *
- *    CPU TYPES		: ALL
- *
- *    AUTHOR		: Richard Logan
- *
- *    CO-PILOT		: William Roske
- *
- *    DATE STARTED	: 06/94
- *
- *    INITIAL RELEASE	: UNICOS 7.0
- *
- *    TEST CASES
- *
- * 	1.) dup(2) returns...(See Description)
- *
- *    INPUT SPECIFICATIONS
- * 	The standard options for system call tests are accepted.
- *	(See the parse_opts(3) man page).
- *
- *    OUTPUT SPECIFICATIONS
- *$
- *    DURATION
- * 	Terminates - with frequency and infinite modes.
- *
- *    SIGNALS
- * 	Uses SIGUSR1 to pause before test if option set.
- * 	(See the parse_opts(3) man page).
- *
- *    RESOURCES
- * 	None
- *
- *    ENVIRONMENTAL NEEDS
- *      No run-time environmental needs.
- *
- *    SPECIAL PROCEDURAL REQUIREMENTS
- * 	None
- *
- *    INTERCASE DEPENDENCIES
- * 	None
- *
- *    DETAILED DESCRIPTION
- *	This is a Phase I test for the dup(2) system call.  It is intended
- *	to provide a limited exposure of the system call, for now.  It
- *	should/will be extended when full functional tests are written for
- *	dup(2).
- *
- * 	Setup:
- * 	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- * 	Test:
- *	 Loop if the proper options are given.
- * 	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- * 	Cleanup:
- * 	  Print errno log and/or timing stats if options given
- *
- *
- *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#**/
+ * Basic test for dup(2) of a system pipe descriptor.
+ */
 
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
-#include <sys/types.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
-#include <signal.h>
-#include "test.h"
-#include "safe_macros.h"
 
-void setup();
-void cleanup();
-
-char *TCID = "dup04";
-int TST_TOTAL = 2;
+#include "tst_test.h"
 
 int fd[2];
 
-int main(int ac, char **av)
+static void run(void)
 {
-	int lc;
+	TEST(dup(fd[0]));
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	if (TST_RET == -1)
+		tst_res(TFAIL | TTERRNO,
+			 "dup of read side of pipe failed");
+	else {
+		tst_res(TPASS,
+			 "dup(%d) read side of syspipe returned %ld",
+			 fd[0], TST_RET);
 
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(dup(fd[0]));
-
-		if (TEST_RETURN == -1)
-			tst_resm(TFAIL | TTERRNO,
-				 "dup of read side of pipe failed");
-		else {
-			tst_resm(TPASS,
-				 "dup(%d) read side of syspipe returned %ld",
-				 fd[0], TEST_RETURN);
-
-			SAFE_CLOSE(cleanup, TEST_RETURN);
-		}
-
-		TEST(dup(fd[1]));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO,
-				 "dup of write side of pipe failed");
-		} else {
-			tst_resm(TPASS,
-				 "dup(%d) write side of syspipe returned %ld",
-				 fd[1], TEST_RETURN);
-
-
-			SAFE_CLOSE(cleanup, TEST_RETURN);
-		}
-
+		SAFE_CLOSE(TST_RET);
 	}
 
-	cleanup();
-	tst_exit();
+	TEST(dup(fd[1]));
+
+	if (TST_RET == -1) {
+		tst_res(TFAIL | TTERRNO,
+			 "dup of write side of pipe failed");
+	} else {
+		tst_res(TPASS,
+			 "dup(%d) write side of syspipe returned %ld",
+			 fd[1], TST_RET);
+
+		SAFE_CLOSE(TST_RET);
+	}
 }
 
 void setup(void)
 {
 	fd[0] = -1;
 
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	SAFE_PIPE(cleanup, fd);
+	SAFE_PIPE(fd);
 }
 
-void cleanup(void)
-{
-	int i;
-
-	for (i = 0; i <= 6; i++)
-		close(i);
-
-	tst_rmdir();
-}
+static struct tst_test test = {
+        .test_all = run,
+        .setup = setup,
+        .needs_tmpdir = 1,
+};
