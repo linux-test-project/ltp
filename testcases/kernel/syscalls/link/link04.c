8e6b2f8f61821e3366f9269010b24d8cc254cca4
tangmeng <tangmeng@uniontech.com>
2021-11-04 07:26:03 +0800

    syscalls/link: Convert to new API and merge link06 and link07 into link04
    Signed-off-by: tangmeng <tangmeng@uniontech.com>
    Signed-off-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/link/link04.c
+++ testcases/kernel/syscalls/link/link04.c
@@ -1,170 +1,138 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
- *  AUTHOR		: Richard Logan
- *  CO-PILOT		: William Roske
+ * AUTHOR		: Richard Logan
+ * CO-PILOT		: William Roske
  * Copyright (c) 2014 Cyril Hrubis <chrubis@suse.cz>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
  */
 
-/*
+/*\
+ * [Description]
+ *
  * Negative test cases for link(2).
  *
  * This test program should contain test cases where link will fail regardless
  * of who executed it (i.e. joe-user or root)
  */
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
+
+#include <pwd.h>
 #include <sys/param.h>
 #include <sys/mman.h>
-#include "test.h"
-#include "safe_macros.h"
+#include "tst_test.h"
+
+#define NOBODY_USER     99
+#define MODE_TO1 S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IXGRP|S_IXOTH|S_IROTH
+#define MODE_TO2 S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IWGRP|S_IXGRP|S_IXOTH|S_IROTH|S_IWOTH
+#define MODE_TE S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH
+#define MODE_RWX        S_IRWXU | S_IRWXG | S_IRWXO
+#define DIR_TEMP        "dir2/testdir_1"
 
 static char longpath[PATH_MAX + 2];
 
-struct test_case_t {
+static struct tcase {
 	char *file1;
 	char *desc1;
 	char *file2;
 	char *desc2;
 	int exp_errno;
-} test_cases[] = {
+} tcases[] = {
 	/* first path is invalid */
 	{"nonexistfile", "non-existent file", "nefile", "nefile", ENOENT},
 	{"", "path is empty string", "nefile", "nefile", ENOENT},
-	{"neefile/file", "path contains a non-existent file", "nefile",
-	 "nefile", ENOENT},
-	{"regfile/file", "path contains a regular file", "nefile", "nefile",
-	 ENOTDIR},
+	{"neefile/file", "path contains a non-existent file", "nefile", "nefile", ENOENT},
+	{"regfile/file", "path contains a regular file", "nefile", "nefile", ENOTDIR},
 	{longpath, "pathname too long", "nefile", "nefile", ENAMETOOLONG},
 	{NULL, "invalid address", "nefile", "nefile", EFAULT},
+
 	/* second path is invalid */
 	{"regfile", "regfile", "", "empty string", ENOENT},
-	{"regfile", "regfile", "neefile/file",
-		    "path contains a non-existent file", ENOENT},
-	{"regfile", "regfile", "file/file",
-		    "path contains a regular file", ENOENT},
+	{"regfile", "regfile", "neefile/file", "path contains a non-existent file", ENOENT},
+	{"regfile", "regfile", "file/file", "path contains a regular file", ENOENT},
 	{"regfile", "regfile", longpath, "pathname too long", ENAMETOOLONG},
 	{"regfile", "regfile", NULL, "invalid address", EFAULT},
+
 	/* two existing files */
 	{"regfile", "regfile", "regfile2", "regfile2", EEXIST},
+	{"dir1/oldpath", "Write access diretory", "dir1/newpath", "newpath", EACCES},
+	{"dir2/testdir_1/tfile_2", "Search access diretory", "dir2/testdir_1/new_tfile_2",
+						"dir2/testdir_1/new_tfile_2", EACCES},
 };
 
-char *TCID = "link04";
-int TST_TOTAL = ARRAY_SIZE(test_cases);
-
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
+static void verify_link(unsigned int i)
 {
-	int lc;
-	char *fname1, *fname2;
-	char *desc1, *desc2;
-	int i;
+	struct passwd *nobody_pwd;
+	struct tcase *tc = &tcases[i];
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	if (tc->exp_errno == EACCES) {
+		SAFE_SETEUID(0);
 
-	setup();
+		if (strcmp(tc->desc1, "Write access diretory") == 0) {
+			/* Modify mode permissions on test directory */
+			SAFE_CHMOD("dir1", MODE_TO1);
+			SAFE_TOUCH(tc->file1, 0777, NULL);
+		} else if (strcmp(tc->desc1, "Search access diretory") == 0) {
+			/* Modify mode permissions on test directory */
+			SAFE_CHMOD("dir2", MODE_TO2);
+			SAFE_TOUCH(tc->file1, 0666, NULL);
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++) {
-
-			fname1 = test_cases[i].file1;
-			desc1 = test_cases[i].desc1;
-			fname2 = test_cases[i].file2;
-			desc2 = test_cases[i].desc2;
-
-			TEST(link(fname1, fname2));
-
-			if (TEST_RETURN == -1) {
-				if (TEST_ERRNO == test_cases[i].exp_errno) {
-					tst_resm(TPASS | TTERRNO,
-						 "link(<%s>, <%s>)",
-						 desc1, desc2);
-				} else {
-					tst_resm(TFAIL | TTERRNO,
-						 "link(<%s>, <%s>) Failed "
-					         "expected errno: %d",
-						 desc1, desc2,
-						 test_cases[i].exp_errno);
-				}
-			} else {
-				tst_resm(TFAIL,
-					 "link(<%s>, <%s>) returned %ld, "
-				         "expected -1, errno:%d",
-					 desc1, desc2, TEST_RETURN,
-					 test_cases[i].exp_errno);
-			}
+			/* Modify mode permissions on test directory - test conditions */
+			SAFE_CHMOD(DIR_TEMP, MODE_TE);
 		}
 
+		nobody_pwd = SAFE_GETPWNAM("nobody");
+		SAFE_SETEUID(nobody_pwd->pw_uid);
 	}
 
-	cleanup();
-	tst_exit();
+	TEST(link(tc->file1, tc->file2));
+
+	if (TST_RET == -1) {
+		if (TST_ERR == tc->exp_errno) {
+			tst_res(TPASS | TTERRNO,
+				"link(<%s>, <%s>)",
+				tc->desc1, tc->desc2);
+		} else {
+			tst_res(TFAIL | TTERRNO,
+				"link(<%s>, <%s>) Failed "
+				"expected errno: %d",
+				tc->desc1, tc->desc2,
+				tc->exp_errno);
+		}
+	} else {
+		tst_res(TFAIL,
+			 "link(<%s>, <%s>) returned %ld, "
+			"expected -1, errno:%d",
+			tc->desc1, tc->desc2, TST_RET,
+			tc->exp_errno);
+	}
 }
 
 static void setup(void)
 {
-	int n;
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
+	unsigned int i;
 	memset(longpath, 'a', PATH_MAX+1);
-	SAFE_TOUCH(cleanup, "regfile", 0777, NULL);
-	SAFE_TOUCH(cleanup, "regfile2", 0777, NULL);
-	SAFE_MKDIR(cleanup, "dir", 0777);
 
-	void *bad_addr = tst_get_bad_addr(cleanup);
+	SAFE_TOUCH("regfile", 0777, NULL);
+	SAFE_TOUCH("regfile2", 0777, NULL);
+	SAFE_MKDIR("dir", 0777);
+	SAFE_MKDIR("dir1", MODE_RWX);
+	SAFE_MKDIR("dir2", MODE_RWX);
+	SAFE_MKDIR(DIR_TEMP, MODE_RWX);
 
-	for (n = 0; n < TST_TOTAL; n++) {
-		if (!test_cases[n].file1)
-			test_cases[n].file1 = bad_addr;
+	for (i = 0; i < ARRAY_SIZE(tcases); i++) {
+		struct tcase *tc = &tcases[i];
 
-		if (!test_cases[n].file2)
-			test_cases[n].file2 = bad_addr;
+		if (!tc->file1)
+			tc->file1 = tst_get_bad_addr(NULL);
+
+		if (!tc->file2)
+			tc->file2 = tst_get_bad_addr(NULL);
 	}
 }
 
-static void cleanup(void)
-{
-	tst_rmdir();
-}
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(tcases),
+	.test = verify_link,
+	.setup = setup,
+	.needs_tmpdir = 1,
+	.needs_root = 1,
+};
