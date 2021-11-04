c093957127e6b68dccc0ad7cba38cfa0a2fed120
Petr Vorel <pvorel@suse.cz>
2021-11-04 17:38:43 +0100

    test_zero_hugepage.sh: Detect errors write to /proc/sys/vm/nr_hugepages
    Fix running make test-c as non-root.
    
    Fixes: 057d6b046 ("lib/tst_hugepage: Fix .request_hugepages = 0 bug")
    
    Reviewed-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/newlib_tests/test_zero_hugepage.sh
+++ lib/newlib_tests/test_zero_hugepage.sh
@@ -3,13 +3,26 @@
 # Copyright (c) 2021 Yang Xu <xuyang2018.jy@fujitsu.com>
 # Copyright (c) 2021 Petr Vorel <pvorel@suse.cz>
 
+tconf()
+{
+	echo "TCONF: $1"
+	exit 32
+}
+
 echo "Testing .request_hugepages = TST_NO_HUGEPAGES"
 
 orig_value=`cat /proc/sys/vm/nr_hugepages`
 
 if grep -q -E '^proc /proc(/sys)? proc ro' /proc/mounts; then
-	echo "TCONF: /proc or /proc/sys mounted as read-only"
-	exit 32
+	tconf "/proc or /proc/sys mounted as read-only"
+fi
+
+if [ ! -f /proc/sys/vm/nr_hugepages ]; then
+	tconf "/proc/sys/vm/nr_hugepages does not exist"
+fi
+
+if [ ! -w /proc/sys/vm/nr_hugepages ]; then
+	tconf "no write permission to /proc/sys/vm/nr_hugepages (run as root)"
 fi
 
 echo "128" > /proc/sys/vm/nr_hugepages
