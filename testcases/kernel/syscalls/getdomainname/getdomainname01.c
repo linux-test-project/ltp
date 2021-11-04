a16b4c7f83231b973bbfb22bd0c4c23901580189
tangmeng <tangmeng@uniontech.com>
2021-11-04 08:20:06 +0800

    getdomainname01: Convert to new API
    Signed-off-by: tangmeng <tangmeng@uniontech.com>
    Signed-off-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/getdomainname/getdomainname01.c
+++ testcases/kernel/syscalls/getdomainname/getdomainname01.c
@@ -1,132 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ *   AUTHOR: Saji Kumar.V.R <saji.kumar@wipro.com>
  */
-/**********************************************************
- *
- *    TEST IDENTIFIER	: getdomainname01
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for getdomainname(2)
- *
- *    TEST CASE TOTAL	: 1
- *
- *    AUTHOR		: Saji Kumar.V.R <saji.kumar@wipro.com>
- *
- *    SIGNALS
- *	Uses SIGUSR1 to pause before test if option set.
- *	(See the parse_opts(3) man page).
- *
- *    DESCRIPTION
- *	This is a Phase I test for the getdomainname(2) system call.
- *	It is intended to provide a limited exposure of the system call.
- *
- *	Setup:
- *	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- *	Test:
- *	 Loop if the proper options are given.
- *	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- *	Cleanup:
- *	  Print errno log and/or timing stats if options given
- *
- * USAGE:  <for command-line>
- *  getdomainname01  [-c n] [-e] [-i n] [-I x] [-P x] [-t] [-h] [-f] [-p]
- *			where,  -c n : Run n copies concurrently.
- *				-e   : Turn on errno logging.
- *				-h   : Show help screen
- *				-f   : Turn off functional testing
- *				-i n : Execute test n times.
- *				-I x : Execute test for x seconds.
- *				-p   : Pause for SIGUSR1 before starting
- *				-P x : Pause for x seconds between iterations.
- *				-t   : Turn on syscall timing.
- *
- ****************************************************************/
 
-#include <errno.h>
+/*\
+ * [Description]
+ *
+ * Basic test for getdomainname(2)
+ *
+ * This is a Phase I test for the getdomainname(2) system call.
+ * It is intended to provide a limited exposure of the system call.
+ */
+
 #include <linux/utsname.h>
-#include "test.h"
+#include "tst_test.h"
 
 #define MAX_NAME_LEN __NEW_UTS_LEN
 
-static void setup();
-static void cleanup();
-
-char *TCID = "getdomainname01";
-int TST_TOTAL = 1;
-
-static char domain_name[MAX_NAME_LEN];
-
-int main(int ac, char **av)
+static void verify_getdomainname(void)
 {
+	char domain_name[MAX_NAME_LEN];
 
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		/*
-		 * Call getdomainname(2)
-		 */
-		TEST(getdomainname(domain_name, sizeof(domain_name)));
-
-		/* check return code */
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL, "getdomainname() Failed, errno = %d :"
-				 " %s", TEST_ERRNO, strerror(TEST_ERRNO));
-		} else {
-			tst_resm(TPASS, "getdomainname() returned %ld ",
-				 TEST_RETURN);
-		}
-
-	}
-
-	/* cleanup and exit */
-	cleanup();
-
-	tst_exit();
-
+	TST_EXP_PASS(getdomainname(domain_name, sizeof(domain_name)));
 }
 
-/* setup() - performs all ONE TIME setup for this test */
-void setup(void)
-{
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-}
-
-/*
- *cleanup() -  performs all ONE TIME cleanup for this test at
- *		completion or premature exit.
- */
-void cleanup(void)
-{
-
-}
+static struct tst_test test = {
+        .test_all = verify_getdomainname,
+};
