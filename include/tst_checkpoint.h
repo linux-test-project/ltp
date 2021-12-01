891c6907066e0e0063d1b4a457d71fdabffc820a
Cyril Hrubis <chrubis@suse.cz>
2021-12-01 12:13:55 +0100

    checkpoints: Fix WAIT/WAKE macros
    There shouldn't be semicolons there in the first place and
    the WAKE_AND_WAIT() should have been wrapped in do { } while (0).
    
    This fixes the usage of the macros in if () conditions with a single
    line as:
    
    	if (foo)
    		TST_CHECKPOINT_WAKE_AND_WAIT(0);
    
    Signed-off-by: Cyril Hrubis <chrubis@suse.cz>

--- include/tst_checkpoint.h
+++ include/tst_checkpoint.h
@@ -8,20 +8,21 @@
 #include "tst_checkpoint_fn.h"
 
 #define TST_CHECKPOINT_WAIT(id) \
-        tst_safe_checkpoint_wait(__FILE__, __LINE__, NULL, id, 0);
+        tst_safe_checkpoint_wait(__FILE__, __LINE__, NULL, id, 0)
 
 #define TST_CHECKPOINT_WAIT2(id, msec_timeout) \
-        tst_safe_checkpoint_wait(__FILE__, __LINE__, NULL, id, msec_timeout);
+        tst_safe_checkpoint_wait(__FILE__, __LINE__, NULL, id, msec_timeout)
 
 #define TST_CHECKPOINT_WAKE(id) \
-        tst_safe_checkpoint_wake(__FILE__, __LINE__, NULL, id, 1);
+        tst_safe_checkpoint_wake(__FILE__, __LINE__, NULL, id, 1)
 
 #define TST_CHECKPOINT_WAKE2(id, nr_wake) \
-        tst_safe_checkpoint_wake(__FILE__, __LINE__, NULL, id, nr_wake);
+        tst_safe_checkpoint_wake(__FILE__, __LINE__, NULL, id, nr_wake)
 
-#define TST_CHECKPOINT_WAKE_AND_WAIT(id) \
+#define TST_CHECKPOINT_WAKE_AND_WAIT(id) do { \
         tst_safe_checkpoint_wake(__FILE__, __LINE__, NULL, id, 1); \
-        tst_safe_checkpoint_wait(__FILE__, __LINE__, NULL, id, 0);
+        tst_safe_checkpoint_wait(__FILE__, __LINE__, NULL, id, 0); \
+} while (0)
 
 extern const char *tst_ipc_path;
 
