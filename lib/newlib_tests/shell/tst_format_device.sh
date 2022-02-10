5eff26c9020da4e89a8512d1c02cc1c4ec46d8ab
Petr Vorel <pvorel@suse.cz>
2022-02-10 17:27:38 +0100

    shell: Add test for TST_FORMAT_DEVICE=1
    Unfortunately GitHub Actions don't have loop devices, thus cannot be run
    in CI:
    
    tst_format_device 1 TINFO: timeout per run is 0h 5m 0s
    /__w/ltp/ltp/lib/tst_device.c:139: TINFO: No free devices found
    
    Acked-by: Yang Xu <xuyang2018.jy@fujitsu.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ lib/newlib_tests/shell/tst_format_device.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2022 Petr Vorel <pvorel@suse.cz>
+
+TST_FORMAT_DEVICE=1
+TST_TESTFUNC=test
+TST_CNT=2
+TST_DEV_FS_OPTS="-b 1024 -O quota"
+TST_DEV_EXTRA_OPTS="5m"
+TST_NEEDS_CMDS="df"
+. tst_test.sh
+
+test1()
+{
+	tst_res TPASS "device formatted"
+}
+
+test2()
+{
+	tst_check_cmds df || return
+	EXPECT_PASS "df $TST_DEVICE | grep -q /dev"
+}
+
+tst_run
