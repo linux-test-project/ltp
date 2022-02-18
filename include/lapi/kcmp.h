03136cd662b5e9f1568f82071472648366412dd7
Yang Xu <xuyang2018.jy@fujitsu.com>
2022-02-18 10:22:54 +0800

    lapi/kcmp.h: Simplify code
    1) Replace GPL with SPDX-License-Identifier
    2) clean up of guards and #ifnde
    3) make check happy
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- include/lapi/kcmp.h
+++ include/lapi/kcmp.h
@@ -1,29 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2015 Cedric Hnyda <chnyda@suse.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write the Free Software Foundation,
- * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef KCMP_H
-#define KCMP_H
+#ifndef LAPI_KCMP_H__
+#define LAPI_KCMP_H__
 
 #include <sys/types.h>
 #include "config.h"
 #include "lapi/syscalls.h"
 
-#if !defined(HAVE_ENUM_KCMP_TYPE)
+#ifndef HAVE_ENUM_KCMP_TYPE
 
 enum kcmp_type {
 	KCMP_FILE,
@@ -42,13 +29,13 @@ enum kcmp_type {
 
 #endif
 
-#if !defined(HAVE_KCMP)
+#ifndef HAVE_KCMP
 
-int kcmp(int pid1, int pid2, int type, int fd1, int fd2)
+static inline int kcmp(int pid1, int pid2, int type, int fd1, int fd2)
 {
 	return tst_syscall(__NR_kcmp, pid1, pid2, type, fd1, fd2);
 }
 
 #endif
 
-#endif /* KCMP_H */
+#endif /* LAPI_KCMP_H__ */
