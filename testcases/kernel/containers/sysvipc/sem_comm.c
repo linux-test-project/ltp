43eeadd542779fa0678839ae8efc4d20f8bd8984
Andrea Cervesato <andrea.cervesato@suse.de>
2022-03-25 13:54:38 +0100

    Rewrite sem_comm.c using new LTP API
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.de>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/containers/sysvipc/sem_comm.c
+++ testcases/kernel/containers/sysvipc/sem_comm.c
@@ -1,20 +1,16 @@
-/* Copyright (c) 2014 Red Hat, Inc.
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2014 Red Hat, Inc.
+ * Copyright (C) 2022 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+
+/*\
+ * [Description]
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of version 2 the GNU General Public License as
- * published by the Free Software Foundation.
+ * Test SysV IPC semaphore usage between cloned processes.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * [Algorithm]
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- ***********************************************************************
- * File: sem_comm.c
- *
- * Description:
  * 1. Clones two child processes with CLONE_NEWIPC flag, each child
  *    creates System V semaphore (sem) with the _identical_ key.
  * 2. Child1 locks the semaphore.
@@ -26,166 +22,95 @@
  */
 
 #define _GNU_SOURCE
-#include <sys/ipc.h>
-#include <sys/types.h>
+
 #include <sys/wait.h>
-#include <stdio.h>
-#include <errno.h>
-#include "ipcns_helper.h"
-#include "test.h"
-#include "safe_macros.h"
+#include <sys/msg.h>
+#include <sys/types.h>
+#include "tst_safe_sysv_ipc.h"
+#include "tst_test.h"
 #include "lapi/sem.h"
+#include "common.h"
 
 #define TESTKEY 124426L
-char *TCID	= "sem_comm";
-int TST_TOTAL	= 1;
 
-static void cleanup(void)
+static int chld1_sem(LTP_ATTRIBUTE_UNUSED void *arg)
 {
-	tst_rmdir();
+	int id;
+	struct sembuf sm = {
+		.sem_num = 0,
+		.sem_op = -1,
+		.sem_flg = IPC_NOWAIT,
+	};
+	union semun su = {
+		.val = 1,
+	};
+
+	id = SAFE_SEMGET(TESTKEY, 1, IPC_CREAT);
+
+	SAFE_SEMCTL(id, 0, SETVAL, su);
+
+	TST_CHECKPOINT_WAKE_AND_WAIT(0);
+
+	SAFE_SEMOP(id, &sm, 1);
+
+	TST_CHECKPOINT_WAKE_AND_WAIT(0);
+
+	SAFE_SEMCTL(id, 0, IPC_RMID);
+
+	return 0;
+}
+
+static int chld2_sem(LTP_ATTRIBUTE_UNUSED void *arg)
+{
+	int id;
+	struct sembuf sm = {
+		.sem_num = 0,
+		.sem_op = -1,
+		.sem_flg = IPC_NOWAIT,
+	};
+	union semun su = {
+		.val = 1,
+	};
+
+	TST_CHECKPOINT_WAIT(0);
+
+	id = SAFE_SEMGET(TESTKEY, 1, IPC_CREAT);
+
+	SAFE_SEMCTL(id, 0, SETVAL, su);
+
+	TST_CHECKPOINT_WAKE_AND_WAIT(0);
+
+	TEST(semop(id, &sm, 1));
+	if (TST_RET < 0) {
+		if (TST_ERR != EAGAIN)
+			tst_brk(TBROK | TERRNO, "semop error");
+
+		tst_res(TFAIL, "semaphore decremented from different namespace");
+	} else {
+		tst_res(TPASS, "semaphore has not been decremented");
+	}
+
+	TST_CHECKPOINT_WAKE(0);
+
+	SAFE_SEMCTL(id, 0, IPC_RMID);
+
+	return 0;
+}
+
+static void run(void)
+{
+	clone_unshare_test(T_CLONE, CLONE_NEWIPC, chld1_sem, NULL);
+	clone_unshare_test(T_CLONE, CLONE_NEWIPC, chld2_sem, NULL);
 }
 
 static void setup(void)
 {
-	tst_require_root();
 	check_newipc();
-	tst_tmpdir();
-	TST_CHECKPOINT_INIT(tst_rmdir);
 }
 
-int chld1_sem(void *arg)
-{
-	int id;
-	union semun su;
-	struct sembuf sm;
-
-	id = semget(TESTKEY, 1, IPC_CREAT);
-	if (id == -1) {
-		perror("semget");
-		return 2;
-	}
-
-	su.val = 1;
-	if (semctl(id, 0, SETVAL, su) == -1) {
-		perror("semctl");
-		semctl(id, 0, IPC_RMID);
-		return 2;
-	}
-
-	/* tell child2 to continue and wait for it to create the semaphore */
-	TST_SAFE_CHECKPOINT_WAKE_AND_WAIT(NULL, 0);
-
-	sm.sem_num = 0;
-	sm.sem_op = -1;
-	sm.sem_flg = IPC_NOWAIT;
-	if (semop(id, &sm, 1) == -1) {
-		perror("semop");
-		semctl(id, 0, IPC_RMID);
-		return 2;
-	}
-
-	/* tell child2 to continue and wait for it to lock the semaphore */
-	TST_SAFE_CHECKPOINT_WAKE_AND_WAIT(NULL, 0);
-
-	sm.sem_op = 1;
-	semop(id, &sm, 1);
-
-	semctl(id, 0, IPC_RMID);
-	return 0;
-}
-
-int chld2_sem(void *arg)
-{
-	int id, rval = 0;
-	struct sembuf sm;
-	union semun su;
-
-	/* wait for child1 to create the semaphore */
-	TST_SAFE_CHECKPOINT_WAIT(NULL, 0);
-
-	id = semget(TESTKEY, 1, IPC_CREAT);
-	if (id == -1) {
-		perror("semget");
-		return 2;
-	}
-
-	su.val = 1;
-	if (semctl(id, 0, SETVAL, su) == -1) {
-		perror("semctl");
-		semctl(id, 0, IPC_RMID);
-		return 2;
-	}
-
-	/* tell child1 to continue and wait for it to lock the semaphore */
-	TST_SAFE_CHECKPOINT_WAKE_AND_WAIT(NULL, 0);
-
-	sm.sem_num = 0;
-	sm.sem_op = -1;
-	sm.sem_flg = IPC_NOWAIT;
-	if (semop(id, &sm, 1) == -1) {
-		if (errno == EAGAIN) {
-			rval = 1;
-		} else {
-			perror("semop");
-			semctl(id, 0, IPC_RMID);
-			return 2;
-		}
-	}
-
-	/* tell child1 to continue */
-	TST_SAFE_CHECKPOINT_WAKE(NULL, 0);
-
-	sm.sem_op = 1;
-	semop(id, &sm, 1);
-
-	semctl(id, 0, IPC_RMID);
-	return rval;
-}
-
-static void test(void)
-{
-	int status, ret = 0;
-
-	ret = do_clone_unshare_test(T_CLONE, CLONE_NEWIPC, chld1_sem, NULL);
-	if (ret == -1)
-		tst_brkm(TBROK | TERRNO, cleanup, "clone failed");
-
-	ret = do_clone_unshare_test(T_CLONE, CLONE_NEWIPC, chld2_sem, NULL);
-	if (ret == -1)
-		tst_brkm(TBROK | TERRNO, cleanup, "clone failed");
-
-
-	while (wait(&status) > 0) {
-		if (WIFEXITED(status) && WEXITSTATUS(status) == 1)
-			ret = 1;
-		if (WIFEXITED(status) && WEXITSTATUS(status) == 2)
-			tst_brkm(TBROK | TERRNO, cleanup, "error in child");
-		if (WIFSIGNALED(status)) {
-			tst_resm(TFAIL, "child was killed with signal %s",
-					tst_strsig(WTERMSIG(status)));
-			return;
-		}
-	}
-
-	if (ret)
-		tst_resm(TFAIL, "SysV sem: communication with identical keys"
-				" between namespaces");
-	else
-		tst_resm(TPASS, "SysV sem: communication with identical keys"
-				" between namespaces");
-}
-
-int main(int argc, char *argv[])
-{
-	int lc;
-
-	tst_parse_opts(argc, argv, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++)
-		test();
-
-	cleanup();
-	tst_exit();
-}
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.needs_root = 1,
+	.needs_checkpoints = 1,
+};
