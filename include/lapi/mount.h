7c373cd3357e64a5dd7fa1490e24c9f734f94e46
Petr Vorel <pvorel@suse.cz>
2022-04-22 15:38:47 +0200

    lapi/mount.h: Fix indent
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- include/lapi/mount.h
+++ include/lapi/mount.h
@@ -14,27 +14,27 @@
 #endif
 
 #ifndef MS_REC
-#define MS_REC 16384
+# define MS_REC 16384
 #endif
 
 #ifndef MS_PRIVATE
-#define MS_PRIVATE (1<<18)
+# define MS_PRIVATE (1<<18)
 #endif
 
 #ifndef MS_STRICTATIME
-#define MS_STRICTATIME (1 << 24)
+# define MS_STRICTATIME (1 << 24)
 #endif
 
 #ifndef MNT_DETACH
-#define MNT_DETACH 2
+# define MNT_DETACH 2
 #endif
 
 #ifndef MNT_EXPIRE
-#define MNT_EXPIRE 4
+# define MNT_EXPIRE 4
 #endif
 
 #ifndef UMOUNT_NOFOLLOW
-#define UMOUNT_NOFOLLOW 8
+# define UMOUNT_NOFOLLOW 8
 #endif
 
 #endif /* LAPI_MOUNT_H__ */
