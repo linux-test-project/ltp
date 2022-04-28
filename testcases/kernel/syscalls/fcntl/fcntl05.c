05709644697d9c6975967c0431d355198e4b4879
Yang Xu <xuyang2018.jy@fujitsu.com>
2022-04-28 10:51:52 +0800

    syscalls/fcntl05: Convert into new api
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- testcases/kernel/syscalls/fcntl/fcntl05.c
+++ testcases/kernel/syscalls/fcntl/fcntl05.c
@@ -1,25 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * Copyright (c) 2000 Silicon Graphics, Inc. All Rights Reserved.
  * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
  * Mountain View, CA  94043, or:
  *
@@ -29,158 +10,63 @@
  *
  * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
  *
+ * AUTHOR            : William Roske
+ * CO-PILOT          : Dave Fenner
  */
-/* $Id: fcntl05.c,v 1.8 2009/11/02 13:57:16 subrata_modak Exp $ */
-/**********************************************************
- *
- *    OS Test - Silicon Graphics, Inc.
- *
- *    TEST IDENTIFIER	: fcntl05
- *
- *    EXECUTED BY	: anyone
- *
- *    TEST TITLE	: Basic test for fcntl(2) using F_GETLK argument.
- *
- *    PARENT DOCUMENT	: usctpl01
- *
- *    TEST CASE TOTAL	: 1
- *
- *    WALL CLOCK TIME	: 1
- *
- *    CPU TYPES		: ALL
- *
- *    AUTHOR		: William Roske
- *
- *    CO-PILOT		: Dave Fenner
- *
- *    DATE STARTED	: 03/30/92
- *
- *    INITIAL RELEASE	: UNICOS 7.0
- *
- *    TEST CASES
- *
- *	1.) fcntl(2) returns...(See Description)
- *
- *    INPUT SPECIFICATIONS
- *	The standard options for system call tests are accepted.
- *	(See the parse_opts(3) man page).
- *
- *    OUTPUT SPECIFICATIONS
- *
- *    DURATION
- *	Terminates - with frequency and infinite modes.
- *
- *    SIGNALS
- *	Uses SIGUSR1 to pause before test if option set.
- *	(See the parse_opts(3) man page).
- *
- *    RESOURCES
- *	None
- *
- *    ENVIRONMENTAL NEEDS
- *      No run-time environmental needs.
- *
- *    SPECIAL PROCEDURAL REQUIREMENTS
- *	None
- *
- *    INTERCASE DEPENDENCIES
- *	None
- *
- *    DETAILED DESCRIPTION
- *	This is a Phase I test for the fcntl(2) system call.  It is intended
- *	to provide a limited exposure of the system call, for now.  It
- *	should/will be extended when full functional tests are written for
- *	fcntl(2).
- *
- *	Setup:
- *	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- *	Test:
- *	 Loop if the proper options are given.
- *	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- *	Cleanup:
- *	  Print errno log and/or timing stats if options given
- *
- *
- *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#**/
 
+/*\
+ * [Description]
+ *
+ * Basic test for fcntl(2) using F_GETLK argument.
+ *
+ * If the lock could be placed, fcntl() does not actually place it, but
+ * returns F_UNLCK in the l_type field of lock and leaves the other field
+ * of the structure unchanged.
+ */
+
+#include <stdio.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include "test.h"
+#include "tst_test.h"
 
-void setup();
-void cleanup();
+static int fd = -1, pid;
+static struct flock flocks;
 
-char *TCID = "fcntl05";
-int TST_TOTAL = 1;
-
-char fname[255];
-int fd;
-struct flock flocks;
-
-int main(int ac, char **av)
+static void verify_fcntl(void)
 {
-	int lc;
+	/* F_GETLK will change flock.l_type to F_UNLCK, so need to reset */
+	flocks.l_type = F_RDLCK;
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		flocks.l_type = F_RDLCK;
-		TEST(fcntl(fd, F_GETLK, &flocks));
-
-		if (TEST_RETURN == -1)
-			tst_resm(TFAIL | TTERRNO, "fcntl failed");
-		else {
-			tst_resm(TPASS, "fcntl returned %ld",
-				 TEST_RETURN);
-		}
-
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_PASS(fcntl(fd, F_GETLK, &flocks), "fcntl(%d, F_GETLK, &flocks)", fd);
+	TST_EXP_EQ_LI(flocks.l_type, F_UNLCK);
+	TST_EXP_EQ_LI(flocks.l_whence, SEEK_CUR);
+	TST_EXP_EQ_LI(flocks.l_start, 0);
+	TST_EXP_EQ_LI(flocks.l_len, 0);
+	TST_EXP_EQ_LI(flocks.l_pid, pid);
 }
 
-void setup(void)
+static void setup(void)
 {
+	pid = getpid();
+	fd = SAFE_OPEN("filename", O_RDWR | O_CREAT, 0700);
 
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	sprintf(fname, "tfile_%d", getpid());
-	if ((fd = open(fname, O_RDWR | O_CREAT, 0700)) == -1)
-		tst_brkm(TBROK | TERRNO, cleanup, "open failed");
-
-	/* set needed flags in the flocks structure */
-	flocks.l_whence = 1;
+	flocks.l_whence = SEEK_CUR;
 	flocks.l_start = 0;
 	flocks.l_len = 0;
-	flocks.l_pid = getpid();
+	flocks.l_pid = pid;
 }
 
-void cleanup(void)
+static void cleanup(void)
 {
-	if (close(fd) == -1)
-		tst_resm(TWARN | TERRNO, "close failed");
-
-	tst_rmdir();
-
+	if (fd > -1)
+		SAFE_CLOSE(fd);
 }
+
+static struct tst_test test = {
+	.needs_tmpdir = 1,
+	.test_all = verify_fcntl,
+	.setup = setup,
+	.cleanup = cleanup,
+};
