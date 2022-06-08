e98f9465925434349d0aa6eb71e81b5464e76df4
Zhao Gongyi via ltp <ltp@lists.linux.it>
2022-06-08 11:19:53 +0800

    futex_waitv01: Add test verifies EAGIN/ETIMEDOUT
    Add test verifies EAGIN/ETIMEDOUT for futex_waitv according to
    https://www.kernel.org/doc/html/latest/userspace-api/futex2.html.
    
    Signed-off-by: Zhao Gongyi <zhaogongyi@huawei.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/futex/futex_waitv01.c
+++ testcases/kernel/syscalls/futex/futex_waitv01.c
@@ -107,10 +107,39 @@ static void test_invalid_nr_futexes(void)
 	init_timeout(&to);
 
 	/* Valid nr_futexes is [1, 128] */
-	TST_EXP_FAIL(futex_waitv(waitv, 129, 0, &to, CLOCK_REALTIME), EINVAL,
+	TST_EXP_FAIL(futex_waitv(waitv, 129, 0, &to, CLOCK_MONOTONIC), EINVAL,
 		     "futex_waitv invalid nr_futexes");
-	TST_EXP_FAIL(futex_waitv(waitv, 0, 0, &to, CLOCK_REALTIME), EINVAL,
+	TST_EXP_FAIL(futex_waitv(waitv, 0, 0, &to, CLOCK_MONOTONIC), EINVAL,
 		     "futex_waitv invalid nr_futexes");
+}
+
+static void test_mismatch_between_uaddr_and_val(void)
+{
+	struct timespec to;
+
+	waitv->uaddr = (uintptr_t)futex;
+	waitv->flags = FUTEX_32 | FUTEX_PRIVATE_FLAG;
+	waitv->val = 1;
+
+	init_timeout(&to);
+
+	TST_EXP_FAIL(futex_waitv(waitv, 1, 0, &to, CLOCK_MONOTONIC), EAGAIN,
+		     "futex_waitv mismatch between value of uaddr and val");
+}
+
+static void test_timeout(void)
+{
+	struct timespec to;
+
+	waitv->uaddr = (uintptr_t)futex;
+	waitv->flags = FUTEX_32 | FUTEX_PRIVATE_FLAG;
+	waitv->val = 0;
+
+	SAFE_CLOCK_GETTIME(CLOCK_REALTIME, &to);
+	to = tst_timespec_add_us(to, 10000);
+
+	TST_EXP_FAIL(futex_waitv(waitv, 1, 0, &to, CLOCK_REALTIME), ETIMEDOUT,
+		     "futex_waitv timeout");
 }
 
 static void cleanup(void)
@@ -126,6 +155,8 @@ static void run(void)
 	test_null_waiters();
 	test_invalid_clockid();
 	test_invalid_nr_futexes();
+	test_mismatch_between_uaddr_and_val();
+	test_timeout();
 }
 
 static struct tst_test test = {
