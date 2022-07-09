a7a278be196f628e784e92b5050dd04443845782
Avinesh Kumar <akumar@suse.de>
2022-07-09 22:20:00 +0530

    open08.c: Remove redundant headers
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>

--- testcases/kernel/syscalls/open/open08.c
+++ testcases/kernel/syscalls/open/open08.c
@@ -20,16 +20,8 @@
 
 #define _GNU_SOURCE		/* for O_DIRECTORY */
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
-#include <errno.h>
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <pwd.h>
 #include "tst_test.h"
-#include "tst_get_bad_addr.h"
 
 static char *existing_fname = "open08_testfile";
 static char *toolong_fname = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstmnopqrstuvwxyzabcdefghijklmnopqrstmnopqrstuvwxyzabcdefghijklmnopqrstmnopqrstuvwxyzabcdefghijklmnopqrstmnopqrstuvwxyzabcdefghijklmnopqrstmnopqrstuvwxyzabcdefghijklmnopqrstmnopqrstuvwxyz";
