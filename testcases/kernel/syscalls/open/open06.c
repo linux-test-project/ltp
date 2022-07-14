94e0b45678970c379535bffe677617cb6bc4e344
Avinesh Kumar <akumar@suse.de>
2022-07-14 18:16:11 +0530

    open06.c: convert to new LTP API
    mknod() was replaced with mkfifo() (using API recommended by POSIX.1-2001).
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>

--- testcases/kernel/syscalls/open/open06.c
+++ testcases/kernel/syscalls/open/open06.c
@@ -1,90 +1,34 @@
-/*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
- */
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 /*
- * DESCRIPTION
- *	Testcase to check open(2) sets errno to ENXIO correctly.
- *
- * ALGORITHM
- *	Create a named pipe using mknod(2).  Attempt to
- *	open(2) the pipe for writing. The open(2) should
- *	fail with ENXIO.
+ * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include "test.h"
 
-char *TCID = "open06";
-int TST_TOTAL = 1;
+/*\
+ * [Description]
+ *
+ * Verify that open(2) fails with ENXIO when
+ * O_NONBLOCK | O_WRONLY is set, the named file is a FIFO,
+ * and no process has the FIFO open for reading.
+ */
 
-static void setup(void);
-static void cleanup(void);
+#include "tst_test.h"
 
-static char fname[100] = "fifo";
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		TEST(open(fname, O_NONBLOCK | O_WRONLY));
-		if (TEST_RETURN != -1) {
-			tst_resm(TFAIL, "open(2) succeeded unexpectedly");
-			continue;
-		}
-
-		if (TEST_ERRNO != ENXIO)
-			tst_resm(TFAIL, "Expected ENXIO got %d", TEST_ERRNO);
-		else
-			tst_resm(TPASS, "call returned expected ENXIO error");
-	}
-
-	cleanup();
-	tst_exit();
-}
+#define TEMP_FIFO "tmpfile"
 
 static void setup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	sprintf(fname, "%s.%d", fname, getpid());
-
-	if (mknod(fname, S_IFIFO | 0644, 0) == -1)
-		tst_brkm(TBROK, cleanup, "mknod FAILED");
+	SAFE_MKFIFO(TEMP_FIFO, 0644);
 }
 
-static void cleanup(void)
+static void run(void)
 {
-	unlink(fname);
-
-	tst_rmdir();
+	TST_EXP_FAIL2(open(TEMP_FIFO, O_NONBLOCK | O_WRONLY), ENXIO);
 }
+
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.needs_tmpdir = 1
+};
