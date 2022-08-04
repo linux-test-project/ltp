916e9037df0b22d2d57b8e5f4e98b9d5f3b9b44e
Andrea Cervesato via ltp <ltp@lists.linux.it>
2022-08-04 10:48:11 +0200

    Refactor pidns01 test using new LTP API
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/containers/pidns/pidns01.c
+++ testcases/kernel/containers/pidns/pidns01.c
@@ -1,118 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
-* Copyright (c) International Business Machines Corp., 2007
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-* the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-*
-***************************************************************************
-
-* File: pidns01.c
-*
-* Description:
-*  The pidns01.c testcase builds into the ltp framework to verify
-*  the basic functionality of PID Namespace.
-*
-* Verify that:
-* 1. When parent clone a process with flag CLONE_NEWPID, the process ID of
-* child should be always one.
-*
-* 2. When parent clone a process with flag CLONE_NEWPID, the parent process ID of
-* should be always zero.
-*
-* Total Tests:
-*
-* Test Name: pidns01
-*
-* Test Assertion & Strategy:
-*
-* From main() clone a new child process with passing the clone_flag as CLONE_NEWPID,
-* Inside the cloned pid check for the getpid() and getppid()
-* Verify with global macro defined value for parent pid & child pid.
-*
-* Usage: <for command-line>
-* pidns01
-*
-* History:
-*
-* FLAG DATE		NAME			DESCRIPTION
-* 27/12/07  RISHIKESH K RAJAK <risrajak@in.ibm.com> Created this test
-*
-*******************************************************************************************/
-#define _GNU_SOURCE
-#include <sys/wait.h>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include "pidns_helper.h"
-#include "test.h"
-
-char *TCID = "pidns01";
-int TST_TOTAL = 1;
-
-#define CHILD_PID       1
-#define PARENT_PID      0
-
-/*
- * child_fn1() - Inside container
+ * Copyright (c) International Business Machines Corp., 2007
+ *               27/12/07  Rishikesh K Rajak <risrajak@in.ibm.com>
+ * Copyright (C) 2022 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
-int child_fn1(void *ttype LTP_ATTRIBUTE_UNUSED)
+
+/*\
+ * [Description]
+ *
+ * Clone a process with CLONE_NEWPID flag and check:
+ *
+ * - child process ID must be 1
+ * - parent process ID must be 0
+ */
+
+#include "tst_test.h"
+#include "lapi/namespaces_constants.h"
+
+static int child_func(LTP_ATTRIBUTE_UNUSED void *arg)
 {
-	int exit_val;
 	pid_t cpid, ppid;
+
 	cpid = getpid();
 	ppid = getppid();
 
-	tst_resm(TINFO, "PIDNS test is running inside container");
-	if (cpid == CHILD_PID && ppid == PARENT_PID) {
-		printf("Got expected cpid and ppid\n");
-		exit_val = 0;
-	} else {
-		printf("Got unexpected result of cpid=%d ppid=%d\n",
-		       cpid, ppid);
-		exit_val = 1;
-	}
+	TST_EXP_PASS(cpid == 1);
+	TST_EXP_PASS(ppid == 0);
 
-	return exit_val;
+	return 0;
 }
 
-static void setup(void)
+static void run(void)
 {
-	tst_require_root();
-	check_newpid();
+	int ret;
+
+	ret = ltp_clone_quick(CLONE_NEWNS | SIGCHLD, child_func, NULL);
+	if (ret < 0)
+		tst_brk(TBROK | TERRNO, "clone failed");
 }
 
-int main(int argc, char *argv[])
-{
-	int status;
-	tst_parse_opts(argc, argv, NULL, NULL);
-	setup();
-
-	TEST(do_clone_unshare_test(T_CLONE, CLONE_NEWPID, child_fn1, NULL));
-
-	if (TEST_RETURN == -1) {
-		tst_brkm(TFAIL | TTERRNO, NULL, "clone failed");
-	} else if ((wait(&status)) == -1) {
-		tst_brkm(TWARN | TERRNO, NULL, "wait failed");
-	}
-
-	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-		tst_resm(TFAIL, "child exited abnormally");
-	else if (WIFSIGNALED(status)) {
-		tst_resm(TFAIL, "child was killed with signal = %d",
-			 WTERMSIG(status));
-	}
-
-	tst_exit();
-}
-
+static struct tst_test test = {
+	.test_all = run,
+	.needs_root = 1,
+};
