8c98a9fdcaff9789739534cd092d9502d4f75574
Andrea Cervesato via ltp <ltp@lists.linux.it>
2022-08-08 10:15:01 +0200

    Refactor pidns12 test using new LTP API
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>
    [ rpalethorpe: Used TST_EXP_EQ_LI on pids ]

--- testcases/kernel/containers/pidns/pidns12.c
+++ testcases/kernel/containers/pidns/pidns12.c
@@ -1,169 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
-* Copyright (c) International Business Machines Corp., 2007
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-* the GNU General Public License for more details.
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-*
-***************************************************************************
-* File: pidns12.c
-* *
-* * Description:
-* *  The pidns12.c testcase verifies that siginfo->si_pid is set to 0
-* *  if sender (parent process) is not in receiver's namespace.
-* *
-* * Test Assertion & Strategy:
-* *  Create a PID namespace container.
-* *  Initialise signal handler for SIGUSR1 in container.
-* *  Let parent send SIGUSR1 to container.
-* *  Check if sender pid is set to 0 from signal info.
-* *
-* * Usage: <for command-line>
-* *  pidns12
-* *
-* * History:
-* *  DATE      NAME                             DESCRIPTION
-* *  13/11/08  Gowrishankar M 			Creation of this test.
-* *            <gowrishankar.m@in.ibm.com>
-*
-******************************************************************************/
-#define _GNU_SOURCE 1
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <signal.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include "pidns_helper.h"
-#include "test.h"
-
-char *TCID = "pidns12";
-int TST_TOTAL = 1;
-int pipefd[2];
-
-#define CHILD_PID       1
-#define PARENT_PID      0
-
-/*
- * child_signal_handler() - dummy function for sigaction()
+ * Copyright (c) International Business Machines Corp., 2007
+ *               13/11/08  Gowrishankar M <gowrishankar.m@in.ibm.com>
+ * Copyright (C) 2022 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
-static void child_signal_handler(int sig, siginfo_t * si, void *unused)
+
+/*\
+ * [Description]
+ *
+ * Clone a process with CLONE_NEWPID flag and verifies that siginfo->si_pid is
+ * set to 0 if sender (parent process) is not in the receiver's namespace.
+ */
+
+#include "tst_test_macros.h"
+#define _GNU_SOURCE 1
+#include <signal.h>
+#include "tst_test.h"
+#include "lapi/namespaces_constants.h"
+
+static void child_signal_handler(LTP_ATTRIBUTE_UNUSED int sig, siginfo_t *si, LTP_ATTRIBUTE_UNUSED void *unused)
 {
-	/* Recieved SIGUSR1. Check sender pid */
-	if (si->si_pid == 0)
-		tst_resm(TPASS, "cinit: signalling PID (from other namespace)"
-			 " is 0 as expected");
-	else
-		tst_resm(TFAIL, "cinit: signalling PID (from other namespace)"
-			 " is not 0, but %d.", si->si_pid);
+	TST_EXP_EQ_LI(si->si_pid, 0);
 }
 
-/*
- * child_fn() - Inside container
- */
-int child_fn(void *arg)
+static int child_func(LTP_ATTRIBUTE_UNUSED void *arg)
 {
 	struct sigaction sa;
-	pid_t pid, ppid;
 
-	/* Set process id and parent pid */
-	pid = getpid();
-	ppid = getppid();
-	if (pid != CHILD_PID || ppid != PARENT_PID) {
-		tst_resm(TBROK, "cinit: pidns is not created.");
-	}
+	TST_EXP_EQ_LI(getpid(), 1);
+	TST_EXP_EQ_LI(getppid(), 0);
 
-	/* Close read end of pipe */
-	close(pipefd[0]);
-
-	/* Set signal handler for SIGUSR1 */
 	sa.sa_flags = SA_SIGINFO;
-	sigfillset(&sa.sa_mask);
+	SAFE_SIGFILLSET(&sa.sa_mask);
 	sa.sa_sigaction = child_signal_handler;
-	if (sigaction(SIGUSR1, &sa, NULL) == -1) {
-		tst_resm(TBROK, "cinit: sigaction() failed(%s).",
-			 strerror(errno));
-	}
 
-	/* Let parent to signal SIGUSR1 */
-	if (write(pipefd[1], "c:go\0", 5) != 5) {
-		tst_resm(TBROK, "cinit: pipe is broken to write");
-	}
+	SAFE_SIGACTION(SIGUSR1, &sa, NULL);
 
-	sleep(3);
+	TST_CHECKPOINT_WAKE_AND_WAIT(0);
 
-	/* cleanup and exit */
-	close(pipefd[1]);
-
-	/* Control won't reach below */
-	exit(0);
+	return 0;
 }
 
-static void setup(void)
+static void run(void)
 {
-	tst_require_root();
-	check_newpid();
+	int ret;
+
+	ret = ltp_clone_quick(CLONE_NEWPID | SIGCHLD, child_func, NULL);
+	if (ret < 0)
+		tst_brk(TBROK | TERRNO, "clone failed");
+
+	TST_CHECKPOINT_WAIT(0);
+
+	SAFE_KILL(ret, SIGUSR1);
+
+	TST_CHECKPOINT_WAKE(0);
 }
 
-/***********************************************************************
-*   M A I N
-***********************************************************************/
-
-int main(void)
-{
-	int status;
-	pid_t pid, cpid;
-	char buf[5];
-
-	setup();
-
-	pid = getpid();
-	tst_resm(TINFO, "parent: PID is %d", pid);
-
-	/* Create pipe for intercommunication */
-	if (pipe(pipefd) == -1) {
-		tst_resm(TBROK, "parent: pipe() failed. aborting!");
-	}
-
-	cpid = ltp_clone_quick(CLONE_NEWPID | SIGCHLD, child_fn, NULL);
-	if (cpid < 0) {
-		tst_resm(TBROK, "parent: clone() failed(%s).", strerror(errno));
-	}
-
-	/* Close write end of pipe */
-	close(pipefd[1]);
-
-	/* Check if container is ready */
-	read(pipefd[0], buf, 5);
-	if (strcmp(buf, "c:go") != 0) {
-		tst_resm(TBROK, "parent: container did not respond!");
-	}
-
-	/* Send SIGUSR1 to container init */
-	if (kill(cpid, SIGUSR1) == -1) {
-		tst_resm(TBROK, "parent: kill() failed(%s).", strerror(errno));
-	}
-
-	if (waitpid(cpid, &status, 0) < 0)
-		tst_resm(TWARN, "parent: waitpid() failed(%s).",
-			 strerror(errno));
-
-	if (WIFSIGNALED(status) && WTERMSIG(status))
-		tst_resm(TBROK, "child is terminated by signal(%s)",
-			 strsignal(WTERMSIG(status)));
-
-	/* Cleanup and exit */
-	close(pipefd[0]);
-
-	/* Control won't reach below */
-	exit(0);
-
-}
+static struct tst_test test = {
+	.test_all = run,
+	.needs_root = 1,
+	.needs_checkpoints = 1,
+};
