f42f6f3b4671f447b743afe8612917ba4362b8a6
Avinesh Kumar <akumar@suse.de>
2022-08-11 18:43:58 +0530

    alarm07: cleanup and simplify
    - description comment into docparse format and reword
    - remove duplicated headers
    - make check fix: un-initialize static var
    - use TST_EXP_* macros
    - copyright update
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>

--- testcases/kernel/syscalls/alarm/alarm07.c
+++ testcases/kernel/syscalls/alarm/alarm07.c
@@ -2,45 +2,38 @@
 /*
  * Copyright (c) International Business Machines  Corp., 2001
  * Author: Wayne Boyer
- *
- * Test Description:
- *  By the SIGALRM signal, check whether the previously specified alarm request
- *  was cleared in the child process or not.
+ * Copyright (c) Linux Test Project, 2002-2022
  */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <signal.h>
+/*\
+ * [Description]
+ *
+ * Verify that SIGALRM signal scheduled by alarm() in the parent process
+ * is not delivered to the child process.
+ */
 
+#include <stdlib.h>
 #include "tst_test.h"
 
-static volatile int alarm_cnt = 0;
+static volatile int alarm_cnt;
 
 static void verify_alarm(void)
 {
 	pid_t pid;
+
 	alarm_cnt = 0;
 
-	TEST(alarm(1));
+	TST_EXP_PASS_SILENT(alarm(1));
 	pid = SAFE_FORK();
 
 	sleep(3);
 
 	if (pid == 0) {
-		if (alarm_cnt == 0) {
-			tst_res(TPASS, "alarm() request cleared in child");
-		} else {
-			tst_res(TFAIL, "alarm() request not cleared in "
-				"child; alarms received:%d", alarm_cnt);
-		}
+		TST_EXP_EQ_LU(alarm_cnt, 0);
 		exit(0);
 	}
 
-	if (alarm_cnt != 1)
-		tst_res(TFAIL, "Sigalarms in parent %i, expected 1", alarm_cnt);
-	else
-		tst_res(TPASS, "Got 1 sigalarm in parent");
+	TST_EXP_EQ_LU(alarm_cnt, 1);
 }
 
 static void sighandler(int sig LTP_ATTRIBUTE_UNUSED)
