2b6953c71739a31a4f55e83282a02077376d05c5
Avinesh Kumar <akumar@suse.de>
2022-08-23 20:22:13 +0530

    setfsuid01.c: Rewrite using new LTP API
    Rewrite with new API and add test to verify that filesystem user ID was
    correctly updated by calling setfsuid(-1) which will always fail but
    will return the previous filesystem user ID.
    
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

--- testcases/kernel/syscalls/setfsuid/setfsuid01.c
+++ testcases/kernel/syscalls/setfsuid/setfsuid01.c
@@ -1,87 +1,45 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
  * Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-/*
- * Testcase to test the basic functionality of the setfsuid(2) system call.
+/*\
+ * [Description]
+ *
+ * Verify that setfsuid() correctly updates the filesystem user ID
+ * to the value given in fsuid argument.
  */
 
-#include <sys/types.h>
-#include <sys/fsuid.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
+#include <pwd.h>
+#include "tst_test.h"
+#include "compat_tst_16.h"
 
-#include "test.h"
-#include "compat_16.h"
-
-static void setup(void);
-static void cleanup(void);
-
-TCID_DEFINE(setfsuid01);
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	uid_t uid;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	uid = geteuid();
-	UID16_CHECK(uid, setfsuid, cleanup);
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(SETFSUID(cleanup, uid));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO,
-				"setfsuid() failed unexpectedly");
-			continue;
-		}
-
-		if (TEST_RETURN != uid) {
-			tst_resm(TFAIL, "setfsuid() returned %ld, expected %d",
-				 TEST_RETURN, uid);
-		} else {
-			tst_resm(TPASS,
-				"setfsuid() returned expected value : %ld",
-				TEST_RETURN);
-		}
-	}
-
-	cleanup();
-	tst_exit();
-}
+static uid_t nobody_uid;
 
 static void setup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+	struct passwd *nobody;
 
-	TEST_PAUSE;
+	nobody = SAFE_GETPWNAM("nobody");
+	nobody_uid = nobody->pw_uid;
 }
 
-static void cleanup(void)
+static void run(void)
 {
+	uid_t uid;
+
+	uid = geteuid();
+	UID16_CHECK(uid, setfsuid);
+
+	SAFE_SETEUID(0);
+	TST_EXP_VAL(setfsuid(nobody_uid), uid, "setfsuid(%d)", nobody_uid);
+	TST_EXP_VAL(setfsuid(-1), nobody_uid);
 }
+
+static struct tst_test test = {
+	.setup = setup,
+	.test_all = run,
+	.needs_root = 1
+};
