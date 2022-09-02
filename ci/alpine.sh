c679fe3884f1b3a6264b3ea3180f610b09659f23
Petr Vorel <petr.vorel@gmail.com>
2022-09-02 23:58:59 +0200

    ci/alpine: Enable getcontext01.c
    It was converted to the new API in 0f519d0da
    (released in 20220527).
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- ci/alpine.sh
+++ ci/alpine.sh
@@ -34,7 +34,6 @@ echo "WARNING: remove unsupported tests (until they're fixed)"
 cd $(dirname $0)/..
 rm -rfv \
 	testcases/kernel/syscalls/fmtmsg/fmtmsg01.c \
-	testcases/kernel/syscalls/getcontext/getcontext01.c \
 	testcases/kernel/syscalls/rt_tgsigqueueinfo/rt_tgsigqueueinfo01.c \
 	testcases/kernel/syscalls/timer_create/timer_create01.c \
 	testcases/kernel/syscalls/timer_create/timer_create03.c
