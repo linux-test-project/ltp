d4e993089819116866be300e47539c84eeab9507
Avinesh Kumar <akumar@suse.de>
2022-09-15 15:49:00 +0800

    getitimer03.c: convert to new LTP API
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Signed-off-by: Li Wang <liwang@redhat.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/getitimer/getitimer02.c
+++ testcases/kernel/syscalls/getitimer/getitimer02.c
@@ -1,30 +1,63 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
- * 03/2001 - Written by Wayne Boyer
+ *               03/2001 - Written by Wayne Boyer
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
 /*\
  * [Description]
  *
- * Check that a getitimer() call fails with EFAULT with invalid itimerval pointer.
+ * Check that getitimer() call fails:
+ *   1. EFAULT with invalid itimerval pointer
+ *   2. EINVAL when called with an invalid first argument
  */
 
+#include <stdlib.h>
 #include <errno.h>
 #include <sys/time.h>
 #include "tst_test.h"
 #include "lapi/syscalls.h"
 
+static struct itimerval *value;
+static struct itimerval *invalid;
+
+static struct tcase {
+       int which;
+       struct itimerval **val;
+       int exp_errno;
+} tcases[] = {
+       {ITIMER_REAL, &invalid, EFAULT},
+       {-ITIMER_PROF, &value, EINVAL},
+};
+
 static int sys_getitimer(int which, void *curr_value)
 {
-	return tst_syscall(__NR_getitimer, which, curr_value);
+        return tst_syscall(__NR_getitimer, which, curr_value);
 }
 
-static void verify_getitimer(void)
+static void setup(void)
 {
-	TST_EXP_FAIL(sys_getitimer(ITIMER_REAL, (struct itimerval *)-1), EFAULT);
+        value = SAFE_MALLOC(sizeof(struct itimerval));
+        invalid = (struct itimerval *)-1;
+}
+
+static void verify_getitimer(unsigned int i)
+{
+        struct tcase *tc = &tcases[i];
+
+        TST_EXP_FAIL(sys_getitimer(tc->which, *(tc->val)), tc->exp_errno);
+}
+
+static void cleanup(void)
+{
+        free(value);
+        value = NULL;
 }
 
 static struct tst_test test = {
-	.test_all = verify_getitimer,
+        .tcnt = ARRAY_SIZE(tcases),
+        .test = verify_getitimer,
+        .setup = setup,
+        .cleanup = cleanup,
 };
