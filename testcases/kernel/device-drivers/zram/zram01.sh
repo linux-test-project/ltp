9b2d7d82a439443d58961b1f12decb567b484841
Petr Vorel <pvorel@suse.cz>
2022-09-29 20:56:24 +0200

    zram01.sh: Fix minimal size for XFS on kernel 5.19
    mkfs.xfs for kernel 5.19 bumps minimum filesystems 300 MB. Due this we
    already updated minimal filesystem size for all_filesystems /
    $TST_ALL_FILESYSTEMS in 66e05c841. But zram01.sh does not use the API,
    thus update it now.
    
    Also require 1G of RAM for XFS (check originally added in f18c8fd3a for
    Btrfs on ppc64le due to larger page size, likely needed for also XFS).
    
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/device-drivers/zram/zram01.sh
+++ testcases/kernel/device-drivers/zram/zram01.sh
@@ -11,13 +11,14 @@ TST_TESTFUNC="do_test"
 TST_NEEDS_CMDS="awk bc dd"
 TST_SETUP="setup"
 
-check_space_for_btrfs()
+check_space_for_fs()
 {
+	local fs="$1"
 	local ram_size
 
 	ram_size=$(awk '/MemTotal:/ {print $2}' /proc/meminfo)
 	if [ "$ram_size" -lt 1048576 ]; then
-		tst_res TINFO "not enough space for Btrfs"
+		tst_res TINFO "not enough space for $fs"
 		return 1
 	fi
 	return 0
@@ -41,9 +42,15 @@ initialize_vars()
 	for fs in $(tst_supported_fs -s tmpfs); do
 		size="26214400"
 		limit="25M"
-		if [ "$fs" = "btrfs" ]; then
-			check_space_for_btrfs || continue
-			size="402653184"
+
+		if [ "$fs" = "btrfs" -o "$fs" = "xfs" ]; then
+			check_space_for_fs "$fs" || continue
+
+			if [ "$fs" = "btrfs" ]; then
+				size="402653184"
+			elif [ "$fs" = "xfs" ]; then
+				size=314572800
+			fi
 			limit="$((size/1024/1024))M"
 		fi
 
