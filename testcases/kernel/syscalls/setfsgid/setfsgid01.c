a2134db43c0674f69b85da8fc03e2d8b89ec0e54
Avinesh Kumar <akumar@suse.de>
2022-10-12 12:19:29 +0530

    setfsgid01: Rewrite using new LTP API
    Rewrite using new API and add test setfsgid(-1) which will always
    fail but will determine if the preceding setfsgid() call changed
    the filesystem group ID as expected.
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>

--- testcases/kernel/syscalls/setfsgid/setfsgid01.c
+++ testcases/kernel/syscalls/setfsgid/setfsgid01.c
@@ -1,86 +1,48 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
  * Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-/*
- * Testcase to check the basic functionality of setfsgid(2) system call.
+/*\
+ * [Description]
+ *
+ * Verify that setfsgid() correctly updates the filesystem group ID
+ * to the value given in fsgid argument.
  */
 
-#include <stdio.h>
+#include <pwd.h>
 #include <unistd.h>
-#include <sys/fsuid.h>
-#include <sys/types.h>
-#include <errno.h>
 
-#include "test.h"
-#include "compat_16.h"
+#include "tst_test.h"
+#include "compat_tst_16.h"
 
-TCID_DEFINE(setfsgid01);
-int TST_TOTAL = 1;
-
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	gid_t gid;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		gid = getegid();
-		GID16_CHECK(gid, setfsgid, cleanup);
-
-		TEST(SETFSGID(cleanup, gid));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO,
-				"setfsgid() failed unexpectedly");
-			continue;
-		}
-
-		if (TEST_RETURN != gid) {
-			tst_resm(TFAIL, "setfsgid() returned %ld, expected %d",
-				 TEST_RETURN, gid);
-		} else {
-			tst_resm(TPASS,
-				"setfsgid() returned expected value : %ld",
-				TEST_RETURN);
-		}
-	}
-
-	cleanup();
-	tst_exit();
-}
+static gid_t nobody_gid;
 
 static void setup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+	struct passwd *nobody;
 
-	TEST_PAUSE;
+	nobody = SAFE_GETPWNAM("nobody");
+	nobody_gid = nobody->pw_gid;
 }
 
-static void cleanup(void)
+static void run(void)
 {
+	gid_t gid;
+
+	gid = getegid();
+	GID16_CHECK(gid, setfsgid);
+
+	SAFE_SETEUID(0);
+	TST_EXP_VAL(SETFSGID(nobody_gid), gid);
+	TST_EXP_VAL(SETFSGID(-1), nobody_gid);
+	TST_EXP_VAL_SILENT(SETFSGID(gid), nobody_gid);
 }
+
+static struct tst_test test = {
+	.setup = setup,
+	.test_all = run,
+	.needs_root = 1
+};
