a380a02011f61d2619dd84c82c41d23d460d9c3b
Luo xiaoyu via ltp <ltp@lists.linux.it>
2022-10-17 09:43:33 +0800

    signal05.c: Rewrite using new LTP API
    Rewrite using new LTP API.
    
    Signed-off-by: Luo xiaoyu <luoxiaoyu9@huawei.com>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>
    [ rpalethorpe: pass sig fn without &, add volatile and rm prototype ]

--- testcases/kernel/syscalls/signal/signal05.c
+++ testcases/kernel/syscalls/signal/signal05.c
@@ -1,70 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2001
  */
 
-/*
- * NAME
- *	signal05.c
+/*\
+ * [Description]
  *
- * DESCRIPTION
- *	signal05 - set the signal handler to our own function
- *
- * ALGORITHM
- *	loop if that option was specified
- *	issue the system call
- *	check the return value
- *	  if return value == -1
- *	    issue a FAIL message, break remaining tests and cleanup
- *	  if we are doing functional testing
- *	    send the signal with kill()
- *	    if we catch the signal in the signal handler
- *	      issue a PASS message
- *	    else
- *	      issue a FAIL message
- *	call cleanup
- *
- * USAGE:  <for command-line>
- *  signal05 [-c n] [-f] [-i n] [-I x] [-p x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -f   : Turn off functionality Testing.
- *	       -i n : Execute test n times.
- *	       -I x : Execute test for x seconds.
- *	       -P x : Pause for x seconds between iterations.
- *	       -t   : Turn on syscall timing.
- *
- * History
- *	07/2001 John George
- *		-Ported
- *
- * Restrictions
- *	none
+ * Set the signal handler to our own function.
  */
 
-#include "test.h"
+#include "tst_test.h"
 
-#include <errno.h>
-#include <signal.h>
-
-void cleanup(void);
-void setup(void);
-void sighandler(int);
-
-int siglist[] = { SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGIOT,
+static int siglist[] = { SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGIOT,
 	SIGBUS, SIGFPE, SIGUSR1, SIGSEGV, SIGUSR2, SIGPIPE, SIGALRM,
 	SIGTERM,
 #ifdef SIGSTKFLT
@@ -78,94 +25,26 @@ int siglist[] = { SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGTRAP, SIGABRT, SIGIOT,
 #endif
 };
 
-char *TCID = "signal05";
-int TST_TOTAL = ARRAY_SIZE(siglist);
+static volatile int sig_pass;
 
-typedef void (*sighandler_t) (int);
-
-sighandler_t Tret;
-
-int pass = 0;
-
-int main(int ac, char **av)
+static void sighandler(int sig)
 {
-	int lc;
+	sig_pass = sig;
+}
+
+static void do_test(unsigned int n)
+{
 	pid_t pid;
-	int i, rval;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	SAFE_SIGNAL(siglist[n], sighandler);
 
-	setup();		/* global setup */
+	pid = getpid();
 
-	/* The following loop checks looping state if -i option given */
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		/* reset tst_count in case we are looping */
-		tst_count = 0;
-
-		/*
-		 * loop through the list of signals and test each one
-		 */
-		for (i = 0; i < TST_TOTAL; i++) {
-
-			errno = 0;
-			Tret = signal(siglist[i], &sighandler);
-			TEST_ERRNO = errno;
-
-			if (Tret == SIG_ERR) {
-				tst_resm(TFAIL, "%s call failed - errno = %d "
-					 ": %s", TCID, TEST_ERRNO,
-					 strerror(TEST_ERRNO));
-				continue;
-			}
-
-			/*
-			 * Send the signals and make sure they are
-			 * handled in our handler.
-			 */
-			pid = getpid();
-
-			if ((rval = kill(pid, siglist[i])) != 0) {
-				tst_brkm(TBROK, cleanup,
-					 "call to kill failed");
-			}
-
-			if (siglist[i] == pass) {
-				tst_resm(TPASS,
-					 "%s call succeeded", TCID);
-			} else {
-				tst_resm(TFAIL,
-					 "received unexpected signal");
-			}
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	SAFE_KILL(pid, siglist[n]);
+	TST_EXP_EQ_SSZ(siglist[n], sig_pass);
 }
 
-/*
- * sighandler() - handle the signals
- */
-void sighandler(int sig)
-{
-	/* set the global pass variable = sig */
-	pass = sig;
-}
-
-/*
- * setup() - performs all the ONE TIME setup for this test.
- */
-void setup(void)
-{
-	TEST_PAUSE;
-}
-
-/*
- * cleanup() - performs all the ONE TIME cleanup for this test at completion
- * 	       or premature exit.
- */
-void cleanup(void)
-{
-
-}
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(siglist),
+	.test = do_test,
+};
