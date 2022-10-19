85f0b84785f8b56f2458bff33b59619fb07a7ce3
Avinesh Kumar <akumar@suse.de>
2022-10-19 12:47:06 +0530

    setfsuid02: Rewrite using new LTP API
    Rewrite using new API and add setfsuid(-1) call to verify that preceding
    setfsuid() call with an invalid fsuid had failed.
    
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>
    [ rpalethorpe: Substitute setfsuid 16-bit macro, sub invalid_uid for -1]

--- testcases/kernel/syscalls/setfsuid/setfsuid02.c
+++ testcases/kernel/syscalls/setfsuid/setfsuid02.c
@@ -1,91 +1,37 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (C) International Business Machines  Corp., 2001
  * Ported by Wayne Boyer
  * Adapted by Dustin Kirkland (k1rkland@us.ibm.com)
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-/*
- * Testcase to test the basic functionality of the setfsuid(2) system
- * call to fail on invalid uid.
+/*\
+ * [Description]
+ *
+ * Verify that setfsuid() syscall fails if an invalid fsuid is given.
  */
 
-#include <stdio.h>
-#include <unistd.h>
 #include <pwd.h>
-#include <sys/types.h>
-#include <errno.h>
+#include "tst_test.h"
+#include "compat_tst_16.h"
 
-#include "test.h"
-#include "compat_16.h"
-
-static void setup(void);
-static void cleanup(void);
-
-TCID_DEFINE(setfsuid02);
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
+static void run(void)
 {
-	int lc;
+	uid_t invalid_uid, current_uid;
 
-	uid_t uid;
+	current_uid = geteuid();
+	invalid_uid = 1;
+	while (getpwuid(invalid_uid))
+		invalid_uid++;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	UID16_CHECK(invalid_uid, setfsuid);
 
-	setup();
-
-	uid = 1;
-	while (getpwuid(uid))
-		uid++;
-
-	UID16_CHECK(uid, setfsuid, cleanup);
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(SETFSUID(cleanup, uid));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO,
-				"setfsuid() failed unexpectedly");
-			continue;
-		}
-
-		if (TEST_RETURN == uid) {
-			tst_resm(TFAIL, "setfsuid() returned %ld, expected %d",
-				 TEST_RETURN, uid);
-		} else {
-			tst_resm(TPASS, "setfsuid() returned expected value : "
-				 "%ld", TEST_RETURN);
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_VAL_SILENT(SETFSUID(invalid_uid), current_uid);
+	TST_EXP_VAL(SETFSUID(invalid_uid), current_uid,
+		    "setfsuid(invalid_fsuid) test for expected failure:");
 }
 
-static void setup(void)
-{
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-}
-
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test_all = run
+};
