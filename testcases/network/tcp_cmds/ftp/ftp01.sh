b9ba74e6ccb8b2a9ef99c440d0289575465f2fbc
Akihiko Odaki <akihiko.odaki@daynix.com>
2022-10-20 21:07:36 +0900

    ftp/ftp01.sh: Partly rewrite into new API
    Motivation was to get rid of rsh setup.
    
    Suggested-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>
    [ pvorel: there should be more fixes than this, e.g. using
    $(tst_ipaddr rhost) and proper FTP setup as used in
    ftp-upload-stress.sh. ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/tcp_cmds/ftp/ftp01.sh
+++ testcases/network/tcp_cmds/ftp/ftp01.sh
@@ -1,158 +1,75 @@
 #!/bin/sh
-#
-#   Copyright (c) International Business Machines  Corp., 2000
-#
-#   This program is free software;  you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY;  without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-#   the GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program;  if not, write to the Free Software
-#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-#
-#
-#
-#  FILE   : ftp
-#
-#  PURPOSE: To test the basic functionality of the `ftp` command.
-#
-#  SETUP: The home directory of root on the machine exported as "RHOST"
-#         MUST have a ".rhosts" file with the hostname of the machine
-#         where the test is executed. Also, both machines MUST have
-#         the same path configuration for the test for proper test data
-#         file transfers. The PASSWD variable should also be set to root's
-#	  login password.
-#
-#  HISTORY:
-#    06/06/03 Manoj Iyer manjo@mail.utexas.edu
-#      - Modified to use LTP test harness APIs
-#    03/01 Robbie Williamson (robbiew@us.ibm.com)
-#      -Ported
-#
-#
-#-----------------------------------------------------------------------
-#
-#----------------------------------------------------------------------
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (c) 2022 Akihiko Odaki <akihiko.odaki@daynix.com>
+# Copyright (c) 2003 Manoj Iyer <manjo@mail.utexas.edu>
+# Copyright (c) 2001 Robbie Williamson <robbiew@us.ibm.com>
 
-#-----------------------------------------------------------------------
-#
-# FUNCTION:  do_setup
-#
-#-----------------------------------------------------------------------
+TST_TESTFUNC=do_test
+TST_CNT=4
+TST_NEEDS_CMDS='awk ftp'
+TST_NEEDS_TMPDIR=1
 
-do_setup()
-{
-
-    TC=ftp
-    TCtmp=${TCtmp:-$LTPROOT/$TC${EXEC_SUFFIX}$$}
-    TCdat=${TCdat:-$LTPROOT/datafiles}
-    SLEEPTIME=${SLEEPTIME:-0}
-    ASCII_FILES=${ASCII_FILES:-"ascii.sm ascii.med ascii.lg ascii.jmb"}
-    BIN_FILES=${BIN_FILES:-"bin.sm bin.med bin.lg bin.jmb"}
-
-    RHOST=${RHOST:-`hostname`}
-    RUSER=${RUSER:-root}
-    PASSWD=${PASSWD:-.pasroot}
-
-    tst_setup
-
-    exists awk ftp rsh
-
-    cd "$TCtmp"
-
-    rsh -n -l root $RHOST mkdir -p "$TCtmp"
-    rsh -n -l root $RHOST chown -R ${RUSER} "$TCtmp"
-    [ $? = 0 ] || end_testcase "Check .rhosts file on remote machine."
-
-}
-
-#-----------------------------------------------------------------------
-#
-# FUNCTION:  do_test
-#
-#-----------------------------------------------------------------------
+RUSER="${RUSER:-root}"
+RHOST="${RHOST:-localhost}"
 
 do_test()
 {
+    case $1 in
+    1) test_get binary;;
+    2) test_get ascii;;
+    3) test_put binary;;
+    4) test_put ascii;;
+    esac
+}
 
-    for i in binary ascii; do
+list_files()
+{
+    case $1 in
+    binary) echo 'ascii.sm ascii.med ascii.lg ascii.jmb';;
+    ascii) echo 'bin.sm bin.med bin.lg bin.jmb';;
+    esac
+}
 
-        if [ $i = "binary" ]; then
-            FILES=$BIN_FILES
-        fi
-        if [ $i = "ascii" ]; then
-            FILES=$ASCII_FILES
-        fi
-        for j in $FILES; do
+test_get()
+{
+    local file sum1 sum2
 
-            for a in get put; do
-                if [ $a = "get" ]; then
-                    {
-                        echo user $RUSER $PASSWD
-                        echo lcd $TCtmp
-                        echo $i
-                        echo cd $TCdat
-                        echo $a $j
-                        echo quit
-                    } | ftp -nv $RHOST
-                    SUM1=`ls -l $TCtmp/$j  | awk '{print $5}'`
-                    SUM2=`ls -l $TCdat/$j | awk '{print $5}'`
-                    rm -f $TCtmp/$j
-                else
-                    {
-                        echo user $RUSER $PASSWD
-                        echo lcd $TCdat
-                        echo $i
-                        echo cd $TCtmp
-                        echo $a $j
-                        echo quit
-                    } | ftp -nv $RHOST
-                    SUM1=`rsh -n -l root $RHOST sum $TCtmp/$j | awk '{print $1}'`
-                    SUM2=`sum $TCdat/$j | awk '{print $1}'`
-                    rsh -n -l root $RHOST rm -f $TCtmp/$j
-                fi
+    for file in $(list_files $1); do
+        {
+            echo user $RUSER $PASSWD
+            echo $1
+            echo cd $TST_NET_DATAROOT
+            echo get $file
+            echo quit
+        } | ftp -nv $RHOST
 
-                if [ $SUM1 = $SUM2 ]; then
-                    tst_resm TINFO "Test Successful doing ftp $a $j $i"
-                else
-                    end_testcase "Test Fail: Wrong sum while performing ftp $a $j $i"
-                fi
-                sleep $SLEEPTIME
-            done
-        done
+        sum1="$(ls -l $file | awk '{print $5}')"
+        sum2="$(ls -l $TST_NET_DATAROOT/$file | awk '{print $5}')"
+        rm -f $file
+        EXPECT_PASS "[ '$sum1' = '$sum2' ]"
     done
 }
 
-
-#-----------------------------------------------------------------------
-#
-# FUNCTION:  do_cleanup
-#
-#-----------------------------------------------------------------------
-
-do_cleanup()
+test_put()
 {
-    rsh -n -l root $RHOST rmdir "$TCtmp"
-    tst_cleanup
+    local file sum1 sum2
+
+    for file in $(list_files $1); do
+        {
+            echo user $RUSER $PASSWD
+            echo lcd $TST_NET_DATAROOT
+            echo $1
+            echo cd $TST_TMPDIR
+            echo post $file
+            echo quit
+        } | ftp -nv $RHOST
+
+        sum1="$(tst_rhost_run -c "sum $TST_TMPDIR/$file" -s | awk '{print $1}')"
+        sum2="$(sum $TST_NET_DATAROOT/$file | awk '{print $1}')"
+        tst_rhost_run -c "rm -f $TST_TMPDIR/$file"
+        EXPECT_PASS "[ '$sum1' = '$sum2' ]"
+    done
 }
 
-#----------------------------------------------------------------------
-# FUNCTION: MAIN
-# PURPOSE:  To invoke the functions to perform the tasks described in
-#           the prologue.
-# INPUT:    None.
-# OUTPUT:   A testcase run log with the results of the execution of this
-#           test.
-#----------------------------------------------------------------------
-. net_cmdlib.sh
-
-read_opts $*
-do_setup
-do_test
-end_testcase
+. tst_net.sh
+tst_run
