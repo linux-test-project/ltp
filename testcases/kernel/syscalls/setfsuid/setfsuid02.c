5398d6c41dac922122fbd7c871a4c6afb687a42c
Avinesh Kumar <akumar@suse.de>
2022-10-27 19:39:54 +0530

    setfsuid02: using -1 as invalid fsuid for setfsuid()
    a uid which does not have an entry in the /etc/passwd file is not really
    an invalid fsuid for setfsuid(), so changing the test to use -1 as an
    invalid fsuid.
    
    And second setfsuid(-1) call is to verify that preceding call has
    actually failed and there is no change in the fsuid.
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Martin Doucha <mdoucha@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    [ pvorel: cast with UID_T and long ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/setfsuid/setfsuid02.c
+++ testcases/kernel/syscalls/setfsuid/setfsuid02.c
@@ -21,14 +21,12 @@ static void run(void)
 	uid_t invalid_uid, current_uid;
 
 	current_uid = geteuid();
-	invalid_uid = 1;
-	while (getpwuid(invalid_uid))
-		invalid_uid++;
+	invalid_uid = (UID_T)-1;
 
 	UID16_CHECK(invalid_uid, setfsuid);
 
-	TST_EXP_VAL_SILENT(SETFSUID(invalid_uid), current_uid);
-	TST_EXP_VAL(SETFSUID(invalid_uid), current_uid,
+	TST_EXP_VAL_SILENT(SETFSUID(invalid_uid), (long)current_uid);
+	TST_EXP_VAL(SETFSUID(invalid_uid), (long)current_uid,
 		    "setfsuid(invalid_fsuid) test for expected failure:");
 }
 
