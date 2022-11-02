b606a7c7dc613b8041f7b3958695f2cdde04fbe5
Andrea Cervesato <andrea.cervesato@suse.com>
2022-11-02 15:59:35 +0100

    setitimer01: Fix checking of setitimer() parameters
    During the rewrite into the new API in 9b7b8f72b, valid expected
    boundaries weren't considered.
    
    Also introduce counter times as a multiple of clock resolution to avoid
    kernel rounding during the setitimer() counter increase.
    
    Fixes: 9b7b8f72b ("setitimer01: rewrite using new API")
    
    Reviewed-by: Martin Doucha <mdoucha@suse.cz>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/setitimer/setitimer01.c
+++ testcases/kernel/syscalls/setitimer/setitimer01.c
@@ -1,27 +1,28 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) Linux Test Project, 2002-2022
+ * Copyright (c) International Business Machines Corp., 2001
  * 03/2001 - Written by Wayne Boyer
- *
  */
 
 /*\
  * [Description]
  *
- * Check that a setitimer() call pass with timer seting.
- * Check if signal is generated correctly when timer expiration.
+ * Spawn a child and verify that setitimer() syscall passes, and it ends up
+ * counting inside expected boundaries. Then verify from the parent that our
+ * syscall sent the correct signal to the child.
  */
 
+#include <time.h>
 #include <errno.h>
 #include <sys/time.h>
 #include <stdlib.h>
 #include "tst_test.h"
 #include "lapi/syscalls.h"
-
-#define USEC1	10000
-#define USEC2	20000
+#include "tst_safe_clocks.h"
 
 static struct itimerval *value, *ovalue;
+static unsigned long time_step;
 
 static struct tcase {
 	int which;
@@ -55,6 +56,7 @@ static void verify_setitimer(unsigned int i)
 {
 	pid_t pid;
 	int status;
+	int usec = 3 * time_step;
 	struct tcase *tc = &tcases[i];
 
 	pid = SAFE_FORK();
@@ -64,14 +66,18 @@ static void verify_setitimer(unsigned int i)
 
 		tst_no_corefile(0);
 
-		set_setitimer_value(USEC1, 0);
+		set_setitimer_value(usec, 0);
 		TST_EXP_PASS(sys_setitimer(tc->which, value, NULL));
 
-		set_setitimer_value(USEC2, USEC2);
+		set_setitimer_value(5 * time_step, 7 * time_step);
 		TST_EXP_PASS(sys_setitimer(tc->which, value, ovalue));
 
-		if (ovalue->it_value.tv_sec != 0 || ovalue->it_value.tv_usec >= USEC2)
-			tst_brk(TFAIL, "old timer value is not within the expected range");
+		tst_res(TINFO, "tv_sec=%ld, tv_usec=%ld",
+			ovalue->it_value.tv_sec,
+			ovalue->it_value.tv_usec);
+
+		if (ovalue->it_value.tv_sec != 0 || ovalue->it_value.tv_usec > usec)
+			tst_res(TFAIL, "Ending counters are out of range");
 
 		for (;;)
 			;
@@ -85,10 +91,26 @@ static void verify_setitimer(unsigned int i)
 		tst_res(TFAIL, "Child: %s", tst_strstatus(status));
 }
 
+static void setup(void)
+{
+	struct timespec res;
+
+	SAFE_CLOCK_GETRES(CLOCK_MONOTONIC, &res);
+
+	time_step = res.tv_nsec / 1000;
+	if (time_step < 10000)
+		time_step = 10000;
+
+	tst_res(TINFO, "clock resolution: %luns, time step: %luus",
+		res.tv_nsec,
+		time_step);
+}
+
 static struct tst_test test = {
 	.tcnt = ARRAY_SIZE(tcases),
 	.forks_child = 1,
 	.test = verify_setitimer,
+	.setup = setup,
 	.bufs = (struct tst_buffers[]) {
 		{&value,  .size = sizeof(struct itimerval)},
 		{&ovalue, .size = sizeof(struct itimerval)},
