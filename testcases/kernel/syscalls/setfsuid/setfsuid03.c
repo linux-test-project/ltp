f8785f30478e44015597b0303c5e00128716e9ca
Avinesh Kumar <akumar@suse.de>
2022-11-03 11:58:11 +0530

    setfsuid03: Convert to new LTP API
    - rewrite using new API
    - using caller's real user ID as fsuid when calling
    setfsuid(fsuid) by non-root user
    - verifying that fsuid is updated correctly, by calling
    setfsuid(-1)
    
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

--- testcases/kernel/syscalls/setfsuid/setfsuid03.c
+++ testcases/kernel/syscalls/setfsuid/setfsuid03.c
@@ -2,105 +2,46 @@
  * Copyright (C) International Business Machines  Corp., 2001
  * Ported by Wayne Boyer
  * Adapted by Dustin Kirkland (k1rkland@us.ibm.com)
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-/*
- * Testcase to test the basic functionality of the setfsuid(2) system
- * call when called by a user other than root.
+/*\
+ * [Description]
+ *
+ * Verify that setfsuid() correctly updates the filesystem uid
+ * when caller is a non-root user and provided fsuid matches
+ * caller's real user ID.
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <errno.h>
 #include <pwd.h>
 
-#include "test.h"
-#include "compat_16.h"
+#include "tst_test.h"
+#include "compat_tst_16.h"
 
-static void setup(void);
-static void cleanup(void);
-
-TCID_DEFINE(setfsuid03);
-int TST_TOTAL = 1;
-
-static char nobody_uid[] = "nobody";
-static struct passwd *ltpuser;
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	uid_t uid;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	uid = 1;
-	while (!getpwuid(uid))
-		uid++;
-
-	UID16_CHECK(uid, setfsuid, cleanup);
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(SETFSUID(cleanup, uid));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO,
-				"setfsuid() failed unexpectedly");
-			continue;
-		}
-
-		if (TEST_RETURN == uid) {
-			tst_resm(TFAIL,
-				 "setfsuid() returned %ld, expected anything but %d",
-				 TEST_RETURN, uid);
-		} else {
-			tst_resm(TPASS, "setfsuid() returned expected value : "
-				 "%ld", TEST_RETURN);
-		}
-	}
-
-	cleanup();
-	tst_exit();
-}
+static uid_t nobody_uid;
 
 static void setup(void)
 {
-	tst_require_root();
+	struct passwd *nobody;
 
-	ltpuser = getpwnam(nobody_uid);
-	if (ltpuser == NULL)
-		tst_brkm(TBROK, cleanup, "getpwnam failed for user id %s",
-			nobody_uid);
-
-	if (setuid(ltpuser->pw_uid) == -1)
-		tst_resm(TINFO | TERRNO, "setuid failed to "
-			 "to set the effective uid to %d", ltpuser->pw_uid);
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
+	nobody = SAFE_GETPWNAM("nobody");
+	nobody_uid = nobody->pw_uid;
 }
 
-static void cleanup(void)
+static void run(void)
 {
+	uid_t ruid, euid, suid;
+
+	SAFE_GETRESUID(&ruid, &euid, &suid);
+	SAFE_SETEUID(nobody_uid);
+	UID16_CHECK(ruid, setfsuid);
+
+	TST_EXP_VAL_SILENT(setfsuid(ruid), nobody_uid);
+	TST_EXP_VAL(setfsuid(-1), ruid, "setfsuid(fsuid) by non-root user:");
 }
+
+static struct tst_test test = {
+	.needs_root = 1,
+	.setup = setup,
+	.test_all = run
+};
