71f8849bd262f748076eaee8949ad7d0f0c76b17
Zhao Gongyi <zhaogongyi@huawei.com>
2022-11-29 16:13:04 +0800

    syscalls/nice01: Add test nice(-1) and nice(-50)
    1. Add test verify that the errno is zero when calling of nice
    legitimately return -1 (nice(-1), the default nice is usually 0).
    
    2. Add test verify that user of root can decrease the nice value of
    the process successfully by passing a lower increment value
    (< min. applicable limits) to nice() system call (nice(-50)).
    
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Zhao Gongyi <zhaogongyi@huawei.com>

--- testcases/kernel/syscalls/nice/nice01.c
+++ testcases/kernel/syscalls/nice/nice01.c
@@ -3,43 +3,47 @@
  * Copyright (c) International Business Machines Corp., 2001
  * Ported to LTP: Wayne Boyer
  * Copyright (c) 2016 Cyril Hrubis <chrubis@suse.cz>
+ * Copyright (c) Linux Test Project, 2003-2022
  */
 
 /*\
  * [Description]
  *
- * Verify that root can provide a negative value to nice()
- * and hence root can decrease the nice value of the process
- * using nice() system call
+ * Verify that root can provide a negative value to nice() system call and hence
+ * root can decrease the nice value of the process using nice().
  */
+
 #include <unistd.h>
 #include <errno.h>
 #include <sys/resource.h>
 #include "tst_test.h"
 
-#define	NICEINC		-12
-#define MIN_PRIO	-20
+#define MIN_PRIO        -20
 
-static void verify_nice(void)
+static int nice_inc[] = {-1, -12, -50};
+
+static void verify_nice(unsigned int i)
 {
 	int new_nice;
 	int orig_nice;
 	int exp_nice;
+	int inc = nice_inc[i];
+	int delta;
 
 	orig_nice = SAFE_GETPRIORITY(PRIO_PROCESS, 0);
 
-	TEST(nice(NICEINC));
+	TEST(nice(inc));
 
-	exp_nice = MAX(MIN_PRIO, (orig_nice + NICEINC));
+	exp_nice = MAX(MIN_PRIO, (orig_nice + inc));
 
 	if (TST_RET != exp_nice) {
 		tst_res(TFAIL | TTERRNO, "nice(%d) returned %li, expected %i",
-			NICEINC, TST_RET, exp_nice);
+				inc, TST_RET, exp_nice);
 		return;
 	}
 
 	if (TST_ERR) {
-		tst_res(TFAIL | TTERRNO, "nice(%d) failed", NICEINC);
+		tst_res(TFAIL | TTERRNO, "nice(%d) failed", inc);
 		return;
 	}
 
@@ -47,18 +51,20 @@ static void verify_nice(void)
 
 	if (new_nice != exp_nice) {
 		tst_res(TFAIL, "Process priority %i, expected %i",
-				new_nice, orig_nice + NICEINC);
+				new_nice, exp_nice);
 		return;
 	}
 
-	tst_res(TPASS, "nice(%d) passed", NICEINC);
+	tst_res(TPASS, "nice(%d) passed", inc);
 
-	TEST(nice(-NICEINC));
+	delta = orig_nice - exp_nice;
+	TEST(nice(delta));
 	if (TST_ERR)
-		tst_brk(TBROK | TTERRNO, "nice(%d) failed", -NICEINC);
+		tst_brk(TBROK | TTERRNO, "nice(%d) failed", delta);
 }
 
 static struct tst_test test = {
-	.test_all = verify_nice,
 	.needs_root = 1,
+	.test = verify_nice,
+	.tcnt = ARRAY_SIZE(nice_inc),
 };
