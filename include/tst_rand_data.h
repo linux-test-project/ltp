4e43d577b8e28ea8461d0e1380d88dd5c8628b4f
Richard Palethorpe <rpalethorpe@suse.com>
2022-12-12 11:39:44 +0000

    lib/tst_rand_data: Add statically defined data pattern
    Often we need to write some pattern to memory or a block device. There
    are a number of requirements:
    
    * It can be identified
    * It can't be compressed in a degenerate way
    * It can be reproduced
    
    We could generate the data at runtime with a pseudo RNG and constant
    seed. However the storage and bandwidth cost of statically defining
    4KB of random data is negligible.
    
    Signed-off-by: Richard Palethorpe <rpalethorpe@suse.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

---
+++ include/tst_rand_data.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later
+ * Copyright (c) Linux Test Project, 2022
+ */
+
+#ifndef TST_RAND_DATA_H__
+#define TST_RAND_DATA_H__
+
+#include <stddef.h>
+
+/* Includes null byte */
+extern const size_t tst_rand_data_len;
+/* statically defined random data */
+extern const char *const tst_rand_data;
+
+#endif
