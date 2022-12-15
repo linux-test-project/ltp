f3037cd5a10e69b6771e3bbcecafff5945042627
Yang Xu <xuyang2018.jy@fujitsu.com>
2022-12-15 12:47:46 +0800

    syscalls/setresuid04: Use SETRESUID instead of SAFE_SETRESUID to check 16bit syscall
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

--- testcases/kernel/syscalls/setresuid/setresuid04.c
+++ testcases/kernel/syscalls/setresuid/setresuid04.c
@@ -40,7 +40,7 @@ static void run(void)
 	pid_t pid;
 	int status;
 
-	SAFE_SETRESUID(-1, ltpuser->pw_uid, -1);
+	TST_EXP_PASS_SILENT(SETRESUID(-1, ltpuser->pw_uid, -1));
 	TST_EXP_FAIL2(open(TEMP_FILE, O_RDWR), EACCES);
 
 	pid = SAFE_FORK();
@@ -52,7 +52,7 @@ static void run(void)
 	if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
 		tst_res(TFAIL, "child process exited with status: %d", status);
 
-	SAFE_SETRESUID(-1, 0, -1);
+	TST_EXP_PASS_SILENT(SETRESUID(-1, 0, -1));
 	TST_EXP_FD(open(TEMP_FILE, O_RDWR));
 	SAFE_CLOSE(TST_RET);
 }
