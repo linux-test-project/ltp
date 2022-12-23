e305ac4a305f522e2206c9e40bdf53275034f056
Avinesh Kumar <akumar@suse.de>
2022-12-23 20:06:52 +0530

    statvfs01: Convert to new LTP API
    * Removed the TINFO statements.
    * Added a validation of statvfs.f_namemax field by trying to create
    files of valid and invalid length names.
    
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>

--- testcases/kernel/syscalls/statvfs/statvfs01.c
+++ testcases/kernel/syscalls/statvfs/statvfs01.c
@@ -1,92 +1,59 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) Wipro Technologies Ltd, 2005.  All Rights Reserved.
  *    AUTHOR: Prashant P Yendigeri <prashant.yendigeri@wipro.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- */
-/*
- *    DESCRIPTION
- *      This is a Phase I test for the statvfs(2) system call.
- *      It is intended to provide a limited exposure of the system call.
- *	This call behaves similar to statfs.
+ * Copyright (c) 2022 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
+/*\
+ * [Description]
+ *
+ * Verify that statvfs() executes successfully for all
+ * available filesystems. Verify statvfs.f_namemax field
+ * by trying to create files of valid and invalid length names.
+ */
+
 #include <sys/statvfs.h>
-#include <stdint.h>
+#include "tst_test.h"
 
-#include "test.h"
+#define MNT_POINT "mntpoint"
+#define TEST_PATH MNT_POINT"/testfile"
+#define NLS_MAX_CHARSET_SIZE 6
 
-#define TEST_PATH "/"
-
-static void setup(void);
-static void cleanup(void);
-
-char *TCID = "statvfs01";
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
+static void run(void)
 {
 	struct statvfs buf;
-	int lc;
+	char valid_fname[PATH_MAX], toolong_fname[PATH_MAX];
+	long fs_type;
+	long valid_len;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	fs_type = tst_fs_type(TEST_PATH);
 
-	setup();
+	TST_EXP_PASS(statvfs(TEST_PATH, &buf));
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
+	valid_len = buf.f_namemax;
+	if (fs_type == TST_VFAT_MAGIC || fs_type == TST_EXFAT_MAGIC)
+		valid_len = buf.f_namemax / NLS_MAX_CHARSET_SIZE;
 
-		tst_count = 0;
+	memset(valid_fname, 'a', valid_len);
+	memset(toolong_fname, 'b', valid_len + 1);
 
-		TEST(statvfs(TEST_PATH, &buf));
+	TST_EXP_FD(creat(valid_fname, 0444));
+	SAFE_CLOSE(TST_RET);
 
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL | TTERRNO, "statvfs(%s, ...) failed",
-				 TEST_PATH);
-		} else {
-			tst_resm(TPASS, "statvfs(%s, ...) passed", TEST_PATH);
-		}
-
-	}
-
-	tst_resm(TINFO, "This call is similar to statfs");
-	tst_resm(TINFO, "Extracting info about the '%s' file system",
-		 TEST_PATH);
-	tst_resm(TINFO, "file system block size = %lu bytes", buf.f_bsize);
-	tst_resm(TINFO, "file system fragment size = %lu bytes", buf.f_frsize);
-	tst_resm(TINFO, "file system free blocks = %ju",
-		 (uintmax_t) buf.f_bfree);
-	tst_resm(TINFO, "file system total inodes = %ju",
-		 (uintmax_t) buf.f_files);
-	tst_resm(TINFO, "file system free inodes = %ju",
-		 (uintmax_t) buf.f_ffree);
-	tst_resm(TINFO, "file system id = %lu", buf.f_fsid);
-	tst_resm(TINFO, "file system max filename length = %lu", buf.f_namemax);
-
-	cleanup();
-	tst_exit();
+	TST_EXP_FAIL(creat(toolong_fname, 0444), ENAMETOOLONG);
 }
 
 static void setup(void)
 {
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
+	SAFE_TOUCH(TEST_PATH, 0666, NULL);
 }
 
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.needs_root = 1,
+	.mount_device = 1,
+	.mntpoint = MNT_POINT,
+	.all_filesystems = 1
+};
