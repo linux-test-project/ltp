4d99247788688360afde57d3cd941116c9282ab8
Avinesh Kumar <akumar@suse.de>
2023-01-10 18:31:35 +0530

    setregid01.c: Turn comment into docparse format and use TST_EXP_PASS macro
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

--- testcases/kernel/syscalls/setregid/setregid01.c
+++ testcases/kernel/syscalls/setregid/setregid01.c
@@ -15,14 +15,16 @@
  * Co-pilot: Dave Fenner
  */
 
-/*
- * Testcase to test the basic functionality of setregid(2) systemm call.
+/*\
+ * [Description]
+ *
+ * Verify the basic functionality of setregid(2) system call.
  */
 
 #include "tst_test.h"
 #include "compat_tst_16.h"
 
-static gid_t gid, egid;	    /* current real and effective group id */
+static gid_t gid, egid;
 static gid_t neg_one = -1;
 
 static struct tcase {
@@ -30,23 +32,18 @@ static struct tcase {
 	gid_t *arg2;
 	const char *msg;
 } tcases[] = {
-	{&neg_one, &neg_one, "Dont change either real or effective gid" },
+	{&neg_one, &neg_one, "Leave real and effective both gids unchanged" },
 	{&neg_one, &egid,    "Change effective to effective gid" },
 	{&gid,     &neg_one, "Change real to real gid" },
 	{&neg_one, &gid,     "Change effective to real gid" },
-	{&gid,     &gid,     "Try to change real to current real" }
+	{&gid,     &gid,     "Change real and effective both gids to current real gid" }
 };
 
 static void run(unsigned int n)
 {
 	struct tcase *tc = &tcases[n];
 
-	TEST(SETREGID(*tc->arg1, *tc->arg2));
-
-	if (TST_RET == -1)
-		tst_res(TFAIL | TTERRNO, "%s", tc->msg);
-	else
-		tst_res(TPASS, "%s", tc->msg);
+	TST_EXP_PASS(SETREGID(*tc->arg1, *tc->arg2), "%s:", tc->msg);
 }
 
 static void setup(void)
