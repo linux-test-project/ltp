8b55003af510eba38c90c9214da8ac4e7bc5846e
Petr Vorel <pvorel@suse.cz>
2023-02-14 08:26:41 +0100

    tst_net.sh: Move run code after loading test lib
    This is important mostly for variable checks (tst_res() would not be
    loaded). Originally added in ecc29beeda on correct place, but left on
    wrong place during 04021637f4.
    
    Fixes: 04021637f4 ("tst_test.sh: Cleanup getopts usage")
    Acked-by: Richard Palethorpe <rpalethorpe@suse.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/lib/tst_net.sh
+++ testcases/lib/tst_net.sh
@@ -84,23 +84,6 @@ tst_brk_()
 	[ -z "$TST_USE_LEGACY_API" ] && tst_brk $@ || tst_brkm $@
 }
 
-if [ "$TST_PARSE_ARGS_CALLER" = "$TST_PARSE_ARGS" ]; then
-	tst_res_ TWARN "TST_PARSE_ARGS_CALLER same as TST_PARSE_ARGS, unset it ($TST_PARSE_ARGS)"
-	unset TST_PARSE_ARGS_CALLER
-fi
-if [ "$TST_SETUP_CALLER" = "$TST_SETUP" ]; then
-	tst_res_ TWARN "TST_SETUP_CALLER same as TST_SETUP, unset it ($TST_SETUP)"
-	unset TST_SETUP_CALLER
-fi
-if [ "$TST_USAGE_CALLER" = "$TST_USAGE" ]; then
-	tst_res_ TWARN "TST_USAGE_CALLER same as TST_USAGE, unset it ($TST_USAGE)"
-	unset TST_USAGE_CALLER
-fi
-
-if [ -n "$TST_USE_LEGACY_API" ]; then
-	tst_net_read_opts "$@"
-fi
-
 tst_net_detect_ipv6()
 {
 	local type="${1:-lhost}"
@@ -969,6 +952,23 @@ tst_default_max_pkt()
 }
 
 [ -n "$TST_USE_LEGACY_API" ] && . test.sh || . tst_test.sh
+
+if [ "$TST_PARSE_ARGS_CALLER" = "$TST_PARSE_ARGS" ]; then
+	tst_res_ TWARN "TST_PARSE_ARGS_CALLER same as TST_PARSE_ARGS, unset it ($TST_PARSE_ARGS)"
+	unset TST_PARSE_ARGS_CALLER
+fi
+if [ "$TST_SETUP_CALLER" = "$TST_SETUP" ]; then
+	tst_res_ TWARN "TST_SETUP_CALLER same as TST_SETUP, unset it ($TST_SETUP)"
+	unset TST_SETUP_CALLER
+fi
+if [ "$TST_USAGE_CALLER" = "$TST_USAGE" ]; then
+	tst_res_ TWARN "TST_USAGE_CALLER same as TST_USAGE, unset it ($TST_USAGE)"
+	unset TST_USAGE_CALLER
+fi
+
+if [ -n "$TST_USE_LEGACY_API" ]; then
+	tst_net_read_opts "$@"
+fi
 
 # detect IPv6 support on lhost for tests which don't use test links
 tst_net_detect_ipv6
