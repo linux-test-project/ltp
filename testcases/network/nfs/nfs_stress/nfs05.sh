9e1baca87dfc9414e6919e06670256f681bd1f58
Petr Vorel <pvorel@suse.cz>
2023-04-28 17:39:13 +0200

    nfs05.sh: Lower down the default values
    nfs05_make_tree.c runs make which needs on Btrfs quite a lot of
    temporary space. This is a preparation for the next commit which
    start using all filesystems via TST_ALL_FILESYSTEMS=1. Currently we use
    300 MB, which was not enough for Btrfs:
    
    Filesystem     Type      Size  Used Avail Use% Mounted on
    /dev/loop0     btrfs     300M   62M   20K 100% /tmp/LTP_nfs05.Vau10kcszO/mntpoint
    
    More space is required on ppc64le which uses 64KB page size (instead of
    the default 4KB for most of archs).
    
    Proper solution would be to detect available size in nfs05_make_tree.c
    and lower down values based on free space.
    
    Link: https://lore.kernel.org/ltp/20230504131414.3826283-4-pvorel@suse.cz/
    Acked-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/network/nfs/nfs_stress/nfs05.sh
+++ testcases/network/nfs/nfs_stress/nfs05.sh
@@ -8,9 +8,9 @@
 #
 # Created by: Robbie Williamson (robbiew@us.ibm.com)
 
-DIR_NUM=${DIR_NUM:-"10"}
-FILE_NUM=${FILE_NUM:-"30"}
-THREAD_NUM=${THREAD_NUM:-"8"}
+DIR_NUM=${DIR_NUM:-"5"}
+FILE_NUM=${FILE_NUM:-"20"}
+THREAD_NUM=${THREAD_NUM:-"5"}
 TST_NEEDS_CMDS="make gcc"
 TST_TESTFUNC="do_test"
 
