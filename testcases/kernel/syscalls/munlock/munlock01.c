50cbff1d1de4eda49ba44b854abed99c52b3b44a
Yang Xu <xuyang2018.jy@fujitsu.com>
2023-06-02 13:56:09 +0800

    syscalls/munlock01: Convert into new api
    Reviewed-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- testcases/kernel/syscalls/munlock/munlock01.c
+++ testcases/kernel/syscalls/munlock/munlock01.c
@@ -1,162 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * AUTHOR: Nirmala Devi Dhanasekar <nirmala.devi@wipro.com>
  */
-/**************************************************************************
- *
- *    TEST IDENTIFIER	: munlock01
- *
- *    EXECUTED BY	: root / superuser
- *
- *    TEST TITLE	: Basic test for munlock(2)
- *
- *    TEST CASE TOTAL	: 4
- *
- *    AUTHOR		: Nirmala Devi Dhanasekar <nirmala.devi@wipro.com>
- *
- *    SIGNALS
- * 	Uses SIGUSR1 to pause before test if option set.
- * 	(See the parse_opts(3) man page).
- *
- *    DESCRIPTION
- *	This is a Phase I test for the munlock(2) system call.
- *	It is intended to provide a limited exposure of the system call.
- *
- * 	Setup:
- *	  Setup signal handling.
- *	  Pause for SIGUSR1 if option specified.
- *
- * 	Test:
- *	 Loop if the proper options are given.
- *	  Execute system call
- *	  Check return code, if system call failed (return=-1)
- *		Log the errno and Issue a FAIL message.
- *	  Otherwise, Issue a PASS message.
- *
- * 	Cleanup:
- * 	  Print errno log and/or timing stats if options given
- *
- * USAGE:  <for command-line>
- *  munlock01 [-c n] [-e] [-i n] [-I x] [-p x] [-t]
- *		where,		-c n : Run n copies concurrently
- *				-e   : Turn on errno logging.
- *				-h   : Show this help screen
- *				-i n : Execute test n times.
- *				-I x : Execute test for x seconds.
- *				-p   : Pause for SIGUSR1 before starting
- *				-P x : Pause for x seconds between iterations.
- *				-t   : Turn on syscall timing.
- *
- * RESTRICTIONS
- * Must be root/superuser to run it.
- *****************************************************************************/
 
-#include <errno.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include "test.h"
+/*\
+ * [Description]
+ *
+ * Test munlock with various valid addresses and lengths.
+ */
 
-void setup();
-void setup1(int);
-void cleanup();
+#include <stdlib.h>
+#include "tst_test.h"
 
-char *TCID = "munlock01";
-int TST_TOTAL = 4;
+static void *addr;
 
-void *addr1;
-
-struct test_case_t {
-	void **addr;
+static struct tcase {
+	char *msg;
 	int len;
-	void (*setupfunc) ();
-} TC[] = {
-	{
-	&addr1, 1, setup1}, {
-	&addr1, 1024, setup1}, {
-	&addr1, 1024 * 1024, setup1}, {
-	&addr1, 1024 * 1024 * 10, setup1}
+} tcases[] = {
+	{"munlock 1 byte", 1},
+	{"munlock 1024 bytes", 1024},
+	{"munlock 1024 * 1024 bytes", 1024 * 1024},
+	{"munlock 1024 * 1024 * 10 bytes", 1024 * 1024 * 10}
 };
 
-int main(int ac, char **av)
+static void verify_munlock(unsigned int i)
 {
-	int lc, i;
+	struct tcase *tc = &tcases[i];
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	/* check looping state */
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++) {
-
-			if (TC[i].setupfunc != NULL)
-				TC[i].setupfunc(i);
-
-			TEST(munlock(*(TC[i].addr), TC[i].len));
-
-			/* check return code */
-			if (TEST_RETURN == -1) {
-				tst_resm(TFAIL | TTERRNO,
-					 "mlock(%p, %d) Failed with "
-					 "return=%ld", TC[i].addr, TC[i].len,
-					 TEST_RETURN);
-			} else {
-				tst_resm(TPASS, "test %d passed length = %d",
-					 i, TC[i].len);
-			}
-		}
-	}
-
-	/* cleanup and exit */
-	cleanup();
-
-	tst_exit();
+	tst_res(TINFO, "%s", tc->msg);
+	addr = SAFE_MALLOC(tc->len);
+	SAFE_MLOCK(addr, tc->len);
+	TST_EXP_PASS(munlock(addr, tc->len), "munlock(%p, %d)", addr, tc->len);
+	free(addr);
+	addr = NULL;
 }
 
-void setup1(int i)
+static void cleanup(void)
 {
-	addr1 = malloc(TC[i].len);
-	if (addr1 == NULL)
-		tst_brkm(TFAIL, cleanup, "malloc failed");
-	TEST(mlock(*(TC[i].addr), TC[i].len));
-
-	/* check return code */
-	if (TEST_RETURN == -1) {
-		tst_brkm(TFAIL | TTERRNO, cleanup,
-			 "mlock(%p, %d) Failed with return=%ld", TC[i].addr,
-			 TC[i].len, TEST_RETURN);
-	}
+	if (addr)
+		free(addr);
 }
 
-/* setup() - performs all ONE TIME setup for this test. */
-void setup(void)
-{
-	tst_require_root();
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-}
-
-/*
- * cleanup() - performs all ONE TIME cleanup for this test at
- *		completion or premature exit.
- */
-void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.needs_root = 1,
+	.test = verify_munlock,
+	.tcnt = ARRAY_SIZE(tcases),
+	.cleanup = cleanup,
+};
