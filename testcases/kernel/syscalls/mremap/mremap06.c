1f6f5780f24df13e19b6e154adb08ed5f0bd540b
Petr Vorel <pvorel@suse.cz>
2023-07-17 11:43:48 +0200

    mremap06: Add mremap() reproducer for 7e7757876f25
    Add a reproducer for kernel fix 7e7757876f25 ("mm/mremap: fix vm_pgoff
    in vma_merge() case 3").
    
    Affected system reports:
    
    mremap06.c:69: TINFO: all pages with compatible mapping
    mremap06.c:90: TPASS: mmap/mremap work properly
    mremap06.c:69: TINFO: third page's mapping incompatible
    mremap06.c:90: TPASS: mmap/mremap work properly
    mremap06.c:69: TINFO: first page's mapping incompatible
    mremap06.c:56: TFAIL: page 1 wrong value 2 (0x32)
    mremap06.c:56: TFAIL: page 2 wrong value 3 (0x33)
    
    Link: https://bugzilla.suse.com/show_bug.cgi?id=1210903
    Link: https://lore.kernel.org/ltp/20230717094348.1105467-1-pvorel@suse.cz/
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

---
+++ testcases/kernel/syscalls/mremap/mremap06.c
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2023 SUSE LLC
+ * Author: Vlastimil Babka <vbabka@suse.cz>
+ * https://bugzilla.suse.com/attachment.cgi?id=867254
+ * LTP port: Petr Vorel <pvorel@suse.cz>
+ */
+
+/*\
+ * [Description]
+ *
+ * Bug reproducer for 7e7757876f25 ("mm/mremap: fix vm_pgoff in vma_merge() case 3")
+ */
+
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include "tst_test.h"
+#include "tst_safe_macros.h"
+
+#define NUM_PAGES 3
+
+static int fd;
+static char *buf, *buf2;
+static int page_size, mmap_size, mremap_size;
+
+static struct tcase {
+	size_t incompatible;
+	const char *desc;
+} tcases[] = {
+	{
+		.desc = "all pages with compatible mapping",
+	},
+	{
+		.incompatible = 3,
+		.desc = "third page's mapping incompatible",
+	},
+	{
+		.incompatible = 1,
+		.desc = "first page's mapping incompatible",
+	},
+};
+
+static int check_pages(void)
+{
+	int fail = 0, i;
+	char val;
+
+	for (i = 0; i < (int)ARRAY_SIZE(tcases); i++) {
+		val = buf[i * page_size];
+		if (val != 0x30 + i) {
+			tst_res(TFAIL, "page %d wrong value %d (0x%x)", i, val - 0x30, val);
+			fail = 1;
+		}
+	}
+
+	return fail;
+}
+
+static void do_test(unsigned int n)
+{
+	struct tcase *tc = &tcases[n];
+	int ret;
+
+	tst_res(TINFO, "%s", tc->desc);
+
+	buf = SAFE_MMAP(0, mmap_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+	buf2 = mremap(buf + page_size, page_size, page_size,
+			MREMAP_MAYMOVE|MREMAP_FIXED, buf + mremap_size);
+	if (buf2 == MAP_FAILED)
+		tst_brk(TBROK, "mremap() failed");
+
+	if (tc->incompatible) {
+		ret = mprotect(buf + (tc->incompatible-1)*page_size, page_size, PROT_READ);
+		if (ret == -1)
+			tst_brk(TBROK, "mprotect() failed");
+	}
+
+	buf2 = mremap(buf + mremap_size, page_size, page_size,
+			MREMAP_MAYMOVE|MREMAP_FIXED, buf + page_size);
+	if (buf2 == MAP_FAILED)
+		tst_brk(TBROK, "mremap() failed");
+
+	if (!check_pages())
+		tst_res(TPASS, "mmap/mremap work properly");
+
+	SAFE_MUNMAP(buf, mremap_size);
+}
+
+static void setup(void)
+{
+	int ret, i;
+
+	page_size = getpagesize();
+	mmap_size = (NUM_PAGES+1) * page_size;
+	mremap_size = NUM_PAGES * page_size;
+
+	fd = SAFE_OPEN("testfile", O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	ret = fallocate(fd, 0, 0, mmap_size);
+	if (ret == -1)
+		tst_brk(TBROK, "fallocate() failed");
+
+	buf = SAFE_MMAP(0, mmap_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+
+	for (i = 0; i < (int)ARRAY_SIZE(tcases)+1; i++)
+		buf[i*page_size] = 0x30 + i;
+
+	/* clear the page tables */
+	SAFE_MUNMAP(buf, mmap_size);
+}
+
+static void cleanup(void)
+{
+	if (fd > 0)
+		SAFE_CLOSE(fd);
+}
+
+static struct tst_test test = {
+	.setup = setup,
+	.cleanup = cleanup,
+	.test = do_test,
+	.tcnt = ARRAY_SIZE(tcases),
+	.tags = (struct tst_tag[]) {
+		{"linux-git", "7e7757876f25"},
+		{}
+	},
+};
