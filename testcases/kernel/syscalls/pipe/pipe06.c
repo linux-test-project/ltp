1d00f0f23bf0f37a360b102e74a587e705e9baf3
Avinesh Kumar <akumar@suse.de>
2023-07-18 14:55:48 +0530

    syscalls/pipe06: Rewrite the test using new LTP API
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>

--- testcases/kernel/syscalls/pipe/pipe06.c
+++ testcases/kernel/syscalls/pipe/pipe06.c
@@ -1,119 +1,57 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2001
+ * 07/2001 Ported by Wayne Boyer
+ * Copyright (c) Linux Test Project, 2002-2015
+ * Copyright (c) 2023 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-/*
- * NAME
- *	pipe06.c
+/*\
+ * [Description]
  *
- * DESCRIPTION
- *	Check what happens when the system runs out of pipes.
- *
- * ALGORITHM
- *	Issue enough pipe calls to run the system out of pipes.
- *	Check that we get EMFILE.
- *
- * USAGE:  <for command-line>
- *  pipe06 [-c n] [-e] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -e   : Turn on errno logging.
- *             -i n : Execute test n times.
- *             -I x : Execute test for x seconds.
- *             -P x : Pause for x seconds between iterations.
- *             -t   : Turn on syscall timing.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS
- *	None
+ * Verify that, pipe(2) syscall fails with errno EMFILE when
+ * limit on the number of open file descriptors has been reached.
  */
-#include <fcntl.h>
-#include <errno.h>
-#include "test.h"
 
-char *TCID = "pipe06";
-int TST_TOTAL = 1;
+#include "tst_test.h"
+#include <stdlib.h>
 
-int pipe_ret, pipes[2];
-void setup(void);
-void cleanup(void);
+static int fds[2];
+static int *opened_fds, num_opened_fds;
 
-int main(int ac, char **av)
+static void setup(void)
 {
-	int lc;
+	int max_fds;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	max_fds = getdtablesize();
+	tst_res(TINFO, "getdtablesize() = %d", max_fds);
+	opened_fds = SAFE_MALLOC(max_fds * sizeof(int));
 
-	setup();
+	do {
+		SAFE_PIPE(fds);
+		opened_fds[num_opened_fds++] = fds[0];
+		opened_fds[num_opened_fds++] = fds[1];
+	} while (fds[1] < max_fds - 2);
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		/* reset tst_count in case we are looping */
-		tst_count = 0;
-
-		TEST(pipe(pipes));
-
-		if (TEST_RETURN != -1) {
-			tst_resm(TFAIL, "call succeeded unexpectedly");
-		}
-
-		if (TEST_ERRNO != EMFILE) {
-			tst_resm(TFAIL | TTERRNO, "pipe failed unexpectedly");
-		} else {
-			tst_resm(TPASS, "failed with EMFILE");
-		}
-
-	}
-	cleanup();
-	tst_exit();
-
+	tst_res(TINFO, "Number of fds opened by pipe calls: %d", num_opened_fds);
 }
 
-/*
- * setup() - performs all ONE TIME setup for this test.
- */
-void setup(void)
+static void run(void)
 {
-	int i, numb_fds;
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-	numb_fds = getdtablesize();
-
-	for (i = 0; i < numb_fds; i++) {
-		pipe_ret = pipe(pipes);
-		if (pipe_ret < 0) {
-			if (errno != EMFILE) {
-				tst_brkm(TBROK | TTERRNO, cleanup,
-					 "didn't get EMFILE");
-			}
-			break;
-		}
-	}
+	TST_EXP_FAIL(pipe(fds), EMFILE);
 }
 
-/*
- * cleanup() - performs all ONE TIME cleanup for this test at
- *	       completion or premature exit.
- */
-void cleanup(void)
+static void cleanup(void)
 {
+	for (int i = 0; i < num_opened_fds; i++)
+		SAFE_CLOSE(opened_fds[i]);
+
+	if (opened_fds)
+		free(opened_fds);
 }
+
+static struct tst_test test = {
+	.setup = setup,
+	.cleanup = cleanup,
+	.test_all = run
+};
