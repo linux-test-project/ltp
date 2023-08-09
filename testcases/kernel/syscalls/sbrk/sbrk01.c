d00409b6bdcd4cbd13dbf53840bfb35ff13eef11
Avinesh Kumar <akumar@suse.de>
2023-08-09 18:48:45 +0530

    sbrk01.c: Refactor the test using new LTP API
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/sbrk/sbrk01.c
+++ testcases/kernel/syscalls/sbrk/sbrk01.c
@@ -1,111 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (c) 2000 Silicon Graphics, Inc.  All Rights Reserved.
- *  AUTHOR		: William Roske
- *  CO-PILOT		: Dave Fenner
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Contact information: Silicon Graphics, Inc., 1600 Amphitheatre Pkwy,
- * Mountain View, CA  94043, or:
- *
- * http://www.sgi.com
- *
- * For further information regarding this notice, see:
- *
- * http://oss.sgi.com/projects/GenInfo/NoticeExplan/
- *
- */
-/*
- * DESCRIPTION
- *	1.) test sbrk(8192) should return successfully.
- *	2.) test sbrk(-8192) should return successfully.
+ *  AUTHOR : William Roske, CO-PILOT : Dave Fenner
+ * Copyright (c) 2023 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/types.h>
+/*\
+ * [Description]
+ *
+ * Verify that sbrk() successfully increments or decrements the program's
+ * data break.
+ */
 
-#include "test.h"
+#include "tst_test.h"
 
-char *TCID = "sbrk01";
-
-static struct test_case_t {
+static struct tcase {
 	long increment;
-} test_cases[] = {
+} tcases[] = {
+	{0},
 	{8192},
-	{-8192},
+	{-8192}
 };
 
-static void setup(void);
-static void sbrk_verify(const struct test_case_t *);
-static void cleanup(void);
-
-int TST_TOTAL = ARRAY_SIZE(test_cases);
-
-int main(int ac, char **av)
+static void run(unsigned int i)
 {
-	int lc;
-	int i;
+	struct tcase *tc = &tcases[i];
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	TESTPTR(sbrk(tc->increment));
 
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		for (i = 0; i < TST_TOTAL; i++)
-			sbrk_verify(&test_cases[i]);
-	}
-
-	cleanup();
-	tst_exit();
-
+	if (TST_RET_PTR == (void *) -1)
+		tst_res(TFAIL | TTERRNO, "sbrk(%ld) failed", tc->increment);
+	else
+		tst_res(TPASS, "sbrk(%ld) returned %p", tc->increment, TST_RET_PTR);
 }
 
-static void setup(void)
-{
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-}
-
-static void sbrk_verify(const struct test_case_t *test)
-{
-	void *tret;
-
-	tret = sbrk(test->increment);
-	TEST_ERRNO = errno;
-
-	if (tret == (void *)-1) {
-		tst_resm(TFAIL | TTERRNO, "sbrk - Increase by %ld bytes failed",
-			 test->increment);
-	} else {
-		tst_resm(TPASS, "sbrk - Increase by %ld bytes returned %p",
-			 test->increment, tret);
-	}
-}
-
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases)
+};
