e2a0680885c3c6d7ef6cf963072a056467f4b30b
Avinesh Kumar <akumar@suse.de>
2023-08-24 13:08:42 +0530

    syscalls/mmap06: Refactor and add scenarios for EACCES test
    As mmap() should always result in EACCES when a file mapping is
    requested but the file descriptor is not open for reading, I've added
    few possible combinations for prot and flags arguments to test for
    EACCES.
    
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/mmap/mmap06.c
+++ testcases/kernel/syscalls/mmap/mmap06.c
@@ -1,143 +1,75 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *  07/2001 Ported by Wayne Boyer
+ * Copyright (c) 2023 SUSE LLC Avinesh Kumar <avinesh.kumar@suse.com>
  */
 
-/*
- * Test Description:
- *  Call mmap() to map a file creating a mapped region with read access
- *  under the following conditions -
- *	- The prot parameter is set to PROT_READ
- *	- The file descriptor is open for writing.
+/*\
+ * [Description]
  *
- *  The call should fail to map the file.
- *
- * Expected Result:
- *  mmap() should fail returning -1 and errno should get set to EACCES.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
+ * Verify that, mmap() call fails when a file mapping is requested but the
+ * file descriptor is not open for reading, and errno is set to EACCES.
  */
-#include <stdio.h>
+
 #include <stdlib.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
+#include "tst_test.h"
 
-#include "test.h"
-
-#define TEMPFILE	"mmapfile"
-
-char *TCID = "mmap06";
-int TST_TOTAL = 1;
-
+#define TEMPFILE "mmapfile"
 static size_t page_sz;
-static char *addr;
-static int fildes;
+static int fd;
 
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		/*
-		 * Call mmap to map the temporary file 'TEMPFILE'
-		 * with read access.
-		 */
-		errno = 0;
-		addr = mmap(0, page_sz, PROT_READ,
-			    MAP_FILE | MAP_SHARED, fildes, 0);
-		TEST_ERRNO = errno;
-
-		/* Check for the return value of mmap() */
-		if (addr != MAP_FAILED) {
-			tst_resm(TFAIL | TERRNO,
-				 "mmap() returned invalid value, expected: %p",
-				 MAP_FAILED);
-			/* Unmap the mapped memory */
-			if (munmap(addr, page_sz) != 0) {
-				tst_resm(TBROK, "munmap() failed");
-				cleanup();
-			}
-			continue;
-		}
-		if (TEST_ERRNO == EACCES) {
-			tst_resm(TPASS, "mmap failed with EACCES");
-		} else {
-			tst_resm(TFAIL | TERRNO,
-				 "mmap failed with unexpected errno");
-		}
-	}
-	cleanup();
-	tst_exit();
-
-}
+static struct tcase {
+	int prot;
+	int flags;
+} tcases[] = {
+	{PROT_WRITE, MAP_FILE | MAP_PRIVATE},
+	{PROT_WRITE, MAP_FILE | MAP_SHARED},
+	{PROT_READ, MAP_FILE | MAP_PRIVATE},
+	{PROT_READ, MAP_FILE | MAP_SHARED},
+	{PROT_READ | PROT_WRITE, MAP_FILE | MAP_PRIVATE},
+	{PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED}
+};
 
 static void setup(void)
 {
-	char *tst_buff;
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
+	char *buf;
 
 	page_sz = getpagesize();
+	buf = SAFE_MALLOC(page_sz);
+	memset(buf, 'A', page_sz);
 
-	/* Allocate space for the test buffer */
-	if ((tst_buff = calloc(page_sz, sizeof(char))) == NULL) {
-		tst_brkm(TFAIL, NULL, "calloc() failed (tst_buff)");
+	fd = SAFE_OPEN(TEMPFILE, O_WRONLY | O_CREAT, 0666);
+	SAFE_WRITE(SAFE_WRITE_ALL, fd, buf, page_sz);
+	free(buf);
+}
+
+static void run(unsigned int i)
+{
+	struct tcase *tc = &tcases[i];
+
+	TESTPTR(mmap(NULL, page_sz, tc->prot, tc->flags, fd, 0));
+
+	if (TST_RET_PTR != MAP_FAILED) {
+		tst_res(TFAIL, "mmap() was successful unexpectedly");
+		SAFE_MUNMAP(TST_RET_PTR, page_sz);
+	} else if (TST_ERR == EACCES) {
+		tst_res(TPASS, "mmap() failed with errno EACCES");
+	} else {
+		tst_res(TFAIL | TERRNO, "mmap() failed but unexpected errno");
 	}
-
-	/* Fill the test buffer with the known data */
-	memset(tst_buff, 'A', page_sz);
-
-	tst_tmpdir();
-
-	/* Creat a temporary file used for mapping */
-	if ((fildes = open(TEMPFILE, O_WRONLY | O_CREAT, 0666)) < 0) {
-		free(tst_buff);
-		tst_brkm(TFAIL, cleanup, "opening %s failed", TEMPFILE);
-	}
-
-	/* Write test buffer contents into temporary file */
-	if (write(fildes, tst_buff, page_sz) < (ssize_t)page_sz) {
-		free(tst_buff);
-		tst_brkm(TFAIL, cleanup, "writing to %s failed", TEMPFILE);
-	}
-
-	free(tst_buff);
 }
 
 static void cleanup(void)
 {
-	close(fildes);
-	tst_rmdir();
+	if (fd > 0)
+		SAFE_CLOSE(fd);
 }
+
+static struct tst_test test = {
+	.setup = setup,
+	.cleanup = cleanup,
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
+	.needs_tmpdir = 1
+};
