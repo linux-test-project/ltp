90660707bc5ade5279600a68daaed11203ae80d9
Andrea Cervesato <andrea.cervesato@suse.com>
2023-09-04 14:54:24 +0200

    Check swap availability in process_madvise01
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/process_madvise/process_madvise01.c
+++ testcases/kernel/syscalls/process_madvise/process_madvise01.c
@@ -123,4 +123,9 @@ static struct tst_test test = {
 	.min_kver = "5.10",
 	.needs_checkpoints = 1,
 	.needs_root = 1,
+	.min_swap_avail = MEM_CHILD / TST_KB,
+	.needs_kconfigs = (const char *[]) {
+		"CONFIG_SWAP=y",
+		NULL
+	},
 };
