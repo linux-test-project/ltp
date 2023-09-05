e662bbfe8ea7c47ba95ad193847ecea909c9b611
Andrea Cervesato <andrea.cervesato@suse.com>
2023-09-05 11:16:13 +0200

    Refactor and merge timerfd02/03 using new LTP API
    timerfd02 and timerfd03 has been merged together, testing the flags
    value via LTP test cases.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

--- testcases/kernel/syscalls/timerfd/timerfd02.c
+++ testcases/kernel/syscalls/timerfd/timerfd02.c
@@ -1,174 +1,51 @@
-/******************************************************************************/
-/*                                                                            */
-/* Copyright (c) Ulrich Drepper <drepper@redhat.com>                          */
-/* Copyright (c) International Business Machines  Corp., 2009                 */
-/*                                                                            */
-/* This program is free software;  you can redistribute it and/or modify      */
-/* it under the terms of the GNU General Public License as published by       */
-/* the Free Software Foundation; either version 2 of the License, or          */
-/* (at your option) any later version.                                        */
-/*                                                                            */
-/* This program is distributed in the hope that it will be useful,            */
-/* but WITHOUT ANY WARRANTY;  without even the implied warranty of            */
-/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See                  */
-/* the GNU General Public License for more details.                           */
-/*                                                                            */
-/* You should have received a copy of the GNU General Public License          */
-/* along with this program;  if not, write to the Free Software               */
-/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA    */
-/*                                                                            */
-/******************************************************************************/
-/******************************************************************************/
-/*                                                                            */
-/* File:        timerfd02.c                                                   */
-/*                                                                            */
-/* Description: This Program tests the new system call introduced in 2.6.27.  */
-/*              Ulrich´s comment as in:                                       */
-/* http://git.kernel.org/?p=linux/kernel/git/torvalds/linux-2.6.git;a=commit;h=11fcb6c14676023d0bd437841f5dcd670e7990a0 */
-/*              says:                                                         */
-/* The timerfd_create syscall already has a flags parameter.  It just is      */
-/* unused so far.  This patch changes this by introducing the TFD_CLOEXEC     */
-/* flag to set the close-on-exec flag for the returned file descriptor. A new */
-/* name TFD_CLOEXEC is introduced which in this implementation must have the  */
-/* same value as O_CLOEXEC.                                                   */
-/* The following test must be adjusted for architectures other than x86 and   */
-/* x86-64 and in case the syscall numbers changed.                            */
-/*                                                                            */
-/* Usage:  <for command-line>                                                 */
-/* timerfd02 [-c n] [-e][-i n] [-I x] [-p x] [-t]                           */
-/*      where,  -c n : Run n copies concurrently.                             */
-/*              -e   : Turn on errno logging.                                 */
-/*              -i n : Execute test n times.                                  */
-/*              -I x : Execute test for x seconds.                            */
-/*              -P x : Pause for x seconds between iterations.                */
-/*              -t   : Turn on syscall timing.                                */
-/*                                                                            */
-/* Total Tests: 1                                                             */
-/*                                                                            */
-/* Test Name:   timerfd02                                                     */
-/*                                                                            */
-/* Author:      Ulrich Drepper <drepper@redhat.com>                           */
-/*                                                                            */
-/* History:     Created - Jan 08 2009 - Ulrich Drepper <drepper@redhat.com>   */
-/*              Ported to LTP                                                 */
-/*                      - Jan 08 2009 - Subrata <subrata@linux.vnet.ibm.com>  */
-/******************************************************************************/
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) Ulrich Drepper <drepper@redhat.com>
+ * Copyright (c) International Business Machines  Corp., 2009
+ * Copyright (C) 2023 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+/*\
+ * [Description]
+ *
+ * This test verifies that:
+ * - TFD_CLOEXEC sets the close-on-exec file status flag on the new open  file
+ * - TFD_NONBLOCK sets the O_NONBLOCK file status flag on the new open file
+ */
 
-#include <stdio.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/syscall.h>
-#include <errno.h>
-
-#include "test.h"
+#include "tst_test.h"
+#include "tst_safe_timerfd.h"
 #include "lapi/fcntl.h"
 #include "lapi/syscalls.h"
 
-#define TFD_CLOEXEC O_CLOEXEC
+static int fdesc;
 
-char *TCID = "timerfd02";
-int testno;
-int TST_TOTAL = 1;
+static struct test_case_t {
+	int flags;
+	int check;
+	int expected;
+} tcases[] = {
+	{ 0, F_GETFD, 0 },
+	{ TFD_CLOEXEC, F_GETFD, FD_CLOEXEC },
+	{ TFD_NONBLOCK, F_GETFL, O_NONBLOCK }
+};
 
-/* Extern Global Functions */
-/******************************************************************************/
-/*                                                                            */
-/* Function:    cleanup                                                       */
-/*                                                                            */
-/* Description: Performs all one time clean up for this test on successful    */
-/*              completion,  premature exit or  failure. Closes all temporary */
-/*              files, removes all temporary directories exits the test with  */
-/*              appropriate return code by calling tst_exit() function.       */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits calling tst_exit(). Non '0' return code.   */
-/*              On success - Exits calling tst_exit(). With '0' return code.  */
-/*                                                                            */
-/******************************************************************************/
-void cleanup(void)
+static void run(unsigned int i)
 {
+	struct test_case_t *tcase = &tcases[i];
 
-	tst_rmdir();
-
+	TST_EXP_FD(fdesc = SAFE_TIMERFD_CREATE(CLOCK_REALTIME, tcase->flags));
+	TST_EXP_EQ_LI(SAFE_FCNTL(fdesc, tcase->check) & tcase->expected, tcase->expected);
+	SAFE_CLOSE(fdesc);
 }
 
-/* Local  Functions */
-/******************************************************************************/
-/*                                                                            */
-/* Function:    setup                                                         */
-/*                                                                            */
-/* Description: Performs all one time setup for this test. This function is   */
-/*              typically used to capture signals, create temporary dirs      */
-/*              and temporary files that may be used in the course of this    */
-/*              test.                                                         */
-/*                                                                            */
-/* Input:       None.                                                         */
-/*                                                                            */
-/* Output:      None.                                                         */
-/*                                                                            */
-/* Return:      On failure - Exits by calling cleanup().                      */
-/*              On success - returns 0.                                       */
-/*                                                                            */
-/******************************************************************************/
-void setup(void)
+static void cleanup(void)
 {
-	/* Capture signals if any */
-	/* Create temporary directories */
-	TEST_PAUSE;
-	tst_tmpdir();
+	if (fcntl(fdesc, F_GETFD) != -1)
+		SAFE_CLOSE(fdesc);
 }
 
-int main(int argc, char *argv[])
-{
-	int fd, coe;
-	int lc;
-
-	tst_parse_opts(argc, argv, NULL, NULL);
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); ++lc) {
-		tst_count = 0;
-		for (testno = 0; testno < TST_TOTAL; ++testno) {
-			fd = tst_syscall(__NR_timerfd_create,
-				CLOCK_REALTIME, 0);
-			if (fd == -1) {
-				tst_brkm(TFAIL, cleanup,
-					 "timerfd_create(0) failed");
-			}
-			coe = fcntl(fd, F_GETFD);
-			if (coe == -1) {
-				tst_brkm(TBROK, cleanup, "fcntl failed");
-			}
-			if (coe & FD_CLOEXEC) {
-				tst_brkm(TFAIL,
-					 cleanup,
-					 "timerfd_create(0) set close-on-exec flag");
-			}
-			close(fd);
-
-			fd = tst_syscall(__NR_timerfd_create, CLOCK_REALTIME,
-				     TFD_CLOEXEC);
-			if (fd == -1) {
-				tst_brkm(TFAIL,
-					 cleanup,
-					 "timerfd_create(TFD_CLOEXEC) failed");
-			}
-			coe = fcntl(fd, F_GETFD);
-			if (coe == -1) {
-				tst_brkm(TBROK, cleanup, "fcntl failed");
-			}
-			if ((coe & FD_CLOEXEC) == 0) {
-				tst_brkm(TFAIL,
-					 cleanup,
-					 "timerfd_create(TFD_CLOEXEC) set close-on-exec flag");
-			}
-			close(fd);
-			tst_resm(TPASS, "timerfd_create(TFD_CLOEXEC) Passed");
-			cleanup();
-		}
-	}
-	tst_exit();
-}
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
+	.cleanup = cleanup,
+};
