0e8ee0bc6c76685a24f36bd87e10714a24e4ef3d
Andrea Cervesato <andrea.cervesato@suse.com>
2023-09-08 11:28:22 +0200

    Merge fork06 and fork11 into new test
    fork06 and fork11 worked in the same way, spawning multiple processes
    inside parent and waiting until they complete by checking wait() pid
    result. fork06 was spawning 1000 children, fork11 was spawning 100
    children. For this reason, fork_procs now merges both of them,
    including an option that selects amount of processes we want to spawn.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

---
+++ testcases/kernel/syscalls/fork/fork_procs.c
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+
+/*\
+ *[Description]
+ *
+ * This test spawns multiple processes using fork() and it checks if wait()
+ * returns the right PID once they end up.
+ */
+
+#include <stdlib.h>
+#include "tst_test.h"
+
+static char *str_numforks;
+static int numforks = 1000;
+
+static void run(void)
+{
+	pid_t pid;
+	int status;
+	int counter = 0;
+
+	tst_res(TINFO, "Forking %d processes", numforks);
+
+	for (int i = 0; i < numforks; i++) {
+		pid = SAFE_FORK();
+		if (!pid)
+			exit(0);
+
+		if (SAFE_WAIT(&status) == pid)
+			counter++;
+	}
+
+	TST_EXP_EXPR(numforks == counter,
+		"%d processes ended successfully",
+		counter);
+}
+
+static void setup(void)
+{
+	if (tst_parse_int(str_numforks, &numforks, 1, INT_MAX))
+		tst_brk(TBROK, "wrong number of forks '%s'", str_numforks);
+}
+
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.forks_child = 1,
+	.options = (struct tst_option[]) {
+		{ "n:", &str_numforks, "Number of processes to spawn" },
+		{},
+	},
+};
