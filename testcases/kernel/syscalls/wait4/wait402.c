299d6e847bb9ae274e3caff874d18528692adeb9
ybonatakis <ybonatakis@suse.com>
2023-10-03 19:41:55 +0200

    wait4/wait402.c: Rewrite to the new API
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Ioannis Bonatakis <ybonatakis@suse.com>
    [ pvorel: Remove _USE_BSD, use pid_max + 1 ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/wait4/wait402.c
+++ testcases/kernel/syscalls/wait4/wait402.c
@@ -1,101 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *   Copyright (c) 2012 Cyril Hrubis <chrubis@suse.cz>
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) 2012 Cyril Hrubis <chrubis@suse.cz>
+ * Copyright (c) Linux Test Project, 2001-2015
+ * Copyright (c) 2023 Ioannis Bonatakis <ybonatakis@suse.com>
  */
 
- /*
-  * wait402 - check for ECHILD errno when using an illegal pid value
-  */
+/*\
+ * [Description]
+ *
+ * Check for ECHILD errno when call wait4(2) with an invalid pid value.
+ */
 
-#include "test.h"
-
-#include <errno.h>
-#define _USE_BSD
-#include <sys/types.h>
-#include <sys/resource.h>
+#include "tst_test.h"
 #include <sys/wait.h>
-#include <stdlib.h>
-#include <string.h>
 
-char *TCID = "wait402";
-int TST_TOTAL = 1;
+static pid_t pid_max;
 
-static void cleanup(void);
-static void setup(void);
-
-static long get_pid_max(void)
+static void run(void)
 {
-	long pid_max;
-
-	SAFE_FILE_SCANF(NULL, "/proc/sys/kernel/pid_max", "%ld", &pid_max);
-
-	return pid_max;
-}
-
-int main(int ac, char **av)
-{
-	int lc;
-	pid_t epid = get_pid_max() + 1;
-
-	int status = 1;
+	int status;
 	struct rusage rusage;
 
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		TEST(wait4(epid, &status, 0, &rusage));
-
-		if (TEST_RETURN == 0) {
-			tst_brkm(TFAIL, cleanup,
-				 "call failed to produce expected error - errno = %d - %s",
-				 TEST_ERRNO, strerror(TEST_ERRNO));
-		}
-
-		switch (TEST_ERRNO) {
-		case ECHILD:
-			tst_resm(TPASS,
-				 "received expected failure - errno = %d - %s",
-				 TEST_ERRNO, strerror(TEST_ERRNO));
-			break;
-		default:
-			tst_brkm(TFAIL, cleanup,
-				 "call failed to produce expected "
-				 "error - errno = %d - %s", TEST_ERRNO,
-				 strerror(TEST_ERRNO));
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_FAIL2(wait4(pid_max + 1, &status, 0, &rusage), ECHILD);
 }
 
 static void setup(void)
 {
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
+	SAFE_FILE_SCANF("/proc/sys/kernel/pid_max", "%d\n", &pid_max);
 }
 
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+};
