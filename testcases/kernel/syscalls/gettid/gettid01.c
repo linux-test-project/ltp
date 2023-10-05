d100a5fa08a9d5933c687e803eb282157ae96a18
Andrea Cervesato <andrea.cervesato@suse.com>
2023-10-05 16:43:34 +0200

    Rewrite gettid01 test
    The old test wasn't doing anything meaningful, but just checking that
    __NR_gettid syscall is working. In this test we check that
    tid == pid, in the case we are not inside a thread.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>
    Reviewed-by: Marius Kittler <mkittler@suse.de>
    
    [rpalethorpe: Re-added the original copyright]

--- testcases/kernel/syscalls/gettid/gettid01.c
+++ testcases/kernel/syscalls/gettid/gettid01.c
@@ -1,96 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Crackerjack Project
- *
  * Copyright (C) 2007-2008, Hitachi, Ltd.
- * Author(s): Takahiro Yasui <takahiro.yasui.mp@hitachi.com>,
- *            Yumiko Sugita <yumiko.sugita.yf@hitachi.com>,
- *            Satoshi Fujiwara <sa-fuji@sdl.hitachi.co.jp>
+ * Copyright (C) 2023 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+/*\
+ * [Description]
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * $Id: gettid01.c,v 1.5 2009/10/26 14:55:47 subrata_modak Exp $
- *
+ * This test checks if parent pid is equal to tid in single-threaded
+ * application.
  */
 
-/* Porting from Crackerjack to LTP is done
-   by Masatake YAMATO <yamato@redhat.com> */
+#include "tst_test.h"
+#include "lapi/syscalls.h"
 
-#include <sys/types.h>
-#include <linux/unistd.h>
-#include <errno.h>
-
-#include "test.h"
-
-void setup();
-void cleanup();
-
-char *TCID = "gettid01";
-
-int TST_TOTAL = 1;
-
-pid_t my_gettid(void)
+static void run(void)
 {
-	return (pid_t) syscall(__NR_gettid);
+	long pid, tid;
+
+	SAFE_FILE_LINES_SCANF("/proc/self/status", "Pid: %ld", &pid);
+	SAFE_FILE_LINES_SCANF("/proc/self/status", "Tgid: %ld", &tid);
+
+	if (pid != tid)
+		tst_brk(TBROK, "Test function has been moved inside a thread?");
+
+	TST_EXP_EQ_LI(tst_syscall(__NR_gettid), tst_syscall(__NR_getpid));
+	TST_EXP_EQ_LI(tst_syscall(__NR_gettid), pid);
 }
 
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	/*
-	 * The following loop checks looping state if -c option given
-	 */
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		TEST(my_gettid());
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL, "gettid() Failed, errno=%d: %s",
-				 TEST_ERRNO, strerror(TEST_ERRNO));
-		} else {
-			tst_resm(TPASS, "gettid() returned %ld",
-				 TEST_RETURN);
-		}
-	}
-
-	cleanup();
-	tst_exit();
-}
-
-/*
- * setup() - performs all ONE TIME setup for this test.
- */
-void setup(void)
-{
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	TEST_PAUSE;
-
-}
-
-/*
- * cleanup() - performs all ONE TIME cleanup for this test at
- *		completion or premature exit.
- */
-void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test_all = run,
+};
