88e828858bbdf2dc2d484ad2da2bb35defd4f96c
Andrea Cervesato <andrea.cervesato@suse.com>
2023-10-05 16:43:35 +0200

    Add gettid02 test
    This new test is checking if __NR_gettid syscall is properly handling a
    multi-threaded application, by assigning a different TID for each
    thread.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Marius Kittler <mkittler@suse.de>
    Reviewed-by: Richard Palethorpe <rpalethorpe@suse.com>

---
+++ testcases/kernel/syscalls/gettid/gettid02.c
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
+ */
+/*\
+ * [Description]
+ *
+ * This test spawns multiple threads, then check for each one of them if the
+ * parent ID is different AND if the thread ID is different from all the other
+ * spwaned threads.
+ */
+
+#include "tst_test.h"
+#include "lapi/syscalls.h"
+#include "tst_safe_pthread.h"
+
+#define THREADS_NUM 10
+
+static pid_t tids[THREADS_NUM];
+
+static void *threaded(void *arg)
+{
+	int i = *(int *)arg;
+	pid_t pid, tid;
+
+	pid = tst_syscall(__NR_getpid);
+	tid = tst_syscall(__NR_gettid);
+
+	TST_EXP_EXPR(pid != tid,
+		"parent ID (%d) differs from thread[%d] ID (%d)",
+		pid, i, tid);
+
+	return (void *)tst_syscall(__NR_gettid);
+}
+
+static void run(void)
+{
+	pthread_t thread;
+	int error = 0;
+
+	for (int i = 0; i < THREADS_NUM; i++) {
+		SAFE_PTHREAD_CREATE(&thread, NULL, threaded, &i);
+		SAFE_PTHREAD_JOIN(thread, (void **)&tids[i]);
+	}
+
+	for (int i = 0; i < THREADS_NUM; i++) {
+		for (int j = i + 1; j < THREADS_NUM; j++) {
+			if (tids[i] == tids[j]) {
+				tst_res(TINFO, "thread[%d] and thread[%d] have the same ID", i, j);
+				error = 1;
+			}
+		}
+	}
+
+	if (error)
+		tst_res(TFAIL, "Some threads have the same TID");
+	else
+		tst_res(TPASS, "All threads have a different TID");
+}
+
+static struct tst_test test = {
+	.test_all = run,
+};
