74e1e2e288373c8d5dab115c98a3be26ee7ba7a5
Petr Vorel <petr.vorel@gmail.com>
2023-10-11 18:08:20 +0200

    tst_ioctl: Cleanup the code
    * Use SAFE_{OPEN,CLOSE}() (simplification), -1 return code is removed.
    * Cleanup unused headers.
    * Include tst_fs.h, to keep signature checked during compilation.
    * Add missing license
    
    Link: https://lore.kernel.org/ltp/20231011160822.578637-2-pvorel@suse.cz/
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- lib/tst_ioctl.c
+++ lib/tst_ioctl.c
@@ -1,37 +1,30 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) Linux Test Project, 2019-2023
+ */
 
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <linux/fs.h>
 
 #define TST_NO_DEFAULT_MAIN
 
 #include "tst_test.h"
+#include "tst_fs.h"
 
 int tst_fibmap(const char *filename)
 {
-	/* test if FIBMAP ioctl is supported */
 	int fd, block = 0;
 
-	fd = open(filename, O_RDWR | O_CREAT, 0666);
-	if (fd < 0) {
-		tst_res(TWARN | TERRNO,
-			 "open(%s, O_RDWR | O_CREAT, 0666) failed", filename);
-		return -1;
-	}
+	fd = SAFE_OPEN(filename, O_RDWR | O_CREAT, 0666);
 
 	if (ioctl(fd, FIBMAP, &block)) {
 		tst_res(TINFO | TERRNO, "FIBMAP ioctl is NOT supported");
-		close(fd);
+		SAFE_CLOSE(fd);
 		return 1;
 	}
-	tst_res(TINFO, "FIBMAP ioctl is supported");
 
-	if (close(fd)) {
-		tst_res(TWARN | TERRNO, "close(fd) failed");
-		return -1;
-	}
+	tst_res(TINFO, "FIBMAP ioctl is supported");
+	SAFE_CLOSE(fd);
+
 	return 0;
 }
