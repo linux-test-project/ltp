603bac198043d03fecc55b5ee3e545fc2773370d
Edward Liaw <edliaw@google.com>
2023-10-18 01:50:16 +0000

    tst_kvercmp: Handle larger kernel version numbers
    Current implementation can only handle revision numbers up to 256.  Bump
    this up to 1024 as some revision numbers are in the 300s.
    
    Link: https://lore.kernel.org/ltp/20231018015016.1897021-1-edliaw@google.com/
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Tested-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Edward Liaw <edliaw@google.com>

--- lib/tst_kvercmp.c
+++ lib/tst_kvercmp.c
@@ -92,8 +92,8 @@ int tst_kvcmp(const char *cur_kver, int r1, int r2, int r3)
 		         cur_kver);
 	}
 
-	testver = (r1 << 16) + (r2 << 8) + r3;
-	currver = (a1 << 16) + (a2 << 8) + a3;
+	testver = (r1 << 20) + (r2 << 10) + r3;
+	currver = (a1 << 20) + (a2 << 10) + a3;
 
 	return currver - testver;
 }
