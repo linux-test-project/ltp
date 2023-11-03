3103485f6f28597e52b6b8fce34615c694d263e0
Petr Vorel <pvorel@suse.cz>
2023-11-03 12:45:54 +0100

    uevent02.c: Convert doc to docparse
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/uevents/uevent02.c
+++ testcases/kernel/uevents/uevent02.c
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (C) 2019 Cyril Hrubis <chrubis@suse.cz>
+ * Copyright (c) Linux Test Project, 2019-2023
  */
 
-/*
+/*\
+ * [Description]
+ *
  * Very simple uevent netlink socket test.
  *
  * We fork a child that listens for a kernel events while parents creates and
