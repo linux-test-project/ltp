dc4946ed8eeedc918a339eb64850a2ff2e4e378a
Andrea Cervesato <andrea.cervesato@suse.com>
2023-11-27 14:31:48 +0100

    Refactor mount04 using new LTP API
    Resolves: #1106
    Signed-off-by: Andrea Cervesato andrea.cervesato@suse.com
    Reviewed-by: Richard Palethorpe <io@richiejp.com>

--- testcases/kernel/syscalls/mount/mount04.c
+++ testcases/kernel/syscalls/mount/mount04.c
@@ -1,119 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) Wipro Technologies Ltd, 2002.  All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * DESCRIPTION
- *	Verify that mount(2) returns -1 and sets errno to  EPERM if the user
- *	is not the super-user.
+ *               Nirmala Devi Dhanasekar <nirmala.devi@wipro.com>
+ * Copyright (C) 2023 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
 
-#include <errno.h>
-#include <sys/mount.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+/*\
+ * [Description]
+ *
+ * Verify that mount(2) returns -1 and sets errno to EPERM if the user
+ * is not root.
+ */
+
+#include "tst_test.h"
 #include <pwd.h>
-#include "test.h"
-#include "safe_macros.h"
+#include <sys/mount.h>
 
-static void setup(void);
-static void cleanup(void);
-
-char *TCID = "mount04";
-
-#define DIR_MODE	S_IRWXU | S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP
-
-static char *mntpoint = "mntpoint";
-static const char *fs_type;
-static const char *device;
-
-int TST_TOTAL = 1;
-
-static void verify_mount(void)
-{
-
-	TEST(mount(device, mntpoint, fs_type, 0, NULL));
-
-	if (TEST_RETURN == -1) {
-		if (TEST_ERRNO == EPERM)
-			tst_resm(TPASS | TTERRNO, "mount() failed expectedly");
-		else
-			tst_resm(TFAIL | TTERRNO,
-			         "mount() was expected to fail with EPERM");
-		return;
-	}
-
-	if (TEST_RETURN == 0) {
-		tst_resm(TFAIL, "mount() succeeded unexpectedly");
-		if (tst_umount(mntpoint))
-			tst_brkm(TBROK, cleanup, "umount() failed");
-		return;
-	}
-
-	tst_resm(TFAIL | TTERRNO, "mount() returned %li", TEST_RETURN);
-}
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-		verify_mount();
-	}
-
-	cleanup();
-	tst_exit();
-}
-
-static void setup(void)
-{
-	char nobody_uid[] = "nobody";
-	struct passwd *ltpuser;
-
-	tst_sig(FORK, DEF_HANDLER, cleanup);
-
-	tst_require_root();
-
-	tst_tmpdir();
-
-	fs_type = tst_dev_fs_type();
-	device = tst_acquire_device(cleanup);
-
-	if (!device)
-		tst_brkm(TCONF, cleanup, "Failed to obtain block device");
-
-	tst_mkfs(cleanup, device, fs_type, NULL, NULL);
-
-	ltpuser = SAFE_GETPWNAM(cleanup, nobody_uid);
-	SAFE_SETEUID(cleanup, ltpuser->pw_uid);
-	SAFE_MKDIR(cleanup, mntpoint, DIR_MODE);
-
-	TEST_PAUSE;
-}
+#define MNTPOINT "mntpoint"
 
 static void cleanup(void)
 {
-	if (seteuid(0))
-		tst_resm(TWARN | TERRNO, "seteuid(0) failed");
-
-	if (device)
-		tst_release_device(device);
-
-	tst_rmdir();
+	if (tst_is_mounted(MNTPOINT))
+		SAFE_UMOUNT(MNTPOINT);
 }
+
+static void run(void)
+{
+	struct passwd *ltpuser;
+
+	ltpuser = SAFE_GETPWNAM("nobody");
+	SAFE_SETEUID(ltpuser->pw_uid);
+
+	TST_EXP_FAIL(
+		mount(tst_device->dev, MNTPOINT, tst_device->fs_type, 0, NULL),
+		EPERM,
+		"mount() failed expectedly for normal user"
+	);
+
+	if (tst_is_mounted(MNTPOINT))
+		SAFE_UMOUNT(MNTPOINT);
+}
+
+static struct tst_test test = {
+	.cleanup = cleanup,
+	.test_all = run,
+	.needs_root = 1,
+	.needs_device = 1,
+	.needs_tmpdir = 1,
+	.format_device = 1,
+	.mntpoint = MNTPOINT,
+};
