60dd23737255aa90b1f5dbdc57fc5fe98ba34fd6
Andrea Cervesato <andrea.cervesato@suse.com>
2023-11-28 14:02:17 +0100

    Refactor mount06 using new LTP API
    Resolves: #1108
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Richard Palethorpe <io@richiejp.com>

--- testcases/kernel/syscalls/mount/mount06.c
+++ testcases/kernel/syscalls/mount/mount06.c
@@ -1,34 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (c) 2013 Fujitsu Ltd.
- * Author: DAN LI <li.dan@cn.fujitsu.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ * Copyright (c) 2013 Fujitsu Ltd. Dan Li <li.dan@cn.fujitsu.com>
+ * Copyright (C) 2023 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
 
-/*
- *  DESCRIPTION
- *	Test for feature MS_MOVE of mount(2).
- *	"Move an existing mount point to the new location."
+/*\
+ * [Description]
+ *
+ * Test for feature MS_MOVE of mount, which moves an existing mount point to
+ * a new location.
  */
 
-#include <errno.h>
+#include "tst_test.h"
+#include <stdio.h>
 #include <sys/mount.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-#include "test.h"
-#include "safe_macros.h"
 
 #ifndef MS_MOVE
 #define MS_MOVE	8192
@@ -38,126 +23,88 @@
 #define MS_PRIVATE	(1 << 18)
 #endif
 
-#define MNTPOINT_SRC	"mnt_src"
-#define MNTPOINT_DES	"mnt_des"
-#define LINELENGTH	256
-#define DIR_MODE	(S_IRWXU | S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP)
+#define MNTPOINT_SRC "mntpoint1"
+#define MNTPOINT_DST "mntpoint2"
 
-static int ismount(char *mntpoint);
-static void setup(void);
-static void cleanup(void);
-
-char *TCID = "mount06";
-int TST_TOTAL = 1;
-
-static const char *fs_type;
-static const char *device;
-static char path_name[PATH_MAX];
+static char *tmppath;
 static char mntpoint_src[PATH_MAX];
-static char mntpoint_des[PATH_MAX];
-static int mount_flag;
-
-int main(int argc, char *argv[])
-{
-	int lc;
-
-	tst_parse_opts(argc, argv, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		SAFE_MOUNT(cleanup, device, mntpoint_src, fs_type, 0, NULL);
-
-		TEST(mount(mntpoint_src, mntpoint_des, fs_type, MS_MOVE, NULL));
-
-		if (TEST_RETURN != 0) {
-			tst_resm(TFAIL | TTERRNO, "mount(2) failed");
-		} else {
-
-			if (!ismount(mntpoint_src) && ismount(mntpoint_des))
-				tst_resm(TPASS, "move mount is ok");
-			else
-				tst_resm(TFAIL, "move mount does not work");
-
-			TEST(tst_umount(mntpoint_des));
-			if (TEST_RETURN != 0)
-				tst_brkm(TBROK | TTERRNO, cleanup,
-					 "umount(2) failed");
-		}
-	}
-
-	cleanup();
-	tst_exit();
-}
-
-int ismount(char *mntpoint)
-{
-	int ret = 0;
-	FILE *file;
-	char line[LINELENGTH];
-
-	file = fopen("/proc/mounts", "r");
-	if (file == NULL)
-		tst_brkm(TFAIL | TERRNO, NULL, "Open /proc/mounts failed");
-
-	while (fgets(line, LINELENGTH, file) != NULL) {
-		if (strstr(line, mntpoint) != NULL) {
-			ret = 1;
-			break;
-		}
-	}
-	fclose(file);
-	return ret;
-}
+static char mntpoint_dst[PATH_MAX];
+static char tstfiles_src[PATH_MAX];
+static char tstfiles_dst[PATH_MAX];
 
 static void setup(void)
 {
-	tst_require_root();
-
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
-
-	tst_tmpdir();
-
-	fs_type = tst_dev_fs_type();
-	device = tst_acquire_device(cleanup);
-
-	if (!device)
-		tst_brkm(TCONF, cleanup, "Failed to obtain block device");
-
-	tst_mkfs(cleanup, device, fs_type, NULL, NULL);
-
-	if (getcwd(path_name, sizeof(path_name)) == NULL)
-		tst_brkm(TBROK, cleanup, "getcwd failed");
+	tmppath = tst_get_tmpdir();
 
 	/*
 	 * Turn current dir into a private mount point being a parent
 	 * mount which is required by move mount.
 	 */
-	SAFE_MOUNT(cleanup, path_name, path_name, "none", MS_BIND, NULL);
+	SAFE_MOUNT(tmppath, tmppath, "none", MS_BIND, NULL);
+	SAFE_MOUNT("none", tmppath, "none", MS_PRIVATE, NULL);
 
-	mount_flag = 1;
+	snprintf(mntpoint_src, PATH_MAX, "%s/%s", tmppath, MNTPOINT_SRC);
+	snprintf(mntpoint_dst, PATH_MAX, "%s/%s", tmppath, MNTPOINT_DST);
 
-	SAFE_MOUNT(cleanup, "none", path_name, "none", MS_PRIVATE, NULL);
+	snprintf(tstfiles_src, PATH_MAX, "%s/%s/testfile", tmppath, MNTPOINT_SRC);
+	snprintf(tstfiles_dst, PATH_MAX, "%s/%s/testfile", tmppath, MNTPOINT_DST);
 
-	snprintf(mntpoint_src, PATH_MAX, "%s/%s", path_name, MNTPOINT_SRC);
-	snprintf(mntpoint_des, PATH_MAX, "%s/%s", path_name, MNTPOINT_DES);
-
-	SAFE_MKDIR(cleanup, mntpoint_src, DIR_MODE);
-	SAFE_MKDIR(cleanup, mntpoint_des, DIR_MODE);
-
-	TEST_PAUSE;
+	SAFE_MKDIR(mntpoint_dst, 0750);
 }
 
 static void cleanup(void)
 {
-	if (mount_flag && tst_umount(path_name) != 0)
-		tst_resm(TWARN | TERRNO, "umount(2) %s failed", path_name);
+	if (tst_is_mounted(mntpoint_src))
+		SAFE_UMOUNT(mntpoint_src);
 
-	if (device)
-		tst_release_device(device);
+	if (tst_is_mounted(mntpoint_dst))
+		SAFE_UMOUNT(mntpoint_dst);
 
-	tst_rmdir();
+	if (tst_is_mounted(tmppath))
+		SAFE_UMOUNT(tmppath);
+
+	SAFE_RMDIR(mntpoint_dst);
 }
+
+static void run(void)
+{
+	SAFE_MOUNT(tst_device->dev, mntpoint_src, tst_device->fs_type, 0, NULL);
+	SAFE_FILE_PRINTF(tstfiles_src, "LTP TEST FILE");
+
+	SAFE_MOUNT(mntpoint_src, mntpoint_dst, tst_device->fs_type, MS_MOVE, NULL);
+
+	TST_EXP_FAIL(
+		access(tstfiles_src, F_OK),
+		ENOENT,
+		"File %s doesn't exist",
+		tstfiles_src);
+
+	TST_EXP_PASS(
+		access(tstfiles_dst, F_OK),
+		"File %s exists :",
+		tstfiles_dst);
+
+	if (tst_is_mounted(mntpoint_src))
+		SAFE_UMOUNT(mntpoint_src);
+
+	if (tst_is_mounted(mntpoint_dst))
+		SAFE_UMOUNT(mntpoint_dst);
+}
+
+static struct tst_test test = {
+	.setup = setup,
+	.cleanup = cleanup,
+	.test_all = run,
+	.needs_root = 1,
+	.needs_device = 1,
+	.needs_tmpdir = 1,
+	.format_device = 1,
+	.mntpoint = MNTPOINT_SRC,
+	.all_filesystems = 1,
+	.skip_filesystems = (const char *const []){
+		"exfat",
+		"vfat",
+		"ntfs",
+		NULL
+	},
+};
