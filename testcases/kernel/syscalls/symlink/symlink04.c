8336592d7eefdca348d9278f228506e9a2046533
Yang Xu <xuyang2018.jy@fujitsu.com>
2023-11-29 01:12:44 -0500

    Refactor and merge symlink04/05 using new LTP API
    symlink04 and symlink05 has been merged together, testing
    the results of symlink(2) for the existence or non-existence
    of a file
    
    Reviewed-by: Xiao Yang <yangx.jy@fujitsu.com>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- testcases/kernel/syscalls/symlink/symlink04.c
+++ testcases/kernel/syscalls/symlink/symlink04.c
@@ -1,193 +1,64 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) Linux Test Project, 2001-2023
+ * Author: John George
  */
 
-/*
- * Test Name : symlink04
+/*\
+ * [Description]
  *
- * Test Description :
- *  Verify that, symlink will succeed to creat a symbolic link of an existing
- *  object name path.
- *
- * Expected Result:
- *  symlink() should return value 0 on success and symbolic link of an
- *  existing object should be created.
- *
- * Algorithm:
- *  Setup:
- *   Setup signal handling.
- *   Create temporary directory.
- *   Pause for SIGUSR1 if option specified.
- *
- *  Test:
- *   Loop if the proper options are given.
- *   Execute system call
- *   Check return code, if system call failed (return=-1)
- *   	Log the errno and Issue a FAIL message.
- *   Otherwise,
- *   	Verify the Functionality of system call
- *      if successful,
- *      	Issue Functionality-Pass message.
- *      Otherwise,
- *		Issue Functionality-Fail message.
- *  Cleanup:
- *   Print errno log and/or timing stats if options given
- *   Delete the temporary directory created.
- *
- * Usage:  <for command-line>
- *  symlink04 [-c n] [-e] [-f] [-i n] [-I x] [-p x] [-t]
- *	where,  -c n : Run n copies concurrently.
- *		-e   : Turn on errno logging.
- *		-f   : Turn off functionality Testing.
- *		-i n : Execute test n times.
- *		-I x : Execute test for x seconds.
- *		-P x : Pause for x seconds between iterations.
- *		-t   : Turn on syscall timing.
- *
- * History
- *	07/2001 John George
- *		-Ported
- *
- * Restrictions:
- *  None.
- *
+ * Check that a symbolic link may point to an existing file or
+ * to a nonexistent one.
  */
 
+#include <stdlib.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <string.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include "test.h"
-#include "safe_macros.h"
+#include "tst_test.h"
 
-#define  TESTFILE	"testfile"
-#define  SYMFILE	"slink_file"
-#define FILE_MODE       S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
+#define TESTFILE    "testfile"
+#define NONFILE     "noexistfile"
+#define SYMFILE     "slink_file"
 
-char *TCID = "symlink04";
-int TST_TOTAL = 1;
+static char *testfile;
+static char *nonfile;
 
-void setup();
-void cleanup();
+static struct tcase {
+	char **srcfile;
+} tcases[] = {
+	{&testfile},
+	{&nonfile},
+};
 
-int main(int ac, char **av)
+static void setup(void)
 {
-	struct stat stat_buf;	/* stat structure buffer */
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		tst_count = 0;
-
-		/*
-		 * Call symlink(2) to create a symlink of
-		 * testfile.
-		 */
-		TEST(symlink(TESTFILE, SYMFILE));
-
-		if (TEST_RETURN == -1) {
-			tst_resm(TFAIL, "symlink(%s, %s) Failed, errno=%d : %s",
-				 TESTFILE, SYMFILE, TEST_ERRNO,
-				 strerror(TEST_ERRNO));
-		} else {
-			/*
-			 * Get the symlink file status information
-			 * using lstat(2).
-			 */
-			if (lstat(SYMFILE, &stat_buf) < 0) {
-				tst_brkm(TFAIL, cleanup, "lstat(2) of "
-					 "%s failed, error:%d", SYMFILE,
-					 errno);
-			}
-
-			/* Check if the st_mode contains a link  */
-			if (!S_ISLNK(stat_buf.st_mode)) {
-				tst_resm(TFAIL,
-					 "symlink of %s doesn't exist",
-					 TESTFILE);
-			} else {
-				tst_resm(TPASS, "symlink(%s, %s) "
-					 "functionality successful",
-					 TESTFILE, SYMFILE);
-			}
-		}
-
-		/* Unlink the symlink file for next loop */
-		SAFE_UNLINK(cleanup, SYMFILE);
-		tst_count++;	/* incr TEST_LOOP counter */
-	}
-
-	cleanup();
-	tst_exit();
-
+	SAFE_TOUCH(TESTFILE, 0644, NULL);
 }
 
-/*
- * void
- * setup() - performs all ONE TIME setup for this test.
- *  Create a temporary directory and change directory to it.
- *  Create a test file under temporary directory and close it
- */
-void setup(void)
+static void verify_symlink(unsigned int i)
 {
-	int fd;			/* file handle for testfile */
+	struct tcase *tc = &tcases[i];
 
-	tst_sig(NOFORK, DEF_HANDLER, cleanup);
+	struct stat stat_buf;
 
-	/* Pause if that option was specified
-	 * TEST_PAUSE contains the code to fork the test with the -i option.
-	 * You want to make sure you do this before you create your temporary
-	 * directory.
-	 */
-	TEST_PAUSE;
+	TST_EXP_PASS(symlink(*tc->srcfile, SYMFILE));
 
-	tst_tmpdir();
+	SAFE_LSTAT(SYMFILE, &stat_buf);
 
-	/* creat/open a testfile */
-	if ((fd = open(TESTFILE, O_RDWR | O_CREAT, FILE_MODE)) == -1) {
-		tst_brkm(TBROK, cleanup,
-			 "open(%s, O_RDWR|O_CREAT, %#o) Failed, errno=%d : %s",
-			 TESTFILE, FILE_MODE, errno, strerror(errno));
-	}
+	if (!S_ISLNK(stat_buf.st_mode))
+		tst_res(TFAIL, "symlink of %s doesn't exist", *tc->srcfile);
 
-	/* Close the temporary file created above */
-	if (close(fd) == -1) {
-		tst_resm(TBROK, "close(%s) Failed, errno=%d : %s",
-			 TESTFILE, errno, strerror(errno));
-	}
+	SAFE_UNLINK(SYMFILE);
 }
 
-/*
- * void
- * cleanup() - performs all ONE TIME cleanup for this test at
- *             completion or premature exit.
- *  Remove the test directory and testfile created in the setup.
- */
-void cleanup(void)
-{
-
-	tst_rmdir();
-
-}
+static struct tst_test test = {
+	.tcnt = ARRAY_SIZE(tcases),
+	.setup = setup,
+	.test = verify_symlink,
+	.bufs = (struct tst_buffers []) {
+		{&testfile, .str = TESTFILE},
+		{&nonfile, .str = NONFILE},
+		{},
+	},
+	.needs_tmpdir = 1,
+};
