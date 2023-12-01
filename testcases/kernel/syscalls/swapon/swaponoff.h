c3e90ae9794fefa5daedf3d39697f9c4985471bc
Yang Xu <xuyang2018.jy@fujitsu.com>
2023-11-30 21:15:04 -0500

    swapon/swaponoff.h: Remove old kernel version switch
    Since the oldest support kernel version is 3.10, we can
    remove these switch directly.
    
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- testcases/kernel/syscalls/swapon/swaponoff.h
+++ testcases/kernel/syscalls/swapon/swaponoff.h
@@ -2,17 +2,7 @@
 #ifndef __SWAP_ON_OFF_H_
 #define __SWAP_ON_OFF_H_
 
-/*
- * Read swapon(2) / swapoff(2) for a full history lesson behind the value of
- * MAX_SWAPFILES.
- */
 #include <linux/version.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 18)
 #define MAX_SWAPFILES 30
-#elif LINUX_VERSION_CODE > KERNEL_VERSION(2, 4, 10)
-#define MAX_SWAPFILES 32
-#else
-#define MAX_SWAPFILES 8
-#endif
 
 #endif
