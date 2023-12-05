c4dfad1064a472480c8b63b52cf27518140ffc68
Petr Vorel <pvorel@suse.cz>
2023-12-05 13:13:13 +0100

    ksm_common.h: Fix checkpatch.pl style warnings
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/mem/ksm/ksm_common.h
+++ testcases/kernel/mem/ksm/ksm_common.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
  * Copyright (C) 2017  Red Hat, Inc.
  */
@@ -17,16 +17,15 @@ char *opt_sizestr, *opt_numstr, *opt_unitstr;
 static inline void parse_ksm_options(char *str_size, int *size,
 		char *str_num, int *num, char *str_unit, int *unit)
 {
-	if(tst_parse_int(str_size, size, 1, INT_MAX))
+	if (tst_parse_int(str_size, size, 1, INT_MAX))
 		tst_brk(TBROK, "Invalid size '%s'", str_size);
 
-	if(tst_parse_int(str_num, num, 3, INT_MAX))
+	if (tst_parse_int(str_num, num, 3, INT_MAX))
 		tst_brk(TBROK, "Invalid num '%s'", str_num);
 
-	if(tst_parse_int(str_unit, unit, 1, *size))
+	if (tst_parse_int(str_unit, unit, 1, *size))
 		tst_brk(TBROK, "Invalid unit '%s'", str_unit);
+
 	if (*size % *unit != 0)
-		tst_brk(TBROK,
-				"the remainder of division of size by unit is "
-				"not zero.");
+		tst_brk(TBROK, "the remainder of division of size by unit is not zero.");
 }
