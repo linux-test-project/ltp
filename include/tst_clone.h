e1537a44af20f146c3dd3499898798789e413927
Andrea Cervesato <andrea.cervesato@suse.com>
2023-12-05 14:54:21 +0100

    Remove ltp_clone_quick and ltp_clone_alloc
    Deprecate ltp_clone_quick() and ltp_clone_alloc() utilities since they
    have been removed during tests factory and replaced with SAFE_CLONE().
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- include/tst_clone.h
+++ include/tst_clone.h
@@ -39,10 +39,6 @@ int ltp_clone(unsigned long flags, int (*fn)(void *arg), void *arg,
 		size_t stack_size, void *stack);
 int ltp_clone7(unsigned long flags, int (*fn)(void *arg), void *arg,
 		size_t stack_size, void *stack, ...);
-int ltp_clone_alloc(unsigned long clone_flags, int (*fn)(void *arg),
-		void *arg, size_t stacksize);
-int ltp_clone_quick(unsigned long clone_flags, int (*fn)(void *arg),
-		void *arg);
 void *ltp_alloc_stack(size_t size);
 
 #define clone(...) (use_the_ltp_clone_functions__do_not_use_clone)
