0229ad5ceb24128ce9a272af029d222a94476e5e
Petr Vorel <pvorel@suse.cz>
2023-12-18 15:17:41 +0100

    oom: Use SPDX, update license
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/mem/oom/oom01.c
+++ testcases/kernel/mem/oom/oom01.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Out Of Memory (OOM)
  *
@@ -8,16 +9,7 @@
  * until all the available memory is consumed and OOM is triggered.
  *
  * Copyright (C) 2010-2017  Red Hat, Inc.
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
+ * Copyright (c) Linux Test Project, 2011-2023
  */
 
 #include <sys/types.h>
