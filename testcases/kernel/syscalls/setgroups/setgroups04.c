070b10e84865834499880ead1b4b49eaf8275abb
Petr Vorel <pvorel@suse.cz>
2024-01-03 02:04:51 +0100

    syscalls/setgroups04: Remove UCLINUX
    Signed-off-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- testcases/kernel/syscalls/setgroups/setgroups04.c
+++ testcases/kernel/syscalls/setgroups/setgroups04.c
@@ -84,8 +84,6 @@ GID_T groups_list[NGROUPS];
 void setup();			/* setup function for the test */
 void cleanup();			/* cleanup function for the test */
 
-#if !defined(UCLINUX)
-
 int main(int ac, char **av)
 {
 	int lc;
@@ -134,16 +132,6 @@ int main(int ac, char **av)
 	tst_exit();
 
 }
-
-#else
-
-int main(void)
-{
-	tst_resm(TINFO, "test is not available on uClinux");
-	tst_exit();
-}
-
-#endif /* if !defined(UCLINUX) */
 
 /*
  * setup()
