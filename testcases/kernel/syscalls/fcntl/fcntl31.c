95c2b9217c5ea735419f18fbcf22b99d9dece026
liufen <liufen@uniontech.com>
2024-01-18 14:09:15 +0800

    fcntl31:Fix resource release bugs.
    The orig_own_ex released in setownex_cleanup is a global variable,
    the actual test uses the local variable tst_own_ex, you need to assign
    tst_own_ex to orig_own_ex, otherwise clean an uninitialized global variable
    
    Signed-off-by: liufen <liufen@uniontech.com>

--- testcases/kernel/syscalls/fcntl/fcntl31.c
+++ testcases/kernel/syscalls/fcntl/fcntl31.c
@@ -211,6 +211,7 @@ static void setownex_tid_test(void)
 	}
 	test_set_and_get_sig(SIGUSR1, "F_GETOWN_EX, F_SETOWN_EX for thread ID");
 
+	memcpy(&orig_own_ex, &tst_own_ex, sizeof(struct f_owner_ex));
 	setownex_cleanup();
 }
 
@@ -229,6 +230,7 @@ static void setownex_pid_test(void)
 	test_set_and_get_sig(SIGUSR1,
 			     "F_GETOWN_EX, F_SETOWN_EX for process ID");
 
+	memcpy(&orig_own_ex, &tst_own_ex, sizeof(struct f_owner_ex));
 	setownex_cleanup();
 }
 
@@ -247,6 +249,7 @@ static void setownex_pgrp_test(void)
 	test_set_and_get_sig(SIGUSR1,
 			     "F_GETOWN_EX, F_SETOWN_EX for process group ID");
 
+	memcpy(&orig_own_ex, &tst_own_ex, sizeof(struct f_owner_ex));
 	setownex_cleanup();
 }
 #endif
