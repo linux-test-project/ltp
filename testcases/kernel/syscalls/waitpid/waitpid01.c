ed5ccf6c12ebddbdac501955fd7ca542448500dc
Martin Doucha <mdoucha@suse.cz>
2024-01-30 17:28:12 +0100

    waitpid01: Test all standard deadly signals
    Extend waitpid01 to test all standard signals that kill the target
    process unless caught. Also remove waitpid02 since testing SIGFPE
    in waitpid01 makes it redundant.
    
    Link: https://lore.kernel.org/ltp/20240130162813.20278-1-mdoucha@suse.cz/
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Martin Doucha <mdoucha@suse.cz>
    [ pvorel: Add SIGUSR* ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/waitpid/waitpid01.c
+++ testcases/kernel/syscalls/waitpid/waitpid01.c
@@ -5,25 +5,59 @@
  * Copyright (c) 2018 Cyril Hrubis <chrubis@suse.cz>
  */
 
-/*
- * Check that when a child kills itself by SIGALRM the waiting parent is
- * correctly notified.
+/*\
+ * [Description]
  *
- * Fork a child that raises(SIGALRM), the parent checks that SIGALRM was
- * returned.
+ * Check that when a child kills itself with one of the standard signals,
+ * the waiting parent is correctly notified.
+ *
+ * Fork a child that raise()s given signal, the parent checks that the signal
+ * was returned.
  */
 #include <stdlib.h>
 #include <sys/wait.h>
 #include "tst_test.h"
 
-static void run(void)
+static int testcase_list[] = {
+	SIGABRT,
+	SIGALRM,
+	SIGBUS,
+	SIGFPE,
+	SIGHUP,
+	SIGILL,
+	SIGINT,
+	SIGKILL,
+	SIGPIPE,
+	SIGPOLL,
+	SIGPROF,
+	SIGQUIT,
+	SIGSEGV,
+	SIGSYS,
+	SIGTERM,
+	SIGTRAP,
+	SIGUSR1,
+	SIGUSR1,
+	SIGVTALRM,
+	SIGXCPU,
+	SIGXFSZ
+};
+
+static void setup(void)
+{
+	struct rlimit lim = { 0 };
+
+	/* Disable core dumps */
+	SAFE_SETRLIMIT(RLIMIT_CORE, &lim);
+}
+
+static void run(unsigned int n)
 {
 	pid_t pid;
-	int status;
+	int status, sig = testcase_list[n];
 
 	pid = SAFE_FORK();
 	if (!pid) {
-		raise(SIGALRM);
+		raise(sig);
 		exit(0);
 	}
 
@@ -40,22 +74,24 @@ static void run(void)
 
 	if (!WIFSIGNALED(status)) {
 		tst_res(TFAIL, "WIFSIGNALED() not set in status (%s)",
-		        tst_strstatus(status));
+			tst_strstatus(status));
 		return;
 	}
 
 	tst_res(TPASS, "WIFSIGNALED() set in status");
 
-	if (WTERMSIG(status) != SIGALRM) {
-		tst_res(TFAIL, "WTERMSIG() != SIGALRM but %s",
-		        tst_strsig(WTERMSIG(status)));
+	if (WTERMSIG(status) != sig) {
+		tst_res(TFAIL, "WTERMSIG() != %s but %s", tst_strsig(sig),
+			tst_strsig(WTERMSIG(status)));
 		return;
 	}
 
-	tst_res(TPASS, "WTERMSIG() == SIGALRM");
+	tst_res(TPASS, "WTERMSIG() == %s", tst_strsig(sig));
 }
 
 static struct tst_test test = {
 	.forks_child = 1,
-	.test_all = run,
+	.setup = setup,
+	.test = run,
+	.tcnt = ARRAY_SIZE(testcase_list)
 };
