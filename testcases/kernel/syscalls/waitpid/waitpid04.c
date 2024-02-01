767ae231ba7d67cc5c0c528967dd3f9f355f8464
Martin Doucha <mdoucha@suse.cz>
2024-02-01 13:57:10 +0100

    waitpid04: Convert to new API
    Convert waitpid() error state checks to new API, fix bugs in the original
    test and add ESRCH subtest.
    
    Link: https://lore.kernel.org/ltp/20240201125713.1811-1-mdoucha@suse.cz/
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Martin Doucha <mdoucha@suse.cz>

--- testcases/kernel/syscalls/waitpid/waitpid04.c
+++ testcases/kernel/syscalls/waitpid/waitpid04.c
@@ -1,144 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2001
+ * Copyright (c) 2024 SUSE LLC <mdoucha@suse.cz>
  */
 
-/*
- * NAME
- *	waitpid04.c
+/*\
+ * [Description]
  *
- * DESCRIPTION
- *	test to check the error conditions in waitpid sys call
- *
- * USAGE:  <for command-line>
- *      waitpid04 [-c n] [-e] [-i n] [-I x] [-P x] [-t]
- *      where,  -c n : Run n copies concurrently.
- *              -e   : Turn on errno logging.
- *              -i n : Execute test n times.
- *              -I x : Execute test for x seconds.
- *              -P x : Pause for x seconds between iterations.
- *              -t   : Turn on syscall timing.
- *
- * History
- *	07/2001 John George
- *		-Ported
- *
- * Restrictions
- *	NONE
+ * Test to check the error conditions in waitpid() syscall.
  */
 
-#include <sys/signal.h>
-#include <sys/types.h>
+#include <signal.h>
 #include <sys/wait.h>
-#include <errno.h>
-#include "test.h"
+#include "tst_test.h"
 
-static void setup(void);
-static void cleanup(void);
+static struct testcase {
+	pid_t pid;
+	int flags;
+	int err;
+} testcase_list[] = {
+	{-1, 0, ECHILD},	/* Wait for any child when none exist */
+	{1, 0, ECHILD},		/* Wait for non-child process */
+	{-1, -1, EINVAL},	/* Invalid flags */
+	{INT_MIN, 0, ESRCH},	/* Wait for invalid process group */
+};
 
-char *TCID = "waitpid04";
-int TST_TOTAL = 1;
-
-#define INVAL_FLAG	-1
-
-static int flag, condition_number;
-
-int main(int ac, char **av)
+static void run(unsigned int n)
 {
-	int pid, status, ret;
+	const struct testcase *tc = testcase_list + n;
 
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	/* check for looping state if -i option is given */
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		/* reset tst_count in case we are looping */
-		tst_count = 0;
-
-		ret = waitpid(pid, &status, WNOHANG);
-		flag = 0;
-		condition_number = 1;
-		if (ret != -1) {
-			tst_resm(TFAIL, "condition %d test failed",
-				 condition_number);
-		} else {
-			if (errno != ECHILD) {
-				tst_resm(TFAIL, "waitpid() set invalid "
-					 "errno, expected ECHILD, got: %d",
-					 errno);
-			} else {
-				tst_resm(TPASS, "condition %d test passed",
-					 condition_number);
-			}
-		}
-		condition_number++;
-
-		if (FORK_OR_VFORK() == 0)
-			exit(0);
-
-		pid = 1;
-		ret = waitpid(pid, &status, WUNTRACED);
-		flag = 0;
-		if (ret != -1) {
-			tst_resm(TFAIL, "condition %d test failed",
-				 condition_number);
-		} else {
-			if (errno != ECHILD) {
-				tst_resm(TFAIL, "waitpid() set invalid "
-					 "errno, expected ECHILD, got: %d",
-					 errno);
-			} else {
-				tst_resm(TPASS, "condition %d test passed",
-					 condition_number);
-			}
-		}
-		condition_number++;
-
-		/* Option is Inval = INVAL_FLAG */
-		ret = waitpid(pid, &status, INVAL_FLAG);
-		flag = 0;
-		if (ret != -1) {
-			tst_resm(TFAIL, "condition %d test failed",
-				 condition_number);
-		} else {
-			if (errno != EINVAL) {
-				tst_resm(TFAIL, "waitpid() set invalid "
-					 "errno, expected EINVAL, got: %d",
-					 errno);
-			} else {
-				tst_resm(TPASS, "condition %d test passed",
-					 condition_number);
-			}
-		}
-		condition_number++;
-	}
-
-	cleanup();
-	tst_exit();
+	TST_EXP_FAIL2(waitpid(tc->pid, NULL, tc->flags), tc->err,
+		"waipid(%d, NULL, 0x%x)", tc->pid, tc->flags);
 }
 
-static void setup(void)
-{
-	TEST_PAUSE;
-}
-
-static void cleanup(void)
-{
-}
+static struct tst_test test = {
+	.test = run,
+	.tcnt = ARRAY_SIZE(testcase_list)
+};
