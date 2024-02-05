15841bafae6a7509435d9ffc30b168aa4b8ecd36
Martin Doucha <mdoucha@suse.cz>
2024-02-05 17:49:56 +0100

    waitpid01: Add subtests from waitpid05
    The waitpid05 test uses mostly the same test scenario as waitpid01
    except the child uses kill(getpid(), sig) instead of raise(sig)
    and the test also verifies that signals trigger coredump generation
    according to POSIX. Add both subtests to waitpid01 and drop the now
    obsolete waitpid05.
    
    Also reset the disposition of the signal to its default (fixes SIGFPE
    failure on Debian 6.6 on a real hardware).
    
    Link: https://lore.kernel.org/ltp/20240205164957.24665-1-mdoucha@suse.cz/
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Martin Doucha <mdoucha@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/waitpid/waitpid01.c
+++ testcases/kernel/syscalls/waitpid/waitpid01.c
@@ -11,35 +11,59 @@
  * Check that when a child kills itself with one of the standard signals,
  * the waiting parent is correctly notified.
  *
- * Fork a child that raise()s given signal, the parent checks that the signal
- * was returned.
+ * Fork a child that sends given signal to itself using raise() or kill(),
+ * the parent checks that the signal was returned.
  */
 #include <stdlib.h>
 #include <sys/wait.h>
 #include "tst_test.h"
 
-static int testcase_list[] = {
-	SIGABRT,
-	SIGALRM,
-	SIGBUS,
-	SIGFPE,
-	SIGHUP,
-	SIGILL,
-	SIGINT,
-	SIGKILL,
-	SIGPIPE,
-	SIGPOLL,
-	SIGPROF,
-	SIGQUIT,
-	SIGSEGV,
-	SIGSYS,
-	SIGTERM,
-	SIGTRAP,
-	SIGUSR1,
-	SIGUSR2,
-	SIGVTALRM,
-	SIGXCPU,
-	SIGXFSZ
+static int test_coredump;
+static struct testcase {
+	int sig;
+	int coredump;
+} testcase_list[] = {
+	{SIGABRT, 1},
+	{SIGALRM, 0},
+	{SIGBUS, 1},
+	{SIGFPE, 1},
+	{SIGHUP, 0},
+	{SIGILL, 1},
+	{SIGINT, 0},
+	{SIGKILL, 0},
+	{SIGPIPE, 0},
+	{SIGPOLL, 0},
+	{SIGPROF, 0},
+	{SIGQUIT, 1},
+	{SIGSEGV, 1},
+	{SIGSYS, 1},
+	{SIGTERM, 0},
+	{SIGTRAP, 1},
+	{SIGUSR1, 0},
+	{SIGUSR2, 0},
+	{SIGVTALRM, 0},
+	{SIGXCPU, 1},
+	{SIGXFSZ, 1}
+};
+
+static void child_raise(int sig)
+{
+	raise(sig);
+	exit(0);
+}
+
+static void child_kill(int sig)
+{
+	kill(getpid(), sig);
+	exit(0);
+}
+
+static struct testvariant {
+	void (*func)(int sig);
+	const char *desc;
+} variant_list[] = {
+	{child_raise, "raise(sig)"},
+	{child_kill, "kill(getpid(), sig)"}
 };
 
 static void setup(void)
@@ -47,19 +71,35 @@ static void setup(void)
 	struct rlimit lim = { 0 };
 
 	/* Disable core dumps */
-	SAFE_SETRLIMIT(RLIMIT_CORE, &lim);
+	SAFE_GETRLIMIT(RLIMIT_CORE, &lim);
+
+	if (lim.rlim_max) {
+		lim.rlim_cur = getpagesize();
+
+		if (lim.rlim_max > 0 && lim.rlim_max < lim.rlim_cur)
+			lim.rlim_cur = lim.rlim_max;
+
+		SAFE_SETRLIMIT(RLIMIT_CORE, &lim);
+		test_coredump = 1;
+	} else {
+		tst_res(TCONF, "Skipping coredump tests due to low rlimit");
+	}
+
+	tst_res(TINFO, "Testing child: %s", variant_list[tst_variant].desc);
 }
 
 static void run(unsigned int n)
 {
 	pid_t pid;
-	int status, sig = testcase_list[n];
+	int status;
+	const struct testcase *tc = testcase_list + n;
+
+	if (tc->sig != SIGKILL)
+		SAFE_SIGNAL(tc->sig, SIG_DFL);
 
 	pid = SAFE_FORK();
-	if (!pid) {
-		raise(sig);
-		exit(0);
-	}
+	if (!pid)
+		variant_list[tst_variant].func(tc->sig);
 
 	TST_EXP_PID_SILENT(waitpid(pid, &status, 0));
 	if (!TST_PASS)
@@ -80,18 +120,37 @@ static void run(unsigned int n)
 
 	tst_res(TPASS, "WIFSIGNALED() set in status");
 
-	if (WTERMSIG(status) != sig) {
-		tst_res(TFAIL, "WTERMSIG() != %s but %s", tst_strsig(sig),
+	if (WTERMSIG(status) != tc->sig) {
+		tst_res(TFAIL, "WTERMSIG() != %s but %s", tst_strsig(tc->sig),
 			tst_strsig(WTERMSIG(status)));
 		return;
 	}
 
-	tst_res(TPASS, "WTERMSIG() == %s", tst_strsig(sig));
+	tst_res(TPASS, "WTERMSIG() == %s", tst_strsig(tc->sig));
+
+	if (!test_coredump)
+		return;
+
+	if (!tc->coredump) {
+		if (WCOREDUMP(status))
+			tst_res(TFAIL, "Child unexpectedly dumped core");
+
+		return;
+	}
+
+	if (!WCOREDUMP(status)) {
+		tst_res(TFAIL, "Child did not dump core when expected");
+		return;
+	}
+
+	tst_res(TPASS, "Child dumped core as expected");
 }
 
 static struct tst_test test = {
 	.forks_child = 1,
 	.setup = setup,
 	.test = run,
-	.tcnt = ARRAY_SIZE(testcase_list)
+	.tcnt = ARRAY_SIZE(testcase_list),
+	.test_variants = ARRAY_SIZE(variant_list),
+	.needs_tmpdir = 1	/* for coredumps */
 };
