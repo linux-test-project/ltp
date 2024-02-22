18ab646923c70250b79624380b373171a8d2f32b
Andrea Manzini <Andrea Manzini andrea.manzini@suse.com>
2024-02-22 12:21:30 +0100

    Refactor gethostbyname_r01 with new API
    Closes: https://github.com/linux-test-project/ltp/pull/1135
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Andrea Manzini <andrea.manzini@suse.com>
    [ pvorel: Add glibc-git and CVE tags, formatting. ]
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/gethostbyname_r/gethostbyname_r01.c
+++ testcases/kernel/syscalls/gethostbyname_r/gethostbyname_r01.c
@@ -1,67 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  *   Copyright (c) 2015 Fujitsu Ltd.
  *   Author: Zeng Linggang <zenglg.jy@cn.fujitsu.com>
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
+ *   Copyright (C) 2024 SUSE LLC Andrea Manzini <andrea.manzini@suse.com>
  */
 
-/*
- * This is a test for glibc bug:
+/*\
+ * [Description]
+ *
+ * Test for GHOST: glibc vulnerability (CVE-2015-0235).
+ *
  * https://www.qualys.com/research/security-advisories/GHOST-CVE-2015-0235.txt
  */
 
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include "test.h"
+#include "tst_test.h"
 
 #define CANARY "in_the_coal_mine"
 
-static void setup(void);
-static void check_vulnerable(void);
-
-static struct {
+static struct
+{
 	char buffer[1024];
 	char canary[sizeof(CANARY)];
 } temp = {
 	"buffer",
 	CANARY,
 };
-
-char *TCID = "gethostbyname_r01";
-int TST_TOTAL = 1;
-
-int main(int ac, char **av)
-{
-	int lc;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-		check_vulnerable();
-	}
-
-	tst_exit();
-}
-
-static void setup(void)
-{
-	tst_sig(NOFORK, DEF_HANDLER, NULL);
-	TEST_PAUSE;
-}
 
 static void check_vulnerable(void)
 {
@@ -78,22 +41,24 @@ static void check_vulnerable(void)
 	 *                sizeof(*h_addr_ptrs) - 1;
 	 */
 	len = sizeof(temp.buffer) - 16 - 2 * sizeof(char *) - 1;
+
 	memset(name, '0', len);
 	name[len] = '\0';
-
 	retval = gethostbyname_r(name, &resbuf, temp.buffer,
 				 sizeof(temp.buffer), &result, &herrno);
 
-	if (strcmp(temp.canary, CANARY) != 0) {
-		tst_resm(TFAIL, "vulnerable");
-		return;
-	}
-
-	if (retval == ERANGE) {
-		tst_resm(TPASS, "not vulnerable");
-		return;
-	}
-
-	tst_resm(TFAIL, "gethostbyname_r() returned %s, expected ERANGE",
-		 tst_strerrno(retval));
+	/* has canary been overwritten? */
+	if (strcmp(temp.canary, CANARY) != 0)
+		tst_res(TFAIL, "GHOST CVE-2015-0235 vulnerable");
+	else
+		TST_EXP_EQ_LI(retval, ERANGE);
 }
+
+static struct tst_test test = {
+	.test_all = check_vulnerable,
+	.tags = (const struct tst_tag[]) {
+		{"glibc-git", "d5dd6189d506"},
+		{"CVE", "CVE-2015-0235"},
+		{}
+	}
+};
