9fa305fe37373452dafd88eb527130db0aeecf6e
Petr Vorel <pvorel@suse.cz>
2024-03-13 09:38:04 +0100

    send02: Fix typo in TINFO message
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/send/send02.c
+++ testcases/kernel/syscalls/send/send02.c
@@ -128,7 +128,7 @@ static void run(unsigned int n)
 	struct test_case *tc = testcase_list + n;
 	socklen_t len = sizeof(addr);
 
-	tst_res(TINFO, "Tesing %s", tc->name);
+	tst_res(TINFO, "Testing %s", tc->name);
 
 	tst_init_sockaddr_inet_bin(&addr, INADDR_LOOPBACK, 0);
 	listen_sock = SAFE_SOCKET(tc->domain, tc->type, tc->protocol);
