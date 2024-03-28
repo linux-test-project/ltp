7a595fc2366ac76525531fc94f56f0bbc46768b1
Andrea Manzini <andrea.manzini@suse.com>
2024-03-28 11:09:07 +0100

    refactor fallocate03 with new LTP API
    Signed-off-by: Andrea Manzini <andrea.manzini@suse.com>

--- testcases/kernel/syscalls/fallocate/fallocate03.c
+++ testcases/kernel/syscalls/fallocate/fallocate03.c
@@ -1,277 +1,92 @@
-/******************************************************************************
- *				 fallocate03.c
- *	Mon Dec 24 2007
- *	Copyright (c) International Business Machines  Corp., 2007
- *	Emali : sharyathi@in.ibm.com
- ******************************************************************************/
+// SPDX-License-Identifier: GPL-2.0-or-later
 
-/***************************************************************************
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-***************************************************************************/
+/*
+ * Copyright (c) International Business Machines  Corp., 2007
+ * Author: Sharyathi Nagesh <sharyathi@in.ibm.com> - Date: 24/12/2007
+ * Copyright (C) 2024 SUSE LLC Andrea Manzini <andrea.manzini@suse.com>
+ */
 
-/*****************************************************************************
- *
- *	OS Test - International Business Machines Corp. 2007.
- *
- *	TEST IDENTIFIER	: fallocate03
- *
- *	EXECUTED BY		: anyone
- *
- *	TEST TITLE		: fallocate
- *
- *	TEST CASE TOTAL	: 8
- *
- *	CPU ARCHITECTURES	: PPC,X86, X86_64
- *
- *	AUTHOR			: Sharyathi Nagesh
- *
- *	CO-PILOT			:
- *
- *	DATE STARTED		: 24/12/2007
- *
- *	TEST CASES
- *	(Working of fallocate on a sparse file)
- *
- *
- *	INPUT SPECIFICATIONS
- *		No input needs to be specified
- *		  fallocate() in-puts are specified through test_data
- *
- *	OUTPUT SPECIFICATIONS
- *		Output describing whether test cases passed or failed.
- *
- *	ENVIRONMENTAL NEEDS
- *		Test Needs to be executed on file system supporting ext4
- *   LTP {TMP} Needs to be set to such a folder
- *
- *	SPECIAL PROCEDURAL REQUIREMENTS
- *		None
- *
- *	DETAILED DESCRIPTION
- *		This is a test case for fallocate() system call.
- *		This test suite tests working of fallocate on sparse file
- *		fallocate is tested for different offsets
- *
- *		Total 8 Test Cases :-
- *			Different offsets with in a sparse file is tested
- *
- *	Setup:
- *		Setup file on which fallocate is to be called
- *		Set up a file with hole, created through lseek
- *
- *	Test:
- *		Loop if the proper options are given
- *		Execute system call
- *		Check return code, if system call failed
- *		TEST fails, PASS the test otherwise
- *
- *	Cleanup:
- *		Cleanup the temporary folder
- *
-*************************************************************************/
+/*\
+ * [Description]
+ * Test fallocate() on sparse file for different offsets, with a total of 8 test cases
+ */
 
 #define _GNU_SOURCE
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <endian.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <sys/types.h>		//Can be done with out
 #include <fcntl.h>
-#include <unistd.h>
-#include <inttypes.h>
-#include <sys/utsname.h>
-
-#include "test.h"
-#include "safe_macros.h"
-#include "lapi/fallocate.h"
+#include "tst_test.h"
 
 #define BLOCKS_WRITTEN 12
 #define HOLE_SIZE_IN_BLOCKS 12
 #define DEFAULT_MODE 0
-#define TRUE 0
 
-void get_blocksize(int);
-void populate_file();
-void file_seek(off_t);
-
-char *TCID = "fallocate03";
-char fname[255];
-int fd;
-struct test_data_t {
+static int fd;
+static struct test_case {
 	int mode;
 	loff_t offset;
-	loff_t len;
-	int error;
-} test_data[] = {
-	{
-	DEFAULT_MODE, 2, 1, TRUE}, {
-	DEFAULT_MODE, BLOCKS_WRITTEN, 1, TRUE}, {
-	DEFAULT_MODE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS / 2 - 1, 1, TRUE},
-	{
-	DEFAULT_MODE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS + 1, 1, TRUE}, {
-	FALLOC_FL_KEEP_SIZE, 2, 1, TRUE}, {
-	FALLOC_FL_KEEP_SIZE, BLOCKS_WRITTEN, 1, TRUE}, {
-	FALLOC_FL_KEEP_SIZE,
-		    BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS / 2 + 1, 1, TRUE}, {
-	FALLOC_FL_KEEP_SIZE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS + 2,
-		    1, TRUE}
+} test_cases[] = {
+	{DEFAULT_MODE, 2},
+	{DEFAULT_MODE, BLOCKS_WRITTEN},
+	{DEFAULT_MODE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS / 2 - 1},
+	{DEFAULT_MODE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS + 1},
+	{FALLOC_FL_KEEP_SIZE, 2},
+	{FALLOC_FL_KEEP_SIZE, BLOCKS_WRITTEN},
+	{FALLOC_FL_KEEP_SIZE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS / 2 + 1},
+	{FALLOC_FL_KEEP_SIZE, BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS + 2}
 };
 
-int TST_TOTAL = sizeof(test_data) / sizeof(test_data[0]);
-int block_size;
-int buf_size;
+static int block_size;
 
-/******************************************************************************
- * Performs all one time clean up for this test on successful
- * completion,  premature exit or  failure. Closes all temporary
- * files, removes all temporary directories exits the test with
- * appropriate return code by calling tst_exit() function.
-******************************************************************************/
-void cleanup(void)
+static void cleanup(void)
 {
-	/* Close all open file descriptors. */
-	if (close(fd) == -1)
-		tst_resm(TWARN | TERRNO, "close(%s) failed", fname);
-
-	tst_rmdir();
-
+	if (fd)
+		SAFE_CLOSE(fd);
 }
 
-/*****************************************************************************
- * Performs all one time setup for this test. This function is
- * used to create temporary dirs and temporary files
- * that may be used in the course of this test
- ******************************************************************************/
-
-void setup(void)
-{
-	/* Create temporary directories */
-	TEST_PAUSE;
-
-	tst_tmpdir();
-
-	sprintf(fname, "tfile_sparse_%d", getpid());
-	fd = SAFE_OPEN(cleanup, fname, O_RDWR | O_CREAT, 0700);
-	get_blocksize(fd);
-	populate_file();
-	file_seek(BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS);	/* create holes */
-	populate_file();
-	file_seek(0);		/* Rewind */
-}
-
-/*****************************************************************************
- * Gets the block size for the file system
- ******************************************************************************/
-void get_blocksize(int fd)
-{
-	struct stat file_stat;
-
-	if (fstat(fd, &file_stat) < 0)
-		tst_resm(TFAIL | TERRNO,
-			 "fstat failed while getting block_size");
-
-	block_size = (int)file_stat.st_blksize;
-	buf_size = block_size;
-}
-
-/*****************************************************************************
- * Create a Hole in the file
- ******************************************************************************/
-void file_seek(off_t offset)
-{
-	offset *= block_size;
-	lseek(fd, offset, SEEK_SET);
-}
-
-/*****************************************************************************
- * Writes data into the file
- ******************************************************************************/
-void populate_file(void)
+static void write_data_to_file(int buf_size)
 {
 	char buf[buf_size + 1];
-	int index;
-	int blocks;
-	int data;
-	for (blocks = 0; blocks < BLOCKS_WRITTEN; blocks++) {
-		for (index = 0; index < buf_size; index++)
-			buf[index] = 'A' + (index % 26);
+
+	for (int blocks = 0; blocks < BLOCKS_WRITTEN; blocks++) {
+		for (int i = 0; i < buf_size; i++)
+			buf[i] = 'A' + (i % 26);
 		buf[buf_size] = '\0';
-		if ((data = write(fd, buf, buf_size)) < 0)
-			tst_brkm(TBROK | TERRNO, cleanup,
-				 "Unable to write to %s", fname);
+		SAFE_WRITE(SAFE_WRITE_ALL, fd, buf, buf_size);
 	}
 }
 
-/*****************************************************************************
- * Main function that calls the system call with the  appropriate parameters
- ******************************************************************************/
-/* ac: number of command line parameters */
-/* av: pointer to the array of the command line parameters */
-int main(int ac, char **av)
+static void setup(void)
 {
-	int test_index = 0;
-	int lc;
+	char fname[NAME_MAX];
 
-	/***************************************************************
-	 * parse standard options
-	***************************************************************/
-	tst_parse_opts(ac, av, NULL, NULL);
+	sprintf(fname, "tfile_sparse_%d", getpid());
+	fd = SAFE_OPEN(fname, O_RDWR | O_CREAT, 0700);
+	struct stat file_stat;
 
-	/* perform global test setup, call setup() function */
-	setup();
+	SAFE_FSTAT(fd, &file_stat);
+	block_size = (int)file_stat.st_blksize;
 
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		/* reset tst_count in case we are looping */
-		tst_count = 0;
-		for (test_index = 0; test_index < TST_TOTAL; test_index++) {
-			TEST(fallocate
-			     (fd, test_data[test_index].mode,
-			      test_data[test_index].offset * block_size,
-			      test_data[test_index].len * block_size));
-
-			/* check return code */
-			if (TEST_RETURN != test_data[test_index].error) {
-				if (TEST_ERRNO == EOPNOTSUPP
-				    || TEST_ERRNO == ENOSYS) {
-					tst_brkm(TCONF, cleanup,
-						 "fallocate system call is not implemented");
-				}
-				tst_resm(TFAIL | TTERRNO,
-					 "fallocate(%s, %d, %" PRId64 ", %"
-					 PRId64 ") failed", fname,
-					 test_data[test_index].mode,
-					 test_data[test_index].offset *
-					 block_size,
-					 test_data[test_index].len *
-					 block_size);
-			} else {
-				tst_resm(TPASS,
-					 "fallocate(%s, %d, %" PRId64
-					 ", %" PRId64 ") returned %ld",
-					 fname,
-					 test_data[test_index].mode,
-					 test_data[test_index].offset *
-					 block_size,
-					 test_data[test_index].len *
-					 block_size, TEST_RETURN);
-			}
-		}
-	}
-
-	cleanup();
-	tst_exit();
+	write_data_to_file(block_size);
+	SAFE_LSEEK(fd, block_size * (BLOCKS_WRITTEN + HOLE_SIZE_IN_BLOCKS), SEEK_SET);
+	write_data_to_file(block_size);
+	SAFE_LSEEK(fd, 0, SEEK_SET);
 }
+
+
+static void verify_fallocate(unsigned int nr)
+{
+	struct test_case *tc = &test_cases[nr];
+
+	TST_EXP_PASS(
+		fallocate(fd, tc->mode, tc->offset*block_size, block_size),
+		"fallocate(fd, %s, %ld, %d)", tc->mode?"FALLOC_FL_KEEP_SIZE":"DEFAULT_MODE",
+		tc->offset*block_size, block_size);
+}
+
+static struct tst_test test = {
+	.setup = setup,
+	.cleanup = cleanup,
+	.test = verify_fallocate,
+	.needs_tmpdir = 1,
+	.tcnt = ARRAY_SIZE(test_cases)
+};
