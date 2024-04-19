7c8997c06e9bfb57ca6f9c57fe32b1075c7dd866
Yang Xu <xuyang2018.jy@fujitsu.com>
2024-04-19 04:25:49 -0400

    gethostname: Add negative test for gethostname
    Add negative cases for gethostname(), when errno is ENAMETOOLONG
    
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>
    Reviewed-by: Avinesh Kumar <akumar@suse.de>
    Reviewed-by: Li Wang <liwang@redhat.com>

---
+++ testcases/kernel/syscalls/gethostname/gethostname02.c
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (c) 2024 FUJITSU LIMITED. All Rights Reserved.
+ * Author: Yang Xu <xuyang2018.jy@fujitsu.com>
+ */
+
+/*\
+ * [Description]
+ *
+ * Verify that gethostname(2) fails with
+ *
+ * - ENAMETOOLONG when len is smaller than the actual size
+ */
+
+#include "tst_test.h"
+
+static void verify_gethostname(void)
+{
+	char hostname[HOST_NAME_MAX];
+	int real_length;
+
+	SAFE_GETHOSTNAME(hostname, sizeof(hostname));
+	real_length = strlen(hostname);
+
+	TST_EXP_FAIL(gethostname(hostname, real_length - 1), ENAMETOOLONG,
+		"len is smaller than the actual size");
+}
+
+static struct tst_test test = {
+	.test_all = verify_gethostname,
+};
