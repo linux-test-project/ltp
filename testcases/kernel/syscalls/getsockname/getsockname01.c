1c0bf86a4d66f0f9b6b90c2ec7f2c89c704c634a
Yang Xu <xuyang2018.jy@fujitsu.com>
2024-04-22 04:35:28 -0400

    getsockname01: Add case for errno EINVAL
    Currently there is no case for EINVAL, so a new case is added.
    
    Link: https://lore.kernel.org/ltp/20240422083528.28355-1-xuyang2018.jy@fujitsu.com/
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/getsockname/getsockname01.c
+++ testcases/kernel/syscalls/getsockname/getsockname01.c
@@ -12,6 +12,7 @@
  * - EBADF on a not open file
  * - ENOTSOCK on a file descriptor not linked to a socket
  * - EFAULT on invalid socket buffer o invalid socklen
+ * - EINVALI on an invalid addrlen
  */
 
 #include "tst_test.h"
@@ -19,6 +20,7 @@
 static struct sockaddr_in sin0, fsin1;
 static int sock_null, sock_bind, sock_fake;
 static socklen_t sinlen;
+static socklen_t sininval = -1;
 
 static struct test_case {
 	int *sock;
@@ -37,6 +39,8 @@ static struct test_case {
 		.experrno = EFAULT, "invalid aligned salen"},
 	{ .sock = &sock_bind, .sockaddr = &fsin1, .addrlen = (socklen_t *) 1,
 		.experrno = EFAULT, "invalid unaligned salen"},
+	{ .sock = &sock_bind, .sockaddr = &fsin1, .addrlen = &sininval,
+		.experrno = EINVAL, "invalid socklen"},
 };
 
 static void check_getsockname(unsigned int nr)
