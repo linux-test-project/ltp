6ab10dec5add5fc8111f4f78b4ef573667537978
Petr Vorel <pvorel@suse.cz>
2024-05-21 11:01:31 +0200

    setsockopt03: Fix typo in docs
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/setsockopt/setsockopt03.c
+++ testcases/kernel/syscalls/setsockopt/setsockopt03.c
@@ -14,7 +14,7 @@
  *
  * The original vulnerability was present in the 32-bit compatibility system
  * call, so the test should be compiled with -m32 and run on a 64-bit kernel.
- * For simplicities sake the test requests root privliges instead of creating
+ * For simplicities sake the test requests root privileges instead of creating
  * a user namespace.
  */
 
