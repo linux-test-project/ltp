linux-test-project/ltp

commit e4410501dcb70fa7133a04b5deb848fad726ddc1

Added three more SAFE_* macros for landlock sandbox
- SAFE_LANDLOCK_CREATE_RULESET
- SAFE_LANDLOCK_ADD_RULE
- SAFE_LANDLOCK_RESTRICT_SELF

Link: https://lore.kernel.org/ltp/20240711-landlock-v3-3-c7b0e9edf9b0@suse.com/
Reviewed-by: Li Wang <liwang@redhat.com>
Reviewed-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

include/lapi/landlock.h
@@ -12,6 +12,8 @@
 # include <linux/landlock.h>
 #endif
 
+#include "lapi/syscalls.h"
+
 #ifndef HAVE_STRUCT_LANDLOCK_RULESET_ATTR
 struct landlock_ruleset_attr
 {
@@ -119,5 +121,64 @@ struct landlock_net_port_attr
 #ifndef LANDLOCK_ACCESS_NET_CONNECT_TCP
 # define LANDLOCK_ACCESS_NET_CONNECT_TCP	(1ULL << 1)
 #endif
+
+static inline int safe_landlock_create_ruleset(const char *file, const int lineno,
+	const struct landlock_ruleset_attr *attr,
+	size_t size , uint32_t flags)
+{
+	int rval;
+
+	rval = tst_syscall(__NR_landlock_create_ruleset, attr, size, flags);
+	if (rval == -1) {
+		tst_brk_(file, lineno, TBROK | TERRNO,
+			"landlock_create_ruleset(%p, %lu, %u)",
+			attr, size, flags);
+	}
+
+	return rval;
+}
+
+static inline int safe_landlock_add_rule(const char *file, const int lineno,
+	int ruleset_fd, enum landlock_rule_type rule_type,
+	const void *rule_attr, uint32_t flags)
+{
+	int rval;
+
+	rval = tst_syscall(__NR_landlock_add_rule,
+		ruleset_fd, rule_type, rule_attr, flags);
+
+	if (rval == -1) {
+		tst_brk_(file, lineno, TBROK | TERRNO,
+			"landlock_add_rule(%d, %d, %p, %u)",
+			ruleset_fd, rule_type, rule_attr, flags);
+	}
+
+	return rval;
+}
+
+static inline int safe_landlock_restrict_self(const char *file, const int lineno,
+	int ruleset_fd, int flags)
+{
+	int rval;
+
+	rval = tst_syscall(__NR_landlock_restrict_self, ruleset_fd, flags);
+	if (rval == -1) {
+		tst_brk_(file, lineno, TBROK | TERRNO,
+			"landlock_restrict_self(%d, %u)",
+			ruleset_fd, flags);
+	}
+
+	return rval;
+}
+
+#define SAFE_LANDLOCK_CREATE_RULESET(attr, size, flags) \
+	safe_landlock_create_ruleset(__FILE__, __LINE__, (attr), (size), (flags))
+
+#define SAFE_LANDLOCK_ADD_RULE(ruleset_fd, rule_type, rule_attr, flags) \
+	safe_landlock_add_rule(__FILE__, __LINE__, \
+		(ruleset_fd), (rule_type), (rule_attr), (flags))
+
+#define SAFE_LANDLOCK_RESTRICT_SELF(ruleset_fd, flags) \
+	safe_landlock_restrict_self(__FILE__, __LINE__, (ruleset_fd), (flags))
 
 #endif

commit 2433c503a6657273da895e36adda73942452c482

landlock.h: Fix printf format for size_t
It fixes warning on 32 bit:

include/lapi/landlock.h:134:56: warning: format '%lu' expects argument
of type 'long unsigned int', but argument 6 has type 'size_t' {aka
'unsigned int'}

Fixes: e4410501dc ("Added three more SAFE_* macros for landlock sandbox")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

include/lapi/landlock.h
@@ -131,7 +131,7 @@ static inline int safe_landlock_create_ruleset(const char *file, const int linen
 	rval = tst_syscall(__NR_landlock_create_ruleset, attr, size, flags);
 	if (rval == -1) {
 		tst_brk_(file, lineno, TBROK | TERRNO,
-			"landlock_create_ruleset(%p, %lu, %u)",
+			"landlock_create_ruleset(%p, %zi, %u)",
 			attr, size, flags);
 	}
 