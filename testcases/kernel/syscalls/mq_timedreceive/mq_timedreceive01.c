37a806c8e5579f0ef4135c91a5ec483378e08163
Petr Vorel <pvorel@suse.cz>
2024-11-12 18:18:31 +0100

    mq_timedreceive01: Workaround segfault on libc variant on 32 bit
    EFAULT test segfaults on newer kernels (e.g. 6.4) on libc variant on
    32bit.  Similarly to 1d4d5a0750 use typical LTP workaround to test by
    forked child + checking the terminating signal.
    
    Link: https://lore.kernel.org/ltp/20241112171831.156440-2-pvorel@suse.cz/
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- testcases/kernel/syscalls/mq_timedreceive/mq_timedreceive01.c
+++ testcases/kernel/syscalls/mq_timedreceive/mq_timedreceive01.c
@@ -129,32 +129,15 @@ static void setup(void)
 	setup_common();
 }
 
-static void do_test(unsigned int i)
+static void verify_mqt_receive(unsigned int i, pid_t pid)
 {
 	struct time64_variants *tv = &variants[tst_variant];
 	const struct test_case *tc = &tcase[i];
+	size_t len = MAX_MSGSIZE;
+	char rmsg[MAX_MSGSIZE];
+	void *abs_timeout;
 	unsigned int j;
 	unsigned int prio;
-	size_t len = MAX_MSGSIZE;
-	char rmsg[len];
-	pid_t pid = -1;
-	void *abs_timeout;
-
-	tst_ts_set_sec(&ts, tc->tv_sec);
-	tst_ts_set_nsec(&ts, tc->tv_nsec);
-
-	if (tc->signal)
-		pid = set_sig(tc->rq, tv->clock_gettime);
-
-	if (tc->timeout)
-		set_timeout(tc->rq, tv->clock_gettime);
-
-	if (tc->send) {
-		if (tv->mqt_send(*tc->fd, smsg, tc->len, tc->prio, NULL) < 0) {
-			tst_res(TFAIL | TTERRNO, "mq_timedsend() failed");
-			return;
-		}
-	}
 
 	if (tc->invalid_msg)
 		len -= 1;
@@ -206,6 +189,60 @@ static void do_test(unsigned int i)
 
 	tst_res(TPASS, "mq_timedreceive() returned %ld, priority %u, length: %zu",
 			TST_RET, prio, len);
+}
+
+static void test_bad_addr(unsigned int i)
+{
+	struct time64_variants *tv = &variants[tst_variant];
+	pid_t pid;
+	int status;
+
+	pid = SAFE_FORK();
+	if (!pid) {
+		verify_mqt_receive(i, pid);
+		exit(0);
+	}
+
+	SAFE_WAITPID(pid, &status, 0);
+
+	if (WIFEXITED(status) && !WEXITSTATUS(status))
+		return;
+
+	if (tv->ts_type == TST_LIBC_TIMESPEC &&
+		WIFSIGNALED(status) && WTERMSIG(status) == SIGSEGV) {
+		tst_res(TPASS, "Child killed by expected signal");
+		return;
+	}
+
+	tst_res(TFAIL, "Child %s", tst_strstatus(status));
+}
+
+static void do_test(unsigned int i)
+{
+	struct time64_variants *tv = &variants[tst_variant];
+	const struct test_case *tc = &tcase[i];
+	pid_t pid = -1;
+
+	tst_ts_set_sec(&ts, tc->tv_sec);
+	tst_ts_set_nsec(&ts, tc->tv_nsec);
+
+	if (tc->bad_ts_addr) {
+		test_bad_addr(i);
+		return;
+	}
+
+	if (tc->signal)
+		pid = set_sig(tc->rq, tv->clock_gettime);
+
+	if (tc->timeout)
+		set_timeout(tc->rq, tv->clock_gettime);
+
+	if (tc->send && tv->mqt_send(*tc->fd, smsg, tc->len, tc->prio, NULL) < 0) {
+		tst_res(TFAIL | TTERRNO, "mq_timedsend() failed");
+		return;
+	}
+
+	verify_mqt_receive(i, pid);
 }
 
 static struct tst_test test = {
