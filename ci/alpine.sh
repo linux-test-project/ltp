6aa211517d9bc4a485e9521bbe3324d501fd9dea
Petr Vorel <petr.vorel@gmail.com>
2024-11-15 00:46:08 +0100

    ci/alpine.sh: Update list of incompatible tests
    rt_tgsigqueueinfo01.c has been ported in previous commit.
    
    Link: https://lore.kernel.org/ltp/20241114234839.1680432-4-petr.vorel@gmail.com/
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- ci/alpine.sh
+++ ci/alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eux
 # SPDX-License-Identifier: GPL-2.0-or-later
-# Copyright (c) 2019-2022 Petr Vorel <petr.vorel@gmail.com>
+# Copyright (c) 2019-2024 Petr Vorel <petr.vorel@gmail.com>
 
 apk update
 
@@ -34,7 +34,6 @@ echo "WARNING: remove unsupported tests (until they're fixed)"
 cd $(dirname $0)/..
 rm -rfv \
 	testcases/kernel/syscalls/fmtmsg/fmtmsg01.c \
-	testcases/kernel/syscalls/rt_tgsigqueueinfo/rt_tgsigqueueinfo01.c \
 	testcases/kernel/syscalls/timer_create/timer_create01.c \
 	testcases/kernel/syscalls/timer_create/timer_create03.c
 
