d2d9c530af8e7758c19feb461d1fbff6c4470869
Andrea Cervesato <andrea.cervesato@suse.com>
2024-11-15 10:07:39 +0100

    Refactor input02 test
    Reviewed-by: Li Wang <liwang@redhat.com>
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- testcases/kernel/input/input02.c
+++ testcases/kernel/input/input02.c
@@ -1,106 +1,78 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) 2015 Cedric Hnyda <chnyda@suse.com>
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it would be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write the Free Software Foundation,
- * Inc.,  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Copyright (C) 2024 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
 
- /*
-  *  Create a virtual device (mouse), send events to /dev/uinput
-  *  and check that the events are not received in /dev/input/eventX
-  *  because the device is grabbed by another process
-  */
+/*\
+ * [Description]
+ *
+ * Verify that /dev/input/eventX won't receive any event sent from a virtual
+ * device, that in our case is a mouse, when the event device has been grabbed
+ * by an another process.
+ */
 
-#include <linux/input.h>
+#include "input_common.h"
 
-#include "test.h"
-#include "safe_macros.h"
-#include "lapi/fcntl.h"
-#include "input_helper.h"
+#define MOVE_X 10
+#define MOVE_Y 1
 
-#define NB_TEST 20
+static int fd_send = -1;
+static int fd_recv = -1;
 
-static void setup(void);
-static void send_information(void);
-static void cleanup(void);
-
-static int fd;
-static int fd2;
-
-char *TCID = "input02";
-
-int main(int ac, char **av)
+static void send_events(void)
 {
-	int lc;
-	int pid;
+	int fd;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	fd = open_event_device();
 
-	setup();
+	SAFE_IOCTL(fd, EVIOCGRAB, 1);
+	tst_res(TINFO, "The virtual device was grabbed");
 
-	for (lc = 0; TEST_LOOPING(lc); ++lc) {
-		pid = tst_fork();
+	send_relative_move(fd_send, MOVE_X, MOVE_Y);
 
-		fd2 = open_device();
+	TST_CHECKPOINT_WAKE_AND_WAIT(0);
 
-		switch (pid) {
-		case 0:
-			send_information();
-			exit(0);
-		case -1:
-			tst_brkm(TBROK | TERRNO, cleanup, "fork() failed");
-		default:
-			if (no_events_queued(fd2, 0))
-				tst_resm(TPASS, "No data received in eventX");
-			else
-				tst_resm(TFAIL, "Data received in eventX");
-			SAFE_CLOSE(NULL, fd2);
-		break;
-		}
+	SAFE_CLOSE(fd);
+}
 
-		SAFE_WAITPID(NULL, pid, NULL, 0);
+static void run(void)
+{
+	if (!SAFE_FORK()) {
+		send_events();
+		exit(0);
 	}
 
-	cleanup();
-	tst_exit();
+	TST_CHECKPOINT_WAIT(0);
+
+	verify_no_events_queued(fd_recv);
+
+	TST_CHECKPOINT_WAKE(0);
 }
 
 static void setup(void)
 {
-	tst_require_root();
+	fd_send = open_uinput();
+	setup_mouse_events(fd_send);
+	create_input_device(fd_send);
 
-	fd = open_uinput();
-	setup_mouse_events(fd);
-	create_device(fd);
-}
-
-static void send_information(void)
-{
-	int nb;
-
-	SAFE_IOCTL(NULL, fd2, EVIOCGRAB, 1);
-	tst_resm(TINFO, "The virtual device was grabbed");
-
-	for (nb = 0; nb < NB_TEST; ++nb) {
-		send_rel_move(fd, 10, 1);
-		usleep(1000);
-	}
-
-	SAFE_CLOSE(NULL, fd2);
+	fd_recv = open_event_device();
 }
 
 static void cleanup(void)
 {
-	destroy_device(fd);
+	if (fd_send != -1)
+		destroy_input_device(fd_send);
+
+	if (fd_recv != -1)
+		SAFE_CLOSE(fd_recv);
 }
+
+static struct tst_test test = {
+	.test_all = run,
+	.setup = setup,
+	.cleanup = cleanup,
+	.forks_child = 1,
+	.needs_root = 1,
+	.needs_checkpoints = 1,
+};
