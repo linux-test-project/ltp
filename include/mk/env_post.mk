4c68d720b318b1fad52ba118e5873c4c2f25e000
Andrea Cervesato <andrea.cervesato@suse.com>
2025-01-15 12:10:57 +0100

    Ignore files paths change during make check
    checkpatch.pl script doesn't need to be run with FILE_PATH_CHANGES,
    since it's used to sync with MAINTAINERS file, not provided by LTP.
    Its usage may lead to a false positive during `make check` command.
    
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>

--- include/mk/env_post.mk
+++ include/mk/env_post.mk
@@ -90,7 +90,7 @@ CHECK_TARGETS			?= $(addprefix check-,$(notdir $(patsubst %.c,%,$(sort $(wildcar
 CHECK_TARGETS			:= $(filter-out $(addprefix check-, $(FILTER_OUT_MAKE_TARGETS)), $(CHECK_TARGETS))
 CHECK_HEADER_TARGETS		?= $(addprefix check-,$(notdir $(sort $(wildcard $(abs_srcdir)/*.h))))
 CHECK				?= $(abs_top_srcdir)/tools/sparse/sparse-ltp
-CHECK_NOFLAGS			?= $(abs_top_srcdir)/scripts/checkpatch.pl -f --no-tree --terse --no-summary --ignore CONST_STRUCT,VOLATILE,SPLIT_STRING
+CHECK_NOFLAGS			?= $(abs_top_srcdir)/scripts/checkpatch.pl -f --no-tree --terse --no-summary --ignore CONST_STRUCT,VOLATILE,SPLIT_STRING,FILE_PATH_CHANGES
 SHELL_CHECK			?= $(abs_top_srcdir)/scripts/checkbashisms.pl --force --extra
 SHELL_CHECK_TARGETS		?= $(addprefix check-,$(notdir $(sort $(wildcard $(abs_srcdir)/*.sh))))
 
