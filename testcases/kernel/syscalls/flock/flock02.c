30f1849901b1e4fc5d0b5fe0e6ace5bd46f7135c
Avinesh Kumar <akumar@suse.de>
2025-01-15 15:54:48 +0100

    flock02: Add test for EWOULDBLOCK errno
    Also fix a make check warning.
    
    Link: https://lore.kernel.org/ltp/20250115145449.3850-1-akumar@suse.de/
    Reviewed-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Signed-off-by: Avinesh Kumar <akumar@suse.de>
    Signed-off-by: Yang Xu <xuyang2018.jy@fujitsu.com>

--- testcases/kernel/syscalls/flock/flock02.c
+++ testcases/kernel/syscalls/flock/flock02.c
@@ -13,6 +13,7 @@
  * - EBADF if the file descriptor is invalid
  * - EINVAL if the argument operation does not include LOCK_SH,LOCK_EX,LOCK_UN
  * - EINVAL if an invalid combination of locking modes is used i.e LOCK_SH with LOCK_EX
+ * - EWOULDBLOCK if the file is locked and the LOCK_NB flag was selected
  */
 
 #include <errno.h>
@@ -31,13 +32,21 @@ static struct tcase {
 	{&badfd, LOCK_SH, EBADF},
 	{&fd, LOCK_NB, EINVAL},
 	{&fd, LOCK_SH | LOCK_EX, EINVAL},
+	{&fd, LOCK_NB | LOCK_EX, EWOULDBLOCK}
 };
 
-static void verify_flock(unsigned n)
+static void verify_flock(unsigned int n)
 {
+	int fd2 = -1;
 	struct tcase *tc = &tcases[n];
 
 	fd = SAFE_OPEN("testfile", O_RDWR);
+
+	if (tc->exp_err == EWOULDBLOCK) {
+		fd2 = SAFE_OPEN("testfile", O_RDWR);
+		flock(fd2, LOCK_EX);
+	}
+
 	TEST(flock(*tc->fd, tc->operation));
 	if (TST_RET == 0) {
 		tst_res(TFAIL | TTERRNO, "flock() succeeded unexpectedly");
@@ -53,6 +62,8 @@ static void verify_flock(unsigned n)
 	}
 
 	SAFE_CLOSE(fd);
+	if (fd2 != -1)
+		SAFE_CLOSE(fd2);
 }
 
 static void setup(void)
