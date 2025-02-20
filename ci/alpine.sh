linux-test-project/ltp

commit 7bcf104564e15054ac580bc0d5a9e9754be4c71e

ci: Don't install asciidoc{,tor}
It was removed from GitHub Actions build previous commit.

Link: https://lore.kernel.org/ltp/20250207105551.1736356-4-pvorel@suse.cz/
Acked-by: Andrea Cervesato <andrea.cervesato@suse.com>
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ci/alpine.sh
@@ -6,8 +6,6 @@ apk update
 
 apk add \
 	acl-dev \
-	asciidoc \
-	asciidoctor \
 	autoconf \
 	automake \
 	clang \

ci/debian.sh
@@ -35,9 +35,6 @@ pkg_minimal="
 
 pkg_nonessential="
 	acl-dev
-	asciidoc-base
-	asciidoc-dblatex
-	asciidoctor
 	libacl1-dev
 	libaio-dev
 	libcap-dev
@@ -50,9 +47,6 @@ pkg_nonessential="
 	libssl-dev
 "
 
-# Missing on Ubuntu 18.04 LTS (Bionic Beaver)
-pkg_maybe_nonessential="ruby-asciidoctor-pdf"
-
 case "$ACTION" in
 	minimal)
 		echo "=== Installing only minimal dependencies ==="
@@ -61,12 +55,10 @@ case "$ACTION" in
 	remove-nonessential)
 		echo "=== Make sure devel libraries are removed ==="
 		$remove $pkg_nonessential
-		$remove $pkg_maybe_nonessential || true
 		;;
 	*)
 		echo "=== Installing dependencies ==="
 		$install $pkg_minimal $pkg_nonessential
-		$install $pkg_maybe_nonessential || true
 		;;
 esac
 

ci/fedora.sh
@@ -5,7 +5,6 @@
 yum="yum -y install --skip-broken"
 
 $yum \
-	asciidoc \
 	autoconf \
 	automake \
 	make \
@@ -24,4 +23,3 @@ $yum \
 
 # CentOS 8 fixes
 $yum libmnl-devel || $yum libmnl
-$yum rubygem-asciidoctor || true

ci/tumbleweed.sh
@@ -5,7 +5,6 @@
 zyp="zypper --non-interactive install --force-resolution --no-recommends"
 
 $zyp \
-	asciidoc \
 	autoconf \
 	automake \
 	clang \
@@ -29,5 +28,3 @@ $zyp \
 	lsb-release \
 	perl-JSON \
 	pkg-config
-
-$zyp ruby2.7-rubygem-asciidoctor || $zyp ruby2.5-rubygem-asciidoctor || true

commit d8ee285915c1cd6e516c96cfb98efaaf7c649a9b

ci: Don't install perl for metadata parsing
Metadata parsing was rewritten into sphinx readthedocs (hosted python),
these modules aren't needed any more.

Fixes: 7bcf104564 ("ci: Don't install asciidoc{,tor}")
Signed-off-by: Petr Vorel <pvorel@suse.cz>

ci/alpine.sh
@@ -23,7 +23,6 @@ apk add \
 	musl-dev \
 	numactl-dev \
 	openssl-dev \
-	perl-json \
 	pkgconfig
 
 cat /etc/os-release

ci/debian.sh
@@ -38,7 +38,6 @@ pkg_nonessential="
 	libacl1-dev
 	libaio-dev
 	libcap-dev
-	libjson-perl
 	libkeyutils-dev
 	libnuma-dev
 	libmnl-dev

ci/fedora.sh
@@ -16,8 +16,6 @@ $yum \
 	numactl-devel \
 	libtirpc \
 	libtirpc-devel \
-	perl-JSON \
-	perl-libwww-perl \
 	pkg-config \
 	redhat-lsb-core
 

ci/tumbleweed.sh
@@ -26,5 +26,4 @@ $zyp \
 	libtirpc-devel \
 	linux-glibc-devel \
 	lsb-release \
-	perl-JSON \
 	pkg-config