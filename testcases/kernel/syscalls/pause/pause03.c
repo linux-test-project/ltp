00f711ce1c1fdb2cd337043e10c4b7b6817bc157
Ricardo B. Marlière <rbm@suse.com>
2025-02-24 15:53:05 -0300

    syscalls/pause03: Refactor into new API
    Signed-off-by: Ricardo B. Marlière <rbm@suse.com>

--- testcases/kernel/syscalls/pause/pause03.c
+++ testcases/kernel/syscalls/pause/pause03.c
@@ -1,104 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright (c) International Business Machines  Corp., 2001
- *  07/2001 Ported by Wayne Boyer
- *
- * This program is free software;  you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY;  without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- * the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program;  if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * 07/2001 Ported by Wayne Boyer
+ * Copyright (c) 2025 Ricardo B. Marlière <rbm@suse.com>
  */
-/*
- * Test Description:
- *  pause() does not return due to receipt of SIGKILL signal and specified
- *  process should be terminated.
+
+/*\
+ * Verifies that pause() does not return after proccess receives a SIGKILL signal.
  */
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/wait.h>
 
-#include "test.h"
-#include "safe_macros.h"
-
-static pid_t cpid;
-
-char *TCID = "pause03";
-int TST_TOTAL = 1;
-
-static void do_child(void);
-static void setup(void);
-static void cleanup(void);
-
-int main(int ac, char **av)
-{
-	int lc;
-	int status;
-
-	tst_parse_opts(ac, av, NULL, NULL);
-
-	setup();
-
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-		tst_count = 0;
-
-		if ((cpid = tst_fork()) == -1)
-			tst_brkm(TBROK | TERRNO, NULL, "fork() failed");
-
-		if (cpid == 0)
-			do_child();
-
-		TST_PROCESS_STATE_WAIT(cleanup, cpid, 'S');
-
-		kill(cpid, SIGKILL);
-
-		SAFE_WAIT(NULL, &status);
-
-		if (WIFSIGNALED(status) && WTERMSIG(status) == SIGKILL) {
-			tst_resm(TPASS, "pause() did not return after SIGKILL");
-			continue;
-		}
-
-		if (WIFSIGNALED(status)) {
-			tst_resm(TFAIL, "child killed by %s unexpectedly",
-			         tst_strsig(WTERMSIG(status)));
-			continue;
-		}
-
-		tst_resm(TFAIL, "child exited with %i", WEXITSTATUS(status));
-	}
-
-	cleanup();
-	tst_exit();
-
-}
+#include "tst_test.h"
 
 void do_child(void)
 {
-	TEST(pause());
-
-	tst_resm(TFAIL, "Unexpected return from pause()");
-
-	exit(0);
+	TST_CHECKPOINT_WAKE(0);
+	pause();
+	tst_res(TFAIL, "pause() returned after SIGKILL");
 }
 
-void setup(void)
+void run(void)
 {
-	tst_sig(FORK, DEF_HANDLER, cleanup);
+	int status;
+	pid_t pid;
 
-	TEST_PAUSE;
+	pid = SAFE_FORK();
+	if (!pid) {
+		do_child();
+		exit(0);
+	}
+
+	TST_CHECKPOINT_WAIT(0);
+	TST_PROCESS_STATE_WAIT(pid, 'S', 10000);
+	SAFE_KILL(pid, SIGKILL);
+	SAFE_WAITPID(pid, &status, 0);
+
+	if (WIFSIGNALED(status) && WTERMSIG(status) == SIGKILL)
+		tst_res(TPASS, "pause() killed by SIGKILL");
+	else
+		tst_res(TFAIL, "Child %s", tst_strstatus(status));
 }
 
-
-void cleanup(void)
-{
-	kill(cpid, SIGKILL);
-}
+static struct tst_test test = {
+	.test_all = run,
+	.needs_checkpoints = 1,
+	.forks_child = 1,
+};
