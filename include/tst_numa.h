e19f6f4d9b9f576247a7fa3a598980fdca4c9f87
Petr Vorel <pvorel@suse.cz>
2025-03-28 11:47:37 +0100

    tst_numa.h: Convert doc to kerneldoc
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- include/tst_numa.h
+++ include/tst_numa.h
@@ -8,51 +8,56 @@
 #include <string.h>
 
 /**
- * Numa nodemap.
+ * struct tst_nodemap - Numa nodemap.
+ *
+ * @cnt: Number of nodes in map.
+ * @counters: Page allocation counters.
+ * @map: Array of numa ids.
  */
 struct tst_nodemap {
-        /** Number of nodes in map */
 	unsigned int cnt;
-	/** Page allocation counters */
 	unsigned int *counters;
-	/** Array of numa ids */
 	unsigned int map[];
 };
 
 /**
- * Clears numa counters. The counters are lazy-allocated on first call of this function.
+ * tst_nodemap_reset_counters() - Clears numa counters. The counters are lazy-allocated on first call of this function.
  *
- * @nodes Numa nodemap.
+ * @nodes: Numa nodemap.
  */
 void tst_nodemap_reset_counters(struct tst_nodemap *nodes);
 
 /**
- * Prints pages allocated per each node.
+ * tst_nodemap_print_counters() - Prints pages allocated per each node.
  *
- * @nodes Numa nodemap.
+ * @nodes: Numa nodemap.
  */
 void tst_nodemap_print_counters(struct tst_nodemap *nodes);
 
 /**
- * Returns a name for a mempolicy/mbind mode.
+ * tst_mempolicy_mode_name() - Returns a name for a mempolicy/mbind mode.
  *
- * @mode Numa mempolicy mode.
+ * @mode: Numa mempolicy mode.
+ *
+ * return: a name for a mempolicy/mbind mode.
  */
 const char *tst_mempolicy_mode_name(int mode);
 
 /**
- * Maps pages into memory, if path is NULL the mapping is anonymous otherwise is backed by the file.
+ * tst_numa_map() - Maps pages into memory, if path is NULL the mapping is anonymous otherwise is backed by the file.
  *
- * @path Path to a file, if not NULL mapping is file based.
- * @size Mapping size.
+ * @path: Path to a file, if not NULL mapping is file based.
+ * @size: Mapping size.
+ *
+ * return: a pointer to a mapped file.
  */
 void *tst_numa_map(const char *path, size_t size);
 
 /**
- * Writes to memory in order to get the pages faulted.
+ * tst_numa_fault() - Writes to memory in order to get the pages faulted.
  *
- * @ptr Start of the mapping.
- * @size Size of the mapping.
+ * @ptr: Start of the mapping.
+ * @size: Size of the mapping.
  */
 static inline void tst_numa_fault(void *ptr, size_t size)
 {
@@ -60,10 +65,10 @@ static inline void tst_numa_fault(void *ptr, size_t size)
 }
 
 /**
- * Frees the memory.
+ * tst_numa_unmap() - Frees the memory.
  *
- * @ptr Start of the mapping.
- * @size Size of the mapping.
+ * @ptr: Start of the mapping.
+ * @size: Size of the mapping.
  */
 static inline void tst_numa_unmap(void *ptr, size_t size)
 {
@@ -71,24 +76,29 @@ static inline void tst_numa_unmap(void *ptr, size_t size)
 }
 
 /**
+ * tst_nodemap_count_pages() - Check which numa node resides each page.
+ *
  * Check on which numa node resides each page of the mapping starting at ptr
  * and continuing pages long and increases nodemap counters accordingly.
  *
- * @nodes Nodemap with initialized counters.
- * @ptr   Pointer to start of a mapping.
- * @size  Size of the mapping.
+ * @nodes: Nodemap with initialized counters.
+ * @ptr:   Pointer to start of a mapping.
+ * @size:  Size of the mapping.
  */
 void tst_nodemap_count_pages(struct tst_nodemap *nodes, void *ptr, size_t size);
 
 /**
- * Frees nodemap.
+ * tst_nodemap_free() - Frees nodemap.
  *
- * @nodes Numa nodemap to be freed.
+ * @nodes: Numa nodemap to be freed.
  */
 void tst_nodemap_free(struct tst_nodemap *nodes);
 
 /**
- * Bitflags for tst_get_nodemap() function.
+ * enum tst_numa_types - Bitflags for tst_get_nodemap() function.
+ *
+ * @TST_NUMA_ANY: general NUMA node.
+ * @TST_NUMA_MEM: NUMA memory node.
  */
 enum tst_numa_types {
 	TST_NUMA_ANY = 0x00,
@@ -96,15 +106,15 @@ enum tst_numa_types {
 };
 
 /**
- * Allocates and returns numa node map, which is an array of numa nodes which
+ * tst_get_nodemap() - Allocates and returns numa node map, which is an array of numa nodes which
  * contain desired resources e.g. memory.
  *
- * @type       Bitflags of enum tst_numa_types specifying desired resources.
- * @min_mem_kb Minimal free RAM on memory nodes, if given node has less than
+ * @type:       Bitflags of enum tst_numa_types specifying desired resources.
+ * @min_mem_kb: Minimal free RAM on memory nodes, if given node has less than
  *             requested amount of free+buffers memory it's not included in
  *             the resulting list of nodes.
  *
- * @return On success returns allocated and initialized struct tst_nodemap which contains
+ * return: On success returns allocated and initialized struct tst_nodemap which contains
  *         array of numa node ids that contains desired resources.
  */
 struct tst_nodemap *tst_get_nodemap(int type, size_t min_mem_kb);
