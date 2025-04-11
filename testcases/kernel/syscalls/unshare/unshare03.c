fc8be6ed405d3a949161c19b917e79ade969a505
lufei <lufei@uniontech.com>
2025-04-11 17:30:49 +0800

    unshare03: Simplify EMFILE test by using fixed fd limit (sizeof(long) * 8)
    This patch simplifies the unshare03 test by removing the dependency
    on /proc/sys/fs/nr_open and RLIMIT_NOFILE adjustments.
    
    Instead of dynamically increasing the open file descriptor limit, we
    use a fixed small value: sizeof(long) * 8 (typically 64). This value
    corresponds to the number of bits in a long integer and is commonly
    used as a minimal and safe bound for file descriptors in low-level
    system programming (e.g., fd_set for select()).
    
    By duplicating an existing file descriptor to nr_open + 1, we simulate
    a situation where the process is close to exhausting its available file
    descriptors. This triggers EMFILE when unshare(CLONE_FILES) is called,
    without relying on privileged operations such as modifying rlimits or
    system-wide parameters.
    
    This change improves test stability, and avoid hard limit lead to dup2
    ENOMEM error which brings the result to TBROK ona small RAM system.
    
    Signed-off-by: lufei <lufei@uniontech.com>
    Signed-off-by: Li Wang <liwang@redhat.com>
    Acked-by: Jan Stancek <jstancek@redhat.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/unshare/unshare03.c
+++ testcases/kernel/syscalls/unshare/unshare03.c
@@ -17,44 +17,25 @@
 #include "lapi/sched.h"
 
 #define FS_NR_OPEN "/proc/sys/fs/nr_open"
-#define NR_OPEN_LIMIT 1024
-#define NR_OPEN_DUP 64
 
 #ifdef HAVE_UNSHARE
 
 static void run(void)
 {
-	int nr_open;
-	int nr_limit;
-	struct rlimit rlimit;
 	struct tst_clone_args args = {
 		.flags = CLONE_FILES,
 		.exit_signal = SIGCHLD,
 	};
 
-	SAFE_FILE_SCANF(FS_NR_OPEN, "%d", &nr_open);
-	tst_res(TDEBUG, "Maximum number of file descriptors: %d", nr_open);
+	int nr_open = sizeof(long) * 8;
 
-	nr_limit = nr_open + NR_OPEN_LIMIT;
-	SAFE_FILE_PRINTF(FS_NR_OPEN, "%d", nr_limit);
-
-	SAFE_GETRLIMIT(RLIMIT_NOFILE, &rlimit);
-
-	rlimit.rlim_cur = nr_limit;
-	rlimit.rlim_max = nr_limit;
-
-	SAFE_SETRLIMIT(RLIMIT_NOFILE, &rlimit);
-	tst_res(TDEBUG, "Set new maximum number of file descriptors to : %d",
-		nr_limit);
-
-	SAFE_DUP2(2, nr_open + NR_OPEN_DUP);
+	SAFE_DUP2(2, nr_open + 1);
 
 	if (!SAFE_CLONE(&args)) {
 		SAFE_FILE_PRINTF(FS_NR_OPEN, "%d", nr_open);
 		TST_EXP_FAIL(unshare(CLONE_FILES), EMFILE);
 		exit(0);
 	}
-
 }
 
 static void setup(void)
