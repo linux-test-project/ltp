e7a3ad1a3963136eac7957ab5be96a95028beed5
Ricardo B. Marlière <rbm@suse.com>
2025-06-16 12:47:44 -0300

    syscalls/kill08: Convert to new API
    Signed-off-by: Ricardo B. Marlière <rbm@suse.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/syscalls/kill/kill08.c
+++ testcases/kernel/syscalls/kill/kill08.c
@@ -1,167 +1,53 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2001
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) IBM  Corp., 07/2001 Ported by Wayne Boyer
+ * Copyright (c) 2025 SUSE LLC Ricardo B. Marlière <rbm@suse.com>
  */
 
-/*
- * NAME
- *	kill08.c
- *
- * DESCRIPTION
- *	Test case to check the basic functionality of kill() when kill an
- *	entire process group.
- *
- * ALGORITHM
- *	call setup
- *	loop if the -i option was given
- *	fork 5 childeren
- *	execute the kill system call
- *	check the return value
- *	if return value is -1
- *		issue a FAIL message, break remaining tests and cleanup
- *	if we are doing functional testing
- *		if the processes were terminated with the expected signal.
- *			issue a PASS message
- *		otherwise
- *			issue a FAIL message
- *	call cleanup
- *
- * USAGE
- *  kill08 [-c n] [-f] [-i n] [-I x] [-P x] [-t]
- *     where,  -c n : Run n copies concurrently.
- *             -f   : Turn off functionality Testing.
- *             -i n : Execute test n times.
- *             -I x : Execute test for x seconds.
- *             -P x : Pause for x seconds between iterations.
- *             -t   : Turn on syscall timing.
- *
- * HISTORY
- *	07/2001 Ported by Wayne Boyer
- *
- * RESTRICTIONS
- *	This test should be run as a non-root user.
+/*\
+ * Test checks the basic functionality of kill() when killing an
+ * entire process group.
  */
 
-#include "test.h"
-
-#include <signal.h>
-#include <errno.h>
-#include <sys/wait.h>
-
-void cleanup(void);
-void setup(void);
-void do_child(void);
-
-char *TCID = "kill08";
-int TST_TOTAL = 1;
+#include "tst_test.h"
 
 #define TEST_SIG SIGKILL
 
-int main(int ac, char **av)
+static void run(void)
 {
-	int lc;
-	pid_t pid1, pid2;
-	int exno, status, nsig, i;
+	pid_t pid;
+	int status, nsig, i;
 
-	tst_parse_opts(ac, av, NULL, NULL);
+	/*
+	 * Fork a process and set the process group so that
+	 * it is different from this one.  Fork 5 more children.
+	 */
+	pid = SAFE_FORK();
+	if (!pid) {
+		setpgrp();
+		for (i = 0; i < 5; i++)
+			if (!SAFE_FORK())
+				pause();
 
-	setup();		/* global setup */
-
-	/* The following loop checks looping state if -i option given */
-	for (lc = 0; TEST_LOOPING(lc); lc++) {
-
-		/* reset tst_count in case we are looping */
-		tst_count = 0;
-		status = 1;
-		exno = 1;
-
-		/* Fork a process and set the process group so that */
-		/* it is different from this one.  Fork 5 more children. */
-
-		pid1 = tst_fork();
-		if (pid1 < 0) {
-			tst_brkm(TBROK, cleanup, "Fork of first child failed");
-		} else if (pid1 == 0) {
-			setpgrp();
-			for (i = 0; i < 5; i++) {
-				pid2 = tst_fork();
-				if (pid2 < 0) {
-					tst_brkm(TBROK, cleanup, "Fork failed");
-				} else if (pid2 == 0) {
-					do_child();
-				}
-			}
-			/* Kill all processes in this process group */
-			TEST(kill(0, TEST_SIG));
-			pause();
-			exit(exno);
-		} else {
-			waitpid(pid1, &status, 0);
-			if (TEST_RETURN != 0) {
-				tst_brkm(TFAIL, cleanup, "%s failed - errno = "
-					 "%d : %s", TCID, TEST_ERRNO,
-					 strerror(TEST_ERRNO));
-			}
-		}
-
-		/*
-		 * Check to see if the process was terminated with the
-		 * expected signal.
-		 */
-		nsig = WTERMSIG(status);
-		if (nsig == TEST_SIG) {
-			tst_resm(TPASS, "received expected signal %d",
-				 nsig);
-		} else {
-			tst_resm(TFAIL,
-				 "expected signal %d received %d",
-				 TEST_SIG, nsig);
-		}
+		/* Kill all processes in this process group */
+		SAFE_KILL(0, TEST_SIG);
+		pause();
 	}
 
-	cleanup();
-	tst_exit();
+	/*
+	 * Check to see if the process was terminated with the
+	 * expected signal.
+	 */
+	SAFE_WAITPID(pid, &status, 0);
+
+	nsig = WTERMSIG(status);
+	if (nsig == TEST_SIG)
+		tst_res(TPASS, "received expected signal %d", nsig);
+	else
+		tst_res(TFAIL, "expected signal %d received %d", TEST_SIG, nsig);
 }
 
-/*
- * do_child()
- */
-void do_child(void)
-{
-	int exno = 1;
-
-	pause();
-	exit(exno);
-}
-
-/*
- * setup() - performs all ONE TIME setup for this test
- */
-void setup(void)
-{
-
-	TEST_PAUSE;
-}
-
-/*
- * cleanup() - performs all the ONE TIME cleanup for this test at completion
- * or premature exit.
- */
-void cleanup(void)
-{
-
-}
+static struct tst_test test = {
+	.test_all = run,
+	.forks_child = 1,
+};
