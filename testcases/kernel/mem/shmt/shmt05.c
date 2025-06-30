62176fed477ba722fe3639459d0dffb570c194d7
Ricardo B. Marlière <rbm@suse.com>
2025-06-30 10:13:11 -0300

    mem: shmt05: Convert to new API
    Signed-off-by: Ricardo B. Marlière <rbm@suse.com>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- testcases/kernel/mem/shmt/shmt05.c
+++ testcases/kernel/mem/shmt/shmt05.c
@@ -1,126 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *
- *   Copyright (c) International Business Machines  Corp., 2002
- *
- *   This program is free software;  you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- *   the GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program;  if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * Copyright (c) International Business Machines  Corp., 2002
+ *	12/20/2002	Port to LTP	robbiew@us.ibm.com
+ *	06/30/2001	Port to Linux	nsharoff@us.ibm.com
+ * Copyright (c) 2025 SUSE LLC Ricardo B. Marlière <rbm@suse.com>
  */
 
-/* 12/20/2002   Port to LTP     robbiew@us.ibm.com */
-/* 06/30/2001   Port to Linux   nsharoff@us.ibm.com */
-
-/*
- * NAME
- *		 shmt05
- *
- * CALLS
- *		 shmctl(2) shmget(2) shmat(2)
- *
- * ALGORITHM
+/*\
  * Create two shared memory segments and attach them to the same process
  * at two different addresses. The addresses DO BUMP into each other.
  * The second attach should Fail.
- *
  */
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
+#include "tst_test.h"
+#include "tst_safe_sysv_ipc.h"
+#include "libnewipc.h"
 #include <sys/shm.h>
-#include <sys/utsname.h>
-#include <errno.h>
-#include <time.h>
 
-/** LTP Port **/
-#include "test.h"
+#define SHMSIZE 16
 
-char *TCID = "shmt05";		/* Test program identifier.    */
-int TST_TOTAL = 2;		/* Total number of test cases. */
-/**************/
-
-key_t key[2];
-
-#define		 SIZE		 (2*SHMLBA)
-
-static int rm_shm(int);
-
-int main(void)
+static void run(void)
 {
 	int shmid, shmid1;
-	char *cp, *cp1;
+	char *cp;
+	key_t key[2];
 
-	srand48((getpid() << 16) + (unsigned)time(NULL));
+	key[0] = GETIPCKEY();
+	key[1] = GETIPCKEY();
 
-	key[0] = (key_t) lrand48();
-	key[1] = (key_t) lrand48();
+	shmid = SAFE_SHMGET(key[0], SHMSIZE, IPC_CREAT | 0666);
+	cp = SAFE_SHMAT(shmid, NULL, 0);
 
-	cp = NULL;
-	cp1 = NULL;
+	shmid1 = SAFE_SHMGET(key[1], SHMSIZE, IPC_CREAT | 0666);
+	TST_EXP_FAIL((long)shmat(shmid, cp + (SHMSIZE / 2), 0), EINVAL);
 
-/*--------------------------------------------------------*/
-
-	if ((shmid = shmget(key[0], SIZE, IPC_CREAT | 0666)) < 0) {
-		perror("shmget");
-		tst_resm(TFAIL,
-			 "Error: shmget: shmid = %d, errno = %d\n",
-			 shmid, errno);
-	} else {
-		cp = shmat(shmid, NULL, 0);
-
-		if (cp == (char *)-1) {
-			tst_resm(TFAIL, "shmat");
-			rm_shm(shmid);
-		}
-	}
-
-	tst_resm(TPASS, "shmget & shmat");
-
-/*--------------------------------------------------------*/
-
-	if ((shmid1 = shmget(key[1], SIZE, IPC_CREAT | 0666)) < 0) {
-		perror("shmget2");
-		tst_resm(TFAIL,
-			 "Error: shmget: shmid1 = %d, errno = %d\n",
-			 shmid1, errno);
-	} else {
-		cp1 = shmat(shmid1, cp + (SIZE / 2), 0);
-		if (cp1 != (char *)-1) {
-			perror("shmat");
-			tst_resm(TFAIL,
-				 "Error: shmat: shmid1 = %d, addr= %p, errno = %d\n",
-				 shmid1, cp1, errno);
-		} else {
-			tst_resm(TPASS, "2nd shmget & shmat");
-		}
-	}
-
-/*------------------------------------------------------*/
-
-	rm_shm(shmid);
-	rm_shm(shmid1);
-
-	tst_exit();
+	SAFE_SHMCTL(shmid, IPC_RMID, NULL);
+	SAFE_SHMCTL(shmid1, IPC_RMID, NULL);
 }
 
-static int rm_shm(int shmid)
-{
-	if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-		perror("shmctl");
-		tst_brkm(TFAIL,
-			 NULL,
-			 "shmctl Failed to remove: shmid = %d, errno = %d\n",
-			 shmid, errno);
-	}
-	return (0);
-}
+static struct tst_test test = {
+	.test_all = run,
+};
