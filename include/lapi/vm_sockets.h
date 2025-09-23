21ac796174e9bb2dcd87d13deb1b6349f4920185
Andrea Cervesato <andrea.cervesato@suse.com>
2025-09-23 09:07:57 +0200

    lapi: add struct sockaddr_vm fallback
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- include/lapi/vm_sockets.h
+++ include/lapi/vm_sockets.h
@@ -7,6 +7,7 @@
 #define LAPI_VM_SOCKETS_H__
 
 #include <sys/socket.h>
+#include "config.h"
 
 #if HAVE_LINUX_VM_SOCKETS_H
 #  include <linux/vm_sockets.h>
@@ -14,6 +15,22 @@
 
 #ifndef VMADDR_CID_LOCAL
 #  define VMADDR_CID_LOCAL 1
+#endif
+
+#ifndef HAVE_STRUCT_SOCKADDR_VM
+struct sockaddr_vm {
+	unsigned short svm_family;
+	unsigned short svm_reserved1;
+	unsigned int svm_port;
+	unsigned int svm_cid;
+	unsigned char svm_flags;
+	unsigned char svm_zero[sizeof(struct sockaddr) -
+			       sizeof(sa_family_t) -
+			       sizeof(unsigned short) -
+			       sizeof(unsigned int) -
+			       sizeof(unsigned int) -
+			       sizeof(unsigned char)];
+};
 #endif
 
 #endif /* LAPI_VM_SOCKETS_H__ */
