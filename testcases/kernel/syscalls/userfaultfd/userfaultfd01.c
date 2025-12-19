df188cd00f3f615e922d8230a741beb2cd6f9bf7
Ricardo Branco <rbranco@suse.de>
2025-12-19 11:24:44 +0100

    userfaultfd: Add test using UFFD_USER_MODE_ONLY
    Signed-off-by: Ricardo Branco <rbranco@suse.de>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Link: https://github.com/linux-test-project/ltp/pull/1280

--- testcases/kernel/syscalls/userfaultfd/userfaultfd01.c
+++ testcases/kernel/syscalls/userfaultfd/userfaultfd01.c
@@ -9,17 +9,38 @@
  * from a different thread.
  */
 
-#include "config.h"
 #include <poll.h>
 #include "tst_test.h"
 #include "tst_safe_macros.h"
 #include "tst_safe_pthread.h"
 #include "lapi/userfaultfd.h"
 
+#define BEFORE_5_11 1
+#define AFTER_5_11 2
+#define DESC(x) .flags = x, .desc = #x
+
+static struct tcase {
+	int flags;
+	const char *desc;
+	int kver;
+} tcases[] = {
+	{ DESC(O_CLOEXEC | O_NONBLOCK) },
+	{ DESC(O_CLOEXEC | O_NONBLOCK | UFFD_USER_MODE_ONLY),  .kver = AFTER_5_11, },
+};
+
 static int page_size;
 static char *page;
 static void *copy_page;
 static int uffd;
+static int kver;
+
+static void setup(void)
+{
+	if (tst_kvercmp(5, 11, 0) >= 0)
+		kver = AFTER_5_11;
+	else
+		kver = BEFORE_5_11;
+}
 
 static void set_pages(void)
 {
@@ -30,10 +51,16 @@ static void set_pages(void)
 			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 }
 
-static void handle_thread(void)
+static void reset_pages(void)
+{
+	SAFE_MUNMAP(page, page_size);
+	SAFE_MUNMAP(copy_page, page_size);
+}
+
+static void *handle_thread(void)
 {
 	static struct uffd_msg msg;
-	struct uffdio_copy uffdio_copy;
+	struct uffdio_copy uffdio_copy = {};
 
 	struct pollfd pollfd;
 	int nready;
@@ -47,7 +74,7 @@ static void handle_thread(void)
 	SAFE_READ(1, uffd, &msg, sizeof(msg));
 
 	if (msg.event != UFFD_EVENT_PAGEFAULT)
-		tst_brk(TBROK | TERRNO, "Received unexpected UFFD_EVENT");
+		tst_brk(TBROK | TERRNO, "Received unexpected UFFD_EVENT %d", msg.event);
 
 	memset(copy_page, 'X', page_size);
 
@@ -56,25 +83,27 @@ static void handle_thread(void)
 	uffdio_copy.dst = (unsigned long) msg.arg.pagefault.address
 			& ~(page_size - 1);
 	uffdio_copy.len = page_size;
-	uffdio_copy.mode = 0;
-	uffdio_copy.copy = 0;
 	SAFE_IOCTL(uffd, UFFDIO_COPY, &uffdio_copy);
 
 	close(uffd);
+	return NULL;
 }
 
-static void run(void)
+static void run(unsigned int i)
 {
 	pthread_t thr;
-	struct uffdio_api uffdio_api;
+	struct uffdio_api uffdio_api = {};
 	struct uffdio_register uffdio_register;
+	struct tcase *tc = &tcases[i];
+
+	if (tc->kver == AFTER_5_11 && kver == BEFORE_5_11)
+		tst_brk(TCONF, "%s requires kernel >= 5.11", tc->desc);
 
 	set_pages();
 
-	uffd = SAFE_USERFAULTFD(O_CLOEXEC | O_NONBLOCK, false);
+	uffd = SAFE_USERFAULTFD(tc->flags, false);
 
 	uffdio_api.api = UFFD_API;
-	uffdio_api.features = 0;
 	SAFE_IOCTL(uffd, UFFDIO_API, &uffdio_api);
 
 	uffdio_register.range.start = (unsigned long) page;
@@ -83,8 +112,7 @@ static void run(void)
 
 	SAFE_IOCTL(uffd, UFFDIO_REGISTER, &uffdio_register);
 
-	SAFE_PTHREAD_CREATE(&thr, NULL,
-			(void * (*)(void *)) handle_thread, NULL);
+	SAFE_PTHREAD_CREATE(&thr, NULL, (void *) handle_thread, NULL);
 
 	char c = page[0xf];
 
@@ -94,9 +122,11 @@ static void run(void)
 		tst_res(TFAIL, "Pagefault not handled!");
 
 	SAFE_PTHREAD_JOIN(thr, NULL);
+	reset_pages();
 }
 
 static struct tst_test test = {
-	.test_all = run,
-	.min_kver = "4.3",
+	.setup = setup,
+	.test = run,
+	.tcnt = ARRAY_SIZE(tcases),
 };
