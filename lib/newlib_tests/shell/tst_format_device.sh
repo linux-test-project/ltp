3fa7ab29fcbb3850394bf16a67e4b541ca306fed
Wei Gao <wegao@suse.com>
2026-01-23 02:36:27 +0000

    ci: Add mount operation for busybox
    Busybox df implementation require mounting (unlike coreutils
    implementation). Following error can be found during alpine ci:
    
    runtest TINFO: * shell/tst_format_device.sh
    tst_format_device 1 TINFO: Running: tst_format_device.sh
    tst_format_device 1 TINFO: Tested kernel: Linux 0ba9f024d6b7 6.11.0-1018-azure #18~24.04.1-Ubuntu SMP Sat Jun 28 04:46:03 UTC 2025 x86_64 Linux
    tst_format_device 1 TINFO: Using /tmp/LTP_tst_format_device.XXXXOajpgN as tmpdir (UNKNOWN filesystem)
    tst_device.c:98: TINFO: Found free device 0 '/dev/loop0'
    tst_format_device 1 TINFO: Formatting ext2 with opts='-b 1024 /dev/loop0 5m'
    tst_format_device 1 TINFO: timeout per run is 0h 5m 0s
    tst_format_device 1 TPASS: device formatted
    df: /dev/loop0: can't find mount point
    tst_format_device 2 TFAIL: df /dev/loop0 | grep -q /dev failed unexpectedly
    tst_format_device 3 TINFO: AppArmor enabled, this may affect test results
    tst_format_device 3 TINFO: it can be disabled with TST_DISABLE_APPARMOR=1 (requires super/root)
    tst_format_device 3 TINFO: loaded AppArmor profiles: none
    
    Signed-off-by: Wei Gao <wegao@suse.com>
    Reviewed-by: Petr Vorel <pvorel@suse.cz>
    Reviewed-by: Li Wang <liwang@redhat.com>

--- lib/newlib_tests/shell/tst_format_device.sh
+++ lib/newlib_tests/shell/tst_format_device.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (c) 2022 Petr Vorel <pvorel@suse.cz>
 
-TST_FORMAT_DEVICE=1
+TST_MOUNT_DEVICE=1
 TST_NEEDS_ROOT=1
 TST_TESTFUNC=test
 TST_CNT=2
