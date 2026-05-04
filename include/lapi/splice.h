99fac721bd49c50fa059716c3856e271f44e0fdb
Andrea Cervesato <andrea.cervesato@suse.com>
2026-05-04 09:50:52 +0200

    lib: Add SAFE_SPLICE() macro
    Add new SAFE_SPLIT() in lapi/splice.h.
    
    Reviewed-by: Cyril Hrubis <chrubis@suse.cz>
    Reviewed-by: LTP AI Reviewer <ltp-ai@noreply.github.com>
    Signed-off-by: Andrea Cervesato <andrea.cervesato@suse.com>

--- include/lapi/splice.h
+++ include/lapi/splice.h
@@ -7,6 +7,13 @@
 #ifndef LAPI_SPLICE_H__
 #define LAPI_SPLICE_H__
 
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE
+#endif
+
+#include <fcntl.h>
+#include <sys/types.h>
+
 #include "config.h"
 #include "lapi/syscalls.h"
 
@@ -18,5 +25,27 @@ static inline ssize_t splice(int fd_in, loff_t *off_in, int fd_out,
 		fd_out, off_out, len, flags);
 }
 #endif
+
+static inline ssize_t safe_splice(const char *file, const int lineno,
+				   int fd_in, loff_t *off_in,
+				   int fd_out, loff_t *off_out,
+				   size_t len, unsigned int flags)
+{
+	ssize_t ret;
+
+	ret = splice(fd_in, off_in, fd_out, off_out, len, flags);
+
+	if (ret < 0) {
+		tst_brk_(file, lineno, TBROK | TERRNO,
+			"splice(%d, %p, %d, %p, %zu, %u) failed",
+			fd_in, off_in, fd_out, off_out, len, flags);
+	}
+
+	return ret;
+}
+
+#define SAFE_SPLICE(fd_in, off_in, fd_out, off_out, len, flags) \
+	safe_splice(__FILE__, __LINE__, (fd_in), (off_in), (fd_out), \
+		    (off_out), (len), (flags))
 
 #endif /* LAPI_SPLICE_H__ */
