aee423e63ab7159ee489b8de462cd433f958e0b3
Petr Vorel <pvorel@suse.cz>
2026-07-10 07:55:50 +0200

    metadata: test.sh: Print the test name and result
    Always print the test name with the result. This helps a readability
    in the CI.
    
        * multiline_macro.c [OK]
        * regression.c [FAIL]
        regression.c output differs!
        --- tmp.json	2026-07-09 14:27:15.590074023 +0200
        +++ regression.c.json	2026-07-09 14:27:06.083967188 +0200
        @@ -6,7 +6,7 @@
    	 ]
    	],
    	"doc": [
        -    "Test for regression group derived from the linux-git tag."
        +    "Test for regression group derived from the foo tag."
    	],
    	"groups": [
    	 "regression"
        * tags.c [OK]
    
    Link: https://lore.kernel.org/ltp/20260710055556.1109220-3-pvorel@suse.cz/
    Reviewed-by: Andrea Cervesato <andrea.cervesato@suse.com>
    Signed-off-by: Petr Vorel <pvorel@suse.cz>

--- metadata/tests/test.sh
+++ metadata/tests/test.sh
@@ -3,13 +3,15 @@
 fail=0
 
 for i in *.c; do
+	printf '* %s ' "$i"
 	../metaparse $i > tmp.json
 	if ! diff tmp.json $i.json >/dev/null 2>&1; then
-		echo "***"
+		echo '[FAIL]'
 		echo "$i output differs!"
 		diff -u tmp.json $i.json
-		echo "***"
 		fail=1
+	else
+		echo '[OK]'
 	fi
 done
 
